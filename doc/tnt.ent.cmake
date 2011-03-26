9ad508e482bfef969bcf182ced1c4ae1d18b522b
Konstantin Osipov <kostja.osipov@gmail.com>
2011-03-27 00:01:02 +0300

    User manual: initial commit

---
+++ doc/tnt.ent.cmake
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!--
+     This file contains all common entities.
+     tnt.ent is built by cmake from tnt.ent.cmake.
+-->
+<!ENTITY tnt_version "@TARANTOOL_VERSION@">
+<!-- Include the standard ISO 8879 publishing entities -->
+<!ENTITY % isopub PUBLIC "ISO 8879:1986//ENTITIES Publishing//EN//XML" "ISOpub.ent" >
+%isopub;
+<!--
+  vim: syntax=dtd
+-->
