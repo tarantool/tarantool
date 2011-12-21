cacf11b107335d69e4276d8b465fa839035c7759
Dmitry Simonenko <pmwkaa@gmail.com>
2011-12-21 17:09:14 +0400

    tarantool_topology.sh: review fixes

--- extra/tarantool_topology.sh
+++ extra/tarantool_topology.sh
@@ -1,24 +1,35 @@
 #!/bin/sh
 
-#
-# Tarantool DB topology setup script
-#
+##
+## Tarantool DB topology setup script
+##
 
-topology_cfg='/usr/local/etc/tarantool_topology.cfg'
+prefix="/usr/local"
+prefix_var="/var"
+prefix_etc="/etc"
+
+topology_cfg="${prefix}/etc/tarantool_topology.cfg"
 topology_exists=0
 topology_count=0
 prompt=1
-ts=`/bin/date +"%Y%m%d-%H%M%S"`
 
-if [ -f $topology_cfg ]; then
-        topology_exists=1
-        topology_count=`cat $topology_cfg`
-        # dont' change topology if it said so in configuration file
-        if [ $topology_count -eq 0 ]; then
-                echo "skipping topology setup"
-                exit 0
-        fi
-fi
+error() {
+	echo "error: $*" 1>&2
+	exit 1
+}
+
+log() {
+	echo "> $*"
+}
+
+toolchain_check() {
+	for tool in $*; do
+		which $tool 1>/dev/null 2>&1
+		if [ $? -gt 0 ]; then
+			error "required utility '$tool' is not found" 
+		fi
+	done
+}
 
 usage() {
         echo "Tarantool DB topology setup script"
@@ -26,6 +37,88 @@ usage() {
         echo "usage: tarantool_topology.sh [-y] <servers>"
         exit 0
 }
+
+backup_instance() {
+        id=$1
+	workdir="${prefix_var}/tarantool$id"
+	workdir_new="${prefix_var}/tarantool${id}_${ts}"
+	config="${prefix}/etc/tarantool$id.cfg"
+	config_new="${prefix}/etc/tarantool${id}-${ts}.cfg"
+
+	log "making instance $id backup"
+
+	# moving old instance directory
+	mv $workdir $workdir_new
+
+	# moving old configuration file
+	mv $config $config_new
+
+	# removing startup and wrapper links
+	rm -f "${prefix_etc}/init.d/tarantool$id"
+	rm -f "${prefix}/bin/tarantool$id.sh"
+}
+
+backup() {
+	log "making backup for $topology_count instances"
+	to=`expr $topology_count - 1`
+	for instance in `seq 0 $to`; do
+		backup_instance $instance
+	done
+}
+
+deploy_instance() {
+	id=$1
+	workdir="${prefix_var}/tarantool$id"
+	config="${prefix}/etc/tarantool$id.cfg"
+
+        log "> deploying instance $id"
+
+	# setting up work environment
+	mkdir $workdir
+	mkdir $workdir/logs
+
+	chown tarantool:tarantool -R $workdir
+
+	# setting up startup snapshot
+	cp "${prefix}/share/tarantool/00000000000000000001.snap" $workdir
+
+	# setting up configuration file
+	cp "${prefix}/etc/tarantool.cfg" $config
+
+	echo "work_dir = \"$workdir\"" >> $config
+	echo "username = \"tarantool\"" >> $config
+	echo "logger = \"cat - >> logs/tarantool.log\"" >> $config
+
+	# setting up wrapper
+	ln -s "${prefix}/bin/tarantool_multi.sh" "/usr/local/bin/tarantool$id.sh"
+	
+	# setting up startup script
+	ln -s "${prefix_etc}/init.d/tarantool" "${prefix_etc}/init.d/tarantool$id"
+}
+
+deploy() {
+	to=`expr $topology_count - 1`
+        for instance in `seq 0 $to`; do
+                deploy_instance $instance
+        done
+}
+
+update() {
+        log "updating topology config"
+        echo $topology_count > $topology_cfg
+}
+
+toolchain_check "date" "expr"
+
+if [ -f $topology_cfg ]; then
+        topology_exists=1
+        topology_count=`cat $topology_cfg`
+        # dont' change topology if it said so in configuration file
+        if [ $topology_count -eq 0 ]; then
+                log "skipping topology setup"
+                exit 0
+        fi
+fi
 
 # processing command line arguments
 #
@@ -50,104 +143,41 @@ if [ $num -eq $num 2> /dev/null ]; then
         isnum=1
 fi
 if [ $isnum -eq 0 ] || [ $num -eq 0 2> /dev/null ]; then
-        echo bad instance number
-        exit 1
+        error "bad instance number"
 fi
+
+# time-stamp
+ts=`/bin/date +"%Y%m%d-%H%M%S"`
 
 # asking permission to continue
 #
 if [ $prompt -eq 1 ]; then
-        echo "About to create new Tarantool DB topology for $num instances."
-        if [ $topology_exists -eq 1 ]; then
-                echo "Old data and configuration will be saved with $ts time-stamp prefix."
-        fi
-        echo "Are you sure? [n/y]"
-        read answer
+	log "About to create new Tarantool DB topology for $num instances."
+	if [ $topology_exists -eq 1 ]; then
+		log "Old data and configuration will be saved with $ts time-stamp prefix."
+	fi
+	log "Are you sure? [n/y]"
+	read answer
         case "$answer" in
                 [Yy]) ;;
                 *)
-			echo "aborting"
+			log "aborting"
 			exit 0
                         ;;
         esac
 fi
 
-backup_instance() {
-        id=$1
-	workdir="/var/tarantool$id"
-	workdir_new="/var/tarantool${id}_${ts}"
-	config="/usr/local/etc/tarantool$id.cfg"
-	config_new="/usr/local/etc/tarantool${id}-${ts}.cfg"
+# stop on error
+set -e
 
-        echo ">> making instance $id backup"
-
-	# moving old instance directory
-	mv $workdir $workdir_new
-
-	# moving old configuration file
-	mv $config $config_new
-
-	# removving startup and wrapper links
-	rm -f "/etc/init.d/tarantool$id"
-	rm -f "/usr/local/bin/tarantool$id.sh"
-}
-
-backup() {
-        echo "making backup for $topology_count instances"
-        to=$(( $topology_count - 1 ))
-        for instance in `seq 0 $to`; do
-                backup_instance $instance
-        done
-}
 if [ $topology_exists -eq 1 ]; then
         backup
 fi
 
+# updating instances count
 topology_count=$num
-
-deploy_instance() {
-	id=$1
-	workdir="/var/tarantool$id"
-	config="/usr/local/etc/tarantool$id.cfg"
-
-        echo ">> deploying instance $id"
-
-	# setting up work environment
-	mkdir $workdir
-	mkdir $workdir/logs
-
-	chown tarantool:tarantool -R $workdir
-
-	# setting up startup snapshot
-	cp "/usr/local/share/tarantool/00000000000000000001.snap" $workdir
-
-	# setting up configuration file
-	cp "/usr/local/etc/tarantool.cfg" $config
-
-	echo "work_dir = \"$workdir\"" >> $config
-	echo "username = \"tarantool\"" >> $config
-	echo "logger = \"cat - >> logs/tarantool.log\"" >> $config
-
-	# setting up wrapper
-	ln -s "/usr/local/bin/tarantool_multi.sh" "/usr/local/bin/tarantool$id.sh"
-	
-	# setting up startup script
-	ln -s "/etc/init.d/tarantool" "/etc/init.d/tarantool$id"
-}
-
-deploy() {
-        to=$(( $topology_count - 1 ))
-        for instance in `seq 0 $to`; do
-                deploy_instance $instance
-        done
-}
-
-update() {
-        echo "updating topology config"
-        echo $topology_count > $topology_cfg
-}
 
 deploy
 update
 
-echo done
+log "done"
