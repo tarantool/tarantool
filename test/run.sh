9fe42da100f70e7a7c5fcc3cb76e54f024d3f9fe
pcherenkov <pcherenkov@gmail.com>
2012-04-19 18:48:00 +0400

    bug741767: added constraint against python 3.x

--- test/run.sh
+++ test/run.sh
@@ -2,11 +2,11 @@
 
 /usr/bin/env python <<__EOB__
 import sys
-if sys.hexversion < 0x02070000:
-    print >>sys.stderr, "Test harness must use python > 2.6"
-    exit(1)
+if sys.hexversion < 0x02070000 or sys.hexversion >= 0x03000000:
+    sys.stderr.write("ERROR: test harness must use python >= 2.6 but not 3.x\n")
+    sys.exit(1)
 else:
-    exit(0)
+    sys.exit(0)
 __EOB__
 
 [ $? -eq 0 ] && ./test-run.py $*
