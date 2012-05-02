f42e8182d2cc6423b0a1c60e16b0fbdf46c9e781
Konstantin Osipov <kostja@tarantool.org>
2012-05-02 22:38:42 +0400

    Fix a gcc warning with the latest libev.

--- third_party/libev/ev.c
+++ third_party/libev/ev.c
@@ -1257,11 +1257,11 @@ typedef struct
   #include "ev_wrap.h"
 
   static struct ev_loop default_loop_struct;
-  EV_API_DECL struct ev_loop *ev_default_loop_ptr = 0; /* needs to be initialised to make it a definition despite extern */
+  EV_API_DEF struct ev_loop *ev_default_loop_ptr = 0; /* needs to be initialised to make it a definition despite extern */
 
 #else
 
-  EV_API_DECL ev_tstamp ev_rt_now = 0; /* needs to be initialised to make it a definition despite extern */
+  EV_API_DEF ev_tstamp ev_rt_now = 0; /* needs to be initialised to make it a definition despite extern */
   #define VAR(name,decl) static decl;
     #include "ev_vars.h"
   #undef VAR
