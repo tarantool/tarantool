9df33348385ceac25a65ee44584258b24a291f2f
Konstantin Osipov <kostja@tarantool.org>
2012-06-08 10:45:23 +0400

    clang-build: review fixes.

--- third_party/gopt/gopt.h
+++ third_party/gopt/gopt.h
@@ -25,12 +25,8 @@ read http://www.purposeful.co.uk/tfl/
 #define GOPT_NOARG  0
 #define GOPT_ARG    2
 
-#ifndef __GNUG__
-	struct goptargs { int k; int f; const char *s; const char*const*l; const char *a; const char *h; };
-	#define gopt_start(...)  (const void*)(const struct goptargs[]){ __VA_ARGS__, {0, 0, NULL, NULL, NULL, NULL}}
-#else
-#define gopt_start(...)  (const void*)( const struct { int k; int f; const char *s; const char*const*l; const char *a; const char *h;}[]){ __VA_ARGS__, {0, 0, NULL, NULL, NULL, NULL}}
-#endif
+struct goptargs { int k; int f; const char *s; const char*const*l; const char *a; const char *h; };
+#define gopt_start(...)  (const void*)(const struct goptargs[]){ __VA_ARGS__, {0, 0, NULL, NULL, NULL, NULL}}
 
 #define gopt_option(k,f,s,l,a,h)    { k, f, s, l, a, h }
 #define gopt_shorts( ... )      (const char*)(const char[]){ __VA_ARGS__, 0 }
