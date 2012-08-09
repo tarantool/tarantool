5eb4492569fced2829863d18aab22cf308d9a1f5
Dmitry Simonenko <pmwkaa@gmail.com>
2012-08-09 19:02:01 +0400

    lua-counter: import of lua box.counter

--- mod/box/box.lua
+++ mod/box/box.lua
@@ -241,6 +241,54 @@ function box.update_ol(space, ops_list, ...)
     return box.process(19, box.pack(format, unpack(args_list)))
 end
 
+--
+-- Simple counter.
+--
+box.counter = {}
+
+--
+-- Increment counter identified by primary key.
+-- Create counter if not exists.
+-- Returns updated value of the counter.
+--
+function box.counter.inc(space, ...)
+    local key = {...}
+    local cnt_index = #key
+
+    local tuple
+    while true do
+        tuple = box.update(space, key, '+p', cnt_index, 1)
+        if tuple ~= nil then break end
+        local data = {...}
+        table.insert(data, 1)
+        tuple = box.insert(space, unpack(data))
+        if tuple ~= nil then break end
+    end
+
+    return box.unpack('i', tuple[cnt_index])
+end
+
+--
+-- Decrement counter identified by primary key.
+-- Delete counter if it decreased to zero.
+-- Returns updated value of the counter.
+--
+function box.counter.dec(space, ...)
+    local key = {...}
+    local cnt_index = #key
+
+    local tuple = box.select(space, 0, ...)
+    if tuple == nil then return 0 end
+    if box.unpack('i', tuple[cnt_index]) == 1 then
+        box.delete(space, ...)
+        return 0
+    else
+        tuple = box.update(space, key, '-p', cnt_index, 1)
+        return box.unpack('i', tuple[cnt_index])
+    end
+end
+
+
 function box.on_reload_configuration()
     local index_mt = {}
     -- __len and __index
