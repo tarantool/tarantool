e0a6e074d5ca061e1100b8966a5e6c1ee2b52c0b
Dmitry Simonenko <pmwkaa@gmail.com>
2012-08-10 15:52:03 +0400

    lua-autoinc: lua box.auto_increment function added

--- mod/box/box.lua
+++ mod/box/box.lua
@@ -241,6 +241,19 @@ function box.update_ol(space, ops_list, ...)
     return box.process(19, box.pack(format, unpack(args_list)))
 end
 
+-- Assumes that spaceno has a TREE int32 (NUM) primary key
+-- inserts a tuple after getting the next value of the
+-- primary key and returns it back to the user
+function box.auto_increment(spaceno, ...)
+    spaceno = tonumber(spaceno)
+    local max_tuple = box.space[spaceno].index[0].idx:max()
+    local max = -1
+    if max_tuple ~= nil then
+        max = box.unpack('i', max_tuple[0])
+    end
+    return box.insert(spaceno, max + 1, ...)
+end
+
 function box.on_reload_configuration()
     local index_mt = {}
     -- __len and __index
