32d416509ff2bca27a436f7220d9b3f1f25aa6d0
Dmitry E. Oboukhov <unera@debian.org>
2012-08-21 22:37:54 +0400

    uses libuuid as depends

--- mod/box/box_lua_uuid.m
+++ mod/box/box_lua_uuid.m
@@ -1,4 +1,4 @@
-#include <dlfcn.h>
+#include <uuid/uuid.h>
 #include <string.h>
 #include <stdio.h>
 #include "box_lua_uuid.h"
@@ -6,39 +6,11 @@
 #include "lauxlib.h"
 #include "lualib.h"
 
-
 #define UUIDLIB_SOVERSION	"1"
 #define LIBNAME			( "libuuid.so." UUIDLIB_SOVERSION )
 
-typedef unsigned char uuid_t[16];
-
-static void (*uuid_generate)(uuid_t out);
-static char *error;
-
-static void check_lib(struct lua_State *L) {
-	
-	if (uuid_generate)
-		return;
-	
-	if (error)
-		luaL_error( L, "box.uuid() error: %s", error );
-
-	void *dl = dlopen(LIBNAME, RTLD_LAZY);
-	if (!dl) {
-		error = strdup( dlerror() );
-		luaL_error( L, "box.uuid() error:  %s", error );
-	}
-
-	uuid_generate = dlsym(dl, "uuid_generate");
-	if (!uuid_generate) {
-		error = strdup( dlerror() );
-		luaL_error( L, "box.uuid() error: %s", error );
-	}
-}
-
 
 int lbox_uuid(struct lua_State *L) {
-	check_lib(L);
 
 	uuid_t uuid;
 	uuid_generate(uuid);
@@ -52,9 +24,6 @@ int lbox_uuid_hex(struct lua_State *L) {
 	unsigned i;
 	char uuid_hex[ sizeof(uuid_t) * 2 + 1 ];
 
-
-	check_lib(L);
-	
 	uuid_t uuid;
 	uuid_generate(uuid);
 
