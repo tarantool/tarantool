f8c0f6f92b22ac9032a4a4256ac57f72f57d7036
Dmitry E. Oboukhov <unera@debian.org>
2012-10-04 17:19:29 +0400

    code style, copyrights

--- src/fiber_ifc_lua.m
+++ src/fiber_ifc_lua.m
@@ -1,3 +1,32 @@
+/*
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above
+ *    copyright notice, this list of conditions and the
+ *    following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials
+ *    provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * <COPYRIGHT HOLDER> OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
 #include "fiber_ifc_lua.h"
 #include "fiber_ifc.h"
 #include "lua.h"
@@ -184,7 +213,8 @@ lbox_fiber_channel_gc(struct lua_State *L)
 }
 
 
-static int lbox_fiber_channel_isfull(struct lua_State *L)
+static int
+lbox_fiber_channel_isfull(struct lua_State *L)
 {
 	say_info(":%s()", __func__);
 	if (lua_gettop(L) != 1 || !lua_isuserdata(L, 1))
@@ -197,7 +227,8 @@ static int lbox_fiber_channel_isfull(struct lua_State *L)
 	return 1;
 }
 
-static int lbox_fiber_channel_isempty(struct lua_State *L)
+static int
+lbox_fiber_channel_isempty(struct lua_State *L)
 {
 	say_info(":%s()", __func__);
 	if (lua_gettop(L) != 1 || !lua_isuserdata(L, 1))
@@ -210,7 +241,8 @@ static int lbox_fiber_channel_isempty(struct lua_State *L)
 	return 1;
 }
 
-static int lbox_fiber_channel_put(struct lua_State *L)
+static int
+lbox_fiber_channel_put(struct lua_State *L)
 {
 	say_info(":%s()", __func__);
 	if (lua_gettop(L) != 2 || !lua_isuserdata(L, -2))
@@ -246,7 +278,8 @@ static int lbox_fiber_channel_put(struct lua_State *L)
 	return 1;
 }
 
-static int lbox_fiber_channel_get(struct lua_State *L)
+static int
+lbox_fiber_channel_get(struct lua_State *L)
 {
 	say_info(":%s()", __func__);
 	if (lua_gettop(L) != 1 || !lua_isuserdata(L, -1))
@@ -374,6 +407,5 @@ fiber_ifc_lua_init(struct lua_State *L)
 		{NULL, NULL}
 	};
 	tarantool_lua_register_type(L, channel_lib, channel_meta);
-
 }
 
