57ca17c8129e34cd337696d93283ed0087fff7a4
Konstantin Osipov <kostja@tarantool.org>
2012-10-19 14:35:51 +0400

    Fix the version in the user guide.

--- doc/tnt.ent.cmake
+++ doc/tnt.ent.cmake
@@ -3,7 +3,7 @@
      This file contains all common entities.
      tnt.ent is built by cmake from tnt.ent.cmake.
 -->
-<!ENTITY tnt_version "@TARANTOOL_VERSION@">
+<!ENTITY tnt_version "@PACKAGE_VERSION@">
 <!-- Include the standard ISO 8879 publishing entities -->
 <!ENTITY % isopub PUBLIC "ISO 8879:1986//ENTITIES Publishing//EN//XML"
                          "http://www.w3.org/2003/entities/iso8879/isopub.ent">
