961bfe80c233e388bdf93a2e85f8100455c1d089
Roman Tsisyk <roman@tsisyk.com>
2012-12-05 18:50:20 +0400

    Reworked box.unpack

--- src/pickle.m
+++ src/pickle.m
@@ -99,58 +99,15 @@ read_u(64)
 u32
 read_varint32(struct tbuf *buf)
 {
-	u8 *b = buf->data;
-	int size = buf->size;
+	void *b = buf->data;
+	u32 ret = load_varint32_s(&b, buf->size);
 
-	if (size < 1) {
-		tnt_raise(IllegalParams, :"packet too short (expected 1 byte)");
-	}
-	if (!(b[0] & 0x80)) {
-		buf->data += 1;
-		buf->capacity -= 1;
-		buf->size -= 1;
-		return (b[0] & 0x7f);
-	}
+	size_t read = (b - buf->data);
+	buf->data = b;
+	buf->capacity -= read;
+	buf->size -= read;
 
-	if (size < 2)
-		tnt_raise(IllegalParams, :"packet too short (expected 2 bytes)");
-	if (!(b[1] & 0x80)) {
-		buf->data += 2;
-		buf->capacity -= 2;
-		buf->size -= 2;
-		return (b[0] & 0x7f) << 7 | (b[1] & 0x7f);
-	}
-	if (size < 3)
-		tnt_raise(IllegalParams, :"packet too short (expected 3 bytes)");
-	if (!(b[2] & 0x80)) {
-		buf->data += 3;
-		buf->capacity -= 3;
-		buf->size -= 3;
-		return (b[0] & 0x7f) << 14 | (b[1] & 0x7f) << 7 | (b[2] & 0x7f);
-	}
-
-	if (size < 4)
-		tnt_raise(IllegalParams, :"packet too short (expected 4 bytes)");
-	if (!(b[3] & 0x80)) {
-		buf->data += 4;
-		buf->capacity -= 4;
-		buf->size -= 4;
-		return (b[0] & 0x7f) << 21 | (b[1] & 0x7f) << 14 |
-			(b[2] & 0x7f) << 7 | (b[3] & 0x7f);
-	}
-
-	if (size < 5)
-		tnt_raise(IllegalParams, :"packet too short (expected 5 bytes)");
-	if (!(b[4] & 0x80)) {
-		buf->data += 5;
-		buf->capacity -= 5;
-		buf->size -= 5;
-		return (b[0] & 0x7f) << 28 | (b[1] & 0x7f) << 21 |
-			(b[2] & 0x7f) << 14 | (b[3] & 0x7f) << 7 | (b[4] & 0x7f);
-	}
-
-	tnt_raise(IllegalParams, :"incorrect BER format");
-	return 0;
+	return ret;
 }
 
 u32
