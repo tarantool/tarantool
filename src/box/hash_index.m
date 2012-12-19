tarantool/tarantool

commit 7fb93f8b0c23148132bb2aa847324f826c9e2374

Pick up some of the less intrusive changes from const-modifier branch.

src/box/hash_index.m
@@ -249,7 +249,7 @@ hash_iterator_lstr_eq(struct iterator *it)
 /* {{{ Hash32Index ************************************************/
 
 static inline struct mh_i32ptr_node_t
-int32_key_to_node(void *key)
+int32_key_to_node(const void *key)
 {
 	u32 key_size = load_varint32(&key);
 	if (key_size != 4)
@@ -296,7 +296,7 @@ int32_tuple_to_node(struct tuple *tuple, struct key_def *key_def)
 	return mh_size(int_hash);
 }
 
-- (struct tuple *) findByKey: (void *) key :(int) part_count
+- (struct tuple *) findByKey: (const void *) key :(int) part_count
 {
 	assert(key_def->is_unique);
 	check_key_parts(key_def, part_count, false);
@@ -416,7 +416,7 @@ int32_tuple_to_node(struct tuple *tuple, struct key_def *key_def)
 /* {{{ Hash64Index ************************************************/
 
 static inline struct mh_i64ptr_node_t
-int64_key_to_node(void *key)
+int64_key_to_node(const void *key)
 {
 	u32 key_size = load_varint32(&key);
 	if (key_size != 8)

include/assoc.h
@@ -71,7 +71,7 @@ struct mh_i64ptr_node_t {
 /*
  * Map: (char *) => (void *)
  */
-static inline int lstrcmp(void *a, void *b)
+static inline int lstrcmp(const void *a, const void *b)
 {
 	unsigned int al, bl;
 
@@ -95,7 +95,7 @@ struct mh_lstrptr_node_t {
 static inline u32
 mh_strptr_hash(const mh_node_t *a, mh_hash_arg_t arg) {
 	(void) arg;
-	void *_k = (a->key);
+	const void *_k = (a->key);
 	const u32 l = load_varint32(&_k);
 	return (u32) MurmurHash2(_k, l, 13);
 }

include/iobuf.h
@@ -154,7 +154,7 @@ obuf_book(struct obuf *obuf, size_t size);
 
 /** Append data to the output buffer. */
 void
-obuf_dup(struct obuf *obuf, void *data, size_t size);
+obuf_dup(struct obuf *obuf, const void *data, size_t size);
 
 /**
  * Output buffer savepoint. It's possible to

include/pickle.h
@@ -55,7 +55,7 @@ u32 valid_tuple(struct tbuf *buf, u32 cardinality);
 size_t varint32_sizeof(u32);
 
 static inline u32
-load_varint32_s(void **data, size_t size)
+load_varint32_s(const void **data, size_t size)
 {
 	assert(data != NULL && *data != NULL);
 	const u8 *b = *data;
@@ -106,7 +106,7 @@ load_varint32_s(void **data, size_t size)
 }
 
 static inline u32
-load_varint32(void **data)
+load_varint32(const void **data)
 {
 	return load_varint32_s(data, 5);
 }
@@ -117,9 +117,9 @@ load_varint32(void **data)
  * @returns size of fields data including size of varint data
  */
 inline static size_t
-tuple_range_size(void **begin, void *end, size_t count)
+tuple_range_size(const void **begin, const void *end, size_t count)
 {
-	void *start = *begin;
+	const void *start = *begin;
 	while (*begin < end && count-- > 0) {
 		size_t len = load_varint32(begin);
 		*begin += len;

include/tbuf.h
@@ -70,7 +70,7 @@ static inline void *
 tbuf_end(struct tbuf *tbuf) { return tbuf->data + tbuf->size; }
 
 static inline size_t
-tbuf_unused(struct tbuf *tbuf) { return tbuf->capacity - tbuf->size; }
+tbuf_unused(const struct tbuf *tbuf) { return tbuf->capacity - tbuf->size; }
 
 struct tbuf *tbuf_clone(struct palloc_pool *pool, const struct tbuf *orig);
 struct tbuf *tbuf_split(struct tbuf *e, size_t at);
@@ -87,7 +87,7 @@ void *tbuf_peek(struct tbuf *b, size_t count);
  */
 void tbuf_ltrim(struct tbuf *b, size_t count);
 
-void tbuf_append_field(struct tbuf *b, void *f);
+void tbuf_append_field(struct tbuf *b, const void *f);
 void tbuf_vprintf(struct tbuf *b, const char *format, va_list ap)
 	__attribute__ ((format(FORMAT_PRINTF, 2, 0)));
 void tbuf_printf(struct tbuf *b, const char *format, ...)

src/box/box_lua.m
@@ -150,7 +150,7 @@ lbox_tuple_slice(struct lua_State *L)
 	int stop = end - 1;
 
 	while (field < tuple->data + tuple->bsize) {
-		size_t len = load_varint32((void **) &field);
+		size_t len = load_varint32((const void **) &field);
 		if (fieldno >= start) {
 			lua_pushlstring(L, (char *) field, len);
 			if (fieldno == stop)
@@ -183,8 +183,8 @@ transform_calculate(struct lua_State *L, struct tuple *tuple,
 		    size_t lr[2])
 {
 	/* calculate size of the new tuple */
-	void *tuple_end = tuple->data + tuple->bsize;
-	void *tuple_field = tuple->data;
+	const void *tuple_end = tuple->data + tuple->bsize;
+	const void *tuple_field = tuple->data;
 
 	lr[0] = tuple_range_size(&tuple_field, tuple_end, offset);
 
@@ -321,7 +321,7 @@ tuple_find(struct lua_State *L, struct tuple *tuple, size_t offset,
 		return 0;
 	u8 *field = tuple_field(tuple, idx);
 	while (field < tuple->data + tuple->bsize) {
-		size_t len = load_varint32((void **) &field);
+		size_t len = load_varint32((const void **) &field);
 		if (len == key_size && (memcmp(field, key, len) == 0)) {
 			lua_pushinteger(L, idx);
 			if (!all)
@@ -387,10 +387,10 @@ static int
 lbox_tuple_unpack(struct lua_State *L)
 {
 	struct tuple *tuple = lua_checktuple(L, 1);
-	u8 *field = tuple->data;
+	const u8 *field = tuple->data;
 
 	while (field < tuple->data + tuple->bsize) {
-		size_t len = load_varint32((void **) &field);
+		size_t len = load_varint32((const void **) &field);
 		lua_pushlstring(L, (char *) field, len);
 		field += len;
 	}
@@ -415,7 +415,7 @@ lbox_tuple_index(struct lua_State *L)
 		if (i >= tuple->field_count)
 			luaL_error(L, "%s: index %d is out of bounds (0..%d)",
 				   tuplelib_name, i, tuple->field_count-1);
-		void *field = tuple_field(tuple, i);
+		const void *field = tuple_field(tuple, i);
 		u32 len = load_varint32(&field);
 		lua_pushlstring(L, field, len);
 		return 1;
@@ -474,7 +474,7 @@ lbox_tuple_next(struct lua_State *L)
 	(void)field;
 	assert(field >= tuple->data);
 	if (field < tuple->data + tuple->bsize) {
-		len = load_varint32((void **) &field);
+		len = load_varint32((const void **) &field);
 		lua_pushlightuserdata(L, field + len);
 		lua_pushlstring(L, (char *) field, len);
 		return 2;

src/box/index.h
@@ -29,7 +29,6 @@
  * SUCH DAMAGE.
  */
 #import "object.h"
-
 #include <stdbool.h>
 #include <util.h>
 
@@ -203,7 +202,7 @@ enum dup_replace_mode {
 - (size_t) size;
 - (struct tuple *) min;
 - (struct tuple *) max;
-- (struct tuple *) findByKey: (void *) key :(int) part_count;
+- (struct tuple *) findByKey: (const void *) key :(int) part_count;
 - (struct tuple *) replace: (struct tuple *) old_tuple
 			  :(struct tuple *) new_tuple
 			  :(enum dup_replace_mode) mode;
@@ -218,7 +217,7 @@ enum dup_replace_mode {
 @end
 
 void
-check_key_parts(struct key_def *key_def, int part_count,
+check_key_parts(const struct key_def *key_def, int part_count,
 		bool partial_key_allowed);
 
 uint32_t

src/box/index.m
@@ -45,7 +45,7 @@ STRS(iterator_type, ITERATOR_TYPE);
 /* {{{ Utilities. **********************************************/
 
 void
-check_key_parts(struct key_def *key_def,
+check_key_parts(const struct key_def *key_def,
 		int part_count, bool partial_key_allowed)
 {
 	if (part_count > key_def->part_count)

commit 0e37d3b28258c5766195258e1661b0a64fdaf39b

Fix debug compilation (broken by 7fb93f8b0c23148132bb2aa847324f826c9e2374)

src/box/hash_index.m
@@ -630,7 +630,7 @@ lstrptr_tuple_to_node(struct tuple *tuple, struct key_def *key_def)
 	if (k != mh_end(str_hash))
 		ret = mh_lstrptr_node(str_hash, k)->val;
 #ifdef DEBUG
-	u32 key_size = load_varint32(&key);
+	u32 key_size = load_varint32((const void **) &key);
 	say_debug("HashStrIndex find(self:%p, key:(%i)'%.*s') = %p",
 		  self, key_size, key_size, (u8 *)key, ret);
 #endif