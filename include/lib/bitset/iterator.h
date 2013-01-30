1a5942d4ef622ae9ab8a0a07b4eef75e2f12703e
Roman Tsisyk <roman@tsisyk.com>
2013-01-30 18:30:25 +0400

    libbitset: bitset_expr and bitset_iterator (splited off from bitmap-index)

---
+++ include/lib/bitset/iterator.h
@@ -0,0 +1,130 @@
+#ifndef TARANTOOL_LIB_BITSET_ITERATOR_H_INCLUDED
+#define TARANTOOL_LIB_BITSET_ITERATOR_H_INCLUDED
+
+/*
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above
+ *    copyright notice, this list of conditions and the
+ *    following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials
+ *    provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * <COPYRIGHT HOLDER> OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/**
+ * @file
+ * @brief Iterator for @link bitset @endlink objects with expressions support.
+ *
+ * @link bitset_iterator @endlink is used to iterate over a result of the
+ * evaluation a @link bitset_expr logical expression @endlink on the set
+ * of bitsets. The iterator evaluates expressions on the fly, without producing
+ * temporary bitsets. Each iteration (@link bitset_iterator_next @endlink)
+ * returns the next position where a given expression evaluates to true on a
+ * given set of bitsets.
+ *
+ * @see expr.h
+ */
+
+#include <lib/bitset/bitset.h>
+#include <lib/bitset/expr.h>
+
+/** @cond false **/
+struct bitset_iterator_conj;
+/** @endcond **/
+
+/**
+ * @brief Bitset Iterator
+ */
+struct bitset_iterator {
+	/** @cond false **/
+	size_t size;
+	size_t capacity;
+	struct bitset_iterator_conj *conjs;
+	struct bitset_page *page;
+	struct bitset_page *page_tmp;
+	void *(*realloc)(void *ptr, size_t size);
+	struct bit_iterator page_it;
+	/** @endcond **/
+};
+
+/**
+ * @brief Construct \a it.
+ *
+ * The created iterator must be initialized by
+ * @link bitset_iterator_init @endlink method before first usage.
+ * @param it bitset iterator
+ * @param realloc memory allocator to use
+ */
+void
+bitset_iterator_create(struct bitset_iterator *it,
+		       void *(*realloc)(void *ptr, size_t size));
+
+/**
+ * @brief Destruct \a it.
+ * @param it bitset iterator
+ */
+void
+bitset_iterator_destroy(struct bitset_iterator *it);
+
+/**
+ * @brief Initialize the \a it using \a expr and \a bitsets and rewind the
+ * iterator to the start position.
+ *
+ * @note It is safe to reinitialize an iterator with a new expression and new
+ * bitsets. All internal buffers are safely reused in this case with minimal
+ * number of new allocations.
+ *
+ * @note @a expr object is only used during initialization time and can be
+ * safetly reused or destroyed just after this call.
+ *
+ * @param it bitset iterator
+ * @param expr bitset expression
+ * @param bitsets array of pointers to bitsets that should be used to bind
+ * the expression parameters.
+ * @param size of @a bitsets array
+ * @retval 0 on success
+ * @retval -1 on memory error
+ * @see expr.h
+ */
+int
+bitset_iterator_init(struct bitset_iterator *it, struct bitset_expr *expr,
+		     struct bitset **bitsets, size_t bitsets_size);
+
+/**
+ * @brief Rewind the \a it to the start position.
+ * @param expr bitset expression
+ * @see @link bitset_iterator_init @endlink
+ */
+void
+bitset_iterator_rewind(struct bitset_iterator *it);
+
+/**
+ * @brief Move \a it to a next position
+ * @param expr bitset expression
+ * @return a next offset where the expression evaluates to true or SIZE_MAX
+ * if there is no more bits in the result set.
+ * @see @link bitset_iterator_init @endlink
+ */
+size_t
+bitset_iterator_next(struct bitset_iterator *it);
+
+#endif /* TARANTOOL_LIB_BITSET_ITERATOR_H_INCLUDED */
---
+++ src/lib/bitset/iterator.c
@@ -0,0 +1,399 @@
+/*
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above
+ *    copyright notice, this list of conditions and the
+ *    following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials
+ *    provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * <COPYRIGHT HOLDER> OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <lib/bitset/iterator.h>
+#include <lib/bitset/expr.h>
+#include "page.h"
+
+#include <assert.h>
+
+const size_t ITERATOR_DEFAULT_CAPACITY = 2;
+const size_t ITERATOR_CONJ_DEFAULT_CAPACITY = 32;
+
+struct bitset_iterator_conj {
+	size_t page_first_pos;
+	size_t size;
+	size_t capacity;
+	struct bitset **bitsets;
+	bool *pre_nots;
+	struct bitset_page **pages;
+};
+
+/**
+ * @brief Construct iterator
+ * @param it iterator
+ * @param realloc memory allocator to use
+ */
+void
+bitset_iterator_create(struct bitset_iterator *it,
+		       void *(*realloc)(void *ptr, size_t size))
+{
+	memset(it, 0, sizeof(*it));
+	it->realloc = realloc;
+}
+
+/**
+ * @brief Destroys the @a it object
+ * @param it object
+ * @see bitset_iterator_new
+ */
+void
+bitset_iterator_destroy(struct bitset_iterator *it)
+{
+	for (size_t c = 0; c < it->size; c++) {
+		it->realloc(it->conjs[c].bitsets, 0);
+		it->realloc(it->conjs[c].pre_nots, 0);
+		it->realloc(it->conjs[c].pages, 0);
+	}
+	it->realloc(it->conjs, 0);
+
+	bitset_page_destroy(it->page);
+	bitset_page_destroy(it->page_tmp);
+	it->realloc(it->page, 0);
+	it->realloc(it->page_tmp, 0);
+	memset(it, 0, sizeof(*it));
+}
+
+
+static int
+bitset_iterator_reserve(struct bitset_iterator *it, size_t size)
+{
+	if (size <= it->capacity)
+		return 0;
+
+	size_t capacity = (it->capacity > 0)
+				? it->capacity
+				: ITERATOR_DEFAULT_CAPACITY;
+
+	while (capacity <= size) {
+		capacity *= 2;
+	}
+
+	struct bitset_iterator_conj *conjs =
+			it->realloc(it->conjs, capacity * sizeof(*it->conjs));
+	if (conjs == NULL)
+		return -1;
+
+	memset(conjs + it->capacity, 0,
+	       (capacity - it->capacity) * sizeof(*it->conjs));
+
+	it->conjs = conjs;
+	it->capacity = capacity;
+
+	return 0;
+}
+
+static int
+bitset_iterator_conj_reserve(struct bitset_iterator *it,
+			     struct bitset_iterator_conj *conj, size_t size)
+{
+	if (size <= conj->capacity)
+		return 0;
+
+	size_t capacity = (conj->capacity > 0)
+				? conj->capacity
+				: ITERATOR_CONJ_DEFAULT_CAPACITY;
+
+	while (capacity <= size) {
+		capacity *= 2;
+	}
+
+	struct bitset **bitsets = it->realloc(conj->bitsets,
+					capacity * sizeof(*conj->bitsets));
+	if (bitsets == NULL)
+		goto error_1;
+	bool *pre_nots = it->realloc(conj->pre_nots,
+					capacity * sizeof(*conj->pre_nots));
+	if (pre_nots == NULL)
+		goto error_2;
+	struct bitset_page **pages = it->realloc(conj->pages,
+					capacity * sizeof(*conj->pages));
+	if (pre_nots == NULL)
+		goto error_3;
+
+	memset(bitsets + conj->capacity, 0,
+	       (capacity - conj->capacity) * sizeof(*conj->bitsets));
+	memset(pre_nots + conj->capacity, 0,
+	       (capacity - conj->capacity) * sizeof(*conj->pre_nots));
+	memset(pages + conj->capacity, 0,
+	       (capacity - conj->capacity) * sizeof(*conj->pages));
+
+	conj->bitsets = bitsets;
+	conj->pre_nots = pre_nots;
+	conj->pages = pages;
+	conj->capacity = capacity;
+
+	return 0;
+
+error_3:
+	it->realloc(pre_nots, 0);
+error_2:
+	it->realloc(bitsets, 0);
+error_1:
+	return -1;
+}
+
+int
+bitset_iterator_init(struct bitset_iterator *it, struct bitset_expr *expr,
+		     struct bitset **p_bitsets, size_t bitsets_size)
+{
+	assert(it != NULL);
+	assert(expr != NULL);
+	assert(p_bitsets != NULL);
+	assert(expr->size > 0);
+
+	size_t page_alloc_size = bitset_page_alloc_size(it->realloc);
+	it->page = it->realloc(NULL, page_alloc_size);
+	it->page_tmp = it->realloc(NULL, page_alloc_size);
+
+	if (it->page == NULL || it->page_tmp == NULL) {
+		it->realloc(it->page, 0);
+		it->realloc(it->page_tmp, 0);
+		return -1;
+	}
+
+	bitset_page_create(it->page);
+	bitset_page_create(it->page_tmp);
+
+	if (bitset_iterator_reserve(it, expr->size) != 0)
+		return -1;
+
+	for (size_t c = 0; c < expr->size; c++) {
+		struct bitset_expr_conj *exconj = &expr->conjs[c];
+		struct bitset_iterator_conj *itconj = &it->conjs[c];
+		assert(exconj->size > 0);
+
+		if (bitset_iterator_conj_reserve(it, itconj, exconj->size) != 0)
+			return -1;
+
+		for (size_t b = 0; b < exconj->size; b++) {
+			assert(exconj->bitset_ids[b] < bitsets_size);
+			itconj->page_first_pos = 0;
+			assert(p_bitsets[exconj->bitset_ids[b]] != NULL);
+			itconj->bitsets[b] = p_bitsets[exconj->bitset_ids[b]];
+			itconj->pre_nots[b] = exconj->pre_nots[b];
+			itconj->pages[b] = NULL;
+		}
+
+		itconj->size = exconj->size;
+	}
+
+	it->size = expr->size;
+
+	bitset_iterator_rewind(it);
+
+	return 0;
+}
+
+static void
+bitset_iterator_conj_rewind(struct bitset_iterator_conj *conj, size_t pos)
+{
+	assert(conj != NULL);
+	assert(pos % (BITSET_PAGE_DATA_SIZE * CHAR_BIT) == 0);
+	assert(conj->size > 0);
+	assert(conj->page_first_pos <= pos);
+
+	struct bitset_page key;
+	key.first_pos = pos;
+
+	restart:
+	for (size_t b = 0; b < conj->size; b++) {
+		conj->pages[b] = bitset_pages_nsearch(&conj->bitsets[b]->pages,
+						      &key);
+#if 0
+		if (conj->pages[b] != NULL) {
+			fprintf(stderr, "rewind [%zu] => %zu (%p)\n", b,
+				conj->pages[b]->first_pos, conj->pages[b]);
+		} else {
+			fprintf(stderr, "rewind [%zu] => NULL\n", b);
+		}
+#endif
+		if (conj->pre_nots[b])
+			continue;
+
+		/* bitset b does not have more pages */
+		if (conj->pages[b] == NULL) {
+			conj->page_first_pos = SIZE_MAX;
+			return;
+		}
+
+		assert(conj->pages[b]->first_pos >= key.first_pos);
+
+		/* bitset b have a next page, but it is beyond pos scope */
+		if (conj->pages[b]->first_pos > key.first_pos) {
+			key.first_pos = conj->pages[b]->first_pos;
+			goto restart;
+		}
+	}
+
+	conj->page_first_pos = key.first_pos;
+}
+
+static int
+bitset_iterator_conj_cmp(const void *p1, const void *p2)
+{
+	assert(p1 != NULL && p2 != NULL);
+
+	struct bitset_iterator_conj *conj1 = (struct bitset_iterator_conj *) p1;
+	struct bitset_iterator_conj *conj2 = (struct bitset_iterator_conj *) p2;
+
+	if (conj1->page_first_pos < conj2->page_first_pos) {
+		return -1;
+	} else if (conj1->page_first_pos > conj2->page_first_pos) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+static void
+bitset_iterator_conj_prepare_page(struct bitset_iterator_conj *conj,
+				  struct bitset_page *dst)
+{
+	assert(conj != NULL);
+	assert(dst != NULL);
+	assert(conj->size > 0);
+	assert(conj->page_first_pos != SIZE_MAX);
+
+	bitset_page_set_ones(dst);
+	for (size_t b = 0; b < conj->size; b++) {
+		if (!conj->pre_nots[b]) {
+			/* conj->pages[b] is rewinded to conj->page_first_pos */
+			assert(conj->pages[b]->first_pos == conj->page_first_pos);
+			bitset_page_and(dst, conj->pages[b]);
+		} else {
+			/*
+			 * If page is NULL or its position is not equal
+			 * to conj->page_first_pos then conj->bitset[b]
+			 * does not have page with the required position and
+			 * all bits in this page are considered to be zeros.
+			 * Since NAND(a, zeros) => a, we can simple skip this
+			 * bitset here.
+			 */
+			if (conj->pages[b] == NULL ||
+			    conj->pages[b]->first_pos != conj->page_first_pos)
+				continue;
+
+			bitset_page_nand(dst, conj->pages[b]);
+		}
+	}
+}
+
+static void
+bitset_iterator_prepare_page(struct bitset_iterator *it)
+{
+	qsort(it->conjs, it->size, sizeof(*it->conjs),
+	      bitset_iterator_conj_cmp);
+
+	bitset_page_set_zeros(it->page);
+	it->page->first_pos = it->conjs[0].page_first_pos;
+
+	/* There is no more conjunctions that can be ORed */
+	if (it->page->first_pos == SIZE_MAX)
+		return;
+
+	/* For each conj where conj->page_first_pos == pos */
+	for (size_t c = 0; c < it->size; c++) {
+		if (it->conjs[c].page_first_pos > it->page->first_pos)
+			break;
+
+		/* Get result from conj */
+		bitset_iterator_conj_prepare_page(&it->conjs[c], it->page_tmp);
+		/* OR page from conjunction with it->page */
+		bitset_page_or(it->page, it->page_tmp);
+	}
+
+	/* Init the bit iterator on it->page */
+	bit_iterator_init(&it->page_it, bitset_page_data(it->page),
+		      BITSET_PAGE_DATA_SIZE, true);
+}
+
+static void
+bitset_iterator_first_page(struct bitset_iterator *it)
+{
+	assert(it != NULL);
+
+	/* Rewind all conjunctions to first positions */
+	for (size_t c = 0; c < it->size; c++) {
+		bitset_iterator_conj_rewind(&it->conjs[c], 0);
+	}
+
+	/* Prepare the result page */
+	bitset_iterator_prepare_page(it);
+}
+
+static void
+bitset_iterator_next_page(struct bitset_iterator *it)
+{
+	assert(it != NULL);
+
+	size_t PAGE_BIT = BITSET_PAGE_DATA_SIZE * CHAR_BIT;
+	size_t pos = it->page->first_pos;
+
+	/* Rewind all conjunctions that at the current position to the
+	 * next position */
+	for (size_t c = 0; c < it->size; c++) {
+		if (it->conjs[c].page_first_pos > pos)
+			break;
+
+		bitset_iterator_conj_rewind(&it->conjs[c], pos + PAGE_BIT);
+		assert(pos + PAGE_BIT <= it->conjs[c].page_first_pos);
+	}
+
+	/* Prepare the result page */
+	bitset_iterator_prepare_page(it);
+}
+
+
+void
+bitset_iterator_rewind(struct bitset_iterator *it)
+{
+	assert(it != NULL);
+
+	/* Prepare first page */
+	bitset_iterator_first_page(it);
+}
+
+size_t
+bitset_iterator_next(struct bitset_iterator *it)
+{
+	assert(it != NULL);
+
+	while (true) {
+		if (it->page->first_pos == SIZE_MAX)
+			return SIZE_MAX;
+
+		size_t pos = bit_iterator_next(&it->page_it);
+		if (pos != SIZE_MAX) {
+			return it->page->first_pos + pos;
+		}
+
+		bitset_iterator_next_page(it);
+	}
+}
