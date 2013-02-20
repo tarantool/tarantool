2124636cbe5d616174949b95e6f6a9a446ec9bf8
Dmitry Isaikin <isaikin@corp.mail.ru>
2013-02-20 17:05:52 +0400

    - box.auto_increment works with NUM64 keys - auto_increment key start from '1' now
    Conflicts:
    
    	test/big/tarantool.cfg

--- src/box/box.lua
+++ src/box/box.lua
@@ -102,15 +102,23 @@ function box.update(space, key, format, ...)
                                 ...))
 end
 
--- Assumes that spaceno has a TREE int32 (NUM) primary key
+-- Assumes that spaceno has a TREE int32 (NUM) or int64 (NUM64) primary key
 -- inserts a tuple after getting the next value of the
 -- primary key and returns it back to the user
 function box.auto_increment(spaceno, ...)
     spaceno = tonumber(spaceno)
     local max_tuple = box.space[spaceno].index[0].idx:max()
-    local max = -1
+    local max = 0
     if max_tuple ~= nil then
-        max = box.unpack('i', max_tuple[0])
+        max = max_tuple[0]
+        local fmt = 'i'
+        if #max == 8 then fmt = 'l' end
+        max = box.unpack(fmt, max)
+    else
+        -- first time
+        if box.space[spaceno].index[0].key_field[0].type == "NUM64" then
+            max = tonumber64(max)
+        end
     end
     return box.insert(spaceno, max + 1, ...)
 end
