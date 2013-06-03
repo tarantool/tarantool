b835d67a7917c41798013f8ae3cfb0ea5d55af48
Konstantin Osipov <kostja@tarantool.org>
2013-06-03 23:28:10 +0400

    lua-multret: review fixes
    Don't allow a mix of scalars and complex returns in Lua multireturn.
    Allow a mix of tables and tuples in Lua multireturn table.

--- src/box/box_lua.m
+++ src/box/box_lua.m
@@ -957,7 +957,7 @@ port_lua(struct port *port) { return (struct port_lua *) port; }
  * For addU32/dupU32 do nothing -- the only u32 Box can give
  * us is tuple count, and we don't need it, since we intercept
  * everything into Lua stack first.
- * @sa iov_add_multret
+ * @sa port_add_lua_multret
  */
 
 static void
@@ -1172,30 +1172,43 @@ port_add_lua_ret(struct port *port, struct lua_State *L, int index)
  * a procedure, we are compatible with SELECT protocol,
  * and return the number of return values first, and
  * then each return value as a tuple.
+ *
+ * If a Lua stack contains at least one scalar, each
+ * value on the stack is converted to a tuple. A Lua
+ * is converted to a tuple with multiple fields.
+ *
+ * If the stack is a Lua table, each member of which is
+ * not scalar, each member of the table is converted to
+ * a tuple. This way very large lists of return values can
+ * be used, since Lua stack size is limited by 8000 elements,
+ * while Lua table size is pretty much unlimited.
  */
 static void
 port_add_lua_multret(struct port *port, struct lua_State *L)
 {
 	int nargs = lua_gettop(L);
-	for (int i = 1; i <= nargs; ++i) {
-		int do_multiret = 0;
-		if (lua_istable(L, i)) {
-			lua_pushnil(L);
-			if (lua_next(L, i) != 0) {
-				if (lua_istable(L, -1))
-					do_multiret = 1;
-				lua_pop(L, 2);
-			}
-		}
-		if (do_multiret) {
-			lua_pushnil(L);
-			while (lua_next(L, i) != 0) {
-				int vali = lua_gettop(L);
-				port_add_lua_ret(port, L, vali);
+	/** Check if we deal with a table of tables. */
+	if (nargs == 1 && lua_istable(L, 1)) {
+		/*
+		 * The table is not empty and consists of tables
+		 * or tuples. Treat each table element as a tuple,
+		 * and push it.
+		 */
+		lua_pushnil(L);
+		int has_keys = lua_next(L, 1);
+		if (has_keys  &&
+		    (lua_istable(L, -1) || lua_isuserdata(L, -1))) {
+
+			do {
+				port_add_lua_ret(port, L, lua_gettop(L));
 				lua_pop(L, 1);
-			}
-			continue;
+			} while (lua_next(L, 1));
+			return;
+		} else if (has_keys) {
+			lua_pop(L, 1);
 		}
+	}
+	for (int i = 1; i <= nargs; ++i) {
 		port_add_lua_ret(port, L, i);
 	}
 }
