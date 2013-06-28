tarantool/tarantool

commit 13054aaa46d3c1db7263c449d085c87156fd86ea

Apply patch from https://bugs.launchpad.net/tarantool/+bug/1170735
Fix compile on older gccs.

src/box/box_lua.m
@@ -967,7 +967,7 @@ port_lua_add_tuple(struct port *port, struct tuple *tuple,
 	lua_State *L = port_lua(port)->L;
 	@try {
 		lbox_pushtuple(L, tuple);
-	} @catch (...) {
+	} @catch (id allOthers) {
 		tnt_raise(ClientError, :ER_PROC_LUA, lua_tostring(L, -1));
 	}
 }
@@ -1306,7 +1306,7 @@ box_lua_execute(struct request *request, struct port *port)
 		port_add_lua_multret(port, L);
 	} @catch (tnt_Exception *e) {
 		@throw;
-	} @catch (...) {
+	} @catch (id allOthers) {
 		tnt_raise(ClientError, :ER_PROC_LUA, lua_tostring(L, -1));
 	} @finally {
 		/*

src/lua/init.m
@@ -510,7 +510,7 @@ box_lua_fiber_run(va_list ap __attribute__((unused)))
 			/* The fiber is detached, log the error. */
 			[e log];
 		}
-	} @catch (...) {
+	} @catch (id allOthers) {
 		lua_settop(L, 1);
 		/*
 		 * The error message is already there.
@@ -972,7 +972,7 @@ lbox_pcall(struct lua_State *L)
 		lua_pushstring(L, e->errmsg);
 	} @catch (tnt_Exception *e) {
 		@throw;
-	} @catch (...) {
+	} @catch (id allOthers) {
 		lua_settop(L, 1);
 		/* completion status */
 		lua_pushboolean(L, false);
@@ -1112,7 +1112,7 @@ tarantool_lua_dostring(struct lua_State *L, const char *str)
 	} @catch (tnt_Exception *e) {
 		lua_pushstring(L, [e errmsg]);
 		return 1;
-	} @catch (...) {
+	} @catch (id allOthers) {
 		return 1;
 	}
 	return 0;

src/lua/session.m
@@ -114,7 +114,7 @@ lbox_session_run_trigger(void *param)
 		lua_call(L, 0, 0);
 	} @catch (tnt_Exception *e) {
 		@throw;
-	} @catch (...) {
+	} @catch ( id allOthers ) {
 		tnt_raise(ClientError, :ER_PROC_LUA,
 			  lua_tostring(L, -1));
 	} @finally {

test/unit/objc_catchcxx.m
@@ -8,7 +8,7 @@ int main()
 	luaL_openlibs(L);
 	@try {
 		luaL_error(L, "test");
-	} @catch (...) {
+	} @catch (id allOthers) {
 		printf("exception handled\n");
 	}
 	lua_close(L);

commit b1caf5ecf43232d6b9d40334162bdd24bf16c947

Fix ObjC Clang build broken by 13054aaa46d3c1db7263c449d085c87156fd86ea

src/box/box_lua.m
@@ -977,7 +977,11 @@ port_lua_add_tuple(struct port *port, struct tuple *tuple,
 	lua_State *L = port_lua(port)->L;
 	@try {
 		lbox_pushtuple(L, tuple);
+#if defined(__clang__)
+	} @catch (...) {
+#else /* defined(__clang__) */
 	} @catch (id allOthers) {
+#endif
 		tnt_raise(ClientError, :ER_PROC_LUA, lua_tostring(L, -1));
 	}
 }
@@ -1316,7 +1320,11 @@ box_lua_execute(struct request *request, struct port *port)
 		port_add_lua_multret(port, L);
 	} @catch (tnt_Exception *e) {
 		@throw;
+#if defined(__clang__)
+	} @catch (...) {
+#else /* !defined(__clang__) */
 	} @catch (id allOthers) {
+#endif
 		tnt_raise(ClientError, :ER_PROC_LUA, lua_tostring(L, -1));
 	} @finally {
 		/*

src/lua/init.m
@@ -510,7 +510,11 @@ box_lua_fiber_run(va_list ap __attribute__((unused)))
 			/* The fiber is detached, log the error. */
 			[e log];
 		}
+#if defined(__clang__)
+	} @catch (...) {
+#else /* !defined(__clang__) */
 	} @catch (id allOthers) {
+#endif
 		lua_settop(L, 1);
 		/*
 		 * The error message is already there.
@@ -972,7 +976,11 @@ lbox_pcall(struct lua_State *L)
 		lua_pushstring(L, e->errmsg);
 	} @catch (tnt_Exception *e) {
 		@throw;
+#if defined(__clang__)
+	} @catch (...) {
+#else /* !defined(__clang__) */
 	} @catch (id allOthers) {
+#endif
 		lua_settop(L, 1);
 		/* completion status */
 		lua_pushboolean(L, false);
@@ -1112,7 +1120,11 @@ tarantool_lua_dostring(struct lua_State *L, const char *str)
 	} @catch (tnt_Exception *e) {
 		lua_pushstring(L, [e errmsg]);
 		return 1;
+#if defined(__clang__)
+	} @catch (...) {
+#else /* !defined(__clang__) */
 	} @catch (id allOthers) {
+#endif
 		return 1;
 	}
 	return 0;

src/lua/session.m
@@ -114,7 +114,11 @@ lbox_session_run_trigger(void *param)
 		lua_call(L, 0, 0);
 	} @catch (tnt_Exception *e) {
 		@throw;
+#if defined(__clang__)
+	} @catch (...) {
+#else /* !defined(__clang__) */
 	} @catch ( id allOthers ) {
+#endif
 		tnt_raise(ClientError, :ER_PROC_LUA,
 			  lua_tostring(L, -1));
 	} @finally {

test/unit/objc_catchcxx.m
@@ -8,7 +8,11 @@ int main()
 	luaL_openlibs(L);
 	@try {
 		luaL_error(L, "test");
+#if defined(__clang__)
+	} @catch (...) {
+#else /* !defined(__clang__) */
 	} @catch (id allOthers) {
+#endif
 		printf("exception handled\n");
 	}
 	lua_close(L);