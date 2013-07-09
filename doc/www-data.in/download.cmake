90cc61c3d16ce0998e018c0b9e79d4556b348fbd
Konstantin Osipov <kostja@tarantool.org>
2013-07-09 14:37:24 +0400

    Update the download page: promote online package archives
    Remove Debian Squeeze support.

--- doc/www-data.in/download.cmake
+++ doc/www-data.in/download.cmake
@@ -34,7 +34,48 @@ To simplify problem analysis and avoid various bugs induced
 by compilation parameters and environment, it is recommended
 that production systems use the builds provided on this site.
 
-### RedHat, FreeBSD, Mac OS X
+### Debian GNU/Linux and Ubuntu
+
+We maintain an always up-to-date Debian GNU/Linux and Ubuntu package
+repository at <a
+href="http://tarantool.org/dist/debian">http://tarantool.org/dist/debian</a> and <a
+href="http://tarantool.org/dist/ubuntu">http://tarantool.org/dist/ubuntu</a> respectively.
+
+At the moment the repository contains builds for Debian "Sid", "Jessie",
+"Wheezy" and Ubuntu "Precise". It can be added to your apt sources list with:
+
+    wget http://tarantool.org/dist/public.key
+    sudo apt-key add ./public.key
+    release=`lsb_release -c -s`
+
+    # For Debian:
+
+    echo "deb http://tarantool.org/dist/debian/ $release main" | sudo tee -a /etc/apt/sources.list.d/tarantool.list
+    echo "deb-src http://tarantool.org/dist/debian/ $release main" | sudo tee -a /etc/apt/sources.list.d/tarantool.list
+
+    # For Ubuntu:
+
+    echo "deb http://tarantool.org/dist/ubuntu/ $release main" | sudo tee -a /etc/apt/sources.list.d/tarantool.list
+    echo "deb-src http://tarantool.org/dist/ubuntu/ $release main" | sudo tee -a /etc/apt/sources.list.d/tarantool.list
+
+    sudo apt-get update
+    sudo apt-get install tarantool tarantool-client
+
+### CentOS 5-6 GNU/Linux
+
+CentOS GNU/Linux repository is available at
+<a href="http://tarantool.org/dist/centos">http://tarantool.org/dist/centos</a>.
+
+Add the following section to your yum repository list (/etc/yum.repos.d/tarantool.repo)
+to enable it:
+
+    [tarantool]
+    name=CentOS-$releasever - Tarantool
+    baseurl=http://tarantool.org/dist/centos/$releasever/os/$basearch/
+    enabled=1
+    gpgcheck=0
+
+### Standalone packages for RedHat, FreeBSD, Mac OS X
 
 <table border=1 title="Download the latest build, @PACKAGE_VERSION@" width=100%>
 
@@ -111,46 +152,6 @@ that production systems use the builds provided on this site.
 
 </table>
 
-### Debian GNU/Linux and Ubuntu
-
-We maintain an always up-to-date Debian GNU/Linux and Ubuntu package
-repository at <a
-href="http://tarantool.org/dist/debian">http://tarantool.org/dist/debian</a> and <a
-href="http://tarantool.org/dist/ubuntu">http://tarantool.org/dist/ubuntu</a> respectively.
-
-At the moment the repository contains builds for Debian "Sid", "Squeeze",
-"Wheezy" and Ubuntu "Precise". It can be added to your apt sources list with:
-
-    wget http://tarantool.org/dist/public.key
-    sudo apt-key add ./public.key
-    release=`lsb_release -c -s`
-
-    # For Debian:
-
-    echo "deb http://tarantool.org/dist/debian/ $release main" | sudo tee -a /etc/apt/sources.list.d/tarantool.list
-    echo "deb-src http://tarantool.org/dist/debian/ $release main" | sudo tee -a /etc/apt/sources.list.d/tarantool.list
-
-    # For Ubuntu:
-
-    echo "deb http://tarantool.org/dist/ubuntu/ $release main" | sudo tee -a /etc/apt/sources.list.d/tarantool.list
-    echo "deb-src http://tarantool.org/dist/ubuntu/ $release main" | sudo tee -a /etc/apt/sources.list.d/tarantool.list
-
-    sudo apt-get update
-    sudo apt-get install tarantool tarantool-client
-
-### CentOS 5-6 GNU/Linux
-
-CentOS GNU/Linux repository is available at
-<a href="http://tarantool.org/dist/centos">http://tarantool.org/dist/centos</a>.
-
-Add the following section to your yum repository list (/etc/yum.repos.d/tarantool.repo)
-to enable it:
-
-    [tarantool]
-    name=CentOS-$releasever - Tarantool
-    baseurl=http://tarantool.org/dist/centos/$releasever/os/$basearch/
-    enabled=1
-    gpgcheck=0
 
 # Development branch
 
@@ -164,7 +165,7 @@ The server roadmap is maintained on [Launchpad](http://launchpad.net/tarantool).
 
 - Perl driver, [DR:Tarantool](http://search.cpan.org/~unera/DR-Tarantool-0.37/lib/DR/Tarantool.pm)
 - Java driver, [Maven repository](http://dgreenru.github.com/tarantool-java)
-- Ruby driver, [http://github.com/tarantool/tarantool-ruby](https://github.com/tarantool/tarantool-ruby)
+- Ruby driver, [http://github.com/mailru/tarantool-ruby](https://github.com/tarantool/tarantool-ruby)
 - Python driver, [http://pypi.python.org/pypi/tarantool](http://pypi.python.org/pypi/tarantool)
 - PHP driver, [https://github.com/tarantool/tarantool-php](https://github.com/tarantool/tarantool-php)
 - node.js driver,
@@ -289,7 +290,7 @@ Tarantool использует стандартную схему нумеров�
 <a href="http://tarantool.org/dist/debian">http://tarantool.org/dist/debian</a>,
 <a href="http://tarantool.org/dist/ubuntu">http://tarantool.org/dist/ubuntu</a>.
 
-Репозитории содержат пакеты для Debian "Sid", "Squeeze",
+Репозитории содержат пакеты для Debian "Sid", "Jessy",
 "Wheezy" и Ubuntu "Precise". Для установки можно воспользоваться следующим
 скриптом:
 
@@ -321,7 +322,7 @@ href="http://tarantool.org/dist">http://tarantool.org/dist</a>.
 ### Доступ из языков программирования
 
 - Perl driver [DR:Tarantool](http://search.cpan.org/~unera/DR-Tarantool-0.37/lib/DR/Tarantool.pm)
-- [Ruby driver](https://github.com/tarantool/tarantool-ruby)
+- [Ruby driver](https://github.com/mailru/tarantool-ruby)
 - Python driver, [hosted at pypi.python.org](http://pypi.python.org/pypi/tarantool)
 - PHP driver [https://github.com/tarantool/tarantool-php](https://github.com/tarantool/tarantool-php)
 - Java driver, [Maven repository](http://dgreenru.github.com/tarantool-java)
