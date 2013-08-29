6efaeaaf0e34ad8d830a821c6625fce31f20a57c
Konstantin Osipov <kostja@tarantool.org>
2013-08-29 19:26:43 +0400

    Convert 'wal' suite to Lua, lua-console, dict-v6.

--- test/wal/lua.test.lua
+++ test/wal/lua.test.lua
@@ -1,21 +1,11 @@
 box.insert(box.schema.SPACE_ID, 0, 0, 'tweedledum')
----
-- [0, 0, 'tweedledum']
-...
 box.insert(box.schema.INDEX_ID, 0, 0, 'primary', 'hash', 1, 1, 0, 'str')
----
-- [0, 0, 'primary', 1752392040, 1, 1, 0, 'str']
-...
 box.insert(box.schema.INDEX_ID, 0, 1, 'secondary', 'tree', 0, 1, 1, 'num')
----
-- [0, 1, 'secondary', 1701147252, 0, 1, 1, 'num']
-...
 space = box.space[0]
----
-...
 -- A test case for Bug#1042738
 -- https://bugs.launchpad.net/tarantool/+bug/1042738
 -- Iteration over a non-unique TREE index
+
 -- setopt delimiter ';'
 for i = 1, 1000 do
     space:truncate()
@@ -30,23 +20,13 @@ for i = 1, 1000 do
         error('bug at iteration '..i..', count is '..count)
     end
 end;
----
-...
 -- setopt delimiter ''
 space:truncate()
----
-...
+
 --
 -- A test case for Bug#1043858 server crash on lua stack overflow on CentOS
 -- 5.4
 --
 for i = 1, 100000, 1 do space:insert(i,i) end
----
-...
 local t1 = {space:select(1)}
----
-- error: 'Lua error: stack overflow'
-...
 space:drop()
----
-...
