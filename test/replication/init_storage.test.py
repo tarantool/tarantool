5e991fd00d980884f9210cfd5f25d5e7d91aabfc
Eugine Blikh <bigbes@gmail.com>
2013-12-04 20:21:12 +0400

    Fix race condition in #144

--- test/replication/init_storage.test.py
+++ test/replication/init_storage.test.py
@@ -11,7 +11,7 @@ master.admin('space:create_index(\'primary\', \'hash\', {parts = { 0, \'num\' } 
 master.admin('for k = 1, 9 do space:insert(k, k*k) end')
 
 for k in glob.glob(os.path.join(master.vardir, '*.xlog')):
-	os.unlink(k)
+    os.unlink(k)
 
 print '-------------------------------------------------------------'
 print 'replica test 1 (must be failed)'
@@ -24,7 +24,7 @@ replica.deploy("replication/cfg/replica.cfg",
                need_init=False)
 
 for i in range(1, 10):
-	replica.admin('box.select(42, 0, %d)' % i)
+    replica.admin('box.select(42, 0, %d)' % i)
 
 replica.stop()
 replica.cleanup(True)
@@ -32,7 +32,7 @@ replica.cleanup(True)
 master.admin('box.snapshot()')
 master.restart()
 master.admin('for k = 10, 19 do box.insert(42, k, k*k*k) end')
-
+lsn = master.get_param('lsn')
 print '-------------------------------------------------------------'
 print 'replica test 2 (must be ok)'
 print '-------------------------------------------------------------'
@@ -44,9 +44,9 @@ replica.deploy("replication/cfg/replica.cfg",
                need_init=False)
 
 replica.admin('space = box.space.test');
-
+replica.wait_lsn(lsn)
 for i in range(1, 20):
-	replica.admin('space:select(0, %d)' % i)
+    replica.admin('space:select(0, %d)' % i)
 
 replica.stop()
 replica.cleanup(True)
