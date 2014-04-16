96cdc78aeb19a4617fa826e7ac2c1672e5b5c9cb
bigbes <bigbes@gmail.com>
2014-04-16 17:03:23 +0400

    Move replication/status.test -> replication/status.test.py, redo format of test from 1.5 to 1.6
    Closes #239

---
+++ test/replication/status.test.py
@@ -0,0 +1,39 @@
+import os
+import re
+import time
+
+from lib.tarantool_server import TarantoolServer
+
+# master server
+master = server
+# replica server
+replica = TarantoolServer()
+replica.script = "replication/replica.lua"
+replica.rpl_master = master
+replica.vardir = os.path.join(master.vardir, 'replica')
+replica.deploy()
+
+replica.get_param("lsn")
+
+cycles = 0
+status = replica.admin.execute_no_reconnect("box.info.status", True)
+while (re.search(r'replica/.*/(connecting|connected)\n', status) == None and cycles < 500):
+    time.sleep(0.01)
+    status = replica.admin.execute_no_reconnect("box.info.status", True)
+    cycles += 1
+print(re.search(r'replica/.*/(connecting|connected)\n', status) != None)
+
+master.stop()
+cycles = 0
+
+while (re.search(r'replica/.*/(connecting|failed)\n', status) == None and cycles < 500):
+    time.sleep(0.01)
+    status = replica.admin.execute_no_reconnect("box.info.status", True)
+    cycles += 1
+print(re.search(r'replica/.*/(connecting|failed)\n', status) != None)
+
+# Cleanup.
+replica.stop()
+replica.cleanup(True)
+
+master.deploy()
