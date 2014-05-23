ce7f9463d5d0dd743779696d6529e90b2d658235
esha <shadrin.e@gmail.com>
2014-05-23 14:25:57 +0400

    Added iterator access tests and other

--- test/box/auth_access.test.lua
+++ test/box/auth_access.test.lua
@@ -17,14 +17,18 @@ box.schema.user.create('testus')
 s = box.schema.create_space('admin_space')
 s:create_index('primary', {type = 'hash', parts = {0, 'NUM'}})
 s:insert({1})
+s:insert({2})
 --
--- Check double grant
+-- Check double grant and read access
 --
 box.schema.user.grant('testus', 'read', 'space', 'admin_space')
 box.schema.user.grant('testus', 'read', 'space', 'admin_space')
 
 box.session.su('testus')
-s:select()
+s:select(1)
+s:insert({3})
+s:delete(1)
+s:drop()
 --
 -- Check double revoke
 --
@@ -33,7 +37,18 @@ box.schema.user.revoke('testus', 'read', 'space', 'admin_space')
 box.schema.user.revoke('testus', 'read', 'space', 'admin_space')
 
 box.session.su('testus')
-s:select()
+s:select(1)
+box.session.su('admin')
+--
+-- Check write access on space
+-- 
+box.schema.user.grant('testus', 'write', 'space', 'admin_space')
+
+box.session.su('testus')
+s:select(1)
+s:delete(1)
+s:insert({3})
+s:drop()
 box.session.su('admin')
 --
 -- Check double drop user
@@ -45,17 +60,24 @@ box.schema.user.drop('testus')
 --
 box.session.su('guest')
 box.session.uid()
-box.space._user:select()
-s:select()
+box.space._user:select(1)
+s:select(1)
+s:insert({4})
+s:delete({3})
+s:drop()
+gs = box.schema.create_space('guest_space')
+box.schema.func.create('guest_func')
 
 box.session.su('admin')
+s:select()
+--
 -- Create user with universe read&write grants
 -- and create this user session
 --
 box.schema.user.create('uniuser')
 box.schema.user.grant('uniuser', 'read, write, execute', 'universe')
 box.session.su('uniuser')
-box.session.uid()
+if box.session.uid() < 2 then return 'uid error' end
 --
 -- Check universal user
 -- Check delete currently authenticated user
@@ -82,12 +104,11 @@ box.schema.user.drop('uniuser_testus')
 -- Check access system and any spaces
 --
 box.space.admin_space:select()
-box.space._user:select()
-box.space._space:select()
+box.space._user:select(1)
+box.space._space:select(280)
 
 us = box.schema.create_space('uniuser_space')
 box.schema.func.create('uniuser_func')
-box.schema.user.create('uniuser_testus')
 
 box.session.su('admin')
 box.schema.user.create('someuser')
@@ -107,6 +128,7 @@ box.schema.user.drop('someuser')
 box.schema.user.drop('uniuser_testus')
 box.schema.user.drop('uniuser')
 box.space._user:delete(2)
+s:drop()
 --
 -- Check write grant on _user
 --
@@ -115,13 +137,13 @@ box.schema.user.create('testuser')
 box.schema.user.grant('testuser', 'write', 'space', '_user')
 box.session.su('testuser')
 box.space._user:delete(2)
-box.space._user:select()
+box.space._user:select(1)
 box.space._user:insert{3,'','someone'}
 box.space._user:delete(3)
 
 box.session.su('admin')
-box.space._user:select()
-
+box.space._user:select(1)
+box.space._user:delete(3)
 box.schema.user.revoke('testuser', 'write', 'space', '_user')
 --
 -- Check read grant on _user
@@ -129,7 +151,7 @@ box.schema.user.revoke('testuser', 'write', 'space', '_user')
 box.schema.user.grant('testuser', 'read', 'space', '_user')
 box.session.su('testuser')
 box.space._user:delete(2)
-box.space._user:select()
+box.space._user:select(1)
 box.space._user:insert{4,'','someone2'}
 
 box.session.su('admin')
@@ -138,7 +160,7 @@ box.session.su('admin')
 --
 box.schema.user.grant('testuser', 'read', 'space', '_index')
 box.session.su('testuser')
-box.space._index:select()
+box.space._index:select(272)
 box.space._index:insert{512, 1,'owner','tree', 1, 1, 0,'num'}
 
 
@@ -149,15 +171,77 @@ box.session.su('admin')
 --# setopt delimiter ';'
 function func_limit()
     local i = 1
-    while i <= 32001 do
+    while true do
         box.schema.func.create('func'..i)
         i = i + 1
     end
     return i
 end;
+function drop_limit_func()
+    local i = 1
+    while true do
+        box.schema.func.drop('func'..i)
+        i = i + 1
+    end
+end;
 func_limit();
+drop_limit_func();
+box.schema.user.grant('testuser', 'read, write, execute', 'universe');
+box.session.su('testuser');
+func_limit();
+drop_limit_func();
 --# setopt delimiter ''
 
-s:drop()
+box.session.su('admin')
+box.schema.user.revoke('testuser', 'read, write, execute', 'universe')
+--
+-- Check that itertors check privileges
+--
+s = box.schema.create_space('glade') 
+box.schema.user.grant('testuser', 'read', 'space', 'glade')
+s:create_index('primary', {unique = true, parts = {0, 'NUM', 1, 'STR'}})
+s:insert({1, 'A'})
+s:insert({2, 'B'})
+s:insert({3, 'C'})
+s:insert({4, 'D'})
+
+t = {}
+for key, v in s.index.primary:pairs(3, {iterator = 'GE'}) do table.insert (t, v) end 
+t
+t = {}
+box.session.su('testuser')
+s:select()
+for key, v in s.index.primary:pairs(3, {iterator = 'GE'}) do table.insert (t, v) end 
+t
+t = {}
+box.session.su('admin')
+box.schema.user.grant('testuser', 'write', 'space', 'glade')
+box.session.su('testuser')
+s:select()
+for key, v in s.index.primary:pairs(1, {iterator = 'GE'}) do table.insert (t, v) end 
+t
+t = {}
+box.session.su('admin')
+box.schema.user.grant('testuser', 'read, write, execute', 'space', 'glade')
+box.session.su('testuser')
+s:select()
+for key, v in s.index.primary:pairs(3, {iterator = 'GE'}) do table.insert (t, v) end 
+t
+t = {}
+
+box.session.su('guest')
+s:select()
+for key, v in s.index.primary:pairs(3, {iterator = 'GE'}) do table.insert (t, v) end 
+t
+t = {}
+
+box.session.su('guest')
+s:select()
+for key, v in s.index.primary:pairs(3, {iterator = 'GE'}) do table.insert (t, v) end 
+t
+
+box.session.su('admin')
 
 box.space._user:select()
+box.space._space:select()
+box.space._func:select()
