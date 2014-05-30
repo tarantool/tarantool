956fed278b481c2cd01fdb4c2f15f4b3530cc03a
bigbes <bigbes@gmail.com>
2014-05-30 19:57:42 +0400

    Add basic logging functions
    * error(fmt, args)
    * warn(fmt, args)
    * info(fmt, args)
    * debug(fmt, args)

--- src/lua/init.cc
+++ src/lua/init.cc
@@ -69,12 +69,15 @@ struct lua_State *tarantool_L;
 
 /* contents of src/lua/ files */
 extern char uuid_lua[], session_lua[], msgpackffi_lua[], fun_lua[],
-       load_cfg_lua[], interactive_lua[], digest_lua[], init_lua[];
+       load_cfg_lua[], interactive_lua[], digest_lua[], init_lua[],
+       log_lua[];
 static const char *lua_sources[] = { init_lua, session_lua, load_cfg_lua, NULL };
 static const char *lua_modules[] = { "box.msgpackffi", msgpackffi_lua,
-	"fun", fun_lua, "box.digest", digest_lua,
+	"fun", fun_lua,
+	"box.digest", digest_lua,
 	"box.interactive", interactive_lua,
-	"box.uuid", uuid_lua, NULL };
+	"box.uuid", uuid_lua,
+	"log", log_lua, NULL };
 /*
  * {{{ box Lua library: common functions
  */
