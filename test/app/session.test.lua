7dd2fdb7b7e47c383c9281d9aefc5ff9bb4aec6f
Konstantin Osipov <kostja@tarantool.org>
2014-06-03 17:14:21 +0400

    A post-merge fix for session test.

--- test/app/session.test.lua
+++ test/app/session.test.lua
@@ -3,6 +3,6 @@
 --
 -- Check that Tarantool creates ADMIN session for #! script
 -- 
-session = require('box.session')
+session = require('session')
 print('session.id()', session.id())
 print('session.uid()', session.uid())
