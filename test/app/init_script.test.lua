77bca171dfdbac9c1dce43452241a9250b57cf14
esha <shadrin.e@gmail.com>
2014-06-10 17:42:50 +0400

    Add require module tests

--- test/app/init_script.test.lua
+++ test/app/init_script.test.lua
@@ -1,6 +1,6 @@
 #!/usr/bin/env tarantool
 --
--- Testing start-up script
+-- Testing init script
 --
 box.cfg{
     admin_port = 3313,
@@ -8,29 +8,26 @@ box.cfg{
     pid_file = "box.pid",
 }
 
-floor = require("math").floor
 yaml = require('yaml')
+fiber = require('fiber')
 
 space = box.schema.create_space('tweedledum', { id = 0 })
 space:create_index('primary', { type = 'hash' })
 
+print[[
 --
--- Access to box.cfg from start-up script
+-- Access to box.cfg from init script
 --
-
+]]
 t = {}
 
 for k,v in pairs(box.cfg) do if type(v) ~= 'table' and type(v) ~= 'function' then table.insert(t,k..':'..tostring(v)) end end
 
 print('box.cfg')
 for k,v in pairs(t) do print(k, v) end
-
 --
 -- Insert tests
 --
-
-fiber = require('fiber')
-
 local function do_insert()
     fiber.detach()
     space:insert{1, 2, 4, 8}
@@ -39,24 +36,41 @@ end
 fiber1 = fiber.create(do_insert)
 fiber.resume(fiber1)
 
-print('\nTest insert from detached fiber')
+print[[
+--
+-- Test insert from detached fiber
+--
+]]
 print(yaml.encode(space:select()))
 
-print('Test insert from start-up script')
+print[[
+--
+-- Test insert from init script
+--
+]]
 space:insert{2, 4, 8, 16}
 print(space:get(1))
 print(space:get(2))
-
 --
 -- Run a dummy insert to avoid race conditions under valgrind
 --
 space:insert{4, 8, 16}
 print(space:get(4))
+print[[
+--
+-- Check that require function(math.floor) reachable in the init script
+--
+]]
+floor = require("math").floor
+print(floor(0.5))
+print(floor(0.9))
+print(floor(1.1))
 
+mod = dofile('../app/require_mod.lua')
+print(mod.test(10, 15))
 --
 -- A test case for https://github.com/tarantool/tarantool/issues/53
 --
-
 assert (require ~= nil)
 fiber.sleep(0.0)
 assert (require ~= nil)
