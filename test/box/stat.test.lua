a19fbc50b86a8e6a2a8a630830ffd1964ed4f8f5
Konstantin Osipov <kostja@tarantool.org>
2014-06-17 13:36:29 +0400

    Update stat.test so that it's totally clear what's going on.
    The test fails sporadically when running as part of a suite.

--- test/box/stat.test.lua
+++ test/box/stat.test.lua
@@ -1,21 +1,33 @@
 -- clear statistics
 --# stop server default
 --# start server default
+box.stat.INSERT.total
+box.stat.DELETE.total
+box.stat.UPDATE.total
+box.stat.REPLACE.total
+box.stat.SELECT.total
 
 space = box.schema.create_space('tweedledum', { id = 0 })
 space:create_index('primary', { type = 'hash' })
 
-
 -- check stat_cleanup
 -- add several tuples
 for i=1,10 do space:insert{i, 'tuple'..tostring(i)} end
-box.stat()
+box.stat.INSERT.total
+box.stat.DELETE.total
+box.stat.UPDATE.total
+box.stat.REPLACE.total
+box.stat.SELECT.total
 
 --# stop server default
 --# start server default
 
 -- statistics must be zero
-box.stat()
+box.stat.INSERT.total
+box.stat.DELETE.total
+box.stat.UPDATE.total
+box.stat.REPLACE.total
+box.stat.SELECT.total
 
 -- cleanup
 box.space[0]:drop()
