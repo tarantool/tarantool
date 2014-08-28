ac2d4852fab9809c5527986e6b8af28e14f0bb78
Roman Tsisyk <roman@tsisyk.com>
2014-08-28 15:31:32 +0400

    Import tutorial

--- src/lua/help.lua
+++ src/lua/help.lua
@@ -1,8 +1,14 @@
+local doc = require('help.en_US')
+
 help = {}
 help[1] = {}
 help[1]["Help topics"] = { "Tutorial", "Basics", "Administration" }
 help[2] = "To get help on a topic, type help('topic') (with quotes)"
 help[3] = "To get help on a function/object, type help(function) (without quotes)"
+help[4] = "To start tutorial, type tutorial()"
+
+tutorial = {}
+tutorial[1] = help[4]
 
 local help_function_data = {};
 help_function_data["Administration"] = {}
@@ -40,4 +46,32 @@ end
 
 setmetatable(help, { __call = help_call })
 
+local screen_id = 1;
 
+local function tutorial_call(table, action)
+    if action == 'start' then
+        screen_id = 1;
+    elseif action == 'next' or action == 'more' then
+        screen_id = screen_id + 1
+    elseif action == 'prev' then
+        screen_id = screen_id - 1
+    elseif type(action) == 'number' and action % 1 == 0 then
+        screen_id = tonumber(action)
+    elseif action ~= nil then
+        error('Usage: tutorial("start" | "next" | "prev" | 1 .. '..
+            #doc.tutorial..')')
+    end
+    if screen_id < 1 then
+        screen_id = 1
+    elseif screen_id > #doc.tutorial then
+        screen_id = #doc.tutorial
+    end
+    return doc.tutorial[screen_id]
+end
+
+setmetatable(tutorial, { __call = tutorial_call })
+
+return {
+    help = help;
+    tutorial= tutorial;
+}
