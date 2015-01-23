85aa64d97855d803c037ababa24a8f496f84908b
Roman Tsisyk <roman@tsisyk.com>
2015-01-23 18:26:30 +0300

    Fix test after fixing #678

--- test/app/sophia_dir.test.lua
+++ test/app/sophia_dir.test.lua
@@ -4,16 +4,6 @@ local tap = require('tap')
 local test = tap.test('cfg')
 test:plan(1)
 
-local function file_exists(name)
-	local f = io.open(name,"r")
-	if f ~= nil then
-		io.close(f)
-		return true
-	else
-		return false
-	end
-end
-
 os.execute("rm -rf sophia")
 local box = require('box')
 box.cfg{
@@ -21,7 +11,7 @@ box.cfg{
     slab_alloc_arena=0.1,
     wal_mode = ""
 }
-test:is(file_exists("sophia"), false, 'sophia_dir is not auto-created')
+test:isnil(io.open("sophia", 'r'), 'sophia_dir is not auto-created')
 test:check()
 
 os.exit(0)
