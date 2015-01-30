b4994ce1f0c0218f4056a69b8b8bb74f63954752
Konstantin Osipov <kostja@tarantool.org>
2015-01-30 18:06:02 +0300

    cmake, lua_source() function: make directory if it does not exist.

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -43,6 +43,11 @@ function(lua_source varname filename)
     set (tmpfile "${CMAKE_CURRENT_BINARY_DIR}/${filename}.new.c")
     set (dstfile "${CMAKE_CURRENT_BINARY_DIR}/${filename}.c")
     get_filename_component(module ${filename} NAME_WE)
+    get_filename_component(dstdir ${dstfile} DIRECTORY)
+    if (IS_DIRECTORY ${dstdir})
+    else()
+        file(MAKE_DIRECTORY ${dstdir})
+    endif()
 
     ADD_CUSTOM_COMMAND(OUTPUT ${dstfile}
         COMMAND ${ECHO} 'const char ${module}_lua[] =' > ${tmpfile}
