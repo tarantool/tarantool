732c467efd5edfce70480ae3ae4d17cd180dc90b
Roman Tsisyk <roman@tsisyk.com>
2015-04-28 14:42:43 +0300

    Extract mysql and pg modules into separate repositories
     - mysql moved to https://github.com/tarantool/mysql
     - pg moved to https://github.com/tarantool/pg
    
    Please consult documentation on GitHub for more details.

--- test/module/net_sql.common.test.lua
+++
@@ -1,6 +0,0 @@
-package.path  = os.getenv("TARANTOOL_SRC_DIR").."/src/module/sql/?.lua"
-package.cpath  = "?.so"
-
-require("sql")
-if type(box.net.sql) ~= "table" then error("net.sql load failed") end
-
