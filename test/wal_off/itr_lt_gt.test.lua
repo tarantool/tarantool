f49817dcfe2751290cd9d6ef2335e10a3ecf80f4
Konstantin Osipov <kostja@tarantool.org>
2015-05-19 20:22:22 +0300

    gh-769: slightly reduce test execution time

--- test/wal_off/itr_lt_gt.test.lua
+++ test/wal_off/itr_lt_gt.test.lua
@@ -3,14 +3,14 @@
 s = box.schema.create_space('test')
 i = s:create_index('primary', { type = 'TREE', parts = {1, 'num', 2, 'num'} })
 s:insert{0, 0} s:insert{2, 0}
-for i=1,100000 do s:insert{1, i} end
+for i=1,10000 do s:insert{1, i} end
 test_itrs = {'EQ', 'REQ', 'GT', 'LT', 'GE', 'LE'}
 test_res = {}
 too_longs = {}
 
 --# setopt delimiter ';'
 function test_run_itr(itr, key)
-    for i=1,10000 do
+    for i=1,50 do
         local gen, param, state = s.index.primary:pairs({key}, {iterator = itr})
         state, v =  gen(param, state)
         test_res[itr .. ' ' .. key] = v
@@ -18,16 +18,17 @@ function test_run_itr(itr, key)
 end;
 
 function test_itr(itr, key)
-    local t = os.time()
     test_run_itr(itr, key)
-    if os.time() - t > 1 then
-        table.insert(too_longs, 'Too long ' .. itr .. ' ' .. key)
-    end
 end;
 
 for _,itr in pairs(test_itrs) do
+    local t = os.clock()
     for key = 0,2 do
         test_itr(itr, key)
+    end
+    local diff = os.clock() - t
+    if  diff > 0.05 then
+        table.insert(too_longs, 'Some of the iterators takes too long to position: '.. diff)
     end
 end;
 --# setopt delimiter ''
