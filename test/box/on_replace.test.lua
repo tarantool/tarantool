5ae66148d6056073c9112b5c885b7316c58e91e8
Sulverus <sulverus@gmail.com>
2015-05-26 11:36:50 +0300

    c triggers test case. lbox_list_all_triggers updated

--- test/box/on_replace.test.lua
+++ test/box/on_replace.test.lua
@@ -1,6 +1,11 @@
+-- test c and lua triggers: must return only lua triggers
+#box.space._space:on_replace()
+function f() print('test') end
+type(box.space._space:on_replace(f))
+#box.space._space:on_replace()
+
 ts = box.schema.space.create('test_space')
 ti = ts:create_index('primary', { type = 'hash' })
-
 type(ts.on_replace)
 ts.on_replace()
 ts:on_replace()
@@ -25,7 +30,6 @@ ts:get{2}
 
 function save_out(told, tnew) o = told n = tnew end
 type(ts:on_replace(save_out, fail))
-
 ts:insert{2, 'a', 'b', 'c'}
 o
 n
@@ -37,3 +41,9 @@ n
 type(ts:on_replace(function() test = 1 end))
 #ts:on_replace()
 ts:drop()
+
+-- test garbage in lua stack
+#box.space._space:on_replace()
+function f2() print('test2') end
+type(box.space._space:on_replace(f2))
+#box.space._space:on_replace()
