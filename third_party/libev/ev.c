8af99883e12a6a3784e69f9935c5cdb67f90006f
Konstantin Osipov <kostja@tarantool.org>
2015-07-06 18:24:22 +0300

    libev: kqueue works OK on modern FreeBSD, use it.

--- third_party/libev/ev.c
+++ third_party/libev/ev.c
@@ -2225,7 +2225,7 @@ ev_recommended_backends (void) EV_THROW
 {
   unsigned int flags = ev_supported_backends ();
 
-#ifndef __NetBSD__
+#if !defined(__NetBSD__) && !defined(__FreeBSD__)
   /* kqueue is borked on everything but netbsd apparently */
   /* it usually doesn't work correctly on anything but sockets and pipes */
   flags &= ~EVBACKEND_KQUEUE;
