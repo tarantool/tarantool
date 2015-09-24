53aa9e085e1af6e993251161c421d652a5665a0e
Nick Zavaritsky <mejedi@gmail.com>
2015-09-24 18:25:40 +0300

    Fix app/clock test (osx)

--- test/app/clock.test.lua
+++ test/app/clock.test.lua
@@ -12,5 +12,5 @@ test:ok(clock.thread64() > 0, "thread64")
 test:ok(clock.monotonic64() > 0, "monotonic64")
 test:ok(clock.proc64() > 0, "proc64")
 
-test:ok(clock.monotonic() < clock.monotonic(), "time is monotonic")
+test:ok(clock.monotonic() <= clock.monotonic(), "time is monotonic")
 test:ok(math.abs(clock.realtime() - os.time()) < 2, "clock.realtime ~ os.time")
