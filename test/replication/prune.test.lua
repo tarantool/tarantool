474fac89c821d9308e23dc7578da6045ae5177bf
Roman Tsisyk <roman@tsisyk.com>
2015-11-13 11:34:52 +0300

    Review fixes for gh-806: can't prune old replicas

--- test/replication/prune.test.lua
+++ test/replication/prune.test.lua
@@ -1,7 +1,14 @@
+print '-------------------------------------------------------------'
+print 'gh-806: cant prune old replicas by deleting their server ids'
+print '-------------------------------------------------------------'
+
 env = require('test_run')
 test_run = env.new('127.0.0.1', 8080)
 replica_set = require('fast_replica')
 fiber = require('fiber')
+
+box.space._cluster:len() == 1
+#box.info.vclock == 1
 
 box.schema.user.grant('guest', 'read,write,execute', 'universe')
 
@@ -11,39 +18,39 @@ index = box.space.test:create_index('primary')
 for i=1,10 do  space:insert{i, 'test'} end
 
 -- create max number of replicas and check
-replica_set.join(test_run, 30)
-while #box.space._cluster:select{} ~= 31 do fiber.sleep(0.001) end
+replica_set.join(test_run, box.schema.REPLICA_MAX - 2)
+while box.space._cluster:len() ~= box.schema.REPLICA_MAX - 1 do fiber.sleep(0.001) end
 
-#box.space._cluster:select{}
-#box.info.vclock
+box.space._cluster:len() == box.schema.REPLICA_MAX - 1
+#box.info.vclock == box.schema.REPLICA_MAX - 1
 
 -- try to add one more replica
 uuid = require('uuid')
-box.space._cluster:insert{#box.info.vclock + 1, uuid.str()}
+box.space._cluster:insert{box.schema.REPLICA_MAX, uuid.str()}
 
 -- Delete all replication nodes
 replica_set.drop_all(test_run)
-#box.space._cluster:select{}
-#box.info.vclock
+box.space._cluster:len() == 1
+#box.info.vclock == 1
 
--- restart and check
+-- Save a snapshot without removed replicas in vclock
 box.snapshot()
+-- Master is not crashed then recovering xlog with {replica_id: 0} in header
 test_run:cmd('restart server default')
 replica_set = require('fast_replica')
 fiber = require('fiber')
 
--- Rejoin 30 new nodes and check
-replica_set.join(test_run, 30)
-while #box.space._cluster:select{} ~= 31 do fiber.sleep(0.001) end
+-- Rejoin replica and check
+replica_set.join(test_run, 1)
+while box.space._cluster:len() ~= 2 do fiber.sleep(0.001) end
 
 -- Check server ids
-test = {}
-test_run:cmd('setopt delimiter ";"')
-for i=1,30 do
-    test[i] = test_run:cmd('eval replica' .. tostring(i) .. ' "return box.info.server.id"')
-end;
-test_run:cmd('setopt delimiter ""');
-test
+test_run:cmd('eval replica1 "return box.info.server.id"')
 
-#box.space._cluster:select{}
-#box.info.vclock
+box.space._cluster:len() == 2
+#box.info.vclock == 2
+
+-- Cleanup
+replica_set.drop_all(test_run)
+box.space._cluster:len() == 1
+#box.info.vclock == 1
