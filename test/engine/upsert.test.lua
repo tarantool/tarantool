3fe6e3332f55476c1542a4b565a21a7c4e995daa
Dmitry Simonenko <pmwkaa@gmail.com>
2015-11-19 18:23:32 +0300

    gh-1162-sophia-upsert: fix #1162; move sophia/upsert.test.lua to engine/

--- test/engine/upsert.test.lua
+++ test/engine/upsert.test.lua
@@ -1,6 +1,10 @@
 
+test_run = require('test_run')
+inspector = test_run.new()
+engine = inspector:get_cfg('engine')
+
 -- upsert (str)
-space = box.schema.space.create('test', { engine = 'sophia' })
+space = box.schema.space.create('test', { engine = engine })
 index = space:create_index('primary', { type = 'tree', parts = {1, 'str'} })
 for key = 1, 100 do space:upsert({tostring(key), 0}, {{'+', 2, 1}}) end
 t = {}
@@ -19,7 +23,7 @@ space:drop()
 
 
 -- upsert (num)
-space = box.schema.space.create('test', { engine = 'sophia' })
+space = box.schema.space.create('test', { engine = engine })
 index = space:create_index('primary', { type = 'tree', parts = {1, 'num'} })
 for key = 1, 100 do space:upsert({key, 0}, {{'+', 2, 1}}) end
 t = {}
@@ -38,7 +42,7 @@ space:drop()
 
 
 -- upsert multi-part (num, num)
-space = box.schema.space.create('test', { engine = 'sophia' })
+space = box.schema.space.create('test', { engine = engine })
 index = space:create_index('primary', { type = 'tree', parts = {1, 'num', 2, 'num'} })
 for key = 1, 100 do space:upsert({key, key, 0}, {{'+', 3, 1}}) end
 t = {}
@@ -57,14 +61,14 @@ space:drop()
 
 
 -- upsert default tuple constraint
-space = box.schema.space.create('test', { engine = 'sophia' })
+space = box.schema.space.create('test', { engine = engine })
 index = space:create_index('primary', { type = 'tree', parts = {1, 'num', 2, 'num'} })
 space:upsert({0, 'key', 0}, {{'+', 3, 1}})
 space:drop()
 
 
 -- upsert primary key modify (skipped)
-space = box.schema.space.create('test', { engine = 'sophia' })
+space = box.schema.space.create('test', { engine = engine })
 index = space:create_index('primary', { type = 'tree', parts = {1, 'num'} })
 space:upsert({0, 0}, {{'+', 1, 1}, {'+', 2, 1}})
 space:get({0})
