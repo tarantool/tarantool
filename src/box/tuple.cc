2b73a585b04e577f50b22b8fe8e4c21f0de340f5
Nick Zavaritsky <mejedi@gmail.com>
2015-12-03 20:37:38 +0000

    Switch to aligned region alocator in engines

--- src/box/tuple.cc
+++ src/box/tuple.cc
@@ -887,7 +887,7 @@ box_tuple_update(const box_tuple_t *tuple, const char *expr, const char *expr_en
 	try {
 		RegionGuard region_guard(&fiber()->gc);
 		struct tuple *new_tuple = tuple_update(tuple_format_ber,
-			region_alloc_xc_cb, &fiber()->gc, tuple,
+			region_aligned_alloc_xc_cb, &fiber()->gc, tuple,
 			expr, expr_end, 1);
 		return tuple_bless(new_tuple);
 	} catch (ClientError *e) {
@@ -901,7 +901,7 @@ box_tuple_upsert(const box_tuple_t *tuple, const char *expr, const char *expr_en
 	try {
 		RegionGuard region_guard(&fiber()->gc);
 		struct tuple *new_tuple = tuple_upsert(tuple_format_ber,
-			region_alloc_xc_cb, &fiber()->gc, tuple,
+			region_aligned_alloc_xc_cb, &fiber()->gc, tuple,
 			expr, expr_end, 1);
 		return tuple_bless(new_tuple);
 	} catch (ClientError *e) {
