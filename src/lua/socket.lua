7418f25a4d6ce102786e764bccfbcf9529ae1ca0
daurnimator <quae@daurnimator.com>
2015-12-14 02:36:36 +1100

    export iowait function from socket module

--- src/lua/socket.lua
+++ src/lua/socket.lua
@@ -1132,6 +1132,21 @@ local function tcp_server(host, port, opts, timeout)
     return nil
 end
 
+local function iowait(fd, events, timeout)
+    if type(events) == "string" then
+        -- events constants are taken from libev/ev.h
+        local ev = 0
+        if events:match"r" then
+            ev = ev + 1 -- EV_READ
+        end
+        if events:match"w" then
+            ev = ev + 2 -- EV_WRITE
+        end
+        events = ev
+    end
+    return internal.iowait(fd, events, timeout or TIMEOUT_INFINITY)
+end
+
 socket_mt   = {
     __index     = socket_methods,
     __tostring  = function(self)
@@ -1160,7 +1175,8 @@ socket_mt   = {
 return setmetatable({
     getaddrinfo = getaddrinfo,
     tcp_connect = tcp_connect,
-    tcp_server = tcp_server
+    tcp_server = tcp_server,
+    iowait = iowait
 }, {
     __call = function(self, ...) return create_socket(...) end;
 })
