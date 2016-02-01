c5a0528cf0777a377c2147afb79baeb1504b0ff9
Roman Tsisyk <roman@tsisyk.com>
2016-02-01 13:59:58 +0300

    Fix compilation with GCC 6.0.0
    > error: ‘__ac_prime_list’ defined but not used [-Werror=unused-const-variable]

--- src/lib/salad/mhash.h
+++ src/lib/salad/mhash.h
@@ -77,20 +77,6 @@
 typedef uint32_t mh_int_t;
 #endif /* MH_TYPEDEFS */
 
-#ifndef __ac_HASH_PRIME_SIZE
-#define __ac_HASH_PRIME_SIZE 31
-static const mh_int_t __ac_prime_list[__ac_HASH_PRIME_SIZE] = {
-	3ul,		11ul,		23ul,		53ul,
-	97ul,		193ul,		389ul,		769ul,
-	1543ul,		3079ul,		6151ul,		12289ul,
-	24593ul,	49157ul,	98317ul,	196613ul,
-	393241ul,	786433ul,	1572869ul,	3145739ul,
-	6291469ul,	12582917ul,	25165843ul,	50331653ul,
-	100663319ul,	201326611ul,	402653189ul,	805306457ul,
-	1610612741ul,	3221225473ul,	4294967291ul
-};
-#endif /* __ac_HASH_PRIME_SIZE */
-
 #ifndef MH_HEADER
 #define MH_HEADER
 
@@ -385,6 +371,20 @@ _mh(remove)(struct _mh(t) *h, const mh_node_t *node,
 }
 
 #ifdef MH_SOURCE
+
+#ifndef __ac_HASH_PRIME_SIZE
+#define __ac_HASH_PRIME_SIZE 31
+static const mh_int_t __ac_prime_list[__ac_HASH_PRIME_SIZE] = {
+	3ul,		11ul,		23ul,		53ul,
+	97ul,		193ul,		389ul,		769ul,
+	1543ul,		3079ul,		6151ul,		12289ul,
+	24593ul,	49157ul,	98317ul,	196613ul,
+	393241ul,	786433ul,	1572869ul,	3145739ul,
+	6291469ul,	12582917ul,	25165843ul,	50331653ul,
+	100663319ul,	201326611ul,	402653189ul,	805306457ul,
+	1610612741ul,	3221225473ul,	4294967291ul
+};
+#endif /* __ac_HASH_PRIME_SIZE */
 
 void __attribute__((noinline))
 _mh(del_resize)(struct _mh(t) *h, mh_int_t x,
