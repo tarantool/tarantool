22208110989aaeda5bb173b58350ea43f3786c22
Alexandr Lyapunov <a.lyapunov@corp.mail.ru>
2016-02-08 18:35:23 +0300

    fixed gh-1320 long run test hangs on finalizers.test

--- test/long_run/lua/finalizers.lua
+++ test/long_run/lua/finalizers.lua
@@ -1,8 +1,4 @@
 #!/usr/bin/env tarantool
-env = require('test_run')
-test_run = env.new()
-
-test_run:cmd("setopt delimiter ';'")
 
 function on_gc(t)
 end;
@@ -12,13 +8,11 @@ function test_finalizers()
     local i = 1
     local ffi = require('ffi')
     while true do
-        result[i] = ffi.gc(ffi.cast('void *', NULL), on_gc)
+        result[i] = ffi.gc(ffi.cast('void *', 0), on_gc)
         i = i + 1
     end
     return "done"
 end;
-
-test_run:cmd("setopt delimiter ''")
 
 test_finalizers()
 test_finalizers()
