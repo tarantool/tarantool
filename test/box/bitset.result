tarantool/tarantool

commit 9036eeeb80702cff55eaaf1a61a5b78cf9e3c015

Fix #1042: bad error message for _vspace, _vindex, _vuser, etc.

src/box/memtx_bitset.cc
@@ -253,15 +253,6 @@ MemtxBitset::allocIterator() const
 	return (struct iterator *) it;
 }
 
-struct tuple *
-MemtxBitset::findByKey(const char *key, uint32_t part_count) const
-{
-	(void) key;
-	(void) part_count;
-	tnt_raise(ClientError, ER_UNSUPPORTED, "MemtxBitset", "findByKey()");
-	return NULL;
-}
-
 static inline const char *
 make_key(const char *field, uint32_t *key_len)
 {
@@ -377,8 +368,7 @@ MemtxBitset::initIterator(struct iterator *iterator, enum iterator_type type,
 						       bitset_key_size);
 			break;
 		default:
-			tnt_raise(ClientError, ER_UNSUPPORTED,
-				  "MemtxBitset", "requested iterator type");
+			return initIterator(iterator, type, key, part_count);
 		}
 
 		if (rc != 0) {

src/box/memtx_rtree.cc
@@ -156,10 +156,9 @@ MemtxRTree::MemtxRTree(struct key_def *key_def)
 	m_dimension = key_def->opts.dimension;
 	if (m_dimension < 1 || m_dimension > RTREE_MAX_DIMENSION) {
 		char message[64];
-		snprintf(message, 64, "dimension (%u) must belong to range "
+		snprintf(message, 64, "dimension (%u): must belong to range "
 			 "[%u, %u]", m_dimension, 1, RTREE_MAX_DIMENSION);
-		tnt_raise(ClientError, ER_UNSUPPORTED,
-			  "RTREE index", message);
+		tnt_raise(UnsupportedIndexFeature, this, message);
 	}
 
 	memtx_index_arena_init();
@@ -242,10 +241,8 @@ MemtxRTree::initIterator(struct iterator *iterator, enum iterator_type type,
 	struct rtree_rect rect;
 	if (part_count == 0) {
 		if (type != ITER_ALL) {
-			tnt_raise(ClientError, ER_UNSUPPORTED,
-				  "R-Tree index",
-				  "It is possible to omit "
-				  "key only for ITER_ALL");
+			tnt_raise(UnsupportedIndexFeature, this,
+				  "empty keys for requested iterator type");
 		}
 	} else if (mp_decode_rect_from_key(&rect, m_dimension,
 					   key, part_count)) {
@@ -280,8 +277,7 @@ MemtxRTree::initIterator(struct iterator *iterator, enum iterator_type type,
 		op = SOP_NEIGHBOR;
 		break;
 	default:
-		tnt_raise(ClientError, ER_UNSUPPORTED,
-			  "RTREE index", "Unsupported search operation for RTREE");
+		return initIterator(iterator, type, key, part_count);
 	}
 	rtree_search(&m_tree, &rect, op, &it->impl);
 }

src/box/sophia_index.cc
@@ -690,8 +690,8 @@ SophiaIndex::initIterator(struct iterator *ptr,
 	assert(it->cursor == NULL);
 	if (part_count > 0) {
 		if (part_count != key_def->part_count) {
-			tnt_raise(ClientError, ER_UNSUPPORTED,
-			          "Sophia Index iterator", "partial keys");
+			tnt_raise(UnsupportedIndexFeature, this,
+				  "partial keys");
 		}
 	} else {
 		key = NULL;
@@ -718,8 +718,7 @@ SophiaIndex::initIterator(struct iterator *ptr,
 	case ITER_LT: compare = "<";
 		break;
 	default:
-		tnt_raise(ClientError, ER_UNSUPPORTED,
-		          "Sophia Index", "requested iterator type");
+		return initIterator(ptr, type, key, part_count);
 	}
 	it->base.next = sophia_iterator_next;
 	it->cursor = sp_cursor(env);

test/box/rtree_misc.result
@@ -193,11 +193,11 @@ s.index.spatial:count()
 ...
 s.index.spatial:min()
 ---
-- error: RTREE does not support min()
+- error: Index 'spatial' (RTREE) of space 'spatial' (memtx) does not support min()
 ...
 s.index.spatial:max()
 ---
-- error: RTREE does not support max()
+- error: Index 'spatial' (RTREE) of space 'spatial' (memtx) does not support max()
 ...
 -- seems that drop can't fail
 s.index.spatial:drop()
@@ -493,12 +493,14 @@ i = s:create_index('p')
 -- dimension too big
 i = s:create_index('s', {type = 'rtree', parts = {2, 'array'}, dimension = 21})
 ---
-- error: RTREE index does not support dimension (21) must belong to range [1, 20]
+- error: 'Index ''s'' (RTREE) of space ''s'' (memtx) does not support dimension (21):
+    must belong to range [1, 20]'
 ...
 -- dimension too low
 i = s:create_index('s', {type = 'rtree', parts = {2, 'array'}, dimension = 0})
 ---
-- error: RTREE index does not support dimension (0) must belong to range [1, 20]
+- error: 'Index ''s'' (RTREE) of space ''s'' (memtx) does not support dimension (0):
+    must belong to range [1, 20]'
 ...
 -- cant be unique
 i = s:create_index('s', {type = 'rtree', parts = {2, 'array'}, unique = true})
@@ -576,7 +578,8 @@ box.space._index:insert{s.id, 2, 's', 'rtree', empty_map, {{2, 'array'}}}
 ...
 box.space._index:insert{s.id, 2, 's', 'rtree', {unique = false, dimension = 22}, {{2, 'array'}}}
 ---
-- error: RTREE index does not support dimension (22) must belong to range [1, 20]
+- error: 'Index ''s'' (RTREE) of space ''s'' (memtx) does not support dimension (22):
+    must belong to range [1, 20]'
 ...
 box.space._index:insert{s.id, 2, 's', 'rtree', {unique = false, dimension = 'dimension'}, {{2, 'array'}}}
 ---

test/big/iterator.result
@@ -893,7 +893,8 @@ iterate('tweedledum', 'i5', 1, 3, box.index.EQ, 'sid_005', 'tid_995', 'a')
 -------------------------------------------------------------------------------
 space.index['primary']:pairs({}, {iterator = -666 })
 ---
-- error: Tree index does not support requested iterator type
+- error: Index 'primary' (TREE) of space 'tweedledum' (memtx) does not support requested
+    iterator type
 ...
 -- Test cases for #123: box.index.count does not check arguments properly
 space.index['primary']:pairs(function() end, { iterator = box.index.EQ })
@@ -1079,7 +1080,8 @@ space:select(2)
 ...
 space:select(5, {iterator="GE"})
 ---
-- error: Hash index does not support requested iterator type
+- error: Index 'primary' (HASH) of space 'test' (memtx) does not support requested
+    iterator type
 ...
 space:select(nil, {iterator="GE"})
 ---

src/box/errcode.h
@@ -165,6 +165,9 @@ struct errcode_record {
 	/*109 */_(ER_WRONG_SCHEMA_VERSION, 2, "Wrong schema version, current: %d, in request: %u") \
 	/*110 */_(ER_SLAB_ALLOC_MAX, 2, "Failed to allocate %u bytes for tuple in the slab allocator: tuple is too large. Check 'slab_alloc_maximal' configuration option.") \
 	/*112 */_(ER_WRONG_SPACE_OPTIONS, 2, "Wrong space options (field %u): %s") \
+	/*113 */_(ER_UNSUPPORTED_INDEX_FEATURE,	2, "Index '%s' (%s) of space '%s' (%s) does not support %s") \
+	/*114 */_(ER_VIEW_IS_RO,		2, "View '%s' is read-only") \
+
 
 /*
  * !IMPORTANT! Please follow instructions at start of the file

src/box/index.cc
@@ -59,6 +59,18 @@ static_assert(sizeof(iterator_type_strs) / sizeof(const char *) ==
 
 /* {{{ Utilities. **********************************************/
 
+UnsupportedIndexFeature::UnsupportedIndexFeature(const char *file,
+	unsigned line, const Index *index, const char *what)
+	: ClientError(file, line, ER_UNKNOWN)
+{
+	struct key_def *key_def = index->key_def;
+	struct space *space = space_cache_find(key_def->space_id);
+	m_errcode = ER_UNSUPPORTED_INDEX_FEATURE;
+	error_format_msg(this, tnt_errcode_desc(m_errcode), key_def->name,
+			 index_type_strs[key_def->type],
+			 space->def.name, space->def.engine_name, what);
+}
+
 void
 key_validate_parts(struct key_def *key_def, const char *key,
 		   uint32_t part_count)
@@ -160,26 +172,21 @@ Index::~Index()
 size_t
 Index::size() const
 {
-	tnt_raise(ClientError, ER_UNSUPPORTED,
-	          index_type_strs[key_def->type],
-	          "size()");
+	tnt_raise(UnsupportedIndexFeature, this, "size()");
+	return 0;
 }
 
 struct tuple *
 Index::min(const char* /* key */, uint32_t /* part_count */) const
 {
-	tnt_raise(ClientError, ER_UNSUPPORTED,
-		  index_type_strs[key_def->type],
-		  "min()");
+	tnt_raise(UnsupportedIndexFeature, this, "min()");
 	return NULL;
 }
 
 struct tuple *
 Index::max(const char* /* key */, uint32_t /* part_count */) const
 {
-	tnt_raise(ClientError, ER_UNSUPPORTED,
-		  index_type_strs[key_def->type],
-		  "max()");
+	tnt_raise(UnsupportedIndexFeature, this, "max()");
 	return NULL;
 }
 
@@ -187,9 +194,7 @@ struct tuple *
 Index::random(uint32_t rnd) const
 {
 	(void) rnd;
-	tnt_raise(ClientError, ER_UNSUPPORTED,
-		  index_type_strs[key_def->type],
-		  "random()");
+	tnt_raise(UnsupportedIndexFeature, this, "random()");
 	return NULL;
 }
 
@@ -197,19 +202,35 @@ size_t
 Index::count(enum iterator_type /* type */, const char* /* key */,
              uint32_t /* part_count */) const
 {
-	tnt_raise(ClientError, ER_UNSUPPORTED,
-		  index_type_strs[key_def->type],
-		  "count()");
+	tnt_raise(UnsupportedIndexFeature, this, "count()");
 	return 0;
+}
+
+struct tuple *
+Index::findByKey(const char *key, uint32_t part_count) const
+{
+	(void) key;
+	(void) part_count;
+	tnt_raise(UnsupportedIndexFeature, this, "findByKey()");
+	return NULL;
 }
 
 struct tuple *
 Index::findByTuple(struct tuple *tuple) const
 {
 	(void) tuple;
-	tnt_raise(ClientError, ER_UNSUPPORTED,
-		  index_type_strs[key_def->type],
-		  "findByTuple()");
+	tnt_raise(UnsupportedIndexFeature, this, "findByTuple()");
+	return NULL;
+}
+
+struct tuple *
+Index::replace(struct tuple *old_tuple, struct tuple *new_tuple,
+		     enum dup_replace_mode mode)
+{
+	(void) old_tuple;
+	(void) new_tuple;
+	(void) mode;
+	tnt_raise(UnsupportedIndexFeature, this, "replace()");
 	return NULL;
 }
 
@@ -217,6 +238,17 @@ size_t
 Index::bsize() const
 {
 	return 0;
+}
+
+void
+Index::initIterator(struct iterator *ptr, enum iterator_type type,
+		    const char *key, uint32_t part_count) const
+{
+	(void) ptr;
+	(void) type;
+	(void) key;
+	(void) part_count;
+	tnt_raise(UnsupportedIndexFeature, this, "requested iterator type");
 }
 
 /**
@@ -227,9 +259,7 @@ void
 Index::createReadViewForIterator(struct iterator *iterator)
 {
 	(void) iterator;
-	tnt_raise(ClientError, ER_UNSUPPORTED,
-		  index_type_strs[key_def->type],
-		  "consistent read view");
+	tnt_raise(UnsupportedIndexFeature, this, "consistent read view");
 }
 
 /**
@@ -240,9 +270,7 @@ void
 Index::destroyReadViewForIterator(struct iterator *iterator)
 {
 	(void) iterator;
-	tnt_raise(ClientError, ER_UNSUPPORTED,
-		  index_type_strs[key_def->type],
-		  "consistent read view");
+	tnt_raise(UnsupportedIndexFeature, this, "consistent read view");
 }
 
 static inline Index *
@@ -341,9 +369,7 @@ box_index_min(uint32_t space_id, uint32_t index_id, const char *key,
 		Index *index = check_index(space_id, index_id, &space);
 		if (index->key_def->type != TREE) {
 			/* Show nice error messages in Lua */
-			tnt_raise(ClientError, ER_UNSUPPORTED,
-				  index_type_strs[index->key_def->type],
-				  "min()");
+			tnt_raise(UnsupportedIndexFeature, index, "min()");
 		}
 		uint32_t part_count = key ? mp_decode_array(&key) : 0;
 		key_validate(index->key_def, ITER_GE, key, part_count);
@@ -370,9 +396,7 @@ box_index_max(uint32_t space_id, uint32_t index_id, const char *key,
 		Index *index = check_index(space_id, index_id, &space);
 		if (index->key_def->type != TREE) {
 			/* Show nice error messages in Lua */
-			tnt_raise(ClientError, ER_UNSUPPORTED,
-				  index_type_strs[index->key_def->type],
-				  "max()");
+			tnt_raise(UnsupportedIndexFeature, index, "max()");
 		}
 		uint32_t part_count = key ? mp_decode_array(&key) : 0;
 		key_validate(index->key_def, ITER_LE, key, part_count);

src/box/index.h
@@ -330,11 +330,11 @@ public:
 	virtual struct tuple *random(uint32_t rnd) const;
 	virtual size_t count(enum iterator_type type, const char *key,
 			     uint32_t part_count) const;
-	virtual struct tuple *findByKey(const char *key, uint32_t part_count) const = 0;
+	virtual struct tuple *findByKey(const char *key, uint32_t part_count) const;
 	virtual struct tuple *findByTuple(struct tuple *tuple) const;
 	virtual struct tuple *replace(struct tuple *old_tuple,
 				      struct tuple *new_tuple,
-				      enum dup_replace_mode mode) = 0;
+				      enum dup_replace_mode mode);
 	virtual size_t bsize() const;
 
 	/**
@@ -356,6 +356,16 @@ public:
 	 * for which createReadViewForIterator() was called.
 	 */
 	virtual void destroyReadViewForIterator(struct iterator *iterator);
+};
+
+/*
+ * A wrapper for ClientError(ER_UNSUPPORTED_INDEX_FEATURE, ...) to format
+ * nice error messages (see gh-1042). You never need to catch this class.
+ */
+class UnsupportedIndexFeature: public ClientError {
+public:
+	UnsupportedIndexFeature(const char *file, unsigned line,
+				const Index *index, const char *what);
 };
 
 struct IteratorGuard

commit 3136f5e0ecc580a5735739f4b3077d7b68da3fb0

Fix #1467: iterator types are not validated in request
Regression introduced by 9036eeeb80702cff55eaaf1a61a5b78cf9e3c015

src/box/memtx_bitset.cc
@@ -368,7 +368,8 @@ MemtxBitset::initIterator(struct iterator *iterator, enum iterator_type type,
 						       bitset_key_size);
 			break;
 		default:
-			return initIterator(iterator, type, key, part_count);
+			return Index::initIterator(iterator, type, key,
+						   part_count);
 		}
 
 		if (rc != 0) {

src/box/memtx_rtree.cc
@@ -277,7 +277,7 @@ MemtxRTree::initIterator(struct iterator *iterator, enum iterator_type type,
 		op = SOP_NEIGHBOR;
 		break;
 	default:
-		return initIterator(iterator, type, key, part_count);
+		return Index::initIterator(iterator, type, key, part_count);
 	}
 	rtree_search(&m_tree, &rect, op, &it->impl);
 }

src/box/sophia_index.cc
@@ -733,7 +733,7 @@ SophiaIndex::initIterator(struct iterator *ptr,
 	case ITER_LT: compare = "<";
 		break;
 	default:
-		return initIterator(ptr, type, key, part_count);
+		return Index::initIterator(ptr, type, key, part_count);
 	}
 	it->cursor = sp_cursor(env);
 	if (it->cursor == NULL)

test/box/bitset.result
@@ -1838,3 +1838,27 @@ dump(box.index.BITS_ANY_SET, 113)
 drop_space()
 ---
 ...
+------------------------------------------------------------------------------
+-- Misc
+------------------------------------------------------------------------------
+-- gh-1467: invalid iterator type
+space = box.schema.space.create('test')
+---
+...
+_ = space:create_index('primary', { type = 'hash', parts = {1, 'num'}, unique = true })
+---
+...
+_ = space:create_index('bitset', { type = 'bitset', parts = {2, 'num'}, unique = false })
+---
+...
+space.index.bitset:select({1}, { iterator = 'OVERLAPS'})
+---
+- error: Index 'bitset' (BITSET) of space 'test' (memtx) does not support requested
+    iterator type
+...
+space:drop()
+---
+...
+space = nil
+---
+...

test/box/bitset.test.lua
@@ -95,3 +95,15 @@ dump(box.index.BITS_ANY_SET, 84)
 dump(box.index.BITS_ANY_SET, 113)
 
 drop_space()
+
+------------------------------------------------------------------------------
+-- Misc
+------------------------------------------------------------------------------
+
+-- gh-1467: invalid iterator type
+space = box.schema.space.create('test')
+_ = space:create_index('primary', { type = 'hash', parts = {1, 'num'}, unique = true })
+_ = space:create_index('bitset', { type = 'bitset', parts = {2, 'num'}, unique = false })
+space.index.bitset:select({1}, { iterator = 'OVERLAPS'})
+space:drop()
+space = nil

test/box/hash.result
@@ -717,3 +717,18 @@ box.space.test:insert{1}
 box.space.test:drop()
 ---
 ...
+-- gh-1467: invalid iterator type
+space = box.schema.space.create('test')
+---
+...
+index = space:create_index('primary', { type = 'hash' })
+---
+...
+space:select({1}, {iterator = 'BITS_ALL_SET' } )
+---
+- error: Index 'primary' (HASH) of space 'test' (memtx) does not support requested
+    iterator type
+...
+space:drop()
+---
+...

test/box/hash.test.lua
@@ -307,3 +307,9 @@ _ = box.schema.create_space('test')
 _ = box.space.test:create_index('i',{parts={1,'STR'}})
 box.space.test:insert{1}
 box.space.test:drop()
+
+-- gh-1467: invalid iterator type
+space = box.schema.space.create('test')
+index = space:create_index('primary', { type = 'hash' })
+space:select({1}, {iterator = 'BITS_ALL_SET' } )
+space:drop()

test/box/rtree_misc.result
@@ -690,6 +690,12 @@ i:select({0, 0, 0}, {iterator = 'neighbor'})
 - - [2, [2, 0, 0]]
   - [1, [1, 1, 1]]
 ...
+-- gh-1467: invalid iterator type
+i:select({1, 2, 3, 4, 5, 6}, {iterator = 'BITS_ALL_SET' } )
+---
+- error: Index 's' (RTREE) of space 's' (memtx) does not support requested iterator
+    type
+...
 s:drop()
 ---
 ...

test/box/rtree_misc.test.lua
@@ -241,4 +241,7 @@ i:select({0, 0, 0}, {iterator = 'neighbor'})
 i:alter{distance = 'manhattan' }
 i:select({0, 0, 0}, {iterator = 'neighbor'})
 
+-- gh-1467: invalid iterator type
+i:select({1, 2, 3, 4, 5, 6}, {iterator = 'BITS_ALL_SET' } )
+
 s:drop()

test/engine/iterator.result
@@ -7,6 +7,10 @@ inspector = test_run.new()
 engine = inspector:get_cfg('engine')
 ---
 ...
+inspector:cmd("push filter '"..engine.."' to 'engine'")
+---
+- true
+...
 -- iterator (str)
 space = box.schema.space.create('test', { engine = engine })
 ---
@@ -2926,4 +2930,23 @@ t
 ...
 space:drop()
 ---
+...
+-- gh-1467: invalid iterator type
+space = box.schema.space.create('test', { engine = engine })
+---
+...
+index = space:create_index('primary')
+---
+...
+space:select({}, {iterator = 'BITS_ALL_SET' } )
+---
+- error: Index 'primary' (TREE) of space 'test' (engine) does not support requested
+    iterator type
+...
+space:drop()
+---
+...
+inspector:cmd("clear filter")
+---
+- true
 ...

test/engine/iterator.test.lua
@@ -1,6 +1,7 @@
 test_run = require('test_run')
 inspector = test_run.new()
 engine = inspector:get_cfg('engine')
+inspector:cmd("push filter '"..engine.."' to 'engine'")
 
 -- iterator (str)
 space = box.schema.space.create('test', { engine = engine })
@@ -91,3 +92,11 @@ t
 t = {} for state, v in index:pairs(box.tuple.new(tostring(77)), {iterator = 'LT'}) do table.insert(t, v) end
 t
 space:drop()
+
+-- gh-1467: invalid iterator type
+space = box.schema.space.create('test', { engine = engine })
+index = space:create_index('primary')
+space:select({}, {iterator = 'BITS_ALL_SET' } )
+space:drop()
+
+inspector:cmd("clear filter")