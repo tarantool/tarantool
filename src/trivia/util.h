e782dac9c9e92f0c59ad690c1d19716c1dd54525
Svyatoslav Feldsherov <feldsherov_sv@mail.ru>
2016-07-20 10:58:25 +0300

    Fix mistype in util.h

--- src/trivia/util.h
+++ src/trivia/util.h
@@ -96,7 +96,7 @@ strindex(const char **haystack, const char *needle, uint32_t hmax);
 #define __offsetof offsetof
 #endif
 
-#ifndef containerof
+#ifndef container_of
 #define container_of(ptr, type, member) ({ \
 	const typeof( ((type *)0)->member  ) *__mptr = (ptr); \
 	(type *)( (char *)__mptr - offsetof(type,member)  );})
