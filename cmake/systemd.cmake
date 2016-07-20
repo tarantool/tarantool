2e64a8c930dc1e4fdcc20a3318a373c693a2d5e5
Roman Tsisyk <roman@tsisyk.com>
2016-07-20 11:28:53 +0300

    Fix #1291: enable systemd for Debian packages

--- cmake/systemd.cmake
+++ cmake/systemd.cmake
@@ -34,6 +34,14 @@ else()
             "${SYSTEMD_UNIT_DIR}")
     endif()
 
+    if ("${SYSTEMD_GENERATOR_DIR}" STREQUAL "")
+        execute_process(COMMAND ${PKG_CONFIG_EXECUTABLE}
+            --variable=systemdsystemgeneratordir systemd
+            OUTPUT_VARIABLE SYSTEMD_GENERATOR_DIR)
+        string(REGEX REPLACE "[ \t\n]+" "" SYSTEMD_GENERATOR_DIR
+            "${SYSTEMD_GENERATOR_DIR}")
+    endif()
+
     if ("${SYSTEMD_TMPFILES_DIR}" STREQUAL "")
         # NOTE: don't use ${CMAKE_INSTALL_LIBDIR} here
         set(SYSTEMD_TMPFILES_DIR "${CMAKE_INSTALL_PREFIX}/lib/tmpfiles.d")
@@ -41,11 +49,13 @@ else()
 
     set(WITH_SYSTEMD "ON")
     message(STATUS "SYSTEMD_UNIT_DIR: ${SYSTEMD_UNIT_DIR}")
+    message(STATUS "SYSTEMD_GENERATOR_DIR: ${SYSTEMD_GENERATOR_DIR}")
     message(STATUS "SYSTEMD_TMPFILES_DIR: ${SYSTEMD_TMPFILES_DIR}")
     # Disable sysvinit if it wasn't implicitly requested
     if (NOT DEFINED WITH_SYSVINIT)
         set(WITH_SYSVINIT "OFF")
     endif()
 endif()
+file(APPEND "${_OptionalPackagesFile}" "\n")
 file(APPEND "${_OptionalPackagesFile}" "-- WITH_SYSTEMD=${WITH_SYSTEMD}\n")
 file(APPEND "${_OptionalPackagesFile}" "-- WITH_SYSVINIT=${WITH_SYSVINIT}\n")
