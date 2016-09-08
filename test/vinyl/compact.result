tarantool/tarantool

commit f991838a413346911b6ee0775c9e22606c281d0d

vinyl: split vy_dump into begin and commit stages
Call 'begin' from task->execute and 'commit' from task->complete.
'begin' writes runs to the disk, while 'commit' manipulates the range
tree, renames range files and updates range index. After this change we
don't need index->lock anymore - the next patch gets rid of it.

This patch makes vy_task look like a real mess - we probably should keep
arguments for different tasks as structs in a union. Also, the wrappers
around vy_dump_{begin,commit} as well as vy_range_compact_{begin,commit}
doesn't look very helpful. But let's leave mere cleanups and refactoring
up to the following patches.

Closes #1685

src/box/vinyl.c
@@ -3385,39 +3385,30 @@ static int64_t
 vy_index_range_id_next(struct vy_index *index);
 
 static int
-vy_dump(struct vy_index *index, struct vy_range *range,
-        int64_t vlsn, int64_t *p_quota_release)
+vy_range_create(struct vy_range *range, struct vy_index *index);
+
+static int
+vy_range_complete(struct vy_range *range, struct vy_index *index);
+
+static int
+vy_dump_begin(struct vy_index *index, struct vy_range *range,
+	      struct vy_mem *i, int64_t vlsn, struct vy_run **result)
 {
 	assert(range->flags & VY_LOCK);
 
-	vy_index_wrlock(index);
-	if (unlikely(range->used == 0)) {
-		vy_range_unlock(range);
-		vy_index_unlock(index);
-		return 0;
-	}
-	struct vy_mem *i;
-	i = vy_range_rotate(range);
-	vy_index_unlock(index);
-
-	char tmp_path[PATH_MAX];
 	if (!range->run) {
 		/* An empty range, create a temp file for it. */
-		snprintf(tmp_path, PATH_MAX, "%s/.rngXXXXXX",
-			 index->path);
-		range->fd = mkstemp(tmp_path);
-		if (range->fd == -1) {
-			vy_error("Can't create temporary file in %s: %s",
-				 index->path, strerror(errno));
+		if (vy_range_create(range, index) < 0)
 			return -1;
-		}
 	}
 
-	struct vy_run *run = NULL;
-	int rc = vy_run_create(index, range, i, vlsn, &run);
-	if (unlikely(rc == -1))
-		return -1;
+	return vy_run_create(index, range, i, vlsn, result);
+}
 
+static int
+vy_dump_commit(struct vy_index *index, struct vy_range *range,
+	       struct vy_mem *i, int64_t *p_quota_release, struct vy_run *run)
+{
 	if (unlikely(run == NULL)) {
 		vy_index_wrlock(index);
 		assert(range->used >= i->used);
@@ -3454,16 +3445,8 @@ vy_dump(struct vy_index *index, struct vy_range *range,
 
 	if (range->run_count == 1) {
 		/* First non-empty run for this range, deploy the range. */
-		range->id = vy_index_range_id_next(index);
-
-		char path[PATH_MAX];
-		snprintf(path, PATH_MAX, "%s/%016"PRIx64".range",
-			 index->path, range->id);
-		if (rename(tmp_path, path)) {
-			vy_error("Can't rename temporary file in %s: %s",
-				 index->path, strerror(errno));
+		if (vy_range_complete(range, index) < 0)
 			return -1;
-		}
 		/*
 		 * The range file was created successfully,
 		 * update the range index on disk.
@@ -4514,6 +4497,11 @@ struct vy_task {
 
 	struct vy_index *index;
 	struct vy_range *range;
+
+	/*
+	 * Memory range to dump.
+	 */
+	struct vy_mem *range_mem;
 	/*
 	 * View sequence number at the time when the task was scheduled.
 	 */
@@ -4526,6 +4514,10 @@ struct vy_task {
 	 * Result of vy_range_compact_begin() is stored here.
 	 */
 	struct rlist compact_result;
+	/*
+	 * Result of vy_dump_begin() is stored here.
+	 */
+	struct vy_run *dump_result;
 	/*
 	 * A link in the list of all pending tasks, generated by
 	 * task scheduler.
@@ -4569,12 +4561,20 @@ vy_task_delete(struct mempool *pool, struct vy_task *task)
 static int
 vy_task_dump_execute(struct vy_task *task)
 {
-	return vy_dump(task->index, task->range, task->vlsn,
-		       &task->quota_release);
+	return vy_dump_begin(task->index, task->range, task->range_mem,
+			     task->vlsn, &task->dump_result);
+}
+
+static int
+vy_task_dump_complete(struct vy_task *task)
+{
+	return vy_dump_commit(task->index, task->range, task->range_mem,
+			      &task->quota_release, task->dump_result);
 }
 
 static struct vy_task_ops vy_task_dump_ops = {
 	.execute = vy_task_dump_execute,
+	.complete = vy_task_dump_complete,
 };
 
 static int
@@ -4773,15 +4773,17 @@ vy_scheduler_peek_checkpoint(struct vy_scheduler *scheduler,
 		range = container_of(pn, struct vy_range, nodedump);
 		if (range->i0.min_lsn > checkpoint_lsn)
 			continue;
-		if (range->flags & VY_LOCK) {
+		if (range->flags & VY_LOCK)
 			continue;
-		}
+		if (range->used == 0)
+			continue;
 		struct vy_task *task = vy_task_new(&scheduler->task_pool,
 						   index, &vy_task_dump_ops);
 		if (task == NULL)
 			return -1; /* OOM */
 		vy_range_lock(range);
 		task->range = range;
+		task->range_mem = vy_range_rotate(range);
 		*ptask = task;
 		return 0; /* new task */
 	}
@@ -4809,6 +4811,7 @@ vy_scheduler_peek_dump(struct vy_scheduler *scheduler, struct vy_index *index,
 			return -1; /* oom */
 		vy_range_lock(range);
 		task->range = range;
+		task->range_mem = vy_range_rotate(range);
 		*ptask = task;
 		return 0; /* new task */
 	}
@@ -4833,6 +4836,8 @@ vy_scheduler_peek_age(struct vy_scheduler *scheduler, struct vy_index *index,
 		range = container_of(pn, struct vy_range, nodedump);
 		if (range->flags & VY_LOCK)
 			continue;
+		if (range->used == 0)
+			continue;
 		if (range->update_time + max_age > now)
 			continue;
 		struct vy_task *task = vy_task_new(&scheduler->task_pool,
@@ -4841,6 +4846,7 @@ vy_scheduler_peek_age(struct vy_scheduler *scheduler, struct vy_index *index,
 			return -1; /* oom */
 		vy_range_lock(range);
 		task->range = range;
+		task->range_mem = vy_range_rotate(range);
 		*ptask = task;
 		return 0; /* new task */
 	}

commit 0adb17e106850956edf6c003e3ac4ce83ba5d79f

vinyl: re-run scheduler without delay if some tasks have been processed
Fix regression introduced by f991838a413346911b6ee0775c9e22606c281d0d

src/box/vinyl.c
@@ -4993,6 +4993,19 @@ vy_scheduler_f(va_list va)
 			vy_task_delete(&scheduler->task_pool, task);
 		}
 
+		if (!stailq_empty(&output_queue)) {
+			/*
+			 * At least one task has been processed and
+			 * initial conditions may have been changed.
+			 * For example, VY_DUMP task increases run_count and
+			 * may be followed by VY_COMPACT task.
+			 * Don't wait for a while and re-run scheduler
+			 * on the next event loop iteration.
+			 */
+			fiber_reschedule();
+			continue;
+		}
+
 		/*
 		 * ipc_channel_get_timeout() is used to
 		 * schedule periodic tasks, 5 seconds is

test/vinyl/compact.result
@@ -1,0 +1,90 @@
+test_run = require('test_run').new()
+---
+...
+fiber = require('fiber')
+---
+...
+space = box.schema.space.create("vinyl", { engine = 'vinyl' })
+---
+...
+_= space:create_index('primary', { parts = { 1, 'unsigned' } })
+---
+...
+function vyinfo() return box.info.vinyl().db[box.space.vinyl.id..'/0'] end
+---
+...
+vyinfo().run_count == 0
+---
+- true
+...
+-- create the frist run
+space:insert({1})
+---
+- [1]
+...
+space:replace({1, 2})
+---
+- [1, 2]
+...
+space:upsert({1},{{'=', 4, 5}})
+---
+...
+space:select()
+---
+- - [1, 2]
+...
+space:select()
+---
+- - [1, 2]
+...
+-- gh-1571: bad upsert should not log on reads
+test_run:grep_log('default', 'UPSERT operation failed') == nil
+---
+- true
+...
+box.snapshot()
+---
+- ok
+...
+vyinfo().run_count == 1
+---
+- true
+...
+-- create the second run
+space:replace({2,2})
+---
+- [2, 2]
+...
+space:upsert({2},{{'=',4,5}})
+---
+...
+box:snapshot() -- create the second run
+---
+- ok
+...
+vyinfo().run_count == 2
+---
+- true
+...
+-- wait for compaction
+while vyinfo().run_count >= 2 do fiber.sleep(0.1) end
+---
+...
+vyinfo().run_count == 1
+---
+- true
+...
+-- gh-1571: bad upsert should log on compaction
+test_run:grep_log('default', 'UPSERT operation failed') ~= nil
+---
+- true
+...
+space:drop()
+---
+...
+fiber = nil
+---
+...
+test_run = nil
+---
+...

test/vinyl/compact.test.lua
@@ -1,0 +1,39 @@
+test_run = require('test_run').new()
+fiber = require('fiber')
+
+space = box.schema.space.create("vinyl", { engine = 'vinyl' })
+_= space:create_index('primary', { parts = { 1, 'unsigned' } })
+
+function vyinfo() return box.info.vinyl().db[box.space.vinyl.id..'/0'] end
+
+vyinfo().run_count == 0
+
+-- create the frist run
+space:insert({1})
+space:replace({1, 2})
+space:upsert({1},{{'=', 4, 5}})
+space:select()
+space:select()
+-- gh-1571: bad upsert should not log on reads
+test_run:grep_log('default', 'UPSERT operation failed') == nil
+
+box.snapshot()
+vyinfo().run_count == 1
+
+-- create the second run
+space:replace({2,2})
+space:upsert({2},{{'=',4,5}})
+box:snapshot() -- create the second run
+vyinfo().run_count == 2
+
+-- wait for compaction
+while vyinfo().run_count >= 2 do fiber.sleep(0.1) end
+vyinfo().run_count == 1
+
+-- gh-1571: bad upsert should log on compaction
+test_run:grep_log('default', 'UPSERT operation failed') ~= nil
+
+space:drop()
+
+fiber = nil
+test_run = nil

test/vinyl/gh.result
@@ -376,60 +376,6 @@ s:drop()
 ---
 ...
 --
--- gh-1571: vinyl upsert with bad update should not log
---
-s = box.schema.space.create('t',{engine='vinyl'})
----
-...
-_ = s:create_index('primary',{})
----
-...
-s:insert{1}
----
-- [1]
-...
-s:replace{1,2}
----
-- [1, 2]
-...
-s:upsert({1},{{'=',4,5}})
----
-...
-s:select{}
----
-- - [1, 2]
-...
-s:select{}
----
-- - [1, 2]
-...
-test_run:grep_log('default', 'UPSERT operation failed') == nil
----
-- true
-...
-box.snapshot()
----
-- ok
-...
-s:replace{2,2}
----
-- [2, 2]
-...
-s:upsert({2},{{'=',4,5}})
----
-...
-box:snapshot()
----
-- ok
-...
-test_run:grep_log('default', 'UPSERT operation failed') ~= nil
----
-- true
-...
-s:drop()
----
-...
---
 -- gh-1684: vinyl: infinite cycle on box.snapshot()
 --
 -- Create and drop several indices

test/vinyl/gh.test.lua
@@ -154,25 +154,6 @@ s:upsert({1, 'test', 'failed'}, {{'=', 3, 33}, {'=', 4, nil}})
 s:select()
 s:drop()
 
-
---
--- gh-1571: vinyl upsert with bad update should not log
---
-s = box.schema.space.create('t',{engine='vinyl'})
-_ = s:create_index('primary',{})
-s:insert{1}
-s:replace{1,2}
-s:upsert({1},{{'=',4,5}})
-s:select{}
-s:select{}
-test_run:grep_log('default', 'UPSERT operation failed') == nil
-box.snapshot()
-s:replace{2,2}
-s:upsert({2},{{'=',4,5}})
-box:snapshot()
-test_run:grep_log('default', 'UPSERT operation failed') ~= nil
-s:drop()
-
 --
 -- gh-1684: vinyl: infinite cycle on box.snapshot()
 --

test/vinyl/info.result
@@ -44,7 +44,7 @@ test_run:cmd("setopt delimiter ';'")
 for _, v in ipairs({ 'path', 'build', 'tx_latency', 'cursor_latency',
                      'get_latency', 'gc_active', 'run_avg', 'run_count',
                      'page_count', 'memory_used', 'run_max', 'run_histogram',
-                     'size', 'size_uncompressed', 'used'}) do
+                     'size', 'size_uncompressed', 'used', 'count'}) do
     test_run:cmd("push filter '"..v..": .*' to '"..v..": <"..v..">'")
 end;
 ---
@@ -71,17 +71,17 @@ box_info_sort(box.info.vinyl())
       - dump_age: 0
   - db:
     - 512/0:
-      - count: 2
+      - count: <count>
       - count_dup: 0
       - memory_used: <used>
-      - page_count: <page_count>
+      - page_count: <count>
       - page_size: <size>
-      - range_count: 1
+      - range_count: <count>
       - range_size: <size>
       - read_cache: 0
       - read_disk: 0
       - run_avg: <run_avg>
-      - run_count: <run_count>
+      - run_count: <count>
       - run_histogram: <run_histogram>
       - run_max: <run_max>
       - size: <size>
@@ -109,7 +109,7 @@ box_info_sort(box.info.vinyl())
     - tx_latency: <tx_latency>
     - tx_ops: 0 1 1.0
     - tx_rollback: 1
-    - write_count: 2
+    - write_count: <count>
   - scheduler:
     - zone: '0'
   - vinyl:

test/vinyl/info.test.lua
@@ -16,7 +16,7 @@ test_run:cmd("setopt delimiter ';'")
 for _, v in ipairs({ 'path', 'build', 'tx_latency', 'cursor_latency',
                      'get_latency', 'gc_active', 'run_avg', 'run_count',
                      'page_count', 'memory_used', 'run_max', 'run_histogram',
-                     'size', 'size_uncompressed', 'used'}) do
+                     'size', 'size_uncompressed', 'used', 'count'}) do
     test_run:cmd("push filter '"..v..": .*' to '"..v..": <"..v..">'")
 end;
 test_run:cmd("setopt delimiter ''");

test/vinyl/split.result
@@ -1,6 +1,3 @@
-#!/usr/bin/env tarantool
----
-...
 test_run = require('test_run').new()
 ---
 ...

test/vinyl/split.test.lua
@@ -1,5 +1,3 @@
-#!/usr/bin/env tarantool
-
 test_run = require('test_run').new()
 fiber = require('fiber')
 