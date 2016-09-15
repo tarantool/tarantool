cf7ecf6ee753695364da17eb70fe8550e1bba9bc
GeorgyKirichenko <kirichenkoga@gmail.com>
2016-09-15 09:47:49 +0300

    Update bundled zstd library

--- cmake/BuildZSTD.cmake
+++ cmake/BuildZSTD.cmake
@@ -1,15 +1,13 @@
 macro(zstd_build)
     add_library(zstd STATIC
-        third_party/zstd/lib/zstd_compress.c
-        third_party/zstd/lib/zstd_decompress.c
-        third_party/zstd/lib/zdict.c
-        third_party/zstd/lib/zbuff.c
-        third_party/zstd/lib/fse.c
-        third_party/zstd/lib/huff0.c
-        third_party/zstd/lib/divsufsort.c
+        third_party/zstd/lib/compress/zstd_compress.c
+        third_party/zstd/lib/decompress/zstd_decompress.c
     )
     set(ZSTD_LIBRARIES zstd)
-    set(ZSTD_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/third_party/zstd/lib)
+    set(ZSTD_INCLUDE_DIRS
+	    ${CMAKE_CURRENT_SOURCE_DIR}/third_party/zstd/lib
+	    ${CMAKE_CURRENT_SOURCE_DIR}/third_party/zstd/lib/common)
+    target_include_directories(zstd PUBLIC ${ZSTD_INCLUDE_DIRS})
     find_package_message(ZSTD "Using bundled ZSTD"
         "${ZSTD_LIBRARIES}:${ZSTD_INCLUDE_DIRS}")
     add_dependencies(build_bundled_libs zstd)
