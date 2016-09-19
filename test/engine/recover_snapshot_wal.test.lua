64e76e4105a4e9644bb791224904c715302ab770
Vladimir Davydov <vdavydov.dev@gmail.com>
2016-09-19 13:58:07 +0300

    test: engine: recover/snapshot: remove useless code

--- test/engine/recover_snapshot_wal.test.lua
+++ test/engine/recover_snapshot_wal.test.lua
@@ -2,14 +2,7 @@ env = require('test_run')
 test_run = env.new()
 -- write data recover from latest snapshot and logs
 
-name = string.match(arg[0], "([^,]+)%.lua")
-os.execute("rm -f " .. name .."/*.snap")
-os.execute("rm -f " .. name .."/*.xlog")
-
 test_run:cmd("restart server default")
-
-name = string.match(arg[0], "([^,]+)%.lua")
-os.execute("touch " .. name .."/lock")
 
 engine = test_run:get_cfg('engine')
 space = box.schema.space.create('test', { engine = engine })
@@ -21,9 +14,6 @@ box.snapshot()
 for key = 352, 1000 do space:insert({key}) end
 
 test_run:cmd("restart server default")
-
-name = string.match(arg[0], "([^,]+)%.lua")
-os.execute("rm -f " .. name .."/lock")
 
 space = box.space['test']
 index = space.index['primary']
--- test/engine/snapshot.test.lua
+++ test/engine/snapshot.test.lua
@@ -1,17 +1,10 @@
 
 -- write data recover from latest snapshot
 
-name = string.match(arg[0], "([^,]+)%.lua")
-os.execute("rm -f " .. name .."/*.snap")
-os.execute("rm -f " .. name .."/*.xlog")
-
 env = require('test_run')
 test_run = env.new()
 test_run:cmd('restart server default')
 engine = test_run:get_cfg('engine')
-
-name = string.match(arg[0], "([^,]+)%.lua")
-os.execute("touch " .. name .."/lock")
 
 space = box.schema.space.create('test', { engine = engine })
 index = space:create_index('primary')
@@ -32,8 +25,5 @@ test_run:cmd('restart server default')
 space = box.space['test']
 index = space.index['primary']
 index:select({}, {iterator = box.index.ALL})
-
-name = string.match(arg[0], "([^,]+)%.lua")
-os.execute("rm -f " .. name .."/lock")
 
 box.space.test:drop()
