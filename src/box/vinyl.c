6d4734138fc2523a8c7c19e10b52583e879191ed
GeorgyKirichenko <kirichenkoga@gmail.com>
2016-11-22 15:18:15 +0300

    Extract vy_stmt_encode/vy_stmt_decode
    Extract vy_stmt_encode and vy_stmt_decode as separate functions to
    read/write xlog files from vinyl.
    
    Issue #1720

--- src/box/vinyl.c
+++ src/box/vinyl.c
@@ -2535,40 +2535,57 @@ insert:
 	say_debug("range recover insert: %s", vy_range_str(range));
 }
 
+/**
+ * Encode vy_stmt as xrow_header
+ *
+ * @retval 0 if OK
+ * @retval -1 if error
+ */
+static int
+vy_stmt_encode(struct vy_stmt *value, struct key_def *key_def,
+	       struct xrow_header *xrow)
+{
+	memset(xrow, 0, sizeof(*xrow));
+	xrow->type = value->type;
+	xrow->lsn = value->lsn;
+
+	struct request request;
+	request_create(&request, value->type);
+	request.space_id = key_def->space_id;
+	request.index_id = key_def->iid;
+	if (value->type == IPROTO_UPSERT || value->type == IPROTO_REPLACE) {
+		/* extract tuple */
+		uint32_t tuple_size;
+		request.tuple = vy_stmt_tuple_data(value, key_def,
+						   &tuple_size);
+		request.tuple_end = request.tuple + tuple_size;
+	}
+	if (value->type == IPROTO_UPSERT) {
+		/* extract operations */
+		uint32_t ops_size;
+		request.ops = vy_stmt_upsert_ops(value, key_def,
+						  &ops_size);
+		request.ops_end = request.ops + ops_size;
+	}
+	if (value->type == IPROTO_DELETE) {
+		/* extract key */
+		request.key = value->data;
+		request.key_end = request.key + value->size;
+	}
+	xrow->bodycnt = request_encode(&request, xrow->body);
+	return xrow->bodycnt >= 0 ? 0: -1;
+}
+
 /* dump statement to the run page buffers (stmt header and data) */
 static int
 vy_run_dump_stmt(struct vy_stmt *value, struct xlog *data_xlog,
 		 struct vy_page_info *info,
 		 struct vy_index *index)
 {
-	struct xrow_header header;
-	memset(&header, 0, sizeof(header));
-	header.type = value->type;
-	header.lsn = value->lsn;
-
-	struct request request;
-	memset(&request, 0, sizeof(request));
-	request.space_id = index->key_def->space_id;
-	request.index_id = index->key_def->iid;
-	request.type = value->type;
-	if (value->type == IPROTO_UPSERT || value->type == IPROTO_REPLACE) {
-		uint32_t tuple_size;
-		request.tuple = vy_stmt_tuple_data(value, index->key_def,
-						   &tuple_size);
-		request.tuple_end = request.tuple + tuple_size;
-	}
-	if (value->type == IPROTO_UPSERT) {
-		uint32_t ops_size;
-		request.ops = vy_stmt_upsert_ops(value, index->key_def,
-						  &ops_size);
-		request.ops_end = request.ops + ops_size;
-	}
-	if (value->type == IPROTO_DELETE) {
-		request.key = value->data;
-		request.key_end = request.key + value->size;
-	}
-	header.bodycnt = request_encode(&request, header.body);
-	if (xlog_write_row(data_xlog, &header) < 0)
+	struct xrow_header xrow;
+	if (vy_stmt_encode(value, index->key_def, &xrow) < 0)
+		return -1;
+	if (xlog_write_row(data_xlog, &xrow) < 0)
 		return -1;
 
 	++info->count;
@@ -6704,6 +6721,67 @@ vy_run_iterator_cache_clean(struct vy_run_iterator *itr)
 }
 
 /**
+ * Reconstruct vinyl tuple info and data from xrow
+ *
+ * @retval 0 if OK
+ * @retval -1 if error
+ */
+static int
+vy_stmt_decode(struct xrow_header *xrow, struct vy_buf *info_buf,
+	       struct vy_buf *data_buf, struct vy_index *index)
+{
+	struct vy_stmt_info *info;
+	if (vy_buf_ensure(info_buf, sizeof(struct vy_stmt_info)))
+		return -1;
+	info = (struct vy_stmt_info *)info_buf->p;
+	vy_buf_advance(info_buf, sizeof(struct vy_stmt_info));
+
+	info->lsn = xrow->lsn;
+	info->offset = data_buf->p - data_buf->s;
+	struct request request;
+	request_create(&request, xrow->type);
+	if (request_decode(&request, xrow->body->iov_base,
+			   xrow->body->iov_len) < 0)
+		return -1;
+	info->type = request.type;
+	if (info->type == IPROTO_DELETE) {
+		/* extract key */
+		info->size = request.key_end - request.key;
+		if (vy_buf_ensure(data_buf, info->size))
+			return -1;
+		void *data = data_buf->p;
+		memcpy(data, request.key,
+		       request.key_end - request.key);
+	} else {
+		/* space for offsets */
+		int start_offset = (index->key_def->part_count) * sizeof(uint32_t);
+		info->size = start_offset;
+		info->size += request.tuple_end - request.tuple;
+		if (info->type == IPROTO_UPSERT) {
+			/* space for series count (1) and operations */
+			info->size += request.ops_end - request.ops;
+		}
+		if (vy_buf_ensure(data_buf, info->size))
+			return -1;
+		void *data = data_buf->p;
+		data += start_offset;
+		/* copy tuple */
+		memcpy(data, request.tuple,
+		       request.tuple_end - request.tuple);
+		/* rebuild offsets */
+		tuple_init_field_map(index->format, (uint32_t *)data, data);
+		data += request.tuple_end - request.tuple;
+		if (info->type == IPROTO_UPSERT) {
+			/* copy ops */
+			memcpy(data, request.ops,
+			       request.ops_end - request.ops);
+		}
+	}
+	vy_buf_advance(data_buf, info->size);
+	return 0;
+}
+
+/**
  * Get a page by the given number the cache or load it from the disk.
  *
  * @retval 0 success
@@ -6780,46 +6858,9 @@ vy_run_iterator_load_page(struct vy_run_iterator *itr, uint32_t page_no,
 
 	struct xrow_header xrow;
 	uint32_t row_cnt = 0;
-	while ((rc = xlog_cursor_next_row(&itr->cursor, &xrow)) == 0) {
-		struct vy_stmt_info *info;
-		vy_buf_ensure(&page->info, sizeof(struct vy_stmt_info));
-		info = (struct vy_stmt_info *)page->info.p;
-		vy_buf_advance(&page->info, sizeof(struct vy_stmt_info));
-
-		info->lsn = xrow.lsn;
-		info->offset = page->data.p - page->data.s;
-		struct request request = {.type = xrow.type};
-		request_decode(&request, xrow.body[0].iov_base,
-			       xrow.body[0].iov_len);
-		info->type = request.type;
-		if (info->type == IPROTO_DELETE) {
-			info->size = request.key_end - request.key;
-			vy_buf_ensure(&page->data, info->size);
-			void *data = page->data.p;
-			memcpy(data, request.key,
-			       request.key_end - request.key);
-		} else {
-			int start_offset = (itr->index->key_def->part_count) * sizeof(uint32_t);
-			info->size = start_offset;
-			info->size += request.tuple_end - request.tuple;
-			if (info->type == IPROTO_UPSERT) {
-				info->size += request.ops_end - request.ops;
-			}
-			vy_buf_ensure(&page->data, info->size);
-			void *data = page->data.p;
-			data += start_offset;
-			memcpy(data, request.tuple,
-			       request.tuple_end - request.tuple);
-			tuple_init_field_map(itr->index->format,
-					     (uint32_t *) (data),
-					     data);
-			data += request.tuple_end - request.tuple;
-			if (info->type == IPROTO_UPSERT) {
-				memcpy(data, request.ops,
-				       request.ops_end - request.ops);
-			}
-		}
-		vy_buf_advance(&page->data, info->size);
+	while ((rc = xlog_cursor_next_row(&itr->cursor, &xrow)) == 0 &&
+	       (rc = vy_stmt_decode(&xrow, &page->info, &page->data,
+					  itr->index) == 0)) {
 		++row_cnt;
 	}
 
@@ -6833,7 +6874,6 @@ vy_run_iterator_load_page(struct vy_run_iterator *itr, uint32_t page_no,
 	if (row_cnt != page->count) {
 		diag_set(ClientError, ER_VINYL,
 			 "row count doesn't match!");
-		say_warn("want %u got %u", page->count, row_cnt);
 		vy_page_delete(page);
 		return -1;
 	}
