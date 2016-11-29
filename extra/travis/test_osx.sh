b23060ff3acfa7e190fa8dbf2b4a73c91cf106b8
Roman Tsisyk <roman@tsisyk.com>
2016-11-29 14:05:28 +0300

    Travis CI: increase the maximum number of files on macOS

--- extra/travis/test_osx.sh
+++ extra/travis/test_osx.sh
@@ -1,1 +1,9 @@
+echo "Increase the maximum number of open file descriptors on macOS"
+NOFILE=20480
+sudo sysctl -w kern.maxfiles=$NOFILE
+sudo sysctl -w kern.maxfilesperproc=$NOFILE
+sudo launchctl limit maxfiles $NOFILE $NOFILE
+ulimit -S -n $NOFILE
+ulimit -n
+
 cd test && python test-run.py
