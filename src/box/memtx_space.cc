aeec869917a2914d46f10b68c908fc92c9d34c20
Konstantin Osipov <kostja@tarantool.org>
2016-12-06 17:32:23 +0300

    memtx: separate memtx_space.h from memtx_engine.cc; no code changes
    * separate MemtxSpace class into own files (declaration and definition)
    * cleanup polluted includes in memtx_engine.cc

---
+++ src/box/memtx_space.cc
@@ -0,0 +1,353 @@
+/*
+ * Copyright 2010-2016, Tarantool AUTHORS, please see AUTHORS file.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above
+ *    copyright notice, this list of conditions and the
+ *    following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials
+ *    provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * <COPYRIGHT HOLDER> OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+#include "memtx_space.h"
+#include "space.h"
+#include "iproto_constants.h"
+#include "txn.h"
+#include "request.h"
+#include "tuple.h"
+#include "xrow.h"
+#include "memtx_hash.h"
+#include "memtx_tree.h"
+#include "memtx_rtree.h"
+#include "memtx_bitset.h"
+#include "port.h"
+
+/**
+ * A version of space_replace for a space which has
+ * no indexes (is not yet fully built).
+ */
+struct tuple *
+memtx_replace_no_keys(struct space *space,
+		      struct tuple * /* old_tuple */,
+		      struct tuple * /* new_tuple */,
+		      enum dup_replace_mode /* mode */)
+{
+	Index *index = index_find(space, 0);
+	assert(index == NULL); /* not reached. */
+	(void) index;
+	return NULL;
+}
+
+MemtxSpace::MemtxSpace(Engine *e)
+	: Handler(e)
+{
+	replace = memtx_replace_no_keys;
+}
+
+static inline enum dup_replace_mode
+dup_replace_mode(uint32_t op)
+{
+	return op == IPROTO_INSERT ? DUP_INSERT : DUP_REPLACE_OR_INSERT;
+}
+
+/**
+ * Do the plumbing necessary for correct statement-level
+ * and transaction rollback.
+ */
+static inline void
+memtx_txn_add_undo(struct txn *txn, struct tuple *old_tuple,
+		   struct tuple *new_tuple)
+{
+	/*
+	 * Remember the old tuple only if we replaced it
+	 * successfully, to not remove a tuple inserted by
+	 * another transaction in rollback().
+	 */
+	struct txn_stmt *stmt = txn_current_stmt(txn);
+	assert(stmt->space);
+	stmt->old_tuple = old_tuple;
+	stmt->new_tuple = new_tuple;
+}
+
+void
+MemtxSpace::applySnapshotRow(struct space *space, struct request *request)
+{
+	assert(request->type == IPROTO_INSERT);
+	struct tuple *new_tuple = tuple_new(space->format, request->tuple,
+					    request->tuple_end);
+	if (new_tuple == NULL)
+		diag_raise();
+	/* GC the new tuple if there is an exception below. */
+	TupleRef ref(new_tuple);
+	if (!rlist_empty(&space->on_replace)) {
+		/*
+		 * Emulate transactions for system spaces with triggers
+		 */
+		assert(in_txn() == NULL);
+		request->header->server_id = 0;
+		struct txn *txn = txn_begin_stmt(space);
+		try {
+			struct tuple *old_tuple = this->replace(space, NULL,
+				new_tuple, DUP_INSERT);
+			memtx_txn_add_undo(txn, old_tuple, new_tuple);
+			txn_commit_stmt(txn, request);
+		} catch (Exception *e) {
+			say_error("rollback: %s", e->errmsg);
+			txn_rollback_stmt();
+			throw;
+		}
+	} else {
+		this->replace(space, NULL, new_tuple, DUP_INSERT);
+	}
+	/** The new tuple is referenced by the primary key. */
+}
+
+struct tuple *
+MemtxSpace::executeReplace(struct txn *txn, struct space *space,
+			   struct request *request)
+{
+	struct tuple *new_tuple = tuple_new(space->format, request->tuple,
+					    request->tuple_end);
+	if (new_tuple == NULL)
+		diag_raise();
+	/* GC the new tuple if there is an exception below. */
+	TupleRef ref(new_tuple);
+	enum dup_replace_mode mode = dup_replace_mode(request->type);
+	struct tuple *old_tuple = this->replace(space, NULL, new_tuple, mode);
+	memtx_txn_add_undo(txn, old_tuple, new_tuple);
+	/** The new tuple is referenced by the primary key. */
+	return new_tuple;
+}
+
+struct tuple *
+MemtxSpace::executeDelete(struct txn *txn, struct space *space,
+			  struct request *request)
+{
+	/* Try to find the tuple by unique key. */
+	Index *pk = index_find_unique(space, request->index_id);
+	const char *key = request->key;
+	uint32_t part_count = mp_decode_array(&key);
+	if (primary_key_validate(pk->key_def, key, part_count))
+		diag_raise();
+	struct tuple *old_tuple = pk->findByKey(key, part_count);
+	if (old_tuple == NULL)
+		return NULL;
+
+	this->replace(space, old_tuple, NULL, DUP_REPLACE_OR_INSERT);
+	memtx_txn_add_undo(txn, old_tuple, NULL);
+	return old_tuple;
+}
+
+struct tuple *
+MemtxSpace::executeUpdate(struct txn *txn, struct space *space,
+			  struct request *request)
+{
+	/* Try to find the tuple by unique key. */
+	Index *pk = index_find_unique(space, request->index_id);
+	const char *key = request->key;
+	uint32_t part_count = mp_decode_array(&key);
+	if (primary_key_validate(pk->key_def, key, part_count))
+		diag_raise();
+	struct tuple *old_tuple = pk->findByKey(key, part_count);
+
+	if (old_tuple == NULL)
+		return NULL;
+
+	/* Update the tuple; legacy, request ops are in request->tuple */
+	struct tuple *new_tuple = tuple_update(space->format,
+					       region_aligned_alloc_xc_cb,
+					       &fiber()->gc,
+					       old_tuple, request->tuple,
+					       request->tuple_end,
+					       request->index_base, NULL);
+	TupleRef ref(new_tuple);
+	this->replace(space, old_tuple, new_tuple, DUP_REPLACE);
+	memtx_txn_add_undo(txn, old_tuple, new_tuple);
+	return new_tuple;
+}
+
+void
+MemtxSpace::executeUpsert(struct txn *txn, struct space *space,
+			  struct request *request)
+{
+	Index *pk = index_find_unique(space, request->index_id);
+
+	/* Check tuple fields */
+	if (tuple_validate_raw(space->format, request->tuple))
+		diag_raise();
+
+	struct key_def *key_def = pk->key_def;
+	uint32_t part_count = pk->key_def->part_count;
+	/*
+	 * Extract the primary key from tuple.
+	 * Allocate enough memory to store the key.
+	 */
+	const char *key = tuple_extract_key_raw(request->tuple,
+						request->tuple_end,
+						key_def, NULL);
+	if (key == NULL)
+		diag_raise();
+	/* Cut array header */
+	mp_decode_array(&key);
+
+	/* Try to find the tuple by primary key. */
+	struct tuple *old_tuple = pk->findByKey(key, part_count);
+
+	if (old_tuple == NULL) {
+		/**
+		 * Old tuple was not found. In a "true"
+		 * non-reading-write engine, this is known only
+		 * after commit. Thus any error that can happen
+		 * at this point is ignored. Emulate this by
+		 * suppressing the error. It's logged and ignored.
+		 *
+		 * Taking into account that:
+		 * 1) Default tuple fields are already fully checked
+		 *    at the beginning of the function
+		 * 2) Space with unique secondary indexes does not support
+		 *    upsert and we can't get duplicate error
+		 *
+		 * Thus we could get only OOM error, but according to
+		 *   https://github.com/tarantool/tarantool/issues/1156
+		 *   we should not suppress it
+		 *
+		 * So we have nothing to catch and suppress!
+		 */
+		if (tuple_update_check_ops(region_aligned_alloc_xc_cb, &fiber()->gc,
+				       request->ops, request->ops_end,
+				       request->index_base)) {
+			diag_raise();
+		}
+		struct tuple *new_tuple = tuple_new(space->format,
+						    request->tuple,
+						    request->tuple_end);
+		if (new_tuple == NULL)
+			diag_raise();
+		TupleRef ref(new_tuple); /* useless, for unified approach */
+		old_tuple = replace(space, NULL, new_tuple, DUP_INSERT);
+		memtx_txn_add_undo(txn, old_tuple, new_tuple);
+	} else {
+		/**
+		 * Update the tuple.
+		 * tuple_upsert throws on totally wrong tuple ops,
+		 * but ignores ops that not suitable for the tuple
+		 */
+		struct tuple *new_tuple;
+		new_tuple = tuple_upsert(space->format,
+					 region_aligned_alloc_xc_cb,
+					 &fiber()->gc, old_tuple,
+					 request->ops, request->ops_end,
+					 request->index_base);
+		TupleRef ref(new_tuple);
+
+		/**
+		 * Ignore and log all client exceptions,
+		 * note that OutOfMemory is not catched.
+		 */
+		try {
+			replace(space, old_tuple, new_tuple, DUP_REPLACE);
+			memtx_txn_add_undo(txn, old_tuple, new_tuple);
+		} catch (ClientError *e) {
+			say_error("UPSERT failed:");
+			e->log();
+		}
+	}
+	/* Return nothing: UPSERT does not return data. */
+}
+
+Index *
+MemtxSpace::createIndex(struct space *space, struct key_def *key_def_arg)
+{
+	(void) space;
+	switch (key_def_arg->type) {
+	case HASH:
+		return new MemtxHash(key_def_arg);
+	case TREE:
+		return new MemtxTree(key_def_arg);
+	case RTREE:
+		return new MemtxRTree(key_def_arg);
+	case BITSET:
+		return new MemtxBitset(key_def_arg);
+	default:
+		unreachable();
+		return NULL;
+	}
+}
+
+void
+MemtxSpace::dropIndex(Index *index)
+{
+	if (index->key_def->iid != 0)
+		return; /* nothing to do for secondary keys */
+	/*
+	 * Delete all tuples in the old space if dropping the
+	 * primary key.
+	 */
+	struct iterator *it = ((MemtxIndex*) index)->position();
+	index->initIterator(it, ITER_ALL, NULL, 0);
+	struct tuple *tuple;
+	while ((tuple = it->next(it)))
+		tuple_unref(tuple);
+}
+
+void
+MemtxSpace::prepareAlterSpace(struct space *old_space, struct space *new_space)
+{
+	(void)new_space;
+	MemtxSpace *handler = (MemtxSpace *) old_space->handler;
+	replace = handler->replace;
+}
+
+void
+MemtxSpace::executeSelect(struct txn *, struct space *space,
+			  uint32_t index_id, uint32_t iterator,
+			  uint32_t offset, uint32_t limit,
+			  const char *key, const char * /* key_end */,
+			  struct port *port)
+{
+	MemtxIndex *index = (MemtxIndex *) index_find(space, index_id);
+
+	ERROR_INJECT_EXCEPTION(ERRINJ_TESTING);
+
+	uint32_t found = 0;
+	if (iterator >= iterator_type_MAX)
+		tnt_raise(IllegalParams, "Invalid iterator type");
+	enum iterator_type type = (enum iterator_type) iterator;
+
+	uint32_t part_count = key ? mp_decode_array(&key) : 0;
+	if (key_validate(index->key_def, type, key, part_count))
+		diag_raise();
+
+	struct iterator *it = index->position();
+	index->initIterator(it, type, key, part_count);
+
+	struct tuple *tuple;
+	while ((tuple = it->next(it)) != NULL) {
+		if (offset > 0) {
+			offset--;
+			continue;
+		}
+		if (limit == found++)
+			break;
+		port_add_tuple(port, tuple);
+	}
+}
---
+++ src/box/memtx_space.h
@@ -0,0 +1,167 @@
+#ifndef TARANTOOL_BOX_MEMTX_SPACE_H_INCLUDED
+#define TARANTOOL_BOX_MEMTX_SPACE_H_INCLUDED
+/*
+ * Copyright 2010-2016, Tarantool AUTHORS, please see AUTHORS file.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above
+ *    copyright notice, this list of conditions and the
+ *    following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials
+ *    provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * <COPYRIGHT HOLDER> OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+#include "engine.h"
+
+typedef struct tuple *
+(*engine_replace_f)(struct space *, struct tuple *, struct tuple *,
+		    enum dup_replace_mode);
+
+struct tuple *
+memtx_replace_no_keys(struct space *space,
+		      struct tuple * /* old_tuple */,
+		      struct tuple * /* new_tuple */,
+		      enum dup_replace_mode /* mode */);
+
+struct MemtxSpace: public Handler {
+	MemtxSpace(Engine *e);
+	virtual ~MemtxSpace()
+	{
+		/* do nothing */
+		/* engine->close(this); */
+	}
+	virtual void
+	applySnapshotRow(struct space *space,
+			 struct request *request) override;
+	virtual struct tuple *
+	executeReplace(struct txn *txn, struct space *space,
+		       struct request *request) override;
+	virtual struct tuple *
+	executeDelete(struct txn *txn, struct space *space,
+		      struct request *request) override;
+	virtual struct tuple *
+	executeUpdate(struct txn *txn, struct space *space,
+		      struct request *request) override;
+	virtual void
+	executeUpsert(struct txn *txn, struct space *space,
+		      struct request *request) override;
+	virtual void
+	executeSelect(struct txn *, struct space *space,
+		      uint32_t index_id, uint32_t iterator,
+		      uint32_t offset, uint32_t limit,
+		      const char *key, const char * /* key_end */,
+		      struct port *port) override;
+
+	virtual Index *createIndex(struct space *space,
+				   struct key_def *key_def) override;
+	virtual void dropIndex(Index *index) override;
+	virtual void prepareAlterSpace(struct space *old_space,
+				       struct space *new_space) override;
+public:
+	/**
+	 * @brief A single method to handle REPLACE, DELETE and UPDATE.
+	 *
+	 * @param sp space
+	 * @param old_tuple the tuple that should be removed (can be NULL)
+	 * @param new_tuple the tuple that should be inserted (can be NULL)
+	 * @param mode      dup_replace_mode, used only if new_tuple is not
+	 *                  NULL and old_tuple is NULL, and only for the
+	 *                  primary key.
+	 *
+	 * For DELETE, new_tuple must be NULL. old_tuple must be
+	 * previously found in the primary key.
+	 *
+	 * For REPLACE, old_tuple must be NULL. The additional
+	 * argument dup_replace_mode further defines how REPLACE
+	 * should proceed.
+	 *
+	 * For UPDATE, both old_tuple and new_tuple must be given,
+	 * where old_tuple must be previously found in the primary key.
+	 *
+	 * Let's consider these three cases in detail:
+	 *
+	 * 1. DELETE, old_tuple is not NULL, new_tuple is NULL
+	 *    The effect is that old_tuple is removed from all
+	 *    indexes. dup_replace_mode is ignored.
+	 *
+	 * 2. REPLACE, old_tuple is NULL, new_tuple is not NULL,
+	 *    has one simple sub-case and two with further
+	 *    ramifications:
+	 *
+	 *	A. dup_replace_mode is DUP_INSERT. Attempts to insert the
+	 *	new tuple into all indexes. If *any* of the unique indexes
+	 *	has a duplicate key, deletion is aborted, all of its
+	 *	effects are removed, and an error is thrown.
+	 *
+	 *	B. dup_replace_mode is DUP_REPLACE. It means an existing
+	 *	tuple has to be replaced with the new one. To do it, tries
+	 *	to find a tuple with a duplicate key in the primary index.
+	 *	If the tuple is not found, throws an error. Otherwise,
+	 *	replaces the old tuple with a new one in the primary key.
+	 *	Continues on to secondary keys, but if there is any
+	 *	secondary key, which has a duplicate tuple, but one which
+	 *	is different from the duplicate found in the primary key,
+	 *	aborts, puts everything back, throws an exception.
+	 *
+	 *	For example, if there is a space with 3 unique keys and
+	 *	two tuples { 1, 2, 3 } and { 3, 1, 2 }:
+	 *
+	 *	This REPLACE/DUP_REPLACE is OK: { 1, 5, 5 }
+	 *	This REPLACE/DUP_REPLACE is not OK: { 2, 2, 2 } (there
+	 *	is no tuple with key '2' in the primary key)
+	 *	This REPLACE/DUP_REPLACE is not OK: { 1, 1, 1 } (there
+	 *	is a conflicting tuple in the secondary unique key).
+	 *
+	 *	C. dup_replace_mode is DUP_REPLACE_OR_INSERT. If
+	 *	there is a duplicate tuple in the primary key, behaves the
+	 *	same way as DUP_REPLACE, otherwise behaves the same way as
+	 *	DUP_INSERT.
+	 *
+	 * 3. UPDATE has to delete the old tuple and insert a new one.
+	 *    dup_replace_mode is ignored.
+	 *    Note that old_tuple primary key doesn't have to match
+	 *    new_tuple primary key, thus a duplicate can be found.
+	 *    For this reason, and since there can be duplicates in
+	 *    other indexes, UPDATE is the same as DELETE +
+	 *    REPLACE/DUP_INSERT.
+	 *
+	 * @return old_tuple. DELETE, UPDATE and REPLACE/DUP_REPLACE
+	 * always produce an old tuple. REPLACE/DUP_INSERT always returns
+	 * NULL. REPLACE/DUP_REPLACE_OR_INSERT may or may not find
+	 * a duplicate.
+	 *
+	 * The method is all-or-nothing in all cases. Changes are either
+	 * applied to all indexes, or nothing applied at all.
+	 *
+	 * Note, that even in case of REPLACE, dup_replace_mode only
+	 * affects the primary key, for secondary keys it's always
+	 * DUP_INSERT.
+	 *
+	 * The call never removes more than one tuple: if
+	 * old_tuple is given, dup_replace_mode is ignored.
+	 * Otherwise, it's taken into account only for the
+	 * primary key.
+	 */
+	engine_replace_f replace;
+};
+
+#endif /* TARANTOOL_BOX_MEMTX_SPACE_H_INCLUDED */
