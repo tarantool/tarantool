0ff46f3c7e1bf90947b64f09a44f2067a984c269
Roman Tsisyk <roman@tsisyk.com>
2017-01-10 13:27:08 +0300

    Travis CI: fix python-gevent installation

--- extra/travis/build_linux.sh
+++ extra/travis/build_linux.sh
@@ -2,7 +2,6 @@ sudo apt-get update > /dev/null
 sudo apt-get -q -y install binutils-dev python-daemon python-yaml
 sudo apt-get -q -y install libmysqlclient-dev libpq-dev postgresql-server-dev-all
 sudo pip install six==1.9.0
-sudo pip install gevent
-sudo pip install geventconnpool
+sudo pip install gevent==1.1.2
 cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo
 make -j8
--- extra/travis/build_osx.sh
+++ extra/travis/build_osx.sh
@@ -1,7 +1,6 @@
 brew install openssl
 sudo pip install python-daemon PyYAML
 sudo pip install six==1.9.0
-sudo pip install gevent
-sudo pip install geventconnpool
+sudo pip install gevent==1.1.2
 cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo
 make -j8
