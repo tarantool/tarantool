acb770edb0f3de9645fd16d10bc69cfdbda1d7ce
antonmes <antonmes@users.noreply.github.com>
2017-02-11 02:06:07 +0300

    Update function1 test

--- test/box/function1.test.lua
+++ test/box/function1.test.lua
@@ -12,14 +12,14 @@ _ = box.space.test:create_index('primary')
 box.schema.user.grant('guest', 'read,write', 'space', 'test')
 
 c:call('function1')
-box.schema.func.drop("function1")
+box.schema.func.drop('function1')
 
 box.schema.func.create('function1.args', {language = "C"})
 box.schema.user.grant('guest', 'execute', 'function', 'function1.args')
 c:call('function1.args')
-c:call('function1.args', "xx")
+c:call('function1.args', 'xx')
 c:call('function1.args', 15)
-box.schema.func.drop("function1.args")
+box.schema.func.drop('function1.args')
 
 box.schema.func.create('function1.multi_inc', {language = "C"})
 box.schema.user.grant('guest', 'execute', 'function', 'function1.multi_inc')
@@ -33,12 +33,12 @@ box.space.test:select{}
 c:call('function1.multi_inc', 0, 2, 4)
 box.space.test:select{}
 
-box.schema.func.drop("function1.multi_inc")
+box.schema.func.drop('function1.multi_inc')
 
 box.schema.func.create('function1.errors', {language = "C"})
 box.schema.user.grant('guest', 'execute', 'function', 'function1.errors')
 c:call('function1.errors')
-box.schema.func.drop("function1.errors")
+box.schema.func.drop('function1.errors')
 
 box.schema.func.create('xxx', {language = 'invalid'})
 
