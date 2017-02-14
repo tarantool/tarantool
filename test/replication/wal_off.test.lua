d539c77f7f9ec61c0d2c32ffefd78466a3c017b0
Roman Tsisyk <roman@tsisyk.com>
2017-02-14 16:22:14 +0300

    Fix a race condition in test/replication/wal_off.test.lua
    Closes #2072

--- test/replication/wal_off.test.lua
+++ test/replication/wal_off.test.lua
@@ -5,6 +5,7 @@
 env = require('test_run')
 test_run = env.new()
 test_run:cmd('switch default')
+fiber = require('fiber')
 box.schema.user.grant('guest', 'replication')
 test_run:cmd("create server wal_off with rpl_master=default, script='replication/wal_off.lua'")
 test_run:cmd("start server wal_off")
@@ -14,9 +15,10 @@ wal_off_uri ~= nil
 wal_off_id = test_run:eval('wal_off', 'return box.info.server.id')[1]
 
 box.cfg { replication_source = wal_off_uri }
--- Replication does not support wal_mode = 'none'
-box.info.replication[wal_off_id].message
+check = "Replication does not support wal_mode = 'none'"
+while box.info.replication[wal_off_id].message ~= check do fiber.sleep(0) end
 box.info.replication[wal_off_id].status
+box.info.replication[wal_off_id].message
 box.cfg { replication_source = "" }
 
 test_run:cmd('switch wal_off')
@@ -24,9 +26,8 @@ box.schema.user.revoke('guest', 'replication')
 test_run:cmd('switch default')
 
 box.cfg { replication_source = wal_off_uri }
--- Read access is denied
-box.info.replication[wal_off_id].message
-box.info.replication[wal_off_id].status
+check = "Read access on universe is denied for user 'guest'"
+while box.info.replication[wal_off_id].message ~= check do fiber.sleep(0) end
 box.cfg { replication_source = "" }
 
 test_run:cmd("stop server wal_off")
