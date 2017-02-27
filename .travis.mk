9f9b8f67247c681ed65368ca0f9035b49e907f5a
Roman Tsisyk <roman@tsisyk.com>
2017-02-27 10:06:48 +0300

    Travis CI: re-enable code coverage analysis

--- .travis.mk
+++ .travis.mk
@@ -41,3 +41,20 @@ test_osx: deps_osx
 	ulimit -n
 	make -j8
 	cd test && python test-run.py unit/ app/ app-tap/ box/ box-tap/
+
+coverage: deps_linux
+	sudo apt-get -q -y install lcov
+	cmake . -DCMAKE_BUILD_TYPE=Debug -DENABLE_GCOV=ON
+	make -j8
+	# Enable --long tests for coverage
+	cd test && /usr/bin/python test-run.py --long
+	lcov --compat-libtool --directory src/ --capture --output-file coverage.info.tmp
+	lcov --compat-libtool --remove coverage.info.tmp 'tests/*' 'third_party/*' '/usr/*' \
+		--output-file coverage.info
+	lcov --list coverage.info
+	@if [ -n "$(COVERALLS_TOKEN)" ]; then \
+		echo "Exporting code coverage information to coveralls.io"; \
+		gem install coveralls-lcov; \
+		echo coveralls-lcov --repo-token [FILTERED] coverage.info; \
+		coveralls-lcov -v --repo-token $(COVERALLS_TOKEN) coverage.info; \
+	fi;
