06ee320483e57ce581ede64b80811292c04d19f3
Roman Tsisyk <roman@tsisyk.com>
2017-03-10 16:06:37 +0300

    Follow up box.cfg() renames
    * Update and re-wrap example.lua
    * Update /etc/default/tarantool
    * Add support for old names in /etc/default/tarantool
    
    See #1927

--- extra/dist/example.lua
+++ extra/dist/example.lua
@@ -3,7 +3,7 @@
 -- replication, sharding and all major features
 -- Complete documentation available in:  http://tarantool.org/doc/
 --
--- To start this example just run "sudo tarantoolctl start example" or
+-- To start this instance please run `systemctl start tarantool@example` or
 -- use init scripts provided by binary packages.
 -- To connect to the instance, use "sudo tarantoolctl enter example"
 -- Features:
@@ -18,95 +18,16 @@
 -- Configuration
 -----------------
 box.cfg {
-    --------------------
-    -- Basic parameters
-    --------------------
-
-    -- An absolute path to directory where write-ahead log (.xlog) files are
-    -- stored. If not specified, defaults to /var/lib/tarantool/INSTANCE
-    -- wal_dir = nil;
-
-    -- An absolute path to directory where snapshot (.snap) files are stored.
-    -- If not specified, defaults to /var/lib/tarantool/INSTANCE
-    -- snap_dir = nil;
-
-    -- An absolute path to directory where vinyl files are stored.
-    -- If not specified, defaults to /var/lib/tarantool/INSTANCE
-    -- vinyl_dir = nil;
+    ------------------------
+    -- Network configuration
+    ------------------------
 
     -- The read/write data port number or URI
     -- Has no default value, so must be specified if
-    -- connections will occur from remote clients 
+    -- connections will occur from remote clients
     -- that do not use “admin address”
     listen = 'localhost:3301';
     -- listen = '*:3301';
-
-    -- Inject the given string into server process title
-    -- custom_proc_title = 'example';
-
-    -------------------------
-    -- Storage configuration
-    -------------------------
-
-    -- How much memory Tarantool allocates
-    -- to actually store tuples, in gigabytes
-    slab_alloc_arena = 0.5;
-
-    -- Size of the smallest allocation unit
-    -- It can be tuned up if most of the tuples are not so small
-    slab_alloc_minimal = 16;
-
-    -- Size of the largest allocation unit
-    -- It can be tuned up if it is necessary to store large tuples
-    slab_alloc_maximal = 1048576;
-
-    -- Use slab_alloc_factor as the multiplier for computing
-    -- the sizes of memory chunks that tuples are stored in
-    slab_alloc_factor = 1.06;
-
-    -------------------
-    -- Snapshot daemon
-    -------------------
-
-    -- The interval between actions by the snapshot daemon, in seconds
-    snapshot_period = 0;
-
-    -- The maximum number of snapshots that the snapshot daemon maintans
-    snapshot_count = 6;
-
-    --------------------------------
-    -- Binary logging and snapshots
-    --------------------------------
-
-    -- Abort if there is an error while reading
-    -- the snapshot file (at server start)
-    panic_on_snap_error = true;
-
-    -- Abort if there is an error while reading a write-ahead
-    -- log file (at server start or to relay to a replica)
-    panic_on_wal_error = true;
-
-    -- How many log records to store in a single write-ahead log file
-    rows_per_wal = 5000000;
-
-    -- Reduce the throttling effect of box.snapshot() on
-    -- INSERT/UPDATE/DELETE performance by setting a limit
-    -- on how many megabytes per second it can write to disk
-    snap_io_rate_limit = nil;
-
-    -- Specify fiber-WAL-disk synchronization mode as:
-    -- "none": write-ahead log is not maintained;
-    -- "write": fibers wait for their data to be written to the write-ahead log;
-    -- "fsync": fibers wait for their data, fsync follows each write;
-    wal_mode = "none";
-
-    -- Number of seconds between periodic scans of the write-ahead-log
-    -- file directory
-    wal_dir_rescan_delay = 2.0;
-
-    ---------------
-    -- Replication
-    ---------------
 
     -- The server is considered to be a Tarantool replica
     -- it will try to connect to the master
@@ -115,16 +36,81 @@ box.cfg {
     -- by default username is "guest"
     -- replication_source="127.0.0.1:3102";
 
-    --------------
-    -- Networking
-    --------------
-
     -- The server will sleep for io_collect_interval seconds
     -- between iterations of the event loop
     io_collect_interval = nil;
 
     -- The size of the read-ahead buffer associated with a client connection
     readahead = 16320;
+
+    ----------------------
+    -- Memtx configuration
+    ----------------------
+
+    -- An absolute path to directory where snapshot (.snap) files are stored.
+    -- If not specified, defaults to /var/lib/tarantool/INSTANCE
+    -- memtx_dir = nil;
+
+    -- How much memory Memtx engine allocates
+    -- to actually store tuples, in bytes.
+    memtx_memory = 128 * 1024 * 1024; -- 128Mb
+
+    -- Size of the smallest allocation unit, in bytes.
+    -- It can be tuned up if most of the tuples are not so small
+    memtx_min_tuple_size = 16;
+
+    -- Size of the largest allocation unit, in bytes.
+    -- It can be tuned up if it is necessary to store large tuples
+    memtx_max_tuple_size = 1 * 1024 * 1024; -- 1Mb
+
+    ----------------------
+    -- Vinyl configuration
+    ----------------------
+
+    -- An absolute path to directory where Vinyl files are stored.
+    -- If not specified, defaults to /var/lib/tarantool/INSTANCE
+    -- vinyl_dir = nil;
+
+    -- How much memory Vinyl engine can use for in-memory level, in bytes.
+    vinyl_memory = 128 * 1024 * 1024; -- 128Mb
+
+    -- How much memory Vinyl engine can use for caches, in bytes.
+    vinyl_cache = 128 * 1024 * 1024; -- 128Mb
+
+    -- The maximum number of background workers for compaction.
+    vinyl_threads = 2;
+
+    ------------------------------
+    -- Binary logging and recovery
+    ------------------------------
+
+    -- An absolute path to directory where write-ahead log (.xlog) files are
+    -- stored. If not specified, defaults to /var/lib/tarantool/INSTANCE
+    -- wal_dir = nil;
+
+    -- Specify fiber-WAL-disk synchronization mode as:
+    -- "none": write-ahead log is not maintained;
+    -- "write": fibers wait for their data to be written to the write-ahead log;
+    -- "fsync": fibers wait for their data, fsync follows each write;
+    wal_mode = "none";
+
+    -- How many log records to store in a single write-ahead log file
+    rows_per_wal = 5000000;
+
+    -- The interval between actions by the snapshot daemon, in seconds
+    checkpoint_interval = 60 * 60; -- one hour
+
+    -- The maximum number of snapshots that the snapshot daemon maintans
+    checkpoint_count = 6;
+
+    -- Reduce the throttling effect of box.snapshot() on
+    -- INSERT/UPDATE/DELETE performance by setting a limit
+    -- on how many megabytes per second it can write to disk
+    snap_io_rate_limit = nil;
+
+    -- Don't abort recovery if there is an error while reading
+    -- files from the disk at server start.
+    force_recovery = true;
 
     ----------
     -- Logging
@@ -145,11 +131,14 @@ box.cfg {
 
     -- If true, tarantool does not block on the log file descriptor
     -- when it’s not ready for write, and drops the message instead
-    logger_nonblock = true;
+    log_nonblock = true;
 
     -- If processing a request takes longer than
     -- the given value (in seconds), warn about it in the log
     too_long_threshold = 0.5;
+
+    -- Inject the given string into server process title
+    -- custom_proc_title = 'example';
 }
 
 local function bootstrap()
