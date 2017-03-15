a06f3fe726be938ba9c5ff0a5bddcaa15366ca41
GeorgyKirichenko <kirichenkoga@gmail.com>
2017-03-15 14:55:33 +0300

    Add a test case for final join
    Restart replica after final join and check data.
    
    Closes #2002

--- test/engine/replica_join.test.lua
+++ test/engine/replica_join.test.lua
@@ -60,5 +60,21 @@ _ = test_run:cmd("cleanup server replica")
 
 space:drop()
 box.snapshot()
+
+space = box.schema.space.create('test', { id = 99998, engine = engine })
+index = space:create_index('primary', { type = test_run:get_cfg('index')})
+for i = 0, 9 do space:insert({i, 'test' .. tostring(i)}) end
+test_run:cmd("deploy server replica")
+test_run:cmd("start server replica")
+test_run:cmd('wait_lsn replica default')
+test_run:cmd('restart server replica')
+test_run:cmd('switch replica')
+box.space.test:select()
+test_run:cmd('switch default')
+test_run:cmd("stop server replica")
+_ = test_run:cmd("cleanup server replica")
+space:drop()
+box.snapshot()
+
 box.schema.user.revoke('guest', 'replication')
 box.schema.user.revoke('guest', 'read,write,execute', 'universe')
