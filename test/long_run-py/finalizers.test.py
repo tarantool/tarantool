d4e96d6ceecaf72aa688adbe7c65f2a10334b356
bigbes <bigbes@gmail.com>
2017-03-15 23:03:40 +0300

    Fix forgotten finalizers test (long running test)

--- test/long_run-py/finalizers.test.py
+++ test/long_run-py/finalizers.test.py
@@ -7,11 +7,10 @@ from lib.tarantool_server import TarantoolServer
 server = TarantoolServer(server.ini)
 server.script = 'long_run-py/lua/finalizers.lua'
 server.vardir = os.path.join(server.vardir, 'finalizers')
+server.crash_expected = True
 try:
     server.deploy()
 except:
     print "Expected error:", sys.exc_info()[0]
 else:
     print "Error! exception did not occur"
-
-
