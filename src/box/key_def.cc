616413f501af1c64638e081a44d262b37648bb42
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2017-08-25 18:48:32 +0300

    Fix typo in key_mp_type

--- src/box/key_def.cc
+++ src/box/key_def.cc
@@ -59,6 +59,7 @@ const uint32_t key_mp_type[] = {
 	/* [FIELD_TYPE_SCALAR]   =  */ (1U << MP_UINT) | (1U << MP_INT) |
 		(1U << MP_FLOAT) | (1U << MP_DOUBLE) | (1U << MP_STR) |
 		(1U << MP_BIN) | (1U << MP_BOOL) | (1U << MP_NIL),
+	/* [FIELD_TYPE_MAP]      =  */ 1U << MP_MAP,
 };
 
 struct key_def *
