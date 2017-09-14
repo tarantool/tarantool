tarantool/tarantool

commit 61ffe793a8ae7d108af7473524d8d89edaea432a

vinyl: fix read iterator restoration to a newer version of the same key
After the read iterator selects the minimal key across all available
sources, it checks mutable sources for new statements using ->restore()
callback. If there is a new statement in a source, it uses it as the min
key provided it is *strictly* less than the current min key. If they are
equal, the min key isn't changed, but this is wrong, because the new
statement may be newer than the statement selected previously. If we
don't select it, we might end up with stale data in the cache. Fix this.

src/box/vy_read_iterator.c
@@ -366,7 +366,10 @@ vy_merge_iterator_next_key(struct vy_merge_iterator *itr, struct tuple **ret)
 
 		int cmp = min_stmt == NULL ? -1 :
 			  dir * vy_tuple_compare(src->stmt, min_stmt, def);
-		if (cmp < 0) {
+		if (cmp > 0)
+			continue;
+
+		if (cmp < 0 || vy_stmt_lsn(src->stmt) > vy_stmt_lsn(min_stmt)) {
 			itr->front_id++;
 			if (min_stmt)
 				tuple_unref(min_stmt);
@@ -374,7 +377,7 @@ vy_merge_iterator_next_key(struct vy_merge_iterator *itr, struct tuple **ret)
 			tuple_ref(min_stmt);
 			itr->curr_src = i;
 			src->front_id = itr->front_id;
-		} else if (cmp == 0) {
+		} else {
 			itr->curr_src = MIN(itr->curr_src, (uint32_t)i);
 			src->front_id = itr->front_id;
 		}

commit 650f656eb172a508806a0d7d500432ed00a02d26

vinyl: don't advance read iterator pos after restore to min key
If the iterator over a merge source was restored to a newer version of
the min key, we must not increment front_id, otherwise the source won't
be advanced on the next iteration, potentially resulting in the same key
returned twice by vy_merge_iterator_next_key(). In the long run, this is
likely to break the cache. Fix this and add an assert() checking that it
never happens.

While we are at it, remove loop from vy_read_iterator_merge_next_key()
that checks that the statement returned by vy_merge_iterator_next_key()
isn't the same as the statement returned to the user last time. The loop
condition should never evaluate to true, which is assured by the new
assert().

Fixes: 61ffe793a8ae ("vinyl: fix read iterator restoration to a newer
version of the same key").

src/box/vy_read_iterator.c
@@ -347,9 +347,6 @@ vy_merge_iterator_next_key(struct vy_merge_iterator *itr, struct tuple **ret)
 	if (itr->skipped_start < itr->src_count)
 		itr->range_ended = false;
 
-	if (itr->curr_stmt != NULL && min_stmt != NULL)
-		assert(dir * vy_tuple_compare(min_stmt, itr->curr_stmt, def) >= 0);
-
 	for (int i = MIN(itr->skipped_start, itr->mutable_end) - 1;
 	     was_yield_possible && i >= (int) itr->mutable_start; i--) {
 		struct vy_merge_src *src = &itr->src[i];
@@ -370,25 +367,28 @@ vy_merge_iterator_next_key(struct vy_merge_iterator *itr, struct tuple **ret)
 			continue;
 
 		if (cmp < 0 || vy_stmt_lsn(src->stmt) > vy_stmt_lsn(min_stmt)) {
-			itr->front_id++;
 			if (min_stmt)
 				tuple_unref(min_stmt);
 			min_stmt = src->stmt;
 			tuple_ref(min_stmt);
-			itr->curr_src = i;
-			src->front_id = itr->front_id;
-		} else {
-			itr->curr_src = MIN(itr->curr_src, (uint32_t)i);
-			src->front_id = itr->front_id;
 		}
-		if (itr->curr_stmt != NULL && min_stmt != NULL)
-			assert(dir * vy_tuple_compare(min_stmt, itr->curr_stmt, def) >= 0);
+
+		if (cmp < 0) {
+			itr->front_id++;
+			itr->curr_src = i;
+		} else
+			itr->curr_src = MIN(itr->curr_src, (uint32_t)i);
+
+		src->front_id = itr->front_id;
 	}
 
 	if (itr->skipped_start < itr->src_count)
 		itr->range_ended = false;
 
 	itr->unique_optimization = false;
+
+	if (itr->curr_stmt != NULL && min_stmt != NULL)
+		assert(dir * vy_tuple_compare(min_stmt, itr->curr_stmt, def) > 0);
 
 	if (itr->curr_stmt != NULL)
 		tuple_unref(itr->curr_stmt);
@@ -734,17 +734,8 @@ vy_read_iterator_merge_next_key(struct vy_read_iterator *itr,
 {
 	int rc;
 	struct vy_merge_iterator *mi = &itr->merge_iterator;
-retry:
-	*ret = NULL;
 	while ((rc = vy_merge_iterator_next_key(mi, ret)) == -2)
 		vy_read_iterator_restore(itr);
-	/*
-	 * If the iterator after next_key is on the same key then
-	 * go to the next.
-	 */
-	if (*ret != NULL && itr->curr_stmt != NULL &&
-	    vy_tuple_compare(itr->curr_stmt, *ret, itr->index->cmp_def) == 0)
-		goto retry;
 	return rc;
 }
 