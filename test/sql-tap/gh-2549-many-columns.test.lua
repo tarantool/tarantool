9fe1e710d21e8172b0d8f5270d799da25905fbda
Bulat Niatshin <niatshin@tarantool.org>
2017-09-20 15:10:49 +0300

    sql: check SQLite column limit in table
    Check that SQLite column limit in table works properly.
    User should be able to create table with <= 2000 columns.
    When column amount is higher than 2000, SQL should raise an error.
    
    Closes #2549

---
+++ test/sql-tap/gh-2549-many-columns.test.lua
@@ -0,0 +1,42 @@
+#!/usr/bin/env tarantool
+-- The maximum number of columns in SQLite is 2000. This test checks it.
+
+test = require("sqltester")
+test:plan(2)
+
+success_column_count = 2000
+fail_column_count = 2001
+
+test:execsql("DROP TABLE IF EXISTS t1")
+test:execsql("DROP TABLE IF EXISts t2")
+
+local function form_create_statement(name, column_count)
+	create_statement = 'CREATE TABLE ' .. name .. '('
+	for i = 1,column_count do
+		if i > 1 then create_statement = create_statement .. ',' end
+		create_statement = create_statement .. 's' .. i .. ' INT'
+		if i == 1 then create_statement = create_statement .. ' PRIMARY KEY' end
+	end
+	create_statement = create_statement .. ')'
+	return create_statement
+end
+
+success_statement = form_create_statement("t1", success_column_count)
+fail_statement = form_create_statement("t2", fail_column_count)
+
+test:do_execsql_test(
+	"columns-1.1",
+	success_statement, {
+		-- <columns-1.1>
+		-- <columns-1.1>
+	})
+
+test:do_catchsql_test(
+	"columns-1.2",
+	fail_statement, {
+		-- <columns-1.2>
+		1, "too many columns on t2"
+		-- <columns-1.2>
+	})
+
+test:finish_test()
