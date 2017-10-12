8b6cefd0410bfbbb4e72a1d265ed36fb149f47a4
Ilya <markovilya197@gmail.com>
2017-10-12 13:21:46 +0300

    box: start database with default without error
    * Call box.cfg() instead of raising an error on the first access to box.XXX
    
    Fixes #2559

--- src/box/lua/load_cfg.lua
+++ src/box/lua/load_cfg.lua
@@ -342,13 +342,6 @@ for k, v in pairs(box) do
     end
 end
 
-setmetatable(box, {
-    __index = function(table, index)
-        error(debug.traceback("Please call box.cfg{} first"))
-        error("Please call box.cfg{} first")
-     end
-})
-
 local function load_cfg(cfg)
     box.internal.schema.init()
     cfg = upgrade_cfg(cfg, translate_cfg)
@@ -388,6 +381,13 @@ local function load_cfg(cfg)
     end
 end
 box.cfg = load_cfg
+
+setmetatable(box, {
+    __index = function(table, index)
+        load_cfg()
+        return rawget(table, index)
+     end
+})
 
 -- gh-810:
 -- hack luajit default cpath
