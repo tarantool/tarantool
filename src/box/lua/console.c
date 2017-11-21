3e8b981a7d876c269b390a7826e27e26d67802c7
Ivan Kosenko <kosenko.ivan@gmail.com>
2017-11-21 23:40:50 +0300

    Coding style fix.

--- src/box/lua/console.c
+++ src/box/lua/console.c
@@ -320,8 +320,7 @@ lbox_console_add_history(struct lua_State *L)
 		luaL_error(L, "add_history(string)");
 
 	const char *s = lua_tostring(L, 1);
-	if (*s)
-	{
+	if (*s) {
 		HIST_ENTRY *hist_ent = history_get(history_length - 1 + history_base);
 		const char *prev_s = hist_ent ? hist_ent->line : "";
 		if (strcmp(prev_s, s) != 0)
