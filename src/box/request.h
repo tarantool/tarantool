05f9d5685485ee6196e7dcb834f275942d49531e
Vladimir Davydov <vdavydov.dev@gmail.com>
2018-01-22 17:24:05 +0300

    Move helpers for updating request from space.c to request.c
    This patch moves helpers used to fix requests after certain DML
    operations to a separate source file. Currently, there are only
    two of them, but there are going to be more so it seems to be a
    good idea to isolate them. No functional changes.
    
    Suggested by @kostja

---
+++ src/box/request.h
@@ -0,0 +1,69 @@
+#ifndef TARANTOOL_BOX_REQUEST_H_INCLUDED
+#define TARANTOOL_BOX_REQUEST_H_INCLUDED
+/*
+ * Copyright 2010-2018, Tarantool AUTHORS, please see AUTHORS file.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above
+ *    copyright notice, this list of conditions and the
+ *    following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials
+ *    provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * <COPYRIGHT HOLDER> OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#if defined(__cplusplus)
+extern "C" {
+#endif /* defined(__cplusplus) */
+
+struct request;
+struct space;
+struct tuple;
+
+/**
+ * Convert a request accessing a secondary key to a primary
+ * key undo record, given it found a tuple.
+ * Flush iproto header of the request to be reconstructed in
+ * txn_add_redo().
+ *
+ * @param request - request to fix
+ * @param space - space corresponding to request
+ * @param found_tuple - tuple found by secondary key
+ */
+void
+request_rebind_to_primary_key(struct request *request, struct space *space,
+			      struct tuple *found_tuple);
+
+/**
+ * Handle INSERT/REPLACE in a space with a sequence attached.
+ *
+ * @param request - request to fix
+ * @param space - space corresponding to request
+ */
+int
+request_handle_sequence(struct request *request, struct space *space);
+
+#if defined(__cplusplus)
+} /* extern "C" */
+#endif /* defined(__cplusplus) */
+
+#endif /* TARANTOOL_BOX_REQUEST_H_INCLUDED */
