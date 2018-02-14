96cf99f56b277f49fd02386110645a35f0c7588a
imarkov <imarkov@tarantool.org>
2018-02-14 14:26:05 +0300

    http: Add cookie parser
    * Add parser of cookies from 'Set-Cookie' header.
    * All cookies are stored in response object in cookie table.
    * The format of each table element is following:
    resp[key] = {value, {option, ...}},
    where key, value is cookie key, value
    options are array of strings, set-cookie header options,
    e.g. HttpOnly, Expires=<date>.
    
    Closes #2801

--- src/lua/httpc.lua
+++ src/lua/httpc.lua
@@ -103,6 +103,119 @@ local special_headers = {
     ["user-agent"] = true,
 }
 
+local special_characters = {
+    ['('] = true,
+    [')'] = true,
+    ['['] = true,
+    [']'] = true,
+    ['<'] = true,
+    ['>'] = true,
+    ['>'] = true,
+    ['@'] = true,
+    [','] = true,
+    [';'] = true,
+    [':'] = true,
+    ['\\'] = true,
+    ['\"'] = true,
+    ['/'] = true,
+    ['?'] = true,
+    ['='] = true,
+    ['{'] = true,
+    ['}'] = true,
+    [' '] = true,
+    ['\t'] = true
+}
+
+local option_keys = {
+    ["Expires"] = true,
+    ["Max-Age"] = true,
+    ["Domain"] = true,
+    ["Path"] = true,
+    ["Secure"] = true,
+    ["HttpOnly"] = true,
+    ["SameSite"] = true,
+}
+
+--local function process_set_cookies(value, result)
+--    local key_start, value_start
+--    local key, val
+--    local symbols = value:gmatch('.')
+--    local options = {}
+--    local cur = 0
+--    for v in symbols do
+--        cur = cur + 1
+--        if v == ' ' or v == '\t' then
+--            goto continue
+--        end
+--        key_start = cur
+--        -- parse cookie name
+--        while not special_characters[v] do
+--            if v == nil then
+--                return
+--            end
+--            v = symbols()
+--            cur = cur + 1
+--        end
+--        key = value:sub(key_start, cur)
+--        if not v or v ~= '=' then
+--            -- invalid header
+--            return
+--        end
+--        while v == ' ' do
+--            v = symbols()
+--            cur = cur + 1
+--        end
+--
+--        if v == nil then
+--            return
+--        end
+--
+--        while v and v ~= ';' do
+--            if v == nil then
+--                break
+--            end
+--            v = symbols()
+--            cur = cur + 1
+--        end
+--
+--        result[key] = {val, options}
+--        ::continue::
+--    end
+--end
+
+local function process_cookie(cookie, result)
+    local vals = cookie:split(';')
+    local val = vals[1]:split('=')
+    if #val < 2 then
+        return
+    end
+    val[1] = string.strip(val[1])
+    for c in val[1]:gmatch('.') do
+        if special_characters[c] then
+            return
+        end
+    end
+
+    local options = {}
+    table.remove(vals, 1)
+    for _, opt in pairs(vals) do
+        local splitted = opt:split('=')
+        splitted = string.strip(splitted[1])
+        if option_keys[splitted] then
+            table.insert(options, string.strip(opt))
+        end
+    end
+    result[val[1]] = {string.strip(val[2]), options}
+end
+
+local function process_cookies(cookies)
+    local result = {}
+    for _, val in pairs(cookies) do
+        process_cookie(val, result)
+    end
+    return result
+end
+
 local function process_headers(headers)
     for header, value in pairs(headers) do
         if type(value) == 'table' then
@@ -185,6 +298,9 @@ curl_mt = {
             end
             local resp = self.curl:request(method, url, body, opts or {})
             if resp and resp.headers then
+                if resp.headers['set-cookie'] ~= nil then
+                    resp.cookies = process_cookies(resp.headers['set-cookie'])
+                end
                 resp.headers = process_headers(resp.headers)
             end
             return resp
