a7871247d7fe520c76cd2ae37729bfb7602d57ad
Konstantin Osipov <kostja@tarantool.org>
2018-02-15 22:22:00 +0300

    Revert "replication: disconnect applier on timeout"
    This reverts commit 99c7a971df04d7234ee88da501b291497c4dd43f.

--- test/replication/replica_ack.lua
+++ test/replication/replica_ack.lua
@@ -4,8 +4,7 @@ box.cfg({
     listen              = os.getenv("LISTEN"),
     replication         = os.getenv("MASTER"),
     memtx_memory        = 107374182,
-    replication_timeout = 0.00001,
-    replication_connect_quorum = 0,
+    replication_timeout = 0.00001
 })
 
 require('console').listen(os.getenv('ADMIN'))
