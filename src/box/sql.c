5735fc36f4ecc2592f9236cefcd29e3d1087afcd
Kirill Shcherbatov <kshcherbatov@tarantool.org>
2018-06-26 15:14:39 +0300

    sql: fix SQL Count for vinyl engine
    As index_size does not match row count for vinyl engine,
    it was replaced with index_count that working for
    ITER_ALL in memtx same way.
    
    Part of #2199.

--- src/box/sql.c
+++ src/box/sql.c
@@ -355,7 +355,8 @@ int tarantoolSqlite3EphemeralCount(struct BtCursor *pCur, i64 *pnEntry)
 	assert(pCur->curFlags & BTCF_TEphemCursor);
 
 	struct index *primary_index = space_index(pCur->space, 0 /* PK */);
-	*pnEntry = index_size(primary_index);
+	*pnEntry = index_count(primary_index, pCur->iter_type, pCur->key,
+			       pCur->nKey);
 	return SQLITE_OK;
 }
 
@@ -363,7 +364,8 @@ int tarantoolSqlite3Count(BtCursor *pCur, i64 *pnEntry)
 {
 	assert(pCur->curFlags & BTCF_TaCursor);
 
-	*pnEntry = index_size(pCur->index);
+	*pnEntry = index_count(pCur->index, pCur->iter_type, pCur->key,
+			       pCur->nKey);
 	return SQLITE_OK;
 }
 
