a09c04bf92da309b48289917314404b94d0235c9
Konstantin Osipov <kostja@tarantool.org>
2018-07-10 10:48:06 +0300

    test: fix a sporadic failure of replication/gc.test
    The test was failing due to a race condition between gc and relay
    threads on the master: replica could have acknoledged the last xlog
    before it was stopped, in which case gc would correctly delete it and
    the total number of xlogs would be equal to 2, not 3.

--- test/replication/gc.test.lua
+++ test/replication/gc.test.lua
@@ -145,7 +145,11 @@ box.snapshot()
 _ = s:auto_increment{}
 box.snapshot()
 #box.info.gc().checkpoints == 1 or box.info.gc()
-#fio.glob('./master/*.xlog') == 3 or fio.listdir('./master')
+xlog_count = #fio.glob('./master/*.xlog')
+-- the replica may have managed to download all data
+-- from xlog #1 before it was stopped, in which case
+-- it's OK to collect xlog #1
+xlog_count == 3 xlog_count == 2 or fio.listdir('./master')
 
 -- The xlog should only be deleted after the replica
 -- is unregistered.
