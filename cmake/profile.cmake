665467f823fc11adb580dce6309faf02ded8fabc
Alexander Turenko <alexander.turenko@tarantool.org>
2018-12-20 15:43:28 +0300

    Fix build with -DENABLE_ASAN=ON on >=clang-3.9.1
    LLVM changes the API in the compiler-rt commit [1]. According to the
    date of the commit it is between clang-3.9.0 and clang-3.9.1.
    
    [1]: https://github.com/llvm-mirror/compiler-rt/commit/0b95585616bd28fc0b738289bcc5f7887d7c304e
    
    Fixes #3070.

--- cmake/profile.cmake
+++ cmake/profile.cmake
@@ -43,4 +43,27 @@ endif()
 option(ENABLE_ASAN "Enable AddressSanitizer, a fast memory error detector based on compiler instrumentation" OFF)
 if (ENABLE_ASAN)
     add_compile_flags("C;CXX" -fsanitize=address)
+
+    set(CMAKE_REQUIRED_FLAGS "-fsanitize=address")
+    check_c_source_compiles("int main(void) {
+        #include <sanitizer/asan_interface.h>
+        void *x;
+	    __sanitizer_finish_switch_fiber(x);
+        return 0;
+        }" ASAN_INTERFACE_OLD)
+    check_c_source_compiles("int main(void) {
+        #include <sanitizer/asan_interface.h>
+        void *x;
+	    __sanitizer_finish_switch_fiber(x, 0, 0);
+        return 0;
+    }" ASAN_INTERFACE_NEW)
+    set(CMAKE_REQUIRED_FLAGS "")
+
+    if (ASAN_INTERFACE_OLD)
+        add_definitions(-DASAN_INTERFACE_OLD=1)
+    elseif (ASAN_INTERFACE_NEW)
+        add_definitions(-UASAN_INTERFACE_OLD)
+    else()
+        message(FATAL_ERROR "Cannot enable AddressSanitizer")
+    endif()
 endif()
