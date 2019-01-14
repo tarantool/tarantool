dfa7a61d53fd8ad1d5aabb8138028eb121a40f61
Konstantin Osipov <kostja@tarantool.org>
2019-01-14 06:38:58 +0300

    box: expose snapshot status in box.info.gc()
    Add box.info.gc.checkpoint_is_in_progress which is true when
    there is an ongoing checkpoint/snapshot and false otherwise
    
    Closes gh-3935
    
    @TarantoolBot document
    Title: box.info.gc().checkpoint_is_in_progress
    
    Extend box.info.gc() documentation with a new member
    - checkpoint_is_in_progress, which is true if there is an ongoing
    checkpoint, false otherwise.

---
+++ test/box-tap/gc.test.lua
@@ -0,0 +1,32 @@
+#!/usr/bin/env tarantool
+local tap = require('tap')
+local fiber = require('fiber')
+
+box.cfg{}
+
+local debug = type(box.error.injection) == "table" 
+
+-- check box.info.gc() is false if snapshot is not in progress
+local test = tap.test('box.info.gc')
+test:plan(1 + (debug and 1 or 0))
+
+
+local gc = box.info.gc()
+test:is(gc.checkpoint_is_in_progress, false, "checkpoint is not in progress")
+
+-- check box.info.gc() is true if snapshot is in progress
+--
+if debug then
+    box.error.injection.set("ERRINJ_SNAP_COMMIT_DELAY", true)
+    local snapshot_f  = function()
+       box.snapshot() 
+    end
+    fiber.create(snapshot_f)
+    local gc = box.info.gc()
+    test:is(gc.checkpoint_is_in_progress, true, "checkpoint is in progress")
+    box.error.injection.set("ERRINJ_SNAP_COMMIT_DELAY", false)
+end
+
+test:check()
+
+os.exit(0)
