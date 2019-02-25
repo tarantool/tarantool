37b883145ff0464640d917fe58b488eb7d539400
Vladimir Davydov <vdavydov.dev@gmail.com>
2019-02-25 20:03:27 +0300

    iproto: don't attempt to close socket if it was not open
    This can't entail any consequences, because socket fd is set to -1 in
    this case, but this just looks a bit messy. Let's clean it up.
    
    Follow-up commit 305dbcf68953 ("iproto: close socket explicitly before
    wal_dir at exit").

--- src/box/iproto.cc
+++ src/box/iproto.cc
@@ -2152,5 +2152,6 @@ iproto_free()
 	* failing to bind in case it tries to bind before socket
 	* is closed by OS.
 	*/
-	close(binary.ev.fd);
+	if (evio_service_is_active(&binary))
+		close(binary.ev.fd);
 }
