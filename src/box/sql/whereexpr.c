20531034548e699b598d16ff6fabe7253db90747
Kirill Shcherbatov <kshcherbatov@tarantool.org>
2019-02-26 19:37:17 +0300

    sql: rework sqlExpr to set diag
    Refactored sqlExpr routine as sql_expr_new and reworked it to set
    diag message in case of memory allocation error. Also performed some
    additional name refactoring in adjacent places.
    This change is necessary because the sqlExpr body has a
    sqlNormalizeName call that will be changed in subsequent patches.
    
    After that patch there are basically 2 ways of errors
    processing and forwarding:
         - Use diag only. It works for all the places out of
           src/box/sql, and for some functions inside it. For
           example, sql_expr_new();
         - Use global flags Parse.is_aborted, sql.mallocFailed.
    
    It is easy to see, that some places use both of them
    implicitly. For example, sql_expr_new() and every other
    place which uses SQLite memory allocators + diag. But it
    is ok until the former is removed. What is more important,
    is that at least one of these two methods should be
    consistent and finished in every functions. And match a
    declared behaviour.
    
    For example, it is incorrect to declare a function as setting
    flags on an error, but in fact set diag only. Or vice versa,
    that it throws a diag, but factually sets flags only.
    
    Part of #3931

--- src/box/sql/whereexpr.c
+++ src/box/sql/whereexpr.c
@@ -307,8 +307,10 @@ like_optimization_is_valid(Parse *pParse, Expr *pExpr, Expr **ppPrefix,
 			Expr *pPrefix;
 			*pisComplete = c == MATCH_ALL_WILDCARD &&
 				       z[cnt + 1] == 0;
-			pPrefix = sqlExpr(db, TK_STRING, z);
-			if (pPrefix)
+			pPrefix = sql_expr_new_named(db, TK_STRING, z);
+			if (pPrefix == NULL)
+				pParse->is_aborted = true;
+			else
 				pPrefix->u.zToken[cnt] = 0;
 			*ppPrefix = pPrefix;
 			if (op == TK_VARIABLE) {
@@ -1306,10 +1308,11 @@ exprAnalyze(SrcList * pSrc,	/* the FROM clause */
 		Expr *pLeft = pExpr->pLeft;
 		int idxNew;
 		WhereTerm *pNewTerm;
-
-		pNewExpr = sqlPExpr(pParse, TK_GT,
-					sqlExprDup(db, pLeft, 0),
-					sqlExprAlloc(db, TK_NULL, 0, 0));
+		struct Expr *expr = sql_expr_new_anon(db, TK_NULL);
+		if (expr == NULL)
+			pParse->is_aborted = true;
+		pNewExpr = sqlPExpr(pParse, TK_GT, sqlExprDup(db, pLeft, 0),
+				    expr);
 
 		idxNew = whereClauseInsert(pWC, pNewExpr,
 					   TERM_VIRTUAL | TERM_DYNAMIC |
@@ -1502,9 +1505,11 @@ sqlWhereTabFuncArgs(Parse * pParse,	/* Parsing context */
 		 * unused.
 		 */
 		assert(k < (int)space_def->field_count);
-		pColRef = sqlExprAlloc(pParse->db, TK_COLUMN, 0, 0);
-		if (pColRef == 0)
+		pColRef = sql_expr_new_anon(pParse->db, TK_COLUMN);
+		if (pColRef == NULL) {
+			pParse->is_aborted = true;
 			return;
+		}
 		pColRef->iTable = pItem->iCursor;
 		pColRef->iColumn = k++;
 		pColRef->space_def = space_def;
