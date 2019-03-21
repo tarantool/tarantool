f5721edc825b0977e73124ad59ff022f71e8a5e0
Kirill Shcherbatov <kshcherbatov@tarantool.org>
2019-03-21 14:30:26 +0300

    test: check hints corner cases
    We must to enshure that hints don't broke tuple comparison.
    As different field_types use different routines to calculate hint,
    we must to ensure that the calculated values are compatible with
    each other.
    
    Also tested the corner cases for large double numbers, due to the
    fact that there may be additional difficulties in hint construction.
    
    Follow-up
        9fba29abb4e0 memtx: introduce tuple compare hint

---
+++ test/engine/hints.test.lua
@@ -0,0 +1,62 @@
+--
+-- gh-3961: Introduce tuple comparison hints
+-- We must to enshure that hints don't broke
+-- tuple comparison.
+--
+ffi = require('ffi')
+
+test_run = require('test_run')
+inspector = test_run.new()
+engine = inspector:get_cfg('engine')
+
+inspector:cmd("setopt delimiter ';'");
+function insert_values(type)
+        local x = 54
+        while (x <= 64) do
+                local val = ffi.new(type, 2^x-1)
+                s:replace({val})
+                x = x + 1
+        end
+end;
+inspector:cmd("setopt delimiter ''");
+
+-- Test that hints does not violate the correct order of
+-- big numeric fields.
+s = box.schema.space.create('test', {engine = engine})
+i1 = s:create_index('i1', {parts = {1, 'unsigned'}})
+insert_values('uint64_t')
+s:select()
+i1:alter{parts = {1, 'integer'}}
+insert_values('int64_t')
+s:select()
+i1:alter{parts = {1, 'number'}}
+insert_values('double')
+s:select()
+s:drop()
+
+-- Test that the use of hint(s) does not violate alter between
+-- scalar and string.
+s = box.schema.space.create('test', {engine = engine})
+i1 = s:create_index('i1', {parts = {1, 'string'}})
+s:insert({"bbb"})
+s:insert({"ccc"})
+i1:alter{parts = {1, 'scalar'}}
+s:insert({"aaa"})
+s:insert({"ddd"})
+s:select()
+s:drop()
+
+-- Test that hints does not violate the correct order of
+-- numeric fields (on alter).
+s = box.schema.space.create('test', {engine = engine})
+i1 = s:create_index('i1', {parts = {1, 'unsigned'}})
+s:insert({11})
+i1:alter{parts = {1, 'integer'}}
+s:insert({-22})
+i1:alter{parts = {1, 'number'}}
+s:insert({-33.33})
+i1:alter{parts = {1, 'scalar'}}
+s:insert({44.44})
+s:insert({"Hello world"})
+s:select()
+s:drop()
