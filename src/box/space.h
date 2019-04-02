5c30b2152085d724aa5ae35583427f3ef69350f2
Vladimir Davydov <vdavydov.dev@gmail.com>
2019-04-02 19:58:50 +0300

    space: fix space_group_id return value
    Should be uint32_t obviously, not bool. This didn't affect anything,
    because there are only two replication groups right now - 0 and 1.
    
    Spotted by @GeorgyKirichenko.
    
    Fixes commit f64f46199e19 ("Introduce replica local spaces").

--- src/box/space.h
+++ src/box/space.h
@@ -235,7 +235,7 @@ space_is_temporary(struct space *space)
 }
 
 /** Return replication group id of a space. */
-static inline bool
+static inline uint32_t
 space_group_id(struct space *space)
 {
 	return space->def->opts.group_id;
