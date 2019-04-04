5ab0763b856d495d1d61055f946f6c0cf87dd065
Serge Petrenko <sergepetrenko@tarantool.org>
2019-04-04 20:40:30 +0300

    lua: add type of operation to space trigger parameters
    Add the type of operation which is being executed to before_replace and
    on_replace triggers.
    
    Closes #4099
    
    @TarantoolBot document
    Title: new parameter for space before_replace and on_replace triggers
    Now before_replace and on_replace triggers accept an additional
    parameter: the type of operation that is being executed.
    (INSERT/REPLACE/DELETE/UPDATE/UPSERT)
    For example, a trigger function may now look like this:
    ```
    function before_replace_trig(old, new, space_name, op_type)
        if op_type == 'INSERT' then
    	return old
        else
    	return new
        end
    end
    ```
    And will restrict all INSERTs, but allow REPLACEs, UPSERTs, DELETEs and
    UPDATEs.

--- src/box/space.c
+++ src/box/space.c
@@ -423,6 +423,15 @@ space_before_replace(struct space *space, struct txn *txn,
 	assert(stmt->old_tuple == NULL && stmt->new_tuple == NULL);
 	stmt->old_tuple = old_tuple;
 	stmt->new_tuple = new_tuple;
+	/*
+	 * A fake row attached to txn_stmt during execution
+	 * of before_replace triggers to store operation type.
+	 * It is pushed to the before_replace trigger in lua.
+	 */
+	struct xrow_header temp_header;
+	temp_header.type = type;
+	assert(stmt->row == NULL);
+	stmt->row = &temp_header;
 
 	int rc = trigger_run(&space->before_replace, txn);
 
@@ -435,6 +444,7 @@ space_before_replace(struct space *space, struct txn *txn,
 	assert(stmt->old_tuple == old_tuple);
 	stmt->old_tuple = NULL;
 	stmt->new_tuple = NULL;
+	stmt->row = NULL;
 
 	if (rc != 0)
 		goto out;
