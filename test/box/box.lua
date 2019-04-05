52c67cccae07d130e197cafca2060bf0327bb6fb
Alexander Turenko <alexander.turenko@tarantool.org>
2019-04-05 10:21:32 +0300

    test: enable cleaning of a test environment
    This commit enables pretest_clean test-run option on 'core = tarantool'
    test suites with Lua tests and 'core = app' test suites. Consider #4094
    for an example of a problem that is eliminated by this option.
    
    For 'core = tarantool': this option drops non-system spaces, drops data
    in system spaces and global variables to the initial state, unloads
    packages except build-in ones.
    
    For 'core = app': this option deletes xlog and snap files before run a
    test.
    
    test-run doesn't remove global variables that are listed in the
    'protected_globals' global variable. Use it for, say, functions that are
    defined in an instance file and called from tests.
    
    Consider test-run/README.md for the information how exactly the option
    works.
    
    Removed unused cfg_filter() function from test/engine/box.lua.
    
    Fixes #4094.
    
    (cherry picked from commit 7474c14e9cdb4bae3ab073d375aa11e628e13aa5)

--- test/box/box.lua
+++ test/box/box.lua
@@ -29,7 +29,7 @@ function cfg_filter(data)
     return result
 end
 
-function compare(a,b)
+local function compare(a,b)
     return a[1] < b[1]
 end
 
@@ -37,3 +37,5 @@ function sorted(data)
     table.sort(data, compare)
     return data
 end
+
+_G.protected_globals = {'cfg_filter', 'sorted'}
--- test/engine/box.lua
+++ test/engine/box.lua
@@ -21,24 +21,3 @@ box.cfg{
 }
 
 require('console').listen(os.getenv('ADMIN'))
-
-_to_exclude = {
-    'pid_file', 'log', 'vinyl_dir',
-    'memtx_dir', 'wal_dir',
-    'memtx_min_tuple_size', 'memtx_max_tuple_size'
-}
-
-_exclude = {}
-for _, f in pairs(_to_exclude) do
-    _exclude[f] = 1
-end
-
-function cfg_filter(data)
-    local result = {}
-    for field, val in pairs(data) do
-        if _exclude[field] == nil then
-            result[field] = val
-        end
-    end
-    return result
-end
