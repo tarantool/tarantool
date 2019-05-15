539aee3d07c9abe654108cd9ed960efc6182c856
Alexander Turenko <alexander.turenko@tarantool.org>
2019-05-15 23:07:35 +0300

    iproto: init coio watcher before join/subscribe
    box_process_join() and box_process_subscribe() use coio_write_xrow(),
    which calls coio_writev_timeout() under hood. If a socket will block at
    write() the function calls ev_io_start() to wake the fiber up when the
    socket will be ready to write. This code assumes that the watcher
    (struct ev_io) is initialized as coio watcher, i.e. coio_create() has
    been called.
    
    The reason why the code works before is that coio_write_xrow() in
    box_process_{join,subscribe}() writes a small piece of data and so the
    situation when a socket write buffer has less free space then needed is
    rare.
    
    Fixes #4110.

--- src/box/iproto.cc
+++ src/box/iproto.cc
@@ -1693,6 +1693,8 @@ tx_process_join_subscribe(struct cmsg *m)
 {
 	struct iproto_msg *msg = tx_accept_msg(m);
 	struct iproto_connection *con = msg->connection;
+	struct ev_io io;
+	coio_create(&io, con->input.fd);
 	try {
 		switch (msg->header.type) {
 		case IPROTO_JOIN:
@@ -1701,7 +1703,7 @@ tx_process_join_subscribe(struct cmsg *m)
 			 * the lambda in the beginning of the block
 			 * will re-activate the watchers for us.
 			 */
-			box_process_join(&con->input, &msg->header);
+			box_process_join(&io, &msg->header);
 			break;
 		case IPROTO_SUBSCRIBE:
 			/*
@@ -1710,7 +1712,7 @@ tx_process_join_subscribe(struct cmsg *m)
 			 * the write watcher will be re-activated
 			 * the same way as for JOIN.
 			 */
-			box_process_subscribe(&con->input, &msg->header);
+			box_process_subscribe(&io, &msg->header);
 			break;
 		default:
 			unreachable();
