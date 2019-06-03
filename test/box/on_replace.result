tarantool/tarantool

commit 5ab0763b856d495d1d61055f946f6c0cf87dd065

lua: add type of operation to space trigger parameters
Add the type of operation which is being executed to before_replace and
on_replace triggers.

Closes #4099

@TarantoolBot document
Title: new parameter for space before_replace and on_replace triggers
Now before_replace and on_replace triggers accept an additional
parameter: the type of operation that is being executed.
(INSERT/REPLACE/DELETE/UPDATE/UPSERT)
For example, a trigger function may now look like this:
```
function before_replace_trig(old, new, space_name, op_type)
    if op_type == 'INSERT' then
	return old
    else
	return new
    end
end
```
And will restrict all INSERTs, but allow REPLACEs, UPSERTs, DELETEs and
UPDATEs.

test/box/before_replace.result
@@ -650,9 +650,12 @@ fio = require('fio')
 xlog = require('xlog')
 ---
 ...
-type(s:before_replace(function(old, new) return old end))
+function ret_old(old,new) return old end
 ---
-- function
+...
+s:before_replace(ret_old) == ret_old
+---
+- true
 ...
 s:insert{1, 1}
 ---
@@ -673,6 +676,9 @@ row.HEADER.type
 row.BODY == nil
 ---
 - true
+...
+s:before_replace(nil, ret_old)
+---
 ...
 -- gh-3128 before_replace with run_triggers
 s2 = box.schema.space.create("test2")
@@ -704,6 +710,7 @@ type(s:before_replace(before_replace2))
 ...
 s:insert{1, 1}
 ---
+- [1, 1]
 ...
 s2:select{}
 ---
@@ -760,6 +767,12 @@ s2:select{}
 - - [1]
   - [2]
 ...
+s:truncate()
+---
+...
+s2:drop()
+---
+...
 s:on_replace(nil, on_replace)
 ---
 ...
@@ -769,8 +782,53 @@ s:before_replace(nil, before_replace1)
 s:before_replace(nil, before_replace2)
 ---
 ...
-s2:drop()
+--
+-- gh-4099 provide type of operation inside before_replace triggers
+--
+save_type = ''
 ---
+...
+function find_type(old, new, name, type) save_type = type return new end
+---
+...
+s:before_replace(find_type) == find_type
+---
+- true
+...
+_ = s:insert{1, 2}
+---
+...
+save_type
+---
+- INSERT
+...
+_ = s:update({1}, {{'+', 2, 1}})
+---
+...
+save_type
+---
+- UPDATE
+...
+_ = s:delete{1}
+---
+...
+save_type
+---
+- DELETE
+...
+_ = s:replace{2, 3}
+---
+...
+save_type
+---
+- REPLACE
+...
+_ = s:upsert({3,4,5}, {{'+', 2, 1}})
+---
+...
+save_type
+---
+- UPSERT
 ...
 s:drop()
 ---

test/box/before_replace.test.lua
@@ -217,8 +217,9 @@ s:select() -- [10, 100]
 -- Check that IPROTO_NOP is actually written to xlog.
 fio = require('fio')
 xlog = require('xlog')
+function ret_old(old,new) return old end
 
-type(s:before_replace(function(old, new) return old end))
+s:before_replace(ret_old) == ret_old
 s:insert{1, 1}
 
 path = fio.pathjoin(box.cfg.wal_dir, string.format('%020d.xlog', box.info.lsn - 1))
@@ -226,6 +227,7 @@ fun, param, state = xlog.pairs(path)
 state, row = fun(param, state)
 row.HEADER.type
 row.BODY == nil
+s:before_replace(nil, ret_old)
 
 -- gh-3128 before_replace with run_triggers
 s2 = box.schema.space.create("test2")
@@ -263,8 +265,29 @@ s:insert{1, 1}
 s:select{}
 s2:select{}
 
+s:truncate()
+s2:drop()
+
 s:on_replace(nil, on_replace)
 s:before_replace(nil, before_replace1)
 s:before_replace(nil, before_replace2)
-s2:drop()
+
+--
+-- gh-4099 provide type of operation inside before_replace triggers
+--
+save_type = ''
+function find_type(old, new, name, type) save_type = type return new end
+s:before_replace(find_type) == find_type
+
+_ = s:insert{1, 2}
+save_type
+_ = s:update({1}, {{'+', 2, 1}})
+save_type
+_ = s:delete{1}
+save_type
+_ = s:replace{2, 3}
+save_type
+_ = s:upsert({3,4,5}, {{'+', 2, 1}})
+save_type
+
 s:drop()

test/box/on_replace.result
@@ -698,3 +698,57 @@ s2:drop()
 s3:drop()
 ---
 ...
+s = box.schema.space.create('test')
+---
+...
+_ = s:create_index('pk')
+---
+...
+save_type = ''
+---
+...
+function find_type(old, new, name, type) save_type = type return new end
+---
+...
+s:on_replace(find_type) == find_type
+---
+- true
+...
+_ = s:insert{1, 2}
+---
+...
+save_type
+---
+- INSERT
+...
+_ = s:update({1}, {{'+', 2, 1}})
+---
+...
+save_type
+---
+- UPDATE
+...
+_ = s:delete{1}
+---
+...
+save_type
+---
+- DELETE
+...
+_ = s:replace{2, 3}
+---
+...
+save_type
+---
+- REPLACE
+...
+_ = s:upsert({3,4,5}, {{'+', 2, 1}})
+---
+...
+save_type
+---
+- UPSERT
+...
+s:drop()
+---
+...

test/box/on_replace.test.lua
@@ -279,3 +279,22 @@ s3:select()
 s1:drop()
 s2:drop()
 s3:drop()
+
+s = box.schema.space.create('test')
+_ = s:create_index('pk')
+save_type = ''
+function find_type(old, new, name, type) save_type = type return new end
+s:on_replace(find_type) == find_type
+
+_ = s:insert{1, 2}
+save_type
+_ = s:update({1}, {{'+', 2, 1}})
+save_type
+_ = s:delete{1}
+save_type
+_ = s:replace{2, 3}
+save_type
+_ = s:upsert({3,4,5}, {{'+', 2, 1}})
+save_type
+
+s:drop()

src/box/lua/space.cc
@@ -49,6 +49,7 @@ extern "C" {
 #include "box/sequence.h"
 #include "box/coll_id_cache.h"
 #include "box/replication.h" /* GROUP_LOCAL */
+#include "box/iproto_constants.h" /* iproto_type_name */
 
 /**
  * Trigger function for all spaces
@@ -70,7 +71,10 @@ lbox_push_txn_stmt(struct lua_State *L, void *event)
 	}
 	/* @todo: maybe the space object has to be here */
 	lua_pushstring(L, stmt->space->def->name);
-	return 3;
+	assert(stmt->row != NULL);
+	/* operation type: INSERT/UPDATE/UPSERT/REPLACE/DELETE */
+	lua_pushstring(L, iproto_type_name(stmt->row->type));
+	return 4;
 }
 
 static int

src/box/space.c
@@ -423,6 +423,15 @@ space_before_replace(struct space *space, struct txn *txn,
 	assert(stmt->old_tuple == NULL && stmt->new_tuple == NULL);
 	stmt->old_tuple = old_tuple;
 	stmt->new_tuple = new_tuple;
+	/*
+	 * A fake row attached to txn_stmt during execution
+	 * of before_replace triggers to store operation type.
+	 * It is pushed to the before_replace trigger in lua.
+	 */
+	struct xrow_header temp_header;
+	temp_header.type = type;
+	assert(stmt->row == NULL);
+	stmt->row = &temp_header;
 
 	int rc = trigger_run(&space->before_replace, txn);
 
@@ -435,6 +444,7 @@ space_before_replace(struct space *space, struct txn *txn,
 	assert(stmt->old_tuple == old_tuple);
 	stmt->old_tuple = NULL;
 	stmt->new_tuple = NULL;
+	stmt->row = NULL;
 
 	if (rc != 0)
 		goto out;

commit 63ebc25a4abaa2f2b34b7cee4ba0ec0db3f5a1b2

lua: fix operation type passing to on_replace triggers
This commit fixes a regression introduced by commit
5ab0763b856d495d1d61055f946f6c0cf87dd065
(pass operation type to lua triggers)

When passing operation type to the trigger, we relied on a corresponding
xrow_header, which would later be written to the log. When before_replace
triggers are fired, there is no row connected to the txn statement yet,
so we faked one to pass operations. We, however, didn't account for
temporary spaces, which never have a row associated with a corresponding
txn stmt. This lead to a segfault on an attemt to use on_replace triggers
with temporary spaces.

Add a fake row for temporary spaces to pass operation type in on_replace
triggers and add some tests.

Closes #4266

src/box/txn.c
@@ -312,7 +312,24 @@ txn_commit_stmt(struct txn *txn, struct request *request)
 	 */
 	if (stmt->space != NULL && !rlist_empty(&stmt->space->on_replace) &&
 	    stmt->space->run_triggers && (stmt->old_tuple || stmt->new_tuple)) {
-		if (trigger_run(&stmt->space->on_replace, txn) != 0)
+		int rc = 0;
+		if(!space_is_temporary(stmt->space)) {
+			rc = trigger_run(&stmt->space->on_replace, txn);
+		} else {
+			/*
+			 * There is no row attached to txn_stmt for
+			 * temporary spaces, since DML operations on them
+			 * are not written to WAL. Fake a row to pass operation
+			 * type to lua on_replace triggers.
+			 */
+			assert(stmt->row == NULL);
+			struct xrow_header temp_header;
+			temp_header.type = request->type;
+			stmt->row = &temp_header;
+			rc = trigger_run(&stmt->space->on_replace, txn);
+			stmt->row = NULL;
+		}
+		if (rc != 0)
 			goto fail;
 	}
 	--txn->in_sub_stmt;

test/box/before_replace.result
@@ -833,3 +833,28 @@ save_type
 s:drop()
 ---
 ...
+--
+-- gh-4266 triggers on temporary space fail
+--
+s = box.schema.space.create('test', {temporary = true})
+---
+...
+_ = s:create_index('pk')
+---
+...
+save_type = ''
+---
+...
+_ = s:before_replace(function(old,new, name, type) save_type = type return new end)
+---
+...
+_ = s:insert{1}
+---
+...
+save_type
+---
+- INSERT
+...
+s:drop()
+---
+...

test/box/before_replace.test.lua
@@ -291,3 +291,18 @@ _ = s:upsert({3,4,5}, {{'+', 2, 1}})
 save_type
 
 s:drop()
+
+--
+-- gh-4266 triggers on temporary space fail
+--
+s = box.schema.space.create('test', {temporary = true})
+
+_ = s:create_index('pk')
+
+save_type = ''
+
+_ = s:before_replace(function(old,new, name, type) save_type = type return new end)
+_ = s:insert{1}
+save_type
+
+s:drop()

test/box/on_replace.result
@@ -752,3 +752,28 @@ save_type
 s:drop()
 ---
 ...
+--
+-- gh-4266 triggers on temporary space fail
+--
+s = box.schema.space.create('test', {temporary = true})
+---
+...
+_ = s:create_index('pk')
+---
+...
+save_type = ''
+---
+...
+_ = s:on_replace(function(old,new, name, type) save_type = type end)
+---
+...
+_ = s:insert{1}
+---
+...
+save_type
+---
+- INSERT
+...
+s:drop()
+---
+...

test/box/on_replace.test.lua
@@ -298,3 +298,18 @@ _ = s:upsert({3,4,5}, {{'+', 2, 1}})
 save_type
 
 s:drop()
+
+--
+-- gh-4266 triggers on temporary space fail
+--
+s = box.schema.space.create('test', {temporary = true})
+
+_ = s:create_index('pk')
+
+save_type = ''
+
+_ = s:on_replace(function(old,new, name, type) save_type = type end)
+_ = s:insert{1}
+save_type
+
+s:drop()