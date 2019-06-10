a5b96c03475bc7f9eab002190cf676a3fd2e1884
Alexander V. Tikhonov <avtikhon@tarantool.org>
2019-06-10 10:57:01 +0300

    Enable GitLab CI testing
    Implemented GitLab CI testing process additionally to existing Travis
    CI. The new testing process is added to run tests faster. It requires to
    control a load of machines to avoid flaky fails on timeouts. GitLab CI
    allows us to run testing on our machines.
    
    Created 2 stages for testing and deploying packages.
    
    The testing stage contains the following jobs that are run for all
    branches:
    
    * Debian 9 (Stretch): release/debug gcc.
    * Debian 10 (Buster): release clang8 + lto.
    * OSX 14 (Mojave): release.
    * FreeBSD 12: release gcc.
    
    And the following jobs that are run of long-term branches (release
    branches: for now it is 1.10, 2.1 and master):
    
    * OSX 13 (Sierra): release clang.
    * OSX 14 (Mojave): release clang + lto.
    
    The deployment stage contains the same jobs as we have in Travis CI.
    They however just build tarballs and packages: don't push them to S3 and
    packagecloud.
    
    In order to run full testing on a short-term branch one can name it with
    '-full-ci' suffix.
    
    The additional manual work is needed when dependencies are changed in
    .travis.mk file ('deps_debian' or 'deps_buster_clang_8' goals):
    
     | make GITLAB_USER=foo -f .gitlab.mk docker_bootstrap
    
    This command pushes docker images into GitLab Registry and then they are
    used in testing. Pre-built images speed up testing.
    
    Fixes #4156
    
    (cherry picked from commit ce623a23416eb192ce70116fd14992e84e7ccbbe)

---
+++ .gitlab.mk
@@ -0,0 +1,107 @@
+GITLAB_MAKE:=${MAKE} -f .gitlab.mk
+TRAVIS_MAKE:=${MAKE} -f .travis.mk
+
+# #####
+# Utils
+# #####
+
+# Update submodules.
+#
+# Note: There is no --force option for `git submodule` on git
+# 1.7.1, which is shiped in CentOS 6.
+git_submodule_update:
+	git submodule update --force --recursive --init 2>/dev/null || \
+		git submodule update --recursive --init
+
+# Pass *_no_deps goals to .travis.mk.
+test_%_no_deps: git_submodule_update
+	${TRAVIS_MAKE} $@
+
+# #######################################################
+# Build and push testing docker images to GitLab Registry
+# #######################################################
+
+# These images contains tarantool dependencies and testing
+# dependencies to run tests in them.
+#
+# How to run:
+#
+# make GITLAB_USER=foo -f .gitlab.mk docker_bootstrap
+#
+# The command will prompt for a password. If two-factor
+# authentication is enabled an access token with 'api' scope
+# should be entered here instead of a password.
+#
+# When to run:
+#
+# When some of deps_* goals in .travis.mk are updated.
+#
+# Keep in a mind that the resulting image is used to run tests on
+# all branches, so avoid removing packages: only add them.
+
+GITLAB_REGISTRY?=registry.gitlab.com
+DOCKER_BUILD=docker build --network=host -f - .
+
+define DEBIAN_STRETCH_DOCKERFILE
+FROM packpack/packpack:debian-stretch
+COPY .travis.mk .
+RUN make -f .travis.mk deps_debian
+endef
+export DEBIAN_STRETCH_DOCKERFILE
+
+define DEBIAN_BUSTER_DOCKERFILE
+FROM packpack/packpack:debian-buster
+COPY .travis.mk .
+RUN make -f .travis.mk deps_buster_clang_8
+endef
+export DEBIAN_BUSTER_DOCKERFILE
+
+IMAGE_PREFIX:=${GITLAB_REGISTRY}/tarantool/tarantool/testing
+DEBIAN_STRETCH_IMAGE:=${IMAGE_PREFIX}/debian-stretch
+DEBIAN_BUSTER_IMAGE:=${IMAGE_PREFIX}/debian-buster
+
+TRAVIS_CI_MD5SUM:=$(firstword $(shell md5sum .travis.mk))
+
+docker_bootstrap:
+	# Login.
+	docker login -u ${GITLAB_USER} ${GITLAB_REGISTRY}
+	# Build images.
+	echo "$${DEBIAN_STRETCH_DOCKERFILE}" | ${DOCKER_BUILD} \
+		-t ${DEBIAN_STRETCH_IMAGE}:${TRAVIS_CI_MD5SUM} \
+		-t ${DEBIAN_STRETCH_IMAGE}:latest
+	echo "$${DEBIAN_BUSTER_DOCKERFILE}" | ${DOCKER_BUILD} \
+		-t ${DEBIAN_BUSTER_IMAGE}:${TRAVIS_CI_MD5SUM} \
+		-t ${DEBIAN_BUSTER_IMAGE}:latest
+	# Push images.
+	docker push ${DEBIAN_STRETCH_IMAGE}:${TRAVIS_CI_MD5SUM}
+	docker push ${DEBIAN_BUSTER_IMAGE}:${TRAVIS_CI_MD5SUM}
+	docker push ${DEBIAN_STRETCH_IMAGE}:latest
+	docker push ${DEBIAN_BUSTER_IMAGE}:latest
+
+# #################################
+# Run tests under a virtual machine
+# #################################
+
+vms_start:
+	VBoxManage controlvm ${VMS_NAME} poweroff || true
+	VBoxManage snapshot ${VMS_NAME} restore ${VMS_NAME}
+	VBoxManage startvm ${VMS_NAME} --type headless
+
+vms_test_%:
+	tar czf - ../tarantool | ssh ${VMS_USER}@127.0.0.1 -p ${VMS_PORT} tar xzf -
+	ssh ${VMS_USER}@127.0.0.1 -p ${VMS_PORT} "/bin/bash -c \
+		'${EXTRA_ENV} \
+		cd tarantool && \
+		${GITLAB_MAKE} git_submodule_update && \
+		${TRAVIS_MAKE} $(subst vms_,,$@)'"
+
+vms_shutdown:
+	VBoxManage controlvm ${VMS_NAME} poweroff
+
+# ########################
+# Build RPM / Deb packages
+# ########################
+
+package: git_submodule_update
+	git clone https://github.com/packpack/packpack.git packpack
+	PACKPACK_EXTRA_DOCKER_RUN_PARAMS='--network=host' ./packpack/packpack
--- .travis.mk
+++ .travis.mk
@@ -3,6 +3,7 @@
 #
 
 DOCKER_IMAGE?=packpack/packpack:debian-stretch
+TEST_RUN_EXTRA_PARAMS?=
 
 all: package
 
@@ -13,8 +14,8 @@ package:
 test: test_$(TRAVIS_OS_NAME)
 
 # Redirect some targets via docker
-test_linux: docker_test_ubuntu
-coverage: docker_coverage_ubuntu
+test_linux: docker_test_debian
+coverage: docker_coverage_debian
 
 docker_%:
 	mkdir -p ~/.cache/ccache
@@ -33,8 +34,12 @@ docker_%:
 		${DOCKER_IMAGE} \
 		make -f .travis.mk $(subst docker_,,$@)
 
-deps_ubuntu:
-	sudo apt-get update && apt-get install -y -f \
+#########
+# Linux #
+#########
+
+deps_debian:
+	apt-get update && apt-get install -y -f \
 		build-essential cmake coreutils sed \
 		libreadline-dev libncurses5-dev libyaml-dev libssl-dev \
 		libcurl4-openssl-dev libunwind-dev libicu-dev \
@@ -42,34 +47,29 @@ deps_ubuntu:
 		python-msgpack python-yaml python-argparse python-six python-gevent \
 		lcov ruby clang llvm llvm-dev
 
-test_ubuntu: deps_ubuntu
+deps_buster_clang_8: deps_debian
+	echo "deb http://apt.llvm.org/buster/ llvm-toolchain-buster-8 main" > /etc/apt/sources.list.d/clang_8.list
+	echo "deb-src http://apt.llvm.org/buster/ llvm-toolchain-buster-8 main" >> /etc/apt/sources.list.d/clang_8.list
+	wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
+	apt-get update
+	apt-get install -y clang-8 llvm-8-dev
+
+build_debian:
 	cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo -DENABLE_WERROR=ON ${CMAKE_EXTRA_PARAMS}
-	make -j8
-	cd test && /usr/bin/python test-run.py --force -j 1
+	make -j
 
-deps_osx:
-	brew update
-	brew install openssl readline curl icu4c --force
-	python2 -V || brew install python2 --force
-	curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | python
-	pip install -r test-run/requirements.txt
+test_debian_no_deps: build_debian
+	cd test && /usr/bin/python test-run.py --force $(TEST_RUN_EXTRA_PARAMS)
 
-test_osx: deps_osx
-	cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo -DENABLE_WERROR=ON ${CMAKE_EXTRA_PARAMS}
-	# Increase the maximum number of open file descriptors on macOS
-	sudo sysctl -w kern.maxfiles=20480 || :
-	sudo sysctl -w kern.maxfilesperproc=20480 || :
-	sudo launchctl limit maxfiles 20480 || :
-	ulimit -S -n 20480 || :
-	ulimit -n
-	make -j8
-	cd test && ./test-run.py --force -j 1 unit/ app/ app-tap/ box/ box-tap/
+test_debian: deps_debian test_debian_no_deps
 
-coverage_ubuntu: deps_ubuntu
+build_coverage_debian:
 	cmake . -DCMAKE_BUILD_TYPE=Debug -DENABLE_GCOV=ON
-	make -j8
+	make -j
+
+test_coverage_debian_no_deps: build_coverage_debian
 	# Enable --long tests for coverage
-	cd test && /usr/bin/python test-run.py --force -j 1 --long
+	cd test && /usr/bin/python test-run.py --force $(TEST_RUN_EXTRA_PARAMS) --long
 	lcov --compat-libtool --directory src/ --capture --output-file coverage.info.tmp
 	lcov --compat-libtool --remove coverage.info.tmp 'tests/*' 'third_party/*' '/usr/*' \
 		--output-file coverage.info
@@ -80,6 +80,59 @@ coverage_ubuntu: deps_ubuntu
 		echo coveralls-lcov --service-name travis-ci --service-job-id $(TRAVIS_JOB_ID) --repo-token [FILTERED] coverage.info; \
 		coveralls-lcov --service-name travis-ci --service-job-id $(TRAVIS_JOB_ID) --repo-token $(COVERALLS_TOKEN) coverage.info; \
 	fi;
+
+coverage_debian: deps_debian test_coverage_debian_no_deps
+
+#######
+# OSX #
+#######
+
+deps_osx:
+	brew update
+	brew install openssl readline curl icu4c --force
+	python2 -V || brew install python2 --force
+	curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py >get-pip.py
+	python get-pip.py --user
+	pip install --user --force-reinstall -r test-run/requirements.txt
+
+build_osx:
+	cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo -DENABLE_WERROR=ON ${CMAKE_EXTRA_PARAMS}
+	make -j
+
+test_osx_no_deps: build_osx
+	# Increase the maximum number of open file descriptors on macOS
+	sudo sysctl -w kern.maxfiles=20480 || :
+	sudo sysctl -w kern.maxfilesperproc=20480 || :
+	sudo launchctl limit maxfiles 20480 || :
+	ulimit -S -n 20480 || :
+	ulimit -n
+	cd test && ./test-run.py --force $(TEST_RUN_EXTRA_PARAMS) unit/ app/ app-tap/ box/ box-tap/
+
+test_osx: deps_osx test_osx_no_deps
+
+###########
+# FreeBSD #
+###########
+
+deps_freebsd:
+	sudo pkg install -y git cmake gmake gcc coreutils \
+		readline ncurses libyaml openssl curl libunwind icu \
+		python27 py27-pip py27-setuptools py27-daemon \
+		py27-yaml py27-argparse py27-six py27-gevent \
+		gdb bash
+
+build_freebsd:
+	cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo -DENABLE_WERROR=ON ${CMAKE_EXTRA_PARAMS}
+	gmake -j
+
+test_freebsd_no_deps: build_freebsd
+	cd test && /usr/bin/python test-run.py --force $(TEST_RUN_EXTRA_PARAMS)
+
+test_freebsd: deps_freebsd test_freebsd_no_deps
+
+####################
+# Sources tarballs #
+####################
 
 source:
 	git clone https://github.com/packpack/packpack.git packpack
