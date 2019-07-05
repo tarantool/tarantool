d690002a2558748726afe27d32daa0ad7b690efe
Alexander V. Tikhonov <avtikhon@tarantool.org>
2019-07-05 14:37:00 +0300

    test: net.box: fix case re invalid msgpack warning
    The test case has two problems that appear from time to time and lead to
    flaky fails. Those fails are look as shown below in a test-run output.
    
     | Test failed! Result content mismatch:
     | --- box/net.box.result	Mon Jun 24 17:23:49 2019
     | +++ box/net.box.reject	Mon Jun 24 17:51:52 2019
     | @@ -1404,7 +1404,7 @@
     |  ...
     |  test_run:grep_log('default', 'ER_INVALID_MSGPACK.*')
     | ---
     | -- 'ER_INVALID_MSGPACK: Invalid MsgPack - packet body'
     | +- 'ER_INVALID_MSGPACK: Invalid MsgPack - packet length'
     | ...
     | -- gh-983 selecting a lot of data crashes the server or hangs the
     | -- connection
    
    'ER_INVALID_MSGPACK.*' regexp should match 'ER_INVALID_MSGPACK: Invalid
    MsgPack - packet body' log message, but if it is not in a log file at a
    time of grep_log() call (just don't flushed to the file yet) a message
    produced by another test case can be matched ('ER_INVALID_MSGPACK:
    Invalid MsgPack - packet length'). The fix here is to match the entire
    message and check for the message periodically during several seconds
    (use wait_log() instead of grep_log()).
    
    Another problem is the race between writing a response to an iproto
    socket on a server side and closing the socket on a client end. If
    tarantool is unable to write a response, it does not produce the warning
    re invalid msgpack, but shows 'broken pipe' message instead. We need
    first grep for the message in logs and only then close the socket on a
    client. The similar problem (with another test case) is described in
    [1].
    
    [1]: https://github.com/tarantool/tarantool/issues/4273#issuecomment-508939695
    
    Closes: #4311
    
    (cherry picked from commit 0f9fdd7217445fe3e989452cccdd1e1ec7597247)

--- test/box/net.box.test.lua
+++ test/box/net.box.test.lua
@@ -547,8 +547,8 @@ socket = require("socket")
 sock = socket.tcp_connect(LISTEN.host, LISTEN.service)
 data = string.fromhex("6783000000000000000000000000000000000000000000800000C8000000000000000000000000000000000000000000FFFF210100373208000000FFFF000055AAEB66486472530D02000000000010A0350001008000001000000000000000000000000000D05700")
 sock:write(data)
+test_run:wait_log('default', 'ER_INVALID_MSGPACK: Invalid MsgPack %- packet length', nil, 10)
 sock:close()
-test_run:grep_log('default', 'ER_INVALID_MSGPACK.*')
 
 -- gh-983 selecting a lot of data crashes the server or hangs the
 -- connection
