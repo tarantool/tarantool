74c43a2261abeee34ec3aa1b4e1db2d95157ede6
Alexander V. Tikhonov <avtikhon@tarantool.org>
2019-07-22 09:07:22 +0300

    travis-ci: fix OSX max files limits
    Increased the maximum number of open file descriptors on macOS.
    
    Travis-ci: needed the "ulimit -n <value>" call, because found
    that the tests fail without it even with launchctl limit tool set.
    
    Gitlab-ci: needed the "launchctl limit maxfiles <value>" call,
    because under gitlib-ci it is needed the password to change the
    limits and we call the sudo tool which separates the local set
    of the environment.
    
    Closes #4373
    
    (cherry picked from commit 027fa1a0699dc3b72d2f613bc32ba098c84938cf)

--- .travis.mk
+++ .travis.mk
@@ -115,22 +115,21 @@ build_osx:
 	make -j
 
 test_osx_no_deps: build_osx
-	# Increase the maximum number of open file descriptors on macOS
-	echo tarantool | sudo -S echo "Initialize sudo for the next commands..." :
-	sudo sysctl -w kern.maxfiles=${MAX_FILES} || echo tarantool | sudo -S sysctl -w kern.maxfiles=${MAX_FILES} || :
-	sysctl -w kern.maxfiles || :
-	sudo sysctl -w kern.maxfilesperproc=${MAX_FILES} || echo tarantool | sudo -S sysctl -w kern.maxfilesperproc=${MAX_FILES} || :
-	sysctl -w kern.maxfilesperproc || :
-	sudo launchctl limit maxfiles ${MAX_FILES} || echo tarantool | sudo -S launchctl limit maxfiles ${MAX_FILES} || :
-	launchctl limit maxfiles || :
-	sudo ulimit -S -n ${MAX_FILES} || echo tarantool | sudo -S ulimit -S -n ${MAX_FILES} || :
-	ulimit -S -n || :
-	sudo ulimit -H -n ${MAX_FILES} || echo tarantool | sudo -S ulimit -H -n ${MAX_FILES} || :
-	ulimit -H -n
-	# temporary excluded replication/ suite with some tests from other suites by issues #4357 and #4370
-	cd test && ./test-run.py --force $(TEST_RUN_EXTRA_PARAMS) \
-		app/ app-tap/ box/ box-py/ box-tap/ engine/ engine_long/ long_run-py/ luajit-tap/ \
-		replication-py/ small/ sql/ sql-tap/ swim/ unit/ vinyl/ wal_off/ xlog/ xlog-py/
+	# Limits: Increase the maximum number of open file descriptors on macOS:
+	#   Travis-ci needs the "ulimit -n <value>" call
+	#   Gitlab-ci needs the "launchctl limit maxfiles <value>" call
+	# Also gitlib-ci needs the password to change the limits, while
+	# travis-ci runs under root user. Limit setup must be in the same
+	# call as tests runs call.
+	# Tests: Temporary excluded replication/ suite with some tests
+	#        from other suites by issues #4357 and #4370
+	echo tarantool | sudo -S launchctl limit maxfiles ${MAX_FILES} || : ; \
+		launchctl limit maxfiles || : ; \
+		ulimit -n ${MAX_FILES} || : ; \
+		ulimit -n ; \
+		cd test && ./test-run.py --force $(TEST_RUN_EXTRA_PARAMS) \
+			app/ app-tap/ box/ box-py/ box-tap/ engine/ engine_long/ long_run-py/ luajit-tap/ \
+			replication-py/ small/ sql/ sql-tap/ swim/ unit/ vinyl/ wal_off/ xlog/ xlog-py/
 
 test_osx: deps_osx test_osx_no_deps
 
