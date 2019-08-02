17db271709b5afe27b76a87419f0a5412a45db82
Vladimir Davydov <vdavydov.dev@gmail.com>
2019-08-02 14:59:47 +0300

    relay: stop relay on subscribe error
    In case an error occurs between relay_start() and cord_costart() in
    relay_subscribe(), the relay status won't be reset to STOPPED. As a
    result, any further attempt to re-subscribe will fail with ER_CFG:
    duplicate connection with the same replica UUID. This may happen, for
    example, if the WAL directory happens to be temporarily inaccessible on
    the master.
    
    Closes #4399
    
    (cherry picked from commit 35ef3320089d927db43c5796494fc699a03b5eec)

--- src/box/relay.cc
+++ src/box/relay.cc
@@ -668,6 +668,11 @@ relay_subscribe(struct replica *replica, int fd, uint64_t sync,
 	}
 
 	relay_start(relay, fd, sync, relay_send_row);
+	auto relay_guard = make_scoped_guard([=] {
+		relay_stop(relay);
+		replica_on_relay_stop(replica);
+	});
+
 	vclock_copy(&relay->local_vclock_at_subscribe, &replicaset.vclock);
 	relay->r = recovery_new(cfg_gets("wal_dir"),
 			        cfg_geti("force_recovery"),
@@ -679,10 +684,6 @@ relay_subscribe(struct replica *replica, int fd, uint64_t sync,
 			      relay_subscribe_f, relay);
 	if (rc == 0)
 		rc = cord_cojoin(&relay->cord);
-
-	relay_stop(relay);
-	replica_on_relay_stop(replica);
-
 	if (rc != 0)
 		diag_raise();
 }
