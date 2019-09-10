44597aa1d2e760a2ad56e736621e4cc3894dc8ce
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2019-09-10 23:35:50 +0200

    app: exit gracefully when a main script throws an error
    Code to run main script (passed via command line args, or
    interactive console) has a footer where it notifies systemd,
    logs a happened error, and panics.
    
    Before the patch that code was unreachable in case of any
    exception in a main script, because panic happened earlier. Now a
    happened exception is correctly carried to the footer with proper
    error processing.
    
    A first and obvious solution was replace all panics with diag_set
    and use fiber_join on the script runner fiber. But appeared, that
    the fiber running a main script can't be joined. This is because
    normally it exits via os.exit() which never returns and therefore
    its caller never dies = can't be joined.
    
    The patch solves this problem by passing main fiber diag to the
    script runner by pointer, eliminating fiber_join necessity.
    
    Closes #4382
    
    (cherry picked from commit 157a2d88a39f180f0581f3caf643e1b63b276b0b)

--- src/main.cc
+++ src/main.cc
@@ -845,8 +845,9 @@ main(int argc, char **argv)
 		 * is why script must run only after the server was fully
 		 * initialized.
 		 */
-		tarantool_lua_run_script(script, interactive, optc, optv,
-					 main_argc, main_argv);
+		if (tarantool_lua_run_script(script, interactive, optc, optv,
+					     main_argc, main_argv) != 0)
+			diag_raise();
 		/*
 		 * Start event loop after executing Lua script if signal_cb()
 		 * wasn't triggered and there is some new events. Initial value
---
+++ test/app-tap/fail_main.test.lua
@@ -0,0 +1,39 @@
+#!/usr/bin/env tarantool
+
+local tap = require('tap')
+local test = tap.test("fail_main")
+local fio = require('fio')
+local tarantool_bin = arg[-1]
+
+test:plan(1)
+
+function run_script(code)
+    local dir = fio.tempdir()
+    local script_path = fio.pathjoin(dir, 'script.lua')
+    local script = fio.open(script_path, {'O_CREAT', 'O_WRONLY', 'O_APPEND'},
+        tonumber('0777', 8))
+    script:write(code)
+    script:close()
+    local output_file = fio.pathjoin(fio.cwd(), 'out.txt')
+    local cmd = [[/bin/sh -c 'cd "%s" && "%s" ./script.lua 0> %s 2> %s']]
+    local code = os.execute(
+        string.format(cmd, dir, tarantool_bin, output_file, output_file)
+    )
+    fio.rmtree(dir)
+    local out_fd = fio.open(output_file, {'O_RDONLY'})
+    local output = out_fd:read(100000)
+    out_fd:close()
+    return code, output
+end
+
+--
+-- gh-4382: an error in main script should be handled gracefully,
+-- with proper logging.
+--
+local code, output = run_script("error('Error in the main script')")
+
+test:ok(output:match("fatal error, exiting the event loop"),
+        "main script error is handled gracefully")
+
+test:check()
+os.exit(0)
