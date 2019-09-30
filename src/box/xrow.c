1795832283eacc993c2c28d124dac4335a7957ad
Serge Petrenko <sergepetrenko@tarantool.org>
2019-09-30 16:43:37 +0300

    replication: add is_orphan field to ballot
    A successfully fetched remote instance ballot isn't updated during
    bootstrap procedure. This leads to a case when different instances
    choose different masters as their bootstrap leaders.
    
    Imagine such a situation.
    You start instance A without replication set up. Instance A successfully
    bootstraps.
    You also have instances B and C both with replication set up to {A, B,
    C} and replication_connect_quorum set to 3
    You first start instance B. It doesn't proceed to choosing a leader
    until one of the events happens: either the replication_connect_timeout
    runs out, or instance C is up and starts listening on its port.
    B has established connection to A and fetched its ballot, with some
    vclock, say, {1: 1}.
    B retries connection to C every replication_timeout seconds.
    Then you start instance C. Instance C succeeds in connecting to A and B
    right away and bootstraps from instance A. Instance A registers C in its
    _cluster table. This registration is replicated to instance C.
    Meanwhile, instance C is trying to sync with quorum instances (which is
    3), and stays in orphan mode.
    Now replication_timeout on instance B finally runs out. It retries a
    previously unsuccessful connection to C and succeeds. C sends its ballot
    to B with vclock = {1: 2, 2:0} (in our example), since it has already
    incremented it after _cluster registration.
    B sees that C has a greater vclock than A, and chooses to bootstrap from
    C instead of A. C is orphan and rejects B's attempt to join. B dies.
    
    To fix such ungentlemanlike behaviour of C, we should at least include
    loading status in ballot and prefer fully bootstrapped instances to the
    ones still syncing with other replicas.
    We also need to use a separate flag instead of ballot's already existent
    is_ro, since we still want to prefer loading instances over the ones
    explicitly configured to be read-only.
    
    Closes #4527
    
    (cherry picked from commit dc1e4009465174dc09d6026d89f3eb3bcbc9ed49)

--- src/box/xrow.c
+++ src/box/xrow.c
@@ -441,8 +441,9 @@ iproto_reply_vote(struct obuf *out, const struct ballot *ballot,
 		  uint64_t sync, uint32_t schema_version)
 {
 	size_t max_size = IPROTO_HEADER_LEN + mp_sizeof_map(1) +
-		mp_sizeof_uint(UINT32_MAX) + mp_sizeof_map(3) +
+		mp_sizeof_uint(UINT32_MAX) + mp_sizeof_map(4) +
 		mp_sizeof_uint(UINT32_MAX) + mp_sizeof_bool(ballot->is_ro) +
+		mp_sizeof_uint(UINT32_MAX) + mp_sizeof_bool(ballot->is_loading) +
 		mp_sizeof_uint(UINT32_MAX) + mp_sizeof_vclock(&ballot->vclock) +
 		mp_sizeof_uint(UINT32_MAX) + mp_sizeof_vclock(&ballot->gc_vclock);
 
@@ -456,9 +457,11 @@ iproto_reply_vote(struct obuf *out, const struct ballot *ballot,
 	char *data = buf + IPROTO_HEADER_LEN;
 	data = mp_encode_map(data, 1);
 	data = mp_encode_uint(data, IPROTO_BALLOT);
-	data = mp_encode_map(data, 3);
+	data = mp_encode_map(data, 4);
 	data = mp_encode_uint(data, IPROTO_BALLOT_IS_RO);
 	data = mp_encode_bool(data, ballot->is_ro);
+	data = mp_encode_uint(data, IPROTO_BALLOT_IS_LOADING);
+	data = mp_encode_bool(data, ballot->is_loading);
 	data = mp_encode_uint(data, IPROTO_BALLOT_VCLOCK);
 	data = mp_encode_vclock(data, &ballot->vclock);
 	data = mp_encode_uint(data, IPROTO_BALLOT_GC_VCLOCK);
@@ -1077,6 +1080,7 @@ int
 xrow_decode_ballot(struct xrow_header *row, struct ballot *ballot)
 {
 	ballot->is_ro = false;
+	ballot->is_loading = false;
 	vclock_create(&ballot->vclock);
 
 	const char *start = NULL;
@@ -1120,6 +1124,11 @@ xrow_decode_ballot(struct xrow_header *row, struct ballot *ballot)
 			if (mp_typeof(*data) != MP_BOOL)
 				goto err;
 			ballot->is_ro = mp_decode_bool(&data);
+			break;
+		case IPROTO_BALLOT_IS_LOADING:
+			if (mp_typeof(*data) != MP_BOOL)
+				goto err;
+			ballot->is_loading = mp_decode_bool(&data);
 			break;
 		case IPROTO_BALLOT_VCLOCK:
 			if (mp_decode_vclock(&data, &ballot->vclock) != 0)
