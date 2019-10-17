86e5514d743dbbd6485c52fc0d0d8f47e5b3bc96
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2019-10-17 22:59:36 +0200

    replication: auto reconnect if password is invalid
    Before the patch there was a race in replication
    password configuration. It was possible that a replica
    connects to a master with a custom password before
    that password is actually set. The replica treated the
    error as critical and exited.
    
    But in fact it is not critical. Replica even can
    withstand absence of a user and keeps reconnecting.
    Wrong password situation arises from the same problem
    of non atomic configuration and is fixed the same -
    keep reconnect attempts if the password was wrong.
    
    Closes #4550
    
    (cherry picked from commit aa2e2c56cdf863e2992356b28ee237639e05da07)

--- test/replication/misc.test.lua
+++ test/replication/misc.test.lua
@@ -153,15 +153,21 @@ test_run:cmd('delete server er_load2')
 test_run:cleanup_cluster()
 
 --
--- Test case for gh-3637. Before the fix replica would exit with
--- an error. Now check that we don't hang and successfully connect.
+-- Test case for gh-3637, gh-4550. Before the fix replica would
+-- exit with an error if a user does not exist or a password is
+-- incorrect. Now check that we don't hang/panic and successfully
+-- connect.
 --
 fiber = require('fiber')
 test_run:cmd("create server replica_auth with rpl_master=default, script='replication/replica_auth.lua'")
 test_run:cmd("start server replica_auth with wait=False, wait_load=False, args='cluster:pass 0.05'")
 -- Wait a bit to make sure replica waits till user is created.
 fiber.sleep(0.1)
-box.schema.user.create('cluster', {password='pass'})
+box.schema.user.create('cluster')
+-- The user is created. Let the replica fail auth request due to
+-- a wrong password.
+fiber.sleep(0.1)
+box.schema.user.passwd('cluster', 'pass')
 box.schema.user.grant('cluster', 'replication')
 
 while box.info.replication[2] == nil do fiber.sleep(0.01) end
