95237ac8cbc2412400c14b5af847df725b9ea92c
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2019-11-01 22:34:01 +0100

    replication: don't drop admin super privileges
    The admin user has universal privileges before bootstrap or
    recovery are done. That allows to, for example, bootstrap from a
    remote master, because to do that the admin should be able to
    insert into system spaces, such as _priv.
    
    But after the patch on online credentials update was implemented
    (#2763, 48d00b0e8b043b4697a474e6593a7608f2d9b0f1) the admin could
    loose its universal access if, for example, a role was granted to
    him before universal access was recovered.
    
    That happened by two reasons:
    
        - Any change in access rights, even in granted roles, led to
          rebuild of universal access;
    
        - Any change in access rights updated the universal access in
          all existing sessions, thanks to #2763.
    
    What happened: two tarantools were started. One of them master,
    granted 'replication' role to admin. Second node, slave, tried to
    bootstrap from the master. The slave created an admin session and
    started loading data. After it loaded 'grant replication role to
    admin' command, this nullified admin universal access everywhere,
    including this session. Next rows could not be applied.
    
    Closes #4606

---
+++ test/replication/gh-4606-admin-creds.test.lua
@@ -0,0 +1,26 @@
+test_run = require('test_run').new()
+--
+-- gh-4606: the admin user has universal privileges before
+-- bootstrap or recovery are done. That allows to, for example,
+-- bootstrap from a remote master, because to do that the admin
+-- should be able to insert into system spaces, such as _priv.
+--
+-- But the admin could lost its universal access if, for
+-- example, a role was granted to him before universal access was
+-- recovered. Because any change in access rights, even in granted
+-- roles, led to rebuild of universal access.
+--
+box.schema.user.passwd('admin', '111')
+box.schema.user.grant('admin', 'replication')
+test_run:cmd("create server replica_auth with rpl_master=default, script='replication/replica_auth.lua'")
+test_run:cmd("start server replica_auth with args='admin:111 0.1'")
+test_run:switch('replica_auth')
+i = box.info
+i.replication[i.id % 2 + 1].upstream.status == 'follow' or i
+test_run:switch('default')
+test_run:cmd("stop server replica_auth")
+test_run:cmd("cleanup server replica_auth")
+test_run:cmd("delete server replica_auth")
+
+box.schema.user.passwd('admin', '')
+box.schema.user.revoke('admin', 'replication')
