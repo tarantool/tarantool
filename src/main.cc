bac732eefb22bd117be2f5aadb9fae752109ed50
Serge Petrenko <sergepetrenko@tarantool.org>
2019-11-26 16:59:33 +0300

    clear terminal state on panic
    The tarantool_free() call in the end of main() works all the time except
    when we exit due to a panic. We need to clear terminal state in this
    case also, so return to using atexit() to clear readline state.
    
    Closes #4466
    
    (cherry picked from commit c6d4f010633a9961233178666d5a3c056018a758)

--- src/main.cc
+++ src/main.cc
@@ -525,6 +525,34 @@ load_cfg()
 }
 
 void
+free_rl_state(void)
+{
+	/* tarantool_lua_free() was formerly reponsible for terminal reset,
+	 * but it is no longer called
+	 */
+	if (isatty(STDIN_FILENO)) {
+		/*
+		 * Restore terminal state. Doesn't hurt if exiting not
+		 * due to a signal.
+		 */
+		rl_cleanup_after_signal();
+	}
+}
+
+void
+tarantool_atexit(void)
+{
+	/* Same checks as in tarantool_free() */
+	if (getpid() != master_pid)
+		return;
+
+	if (!cord_is_main())
+		return;
+
+	free_rl_state();
+}
+
+void
 tarantool_free(void)
 {
 	/*
@@ -558,16 +586,6 @@ tarantool_free(void)
 #ifdef ENABLE_GCOV
 	__gcov_flush();
 #endif
-	/* tarantool_lua_free() was formerly reponsible for terminal reset,
-	 * but it is no longer called
-	 */
-	if (isatty(STDIN_FILENO)) {
-		/*
-		 * Restore terminal state. Doesn't hurt if exiting not
-		 * due to a signal.
-		 */
-		rl_cleanup_after_signal();
-	}
 	cbus_free();
 #if 0
 	/*
@@ -744,6 +762,15 @@ main(int argc, char **argv)
 
 		/* main core cleanup routine */
 		atexit(tarantool_free);
+
+		/*
+		 * The call to tarantool_free() below, thanks to
+		 * on_shutdown triggers, works all the time
+		 * except when we panic. So leave the ever-
+		 * necessary cleanups in atexit handler, which
+		 * is executed always.
+		 */
+		atexit(tarantool_atexit);
 
 		if (!loop())
 			panic("%s", "can't init event loop");
