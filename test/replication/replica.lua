07ee8a2766ca736c35add5bb41231ed978e1443c
Serge Petrenko <sergepetrenko@tarantool.org>
2020-02-28 14:43:17 +0300

    replication: fix rebootstrap in case the instance is listed in box.cfg.replication
    When checking wheter rejoin is needed, replica loops through all the
    instances in box.cfg.replication, which makes it believe that there is a
    master holding files, needed by it, since it accounts itself just like
    all other instances.
    So make replica skip itself when finding an instance which holds files
    needed by it, and determining whether rebootstrap is needed.
    
    We already have a working test for the issue, it missed the issue due to
    replica.lua replication settings. Fix replica.lua to optionally include
    itself in box.cfg.replication, so that the corresponding test works
    correctly.
    
    Closes #4759
    
    (cherry picked from commit dbcfaf70d9312bbcb2189009cd63dbe75f955733)

--- test/replication/replica.lua
+++ test/replication/replica.lua
@@ -1,8 +1,17 @@
 #!/usr/bin/env tarantool
 
+repl_include_self = arg[1] and arg[1] == 'true' or false
+repl_list = nil
+
+if repl_include_self then
+    repl_list = {os.getenv("MASTER"), os.getenv("LISTEN")}
+else
+    repl_list = os.getenv("MASTER")
+end
+
 box.cfg({
     listen              = os.getenv("LISTEN"),
-    replication         = os.getenv("MASTER"),
+    replication         = repl_list,
     memtx_memory        = 107374182,
     replication_timeout = 0.1,
     replication_connect_timeout = 0.5,
