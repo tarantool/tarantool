50163b1163e68a7e49a338c098d1bbb145991974
Cyrill Gorcunov <gorcunov@gmail.com>
2020-03-05 15:29:34 +0300

    box: recovery_journal_create -- set journal here
    Allows to eliminate code duplication.
    
    Acked-by: Konstantin Osipov <kostja.osipov@gmail.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- src/box/box.cc
+++ src/box/box.cc
@@ -331,6 +331,7 @@ recovery_journal_create(struct recovery_journal *journal, struct vclock *v)
 {
 	journal_create(&journal->base, recovery_journal_write, NULL);
 	journal->vclock = v;
+	journal_set(&journal->base);
 }
 
 static void
@@ -2068,7 +2069,6 @@ bootstrap_from_master(struct replica *master)
 	engine_begin_final_recovery_xc();
 	struct recovery_journal journal;
 	recovery_journal_create(&journal, &replicaset.vclock);
-	journal_set(&journal.base);
 
 	if (!replication_anon) {
 		applier_resume_to_state(applier, APPLIER_JOINED,
@@ -2234,7 +2234,6 @@ local_recovery(const struct tt_uuid *instance_uuid,
 
 	struct recovery_journal journal;
 	recovery_journal_create(&journal, &recovery->vclock);
-	journal_set(&journal.base);
 
 	/*
 	 * We explicitly request memtx to recover its
