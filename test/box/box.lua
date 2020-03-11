6e5cfa3ee6dd9ef17a38e9f2d239c8b9fa2f3f69
Chris Sosnin <k.sosnin@tarantool.org>
2020-03-11 11:30:46 +0300

    test: introduce iproto_request helper function
    It is needed for performing iproto tests in Lua.
    
    Needed for #4769
    
    Reviewed-by: Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
    Reviewed-by: Nikita Pettik <korablev@tarantool.org>
    (cherry picked from commit 05cdeb59910bb8e6db0b8f4e4bc5e99dc8a0d959)

--- test/box/box.lua
+++ test/box/box.lua
@@ -1,6 +1,8 @@
 #!/usr/bin/env tarantool
 os = require('os')
 
+local msgpack = require('msgpack')
+
 box.cfg{
     listen              = os.getenv("LISTEN"),
     memtx_memory        = 107374182,
@@ -38,4 +40,22 @@ function sorted(data)
     return data
 end
 
-_G.protected_globals = {'cfg_filter', 'sorted'}
+function iproto_request(socket, query_header, query_body)
+    local header = msgpack.encode(query_header)
+    local body = msgpack.encode(query_body)
+    local size = msgpack.encode(header:len() + body:len())
+    assert(socket:write(size .. header .. body) ~= nil,
+           'Failed to send request')
+    size = socket:read(5)
+    assert(size ~= nil, 'Failed to read response')
+    size = msgpack.decode(size)
+    local response = socket:read(size)
+    local header, header_len = msgpack.decode(response)
+    body = msgpack.decode(response:sub(header_len))
+    return {
+        ['header'] = header,
+        ['body'] = body,
+    }
+end
+
+_G.protected_globals = {'cfg_filter', 'sorted', 'iproto_request'}
