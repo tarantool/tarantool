649068db145bf4e203dc114d4348d09898535587
Cyrill Gorcunov <gorcunov@gmail.com>
2020-03-18 15:18:18 +0300

    box: recovery_journal -- declare it as static
    There is no need for several instances of
    recovery journal variable. Lets make it
    statically allocated inside recovery_journal_create
    routine.
    
    And drop the inline annotation because there is
    absolutely no need for this routine being inline.
    
    Suggested-by: Konstantin Osipov <kostja.osipov@gmail.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- src/box/box.cc
+++ src/box/box.cc
@@ -320,12 +320,14 @@ recovery_journal_write(struct journal *base,
 	return 0;
 }
 
-static inline void
-recovery_journal_create(struct recovery_journal *journal, struct vclock *v)
+static void
+recovery_journal_create(struct vclock *v)
 {
-	journal_create(&journal->base, recovery_journal_write, NULL);
-	journal->vclock = v;
-	journal_set(&journal->base);
+	static struct recovery_journal journal;
+
+	journal_create(&journal.base, recovery_journal_write, NULL);
+	journal.vclock = v;
+	journal_set(&journal.base);
 }
 
 static void
@@ -1814,8 +1816,7 @@ bootstrap_from_master(struct replica *master)
 	 * Process final data (WALs).
 	 */
 	engine_begin_final_recovery_xc();
-	struct recovery_journal journal;
-	recovery_journal_create(&journal, &replicaset.vclock);
+	recovery_journal_create(&replicaset.vclock);
 
 	applier_resume_to_state(applier, APPLIER_JOINED, TIMEOUT_INFINITY);
 
@@ -1977,8 +1978,7 @@ local_recovery(const struct tt_uuid *instance_uuid,
 	memtx = (struct memtx_engine *)engine_by_name("memtx");
 	assert(memtx != NULL);
 
-	struct recovery_journal journal;
-	recovery_journal_create(&journal, &recovery->vclock);
+	recovery_journal_create(&recovery->vclock);
 
 	/*
 	 * We explicitly request memtx to recover its
