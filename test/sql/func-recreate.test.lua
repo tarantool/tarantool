b9c4c7c04ae99c5d00124f6c578b7f19a0b98174
Mergen Imeev <imeevma@gmail.com>
2020-04-16 21:35:02 +0300

    lua: fix test sql/func-recreate.test.lua
    Closes #4384

--- test/sql/func-recreate.test.lua
+++ test/sql/func-recreate.test.lua
@@ -4,14 +4,15 @@ box.execute('pragma sql_default_engine=\''..engine..'\'')
 
 -- Check errors during function create process
 fiber = require('fiber')
-box.internal.sql_create_function('WAITFOR', 'INT', function (n) fiber.sleep(n) return n end)
+flag = true
+box.internal.sql_create_function('WAITFOR', 'INT', function () while flag do fiber.sleep(0.01) end return 0 end)
 
 ch = fiber.channel(1)
 
-_ = fiber.create(function () ch:put(box.execute('select WAITFOR(0.2)')) end)
-fiber.sleep(0.1)
+_ = fiber.create(function () ch:put(box.execute('select WAITFOR()')) end)
 
-box.internal.sql_create_function('WAITFOR', 'INT', function (n) require('fiber').sleep(n) return n end)
+box.internal.sql_create_function('WAITFOR', 'INT', function () while flag do fiber.sleep(0.01) end return 0 end)
+flag = false
 ch:get()
-box.internal.sql_create_function('WAITFOR', 'INT', function (n) require('fiber').sleep(n) return n end)
+box.internal.sql_create_function('WAITFOR', 'INT', function () while flag do fiber.sleep(0.01) end return 0 end)
 
