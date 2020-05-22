d0ac80c1afc3c4c438026dc7879f76a043d7b905
Nikita Pettik <korablev@tarantool.org>
2020-05-22 17:26:56 +0300

    vinyl: add NULL check of xrow_upsert_execute() retval
    xrow_upsert_execute() can fail and return NULL for various reasons.
    However, in vy_apply_upsert() the result of xrow_upsert_execute() is
    used unconditionally which may lead to crash. Let's fix it and in case
    xrow_upser_execute() fails return from vy_apply_upsert() NULL value.
    
    Part of #4957
    
    N.B. Test becomes release-disabled in 2.4, 2.3 and 1.10 since we have to
    unthrottle scheduler manually to force checkpoint process. To achieve
    this, we use error injection ERRINJ_VY_SCHED_TIMEOUT which is available
    only in debug mode.
    
    (cherry picked from commit 35162fa4717054282c3edc4c575b94846921cc42)

---
+++ test/vinyl/gh-4957-too-many-upserts.test.lua
@@ -0,0 +1,40 @@
+s = box.schema.create_space('test', {engine = 'vinyl'})
+pk = s:create_index('pk')
+s:insert{1, 1}
+box.snapshot()
+
+-- Let's test number of upserts in one transaction that exceeds
+-- the limit of operations allowed in one update.
+--
+ups_cnt = 5000
+box.begin()
+for i = 1, ups_cnt do s:upsert({1}, {{'&', 2, 1}}) end
+box.commit()
+-- Upserts are not able to squash, so scheduler will get stuck.
+-- So let's not waste much time here, just check that no crash
+-- takes place.
+--
+box.error.injection.set('ERRINJ_VY_SCHED_TIMEOUT', 0.001)
+box.snapshot()
+
+fiber = require('fiber')
+fiber.sleep(0.01)
+
+s:drop()
+
+s = box.schema.create_space('test', {engine = 'vinyl'})
+pk = s:create_index('pk')
+
+tuple = {}
+for i = 1, ups_cnt do tuple[i] = i end
+_ = s:insert(tuple)
+box.snapshot()
+
+box.begin()
+for k = 1, ups_cnt do s:upsert({1}, {{'+', k, 1}}) end
+box.commit()
+box.snapshot()
+fiber.sleep(0.01)
+
+box.error.injection.set('ERRINJ_VY_SCHED_TIMEOUT', 0)
+s:drop()
