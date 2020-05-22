3e58445e0d9d93f00e3a7c2ee80d46581b97d778
Nikita Pettik <korablev@tarantool.org>
2020-05-22 17:26:56 +0300

    vinyl: add NULL check of xrow_upsert_execute() retval
    xrow_upsert_execute() can fail and return NULL for various reasons.
    However, in vy_apply_upsert() the result of xrow_upsert_execute() is
    used unconditionally which may lead to crash. Let's fix it and in case
    xrow_upser_execute() fails return from vy_apply_upsert() NULL value.
    
    Part of #4957
    
    N.B. Test becomes release-disabled in 2.4, 2.3 and 1.10 since we have to
    unthrottle scheduler manually to force checkpoint process. To achieve
    this, we use error injection ERRINJ_VY_SCHED_TIMEOUT which is available
    only in debug mode.
    
    (cherry picked from commit 35162fa4717054282c3edc4c575b94846921cc42)

--- src/box/vy_upsert.c
+++ src/box/vy_upsert.c
@@ -133,6 +133,10 @@ vy_apply_upsert(const struct tuple *new_stmt, const struct tuple *old_stmt,
 					 new_ops_end, result_mp, result_mp_end,
 					 &mp_size, 0, suppress_error,
 					 &column_mask);
+	if (result_mp == NULL) {
+		region_truncate(region, region_svp);
+		return NULL;
+	}
 	result_mp_end = result_mp + mp_size;
 	if (old_type != IPROTO_UPSERT) {
 		assert(old_type == IPROTO_INSERT ||
