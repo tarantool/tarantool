ed04a0d7988244cc3b6478c1d99d04aa60f8cfe9
Sergey Bronnikov <sergeyb@tarantool.org>
2020-05-29 19:31:27 +0000

    test: fix luacheck warnings in test/xlog
    Closes #5468
    
    Reviewed-by: Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
    Reviewed-by: Igor Munkin <imun@tarantool.org>
    
    Co-authored-by: Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
    Co-authored-by: Igor Munkin <imun@tarantool.org>
    (cherry picked from commit 6e55c30cf5055ef5855d5de0e6a4c8f94c3fd4dd)

--- test/xlog/upgrade/2.1.3/gh-4771-upgrade-sequence/fill.lua
+++ test/xlog/upgrade/2.1.3/gh-4771-upgrade-sequence/fill.lua
@@ -1,14 +1,14 @@
 box.cfg{}
-s1 = box.schema.create_space('test1')
-pk = s1:create_index('pk', {sequence = true})
+local s1 = box.schema.create_space('test1')
+s1:create_index('pk', {sequence = true})
 s1:replace{box.NULL}
 
-seq2 = box.schema.sequence.create('seq2')
-s2 = box.schema.create_space('test2')
-pk = s2:create_index('pk', {sequence = 'seq2'})
+box.schema.sequence.create('seq2')
+local s2 = box.schema.create_space('test2')
+s2:create_index('pk', {sequence = 'seq2'})
 s2:replace{box.NULL}
 
-seq3 = box.schema.sequence.create('seq3')
+local seq3 = box.schema.sequence.create('seq3')
 seq3:next()
 
 box.snapshot()
