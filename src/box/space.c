8e40047064a231d6e7065efdb4be179ef5b774e4
Ilya Kosarev <i.kosarev@tarantool.org>
2020-07-12 22:52:41 +0300

    engine: validate key in space_before_replace
    Since 9fba29abb4e05babb9b23b4413bd8083f0fba933 (memtx: introduce tuple
    compare hint) index_get uses key_hint to get a comparison hint for the
    key. It requires key type to be validated. However in
    space_before_replace index_get was used without key validation on some
    execution paths. Now it is fixed and validation is being performed if
    needed. Corresponding test case is introduced. Also
    space_before_replace() is minor refactored to reduce conditions amount
    on a hot path.
    
    Closes #5093
    
    (cherry picked from commit d82804ae947d582a977ccd8c55463ec65092bd99)

--- src/box/space.c
+++ src/box/space.c
@@ -276,6 +276,7 @@ space_before_replace(struct space *space, struct txn *txn,
 	const char *key = NULL;
 	uint32_t part_count = 0;
 	struct index *index = NULL;
+	struct tuple *old_tuple = NULL;
 
 	/*
 	 * Lookup the old tuple.
@@ -288,15 +289,12 @@ space_before_replace(struct space *space, struct txn *txn,
 			return -1;
 		key = request->key;
 		part_count = mp_decode_array(&key);
-		if (exact_key_validate(index->def->key_def,
-				       key, part_count) != 0)
-			return -1;
 		break;
 	case IPROTO_INSERT:
 	case IPROTO_REPLACE:
 	case IPROTO_UPSERT:
 		if (pk == NULL)
-			break;
+			goto after_old_tuple_lookup;
 		index = pk;
 		key = tuple_extract_key_raw(request->tuple, request->tuple_end,
 					    index->def->key_def, NULL);
@@ -309,10 +307,13 @@ space_before_replace(struct space *space, struct txn *txn,
 		return 0;
 	}
 
-	struct tuple *old_tuple = NULL;
-	if (index != NULL &&
-	    index_get(index, key, part_count, &old_tuple) != 0)
+	if (exact_key_validate(index->def->key_def, key, part_count) != 0)
 		return -1;
+
+	if (index_get(index, key, part_count, &old_tuple) != 0)
+		return -1;
+
+after_old_tuple_lookup:;
 
 	/*
 	 * Create the new tuple.
---
+++ test/engine/gh-5093-before_replace-key-type.test.lua
@@ -0,0 +1,8 @@
+test_run = require('test_run').new()
+trigger = function() end
+
+s = box.schema.space.create('gh-5093', {engine=test_run:get_cfg('engine')})
+_ = s:create_index('value', {parts={{1, type='decimal'}}})
+_ = s:before_replace(trigger)
+s:replace{'1111.1111'}
+s:drop()
