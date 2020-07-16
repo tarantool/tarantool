680e1b845be34a559e960948261ad17b7e471717
Alexander V. Tikhonov <avtikhon@tarantool.org>
2020-07-16 14:42:40 +0300

    Divide replication/misc.test.lua
    To fix flaky issues of replication/misc.test.lua the test had to be
    divided into smaller tests to be able to localize the flaky results:
    
      gh-2991-misc-asserts-on-update.test.lua
      gh-3111-misc-rebootstrap-from-ro-master.test.lua
      gh-3160-misc-heartbeats-on-master-changes.test.lua
      gh-3247-misc-iproto-sequence-value-not-replicated.test.lua
      gh-3510-misc-assert-replica-on-applier-disconnect.test.lua
      gh-3606-misc-crash-on-box-concurrent-update.test.lua
      gh-3610-misc-assert-connecting-master-twice.test.lua
      gh-3637-misc-error-on-replica-auth-fail.test.lua
      gh-3642-misc-no-socket-leak-on-replica-disconnect.test.lua
      gh-3704-misc-replica-checks-cluster-id.test.lua
      gh-3711-misc-no-restart-on-same-configuration.test.lua
      gh-3760-misc-return-on-quorum-0.test.lua
      gh-4399-misc-no-failure-on-error-reading-wal.test.lua
      gh-4424-misc-orphan-on-reconfiguration-error.test.lua
    
    Needed for #4940
    
    (cherry picked from commit 867e6b3da9f404defa49f9cf868b414e8e523ed0)

---
+++ test/replication/gh-3642-misc-no-socket-leak-on-replica-disconnect.test.lua
@@ -0,0 +1,43 @@
+test_run = require('test_run').new()
+test_run:cmd("restart server default")
+
+box.schema.user.grant('guest', 'replication')
+
+-- gh-3642 - Check that socket file descriptor doesn't leak
+-- when a replica is disconnected.
+rlimit = require('rlimit')
+lim = rlimit.limit()
+rlimit.getrlimit(rlimit.RLIMIT_NOFILE, lim)
+old_fno = lim.rlim_cur
+lim.rlim_cur = 64
+rlimit.setrlimit(rlimit.RLIMIT_NOFILE, lim)
+
+test_run:cmd('create server sock with rpl_master=default, script="replication/replica.lua"')
+test_run:cmd('start server sock')
+test_run:cmd('switch sock')
+test_run = require('test_run').new()
+fiber = require('fiber')
+test_run:cmd("setopt delimiter ';'")
+for i = 1, 64 do
+    local replication = box.cfg.replication
+    box.cfg{replication = {}}
+    box.cfg{replication = replication}
+    while box.info.replication[1].upstream.status ~= 'follow' do
+        fiber.sleep(0.001)
+    end
+end;
+test_run:cmd("setopt delimiter ''");
+
+box.info.replication[1].upstream.status
+
+test_run:cmd('switch default')
+
+lim.rlim_cur = old_fno
+rlimit.setrlimit(rlimit.RLIMIT_NOFILE, lim)
+
+test_run:cmd("stop server sock")
+test_run:cmd("cleanup server sock")
+test_run:cmd("delete server sock")
+test_run:cleanup_cluster()
+
+box.schema.user.revoke('guest', 'replication')
---
+++ test/replication/gh-4424-misc-orphan-on-reconfiguration-error.test.lua
@@ -0,0 +1,35 @@
+test_run = require('test_run').new()
+
+--
+-- gh-4424 Always enter orphan mode on error in replication
+-- configuration change.
+--
+replication_connect_timeout = box.cfg.replication_connect_timeout
+replication_connect_quorum = box.cfg.replication_connect_quorum
+box.cfg{replication="12345", replication_connect_timeout=0.1, replication_connect_quorum=1}
+box.info.status
+box.info.ro
+-- reset replication => leave orphan mode
+box.cfg{replication=""}
+box.info.status
+box.info.ro
+-- no switch to orphan when quorum == 0
+box.cfg{replication="12345", replication_connect_quorum=0}
+box.info.status
+box.info.ro
+
+-- we could connect to one out of two replicas. Set orphan.
+box.cfg{replication_connect_quorum=2}
+box.cfg{replication={box.cfg.listen, "12345"}}
+box.info.status
+box.info.ro
+-- lower quorum => leave orphan mode
+box.cfg{replication_connect_quorum=1}
+box.info.status
+box.info.ro
+
+box.cfg{                                                        \
+    replication = {},                                           \
+    replication_connect_quorum = replication_connect_quorum,    \
+    replication_connect_timeout = replication_connect_timeout   \
+}
