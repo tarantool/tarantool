447cf459762fefa068063d67449eeeeadb368ce6
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2020-08-05 00:29:10 +0200

    tuple: fix multikey field JSON access crash
    When a tuple had format with multikey indexes in it, any attempt
    to get a multikey indexed field by a JSON path from Lua led to a
    crash.
    
    That was because of incorrect interpretation of offset slot value
    in tuple's field map.
    
    Tuple field map is an array stored before the tuple's MessagePack
    data. Each element is a 4 byte offset to an indexed value to be
    able to get it for O(1) time without MessagePack decoding of all
    the previous fields.
    
    At least it was so before multikeys. Now tuple field map is not
    just an array. It is rather a 2-level array, somehow similar to
    ext4 FS. Some elements of the root array are positive numbers
    pointing at data. Some elements point at a second 'indirect'
    array, so called 'extra', size of which is individual for each
    tuple. These second arrays are used by multikey indexes to store
    offsets to each multikey indexed value in a tuple.
    
    It means, that if there is an offset slot, it can't be just used
    as is. It is allowed only if the field is not multikey. Otherwise
    it is neccessary to somehow get an index in the second 'indirect'
    array.
    
    This is what was happening - a multikey field was found, its
    offset slot was valid, but it was pointing at an 'indirect' array,
    not at the data. JSON tuple field access tried to use it as a data
    offset.
    
    The patch makes JSON field access degrade to fullscan when a field
    is multikey, but no multikey array index is provided.
    
    Closes #5224
    
    (cherry picked from commit 5c15df680b2f4acfc6f371c927469f15e998178b)

---
+++ test/box/gh-5224-multikey-field-access.test.lua
@@ -0,0 +1,66 @@
+--
+-- gh-5224: tuple field access by JSON path crashed when tried to get a multikey
+-- indexed field.
+--
+format = {}
+format[1] = {name = 'f1', type = 'unsigned'}
+format[2] = {name = 'f2', type = 'array'}
+s = box.schema.create_space('test', {format = format})
+_ = s:create_index('pk')
+_ = s:create_index('sk', {                                                      \
+    parts = {                                                                   \
+        {field = 2, path = "[*].tags", type = "unsigned"}                       \
+    }                                                                           \
+})
+
+t = s:replace{1, {{tags = 2}}}
+t['[2][1].tags']
+
+t = s:replace{1, {{tags = 2}, {tags = 3}, {tags = 4}}}
+t['[2]']
+t['[2][1]']
+t['[2][1].tags']
+t['[2][2]']
+t['[2][2].tags']
+t['[2][3]']
+t['[2][3].tags']
+
+s:truncate()
+s.index.sk:drop()
+_ = s:create_index('sk', {                                                      \
+    parts = {                                                                   \
+        {field = 2, path = "[*].p1.p2", type = "unsigned"}                      \
+    }                                                                           \
+})
+
+t = s:replace{1, {{p1 = {p2 = 2}}}}
+t['[2][1].p1.p2']
+
+t = s:replace{1, {                                                              \
+    {                                                                           \
+        p1 = {                                                                  \
+            p2 = 2, p3 = 3                                                      \
+        },                                                                      \
+        p4 = 4                                                                  \
+    },                                                                          \
+    {                                                                           \
+        p1 = {p2 = 5}                                                           \
+    },                                                                          \
+    {                                                                           \
+        p1 = {p2 = 6}                                                           \
+    }                                                                           \
+}}
+
+t['[2][1].p1.p2']
+t['[2][1].p1.p3']
+t['[2][1].p1']
+t['[2][1]']
+t['[2][1].p4']
+t['[2][2].p1.p2']
+t['[2][2].p1']
+t['[2][2]']
+t['[2][3].p1.p2']
+t['[2][3].p1']
+t['[2][3]']
+
+s:drop()
