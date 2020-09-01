95e94797834bf51e94f24c07df8fafd9c5af78b3
Ilya Kosarev <i.kosarev@tarantool.org>
2020-09-01 13:27:12 +0300

    msgpack: print mp_exp type as signed integer
    MsgPack extension types allow applications to define
    application-specific types. They consist of an 8-bit signed integer and
    a byte array where the integer represents a kind of types and the byte
    array represents data. Types from 0 to 127 are application-specific
    types and types from -128 to -1 are reserved for predefined types.
    However, extension types were printed as unsigned integers. Now it is
    fixed and extension types are being printed in a correct way as signed
    integers. Also the typo in word "Unsupported" was fixed. According test
    case is introduced.
    
    Closes #5016
    
    (cherry picked from commit 2a01ce91270267dfcf344f83bdeed099806fe5ff)

--- src/box/lua/init.c
+++ src/box/lua/init.c
@@ -431,7 +431,7 @@ luamp_decode_extension_box(struct lua_State *L, const char **data)
 	uint32_t len = mp_decode_extl(data, &ext_type);
 
 	if (ext_type != MP_ERROR) {
-		luaL_error(L, "Unsuported MsgPack extension type: %u",
+		luaL_error(L, "Unsupported MsgPack extension type: %d",
 			   ext_type);
 		return;
 	}
