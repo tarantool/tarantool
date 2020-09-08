85f19a875175c0402d65da4d3cee61dde5abc451
Oleg Babin <babinoleg@mail.ru>
2020-09-08 18:30:31 +0300

    lua: fix panic in case when log.cfg.log incorrecly specified
    This patch makes log.cfg{log = ...} behaviour the same as in
    box.cfg{log = ...} and fixes panic if "log" is incorrectly
    specified. For such purpose we export "say_parse_logger_type"
    function and use for logger type validation and logger type
    parsing.
    
    Closes #5130

---
+++ test/app-tap/gh-5130-panic-on-invalid-log.test.lua
@@ -0,0 +1,23 @@
+#!/usr/bin/env tarantool
+
+local test = require('tap').test('gh-5130')
+local log = require('log')
+test:plan(3)
+
+--
+-- gh-5130: panic on invalid "log"
+--
+
+-- Invalid log. No panic, just error
+_, err = pcall(log.cfg, {log=' :invalid'})
+test:like(err, "expecting a file name or a prefix")
+
+-- Empty string - default log (to be compatible with box.cfg)
+ok = pcall(log.cfg, {log=''})
+test:ok(ok)
+
+-- Dynamic reconfiguration - error, no info about invalid logger type
+_, err = pcall(log.cfg, {log=' :invalid'})
+test:like(err, "can't be set dynamically")
+
+os.exit(test:check() and 0 or 1)
