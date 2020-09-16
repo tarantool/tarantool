eea90d7ce24056612895b563d2fcfc9b7a0f6378
Alexander Turenko <alexander.turenko@tarantool.org>
2020-09-16 22:45:16 +0300

    module api: add box_key_def_validate_key()
    The function allows to verify a key against a key definition. It accepts
    a partial key.
    
    Part of #5273
    
    (cherry picked from commit 4a12985f1a2ad8779e7bb25565f5fc18fc8b55ae)

--- test/app-tap/module_api.test.lua
+++ test/app-tap/module_api.test.lua
@@ -177,7 +177,7 @@ local function test_iscdata(test, module)
 end
 
 local test = require('tap').test("module_api", function(test)
-    test:plan(33)
+    test:plan(34)
     local status, module = pcall(require, 'module_api')
     test:is(status, true, "module")
     test:ok(status, "module is loaded")
