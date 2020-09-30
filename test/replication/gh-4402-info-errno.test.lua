c34d1d6756666687411ff01e38a5978711b210a6
Alexander V. Tikhonov <avtikhon@tarantool.org>
2020-09-30 11:15:43 +0300

    test: move error messages into logs gh-5366
    Set error message to log output in test:
    
      replication/gh-4402-info-errno.test.lua

--- test/replication/gh-4402-info-errno.test.lua
+++ test/replication/gh-4402-info-errno.test.lua
@@ -12,13 +12,13 @@ test_run:cmd('create server replica with rpl_master=default, script="replication
 test_run:cmd('start server replica')
 i = box.info
 replica_id = i.id % 2 + 1
-test_run:wait_downstream(replica_id, {status = 'follow'}) or i
+test_run:wait_downstream(replica_id, {status = 'follow'}) or require('log').error(i)
 
 test_run:cmd('stop server replica')
 test_run:cmd("cleanup server replica")
 test_run:cmd("delete server replica")
 i = box.info
 d = i.replication[replica_id].downstream
-d ~= nil and d.system_message ~= nil and d.message ~= nil or i
+d ~= nil and d.system_message ~= nil and d.message ~= nil or require('log').error(i)
 
 box.schema.user.revoke('guest', 'replication')
