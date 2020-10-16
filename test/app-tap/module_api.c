533ac8c511ec222a40d50eacea512b8a42357a92
Timur Safin <tsafin@tarantool.org>
2020-10-16 19:47:52 +0300

    module api: fix asan failures in test
    Adding forgotten resource cleanup to the
    `test_tuple_validate_formatted` test, to make ASAN
    happy again.
    
    Closes #5432
    Relates to #5384
    
    (cherry picked from commit 0c40bd0a7b801cd1a1758160157625694d3feb1f)

--- test/app-tap/module_api.c
+++ test/app-tap/module_api.c
@@ -1982,6 +1982,8 @@ test_tuple_validate_formatted(lua_State *L)
 		assert(format);
 
 		valid = box_tuple_validate(tuple, format) == 0;
+		box_tuple_format_unref(format);
+		box_key_def_delete(key_defs[0]);
 	}
 	lua_pushboolean(L, valid);
 
