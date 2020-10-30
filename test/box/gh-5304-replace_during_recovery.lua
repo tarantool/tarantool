785ac7027e7936c49d9ad9575f072e82ee050115
mechanik20051988 <mechanik20.05.1988@gmail.com>
2020-10-30 20:30:50 +0300

    memtx: fix a bug with insertion to space during recovery
    There was a problem whith on_schema_init trigger.
    This trigger gives a way to create on_replace trigger
    that will modify temporary or is_local spaces during recovery
    from snapshot, but on that stage of recovery process
    all space indexes are in special build mode when no check
    for uniqueness are made. I added a new function
    'is_recovery_finished' in box.ctl, which gives
    user ability to check that we are in snapshot recovery stage
    and can't insert/replace/update/upsert something. Also i added a
    check for corresponding operations, now they are failed
    if user tries to do them during snapshot recovery.
    
    @TarantoolBot document
    Title: Add 'is_recovery_finished' function
    Add 'is_recovery_finished' function in box.ctl
    to add user ability to check that we are
    in snapshot recovery stage and can't
    insert/replace/update/upsert something
    
    Closes #5304
    
    (cherry picked from commit 3bc4a156e937102f23e2157ef88aa6c007759005)

---
+++ test/box/gh-5304-replace_during_recovery.lua
@@ -0,0 +1,22 @@
+#!/usr/bin/env tarantool
+
+if arg[1] == 'replace' then
+    box.ctl.on_schema_init(function()
+        box.space._index:on_replace(function(old_space, new_space)
+            if new_space[1] == 512 then
+                box.space.test:on_replace(function(old_tup, new_tup)
+                    box.space.temp:replace({1})
+                    box.space.temp:replace({1})
+                    box.space.loc:replace({1})
+                    box.space.loc:replace({1})
+                end)
+            end
+        end)
+    end)
+end
+
+require('console').listen(os.getenv('ADMIN'))
+
+box.cfg({
+    listen = os.getenv("LISTEN"),
+})
