eacbef1a3ed9f1dd59c655148aa7cdefd3626634
Aleksandr Lyapunov <alyapunov@tarantool.org>
2020-12-15 14:00:11 +0300

    txm: fix a simple bug in tx manager
    The problem happened when a tuple story was delete by two
    statements, one committed and one not committed.
    
    Part of #5628
    
    (cherry picked from commit 122dc47f230814a07fd2132e51b12f573e81cec7)

--- test/box/tx_man.test.lua
+++ test/box/tx_man.test.lua
@@ -252,6 +252,20 @@ s:replace{1, 2 }
 
 s:drop()
 
+-- https://github.com/tarantool/tarantool/issues/5628
+s = box.schema.space.create('test')
+i = s:create_index('pk', {parts={{1, 'uint'}}})
+
+s:replace{1, 0}
+s:delete{1}
+tx1:begin()
+tx1("s:replace{1, 1}")
+s:select{}
+tx1:commit()
+s:select{}
+
+s:drop()
+
 test_run:cmd("switch default")
 test_run:cmd("stop server tx_man")
 test_run:cmd("cleanup server tx_man")
