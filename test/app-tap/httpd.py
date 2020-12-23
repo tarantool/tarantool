e6633076e642d668e0841839886b53f11d5a1705
Sergey Bronnikov <estetus@gmail.com>
2020-12-23 13:39:52 +0300

    test: get rid of iteritems()
    For Python 3, PEP 3106 changed the design of the dict builtin and the
    mapping API in general to replace the separate list based and iterator
    based APIs in Python 2 with a merged, memory efficient set and multiset
    view based API. This new style of dict iteration was also added to the
    Python 2.7 dict type as a new set of iteration methods. PEP-0469 [1]
    recommends to replace d.iteritems() to iter(d.items()) to make code
    compatible with Python 3.
    
    1. https://www.python.org/dev/peps/pep-0469/
    
    Part of #5538
    
    (cherry picked from commit 6394a997d8cbab96f209bce5642de6167d1af02e)

--- test/app-tap/httpd.py
+++ test/app-tap/httpd.py
@@ -64,7 +64,7 @@ def read_handle(env, response):
     body = [b'Not Found']
     if env["PATH_INFO"] in paths:
         code, body, headers = paths[env["PATH_INFO"]]()
-    for key,value in env.iteritems():
+    for key,value in iter(env.items()):
         if "HTTP_" in key:
             headers.append((key[5:].lower(), value))
     response(code, headers)
