d712f83c105d107bac2491703fef5e154f54f55d
Sergey Bronnikov <sergeyb@tarantool.org>
2021-01-15 15:14:26 +0000

    test: remove unused test files
    Closes #5169
    
    (cherry picked from commit a661bef70d4ff2ba511c6e6ea71e95c6f50beb49)

--- test/box/lua/require_init.lua
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env tarantool
-box.load_cfg()
-mod = require("require_mod")
-package_path = package.path
-package_cpath = package.cpath
--- test/box/lua/require_mod.lua
+++
@@ -1,7 +0,0 @@
-exports = {}
-
-function exports.test(a, b)
-  return a+b
-end
-
-return exports
