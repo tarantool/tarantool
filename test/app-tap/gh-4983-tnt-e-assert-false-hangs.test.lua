bc0a4a20420f86b86f6068ddf37b8982b2b14436
Artem Starshov <artemreyt@tarantool.org>
2021-01-18 15:13:38 +0300

    test: change -e assert(false) test using process_timeout module
    Removed definitions of functions with timeout. Now they are used
    from separated module process_timeout.lua
    
    Follows up #4983
    
    (cherry picked from commit 6bd3663d9fa85b4f6ae0de0ec4b63a1c15da2c31)

--- test/app-tap/gh-4983-tnt-e-assert-false-hangs.test.lua
+++ test/app-tap/gh-4983-tnt-e-assert-false-hangs.test.lua
@@ -1,60 +1,16 @@
 #!/usr/bin/env tarantool
 
-local fiber = require('fiber')
-local clock = require('clock')
-local ffi = require('ffi')
-local fio = require('fio')
-local errno = require('errno')
+-- Using io.popen and self written module process_timeout
+-- in presence of the 'popen' built-in module because the
+-- last one isn't available in 1.10
+local process_timeout = require('process_timeout')
 local tap = require('tap')
+local fio = require('fio')
+local ffi = require('ffi')
 
 --
 -- gh-4983: tarantool -e 'assert(false)' hangs
 --
-
--- For process_is_alive.
-ffi.cdef([[
-    int kill(pid_t pid, int sig);
-]])
-
----  Verify whether a process is alive.
-local function process_is_alive(pid)
-    local rc = ffi.C.kill(pid, 0)
-    return rc == 0 or errno() ~= errno.ESRCH
-end
-
---- Returns true if process completed before timeout, false otherwise.
-local function wait_process_completion(pid, timeout)
-    local start_time = clock.monotonic()
-    local process_completed = false
-    while clock.monotonic() - start_time < timeout do
-        if not process_is_alive(pid) then
-            process_completed = true
-            break
-        end
-    end
-    return process_completed
-end
-
---- Open file on reading with timeout.
-local function open_with_timeout(filename, timeout)
-    local fh
-    local start_time = clock.monotonic()
-    while not fh and clock.monotonic() - start_time < timeout do
-        fh = fio.open(filename, {'O_RDONLY'})
-    end
-    return fh
-end
-
---- Try to read from file with timeout with interval.
-local function read_with_timeout(fh, timeout, interval)
-    local data = ''
-    local start_time = clock.monotonic()
-    while #data == 0 and clock.monotonic() - start_time < timeout do
-        data = fh:read()
-        if #data == 0 then fiber.sleep(interval) end
-    end
-    return data
-end
 
 local TARANTOOL_PATH = arg[-1]
 local output_file = fio.abspath('out.txt')
@@ -73,7 +29,7 @@ local res = tap.test('gh-4983-tnt-e-assert-false-hangs', function(test)
     local pid = tonumber(io.popen(line):read('*line'))
     assert(pid, 'pid of proccess can\'t be recieved')
 
-    local process_completed = wait_process_completion(pid,
+    local process_completed = process_timeout.wait_process_completion(pid,
             process_waiting_timeout)
 
     test:ok(process_completed,
@@ -82,10 +38,10 @@ local res = tap.test('gh-4983-tnt-e-assert-false-hangs', function(test)
     -- Kill process if hangs.
     if not process_completed then ffi.C.kill(pid, 9) end
 
-    local fh = open_with_timeout(output_file, file_open_timeout)
+    local fh = process_timeout.open_with_timeout(output_file, file_open_timeout)
     assert(fh, 'error while opening ' .. output_file)
 
-    local data = read_with_timeout(fh, file_read_timeout, file_read_interval)
+    local data = process_timeout.read_with_timeout(fh, file_read_timeout, file_read_interval)
     test:like(data, 'assertion failed', 'assertion failure is displayed')
 
     fh:close()
