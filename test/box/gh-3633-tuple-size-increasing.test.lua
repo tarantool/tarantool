c6b0d71dc38f9838ab02d9f497a9ff27d356cd9d
mechanik20051988 <mechanik20.05.1988@gmail.com>
2021-01-27 15:46:18 +0300

    memtx: fix memory allocation errors for distributed size tuples
    Changed small allocator strategy. In previous version small allocate
    memory from the most appropriate pool. This strategy has one significant
    disadvantage: during memory allocation for tuples with highly distributed
    sizes, we allocate an entire whole slab for one or two objects, moreover,
    when they are further deleted, slab is not released (see spare slab in
    mempool). Now we divide mempools with the same slab size into groups
    containing no more than 32 pools. First, we allocate memory from mempool
    with the largest size in group, then when memory waste for a certain size
    of objects as a result of a non-optimal pool selection became larger then
    slab size / 4, we start allocating memory for them from the most suitable
    mempool. At the same time, for other objects, we can use both of these
    mempools, in case if new mempool has a larger objsize. With this strategy,
    we avoid losing memory.
    Also change allocator behaviour in the matter of saving spare slab. With new
    allocator strategy we don't need to save spare slab for all mempools, we need
    to save it only for the last mempool in group. This strategy solves both
    problems - there is no unnecessary memory loss on the spare slabs and we
    prevent oscillations when single object is repeatedly allocated.
    
    Closes #3633

---
+++ test/box/gh-3633-tuple-size-increasing.test.lua
@@ -0,0 +1,41 @@
+env = require('test_run')
+test_run = env.new()
+test_run:cmd("create server test with script='box/gh-3633-simple-tuple-size-increasing.lua'")
+test_run:cmd("start server test")
+test_run:cmd("switch test")
+digest = require('digest')
+
+s = box.schema.space.create('test')
+test_run:cmd("setopt delimiter ';'")
+_ = s:create_index('primary', {
+    if_not_exists = true,
+    type = 'tree',
+    parts = {1, 'integer'}
+});
+test_run:cmd("setopt delimiter ''");
+
+size = 500 * 1024
+str = digest.urandom(size)
+-- insert tuples, until we get error due to no enough of memory
+for i = 1, 1024 do s:insert({i, str}) end
+-- truncate space, and collect garbage (free previous allocated memory)
+s:truncate()
+collectgarbage('collect')
+
+-- check that we have enought space for tuples, in previous allocator strategy,
+-- we get error, because spare slabs takes a lot of memory
+test_run:cmd("setopt delimiter ';'")
+for j = 9, 40 do
+    size = j * 8 * 1024
+    str = digest.urandom(size)
+    for i = 1, 1024 do s:insert({i, str}) end
+    for i = 1, 1024 do s:delete(i) end
+    collectgarbage('collect')
+end;
+test_run:cmd("setopt delimiter ''");
+
+box.space.test:drop()
+
+test_run:cmd("switch default")
+test_run:cmd("stop server test")
+test_run:cmd("cleanup server test")
