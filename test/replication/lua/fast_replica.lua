b8af9f1305a66aeee0ff4878a1bf32d0b780a9d4
Sergey Bronnikov <sergeyb@tarantool.org>
2021-01-29 14:52:18 +0000

    test: fix luacheck warnings in test/replication
    Closes #5461
    
    Reviewed-by: Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
    Reviewed-by: Igor Munkin <imun@tarantool.org>
    
    Co-authored-by: Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
    Co-authored-by: Igor Munkin <imun@tarantool.org>

--- test/replication/lua/fast_replica.lua
+++ test/replication/lua/fast_replica.lua
@@ -1,5 +1,5 @@
 
-function join(inspector, n)
+local function join(inspector, n)
     local path = os.getenv('TARANTOOL_SRC_DIR')
     for i=1,n do
         local rid = tostring(i)
@@ -7,13 +7,14 @@ function join(inspector, n)
         os.execute('cp '..path..'/test/replication/replica.lua ./tmp/replica'..rid..'.lua')
         os.execute('chmod +x ./tmp/replica'..rid..'.lua')
         local out_dir = box.cfg.wal_dir
-        inspector:cmd("create server replica"..rid.." with rpl_master=default, script='"..out_dir.."/../tmp/replica"..rid..".lua'")
+        inspector:cmd("create server replica"..rid.." with rpl_master=default, script='"
+			..out_dir.."/../tmp/replica"..rid..".lua'")
         inspector:cmd("start server replica"..rid)
     end
 end
 
 
-function call_all(callback)
+local function call_all(callback)
     local all = box.space._cluster:select{}
     for _, tuple in pairs(all) do
         local id = tuple[1]
@@ -23,54 +24,54 @@ function call_all(callback)
     end
 end
 
-function unregister(inspector, id)
+local function unregister(inspector, id) -- luacheck: no unused args
     box.space._cluster:delete{id}
 end
 
-function start(inspector, id)
+local function start(inspector, id)
     inspector:cmd('start server replica'..tostring(id - 1))
 end
 
-function stop(inspector, id)
+local function stop(inspector, id)
     inspector:cmd('stop server replica'..tostring(id - 1))
 end
 
-function wait(inspector, id)
+local function wait(inspector, id)
     inspector:wait_lsn('replica'..tostring(id - 1), 'default')
 end
 
-function delete(inspector, id)
+local function delete(inspector, id)
     inspector:cmd('stop server replica'..tostring(id - 1))
     inspector:cmd('delete server replica'..tostring(id - 1))
 end
 
-function drop(inspector, id)
+local function drop(inspector, id)
     unregister(inspector, id)
     delete(inspector, id)
 end
 
-function start_all(inspector)
+local function start_all(inspector)
     call_all(function (id) start(inspector, id) end)
 end
 
-function stop_all(inspector)
+local function stop_all(inspector)
     call_all(function (id) stop(inspector, id) end)
 end
 
-function wait_all(inspector)
+local function wait_all(inspector)
     call_all(function (id) wait(inspector, id) end)
 end
 
-function drop_all(inspector)
+local function drop_all(inspector)
     call_all(function (id) drop(inspector, id) end)
 end
 
-function vclock_diff(left, right)
+local function vclock_diff(left, right)
     local diff = 0
-    for id, lsn in ipairs(left) do
+    for id, lsn in ipairs(left) do -- luacheck: no unused
         diff = diff + (right[id] or 0) - left[id]
     end
-    for id, lsn in ipairs(right) do
+    for id, lsn in ipairs(right) do -- luacheck: no unused
         if left[id] == nil then
             diff = diff + right[id]
         end
