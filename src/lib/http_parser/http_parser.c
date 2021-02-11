bf36a103b2b5f4d741b3b44bbaa42db906ba4dc1
Sergey Bronnikov <sergeyb@tarantool.org>
2021-02-12 00:54:50 +0300

    http_parser: fix UB triggered by using negative array index
    http_parser() function resolves symbol to lower case using array, where
    an char code maps to an appropriate symbol. Some symbols may have
    negative ASCII code, but array index cannot be negative, it is an
    undefined behaviour.
    
    I read parts relevant to message headers in "Hypertext Transfer Protocol
    (HTTP/1.1): Message Syntax and Routing" [1] and "Hypertext Transfer
    Protocol -- HTTP/1.1" [2] and found that only ASCII symbols only allowed
    in message headers. It means that symbols with negative ASCII code are
    not allowed. Patch adds a check for each processed symbol to have
    positive ASCII code, otherwise function returns HTTP_PARSE_INVALID.
    
    1. RFC 7230 - 3.2 Header Fields, https://tools.ietf.org/html/rfc7230#section-3.2
    2. RFC 2616 - Message Headers, https://www.ietf.org/rfc/rfc2616.txt
    
    Bug found using LibFuzzer
    
    Closes https://github.com/tarantool/security/issues/6
    Needed for: https://github.com/google/oss-fuzz/pull/4723

--- src/lib/http_parser/http_parser.c
+++ src/lib/http_parser/http_parser.c
@@ -269,6 +269,9 @@ http_parse_header_line(struct http_parser *prsr, char **bufp,
 			default:
 				state = sw_name;
 
+				if (ch < 0) {
+					return HTTP_PARSE_INVALID;
+				}
 				c = lowcase[ch];
 				if (c != 0) {
 					prsr->hdr_name[0] = c;
@@ -303,6 +306,9 @@ http_parse_header_line(struct http_parser *prsr, char **bufp,
 			break;
 		/* http_header name */
 		case sw_name:
+			if (ch < 0) {
+				return HTTP_PARSE_INVALID;
+			}
 			c = lowcase[ch];
 			if (c != 0) {
 				if (prsr->hdr_name_idx < max_hname_len) {
