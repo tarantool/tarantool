3aef12f5c1e97fbac08a66947a6a5842aea15dcb
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2021-03-04 22:31:48 +0100

    cord_buf: introduce cord_buf API
    There was a global ibuf object called tarantool_lua_ibuf. It was
    used in all the places working with Lua which didn't have yields,
    and where fiber's region could be potentially slower due to not
    being able to guarantee the allocated memory is contiguous.
    
    Yields during the ibuf usage were prohibited because another fiber
    would take the same ibuf and override its previous content which
    was still used by another fiber.
    
    But it wasn't taken into account that there is Lua GC. It can be
    invoked from any Lua function in Lua C code, and almost on any
    line in the Lua scripts. During GC some deleted objects might have
    GC handlers installed as __gc metamethods. From the handler they
    could call Tarantool functions, including the ones using the
    global ibuf.
    
    Therefore ibuf could be overridden not only at yields, but almost
    in any moment. Because with the Lua GC at hand, the multitasking
    is not strictly "cooperative" anymore.
    
    It is necessary to implement ownership for the global buffer. The
    patch prepares the API for this: the buffer is moved to its own
    file, and has methods take(), put(), and drop().
    
    Take() is supposed to make the current fiber own the buffer. Put()
    makes it available again. Drop() does the same but also clears the
    buffer (frees its memory). The ownership itself is a subject for
    the next patches. Here only the API is prepared.
    
    The patch "hits" performance a little. Previously the get of
    buffer.IBUF_SHARED cost around 1 ns. Now cord_ibuf_take() +
    cord_ibuf_put() cost around 5 ns together. The next patches will
    make it worse, up to 15 ns until #5871 is done.
    
    Part of #5632
    
    (cherry picked from commit ade45685f4fdab47204e11cbd82048bb68bbd03b)

--- static-build/test/static-build/exports.test.lua
+++ static-build/test/static-build/exports.test.lua
@@ -26,6 +26,9 @@ local check_symbols = {
     'guava',
     'base64_decode',
     'base64_encode',
+    'cord_ibuf_drop',
+    'cord_ibuf_put',
+    'cord_ibuf_take',
     'SHA1internal',
     'random_bytes',
     'fiber_time',
@@ -42,7 +45,6 @@ local check_symbols = {
     'exception_get_int',
     'exception_get_string',
 
-    'tarantool_lua_ibuf',
     'uuid_nil',
     'tt_uuid_create',
     'tt_uuid_str',
