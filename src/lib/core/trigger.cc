c1020a69a4932bec538c96e05ae07a644d3d4b3c
mechanik20051988 <mechanik20.05.1988@gmail.com>
2021-03-05 13:46:15 +0300

    Implement trigger_fiber_run function
    Implemented function for starting a chain of triggers
    in separate fibers, which is required for on_shutdown
    API implementation.
    
    Part of #5723

--- src/lib/core/trigger.cc
+++ src/lib/core/trigger.cc
@@ -31,6 +31,29 @@
 
 #include "trigger.h"
 
+#include "fiber.h"
+
+#include <small/region.h>
+
+static void
+trigger_fiber_run_timeout(ev_loop *loop, ev_timer *watcher, int revents)
+{
+	(void) loop;
+	(void) revents;
+
+	bool *expired = (bool *)watcher->data;
+	assert(expired != NULL);
+	*expired = true;
+}
+
+static int
+trigger_fiber_f(va_list ap)
+{
+	struct trigger *trigger = va_arg(ap, struct trigger *);
+	void *event = va_arg(ap, void *);
+	return trigger->run(trigger, event);
+}
+
 int
 trigger_run(struct rlist *list, void *event)
 {
@@ -49,4 +72,73 @@ trigger_run_reverse(struct rlist *list, void *event)
 		if (trigger->run(trigger, event) != 0)
 			return -1;
 	return 0;
+}
+
+void
+trigger_fiber_run(struct rlist *list, void *event, double timeout)
+{
+	struct trigger *trigger, *tmp;
+	unsigned trigger_count = 0;
+	struct region *region = &fiber()->gc;
+	RegionGuard guard(region);
+	bool expired = false;
+	struct ev_timer timer;
+	ev_timer_init(&timer, trigger_fiber_run_timeout, timeout, 0);
+	timer.data = &expired;
+	/*
+	 * We don't check if triggers are timed out during they launch
+	 * since we want to give them all a chance to run. So in
+	 * rlist_foreach_entry_safe loop, we run all the triggers,
+	 * regardless of whether the timeout has expired or not.
+	 */
+	ev_timer_start(loop(), &timer);
+
+	/*
+	 * Calculating the total number of triggers.
+	 */
+	rlist_foreach_entry(trigger, list, link)
+		trigger_count++;
+
+	struct fiber **fibers = (struct fiber **)
+		region_alloc(region, trigger_count * sizeof(struct fiber *));
+	if (fibers == NULL) {
+		say_error("Failed to allocate %lu bytes in region_alloc "
+			  "for fiber", trigger_count * sizeof(struct fiber));
+		ev_timer_stop(loop(), &timer);
+		return;
+	}
+
+	unsigned current_fiber = 0;
+	rlist_foreach_entry_safe(trigger, list, link, tmp) {
+		char name[FIBER_NAME_INLINE];
+		snprintf(name, FIBER_NAME_INLINE,
+			 "trigger_fiber%d", current_fiber);
+		fibers[current_fiber] = fiber_new(name, trigger_fiber_f);
+		if (fibers[current_fiber] != NULL) {
+			fiber_set_joinable(fibers[current_fiber], true);
+			fiber_start(fibers[current_fiber], trigger, event);
+			current_fiber++;
+		} else {
+			diag_log();
+			diag_clear(diag_get());
+			ev_timer_stop(loop(), &timer);
+			return;
+		}
+	}
+
+	/*
+	 * Waiting for all triggers completion.
+	 */
+	for (unsigned int i = 0; i < current_fiber && ! expired; i++) {
+		if (fiber_join_timeout(fibers[i], timeout) != 0) {
+			assert(! diag_is_empty(diag_get()));
+			diag_log();
+			diag_clear(diag_get());
+		}
+	}
+	if (expired) {
+		say_error("on_shutdown triggers are timed out: "
+			  "not all triggers might have finished yet");
+	}
+	ev_timer_stop(loop(), &timer);
 }
--- src/lib/core/trigger.h
+++ src/lib/core/trigger.h
@@ -131,6 +131,17 @@ trigger_run(struct rlist *list, void *event);
 int
 trigger_run_reverse(struct rlist *list, void *event);
 
+/**
+ * Runs registered triggers in fibers.
+ * Note, since triggers are added to the list head, this
+ * function first runs triggers that were added last. Waits
+ * for their completion for a specified period of time.
+ * If timeout has expired and function immediately stops,
+ * without waiting for other triggers completion.
+ */
+void
+trigger_fiber_run(struct rlist *list, void *event, double timeout);
+
 #if defined(__cplusplus)
 } /* extern "C" */
 
