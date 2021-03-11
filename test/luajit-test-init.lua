4f703e77aacd8239b81cde951f2445cce85be23f
Sergey Kaplun <skaplun@tarantool.org>
2021-03-11 12:00:44 +0300

    luajit: bump new version
    LuaJIT submodule is bumped to introduce the following changes:
    * test: disable LuaJIT CLI tests in lua-Harness suite
    * test: set USERNAME env var for lua-Harness suite
    * test: adjust lua-Harness tests that use dofile
    * test: adjust lua-Harness suite to CMake machinery
    * test: add lua-Harness test suite
    
    Within this changeset lua-Harness suite[1] is added to Tarantool
    testing. Considering Tarantool specific changes in runtime the suite
    itself is adjusted in LuaJIT submodule.
    
    However, Tarantool provides and unconditionally loads TAP module
    conflicting with the one used in the new suite. Hence, the Tarantool
    built-in module is "unloaded" in test/luajit-test-init.lua.
    
    Furthermore, Tarantool provides UTF-8 support via another built-in
    module. Its interfaces differ from the ones implemented in Lua5.3 and
    moonjit. At the same time our LuaJIT fork provides no UTF-8 support, so
    lua-Harness UTF-8 detector is simply confused with non-nil utf8 global
    variable. As a result, utf8 is set to nil in test/luajit-test-init.lua.
    
    There are also some tests launching Lua interpreter, so strict need to
    be disabled for their child tests too. Hence `strict.off()` is added to
    `progname` (i.e. arg[-1] considering the way Tarantool parses its CLI
    arguments) command used in these tests.
    
    [1]: https://framagit.org/fperrad/lua-Harness/tree/a74be27/test_lua
    
    Closes #5844
    Part of #4473
    
    Reviewed-by: Sergey Ostanevich <sergos@tarantool.org>
    Reviewed-by: Igor Munkin <imun@tarantool.org>
    Signed-off-by: Igor Munkin <imun@tarantool.org>
    (cherry picked from commit a0ade20662e0d55c912a72656e6dc75defd7c7f4)

--- test/luajit-test-init.lua
+++ test/luajit-test-init.lua
@@ -1,2 +1,24 @@
 -- Disable strict for Tarantool.
 require("strict").off()
+
+-- XXX: lua-Harness test suite uses it's own tap.lua module
+-- that conflicts with the Tarantool's one.
+package.loaded.tap = nil
+-- XXX: lua-Harness test suite checks that utf8 module presents
+-- only in Lua5.3 or moonjit.
+utf8 = nil
+
+-- There are some tests launching Lua interpreter, so strict need
+-- to be disabled for the child tests too. Hence `strict.off()` is
+-- added to `progname` command used in these tests.
+-- Unlike LuaJIT, Tarantool doesn't store the given CLI flags in
+-- `arg`, so the table has the following layout:
+-- * arg[-1] -- the binary name
+-- * arg[0]  -- the script name
+-- * arg[N]  -- the script argument for all N in [1, #arg]
+-- The former one can be used to adjust the command to be used in
+-- child tests.
+-- XXX: Quotes types are important.
+-- XXX: luacheck thinks that `arg` is read-only global variable.
+-- luacheck: no global
+arg[-1] = arg[-1]..' -e "require[[strict]].off()"'
