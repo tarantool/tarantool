55f93fd93093f12c21c07fdde17f0873a42b1bb6
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2021-03-16 23:38:51 +0100

    uuid: drop tt_uuid_str() from Lua
    The function converts struct tt_uuid * to a string. The string is
    allocated on the static buffer, which can't be used in Lua due to
    unpredictable GC behaviour. It can start working any moment even
    if tt_uuid_str() has returned, but its result wasn't passed to
    ffi.string() yet. Then the buffer might be overwritten.
    
    Lua uuid now uses tt_uuid_to_string() which does the same but
    takes the buffer pointer. The buffer is stored in an ffi stash,
    because it is x4 times faster than ffi.new('char[37]') (where 37
    is length of a UUID string + terminating 0) (2.4 ns vs 0.8 ns).
    
    After this patch UUID is supposed to be fully compatible with Lua
    GC handlers.
    
    Part of #5632

---
+++ test/app-tap/gh-5632-gc-buf-reuse.test.lua
@@ -0,0 +1,42 @@
+#!/usr/bin/env tarantool
+
+local tap = require('tap')
+local ffi = require('ffi')
+local uuid = require('uuid')
+
+local function test_uuid(test)
+    test:plan(1)
+
+    local gc_count = 100
+    local iter_count = 1000
+    local is_success = true
+
+    local function uuid_to_str()
+        local uu = uuid.new()
+        local str1 = uu:str()
+        local str2 = uu:str()
+        if str1 ~= str2 then
+            is_success = false
+            assert(false)
+        end
+    end
+
+    local function create_gc()
+        for i = 1, gc_count do
+            ffi.gc(ffi.new('char[1]'), function() uuid_to_str() end)
+        end
+    end
+
+    for i = 1, iter_count do
+        create_gc()
+        uuid_to_str()
+    end
+
+    test:ok(is_success, 'uuid in gc')
+end
+
+local test = tap.test('gh-5632-gc-buf-reuse')
+test:plan(1)
+test:test('uuid in __gc', test_uuid)
+
+os.exit(test:check() and 0 or 1)
