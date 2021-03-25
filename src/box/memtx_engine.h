c1e274d87818cf8ebc977b2ce0a4fcd03bb47685
mechanik20051988 <mechanik20.05.1988@gmail.com>
2021-03-25 11:30:38 +0300

    memtx: implement api for memory allocator selection
    Add new 'memtx_allocator' option to box.cfg{} which allows to
    select the appropriate allocator for memtx tuples if necessary.
    Possible values are "system" for malloc allocator and
    "small" for default small allocator.
    Closes #5419
    
    @TarantoolBot document
    Title: Add new 'memtx_allocator' option to box.cfg{}
    Add new 'memtx_allocator' option to box.cfg{} which allows to
    select the appropriate allocator for memtx tuples if necessary.
    Possible values are "system" for malloc allocator and
    "small" for default small allocator.

--- src/box/memtx_engine.h
+++ src/box/memtx_engine.h
@@ -40,6 +40,7 @@
 #include "engine.h"
 #include "xlog.h"
 #include "salad/stailq.h"
+#include "sysalloc.h"
 
 #if defined(__cplusplus)
 extern "C" {
@@ -194,6 +195,8 @@ struct memtx_engine {
 	enum memtx_engine_free_mode free_mode;
 	/** List of tuples for delayed free. */
 	struct lifo delayed;
+	/** List of tuples for delayed free in case when sys_alloc selected */
+	struct lifo sys_delayed;
 };
 
 struct memtx_gc_task;
@@ -227,9 +230,9 @@ memtx_engine_schedule_gc(struct memtx_engine *memtx,
 
 struct memtx_engine *
 memtx_engine_new(const char *snap_dirname, bool force_recovery,
-		 uint64_t tuple_arena_max_size,
-		 uint32_t objsize_min, bool dontdump,
-		 unsigned granularity, float alloc_factor);
+		 uint64_t tuple_arena_max_size, uint32_t objsize_min,
+		 bool dontdump, unsigned granularity,
+		 const char *allocator, float alloc_factor);
 
 int
 memtx_engine_recover_snapshot(struct memtx_engine *memtx,
@@ -305,15 +308,15 @@ memtx_index_def_change_requires_rebuild(struct index *index,
 
 static inline struct memtx_engine *
 memtx_engine_new_xc(const char *snap_dirname, bool force_recovery,
-		    uint64_t tuple_arena_max_size,
-		    uint32_t objsize_min, bool dontdump,
-		    unsigned granularity, float alloc_factor)
+		    uint64_t tuple_arena_max_size, uint32_t objsize_min,
+		    bool dontdump, unsigned granularity,
+		    const char *allocator, float alloc_factor)
 {
 	struct memtx_engine *memtx;
 	memtx = memtx_engine_new(snap_dirname, force_recovery,
 				 tuple_arena_max_size,
 				 objsize_min, dontdump,
-				 granularity, alloc_factor);
+				 granularity, allocator, alloc_factor);
 	if (memtx == NULL)
 		diag_raise();
 	return memtx;
