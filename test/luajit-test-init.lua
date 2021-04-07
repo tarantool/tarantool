8343a8129fbe84333999157060649b414ef7defd
Sergey Kaplun <skaplun@tarantool.org>
2021-04-07 18:16:50 +0300

    luajit: bump new version
    LuaJIT submodule is bumped to introduce the following changes:
    * test: disable too deep recursive PUC-Rio test
    * test: disable PUC-Rio hanging GC test
    * test: disable PUC-Rio test checking -h option
    * test: disable PUC-Rio test for checking arg layout
    * test: disable PUC-Rio tests for several -l options
    * test: disable PUC-Rio test for syntax level error
    * test: disable PUC-Rio test for non-ascii variable
    * test: disable PUC-Rio test for fast function name
    * test: disable PUC-Rio test for variables in error
    * test: disable PUC-Rio test for getfenv in tailcall
    * test: remove string.gfind assert in PUC-Rio test
    * test: use math.fmod in PUC-Rio tests
    * test: disable locale-dependent PUC-Rio tests
    * test: adapt PUC-Rio test for %q in string.format
    * test: disable PUC-Rio test for per-coroutine hooks
    * test: adapt PUC-Rio test with activeline check
    * test: disable PUC-Rio test for tailcall info
    * test: adapt PUC-Rio test with count hooks
    * test: adapt PUC-Rio test for debug in vararg func
    * test: adapt PUC-Rio tests with vararg functions
    * test: disable PUC-Rio suite tests for line hook
    * test: adapt PUC-Rio tests counting GC steps
    * test: disable PUC-Rio tests for bytecode header
    * test: disable PUC-Rio tests confused by -v output
    * test: adapt PUC-Rio test for arg presence
    * test: remove quotes in progname from PUC-Rio
    * test: adapt PUC-Rio suite for out-of-source build
    * test: build auxiliary C libs from PUC-Rio Lua 5.1
    * test: add PUC-Rio Lua 5.1 test suite
    
    Within this changeset PUC-Rio Lua 5.1 suite[1] is added to Tarantool
    testing. Considering Tarantool specific changes in runtime the suite
    itself is adjusted in LuaJIT submodule.
    
    <test/luajit-test-init.lua> pretest runner is adjusted by setting custom
    `_loadstring()` and `_dofile()` global function to run tests correctly
    for out-of-source build.
    
    Also, this patch excludes PUC-Rio-Lua-5.1 test suite from ASAN checks.
    
    [1]: https://www.lua.org/tests/lua5.1-tests.tar.gz
    
    Closes #5845
    Closes #5686
    Closes #5694
    Closes #5701
    Closes #5708
    Closes #5710
    Closes #5711
    Closes #5712
    Part of #4473
    
    Reviewed-by: Igor Munkin <imun@tarantool.org>
    Signed-off-by: Igor Munkin <imun@tarantool.org>
    (cherry picked from commit 8a09e18ce723ffaed85f04d27a388ffdb681308f)

--- test/luajit-test-init.lua
+++ test/luajit-test-init.lua
@@ -22,3 +22,22 @@ utf8 = nil
 -- XXX: luacheck thinks that `arg` is read-only global variable.
 -- luacheck: no global
 arg[-1] = arg[-1]..' -e "require[[strict]].off()"'
+
+-- XXX: PUC Rio Lua 5.1 test suite checks that global variable
+-- `_loadfile()` exists and uses it for code loading from test
+-- files. If the variable is not defined then suite uses
+-- `loadfile()` as default. Same for the `_dofile()`.
+
+-- XXX: Some tests in PUC Rio Lua 5.1 test suite clean `arg`
+-- variable, so evaluate this once and use later.
+local path_to_sources = arg[0]:gsub("[^/]+$", "")
+
+-- luacheck: no global
+function _loadfile(filename)
+  return loadfile(path_to_sources..filename)
+end
+
+-- luacheck: no global
+function _dofile(filename)
+  return dofile(path_to_sources..filename)
+end
