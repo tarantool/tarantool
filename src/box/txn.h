21397f49e1ec1976678abed612c7dee757129a87
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2021-04-25 17:24:38 +0200

    txn: destroy commit and rollback triggers
    They were not deleted ever. Worked fine for DDL and replication,
    for which they were introduced in the first place, because these
    triggers are on the region memory.
    
    But didn't work when the triggers became available in the public
    API, because these are allocated on the heap. As a result, all the
    box.on_commit() and box.on_rollback() triggers leaked.
    
    The patch ensures all the on_commit/on_rollback triggers are
    destroyed.
    
    The statement triggers on_commit/on_rollback are left intact since
    they are private and never need deletion, but the patch adds
    assertions in case they ever would need to be destroyed.
    
    Another option was to force all the commit and rollback triggers
    clear themselves. For example, in case of commit all the on_commit
    triggers must clear themselves, and the rollback triggers are
    destroyed. Vice versa when a rollback happens. This would allow
    not to manually destroy on_commit triggers in case of commit. But
    it didn't work because the Lua triggers all work via a common
    runner lbox_trigger_run(), which can't destroy its argument in
    most of the cases (space:on_replace, :before_replace, ...). It
    could be patched but requires to add some work to the Lua triggers
    affecting all of them, which in total might be not better.
    
    Closes #6025
    
    (cherry picked from commit 7fd53b4c5264bdbc8f01858409abe52bc38764c8)

--- src/box/txn.h
+++ src/box/txn.h
@@ -545,6 +545,8 @@ static inline void
 txn_stmt_on_commit(struct txn_stmt *stmt, struct trigger *trigger)
 {
 	txn_stmt_init_triggers(stmt);
+	/* Statement triggers are private and never have anything to free. */
+	assert(trigger->destroy == NULL);
 	trigger_add(&stmt->on_commit, trigger);
 }
 
@@ -552,6 +554,8 @@ static inline void
 txn_stmt_on_rollback(struct txn_stmt *stmt, struct trigger *trigger)
 {
 	txn_stmt_init_triggers(stmt);
+	/* Statement triggers are private and never have anything to free. */
+	assert(trigger->destroy == NULL);
 	trigger_add(&stmt->on_rollback, trigger);
 }
 
