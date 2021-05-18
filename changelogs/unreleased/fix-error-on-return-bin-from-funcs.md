77c0af50120f28026eb66b364a8d7c1428ab1c3d
Mergen Imeev <imeevma@gmail.com>
2021-05-18 18:32:09 +0300

    sql: VARBINARY result for LUA functions
    This patch allows VARBINARY to be returned for user-defined LUA
    functions. However, there are currently no values that can be
    interpreted as VARBINARY by the serializer, so the only way to get a
    VARBINARY result for user-defined LUA functions is to return a UUID or
    DECIMAL. Both types are not supported by SQL and are treated as
    VARBINARY.
    
    Closes #6024
    
    (cherry picked from commit 2b25ca6c70908b57c011ee24d2eeb9f6155d2750)

---
+++ changelogs/unreleased/fix-error-on-return-bin-from-funcs.md
@@ -0,0 +1,3 @@
+## bugfix/sql
+
+* User-defined functions can now return VARBINARY to SQL as result (gh-6024).
--- test/sql-tap/gh-6024-funcs-return-bin.test.lua
+++ test/sql-tap/gh-6024-funcs-return-bin.test.lua
@@ -3,7 +3,7 @@ local build_path = os.getenv("BUILDDIR")
 package.cpath = build_path..'/test/sql-tap/?.so;'..build_path..'/test/sql-tap/?.dylib;'..package.cpath
 
 local test = require("sqltester")
-test:plan(3)
+test:plan(5)
 
 box.schema.func.create("gh-6024-funcs-return-bin.ret_bin", {
     language = "C",
@@ -50,8 +50,42 @@ test:do_execsql_test(
         "varbinary"
     })
 
+box.schema.func.create("get_uuid", {
+    language = "LUA",
+    param_list = {},
+    returns = "varbinary",
+    body = "function(x) return require('uuid').fromstr('11111111-1111-1111-1111-111111111111') end",
+    exports = {"SQL"},
+})
+
+test:do_execsql_test(
+    "gh-6024-4",
+    [[
+        SELECT typeof("get_uuid"()), "get_uuid"() == "gh-6024-funcs-return-bin.ret_uuid"();
+    ]], {
+        "varbinary", true
+    })
+
+box.schema.func.create("get_decimal", {
+    language = "LUA",
+    param_list = {},
+    returns = "varbinary",
+    body = "function(x) return require('decimal').new('9999999999999999999.9999999999999999999') end",
+    exports = {"SQL"},
+})
+
+test:do_execsql_test(
+    "gh-6024-5",
+    [[
+        SELECT typeof("get_decimal"()), "get_decimal"() == "gh-6024-funcs-return-bin.ret_decimal"();
+    ]], {
+        "varbinary", true
+    })
+
 box.schema.func.drop("gh-6024-funcs-return-bin.ret_bin")
 box.schema.func.drop("gh-6024-funcs-return-bin.ret_uuid")
 box.schema.func.drop("gh-6024-funcs-return-bin.ret_decimal")
+box.schema.func.drop("get_uuid")
+box.schema.func.drop("get_decimal")
 
 test:finish_test()
