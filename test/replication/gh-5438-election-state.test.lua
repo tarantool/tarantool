0ea62adb66288bbbb0ad6e97f425daece0215987
Serge Petrenko <sergepetrenko@tarantool.org>
2021-06-10 15:03:25 +0300

    replication: always send raft state to subscribers
    Tarantool used to send out raft state on subscribe only when raft was
    enabled. This was a safeguard against partially-upgraded clusters, where
    some nodes had no clue about Raft messages and couldn't handle them
    properly.
    
    Actually, Raft state should be sent out always. For example, promote
    will be changed to bump Raft term even when Raft is disabled, and it's
    important that everyone in cluster has the same term for the sake of promote
    at least.
    
    So, send out Raft state to every subscriber with version >= 2.6.0
    (that's when Raft was introduced).
    Do the same for Raft broadcasts. They should be sent only to replicas
    with version >= 2.6.0
    
    Closes #5438

---
+++ test/replication/gh-5438-election-state.test.lua
@@ -0,0 +1,28 @@
+test_run = require('test_run').new()
+
+--
+-- gh-5428 send out Raft state to subscribers, even when Raft is disabled.
+--
+-- Bump Raft term while the replica's offline.
+term = box.info.election.term
+old_election_mode = box.cfg.election_mode
+box.cfg{election_mode = 'candidate'}
+test_run:wait_cond(function() return box.info.election.term > term end)
+
+-- Make sure the replica receives new term on subscribe.
+box.cfg{election_mode = 'off'}
+
+box.schema.user.grant('guest', 'replication')
+test_run:cmd('create server replica with rpl_master=default,\
+                                         script="replication/replica.lua"')
+test_run:cmd('start server replica')
+test_run:wait_cond(function()\
+    return test_run:eval('replica', 'return box.info.election.term')[1] ==\
+           box.info.election.term\
+end)
+
+-- Cleanup.
+box.cfg{election_mode = old_election_mode}
+test_run:cmd('stop server replica')
+test_run:cmd('delete server replica')
+box.schema.user.revoke('guest', 'replication')
