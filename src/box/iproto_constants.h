d885a10d95e092d4f81ec98a8a9316caeecc7239
Iskander Sagitov <i.sagitov@tarantool.org>
2021-06-16 12:08:24 +0300

    iproto: introduce IPROTO_SHUTDOWN command code
    This command code is used in 3 cases in shutdown process:
    1. Server sends IPROTO_SHUTDOWN to all remote clients that support
       graceful shutdown at the start of shutdown process.
    2. Remote client sends IPROTO_SHUTDOWN in response to IPROTO_SHUTDOWN
       from server.
    3. Remote client sends IPROTO_SHuTDOWN to the server just because it
       can. In this case server should mark this remote client as support
       graceful shutdown and make shutdown(con.input.fd, SHUT_RD).
    
    Part of #5924

--- src/box/iproto_constants.h
+++ src/box/iproto_constants.h
@@ -247,6 +247,8 @@ enum iproto_type {
 	/** A rollback message for synchronous transactions. */
 	IPROTO_ROLLBACK = 41,
 
+	/** SHUTDOWN command code */
+	IPROTO_SHUTDOWN = 63,
 	/** PING request */
 	IPROTO_PING = 64,
 	/** Replication JOIN command */
