338e867568869fd4d88db6ccbc0c410d11ef7ff4
Serge Petrenko <sergepetrenko@tarantool.org>
2021-07-09 14:22:22 +0300

    box: test the assertion failure after a spurious wakeup in promote
    The failure itself was fixed in 68de8753cc416fd472230f394f1a1493a2e60162
    (raft: replace raft_start_candidate with _promote), let's add a
    regression test now.
    
    Follow-up #3055

---
+++ test/replication/gh-3055-promote-wakeup-crash.test.lua
@@ -0,0 +1,21 @@
+test_run = require('test_run').new()
+--
+-- gh-3055 follow-up: box.ctl.promote() could crash on an assertion after a
+-- spurious wakeup.
+--
+_ = box.space._cluster:insert{2, require('uuid').str()}
+box.cfg{election_mode='manual',\
+        replication_synchro_quorum=2,\
+        election_timeout=1000}
+
+fiber = require('fiber')
+f = fiber.create(function() box.ctl.promote() end)
+f:set_joinable(true)
+f:wakeup()
+fiber.yield()
+
+-- Cleanup.
+f:cancel()
+f:join()
+box.cfg{election_mode='off'}
+test_run:cleanup_cluster()
