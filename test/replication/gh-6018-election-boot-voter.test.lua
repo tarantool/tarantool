80fd87a72c255d19df132d3104d0defce31e21f3
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2021-07-17 00:16:02 +0200

    election: promote 'manual' bootstrap master
    A cluster may consist of only voters and manual nodes. This means
    on bootstrap nobody would be elected as a Raft leader
    automatically to create the first snapshot and register the
    others.
    
    After the previous commit the manual nodes were preferred to be
    bootstrap masters, but they couldn't do anything.
    
    This patch makes 'manual' bootstrap master promote itself for one
    term so as it could boot the cluster.
    
    Closes #6018

--- test/replication/gh-6018-election-boot-voter.test.lua
+++ test/replication/gh-6018-election-boot-voter.test.lua
@@ -3,6 +3,11 @@
 -- as bootstrap leaders. They should not, because a voter can't be ever writable
 -- and it can neither boot itself nor register other nodes.
 --
+-- Similar situation was with the manual election. All instances might have
+-- manual election mode. Such a cluster wouldn't be able to boot if their
+-- bootstrap master wouldn't become an elected leader automatically at least
+-- once.
+--
 test_run = require('test_run').new()
 
 function boot_with_master_election_mode(mode)                                   \
@@ -25,6 +30,22 @@ end
 -- Candidate leader.
 --
 boot_with_master_election_mode('candidate')
+
+test_run:switch('master')
+test_run:wait_cond(function() return not box.info.ro end)
+assert(box.info.election.state == 'leader')
+
+test_run:switch('replica')
+assert(box.info.ro)
+assert(box.info.election.state == 'follower')
+
+test_run:switch('default')
+stop_cluster()
+
+--
+-- Manual leader.
+--
+boot_with_master_election_mode('manual')
 
 test_run:switch('master')
 test_run:wait_cond(function() return not box.info.ro end)
