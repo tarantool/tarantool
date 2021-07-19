22b494040ba88681f1b960700d0ec2d2231b8ecf
Vladimir Davydov <vdavydov@tarantool.org>
2021-07-19 15:55:27 +0300

    net.box: fix console connection breakage when request is discarded
    If a timed-out console request is collected by the garbage collector
    before the response is processed, the connection will stop serving new
    requests. This happens because of the erroneous 'return' statement in
    console_sm. Fix it and add a test.
    
    Closes #6249

--- src/lib/core/errinj.h
+++ src/lib/core/errinj.h
@@ -154,6 +154,7 @@ struct errinj {
 	_(ERRINJ_IPROTO_SINGLE_THREAD_STAT, ERRINJ_INT, {.iparam = -1}) \
 	_(ERRINJ_IPROTO_WRITE_ERROR_DELAY, ERRINJ_BOOL, {.bparam = false})\
 	_(ERRINJ_APPLIER_READ_TX_ROW_DELAY, ERRINJ_BOOL, {.bparam = false})\
+	_(ERRINJ_NETBOX_IO_DELAY, ERRINJ_BOOL, {.bparam = false}) \
 
 ENUM0(errinj_id, ERRINJ_LIST);
 extern struct errinj errinjs[];
