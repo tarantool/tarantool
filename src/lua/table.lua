0afe1f78a51990d7849a11339fe9c5af06165bd4
Serge Petrenko <sergepetrenko@tarantool.org>
2021-08-12 22:50:19 +0300

    lua: introduce table.equals method
    Introduce table.equals for comparing tables.
    The method respects __eq metamethod, if provided.
    
    Needed-for #5894
    
    @TarantoolBot document
    Title: lua: new method table.equals
    
    Document the new lua method table.equals
    It compares two tables deeply. For example:
    ```
    tarantool> t1 = {a=3}
    ---
    ...
    
    tarantool> t2 = {a=3}
    ---
    ...
    
    tarantool> t1 == t2
    ---
    - false
    ...
    
    tarantool> table.equals(t1, t2)
    ---
    - true
    ...
    ```
    The method respects the __eq metamethod. When both tables being compared
    have the same __eq metamethod, it's used for comparison (just like this
    is done in Lua 5.1)

--- src/lua/table.lua
+++ src/lua/table.lua
@@ -57,6 +57,34 @@ local function table_shallowcopy(orig)
     return copy
 end
 
+--- Compare two lua tables
+-- Supports __eq metamethod for comparing custom tables with metatables
+-- @function equals
+-- @return true when the two tables are equal (false otherwise).
+local function table_equals(a, b)
+    if type(a) ~= 'table' or type(b) ~= 'table' then
+        return type(a) == type(b) and a == b
+    end
+    local mta = getmetatable(a)
+    local mtb = getmetatable(b)
+    -- Let Lua decide what should happen when at least one of the tables has a
+    -- metatable.
+    if mta and mta.__eq or mtb and mtb.__eq then
+        return a == b
+    end
+    for k, v in pairs(a) do
+        if not table_equals(v, b[k]) then
+            return false
+        end
+    end
+    for k, _ in pairs(b) do
+        if not a[k] then
+            return false
+        end
+    end
+    return true
+end
+
 -- table library extension
 local table = require('table')
 -- require modifies global "table" module and adds "clear" function to it.
@@ -65,3 +93,4 @@ require('table.clear')
 
 table.copy     = table_shallowcopy
 table.deepcopy = table_deepcopy
+table.equals   = table_equals
