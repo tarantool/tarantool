fb7fe96b5a0cb29da849fcd0d5258fdc65ff0bff
Vladimir Davydov <vdavydov@tarantool.org>
2021-08-20 19:13:40 +0300

    test: fix box/net.box_is_nullable_gh-3256
    The new version (2.10.0) is considered to be lower than 2.2.1, because
    the test uses string comparison, which results in a test failure.
    
    Remove the useless check.

--- test/box/net.box_is_nullable_gh-3256.test.lua
+++ test/box/net.box_is_nullable_gh-3256.test.lua
@@ -24,11 +24,7 @@ parts = c.space.test.index.sk.parts
 parts[1].fieldno == 1
 parts[1].type == 'string'
 parts[1].is_nullable == false
-if _TARANTOOL >= '2.2.1' then                    \
-    return parts[1].collation == 'test'          \
-else                                             \
-    return parts[1].collation_id == collation_id \
-end
+parts[1].collation == 'test'
 c:close()
 box.internal.collation.drop('test')
 space:drop()
