6a92aab7c2130206a3e8e711f1e7f5aa89657e11
Vladimir Davydov <vdavydov@tarantool.org>
2021-09-27 14:21:57 +0300

    Move xmalloc to trivia/util.h
    We want to use the xmalloc helper throughout the code, not only in
    the core lib. Move its definition to trivia/util.h and use fprintf+exit
    instead of say/panic in order not to create circular dependencies.

--- src/trivia/util.h
+++ src/trivia/util.h
@@ -105,6 +105,27 @@ strnindex(const char **haystack, const char *needle, uint32_t len, uint32_t hmax
 #define lengthof(array) (sizeof (array) / sizeof ((array)[0]))
 #endif
 
+/**
+ * An x* variant of a memory allocation function calls the original function
+ * and panics if it fails (i.e. it should never return NULL).
+ */
+#define xalloc_impl(size, func, args...)					\
+	({									\
+		void *ret = func(args);						\
+		if (unlikely(ret == NULL)) {					\
+			fprintf(stderr, "Can't allocate %zu bytes at %s:%d",	\
+				(size_t)(size), __FILE__, __LINE__);		\
+			exit(EXIT_FAILURE);					\
+		}								\
+		ret;								\
+	})
+
+#define xmalloc(size)		xalloc_impl((size), malloc, (size))
+#define xcalloc(n, size)	xalloc_impl((n) * (size), calloc, (n), (size))
+#define xrealloc(ptr, size)	xalloc_impl((size), realloc, (ptr), (size))
+#define xstrdup(s)		xalloc_impl(strlen((s)) + 1, strdup, (s))
+#define xstrndup(s, n)		xalloc_impl((n) + 1, strndup, (s), (n))
+
 /** \cond public */
 
 /**
