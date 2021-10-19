d03146a71160941b9d62f2f6a616930058101862
mechanik20051988 <mechanik20051988@tarantool.org>
2021-10-19 11:28:34 +0300

    iproto: fix crash if box.cfg listen is woken up.
    There was access to previously freed memory in case when `cbus_call`
    is interrupted: `cbus_call_msg` in iproto allocates on stack, and if
    `cbus_call` failed due to fiber cancelation or wake up, `cbus_call_msg`
    memory is released. But function called through cbus is still work in
    iproto thread and there will be an attempt to access this memory when
    this function in iproto thread finished it's work. This patch rework
    this behaviour, now before `cbus_call` we reset FIBER_IS_CANCELLABLE
    flag, to prevent fiber cancellation or it's wake up.
    
    Closes #6480
    
    (cherry picked from commit e7a9fd0b255ebf4485feec2841876cb4c667bd28)

---
+++ test/box/gh-6480-crash-if-box-cfg-listen-is-woken-up.test.lua
@@ -0,0 +1,8 @@
+test_run = require('test_run').new()
+fiber = require('fiber')
+
+old_listen = box.cfg.listen
+new_listen = old_listen .. "A"
+f = fiber.create(function() box.cfg{listen = new_listen} end) f:wakeup()
+test_run:wait_cond(function() return box.cfg.listen == new_listen end)
+box.cfg{listen = old_listen}
