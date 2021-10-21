8a23a337793fa4f0a179e5ac94dd7000f3867af2
VitaliyaIoffe <vitaioffe@yandex.ru>
2021-10-21 12:56:57 +0300

    Migrate quorum.test.lua
    Quorum.test.lua was migrated to luatest.
    It was divided into several tests.
    
    Delete quorum.test.lua, fix suite.ini in replication tests.
    
    Part of tarantool/test-run#304
    
    (cherry picked from commit c3e231d5f2e4d9033558f69d876a4e4149353d7b)

---
+++ test/replication-luatest/quorum_master_test.lua
@@ -0,0 +1,61 @@
+local t = require('luatest')
+local log = require('log')
+local Cluster =  require('test.luatest_helpers.cluster')
+local helpers = require('test.luatest_helpers')
+local json = require('json')
+
+local pg = t.group('quorum_master', {{engine = 'memtx'}, {engine = 'vinyl'}})
+
+pg.before_each(function(cg)
+    local engine = cg.params.engine
+    cg.cluster = Cluster:new({})
+
+    cg.box_cfg = {
+        replication = {
+            helpers.instance_uri('master_quorum', 1);
+            helpers.instance_uri('master_quorum', 2);
+        };
+        replication_connect_quorum = 0;
+        replication_timeout = 0.1;
+    }
+
+    cg.master_quorum1 = cg.cluster:build_server(
+        {alias = 'master_quorum1', engine = engine, box_cfg = cg.box_cfg})
+
+    cg.master_quorum2 = cg.cluster:build_server(
+        {alias = 'master_quorum2', engine = engine, box_cfg = cg.box_cfg})
+
+    pcall(log.cfg, {level = 6})
+
+end)
+
+pg.after_each(function(cg)
+    cg.cluster.servers = nil
+    cg.cluster:drop()
+end)
+
+pg.before_test('test_master_master_works', function(cg)
+    cg.cluster:add_server(cg.master_quorum1)
+    cg.cluster:add_server(cg.master_quorum2)
+    cg.cluster:start()
+    local bootstrap_function = function()
+        box.schema.space.create('test', {engine = os.getenv('TARANTOOL_ENGINE')})
+        box.space.test:create_index('primary')
+    end
+    cg.cluster:exec_on_leader(bootstrap_function)
+
+end)
+
+pg.after_test('test_master_master_works', function(cg)
+    cg.cluster:drop({cg.master_quorum1, cg.master_quorum2})
+end)
+
+pg.test_master_master_works = function(cg)
+    local repl = json.encode({replication = cg.box_cfg.replication})
+    cg.master_quorum1:eval('box.cfg{replication = ""}')
+    t.assert_equals(cg.master_quorum1:eval('return box.space.test:insert{1}'), {1})
+    cg.master_quorum1:eval(('box.cfg{replication = %s}'):format(repl.replication))
+    local vclock = cg.master_quorum1:eval('return box.info.vclock')
+    helpers:wait_vclock(cg.master_quorum2, vclock)
+    t.assert_equals(cg.master_quorum2:eval('return box.space.test:select()'), {{1}})
+end
---
+++ test/replication-luatest/quorum_orphan_test.lua
@@ -0,0 +1,120 @@
+local t = require('luatest')
+local log = require('log')
+local Cluster =  require('test.luatest_helpers.cluster')
+local asserts = require('test.luatest_helpers.asserts')
+local helpers = require('test.luatest_helpers')
+
+local pg = t.group('quorum_orphan', {{engine = 'memtx'}, {engine = 'vinyl'}})
+
+pg.before_each(function(cg)
+    local engine = cg.params.engine
+    cg.cluster = Cluster:new({})
+
+    local box_cfg = {
+        replication_timeout = 0.1;
+        replication_connect_timeout = 10;
+        replication_sync_lag = 0.01;
+        replication_connect_quorum = 3;
+        replication = {
+            helpers.instance_uri('quorum', 1);
+            helpers.instance_uri('quorum', 2);
+            helpers.instance_uri('quorum', 3);
+        };
+    }
+
+    cg.quorum1 = cg.cluster:build_server({alias = 'quorum1', engine = engine, box_cfg = box_cfg})
+    cg.quorum2 = cg.cluster:build_server({alias = 'quorum2', engine = engine, box_cfg = box_cfg})
+    cg.quorum3 = cg.cluster:build_server({alias = 'quorum3', engine = engine, box_cfg = box_cfg})
+
+    pcall(log.cfg, {level = 6})
+
+end)
+
+pg.after_each(function(cg)
+    cg.cluster.servers = nil
+    cg.cluster:drop()
+end)
+
+pg.before_test('test_replica_is_orphan_after_restart', function(cg)
+    cg.cluster:add_server(cg.quorum1)
+    cg.cluster:add_server(cg.quorum2)
+    cg.cluster:add_server(cg.quorum3)
+    cg.cluster:start()
+    local bootstrap_function = function()
+        box.schema.space.create('test', {engine = os.getenv('TARANTOOL_ENGINE')})
+        box.space.test:create_index('primary')
+    end
+   cg.cluster:exec_on_leader(bootstrap_function)
+
+end)
+
+pg.test_replica_is_orphan_after_restart = function(cg)
+    -- Stop one replica and try to restart another one.
+    -- It should successfully restart, but stay in the
+    -- 'orphan' mode, which disables write accesses.
+    -- There are three ways for the replica to leave the
+    -- 'orphan' mode:
+    -- * reconfigure replication
+    -- * reset box.cfg.replication_connect_quorum
+    -- * wait until a quorum is formed asynchronously
+    asserts:wait_fullmesh({cg.quorum1, cg.quorum2, cg.quorum3})
+    cg.quorum1:stop()
+    cg.quorum2:restart()
+    t.assert_equals(cg.quorum2.net_box.state, 'active')
+    t.assert_str_matches(cg.quorum2:eval('return box.info.status'), 'orphan')
+    t.assert_error_msg_content_equals('timed out', function()
+        cg.quorum2:eval('return box.ctl.wait_rw(0.001)')
+    end)
+    t.assert(cg.quorum2:eval('return box.info.ro'))
+    t.helpers.retrying({timeout = 30}, function()
+        t.assert(cg.quorum2:eval('return box.space.test ~= nil'))
+    end)
+    t.assert_error_msg_content_equals(
+        "Can't modify data because this instance is in read-only mode.",
+        function()
+            cg.quorum2:eval('return box.space.test:replace{100}')
+        end
+    )
+    cg.quorum2:eval('box.cfg{replication={}}')
+    t.assert_str_matches(
+        cg.quorum2:eval('return box.info.status'), 'running')
+    cg.quorum2:restart()
+    t.assert_equals(cg.quorum2.net_box.state, 'active')
+    t.assert_str_matches(
+        cg.quorum2:eval('return box.info.status'), 'orphan')
+    t.assert_error_msg_content_equals('timed out', function()
+        cg.quorum2:eval('return box.ctl.wait_rw(0.001)')
+    end)
+    t.assert(cg.quorum2:eval('return box.info.ro'))
+    t.assert(cg.quorum2:eval('return box.space.test ~= nil'))
+    t.assert_error_msg_content_equals(
+        "Can't modify data because this instance is in read-only mode.",
+        function()
+            cg.quorum2:eval('return box.space.test:replace{100}')
+        end
+    )
+
+    cg.quorum2:eval('box.cfg{replication_connect_quorum = 2}')
+    cg.quorum2:eval('return box.ctl.wait_rw()')
+    t.assert_not(cg.quorum2:eval('return box.info.ro'))
+    t.assert_str_matches(cg.quorum2:eval('return box.info.status'), 'running')
+    cg.quorum2:restart()
+    t.assert_equals(cg.quorum2.net_box.state, 'active')
+    t.assert_str_matches(cg.quorum2:eval('return box.info.status'), 'orphan')
+    t.assert_error_msg_content_equals('timed out', function()
+            cg.quorum2:eval('return box.ctl.wait_rw(0.001)')
+    end)
+    t.assert(cg.quorum2:eval('return box.info.ro'))
+    t.assert(cg.quorum2:eval('return box.space.test ~= nil'))
+    t.assert_error_msg_content_equals(
+        "Can't modify data because this instance is in read-only mode.",
+        function()
+            cg.quorum2:eval('return box.space.test:replace{100}')
+        end
+    )
+    cg.quorum1:start()
+    cg.quorum2:eval('return box.ctl.wait_rw()')
+    t.assert_not(cg.quorum2:eval('return box.info.ro'))
+    t.assert_str_matches(cg.quorum2:eval('return box.info.status'), 'running')
+
+end
