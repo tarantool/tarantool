3bc176623f19a662cdb022ae99c15cc8afd2bc53
Vladimir Davydov <vdavydov@tarantool.org>
2021-11-02 14:15:34 +0300

    test: fix flaky box-tap/gh-5602-environment-vars-cfg test
    The test is flaky, because it tries to listen on a fixed TCP port, which
    may be busy. Let's use Unix sockets to fix the test.
    
    While we are at it, let's also remove useless creation of a temporary
    directory - the test copies a script there, but never uses it.

--- test/box-tap/gh-5602-environment-cfg-test-cases.lua
+++ test/box-tap/gh-5602-environment-cfg-test-cases.lua
@@ -1,14 +1,19 @@
+local fio = require('fio')
 local tap = require('tap')
 
 local test = tap.test('gh-5602')
 
 local status, err = pcall(box.cfg, {background = false, vinyl_timeout = 70.1})
 
+local listen_sock = fio.pathjoin(fio.cwd(), 'listen.sock')
+local repl1_sock = fio.pathjoin(fio.cwd(), 'repl1.sock')
+local repl2_sock = fio.pathjoin(fio.cwd(), 'repl2.sock')
+
 -- Check that environment cfg values are set correctly.
 if arg[1] == '1' then
     test:plan(6)
     test:ok(status, 'box.cfg is successful')
-    test:is(box.cfg['listen'], '3301', 'listen')
+    test:is(box.cfg['listen'], listen_sock, 'listen')
     test:is(box.cfg['readahead'], 10000, 'readahead')
     test:is(box.cfg['strip_core'], false, 'strip_core')
     test:is(box.cfg['log_format'], 'json', 'log_format is not set')
@@ -17,13 +22,11 @@ end
 if arg[1] == '2' then
     test:plan(7)
     test:ok(status, 'box.cfg is successful')
-    test:is(box.cfg['listen'], '3301', 'listen')
+    test:is(box.cfg['listen'], listen_sock, 'listen')
     local replication = box.cfg['replication']
     test:is(type(replication), 'table', 'replication is table')
-    test:ok(replication[1] == '0.0.0.0:12345' or
-            replication[1] == '1.1.1.1:12345', 'replication URI 1')
-    test:ok(replication[2] == '0.0.0.0:12345' or
-            replication[2] == '1.1.1.1:12345', 'replication URI 2')
+    test:ok(replication[1] == repl1_sock, 'replication URI 1')
+    test:ok(replication[2] == repl2_sock, 'replication URI 2')
     test:is(box.cfg['replication_connect_timeout'], 0.01,
             'replication_connect_timeout')
     test:is(box.cfg['replication_synchro_quorum'], '4 + 1',
