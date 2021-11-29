442345626996d95bf1403888f0c702015ed8ce7d
Vladimir Davydov <vdavydov@tarantool.org>
2021-11-29 14:47:08 +0300

    net.box: add return_raw request option
    Closes #4861
    
    @TarantoolBot document
    Title: Document return_raw net.box option
    
    If the `return_raw` flag is set, net.box will return response data
    wrapped in a msgpack object (see `msgpack.object()`) instead of decoding
    it to Lua. This can be useful when a response is supposed to be passed
    through without decoding or with partial decoding - creating a msgpack
    object can reduce pressure on the Lua garbage collector in this case.
    
    Example:
    
    ```lua
    local c = require('net.box').connect(uri)
    local mp = c.eval('eval ...', {1, 2, 3}, {return_raw = true})
    mp:decode() -- {1, 2, 3}
    ```
    
    The option is ignored for methods that return nil (`begin`, `commit`,
    `rollback`, `upsert`, `prepare` will return nil even if `return_raw` is
    set) and for `index.count` (it returns a number). For `execute`, the
    option is applied only to data (`rows`) while metadata is decoded
    irrespective of the value of `return_raw`.

---
+++ changelogs/unreleased/gh-4861-net-box-return-raw.md
@@ -0,0 +1,4 @@
+## feature/lua/netbox
+
+* Added `return_raw` net.box option for returning `msgpack.object` instead of
+  decoding the response (gh-4861).
--- test/box-luatest/net_box_test.lua
+++ test/box-luatest/net_box_test.lua
@@ -74,3 +74,125 @@ g.test_msgpack_object_args = function()
 
     c:close()
 end
+
+g.before_test('test_msgpack_object_return', function()
+    g.server:eval('function echo(...) return ... end')
+    g.server:eval([[function echo_mp(...)
+        return require('msgpack').object({...})
+    end]])
+    g.server:exec(function()
+        box.schema.space.create(
+            'T', {format = {{'F1', 'unsigned'}, {'F2', 'string'}}})
+        box.space.T:create_index('PK')
+    end)
+end)
+
+g.after_test('test_msgpack_object_return', function()
+    g.server:eval('echo = nil')
+    g.server:eval('echo_mp = nil')
+    g.server:exec(function()
+        box.space.T:drop()
+    end)
+end)
+
+g.test_msgpack_object_return = function()
+    local c = net.connect(g.server.net_box_uri)
+    local opts = {return_raw = true}
+    local ret
+
+    -- eval
+    ret = c:eval('return ...', {}, opts)
+    t.assert(msgpack.is_object(ret))
+    t.assert_equals(ret:decode(), {})
+    ret = c:eval('return ...', {1, 2, 3}, opts)
+    t.assert(msgpack.is_object(ret))
+    t.assert_equals(ret:decode(), {1, 2, 3})
+    ret = c:eval("return require('msgpack').object({1, 2, 3})", {}, opts)
+    t.assert(msgpack.is_object(ret))
+    t.assert_equals(ret:decode(), {{1, 2, 3}})
+    -- sic: no return_raw
+    ret = c:eval("return require('msgpack').object({1, 2, 3})")
+    t.assert_not(msgpack.is_object(ret))
+    t.assert_equals(ret, {1, 2, 3})
+
+    -- call
+    ret = c:call('echo', {}, opts)
+    t.assert(msgpack.is_object(ret))
+    t.assert_equals(ret:decode(), {})
+    ret = c:call('echo', {1, 2, 3}, opts)
+    t.assert(msgpack.is_object(ret))
+    t.assert_equals(ret:decode(), {1, 2, 3})
+    ret = c:call('echo_mp', {1, 2, 3}, opts)
+    t.assert(msgpack.is_object(ret))
+    t.assert_equals(ret:decode(), {{1, 2, 3}})
+    -- sic: no return_raw
+    ret = c:call('echo_mp', {1, 2, 3})
+    t.assert_not(msgpack.is_object(ret))
+    t.assert_equals(ret, {1, 2, 3})
+
+    -- dml
+    ret = c.space.T:insert({1, 'foo'}, opts)
+    t.assert(msgpack.is_object(ret))
+    t.assert_equals(ret:decode(), {1, 'foo'})
+    ret = c.space.T:replace({1, 'bar'}, opts)
+    t.assert(msgpack.is_object(ret))
+    t.assert_equals(ret:decode(), {1, 'bar'})
+    ret = c.space.T:update({1}, {{'=', 2, 'baz'}}, opts)
+    t.assert(msgpack.is_object(ret))
+    t.assert_equals(ret:decode(), {1, 'baz'})
+    ret = c.space.T:select({1}, opts)
+    t.assert(msgpack.is_object(ret))
+    t.assert_equals(ret:decode(), {{1, 'baz'}})
+    ret = c.space.T:delete({1}, opts)
+    t.assert(msgpack.is_object(ret))
+    t.assert_equals(ret:decode(), {1, 'baz'})
+
+    -- min/max/get
+    c.space.T:insert({1, 'a'})
+    c.space.T:insert({2, 'b'})
+    ret = c.space.T.index.PK:min({}, opts)
+    t.assert(msgpack.is_object(ret))
+    t.assert_equals(ret:decode(), {1, 'a'})
+    ret = c.space.T.index.PK:max({}, opts)
+    t.assert(msgpack.is_object(ret))
+    t.assert_equals(ret:decode(), {2, 'b'})
+    ret = c.space.T.index.PK:get({2}, opts)
+    t.assert(msgpack.is_object(ret))
+    t.assert_equals(ret:decode(), {2, 'b'})
+
+    -- count (ignores return_raw)
+    ret = c.space.T.index.PK:count({}, opts)
+    t.assert_equals(ret, 2)
+
+    -- upsert (returns nil)
+    ret = c.space.T:upsert({1, 'a'}, {{'=', 2, 'a'}}, opts)
+    t.assert_equals(ret, nil)
+
+    -- begin/commit/rollback (returns nil)
+    local s = c:new_stream()
+    ret = s:begin(nil, opts)
+    t.assert_equals(ret, nil)
+    ret = s:commit(nil, opts)
+    t.assert_equals(ret, nil)
+    ret = s:rollback(nil, opts)
+    t.assert_equals(ret, nil)
+
+    -- sql (ignores return_raw except for rows)
+    local metadata = {
+        {name = 'F1', type = 'unsigned'},
+        {name = 'F2', type = 'string'},
+    }
+    ret = c:execute('SELECT * FROM T WHERE F1 < ?;', {2}, nil, opts)
+    t.assert(msgpack.is_object(ret.rows))
+    ret.rows = ret.rows:decode()
+    t.assert_equals(ret, {rows = {{1, 'a'}}, metadata = metadata})
+    ret = c:prepare('SELECT * FROM T WHERE F1 < ?;', nil, nil, opts)
+    local stmt_id = ret.stmt_id
+    ret.stmt_id = nil
+    t.assert_equals(ret, {metadata = metadata, param_count = 1,
+                          params = {{name = "?", type = "ANY"}}})
+    ret = c:unprepare(stmt_id, nil, nil, opts)
+    t.assert_equals(ret, nil)
+
+    c:close()
+end
