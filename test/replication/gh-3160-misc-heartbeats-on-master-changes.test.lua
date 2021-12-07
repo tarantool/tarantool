dd0520b6476652e63b29dba152a1ae86ff608bec
Serge Petrenko <sergepetrenko@tarantool.org>
2021-12-07 16:43:08 +0300

    replication: fix flaky gh-3160-misc... test
    The test fails quite often with one of the following results.
    
    Either
    
    [001] @@ -60,7 +60,7 @@
    [001]  ...
    [001]  test_timeout()
    [001]  ---
    [001] -- true
    [001] +- false
    [001]  ...
    [001]  test_run:cmd("switch default")
    [001]  ---
    [001]
    
    Or
    
    [034]  test_timeout()
    [034]  ---
    [034] -- true
    [034] +- error: 'replicas are not in the follow status'
    [034]  ...
    
    Both errors are caused by wait_cond checking saved
    `box.info.replication` values instead of actual `box.info.replication`
    output. Fix this.
    
    Also, the test's quite long for no reason.
    
    The wait_cond waiting for replication to break lasts a whole
    `replication_timeout`, which is excess.
    
    The test's idea, as stated in the commit that has introduced it
    (195d4462: Send relay heartbeat if wal changes won't be send) is to
    constantly relay some data to the remote peers and make
    it so their relays never send heartbeats.
    
    If we wait for a whole replication timeout between inserts, there's a high
    chance of peer relays waking up naturally (after replication_timeout passes).
    
    In this case the test tests nothing. Fix the issue by waiting for ~ 1/5
    of replication_timeout in between the despatches. This also reduces test
    run time from ~4.5 to ~1.5 seconds on my machine.
    
    Remove the test from fragile list, since it shouldn't be flaky anymore.
    
    Closes #4940
    
    (cherry picked from commit 8c187df5bd0522dee7bbbd6c3a621e0f30b5e7c4)

--- test/replication/gh-3160-misc-heartbeats-on-master-changes.test.lua
+++ test/replication/gh-3160-misc-heartbeats-on-master-changes.test.lua
@@ -10,21 +10,27 @@ test_run:cmd("switch autobootstrap3")
 test_run = require('test_run').new()
 _ = box.schema.space.create('test_timeout'):create_index('pk')
 test_run:cmd("setopt delimiter ';'")
-function wait_not_follow(replicaA, replicaB)
+
+local function replica(id)
+    return box.info.replication[id].upstream
+end
+
+function wait_not_follow(id_a, id_b)
     return test_run:wait_cond(function()
-        return replicaA.status ~= 'follow' or replicaB.status ~= 'follow'
-    end, box.cfg.replication_timeout)
+        return replica(id_a).status ~= 'follow' or
+               replica(id_b).status ~= 'follow'
+    end, box.cfg.replication_timeout / 5)
 end;
+
 function test_timeout()
-    local replicaA = box.info.replication[1].upstream or box.info.replication[2].upstream
-    local replicaB = box.info.replication[3].upstream or box.info.replication[2].upstream
-    local follows = test_run:wait_cond(function()
-        return replicaA.status == 'follow' or replicaB.status == 'follow'
-    end)
+    local id_a = box.info.id % 3 + 1
+    local id_b = id_a % 3 + 1
+    local follows = test_run:wait_upstream(id_a, {status = 'follow'})
+    follows = follows and test_run:wait_upstream(id_b, {status = 'follow'})
     if not follows then error('replicas are not in the follow status') end
     for i = 0, 99 do
         box.space.test_timeout:replace({1})
-        if wait_not_follow(replicaA, replicaB) then
+        if wait_not_follow(id_a, id_b) then
             require('log').error(box.info.replication)
             return false
         end
