ead890a51c7daed262bb6681a353ade895514146
Sergey Bronnikov <sergeyb@tarantool.org>
2022-01-12 15:06:23 +0300

    test/fuzz: add fuzzing tests for IPROTO decoders
    Examples of IPROTO decoding issues: #3900, #1928, #6781.
    Patch adds a number of fuzzing tests that covers IPROTO decoding:
    
    - xrow_decode_auth
    - xrow_decode_begin
    - xrow_decode_call
    - xrow_decode_dml
    - xrow_decode_id
    - xrow_decode_raft
    - xrow_decode_sql
    - xrow_decode_watch
    - xrow_greeting_decode
    
    NO_DOC=testing
    NO_CHANGELOG=testing
    
    (cherry picked from commit 46cacf35bd6454102a6f8e3e47281f66cc3fdd8c)

--- src/box/xrow.c
+++ src/box/xrow.c
@@ -1249,7 +1249,8 @@ int
 xrow_decode_raft(const struct xrow_header *row, struct raft_request *r,
 		 struct vclock *vclock)
 {
-	assert(row->type == IPROTO_RAFT);
+	if (row->type != IPROTO_RAFT)
+		goto bad_msgpack;
 	if (row->bodycnt != 1 || row->group_id != GROUP_LOCAL) {
 		diag_set(ClientError, ER_INVALID_MSGPACK,
 			 "malformed raft request");
@@ -1595,7 +1596,8 @@ error:
 int
 xrow_decode_begin(const struct xrow_header *row, struct begin_request *request)
 {
-	assert(row->type == IPROTO_BEGIN);
+	if (row->type != IPROTO_BEGIN)
+		goto bad_msgpack;
 	memset(request, 0, sizeof(*request));
 
 	/** Request without extra options. */
