cacef661ba59814cb9b25dda6644fa790d7f62a5
Sergey Bronnikov <sergeyb@tarantool.org>
2022-01-14 16:22:44 +0300

    test: add new fuzzers for parsing datetime
    Part of #6731
    
    NO_DOC=internal
    NO_CHANGELOG=internal

---
+++ test/fuzz/datetime_parse_full_fuzzer.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include "datetime.h"
+#include "trivia/util.h"
+
+int
+LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+	char *buf = xcalloc(size + 1, sizeof(char));
+	if (buf == NULL)
+		return 0;
+	memcpy(buf, data, size);
+	buf[size] = '\0';
+	struct datetime date_expected;
+	datetime_parse_full(&date_expected, buf, size, 0);
+	free(buf);
+
+	return 0;
+}
