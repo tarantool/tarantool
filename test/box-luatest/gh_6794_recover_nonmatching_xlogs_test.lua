0a34eb3f842d7a3d986a7c98d9489fa52d054eec
Serge Petrenko <sergepetrenko@tarantool.org>
2022-01-26 11:37:17 +0300

    recovery: fix recovering from 1.6 xlogs/snaps
    Tarantool 1.6 didn't rotate xlogs on snap creation.
    This means, that Tarantool 1.6 data dir may contain files like
    0...0.xlog and 0...01.snap, where 0...0.xlog covers changes both before
    and after the snap creation.
    
    New Tarantool versions fail to recover from such files: they recover the
    data correctly, but fail to update the vclock accordingly.
    
    Let's allow recovering from such files and update the vclock
    accordingly, but only when force_recovery is set.
    
    Patch 634f59c7 ("recovery: panic in case of recovery and replicaset
    vclock mismatch") added the panic, which helped to discover the issue.
    Before that patch Tarantool would silently recover with an outdated
    vclock.
    
    Also, while we're at it, refactor the check leading to a panic: it never
    happens after hot_standby, so move it into "else" branch of hot_standby.
    
    Closes #6794

---
+++ test/box-luatest/gh_6794_recover_nonmatching_xlogs_test.lua
@@ -0,0 +1,44 @@
+local server = require('test.luatest_helpers.server')
+local t = require('luatest')
+local g = t.group()
+local fio = require('fio')
+
+g.before_each(function()
+    g.server = server:new({alias = 'master'})
+    -- Initialize server's workdir with specially crafted snap/xlog pair.
+    fio.copytree('./test/box-luatest/gh_6794_data', g.server.workdir)
+end)
+
+g.after_test("test_panic_without_force_recovery", function()
+    g.server:cleanup()
+end)
+
+g.after_test("test_ignore_with_force_recovery", function()
+    g.server:drop()
+end)
+
+local mismatch_msg = "Replicaset vclock {.*} doesn't match recovered data {.*}"
+
+g.test_panic_without_force_recovery = function()
+    g.server:start({wait_for_readiness = false})
+    t.helpers.retrying({}, function()
+        local msg = "Can't proceed. " .. mismatch_msg
+        local filename = fio.pathjoin(g.server.workdir, g.server.alias..'.log')
+        t.assert(g.server:grep_log(msg, nil, {filename = filename}))
+    end)
+end
+
+g.test_ingore_with_force_recovery = function()
+    g.server.box_cfg = {force_recovery = true}
+    g.server:start()
+    t.helpers.retrying({}, function()
+        local msg = mismatch_msg .. ": ignoring, because 'force_recovery' "..
+                    "configuration option is set."
+        t.assert(g.server:grep_log(msg))
+    end)
+    t.assert(g.server:exec(function()
+                return box.info.signature >= 2 and
+                       box.info.status == 'running' and
+                       box.info.ro == false end),
+             "Failed to recover data")
+end
