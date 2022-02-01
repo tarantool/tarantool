2f5099a8c114de2388086addb5ad2f4de750a680
Vladimir Davydov <vdavydov@tarantool.org>
2022-02-01 15:18:12 +0300

    net.box: don't block close if called from state machine fiber
    Currently, to resubscribe registered watchers net.box installs an
    on_connect trigger that writes IPROTO_WATCH requests to the send buffer.
    The requests will be sent by the net.box state machine once all
    on_connect triggers return. The problem is this trigger may run before a
    user-defined trigger that closes the connection, in which case close()
    will hang forever, because since #6338, the close method blocks until
    the send buffer is emptied, while it can't be emptied in case close is
    called by the state machine itself (see #5358).
    
    Let's fix this issue by making close() omit blocking if called from
    the state machine fiber.
    
    No user is affected, because IPROTO_WATCH hasn't been officially
    released yet so neither changelog nor doc is required.
    
    Closes #6824
    
    NO_CHANGELOG=bug not released
    NO_DOC=NO_CHANGELOG

---
+++ test/box-luatest/gh_6824_net_box_close_on_connect_after_watch_test.lua
@@ -0,0 +1,33 @@
+local net = require('net.box')
+local server = require('test.luatest_helpers.server')
+local t = require('luatest')
+local g = t.group()
+
+g.before_all = function()
+    g.server = server:new({alias = 'master'})
+    g.server:start()
+end
+
+g.after_all = function()
+    g.server:drop()
+end
+
+-- Checks that close() doesn't hang if called from on_connect trigger
+-- of a connection that has watchers.
+g.test_close_on_connect_after_watch = function()
+    local conn = net.connect(g.server.net_box_uri, {
+        user = 'test',
+        password = 'test',
+        wait_connected = false,
+        reconnect_after = 0.01,
+    })
+    conn:watch('foo', function() end)
+    conn:on_connect(function(conn)
+        conn:close()
+    end)
+    g.server:exec(function()
+        box.schema.user.create('test')
+        box.schema.user.passwd('test', 'test')
+    end)
+    t.assert(conn:wait_state('closed', 10))
+end
