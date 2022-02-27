f7edb0f4dd48b4a7c29c3176557f4e6f539066fd
Timur Safin <tsafin@tarantool.org>
2022-02-27 16:48:43 +0300

    temp - massage timestamp test

--- test/sql/dt-timestamp.test.sql
+++ test/sql/dt-timestamp.test.sql
@@ -2,7 +2,7 @@
 -- TIMESTAMP
 --
 
-CREATE TABLE TIMESTAMP_TBL (d1 timestamp(2) without time zone);
+CREATE TABLE TIMESTAMP_TBL (d1 timestamp(2) without time zone primary key);
 
 -- Test shorthand input values
 -- We can't just "select" the results since they aren't constants; test for
@@ -10,20 +10,20 @@ CREATE TABLE TIMESTAMP_TBL (d1 timestamp(2) without time zone);
 -- block, within which the value of 'now' shouldn't change, and so these
 -- related values shouldn't either.
 
-BEGIN;
+--BEGIN;
 
-INSERT INTO TIMESTAMP_TBL VALUES ('today');
-INSERT INTO TIMESTAMP_TBL VALUES ('yesterday');
-INSERT INTO TIMESTAMP_TBL VALUES ('tomorrow');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'today');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'yesterday');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'tomorrow');
 -- time zone should be ignored by this data type
-INSERT INTO TIMESTAMP_TBL VALUES ('tomorrow EST');
-INSERT INTO TIMESTAMP_TBL VALUES ('tomorrow zulu');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'tomorrow EST');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'tomorrow zulu');
 
 SELECT count(*) AS One FROM TIMESTAMP_TBL WHERE d1 = timestamp without time zone 'today';
 SELECT count(*) AS Three FROM TIMESTAMP_TBL WHERE d1 = timestamp without time zone 'tomorrow';
 SELECT count(*) AS One FROM TIMESTAMP_TBL WHERE d1 = timestamp without time zone 'yesterday';
 
-COMMIT;
+--COMMIT;
 
 DELETE FROM TIMESTAMP_TBL;
 
@@ -31,117 +31,123 @@ DELETE FROM TIMESTAMP_TBL;
 -- and that it doesn't change over the same interval within a transaction block
 
 INSERT INTO TIMESTAMP_TBL VALUES ('now');
-SELECT pg_sleep(0.1);
+--SELECT pg_sleep(0.1);
 
-BEGIN;
+--BEGIN;
 INSERT INTO TIMESTAMP_TBL VALUES ('now');
-SELECT pg_sleep(0.1);
+--SELECT pg_sleep(0.1);
 INSERT INTO TIMESTAMP_TBL VALUES ('now');
-SELECT pg_sleep(0.1);
+--SELECT pg_sleep(0.1);
 SELECT count(*) AS two FROM TIMESTAMP_TBL WHERE d1 = timestamp(2) without time zone 'now';
 SELECT count(d1) AS three, count(DISTINCT d1) AS two FROM TIMESTAMP_TBL;
-COMMIT;
+--COMMIT;
 
-TRUNCATE TIMESTAMP_TBL;
+TRUNCATE table TIMESTAMP_TBL;
 
 -- Special values
-INSERT INTO TIMESTAMP_TBL VALUES ('-infinity');
-INSERT INTO TIMESTAMP_TBL VALUES ('infinity');
-INSERT INTO TIMESTAMP_TBL VALUES ('epoch');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '-infinity');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'infinity');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'epoch');
 
 -- Postgres v6.0 standard output format
-INSERT INTO TIMESTAMP_TBL VALUES ('Mon Feb 10 17:32:01 1997 PST');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Mon Feb 10 17:32:01 1997 PST');
 
 -- Variations on Postgres v6.1 standard output format
-INSERT INTO TIMESTAMP_TBL VALUES ('Mon Feb 10 17:32:01.000001 1997 PST');
-INSERT INTO TIMESTAMP_TBL VALUES ('Mon Feb 10 17:32:01.999999 1997 PST');
-INSERT INTO TIMESTAMP_TBL VALUES ('Mon Feb 10 17:32:01.4 1997 PST');
-INSERT INTO TIMESTAMP_TBL VALUES ('Mon Feb 10 17:32:01.5 1997 PST');
-INSERT INTO TIMESTAMP_TBL VALUES ('Mon Feb 10 17:32:01.6 1997 PST');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Mon Feb 10 17:32:01.000001 1997 PST');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Mon Feb 10 17:32:01.999999 1997 PST');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Mon Feb 10 17:32:01.4 1997 PST');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Mon Feb 10 17:32:01.5 1997 PST');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Mon Feb 10 17:32:01.6 1997 PST');
 
 -- ISO 8601 format
-INSERT INTO TIMESTAMP_TBL VALUES ('1997-01-02');
-INSERT INTO TIMESTAMP_TBL VALUES ('1997-01-02 03:04:05');
-INSERT INTO TIMESTAMP_TBL VALUES ('1997-02-10 17:32:01-08');
-INSERT INTO TIMESTAMP_TBL VALUES ('1997-02-10 17:32:01-0800');
-INSERT INTO TIMESTAMP_TBL VALUES ('1997-02-10 17:32:01 -08:00');
-INSERT INTO TIMESTAMP_TBL VALUES ('19970210 173201 -0800');
-INSERT INTO TIMESTAMP_TBL VALUES ('1997-06-10 17:32:01 -07:00');
-INSERT INTO TIMESTAMP_TBL VALUES ('2001-09-22T18:19:20');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '1997-01-02');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '1997-01-02 03:04:05');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '1997-02-10 17:32:01-08');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '1997-02-10 17:32:01-0800');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '1997-02-10 17:32:01 -08:00');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '19970210 173201 -0800');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '1997-06-10 17:32:01 -07:00');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '2001-09-22T18:19:20');
 
 -- POSIX format (note that the timezone abbrev is just decoration here)
-INSERT INTO TIMESTAMP_TBL VALUES ('2000-03-15 08:14:01 GMT+8');
-INSERT INTO TIMESTAMP_TBL VALUES ('2000-03-15 13:14:02 GMT-1');
-INSERT INTO TIMESTAMP_TBL VALUES ('2000-03-15 12:14:03 GMT-2');
-INSERT INTO TIMESTAMP_TBL VALUES ('2000-03-15 03:14:04 PST+8');
-INSERT INTO TIMESTAMP_TBL VALUES ('2000-03-15 02:14:05 MST+7:00');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '2000-03-15 08:14:01 GMT+8');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '2000-03-15 13:14:02 GMT-1');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '2000-03-15 12:14:03 GMT-2');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '2000-03-15 03:14:04 PST+8');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '2000-03-15 02:14:05 MST+7:00');
 
 -- Variations for acceptable input formats
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 10 17:32:01 1997 -0800');
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 10 17:32:01 1997');
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 10 5:32PM 1997');
-INSERT INTO TIMESTAMP_TBL VALUES ('1997/02/10 17:32:01-0800');
-INSERT INTO TIMESTAMP_TBL VALUES ('1997-02-10 17:32:01 PST');
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb-10-1997 17:32:01 PST');
-INSERT INTO TIMESTAMP_TBL VALUES ('02-10-1997 17:32:01 PST');
-INSERT INTO TIMESTAMP_TBL VALUES ('19970210 173201 PST');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 10 17:32:01 1997 -0800');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 10 17:32:01 1997');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 10 5:32PM 1997');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '1997/02/10 17:32:01-0800');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '1997-02-10 17:32:01 PST');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb-10-1997 17:32:01 PST');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '02-10-1997 17:32:01 PST');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '19970210 173201 PST');
 set datestyle to ymd;
-INSERT INTO TIMESTAMP_TBL VALUES ('97FEB10 5:32:01PM UTC');
-INSERT INTO TIMESTAMP_TBL VALUES ('97/02/10 17:32:01 UTC');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '97FEB10 5:32:01PM UTC');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '97/02/10 17:32:01 UTC');
 reset datestyle;
-INSERT INTO TIMESTAMP_TBL VALUES ('1997.041 17:32:01 UTC');
-INSERT INTO TIMESTAMP_TBL VALUES ('19970210 173201 America/New_York');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '1997.041 17:32:01 UTC');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '19970210 173201 America/New_York');
 -- this fails (even though TZ is a no-op, we still look it up)
-INSERT INTO TIMESTAMP_TBL VALUES ('19970710 173201 America/Does_not_exist');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '19970710 173201 America/Does_not_exist');
 
 -- Check date conversion and date arithmetic
-INSERT INTO TIMESTAMP_TBL VALUES ('1997-06-10 18:32:01 PDT');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp '1997-06-10 18:32:01 PDT');
 
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 10 17:32:01 1997');
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 11 17:32:01 1997');
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 12 17:32:01 1997');
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 13 17:32:01 1997');
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 14 17:32:01 1997');
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 15 17:32:01 1997');
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 16 17:32:01 1997');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 10 17:32:01 1997');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 11 17:32:01 1997');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 12 17:32:01 1997');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 13 17:32:01 1997');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 14 17:32:01 1997');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 15 17:32:01 1997');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 16 17:32:01 1997');
 
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 16 17:32:01 0097 BC');
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 16 17:32:01 0097');
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 16 17:32:01 0597');
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 16 17:32:01 1097');
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 16 17:32:01 1697');
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 16 17:32:01 1797');
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 16 17:32:01 1897');
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 16 17:32:01 1997');
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 16 17:32:01 2097');
+--INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 16 17:32:01 0097 BC');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 16 17:32:01 -0097');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 16 17:32:01 0097');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 16 17:32:01 0597');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 16 17:32:01 1097');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 16 17:32:01 1697');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 16 17:32:01 1797');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 16 17:32:01 1897');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 16 17:32:01 1997');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 16 17:32:01 2097');
 
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 28 17:32:01 1996');
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 29 17:32:01 1996');
-INSERT INTO TIMESTAMP_TBL VALUES ('Mar 01 17:32:01 1996');
-INSERT INTO TIMESTAMP_TBL VALUES ('Dec 30 17:32:01 1996');
-INSERT INTO TIMESTAMP_TBL VALUES ('Dec 31 17:32:01 1996');
-INSERT INTO TIMESTAMP_TBL VALUES ('Jan 01 17:32:01 1997');
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 28 17:32:01 1997');
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 29 17:32:01 1997');
-INSERT INTO TIMESTAMP_TBL VALUES ('Mar 01 17:32:01 1997');
-INSERT INTO TIMESTAMP_TBL VALUES ('Dec 30 17:32:01 1997');
-INSERT INTO TIMESTAMP_TBL VALUES ('Dec 31 17:32:01 1997');
-INSERT INTO TIMESTAMP_TBL VALUES ('Dec 31 17:32:01 1999');
-INSERT INTO TIMESTAMP_TBL VALUES ('Jan 01 17:32:01 2000');
-INSERT INTO TIMESTAMP_TBL VALUES ('Dec 31 17:32:01 2000');
-INSERT INTO TIMESTAMP_TBL VALUES ('Jan 01 17:32:01 2001');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 28 17:32:01 1996');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 29 17:32:01 1996');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Mar 01 17:32:01 1996');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Dec 30 17:32:01 1996');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Dec 31 17:32:01 1996');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Jan 01 17:32:01 1997');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 28 17:32:01 1997');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 29 17:32:01 1997');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Mar 01 17:32:01 1997');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Dec 30 17:32:01 1997');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Dec 31 17:32:01 1997');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Dec 31 17:32:01 1999');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Jan 01 17:32:01 2000');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Dec 31 17:32:01 2000');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Jan 01 17:32:01 2001');
 
 -- Currently unsupported syntax and ranges
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 16 17:32:01 -0097');
-INSERT INTO TIMESTAMP_TBL VALUES ('Feb 16 17:32:01 5097 BC');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 16 17:32:01 -0097');
+--INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 16 17:32:01 5097 BC');
+INSERT INTO TIMESTAMP_TBL VALUES (timestamp 'Feb 16 17:32:01 -5097');
 
 SELECT d1 FROM TIMESTAMP_TBL;
 
 -- Check behavior at the boundaries of the timestamp range
-SELECT '4714-11-24 00:00:00 BC'::timestamp;
-SELECT '4714-11-23 23:59:59 BC'::timestamp;  -- out of range
-SELECT '294276-12-31 23:59:59'::timestamp;
-SELECT '294277-01-01 00:00:00'::timestamp;  -- out of range
+--SELECT 4714-11-24 00:00:00 BC'::timestamp;
+SELECT timestamp '-4714-11-24 00:00:00';
+--SELECT '4714-11-23 23:59:59 BC'::timestamp;  -- out of range
+SELECT timestamp '-4714-11-23 23:59:59';  -- out of range
+--SELECT '294276-12-31 23:59:59'::timestamp;
+SELECT timestamp '294276-12-31 23:59:59';
+--SELECT '294277-01-01 00:00:00'::timestamp;  -- out of range
+SELECT timestamp '294277-01-01 00:00:00';  -- out of range
 
 -- Demonstrate functions and operators
 SELECT d1 FROM TIMESTAMP_TBL
@@ -172,8 +178,9 @@ SELECT date_trunc( 'week', timestamp '2004-02-29 15:44:17.71393' ) AS week_trunc
 -- case 1: AD dates, origin < input
 SELECT
   str,
-  interval,
-  date_trunc(str, ts) = date_bin(interval::interval, ts, timestamp '2001-01-01') AS equal
+  --interval,
+  --date_trunc(str, ts) = date_bin(interval::interval, ts, timestamp '2001-01-01') AS equal
+  interval
 FROM (
   VALUES
   ('week', '7 d'),
@@ -189,8 +196,9 @@ FROM (
 -- case 2: BC dates, origin < input
 SELECT
   str,
-  interval,
-  date_trunc(str, ts) = date_bin(interval::interval, ts, timestamp '2000-01-01 BC') AS equal
+  --interval,
+  --date_trunc(str, ts) = date_bin(interval::interval, ts, timestamp '2000-01-01 BC') AS equal
+  interval
 FROM (
   VALUES
   ('week', '7 d'),
@@ -206,8 +214,9 @@ FROM (
 -- case 3: AD dates, origin > input
 SELECT
   str,
-  interval,
-  date_trunc(str, ts) = date_bin(interval::interval, ts, timestamp '2020-03-02') AS equal
+  --interval,
+  --date_trunc(str, ts) = date_bin(interval::interval, ts, timestamp '2020-03-02') AS equal
+  interval
 FROM (
   VALUES
   ('week', '7 d'),
@@ -223,8 +232,9 @@ FROM (
 -- case 4: BC dates, origin > input
 SELECT
   str,
-  interval,
-  date_trunc(str, ts) = date_bin(interval::interval, ts, timestamp '0055-06-17 BC') AS equal
+  --interval,
+  --date_trunc(str, ts) = date_bin(interval::interval, ts, timestamp '0055-06-17 BC') AS equal
+  interval
 FROM (
   VALUES
   ('week', '7 d'),
@@ -257,17 +267,22 @@ FROM (
 (VALUES (timestamp '2001-01-01')) origin (origin);
 
 -- shift bins using the origin parameter:
-SELECT date_bin('5 min'::interval, timestamp '2020-02-01 01:01:01', timestamp '2020-02-01 00:02:30');
+--SELECT date_bin('5 min'::interval, timestamp '2020-02-01 01:01:01', timestamp '2020-02-01 00:02:30');
+SELECT date_bin(interval '5 min', timestamp '2020-02-01 01:01:01', timestamp '2020-02-01 00:02:30');
 
 -- disallow intervals with months or years
-SELECT date_bin('5 months'::interval, timestamp '2020-02-01 01:01:01', timestamp '2001-01-01');
-SELECT date_bin('5 years'::interval,  timestamp '2020-02-01 01:01:01', timestamp '2001-01-01');
+--SELECT date_bin('5 months'::interval, timestamp '2020-02-01 01:01:01', timestamp '2001-01-01');
+--SELECT date_bin('5 years'::interval,  timestamp '2020-02-01 01:01:01', timestamp '2001-01-01');
+SELECT date_bin(interval '5 months', timestamp '2020-02-01 01:01:01', timestamp '2001-01-01');
+SELECT date_bin(interval '5 years',  timestamp '2020-02-01 01:01:01', timestamp '2001-01-01');
 
 -- disallow zero intervals
-SELECT date_bin('0 days'::interval, timestamp '1970-01-01 01:00:00' , timestamp '1970-01-01 00:00:00');
+--SELECT date_bin('0 days'::interval, timestamp '1970-01-01 01:00:00' , timestamp '1970-01-01 00:00:00');
+SELECT date_bin(interval '0 days', timestamp '1970-01-01 01:00:00' , timestamp '1970-01-01 00:00:00');
 
 -- disallow negative intervals
-SELECT date_bin('-2 days'::interval, timestamp '1970-01-01 01:00:00' , timestamp '1970-01-01 00:00:00');
+--SELECT date_bin('-2 days'::interval, timestamp '1970-01-01 01:00:00' , timestamp '1970-01-01 00:00:00');
+SELECT date_bin(interval '-2 days', timestamp '1970-01-01 01:00:00' , timestamp '1970-01-01 00:00:00');
 
 -- Test casting within a BETWEEN qualifier
 SELECT d1 - timestamp without time zone '1997-01-02' AS diff
@@ -312,10 +327,13 @@ SELECT d1 as "timestamp",
    FROM TIMESTAMP_TBL;
 
 -- value near upper bound uses special case in code
-SELECT date_part('epoch', '294270-01-01 00:00:00'::timestamp);
-SELECT extract(epoch from '294270-01-01 00:00:00'::timestamp);
+--SELECT date_part('epoch', '294270-01-01 00:00:00'::timestamp);
+SELECT date_part('epoch', timestamp '294270-01-01 00:00:00');
+--SELECT extract(epoch from '294270-01-01 00:00:00'::timestamp);
+SELECT extract(epoch from timestamp '294270-01-01 00:00:00');
 -- another internal overflow test case
-SELECT extract(epoch from '5000-01-01 00:00:00'::timestamp);
+--SELECT extract(epoch from '5000-01-01 00:00:00'::timestamp);
+SELECT extract(epoch from timestamp '5000-01-01 00:00:00');
 
 -- TO_CHAR()
 SELECT to_char(d1, 'DAY Day day DY Dy dy MONTH Month month RM MON Mon mon')
@@ -353,7 +371,8 @@ SELECT to_char(d1, 'FMIYYY FMIYY FMIY FMI FMIW FMIDDD FMID')
 
 SELECT to_char(d, 'FF1 FF2 FF3 FF4 FF5 FF6  ff1 ff2 ff3 ff4 ff5 ff6  MS US')
    FROM (VALUES
-       ('2018-11-02 12:34:56'::timestamp),
+       --('2018-11-02 12:34:56'::timestamp),
+       (timestamp '2018-11-02 12:34:56'),
        ('2018-11-02 12:34:56.78'),
        ('2018-11-02 12:34:56.78901'),
        ('2018-11-02 12:34:56.78901234')
