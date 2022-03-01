90faa5d0b3ce1600f63127b47c032b12c54bac12
Vladimir Davydov <vdavydov@tarantool.org>
2022-03-01 13:22:34 +0300

    test: fix engine/gh-5093-before_replace-key-type
    1.10 doesn't have decimal. Use number instead.
    
    Fixes commit 8e40047064a231d6e7065efdb4be179ef5b774e4
    ("engine: validate key in space_before_replace").

--- test/engine/gh-5093-before_replace-key-type.test.lua
+++ test/engine/gh-5093-before_replace-key-type.test.lua
@@ -2,7 +2,7 @@ test_run = require('test_run').new()
 trigger = function() end
 
 s = box.schema.space.create('gh-5093', {engine=test_run:get_cfg('engine')})
-_ = s:create_index('value', {parts={{1, type='decimal'}}})
+_ = s:create_index('value', {parts={{1, type='number'}}})
 _ = s:before_replace(trigger)
 s:delete('1111.1111')
 s:drop()
