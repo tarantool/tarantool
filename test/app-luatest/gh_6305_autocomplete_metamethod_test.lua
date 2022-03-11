2db9f5a5b053b8b1135425da6c417dbe50b7a85c
Sergey Ostanevich <sergos@tarantool.org>
2022-03-11 17:21:09 +0300

    console: introduce __autocomplete metamethod
    This patch updates the logic in lua_rl_complete() where __index from
    a metatable apparently expected to be a table, not a function. It is
    rather simple for a table object, but some of them are userdata, so
    an __autocomplete method is introduced to be sure it returns a table
    with all names suitable for tabcompletion.
    
    Part of #6305
    NO_DOC=internal
    NO_CHANGELOG=part of a fix

---
+++ test/app-luatest/gh_6305_autocomplete_metamethod_test.lua
@@ -0,0 +1,60 @@
+local t = require('luatest')
+local g = t.group()
+local console = require('console')
+
+local function tabcomplete(s)
+    return console.completion_handler(s, 0, #s)
+end
+
+g.test_no_metatable = function()
+    local tab = {first = true, second = true}
+    rawset(_G, 'table11', tab)
+    t.assert_items_equals(tabcomplete('table1'), {'table11', 'table11'})
+    t.assert_items_equals(tabcomplete('table11.'), {'table11.',
+                                                        'table11.first',
+                                                        'table11.second',
+                                                        })
+end
+
+g.test__index = function()
+    local tab = {first = true, second = true}
+    rawset(_G, 'table11', tab)
+    setmetatable(tab, {__index = {index = true}})
+    t.assert_items_equals(tabcomplete('table11.'), {'table11.',
+                                                    'table11.first',
+                                                    'table11.second',
+                                                    'table11.index',
+                                                    })
+end
+
+g.test__autocomplete = function()
+    local tab = {first = true, second = true}
+    rawset(_G, 'table11', tab)
+    -- __autocomplete should always be a function, no extra completions
+    setmetatable(tab, {__autocomplete = {auto = true}})
+   t.assert_items_equals(tabcomplete('table11.'), {'table11.',
+                                                   'table11.first',
+                                                   'table11.second',
+                                                   })
+   -- __autocomplete function can throw an error - should be Ok
+   setmetatable(tab, {__autocomplete = function() error('test error') end})
+   t.assert_items_equals(tabcomplete('table11.'), {'table11.',
+                                                   'table11.first',
+                                                   'table11.second',
+                                                   })
+   -- the right way - should add 'auto' comletion
+   setmetatable(tab, {__autocomplete = function() return {auto = true} end})
+   t.assert_items_equals(tabcomplete('table11.'), {'table11.',
+                                                   'table11.first',
+                                                   'table11.second',
+                                                   'table11.auto',
+                                                   })
+   -- __autocomplete supercedes __index, no completions from the latter
+   setmetatable(tab, {__autocomplete = function() return {auto = true} end,
+                      __index = {index = true}})
+   t.assert_items_equals(tabcomplete('table11.'), {'table11.',
+                                                   'table11.first',
+                                                   'table11.second',
+                                                   'table11.auto',
+                                                   })
+end
