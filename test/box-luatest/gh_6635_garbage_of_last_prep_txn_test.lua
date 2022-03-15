b55b4981adf7550354e11065a7c92a1c54727bf1
Andrey Saranchin <Andrey22102001@gmail.com>
2022-03-15 15:01:01 +0300

    txm: relax conditions for memtx_story delete
    Since a transaction has been prepared its garbage (produced stories
    and replaced tuples) cannot be deleted because they are recognized
    as used in read-view even if there are no transactions in read-view.
    This problem makes it difficult to test the memory monitoring system,
    so this patch solves the problem.
    
    Close #6635
    Part of #6150
    
    NO_DOC=no visible changes
    NO_CHANGELOG=no visible changes

---
+++ test/box-luatest/gh_6635_garbage_of_last_prep_txn_test.lua
@@ -0,0 +1,41 @@
+local server = require('test.luatest_helpers.server')
+local t = require('luatest')
+
+local g = t.group()
+
+g.before_all = function()
+    g.server = server:new{
+        alias   = 'default',
+        box_cfg = {memtx_use_mvcc_engine = true}
+    }
+    g.server:start()
+end
+
+g.after_all = function()
+    g.server:drop()
+end
+
+g.test_garbage_of_last_prepared_txn_cannot_be_deleted = function()
+    g.server:exec(function()
+        local t = require('luatest')
+
+        local s = box.schema.create_space('test')
+        s:create_index('pk')
+
+        -- Collect garbage before replacing tuples
+        box.internal.memtx_tx_gc(10)
+        collectgarbage('collect')
+
+        s:replace{0, string.rep('a', 1100)}
+        box.internal.memtx_tx_gc(10)
+        collectgarbage('collect')
+        local items_used_with_huge_tuple = box.slab.info()['items_used']
+
+        s:replace{0, 1}
+        box.internal.memtx_tx_gc(10)
+        collectgarbage('collect')
+        local items_used_without_huge_tuple = box.slab.info()['items_used']
+        t.assert_lt(items_used_without_huge_tuple, items_used_with_huge_tuple)
+        t.assert_ge(items_used_with_huge_tuple - items_used_without_huge_tuple, 1000)
+    end)
+end
