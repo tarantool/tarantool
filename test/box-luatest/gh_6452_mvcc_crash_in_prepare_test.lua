c2dd8b0a940a56887b424a70ae245e75b4692cff
Aleksandr Lyapunov <alyapunov@tarantool.org>
2022-03-16 11:40:33 +0300

    txm: fix a crash in mvcc with secondary index conflict
    Memtx TX manager stores a pointer to deleting statement in prepared
    story. This pointer is set in two cases:
    1. a statement deletes (or overwrites) a tuple
    2. a story becomes prepared while other inprogress TX overwrites it
    
    By design a tuple can be deleted only by primary index, the case
    when a transaction overwrites somehting in secondary index but does
    not overwrite the same tuple in primary index is prohibited. That's
    why the pointer (to deleting statement) must be set by and only by
    the next statement in the primary index chain.
    
    By mistake the pointer is set also in second index chain analysis
    after reordering which led to unexpected state of a story.
    
    The patch removes the problem.
    
    Closes #6452
    NO_DOC=bugfix

---
+++ test/box-luatest/gh_6452_mvcc_crash_in_prepare_test.lua
@@ -0,0 +1,123 @@
+local server = require('test.luatest_helpers.server')
+local t = require('luatest')
+local g = t.group()
+
+g.before_all = function()
+    g.server = server:new{
+        alias   = 'default',
+        box_cfg = {memtx_use_mvcc_engine = true}
+    }
+    g.server:start()
+end
+
+g.after_all = function()
+    g.server:drop()
+end
+
+g.before_each(function()
+    g.server:exec(function()
+        local s = box.schema.space.create('s', {engine = 'memtx'})
+        s:create_index("pk", {type = "tree"})
+        s:create_index("sk", {parts = {2}, type = "hash"})
+    end)
+end)
+
+g.after_each(function()
+    g.server:exec(function() box.space.s:drop() end)
+end)
+
+-- Simplified 1st case from the issue that better describes the problem.
+g.test_mvcc_crash_in_prepare_case1_simplified = function()
+    g.server:exec(function()
+        local t = require('luatest')
+        local txn_proxy = require("test.box.lua.txn_proxy")
+
+        local tx1 = txn_proxy.new()
+        local tx2 = txn_proxy.new()
+
+        tx1:begin()
+        tx2:begin()
+        tx1("box.space.s:insert{1, 'A'}")
+        tx1("box.space.s:insert{2, 'B'}")
+        tx2("box.space.s:replace{1, 'B'}")
+        t.assert_equals(tx1:commit(), "")
+        t.assert_equals(tx2:commit(),
+                        {{error = "Transaction has been aborted by conflict"}})
+    end)
+end
+
+-- Exactly the 1st case from the issue.
+g.test_mvcc_crash_in_prepare_case1 = function()
+    g.server:exec(function()
+        local t = require('luatest')
+        local txn_proxy = require("test.box.lua.txn_proxy")
+
+        local tx1 = txn_proxy.new()
+        local tx2 = txn_proxy.new()
+        local tx3 = txn_proxy.new()
+        local tx4 = txn_proxy.new()
+
+        tx1:begin()
+        tx2:begin()
+        tx3:begin()
+        tx4:begin()
+
+        tx1("box.space.s:insert{1, 'A'}")
+        tx1:commit()
+        tx2("box.space.s:replace{2, 'B'}")
+        tx3("box.space.s:replace{1, 'B'}")
+        tx4("box.space.s:replace{3, 'B'}")
+
+        tx2:rollback()
+        tx4:commit()
+        t.assert_equals(tx3:commit(),
+                        {{error = "Transaction has been aborted by conflict"}})
+    end)
+end
+
+-- Simplified 2nd case from the issue that better describes the problem.
+g.test_mvcc_crash_in_prepare_case2_simplified = function()
+    g.server:exec(function()
+        local t = require('luatest')
+        local txn_proxy = require("test.box.lua.txn_proxy")
+
+        local tx1 = txn_proxy.new()
+        local tx2 = txn_proxy.new()
+
+        tx1:begin()
+        tx2:begin()
+        tx1('box.space.s:insert{1, "A"}')
+        tx1('box.space.s:insert{2, "B"}')
+        tx2('box.space.s:replace{2, "A"}')
+        t.assert_equals(tx1:commit(), "")
+        t.assert_equals(tx2:commit(),
+                        {{error = "Transaction has been aborted by conflict"}})
+    end)
+end
+
+-- Exactly the 2nd case from the issue (except some changed quotes).
+g.test_mvcc_crash_in_prepare_case2 = function()
+    g.server:exec(function()
+        local t = require('luatest')
+        local txn_proxy = require("test.box.lua.txn_proxy")
+
+        local tx1 = txn_proxy.new()
+        local tx2 = txn_proxy.new()
+        local tx3 = txn_proxy.new()
+
+        tx1:begin()
+        tx2:begin()
+        tx3:begin()
+
+        tx1("box.space.s:replace{1, 'A'}")
+        tx2("box.space.s:select{1}")
+        tx2("box.space.s:insert{2, 'B'}")
+        tx3("box.space.s:replace{3, 'A'}")
+        tx3("box.space.s:replace{1, 'C'}")
+
+        tx3:commit()
+        tx1:rollback()
+        t.assert_equals(tx2:commit(),
+                        {{error = "Transaction has been aborted by conflict"}})
+    end)
+end
