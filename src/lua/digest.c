4b01ce5b0fdd96f60bf5f27da162b68765f19bb4
Igor Munkin <imun@tarantool.org>
2022-03-25 12:22:24 +0300

    lua: rewrite crc32 digest via Lua C API
    As a result of recording <crc32:update> method or <digest.crc32>
    function wrong semantics is compiled (strictly saying, the resulting
    trace produces the different result from the one yielded by
    interpreter). The easiest solution is disabling JIT for particular
    functions, however, such approach drops the overall platform
    performance. Hence, the mentioned functions are rewritten line by line
    via Lua C API to avoid JIT misbehaviour.
    
    NO_DOC=no visible changes
    NO_CHANGELOG=no visible changes
    
    (cherry picked from commit 6b9131981f57e72c790561f0b7fdbf9b4a72e787)

--- src/lua/digest.c
+++ src/lua/digest.c
@@ -37,6 +37,7 @@
 #include <lua.h>
 #include <lauxlib.h>
 #include "utils.h"
+#include "crc32.h"
 
 #define PBKDF2_MAX_DIGEST_SIZE 128
 
@@ -90,6 +91,82 @@ lua_pbkdf2(lua_State *L)
 	return 1;
 }
 
+/* CRC32 internal {{{ */
+
+int
+crc32_methods_update(lua_State *L)
+{
+	size_t strl;
+	/* Get <string>. */
+	const char *str = lua_tolstring(L, 2, &strl);
+	if (str == NULL)
+		luaL_error(L, "Usage crc32:update(string)");
+	/* Get <self.value>. */
+	lua_getfield(L, 1, "value");
+	uint32_t crc32_begin = lua_tointeger(L, -1);
+	uint32_t crc32_result = crc32_calc(crc32_begin, str,
+					   (unsigned int)strl);
+	/* Push the result. */
+	lua_pushinteger(L, crc32_result);
+	/* Set <self.value>. */
+	lua_setfield(L, 1, "value");
+	return 0;
+}
+
+int
+crc32___call(lua_State *L)
+{
+	size_t strl;
+	/* Get <string>. */
+	const char *str = lua_tolstring(L, 2, &strl);
+	if (str == NULL)
+		luaL_error(L, "Usage digest.crc32(string)");
+	/* Get <CRC32> upvalue. */
+	lua_pushvalue(L, lua_upvalueindex(1));
+	/* Get <CRC32.crc_begin>. */
+	lua_getfield(L, -1, "crc_begin");
+	uint32_t crc32_begin = lua_tointeger(L, -1);
+	uint32_t crc32_result = crc32_calc(crc32_begin, str,
+					   (unsigned int)strl);
+	/* Push the result. */
+	lua_pushinteger(L, crc32_result);
+	return 1;
+}
+
+int
+crc32_internal_init(lua_State *L)
+{
+	/* Create the table with internal methods */
+	lua_createtable(L, 0, 2);
+	/* Copy <CRC32> on the top of the stack. */
+	lua_pushvalue(L, 1);
+	/* Create function <__call>. */
+	lua_pushcclosure(L, crc32___call, 1);
+	/* Store it to the table with internal methods. */
+	lua_setfield(L, -2, "__call");
+	/* Create function <update>. */
+	lua_pushcfunction(L, crc32_methods_update);
+	/* Store it to the table with internal methods. */
+	lua_setfield(L, -2, "update");
+	/* Copy the table with internal methods on the top of the stack. */
+	lua_pushvalue(L, -1);
+	/* Get the <crc32.internal> field from <package.loaded>. */
+	lua_getfield(L, LUA_REGISTRYINDEX, "_LOADED");
+	/* Set the table with internal methods in <package.loaded>. */
+	lua_setfield(L, -2, "crc32.internal");
+	/* Return the table with internal methods */
+	return 1;
+}
+
+LUA_API int
+luaopen_crc32_internal(lua_State *L)
+{
+	lua_pushcfunction(L, crc32_internal_init);
+	return 1;
+}
+
+/* }}} */
+
 void
 tarantool_lua_digest_init(struct lua_State *L)
 {
@@ -97,6 +174,10 @@ tarantool_lua_digest_init(struct lua_State *L)
 		{"pbkdf2", lua_pbkdf2},
 		{NULL, NULL}
 	};
+	lua_getfield(L, LUA_REGISTRYINDEX, "_PRELOAD");
+	lua_pushcfunction(L, luaopen_crc32_internal);
+	lua_setfield(L, -2, "crc32.internal");
+	lua_pop(L, 1);
 	luaL_register_module(L, "digest", lua_digest_methods);
 	lua_pop(L, 1);
 };
