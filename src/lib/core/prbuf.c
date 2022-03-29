1c8914e5c024e458e4117ea2d78a746859f7e32a
Nikita Pettik <korablev@tarantool.org>
2022-03-29 16:00:51 +0300

    core: introduce prbuf
    prbuf is partitioned ring buffer. The main property of the buffer is
    that it can be recovered from raw memory. To achieve this buffer saves
    metadata before each stored entry. For further details see source code.
    
    NO_DOC=<Private data structure>
    NO_CHANGELOG=<No user visible changes>

---
+++ src/lib/core/prbuf.c
@@ -0,0 +1,363 @@
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright 2010-2022, Tarantool AUTHORS, please see AUTHORS file.
+ */
+#include "prbuf.h"
+#include "bit/bit.h"
+
+/**
+ * Partitioned ring buffer. Each entry stores size before user data.
+ * So the typical buffer looks like:
+ * HEADER uint32 DATA uint32 DATA ...
+ *
+ * We have to store offsets to be able to restore buffer (including
+ * all metadata) from raw pointer. Otherwise it is impossible to point
+ * out where head/tail are located.
+ */
+struct PACKED prbuf_header {
+	/**
+	 * Buffer's data layout can be changed in the future, so for the sake
+	 * of proper recovery of the buffer we store its version.
+	 */
+	uint32_t version;
+	/** Total size of buffer (including header). */
+	uint32_t size;
+	/**
+	 * Offset of the oldest entry - it is the first candidate to be
+	 * overwritten. Note that in contrast to iterator/entry - this offset
+	 * is calculated to the first byte of entry (i.e. header containing
+	 * size of entry).
+	 */
+	uint32_t begin;
+	/** Offset of the next byte after the last (written) record. */
+	uint32_t end;
+};
+
+/**
+ * Structure representing record stored in the buffer so it has the same
+ * memory layout.
+ */
+struct PACKED prbuf_record {
+	/** Size of data. */
+	uint32_t size;
+	/** Data. */
+	char data[];
+};
+
+/**
+ * Current prbuf implementation version. Must be bumped in case storage
+ * format is changed.
+ */
+static const uint32_t prbuf_version = 0;
+
+/**
+ * prbuf is assumed to store all metadata in little-endian format. Beware
+ * when decoding its content. Size overhead per store operation is 4 bytes;
+ * moreover not the whole space of the buffer can be used since we do not
+ * wrap entry if it doesn't fit til the buffer's end.
+ *
+ * There are several assumptions regarding the buffer:
+ * - The end of the buffer (in the linear sense) contains "end mark";
+ * - The minimal size of the buffer is restricted;
+ * - Iteration direction - from the oldest entry to the newest.
+ */
+
+/** A mark of unused space in the buffer: trash is located after this point. */
+static const uint32_t prbuf_end_position = (uint32_t)(-1);
+
+/** Before storing a data in the buffer we place its size (i.e. header). */
+static const size_t record_size_overhead = sizeof(struct prbuf_record);
+
+/** Real size of allocation is (data size + record's header). */
+static uint32_t
+prbuf_record_alloc_size(size_t size)
+{
+	return size + offsetof(struct prbuf_record, data);
+}
+
+/** Returns pointer to the next byte after end of given buffer. */
+static char *
+prbuf_linear_end(struct prbuf *buf)
+{
+	return (char *)buf->header + buf->header->size;
+}
+
+/** Returns pointer to the first writable byte of given buffer. */
+static char *
+prbuf_linear_begin(struct prbuf *buf)
+{
+	return (char *)buf->header + sizeof(struct prbuf_header);
+}
+
+/** Returns pointer to the next byte after the last written record. */
+static char *
+prbuf_current_raw(struct prbuf *buf)
+{
+	return prbuf_linear_begin(buf) + buf->header->end;
+}
+
+static struct prbuf_record *
+prbuf_current_record(struct prbuf *buf)
+{
+	return (struct prbuf_record *)prbuf_current_raw(buf);
+}
+
+/** Returns first (in historical sense) record. */
+static struct prbuf_record *
+prbuf_first_record(struct prbuf *buf)
+{
+	assert(buf->header->begin != prbuf_end_position);
+	char *first_ptr = prbuf_linear_begin(buf) + buf->header->begin;
+	return (struct prbuf_record *)first_ptr;
+}
+
+/** Calculate offset from the buffer's start to the given entry. */
+static uint32_t
+prbuf_record_offset(struct prbuf *buf, struct prbuf_record *record)
+{
+	assert((char *)record >= prbuf_linear_begin(buf));
+	return (uint32_t)((char *)record - prbuf_linear_begin(buf));
+}
+
+/** Returns true in case buffer has at least @a size bytes until
+ * its linear end.
+ */
+static bool
+prbuf_has_before_end(struct prbuf *buf, uint32_t size)
+{
+	assert(prbuf_linear_end(buf) >= prbuf_current_raw(buf));
+	if ((uint32_t)(prbuf_linear_end(buf) - prbuf_current_raw(buf)) >= size)
+		return true;
+	return false;
+}
+
+static uint32_t
+prbuf_size(struct prbuf *buf)
+{
+	return buf->header->size - sizeof(struct prbuf_header);
+}
+
+void
+prbuf_create(struct prbuf *buf, void *mem, size_t size)
+{
+	assert(size > sizeof(struct prbuf_header));
+#ifndef NDEBUG
+	memset(mem, '#', size);
+#endif
+	buf->header = (struct prbuf_header *)mem;
+	buf->header->end = 0;
+	buf->header->begin = 0;
+	buf->header->version = prbuf_version;
+	buf->header->size = size;
+}
+
+/** Returns true in case buffer contains no entries. */
+static bool
+prbuf_is_empty(struct prbuf *buf)
+{
+	return buf->header->end == 0;
+}
+
+/**
+ * Assuming @a record pointing to the valid buffer position, move it to the
+ * next entry. In case of current entry is broken (contains wrong size or
+ * points out of the buffer) function returns -1.
+ */
+static int
+prbuf_next_record(struct prbuf *buf, struct prbuf_record **record)
+{
+	struct prbuf_record *current = *record;
+	*record = NULL;
+	if (current->size > prbuf_size(buf))
+		return -1;
+	if ((char *)current < prbuf_linear_begin(buf))
+		return -1;
+	if ((char *)current > prbuf_linear_end(buf))
+		return -1;
+	char *next_record_ptr = current->data + current->size;
+	if (next_record_ptr > prbuf_linear_end(buf))
+		return -1;
+
+	/* We have reached the end of buffer. */
+	if (next_record_ptr == prbuf_current_raw(buf))
+		return 0;
+
+	current = (struct prbuf_record *)next_record_ptr;
+	if ((uint32_t)(prbuf_linear_end(buf) - next_record_ptr) <
+	    record_size_overhead)
+		current = (struct prbuf_record *)prbuf_linear_begin(buf);
+	else if (current->size == prbuf_end_position)
+		current = (struct prbuf_record *)prbuf_linear_begin(buf);
+	*record = current;
+	return 0;
+}
+
+/**
+ * Verify that prbuf remains in the consistent state: header is valid and
+ * all entries have readable sizes.
+ */
+static bool
+prbuf_check(struct prbuf *buf)
+{
+	if (buf->header->version != prbuf_version)
+		return false;
+	if (buf->header->begin > prbuf_size(buf))
+		return false;
+	if (buf->header->end > prbuf_size(buf))
+		return false;
+	struct prbuf_record *current;
+	current = prbuf_first_record(buf);
+	uint32_t total_size = current->size;
+	int rc = 0;
+	while ((rc = prbuf_next_record(buf, &current)) == 0 &&
+	       current != NULL)
+		total_size += current->size;
+	return rc == 0 && (total_size <= buf->header->size);
+}
+
+int
+prbuf_open(struct prbuf *buf, void *mem)
+{
+	buf->header = (struct prbuf_header *)mem;
+	if (!prbuf_check(buf))
+		return -1;
+	return 0;
+}
+
+/**
+ * Starting from @a current issue skip @a to_store bytes and return
+ * the next record after that.
+ */
+static struct prbuf_record *
+prbuf_skip_record(struct prbuf *buf, struct prbuf_record *current,
+		  ssize_t to_store)
+{
+	assert(to_store > 0);
+	assert(to_store <= buf->header->size);
+
+	while (to_store > 0) {
+		assert(current->size != prbuf_end_position);
+		assert(current->size != 0);
+		to_store -= prbuf_record_alloc_size(current->size);
+		int rc = prbuf_next_record(buf, &current);
+		assert(rc == 0);
+		(void)rc;
+		if (current == NULL)
+			return (struct prbuf_record *)prbuf_linear_begin(buf);
+	}
+	return current;
+}
+
+/** Place special mark at the end of buffer to avoid out-of-bound access. */
+static void
+prbuf_set_end_position(struct prbuf *buf)
+{
+	if (prbuf_has_before_end(buf, record_size_overhead))
+		prbuf_current_record(buf)->size = prbuf_end_position;
+}
+
+/** Store entry's size. */
+static void *
+prbuf_prepare_record(struct prbuf_record *record, size_t size)
+{
+	record->size = size;
+	return record->data;
+}
+
+void *
+prbuf_prepare(struct prbuf *buf, size_t size)
+{
+	assert(size > 0);
+	uint32_t alloc_size = prbuf_record_alloc_size(size);
+	if (alloc_size > prbuf_size(buf))
+		return NULL;
+	if (prbuf_has_before_end(buf, alloc_size)) {
+		/*
+		 * Head points to the byte right after the last
+		 * written entry.
+		 */
+		struct prbuf_record *head = prbuf_current_record(buf);
+		if (prbuf_is_empty(buf))
+			return prbuf_prepare_record(head, size);
+		struct prbuf_record *next = prbuf_first_record(buf);
+		uint32_t free_space = next >= head ?
+			(char *)next - (char *)head : UINT32_MAX;
+		/*
+		 * We can safely write entry in case it won't overwrite
+		 * anything. Either trash space between two entries
+		 * is large enough or the next entry to be overwritten
+		 * is located at the start of the buffer.
+		 */
+		if (free_space < alloc_size) {
+			struct prbuf_record *next_overwritten =
+				prbuf_skip_record(buf, next,
+						  alloc_size - free_space);
+			buf->header->begin =
+				prbuf_record_offset(buf, next_overwritten);
+		}
+		return prbuf_prepare_record(head, size);
+	}
+	/*
+	 * Data doesn't fit till the end of buffer, so we'll put the entry
+	 * at the buffer's start. Moreover, we should mark the last entry
+	 * (in linear sense) to avoid oud-of-bound access while parsing buffer
+	 * (after this mark trash is stored so we can't process further).
+	 */
+	prbuf_set_end_position(buf);
+	struct prbuf_record *head =
+		(struct prbuf_record *)prbuf_linear_begin(buf);
+	struct prbuf_record *next_overwritten =
+		prbuf_skip_record(buf, head, alloc_size);
+	buf->header->begin = prbuf_record_offset(buf, next_overwritten);
+	buf->header->end = 0;
+	return prbuf_prepare_record(head, size);
+}
+
+void
+prbuf_commit(struct prbuf *buf)
+{
+	if (prbuf_has_before_end(buf, record_size_overhead)) {
+		struct prbuf_record *last = prbuf_current_record(buf);
+		if (prbuf_has_before_end(buf, last->size)) {
+			buf->header->end +=
+				prbuf_record_alloc_size(last->size);
+			return;
+		}
+	}
+	struct prbuf_record *last =
+		(struct prbuf_record *)prbuf_linear_begin(buf);
+	buf->header->end = prbuf_record_alloc_size(last->size);
+}
+
+void
+prbuf_iterator_create(struct prbuf *buf, struct prbuf_iterator *iter)
+{
+	iter->buf = buf;
+	iter->current = NULL;
+}
+
+int
+prbuf_iterator_next(struct prbuf_iterator *iter, struct prbuf_entry *result)
+{
+	struct prbuf *buf = iter->buf;
+	if (iter->current == NULL) {
+		/* Check if the buffer is empty. */
+		if (prbuf_is_empty(buf))
+			return -1;
+		iter->current = prbuf_first_record(buf);
+		result->size = iter->current->size;
+		result->ptr = iter->current->data;
+		assert(iter->current->size < buf->header->size);
+		return 0;
+	}
+
+	int rc = prbuf_next_record(buf, &iter->current);
+	assert(rc == 0);
+	(void)rc;
+	if (iter->current == NULL)
+		return -1;
+	result->size = iter->current->size;
+	result->ptr = iter->current->data;
+	return 0;
+}
---
+++ test/unit/prbuf.c
@@ -0,0 +1,345 @@
+#include <assert.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "unit.h"
+#include "core/prbuf.h"
+#include "trivia/util.h"
+
+const size_t buffer_size_arr[] = { 128, 256, 512 };
+const size_t copy_number_arr[] = { 16, 32, 64 };
+const char payload_small[] = { 0xab, 0xdb, 0xee, 0xcc };
+const char payload_avg[] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+			     0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F };
+#define PAYLOAD_LARGE_SIZE 40
+static char payload_large[PAYLOAD_LARGE_SIZE];
+
+enum test_buffer_status {
+	OK = 0,
+	WRONG_PAYLOAD_SIZE,
+	WRONG_PAYLOAD_CONTENT,
+	RECOVERY_ERROR,
+	ALLOCATION_ERROR,
+	TEST_BUFFER_STATUS_COUNT
+};
+
+const char *info_msg = "prbuf(size=%lu, payload=%lu, iterations=%lu) %s";
+
+const char *test_buffer_status_strs[TEST_BUFFER_STATUS_COUNT] = {
+	"has been validated",
+	"failed due to wrong size of payload after recovery",
+	"failed due to wrong content of payload after recovery",
+	"failed to recover",
+	"failed to allocate memory"
+};
+
+static void
+payload_large_init(void)
+{
+	for (size_t i = 0; i < PAYLOAD_LARGE_SIZE; ++i)
+		payload_large[i] = (char)i;
+}
+
+static int
+test_buffer(uint32_t buffer_size, const char *payload, uint32_t payload_size,
+	    size_t copy_number)
+{
+	int rc = OK;
+	char *mem = xmalloc(buffer_size);
+	assert(mem != NULL);
+
+	struct prbuf buf;
+	prbuf_create(&buf, mem, buffer_size);
+
+	for (size_t i = 0; i < copy_number; ++i) {
+		char *p = prbuf_prepare(&buf, payload_size);
+		if (p == NULL) {
+			rc = ALLOCATION_ERROR;
+			goto finish;
+		}
+		memcpy(p, payload, payload_size);
+		prbuf_commit(&buf);
+	}
+
+	struct prbuf recovered_buf;
+	if (prbuf_open(&recovered_buf, mem) != 0) {
+		rc = RECOVERY_ERROR;
+		goto finish;
+	}
+
+	struct prbuf_iterator iter;
+	struct prbuf_entry entry;
+	prbuf_iterator_create(&recovered_buf, &iter);
+	while (prbuf_iterator_next(&iter, &entry) == 0) {
+		if (entry.size != payload_size) {
+			rc = WRONG_PAYLOAD_SIZE;
+			goto finish;
+		}
+		if (memcmp(payload, entry.ptr, payload_size) != 0) {
+			rc = WRONG_PAYLOAD_CONTENT;
+			goto finish;
+		}
+	};
+	for (size_t i = 0; i < copy_number; ++i) {
+		char *p = prbuf_prepare(&recovered_buf, payload_size);
+		if (p == NULL) {
+			rc = ALLOCATION_ERROR;
+			goto finish;
+		}
+		memcpy(p, payload, payload_size);
+		prbuf_commit(&recovered_buf);
+	}
+
+finish:
+	free(mem);
+	return rc;
+}
+
+static void
+test_buffer_foreach_copy_number(uint32_t buffer_size,
+				const char *payload, uint32_t payload_size)
+{
+	header();
+	int rc = 0;
+	for (size_t i = 0; i < lengthof(copy_number_arr); ++i) {
+		rc = test_buffer(buffer_size, payload, payload_size,
+				 copy_number_arr[i]);
+		is(rc, 0, info_msg, buffer_size, payload_size,
+		   copy_number_arr[i], test_buffer_status_strs[rc]);
+	}
+	footer();
+}
+
+static void
+test_buffer_foreach_payload(uint32_t buffer_size)
+{
+	test_buffer_foreach_copy_number(buffer_size, payload_small,
+					lengthof(payload_small));
+	test_buffer_foreach_copy_number(buffer_size, payload_avg,
+					lengthof(payload_avg));
+	test_buffer_foreach_copy_number(buffer_size, payload_large,
+					PAYLOAD_LARGE_SIZE);
+}
+
+static void
+test_buffer_foreach_size(void)
+{
+	for (size_t i = 0; i < lengthof(buffer_size_arr); ++i)
+		test_buffer_foreach_payload(buffer_size_arr[i]);
+}
+
+static void
+test_buffer_bad_version(void)
+{
+	header();
+	size_t buffer_size = buffer_size_arr[0];
+	size_t copy_number = copy_number_arr[0];
+	size_t payload_size = lengthof(payload_small);
+	char mem[buffer_size];
+
+	struct prbuf buf;
+	prbuf_create(&buf, mem, buffer_size);
+
+	for (size_t i = 0; i < copy_number; ++i) {
+		char *p = prbuf_prepare(&buf, payload_size);
+		memcpy(p, payload_small, payload_size);
+		prbuf_commit(&buf);
+	}
+	uint32_t bad_version = 666;
+	memcpy(buf.header, &bad_version, sizeof(uint32_t));
+
+	struct prbuf recovered_buf;
+	int rc = prbuf_open(&recovered_buf, mem);
+	is(rc, -1, "Failed to open buffer with malformed version");
+	footer();
+}
+
+static void
+test_buffer_bad_header(void)
+{
+	header();
+	size_t buffer_size = buffer_size_arr[0];
+	size_t copy_number = copy_number_arr[0];
+	size_t payload_size = lengthof(payload_small);
+	char mem[buffer_size];
+
+	struct prbuf buf;
+	prbuf_create(&buf, mem, buffer_size);
+
+	for (size_t i = 0; i < copy_number; ++i) {
+		char *p = prbuf_prepare(&buf, payload_size);
+		memcpy(p, payload_small, payload_size);
+		prbuf_commit(&buf);
+	}
+
+	/* Change begin and end to wrong values. */
+	mem[15] = 0xDD;
+	mem[10] = 0xDD;
+
+	struct prbuf recovered_buf;
+	int rc = prbuf_open(&recovered_buf, mem);
+	is(rc, -1, "Failed to open buffer with malformed header");
+	footer();
+}
+
+static void
+test_buffer_corrupted_record(void)
+{
+	header();
+	size_t buffer_size = buffer_size_arr[0];
+	size_t copy_number = copy_number_arr[0];
+	size_t payload_size = lengthof(payload_small);
+	char mem[buffer_size];
+
+	struct prbuf buf;
+	prbuf_create(&buf, mem, buffer_size);
+
+	for (size_t i = 0; i < copy_number; ++i) {
+		char *p = prbuf_prepare(&buf, payload_size);
+		memcpy(p, payload_small, payload_size);
+		prbuf_commit(&buf);
+	}
+
+	/* Corrupt first record. */
+	mem[17] = 0xDD;
+
+	struct prbuf recovered_buf;
+	int rc = prbuf_open(&recovered_buf, mem);
+	is(rc, -1, "Failed to open buffer with malformed record");
+	footer();
+}
+
+static void
+test_buffer_too_large_entry(void)
+{
+	header();
+	size_t buffer_size = buffer_size_arr[0];
+	char mem[buffer_size];
+
+	struct prbuf buf;
+	prbuf_create(&buf, mem, buffer_size);
+
+	char *p = prbuf_prepare(&buf, buffer_size);
+	is(p, NULL, "Failed to allocate too large entry");
+	footer();
+}
+
+static void
+test_buffer_empty(void)
+{
+	header();
+	size_t buffer_size = buffer_size_arr[0];
+	char mem[buffer_size];
+
+	struct prbuf buf;
+	prbuf_create(&buf, mem, buffer_size);
+
+	struct prbuf_iterator iter;
+	struct prbuf_entry entry;
+	prbuf_iterator_create(&buf, &iter);
+	int rc = prbuf_iterator_next(&iter, &entry);
+	is(rc, -1, "Buffer is empty");
+	footer();
+}
+
+static void
+test_buffer_prepared(void)
+{
+	header();
+	size_t buffer_size = buffer_size_arr[0];
+	char mem[buffer_size];
+	size_t payload_size = lengthof(payload_small);
+
+	struct prbuf buf;
+	prbuf_create(&buf, mem, buffer_size);
+
+	size_t copy_count = 32;
+	for (size_t i = 0; i < copy_count; ++i) {
+		char *p = prbuf_prepare(&buf, payload_size);
+		memcpy(p, payload_small, payload_size);
+		prbuf_commit(&buf);
+	}
+	/* Count the actual number of entries stored in the buffer. */
+	struct prbuf_iterator iter;
+	struct prbuf_entry entry;
+	prbuf_iterator_create(&buf, &iter);
+	size_t entry_count = 0;
+	while (prbuf_iterator_next(&iter, &entry) == 0)
+		entry_count++;
+	char *p = prbuf_prepare(&buf, payload_size);
+	isnt(p, NULL, "Prepare has not failed");
+	/*
+	 * The number of entries after prepare should decrease since
+	 * it must overwrite some of the old records.
+	 */
+	prbuf_iterator_create(&buf, &iter);
+	size_t new_entry_count = 0;
+	while (prbuf_iterator_next(&iter, &entry) == 0)
+		new_entry_count++;
+	is(new_entry_count < entry_count, true, "Entry count has decreased")
+	footer();
+}
+
+static void
+test_buffer_prepared_large(void)
+{
+	header();
+	size_t buffer_size = buffer_size_arr[0];
+	char mem[buffer_size];
+	size_t payload_size = lengthof(payload_small);
+
+	struct prbuf buf;
+	prbuf_create(&buf, mem, buffer_size);
+
+	/* Fill more than a half of the buffer. */
+	size_t entry_count = 8;
+	for (size_t i = 0; i < entry_count; ++i) {
+		char *p = prbuf_prepare(&buf, payload_size);
+		memcpy(p, payload_small, payload_size);
+		prbuf_commit(&buf);
+	}
+	/*
+	 * Prepare one single entry which is going to overwrite
+	 * all other records so in fact buffer should be empty until commit.
+	 */
+	char *p = prbuf_prepare(&buf, 90);
+	isnt(p, NULL, "Prepare has not failed");
+	struct prbuf_iterator iter;
+	struct prbuf_entry entry;
+	prbuf_iterator_create(&buf, &iter);
+	int rc = prbuf_iterator_next(&iter, &entry);
+	is(rc, -1, "Buffer is empty");
+	for (size_t i = 0; i < entry_count; ++i) {
+		p = prbuf_prepare(&buf, payload_size);
+		memcpy(p, payload_small, payload_size);
+		prbuf_commit(&buf);
+	}
+	prbuf_iterator_create(&buf, &iter);
+	size_t entry_count_after = 0;
+	while (prbuf_iterator_next(&iter, &entry) == 0)
+		entry_count_after++;
+	is(entry_count_after, entry_count, "Buffer is in correct state")
+	footer();
+}
+
+/**
+ * There are three possible configurations of test:
+ * 1. The size of buffer;
+ * 2. The size of payload;
+ * 3. The number of saves to the buffer.
+ */
+int
+main(void)
+{
+	plan(37);
+	payload_large_init();
+	test_buffer_foreach_size();
+	test_buffer_bad_version();
+	test_buffer_bad_header();
+	test_buffer_corrupted_record();
+	test_buffer_too_large_entry();
+	test_buffer_empty();
+	test_buffer_prepared();
+	test_buffer_prepared_large();
+	return check_plan();
+}
