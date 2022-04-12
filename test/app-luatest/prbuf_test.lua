c53a45ec9d417e7289f2630d1b6d3f96041dc3e9
Nikita Pettik <korablev@tarantool.org>
2022-04-12 15:08:33 +0300

    lua: introduce interface for prbuf
    Simply add Lua wrappers for recently introduced prbuf.
    
    Introduce new buffer (in addition to ibuf) - prbuf. "pr" stands for
    "partitioned ring-". It save all metadata in the same memory chunk
    provided for storage, so it can be completely restored from the 'raw'
    memory. API:
    
    ```
    -- mem is a chunk of raw (char *) memory, of size mem_size.
    -- It is used for data storage. Note that available space is of less
    -- size due to prbuf metadata overhead.
    -- Returns handle to prbuf.
    --
    require('buffer').prbuf_create(mem, mem_size)
    
    -- mem is a chunk of memory, which contains already created prbuf.
    -- It implies that once prbuf_create() was called with the same memory.
    -- If mem does not contain valid buffer - raise an appropriate error.
    require('buffer').prbuf_open(mem)
    
    -- Returns continuous chunk of memory with given size. May return nil
    -- in case if requested chunk is too large. Note that after copying
    -- object to returned chunk, it should be committed with prbuf:commit();
    -- otherwise :prepare() may return the same chunk twice.
    prbuf:prepare(size)
    
    -- Commits the last prepared chunk. Undefined behaviour in case of
    -- committing the same chunk twice.
    prbuf:commit()
    
    -- Create and return prbuf_iterator. Does not fail. Created iterator
    -- points to nowhere - it should be adjusted with :next() call to
    -- the first entry.
    prbuf:iterator_create()
    
    -- Advance iterator position. Returns prbuf_entry or nil in case
    -- iterator has reached the end. Entry consists of two members:
    -- size and ptr. The last one is an array of characters of given size.
    iterator:next()
    ```
    
     Usage examples:
    
    ```
    
    local ibuf = buffer.ibuf()
    local prbuf_size = 100
    local memory = ibuf:alloc(prbuf_size)
    local prbuf = buffer.prbuf_create(memory, prbuf_size)
    
    local sample_size = 4
    local raw = prbuf:prepare(4)
    if raw == nil then
        -- allocation size is too large, try smaller.
    end
    raw[0] = ...
    ...
    raw[3] = ...
    prbuf:commit()
    local prbuf_recovered = buffer.prbuf_open(memory)
    local iter = prbuf_recovered:iterator_create()
    local entry = iter:next()
    assert(tonumber(entry.size) == 4)
    -- Check values stored in the buffer.
    assert(entry.ptr[0] == ...)
    entry = iter:next()
    -- Our buffer has only one entry.
    assert(entry == nil)
    
    ```
    
    NO_DOC=<Feature for internal usage>
    NO_CHANGELOG=<Feature for internal usage>

---
+++ test/app-luatest/prbuf_test.lua
@@ -0,0 +1,44 @@
+local buffer = require('buffer')
+local t = require('luatest')
+local g = t.group()
+
+local function fill_memory(memory, size)
+    for i = 0, size - 1 do
+        memory[i] = i
+    end
+end
+
+local function check_memory(memory, size)
+    for i = 0, size - 1 do
+        if memory[i] ~= i then return false end
+    end
+    return true
+end
+
+g.test_object_misc = function()
+    local ibuf = buffer.ibuf()
+    local prbuf_size = 100
+    local memory = ibuf:alloc(prbuf_size)
+    local prbuf = buffer.prbuf_create(memory, prbuf_size)
+
+    local sample_size = 4
+    local entry_count = 5
+    for _ = 1, entry_count do
+        local raw = prbuf:prepare(sample_size)
+        t.assert_equals(raw ~= nil, true)
+        fill_memory(raw, sample_size)
+        prbuf:commit()
+    end
+
+    local prbuf_recovered = buffer.prbuf_open(memory)
+    local iter = prbuf_recovered:iterator_create()
+    entry_count = 0
+    local entry = iter:next()
+    while entry ~= nil do
+        entry_count = entry_count + 1
+        t.assert_equals(entry.size, sample_size)
+        t.assert_equals(check_memory(entry.ptr, tonumber(entry.size)), true)
+        entry = iter:next()
+    end
+    t.assert_equals(entry_count, 5)
+end
