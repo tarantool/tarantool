ed46feb27087247ab5ee47fcf63f28c3ad842099
Boris Stepanenko <Boris.Stepanenko@gmail.com>
2022-04-20 05:18:48 +0300

    raft: leader fencing on quorum loss
    Losing quorum didn't modify RAFT leader behaviour, this led to easy
    split brain situation, when current leader gets disconnected from the
    rest of the replicaset. Old leader remained RW while new leader could be
    elected in the rest of the replicaset.
    
    Now when current leader looses connection to replica (connection being
    alive relay and applier for that replica) leader checks that he still
    has at least quorum of alive connections to replicas. If less than
    quorum of alive connection remain: leader resigns its role becoming
    follower without starting new term. This makes old leader RO. Old leader
    won't start new elections because of pre-vote, added in 421a096.
    
    Fenced leader will "freeze" its limbo, and won't CONFIRM nor ROLLBACK
    any synchronous transactions until it regains leadership. If another
    leader is elected while old one is fenced, once old leader is reconnected
    to new one "frozen" transaction will be rolled back.
    
    When quorum of connections is regained and new leader is not elected
    old leader or another node will start elections and elect new leader.
    
    Closes #6661
    
    @TarantoolBot document
    Title: RAFT leader fencing
    
    RAFT documentation must include that RAFT leader will resign its
    leadership if it has less than replication_synchro_quruom of alive
    connections to replicas (alive connection is connected relay and applier)
    if fencing is on. This applies to election_mode 'candidate' or 'manual'.
    Resigning leadership is becoming follower in current RAFT term, this
    leads to resigned leader becoming read-only.
    
    Introduce a new `box.cfg` option - `election_fencing_enabled`. If set to
    `true` fencing is on (default behaviour). If set to `false` fencing is off
    and leader doesn't resign it's leadership when looses quorum. If enabled
    on the current leader, when it doesn't have quorum of alive connections -
    leader will immediately resign its leadership.

--- src/box/lua/cfg.cc
+++ src/box/lua/cfg.cc
@@ -299,6 +299,14 @@ lbox_cfg_set_election_timeout(struct lua_State *L)
 }
 
 static int
+lbox_cfg_set_election_fencing_enabled(struct lua_State *L)
+{
+	if (box_set_election_fencing_enabled() != 0)
+		luaT_error(L);
+	return 0;
+}
+
+static int
 lbox_cfg_set_replication_timeout(struct lua_State *L)
 {
 	try {
@@ -439,6 +447,7 @@ box_lua_cfg_init(struct lua_State *L)
 		{"cfg_set_vinyl_timeout", lbox_cfg_set_vinyl_timeout},
 		{"cfg_set_election_mode", lbox_cfg_set_election_mode},
 		{"cfg_set_election_timeout", lbox_cfg_set_election_timeout},
+		{"cfg_set_election_fencing_enabled", lbox_cfg_set_election_fencing_enabled},
 		{"cfg_set_replication_timeout", lbox_cfg_set_replication_timeout},
 		{"cfg_set_replication_connect_quorum", lbox_cfg_set_replication_connect_quorum},
 		{"cfg_set_replication_connect_timeout", lbox_cfg_set_replication_connect_timeout},
