1aa9eca38fbfb4f549ca6f36c7a7d2e3fa636233
artembo <artembo@me.com>
2022-04-24 23:51:12 +0300

    ci: build Tarantool with enabled gc64
    To make tarantool-gc64 available in repository we need to provide
    a product name in a package itself and in the repository.
    
    To build Tarantool with gc64 enabled, the flag -DLUAJIT_ENABLE_GC64
    needs to be passed to cmake flags. For this purposes GC64 env var
    was added to all workflows for Debian, Ubuntu, Centos and Fedora.
    
    This variable is processed to rename PRODUCT_NAME. If GC64 is
    eanbled, PRODUCT_NAME is `tarantool-gc64`, otherwise it stays
    `tarantool`, which goes to repository name and the package itself.
    Thus, Tarantool with gc64 enabled can be installed by
    `<packaen_manager> install -y tarantool-gc64`
    in the one single repository.
    
    NO_DOC=ci
    NO_TEST=ci
    NO_CHANGELOG=ci
    
    Closes: tarantool/tarantool-qa#161

--- .gitlab.mk
+++ .gitlab.mk
@@ -101,7 +101,6 @@ endif
 
 RWS_BASE_URL=https://rws.tarantool.org
 RWS_ENDPOINT=${RWS_BASE_URL}/${REPO_TYPE}/${TARANTOOL_SERIES}/${OS}/${DIST}
-PRODUCT_NAME=tarantool
 
 deploy_prepare:
 	rm -rf packpack
@@ -110,6 +109,15 @@ deploy_prepare:
 	rm -rf build
 
 package: deploy_prepare
+	# Set PRODUCT_NAME for the package itself.
+	if [ "$$(echo $(GC64) | sed 's/.*=//')" = ON ]; then                                                           \
+		export PRODUCT_NAME=tarantool-gc64; \
+		if [ "${OS}" = "ubuntu" ] || [ "${OS}" = "debian" ]; then \
+		    $$(sed -i'' -e 's/Package: tarantool$$/Package: tarantool-gc64/' debian/control); \
+		fi; \
+	else \
+		export PRODUCT_NAME=tarantool; \
+	fi; \
 	if [ -n "$(GIT_TAG)" ]; then                                                                                    \
 		export VERSION="$$(echo $(GIT_TAG) | sed 's/-/~/')";                                                    \
 	else                                                                                                            \
@@ -118,12 +126,18 @@ package: deploy_prepare
 	echo VERSION=$$VERSION;                                                                                         \
 	PACKPACK_EXTRA_DOCKER_RUN_PARAMS="--network=host ${PACKPACK_EXTRA_DOCKER_RUN_PARAMS}"                           \
 	TARBALL_EXTRA_ARGS="--exclude=*.exe --exclude=*.dll"                                                            \
-	PRESERVE_ENVVARS="TARBALL_EXTRA_ARGS,${PRESERVE_ENVVARS}" ./packpack/packpack
+	PRESERVE_ENVVARS="PRODUCT_NAME,TARBALL_EXTRA_ARGS,${PRESERVE_ENVVARS}" ./packpack/packpack
 
 deploy:
 	if [ -z "${REPO_TYPE}" ]; then \
 		echo "Env variable 'REPO_TYPE' must be defined!"; \
 		exit 1; \
+	fi; \
+	# Set PRODUCT_NAME for the package in the repository.
+	if [ "$$(echo $(GC64) | sed 's/.*=//')" = ON ]; then                                                                                    \
+		PRODUCT_NAME=tarantool-gc64; \
+	else \
+		PRODUCT_NAME=tarantool; \
 	fi; \
 	CURL_CMD="curl \
 		--location \
@@ -134,7 +148,7 @@ deploy:
 		--retry-delay 5 \
 		--request PUT ${RWS_ENDPOINT} \
 		--user $${RWS_AUTH} \
-		--form product=${PRODUCT_NAME}"; \
+		--form product=$${PRODUCT_NAME}"; \
 	for f in $$(ls -I '*build*' -I '*.changes' ./build); do \
 		CURL_CMD="$${CURL_CMD} --form $$(basename $${f})=@./build/$${f}"; \
 	done; \
