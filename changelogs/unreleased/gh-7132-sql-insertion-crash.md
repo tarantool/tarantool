287e65c504725db28ab2531c936f5aa74b61a35e
Denis Smirnov <sd@picodata.io>
2022-04-28 10:28:58 +0700

    sql: fix insertion crash
    Previously SQL didn't validate for all cases, that the amount of the
    source and destination columns during insertion is equal. The problem
    was detected when we insert an incorrect amount of values into the
    table. For example, the query
    
    insert into t(a) select a, b from t
    
    produced an instance crash. Fixed.
    
    Closes #7132
    
    NO_DOC=bug fix
    
    Co-authored-by: Mergen Imeev <imeevma@gmail.com>
    (cherry picked from commit dfa0ea2b4436fab91318f7fb0787db5f9f6f1f75)

---
+++ changelogs/unreleased/gh-7132-sql-insertion-crash.md
@@ -0,0 +1,4 @@
+## bugfix/sql
+
+* Fixed a crash when a table inserted a data into itself with an incorrect
+amount of columns (gh-7132).
---
+++ test/sql-luatest/gh_7132_insertion_crash_test.lua
@@ -0,0 +1,36 @@
+local server = require('test.luatest_helpers.server')
+local t = require('luatest')
+local g = t.group()
+
+g.before_all(function()
+    g.server = server:new({alias = 'insertion_crash'})
+    g.server:start()
+end)
+
+g.after_all(function()
+    g.server:stop()
+end)
+
+g.test_insertion_crash_1 = function()
+    g.server:exec(function()
+        local t = require('luatest')
+        box.execute([[CREATE TABLE t(i INT PRIMARY KEY, a INT);]])
+        local sql = [[INSERT INTO t(i) SELECT a, i FROM t;]]
+        local res = [[2 values for 1 columns]]
+        local _, err = box.execute(sql)
+        t.assert_equals(err.message, res)
+        box.execute([[DROP TABLE t;]])
+    end)
+end
+
+g.test_insertion_crash_2 = function()
+    g.server:exec(function()
+        local t = require('luatest')
+        box.execute([[CREATE TABLE t(i INT PRIMARY KEY, a INT);]])
+        local sql = [[INSERT INTO t SELECT a, i, 1 FROM t;]]
+        local res = [[table T has 2 columns but 3 values were supplied]]
+        local _, err = box.execute(sql)
+        t.assert_equals(err.message, res)
+        box.execute([[DROP TABLE t;]])
+    end)
+end
