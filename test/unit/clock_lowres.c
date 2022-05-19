20358a07ee6a521de114eedcca3cd1acad61f6de
Andrey Saranchin <Andrey22102001@gmail.com>
2022-05-19 14:09:55 +0300

    core: introduce clock_lowres
    This patch introduces not thread-safe low resolution
    monotonic clock, based on interval timer. It should
    be used only by thread that initialized it.
    
    Part of #6085
    
    NO_CHANGELOG=internal feature
    NO_DOC=internal feature

---
+++ test/unit/clock_lowres.c
@@ -0,0 +1,43 @@
+#include "unit.h"
+
+#include <math.h>
+#include <unistd.h>
+#include "clock_lowres.h"
+#include "clock.h"
+
+/** Test duration in seconds. */
+#define TEST_LEN 1.5
+
+int
+main(void)
+{
+	plan(1);
+	clock_lowres_signal_init();
+
+	/* Such a large resolution to pass the test in debug or apple build. */
+	double resolution = clock_lowres_resolution() * 2;
+	bool success = true;
+	double start = clock_monotonic();
+	double clock = start;
+	while (clock < start + TEST_LEN) {
+		/*
+		 * Use pause before getting time so that the process
+		 * does not use all time and unlikely to be
+		 * rescheduled in the middle of check.
+		 * The process will wake up after receiving a signal:
+		 * the clock receives SIGALRM every resolution seconds.
+		 */
+		pause();
+		double lowres = clock_lowres_monotonic();
+		clock = clock_monotonic();
+		if (fabs(clock - lowres) > resolution) {
+			success = false;
+			break;
+		}
+	}
+	ok(success, "Check that monotonic lowres clock does not diverge "
+		    "too much from monotonic clock");
+
+	clock_lowres_signal_reset();
+	return check_plan();
+}
