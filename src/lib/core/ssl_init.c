e3bf73c8f062af4808c9d009b29ef18e77da920b
Vladimir Davydov <vdavydov@tarantool.org>
2022-05-31 15:20:07 +0300

    crypto: OpenSSL 3.0 support
    Two things we need to do to fix build with OpenSSL 3.0:
    
    1. Use EVP_MAC_* functions instead of HMAC_*
       https://www.openssl.org/docs/man3.0/man3/HMAC_CTX_new.html
    
    2. Load the Legacy provider to enable legacy algorithms, such as MD4
       https://wiki.openssl.org/index.php/OpenSSL_3.0#Programming_in_OpenSSL_3.0
    
    Closes #6477
    
    NO_DOC=build fix
    NO_TEST=build fix
    NO_CHANGELOG=build fix

--- src/lib/core/ssl_init.c
+++ src/lib/core/ssl_init.c
@@ -11,6 +11,14 @@
 #include <openssl/ssl.h>
 #include <stddef.h>
 
+#include "say.h"
+
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
+#include <openssl/provider.h>
+static OSSL_PROVIDER *legacy_provider = NULL;
+static OSSL_PROVIDER *default_provider = NULL;
+#endif
+
 void
 ssl_init_impl(void)
 {
@@ -22,11 +30,26 @@ ssl_init_impl(void)
 	OPENSSL_init_crypto(0, NULL);
 	OPENSSL_init_ssl(0, NULL);
 #endif
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
+	/* Needed to enable legacy algorithms, such as MD4. */
+	legacy_provider = OSSL_PROVIDER_load(NULL, "legacy");
+	if (legacy_provider == NULL)
+		say_error("cannot load the Legacy OpenSSL provider");
+	default_provider = OSSL_PROVIDER_load(NULL, "default");
+	if (default_provider == NULL)
+		say_error("cannot load the Default OpenSSL provider");
+#endif
 }
 
 void
 ssl_free_impl(void)
 {
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
+	if (legacy_provider != NULL)
+		OSSL_PROVIDER_unload(legacy_provider);
+	if (default_provider != NULL)
+		OSSL_PROVIDER_unload(default_provider);
+#endif
 #ifdef OPENSSL_cleanup
 	OPENSSL_cleanup();
 #endif
