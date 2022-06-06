1e5884b7d17fce3caeea30e7ccd51bb0cb469a0c
Georgiy Lebedev <g.lebedev@tarantool.org>
2022-06-06 09:13:53 +0300

    memtx: fix DML causing transaction self-conflict after full scan in HASH
    When full scans are checked on writes, we must only conflict transactions
    other than the one that did the full scan.
    
    NO_CHANGELOG=internal bugfix
    NO_DOC=bugfix
    
    Closes #7221

---
+++ test/box-luatest/gh_7221_memtx_tx_dml_self_conflict_after_fsc_in_hash_idx_test.lua
@@ -0,0 +1,62 @@
+local server = require('test.luatest_helpers.server')
+local t = require('luatest')
+
+local g = t.group()
+
+g.before_all(function()
+    g.server = server:new{
+        alias   = 'dflt',
+        box_cfg = {memtx_use_mvcc_engine = true}
+    }
+    g.server:start()
+    g.server:exec(function()
+        local s = box.schema.create_space('s')
+        s:create_index('pk', {type = 'HASH'})
+    end)
+end)
+
+g.after_all(function()
+    g.server:drop()
+end)
+
+g.test_replace_self_conflict_after_fsc_in_hash_idx = function()
+    g.server:exec(function()
+        local t = require('luatest')
+        local txn_proxy = require('test.box.lua.txn_proxy')
+
+        local tx = txn_proxy:new()
+
+        tx('box.begin()')
+        tx('box.space.s:select{}')
+        tx('box.space.s:replace{0}')
+        t.assert_equals(tx:commit(), "")
+    end)
+end
+
+g.test_insert_self_conflict_after_fsc_in_hash_idx = function()
+    g.server:exec(function()
+        local t = require('luatest')
+        local txn_proxy = require('test.box.lua.txn_proxy')
+
+        local tx = txn_proxy:new()
+
+        tx('box.begin()')
+        tx('box.space.s:select{}')
+        tx('box.space.s:insert{0}')
+        t.assert_equals(tx:commit(), "")
+    end)
+end
+
+g.test_upsert_self_conflict_after_fsc_in_hash_idx = function()
+    g.server:exec(function()
+        local t = require('luatest')
+        local txn_proxy = require('test.box.lua.txn_proxy')
+
+        local tx = txn_proxy:new()
+
+        tx('box.begin()')
+        tx('box.space.s:select{}')
+        tx('box.space.s:upsert({0}, {{"=", 1, 0}})')
+        t.assert_equals(tx:commit(), "")
+    end)
+end
