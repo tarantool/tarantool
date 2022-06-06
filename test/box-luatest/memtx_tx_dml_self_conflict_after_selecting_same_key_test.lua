0b2f90e7ead7a422232780e6af3a3de77993c9b1
Georgiy Lebedev <g.lebedev@tarantool.org>
2022-06-06 11:30:14 +0300

    memtx: fix DML after select of key causing self-conflict of transaction
    On insertion, when point holes are checked on insertion, we must only
    conflict transactions other than the one that read the hole.
    
    NO_CHANGELOG=internal bugfix
    NO_DOC=bugfix
    
    Closes #7234
    Closes #7235

---
+++ test/box-luatest/memtx_tx_dml_self_conflict_after_selecting_same_key_test.lua
@@ -0,0 +1,63 @@
+local server = require('test.luatest_helpers.server')
+local t = require('luatest')
+
+local pg = t.group(nil, {{idx = 'TREE'}, {idx = 'HASH'}})
+
+pg.before_each(function(cg)
+    cg.server = server:new{
+        alias   = 'dflt',
+        box_cfg = {memtx_use_mvcc_engine = true}
+    }
+    cg.server:start()
+    cg.server:exec(function()
+        box.schema.create_space('s')
+    end)
+    local fmt = 'box.space.s:create_index("pk", {type = "%s"})'
+    cg.server:eval(fmt:format(cg.params.idx))
+end)
+
+pg.after_each(function(cg)
+    cg.server:drop()
+end)
+
+pg.test_replace_self_conflict_after_selecting_same_key = function(cg)
+    cg.server:exec(function()
+        local t = require('luatest')
+        local txn_proxy = require('test.box.lua.txn_proxy')
+
+        local tx = txn_proxy:new()
+
+        tx('box.begin()')
+        tx('box.space.s:select{0}')
+        tx('box.space.s:replace{0}')
+        t.assert_equals(tx:commit(), "")
+    end)
+end
+
+pg.test_insert_self_conflict_after_selecting_same_key = function(cg)
+    cg.server:exec(function()
+        local t = require('luatest')
+        local txn_proxy = require('test.box.lua.txn_proxy')
+
+        local tx = txn_proxy:new()
+
+        tx('box.begin()')
+        tx('box.space.s:select{0}')
+        tx('box.space.s:insert{0}')
+        t.assert_equals(tx:commit(), "")
+    end)
+end
+
+pg.test_upsert_self_conflict_after_selecting_same_key = function(cg)
+    cg.server:exec(function()
+        local t = require('luatest')
+        local txn_proxy = require('test.box.lua.txn_proxy')
+
+        local tx = txn_proxy:new()
+
+        tx('box.begin()')
+        tx('box.space.s:select{0}')
+        tx('box.space.s:upsert({0}, {{"=", 1, 0}})')
+        t.assert_equals(tx:commit(), "")
+    end)
+end
