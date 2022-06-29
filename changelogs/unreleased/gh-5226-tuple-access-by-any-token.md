2eda6b3efd4d6f8c0e997ab35ea4e5deb93d2934
Vladimir Davydov <vdavydov@tarantool.org>
2022-06-29 15:19:28 +0300

    json: don't match any nodes if there's [*] in the path
    If a nested tuple field is indexed, it can be accessed by [*] aka
    multikey or any token:
    
      s = box.schema.create_space('test')
      s:create_index('pk')
      s:create_index('sk', {parts = {{2, 'unsigned', path = '[1][1]'}}})
      t = s:replace{1, {{1}}}
      t['[2][1][*]'] -- returns 1!
    
    If a nested field isn't indexed (remove creation of the secondary index
    in the example above), then access by [*] returns nil.
    
    Call graph:
    
      lbox_tuple_field_by_path:
        tuple_field_raw_by_full_path
          tuple_field_raw_by_path
            tuple_format_field_by_path
              json_tree_lookup_entry
                json_tree_lookup
    
    And json_tree_lookup matches the first node if the key is [*].
    We shouldn't match anything to [*].
    
    Closes #5226
    
    NO_DOC=bug fix
    
    (cherry picked from commit 35802a23cff47769bbe9c000859424902d4f3e9d)

---
+++ changelogs/unreleased/gh-5226-tuple-access-by-any-token.md
@@ -0,0 +1,4 @@
+## bugfix/core
+
+* Fixed a bug that allowed to access indexed nested tuple fields by `[*]`
+  in Lua (gh-5226).
---
+++ test/box-luatest/gh_5226_tuple_access_by_any_token_test.lua
@@ -0,0 +1,47 @@
+local server = require('test.luatest_helpers.server')
+local t = require('luatest')
+local g = t.group()
+
+g.before_all = function()
+    g.server = server:new({alias = 'master'})
+    g.server:start()
+end
+
+g.after_all = function()
+    g.server:drop()
+end
+
+--
+-- Checks that accessing a tuple field by [*] ('any' token) returns nothing
+-- (gh-5226).
+--
+g.test_tuple_access_by_any_token = function()
+    g.server:exec(function()
+        local t = require('luatest')
+        local s = box.schema.space.create('test')
+        s:create_index('pk')
+        s:create_index('sk', {parts = {{'[2][1][1]', 'unsigned'}}})
+        local tuple = s:replace({1, {{1, 2}}, {{3, 4}}})
+        t.assert_equals(tuple['[*]'], nil)
+        t.assert_equals(tuple['[*][1]'], nil)
+        t.assert_equals(tuple['[1]'], 1)
+        t.assert_equals(tuple['[1][*]'], nil)
+        t.assert_equals(tuple['[2]'], {{1, 2}})
+        t.assert_equals(tuple['[2][*]'], nil)
+        t.assert_equals(tuple['[2][*][1]'], nil)
+        t.assert_equals(tuple['[2][1]'], {1, 2})
+        t.assert_equals(tuple['[2][1][*]'], nil)
+        t.assert_equals(tuple['[2][1][1]'], 1)
+        t.assert_equals(tuple['[2][1][2]'], 2)
+        t.assert_equals(tuple['[2][1][2][*]'], nil)
+        t.assert_equals(tuple['[3]'], {{3, 4}})
+        t.assert_equals(tuple['[3][*]'], nil)
+        t.assert_equals(tuple['[3][*][1]'], nil)
+        t.assert_equals(tuple['[3][1]'], {3, 4})
+        t.assert_equals(tuple['[3][1][*]'], nil)
+        t.assert_equals(tuple['[3][1][1]'], 3)
+        t.assert_equals(tuple['[3][1][2]'], 4)
+        t.assert_equals(tuple['[3][1][2][*]'], nil)
+        s:drop()
+    end)
+end
