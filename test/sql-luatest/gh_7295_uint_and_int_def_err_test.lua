0c2152547f465520ab47c3aa0b04c74db3aaa4a0
Mergen Imeev <imeevma@tarantool.org>
2022-07-04 14:38:16 +0300

    sql: ariphmetic of unsigned values
    After this patch, the result type of arithmetic between two unsigned
    values will be INTEGER.
    
    Closes #7295
    
    NO_DOC=bugfix

---
+++ test/sql-luatest/gh_7295_uint_and_int_def_err_test.lua
@@ -0,0 +1,43 @@
+local server = require('test.luatest_helpers.server')
+local t = require('luatest')
+local g = t.group()
+
+g.before_all(function()
+    g.server = server:new({alias = 'test_uint_int'})
+    g.server:start()
+    g.server:exec(function()
+        box.execute([[CREATE TABLE t(i UNSIGNED PRIMARY KEY, a UNSIGNED);]])
+        box.execute([[INSERT INTO t VALUES (1, 2);]])
+    end)
+end)
+
+g.after_all(function()
+    g.server:exec(function()
+        box.execute([[DROP TABLE t;]])
+    end)
+    g.server:stop()
+end)
+
+g.test_uint_int_1 = function()
+    g.server:exec(function()
+        local t = require('luatest')
+        local res = {{name = "COLUMN_1", type = "integer"}}
+        t.assert_equals(box.execute([[SELECT i + a FROM t;]]).metadata, res)
+        t.assert_equals(box.execute([[SELECT i - a FROM t;]]).metadata, res)
+        t.assert_equals(box.execute([[SELECT i * a FROM t;]]).metadata, res)
+        t.assert_equals(box.execute([[SELECT i / a FROM t;]]).metadata, res)
+        t.assert_equals(box.execute([[SELECT i % a FROM t;]]).metadata, res)
+        t.assert_equals(box.execute([[SELECT i & a FROM t;]]).metadata, res)
+        t.assert_equals(box.execute([[SELECT i | a FROM t;]]).metadata, res)
+        t.assert_equals(box.execute([[SELECT i >> a FROM t;]]).metadata, res)
+        t.assert_equals(box.execute([[SELECT i << a FROM t;]]).metadata, res)
+    end)
+end
+
+g.test_uint_int_2 = function()
+    g.server:exec(function()
+        local t = require('luatest')
+        local sql = [[SELECT i - a FROM t ORDER BY 1 LIMIT 1;]]
+        t.assert_equals(box.execute(sql).rows, {{-1}})
+    end)
+end
