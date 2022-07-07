fa0a198d05923cc052b397bbf4f369a831addcab
Yaroslav Lobankov <y.lobankov@tarantool.org>
2022-07-08 01:06:48 +0300

    ci: fix deps installation for FreeBSD systems
    The issue was in the `deps_freebsd` target that installed test-run deps
    (py38-yaml, py38-six, py38-gevent) via the `pkg` package manager.
    
    At some moment these packages were upgraded to 39 version on the mirrors
    and became not available for installation anymore.
    
    Now test-run deps are installed via `pip` package installer to avoid
    such issues in the future. Installation via `pip` is more robust because
    we have deps versions pinned in test-run and also we have python version
    pinned to 3.8.
    
    NO_DOC=ci
    NO_TEST=ci
    NO_CHANGELOG=ci
    
    (cherry picked from commit 7d7b56bb06c85ee10965cd503c09b2870ea06580)

--- .travis.mk
+++ .travis.mk
@@ -460,9 +460,10 @@ test_static_build_cmake_osx_github_actions: base_deps_osx_github_actions test_st
 ###########
 
 deps_freebsd:
-	sudo pkg install -y git cmake gmake icu libiconv \
-		python38 py38-yaml py38-six py38-gevent
+	sudo pkg install -y git cmake gmake icu libiconv python38
 	which python3 || sudo ln -s /usr/local/bin/python3.8 /usr/local/bin/python3
+	python3 -m pip -V || curl -fsSL https://bootstrap.pypa.io/get-pip.py | python3
+	python3 -m pip install -r test-run/requirements.txt
 
 build_freebsd:
 	if [ "$$(swapctl -l | wc -l)" != "1" ]; then sudo swapoff -a ; fi ; swapctl -l
