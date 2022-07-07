ea07854ece9208de5fa9fab937b470329341d5bb
Gleb Kashkin <g.kashkin@tarantool.org>
2022-07-07 13:47:58 +0000

    test: fix reading STDIN command on openSUSE
    Inspired by gh-5064, that breaks the previous version of the test on
    openSUSE. When using `io.popen:write()` on tarantool with `-i` flag, it
    failed to run the command on openSUSE. This happened because before
    gh-5064 patch it used to employ `luaL_loadfile()` that interprets EOF
    as the end of the command, while when it is loaded as a string openSUSE
    expects it to end with '\n'.
    
    Needed for #5064
    NO_DOC=test fix
    NO_TEST=test fix
    NO_CHANGELOG=test fix

--- test/box/gh-4703-on_shutdown-bug.test.lua
+++ test/box/gh-4703-on_shutdown-bug.test.lua
@@ -8,9 +8,10 @@ test_run = env.new()
 --
 file_name = "on_shutdown_triggered.txt"
 test_run:cmd("setopt delimiter ';'");
+-- Extra \n is required on openSUSE.
 on_shutdown_cmd = "box.ctl.on_shutdown(function() local fio = require('fio') "..
                    "fio.open('"..file_name.."', "..
-                   "{'O_CREAT', 'O_TRUNC', 'O_WRONLY'}, 777):close() end)";
+                   "{'O_CREAT', 'O_TRUNC', 'O_WRONLY'}, 777):close() end)\n";
 test_run:cmd("setopt delimiter ''");
 server = io.popen('tarantool -i', 'w')
 server:write(on_shutdown_cmd)
