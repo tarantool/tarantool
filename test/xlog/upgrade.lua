04852e965e8378f611568d5c1afaffc1ad791245
Serge Petrenko <sergepetrenko@tarantool.org>
2022-07-12 15:14:38 +0300

    box: fix finding snapshot in upgrade script
    The upgrade script first tries to determine if the node is booted from
    old snaps not recoverable on current Tarantool versions. If this is the
    case, it sets up special triggers so that snaps are automatically
    converted to a suitable format.
    
    This happens before box.cfg{}, so the workdir is not set at this point
    in time, and the upgrade script should take configured work_dir into
    account explicitly. Fix this.
    
    Closes #7232
    
    NO_DOC=bugfix
    
    (cherry picked from commit 30db3c235c1b60ad8925571bfb009fcf0c4383ae)

--- test/xlog/upgrade.lua
+++ test/xlog/upgrade.lua
@@ -1,8 +1,20 @@
 #!/usr/bin/env tarantool
 
+local work_dir = nil
+local memtx_dir = nil
+
+if arg[1] ~= nil then
+    -- When no args are passed to the script test-run fills args with
+    -- "nil upgrade.lua" for some reason.
+    work_dir = arg[1]
+    memtx_dir = arg[2]
+end
+
 box.cfg {
     listen              = os.getenv("LISTEN"),
-    memtx_memory        = 107374182
+    memtx_memory        = 107374182,
+    work_dir            = work_dir,
+    memtx_dir           = memtx_dir,
 }
 
 require('console').listen(os.getenv('ADMIN'))
