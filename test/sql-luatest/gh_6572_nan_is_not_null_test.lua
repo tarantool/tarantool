820b7b1216e254416c590ccffbe77e57dc4df189
Mergen Imeev <imeevma@tarantool.org>
2022-07-14 19:03:54 +0300

    sql: always treat NaN as NULL
    In most cases, NaN was treated as NULL. But in case NaN was returned as
    a result of a Lua or C user defined function, it was considered a
    double. After this patch, NaN will also be considered NULL in the
    specified cases.
    
    Closes #6374
    Closes #6572
    
    NO_DOC=bugfix

---
+++ test/sql-luatest/gh_6572_nan_is_not_null_test.lua
@@ -0,0 +1,42 @@
+local server = require('test.luatest_helpers.server')
+local t = require('luatest')
+local g = t.group()
+
+g.before_all(function()
+    g.server = server:new({alias = 'test_nan_null'})
+    g.server:start()
+end)
+
+g.after_all(function()
+    g.server:stop()
+end)
+
+g.test_nan_null_lua = function()
+    g.server:exec(function()
+        local t = require('luatest')
+        local sql = [[SELECT LUA('return 0/0'), TYPEOF(LUA('return 0/0'));]]
+        t.assert_equals(box.execute(sql).rows, {{nil, 'NULL'}})
+    end)
+end
+
+g.test_nan_null_c = function()
+    g.server:exec(function()
+        local build_path = os.getenv("BUILDDIR")
+        package.cpath = build_path..'/test/sql-luatest/?.so;'..
+                        build_path..'/test/sql-luatest/?.dylib;'..package.cpath
+        local func = {language = 'C', returns = 'double', exports = {'SQL'}}
+        box.schema.func.create('gh_6572.get_nan', func);
+
+        local t = require('luatest')
+        local sql = [[SELECT "gh_6572.get_nan"(), TYPEOF("gh_6572.get_nan"());]]
+        t.assert_equals(box.execute(sql).rows, {{nil, 'NULL'}})
+    end)
+end
+
+g.test_nan_comp_dec = function()
+    g.server:exec(function()
+        local t = require('luatest')
+        local sql = [[SELECT 1.0 > LUA('return 0/0');]]
+        t.assert_equals(box.execute(sql).rows, {{}})
+    end)
+end
