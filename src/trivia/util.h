dc9e11fa39ab5235612725e99bbacb5477276b53
Timur Safin <tsafin@tarantool.org>
2022-07-14 13:31:11 +0300

    build: temp - introducing EXPORT_SYMBOL
    NO_DOC=refactoring
    NO_CHANGELOG=refactoring
    NO_TEST=see it later

--- src/trivia/util.h
+++ src/trivia/util.h
@@ -632,6 +632,28 @@ thread_sleep(double sec);
 # define static_assert _Static_assert
 #endif
 
+#define EXPORT_SYMBOL(name, alias) _EXPORT_SYMBOL(name, alias)
+
+#if defined(__arm64__) || defined(__arm__)
+#  define JMP "b "
+#else
+#  define JMP "jmp "
+#endif
+
+#ifdef __APPLE__
+#  define _U_ "_"
+#else
+#  define _U_
+#endif
+
+#define _EXPORT_SYMBOL(name, aliasname) \
+	static void *ref_fun_##aliasname __attribute__((used)) = \
+		(void *)name; \
+	__asm__("\t.text\n" \
+		"\t.globl " _U_ #aliasname "\n" \
+		_U_ #aliasname ": " JMP _U_ #name "\n"); \
+	extern __typeof(name) aliasname;
+
 #if defined(__cplusplus)
 } /* extern "C" */
 #endif /* defined(__cplusplus) */
