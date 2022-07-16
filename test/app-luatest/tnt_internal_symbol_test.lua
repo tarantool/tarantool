395c30e8f85d9808b65ba1f121d4f964f373410f
Alexander Turenko <alexander.turenko@tarantool.org>
2022-07-16 18:35:36 +0300

    fiber_channel: add accessor to internal functions
    The Rust module [1] leans on several internal symbols. They were open in
    Tarantool 2.8 (see #2971 and #5932), but never were in the public API.
    Tarantool 2.10.0 hides the symbols and we need a way to get them back to
    use in the module.
    
    We have the following options:
    
    1. Design and expose a module API for fiber channels.
    2. Export the symbols with a prefix like `tnt_internal_` (to don't spoil
       the global namespace).
    3. Provide a `dlsym()` alike function to get an address of an internal
       symbol for users who knows what they're doing.
    
    I think that the third way offers the best compromise between amount of
    effort, quality of the result and opportunities to extend. In this
    commit I hardcoded the list of functions to make the change as safe as
    possible. Later I'll return here to autogenerate the list.
    
    Exported the following function from the tarantool executable:
    
    ```c
    void *
    tnt_internal_symbol(const char *name);
    ```
    
    I don't add it into the module API headers, because the function is to
    perform a dark magic and we don't suggest it for users.
    
    While I'm here, added `static` to a couple of fiber channel functions,
    which are only used within the compilation unit.
    
    [1]: https://github.com/picodata/tarantool-module
    
    Part of #7228
    Related to #6372
    
    NO_DOC=don't advertize the dangerous API
    NO_CHANGELOG=don't advertize the dangerous API

---
+++ test/app-luatest/tnt_internal_symbol_test.lua
@@ -0,0 +1,40 @@
+local ffi = require('ffi')
+local t = require('luatest')
+local g = t.group()
+
+g.before_all = function()
+    ffi.cdef([[
+        void *
+        tnt_internal_symbol(const char *name);
+    ]])
+end
+
+g.test_fiber_channel = function()
+    local symbols = {
+        'fiber_channel_close',
+        'fiber_channel_create',
+        'fiber_channel_delete',
+        'fiber_channel_destroy',
+        'fiber_channel_get_msg_timeout',
+        'fiber_channel_get_timeout',
+        'fiber_channel_has_readers',
+        'fiber_channel_has_writers',
+        'fiber_channel_new',
+        'fiber_channel_put_msg_timeout',
+        'fiber_channel_put_timeout',
+        'ipc_value_delete',
+        'ipc_value_new',
+    }
+    -- Verify only that we can get the address. The functions are
+    -- provided 'as is' and we don't guarantee anything regarding
+    -- them.
+    for _, name in ipairs(symbols) do
+        local addr = ffi.C.tnt_internal_symbol(name)
+        t.assert_not_equals(addr, nil)
+    end
+end
+
+g.test_non_existing_symbol = function()
+    local addr = ffi.C.tnt_internal_symbol('non_existing_symbol')
+    t.assert_equals(addr, nil)
+end
