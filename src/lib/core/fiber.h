555839ca501f750455ca4bd24c8f141b35fb1108
Nikita Zheleztsov <n.zheleztsov@proton.me>
2022-08-03 14:42:14 +0300

    core: introduce system fiber
    There are a number of internal system fibers which are not supposed to
    be cancelled.
    
    Let's introduce `FIBER_IS_SYSTEM` flag that will indicate, if the fiber
    can be explicitly killed. If this flag is set, killing functions will
    just ignore cancellation request.
    
    This commit introduce blocking system fiber cancelling only from the Lua
    public API, as it is more important to have it right. The prohibition to
    cancel fibers from C API will be introduced later.
    
    Related to #7448
    Part of #7473
    
    NO_DOC=internal
    NO_TEST=will be added in subsequent commit
    NO_CHANGELOG=internal

--- src/lib/core/fiber.h
+++ src/lib/core/fiber.h
@@ -170,6 +170,10 @@ enum {
 	 * This flag is set when fiber has custom max slice.
 	 */
 	FIBER_CUSTOM_SLICE	= 1 << 8,
+	/**
+	 * This flag idicates, if the fiber can be killed from the Lua world.
+	 */
+	FIBER_IS_SYSTEM         = 1 << 9,
 	FIBER_DEFAULT_FLAGS = FIBER_IS_CANCELLABLE
 };
 
@@ -850,6 +854,20 @@ cord_is_main(void);
 void
 cord_collect_garbage(struct cord *cord);
 
+/**
+ * @brief Create a new system fiber.
+ *
+ * @details
+ * Works the same way as fiber_new(), but uses fiber_attr_default
+ * supplemented by the FIBER_IS_SYSTEM flag in order to create a
+ * fiber.
+ *
+ * @param name       string with fiber name
+ * @param fiber_func func for run inside fiber
+ */
+struct fiber *
+fiber_new_system(const char *name, fiber_func f);
+
 void
 fiber_init(int (*fiber_invoke)(fiber_func f, va_list ap));
 
@@ -1035,19 +1053,19 @@ void
 fiber_top_disable(void);
 
 #ifdef ENABLE_BACKTRACE
-/*
+/**
  * Returns current value of fiber parent backtrace collection option.
  */
 bool
 fiber_parent_backtrace_is_enabled(void);
 
-/*
+/**
  * Enables collection of fiber parent's backtrace.
  */
 void
 fiber_parent_backtrace_enable(void);
 
-/*
+/**
  * Disables collection of fiber parent's backtrace.
  */
 void
@@ -1064,7 +1082,7 @@ fiber_c_invoke(fiber_func f, va_list ap)
 #if defined(__cplusplus)
 } /* extern "C" */
 
-/*
+/**
  * Test if this fiber is in a cancellable state and was indeed
  * cancelled, and raise an exception (FiberIsCancelled) if
  * that's the case.
@@ -1087,6 +1105,20 @@ static inline struct fiber *
 fiber_new_xc(const char *name, fiber_func func)
 {
 	struct fiber *f = fiber_new(name, func);
+	if (f == NULL) {
+		diag_raise();
+		unreachable();
+	}
+	return f;
+}
+
+/**
+ * The same as fiber_new_system(), but throws an exception
+ */
+static inline struct fiber *
+fiber_new_system_xc(const char *name, fiber_func func)
+{
+	struct fiber *f = fiber_new_system(name, func);
 	if (f == NULL) {
 		diag_raise();
 		unreachable();
