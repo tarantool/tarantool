e65e0eb8f5ab04c84f89cebb19ef5b7c3b9ca60d
Vladimir Davydov <vdavydov@tarantool.org>
2022-08-23 10:33:12 +0300

    memtx: fix tuple garbage collection not resumed after snapshot
    This is a degradation introduced by commit 26f7056f9c12 ("Introduce
    internal database read view API"): tuple garbage collection is never
    resumed after a snapshot. This happens, because we don't add engine read
    views to the read view engine list on read view construction. As a
    result, read_view_close() never closes the memtx engine read view.
    
    NO_DOC=bug fix
    NO_CHANGELOG=unreleased

---
+++ test/box-luatest/memtx_gc_after_snapshot_test.lua
@@ -0,0 +1,38 @@
+local server = require('test.luatest_helpers.server')
+local t = require('luatest')
+local g = t.group()
+
+g.before_all = function(cg)
+    cg.server = server:new({alias = 'master'})
+    cg.server:start()
+end
+
+g.after_all = function(cg)
+    cg.server:stop()
+end
+
+-- Checks that memtx tuple garbage collection is resumed after snapshot.
+g.test_memtx_gc_after_snapshot = function(cg)
+    cg.server:exec(function()
+        local t = require('luatest')
+        box.schema.space.create('test')
+        box.space.test:create_index('primary')
+        local mem_used_1 = box.info.memory().data
+        box.begin()
+        for i = 1, 100 do
+            box.space.test:insert({i, string.rep('x', 10000)})
+        end
+        box.commit()
+        local mem_used_2 = box.info.memory().data
+        t.assert_gt(mem_used_2 - mem_used_1, 1000000)
+        box.snapshot()
+        box.begin()
+        for i = 1, 100 do
+            box.space.test:delete(i)
+        end
+        box.commit()
+        collectgarbage('collect') -- drop Lua refs
+        local mem_used_3 = box.info.memory().data
+        t.assert_gt(mem_used_2 - mem_used_3, 1000000)
+    end)
+end
