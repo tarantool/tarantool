22d0ac8f888584263de9eaf75c028c9811af5749
Timur Safin <tsafin@tarantool.org>
2022-08-25 18:07:47 +0300

    debugger: luatest for console debugger
    Created luatest test for interactive debugger luadebug.lua.
    We use separate debug-target.lua for execution under
    control of debugger session.
    
    NO_DOC=test
    NO_CHANGELOG=test

---
+++ test/app-luatest/debug-target.lua
@@ -0,0 +1,14 @@
+local debugger = require 'luadebug'
+debugger()
+local date = require 'datetime'
+
+local T = date.new{hour = 3, tzoffset = '+0300'}
+print(T)
+
+local fmt = '%Y-%m-%dT%H%M%z'
+local S  = T:format(fmt)
+print(S)
+local T1 = date.parse(S, {format = fmt})
+print(T1)
+
+os.exit(0)
