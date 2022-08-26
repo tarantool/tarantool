647ec1b2c11bd0f2030bab2451136c473367b18f
Nikolay Shirokovskiy <nshirokovskiy@tarantool.org>
2022-08-26 18:00:26 +0300

    box: fix inheriting format options for old-style parts
    If index parts are specified using old syntax like:
    
    	parts = {1, 'number', 2, 'string'},
    
    then (except if parts count is 1) index options set in space format
    are not taken into account. Solution is to continue after parsing 1.6.0
    style parts so to use code that check format options.
    
    Closes #7614
    
    NO_DOC=bugfix

---
+++ test/box-luatest/gh_7614_fix_nullable_from_format_test.lua
@@ -0,0 +1,30 @@
+local server = require('test.luatest_helpers.server')
+local t = require('luatest')
+local g = t.group()
+
+g.before_all = function(cg)
+    cg.server = server:new({alias = 'master'})
+    cg.server:start()
+end
+
+g.after_all = function(cg)
+    cg.server:stop()
+end
+
+g.test_1_6_style_parts_with_format = function(cg)
+    cg.server:exec(function()
+        local t = require('luatest')
+
+        box.schema.space.create('test')
+        box.space.test:format({
+            {name = 'id', type = 'number'},
+            {name = 'data', type = 'string', is_nullable = true},
+        })
+        box.space.test:create_index('primary')
+        box.space.test:create_index('sec1', {parts = {2, 'string'}})
+        t.assert(box.space.test.index.sec1.parts[1].is_nullable)
+        box.space.test:create_index('sec2',
+                                    {parts = {1, 'number', 2, 'string' }})
+        t.assert(box.space.test.index.sec2.parts[2].is_nullable)
+    end)
+end
