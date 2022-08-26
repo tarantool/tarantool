4a2cc492de9de9121bbc3dd8868fd4fd538a9da3
Nikolay Shirokovskiy <nshirokovskiy@tarantool.org>
2022-08-26 18:15:58 +0300

    test: fix readline dependent tests
    The tests fail on dev installation if it's readline configuration
    (.inputrc) changes prompt. Use default readline configuration for
    the tests.
    
    Closes #7620
    
    NO_DOC=test changes
    NO_TEST=test changes
    NO_CHANGELOG=test changes

--- test/app-luatest/gh_5064_console_ignore_i_flag_without_tty_test.lua
+++ test/app-luatest/gh_5064_console_ignore_i_flag_without_tty_test.lua
@@ -11,7 +11,9 @@ tarantool> ]]
 local TARANTOOL_PATH = arg[-1]
 
 g.test_console_ignores_i_flag_without_tty = function()
-    local cmd = [[printf '42\n' | ]] .. TARANTOOL_PATH .. [[ -i 2>/dev/null]]
+    -- Suppress prompt changes by custom readline config to simplify assertions.
+    local cmd = [[printf '42\n' | ]] .. 'INPUTRC=non_existent_file ' ..
+                TARANTOOL_PATH .. [[ -i 2>/dev/null]]
     local fh = io.popen(cmd, 'r')
 
     -- Readline on CentOS 7 produces \e[?1034h escape sequence before tarantool> prompt, remove it.
