217ac32169af6bebd757b3e41538d7dac1b57ad3
Yaroslav Lobankov <y.lobankov@tarantool.org>
2022-09-12 21:34:13 +0300

    test: slight refactoring of replication-py tests
    - Remove unused imports
    - Remove unnecessary creation of 'replica' instance objects
    - Use `<instance>.iproto.uri` object attribute instead of calling
      `box.cfg.listen` via admin connection
    
    NO_DOC=testing stuff
    NO_TEST=testing stuff
    NO_CHANGELOG=testing stuff
    
    (cherry picked from commit d13b06bda58e8cc69b2a2541d6560982013bef14)

--- test/replication-py/init_storage.test.py
+++ test/replication-py/init_storage.test.py
@@ -1,7 +1,5 @@
 from __future__ import print_function
 
-import os
-import glob
 from lib.tarantool_server import TarantoolStartError
 from lib.tarantool_server import TarantoolServer
 
@@ -10,19 +8,20 @@ master = server
 master_id = master.get_param("id")
 master.admin("box.schema.user.grant('guest', 'replication')")
 
+# replica server
+replica = TarantoolServer(server.ini)
+replica.script = "replication-py/replica.lua"
+replica.vardir = server.vardir
+replica.rpl_master = master
+
 print("-------------------------------------------------------------")
 print("gh-484: JOIN doesn't save data to snapshot with TREE index")
 print("-------------------------------------------------------------")
 
 master.admin("space = box.schema.space.create('test', {id =  42})")
 master.admin("index = space:create_index('primary', { type = 'tree'})")
-
 master.admin("for k = 1, 9 do space:insert{k, k*k} end")
 
-replica = TarantoolServer(server.ini)
-replica.script = "replication-py/replica.lua"
-replica.vardir = server.vardir
-replica.rpl_master = master
 replica.deploy()
 replica.admin("box.space.test:select()")
 
@@ -40,12 +39,7 @@ master.admin("for k = 10, 19 do box.space[42]:insert{k, k*k*k} end")
 master.admin("for k = 20, 29 do box.space[42]:upsert({k}, {}) end")
 lsn = master.get_lsn(master_id)
 
-replica = TarantoolServer(server.ini)
-replica.script = "replication-py/replica.lua"
-replica.vardir = server.vardir
-replica.rpl_master = master
 replica.deploy()
-
 replica.admin("space = box.space.test");
 replica.wait_lsn(master_id, lsn)
 for i in range(1, 20):
@@ -59,10 +53,6 @@ print("reconnect on JOIN/SUBSCRIBE")
 print("-------------------------------------------------------------")
 
 server.stop()
-replica = TarantoolServer(server.ini)
-replica.script = "replication-py/replica.lua"
-replica.vardir = server.vardir
-replica.rpl_master = master
 replica.deploy(wait=False)
 
 print("waiting reconnect on JOIN...")
