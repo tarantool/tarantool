89b751dedeb4f52074d96c53ae51f97c444370ad
Georgiy Lebedev <g.lebedev@tarantool.org>
2022-09-12 18:40:44 +0300

    memtx: fix TREE index `get` check for part count
    If TREE index `get` result is empty, the key part count is incorrectly
    compared to the tree's `cmp_def->part_count`, though it should be compared
    with `cmp_def->unique_part_count`. But we can actually assume that by the
    time we get to the index's `get` method the part count is equal to the
    unique part count (partial keys are rejected and `get` is not
    supported for non-unique indexes): change check to correct assertion.
    
    Closes #7685
    
    NO_DOC=<bugfix>

---
+++ test/box-luatest/gh_7685_memtx_tree_idx_get_with_nullable_field_phantom_read_test.lua
@@ -0,0 +1,66 @@
+local server = require('test.luatest_helpers.server')
+local t = require('luatest')
+
+local g = t.group()
+
+g.before_all(function(cg)
+    cg.server = server:new{
+        alias   = 'dflt',
+        box_cfg = {memtx_use_mvcc_engine = true}
+    }
+    cg.server:start()
+end)
+
+g.after_all(function(cg)
+    cg.server:drop()
+end)
+
+g.before_each(function(cg)
+    cg.server:exec(function()
+        local s = box.schema.space.create('s')
+        s:create_index('pk')
+        s:create_index('sk', {parts = {{2, 'uint', is_nullable = true}}})
+    end)
+end)
+
+g.after_each(function(cg)
+    cg.server:exec(function()
+        box.space.s:drop()
+    end)
+end)
+
+--[[
+Checks that phantom reads with `get` from nullable TREE index are not allowed.
+]]
+g.test_memtx_tree_idx_get_with_nullable_field_phantom_read = function(cg)
+    cg.server:exec(function()
+        local t = require('luatest')
+        local txn_proxy = require('test.box.lua.txn_proxy')
+
+        local tx = txn_proxy:new()
+        tx:begin()
+
+        tx('box.space.s.index[1]:get{0}')
+        box.space.s:replace{0, 0}
+        t.assert_equals(tx('box.space.s.index[1]:get{0}'), '')
+    end)
+end
+
+--[[
+Checks that reads with `get` from nullable TREE index are tracked correctly.
+]]
+g.test_memtx_tree_idx_get_with_nullable_field_read_tracked = function(cg)
+    cg.server:exec(function()
+        local t = require('luatest')
+        local txn_proxy = require('test.box.lua.txn_proxy')
+
+        local tx = txn_proxy:new()
+        tx:begin()
+
+        tx('box.space.s.index[1]:get{0}')
+        tx('box.space.s:replace{1, 1}')
+        box.space.s:replace{0, 0}
+        t.assert_equals(tx:commit(),
+                        {{error = 'Transaction has been aborted by conflict'}})
+    end)
+end
