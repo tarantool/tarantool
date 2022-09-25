483c0cfeeaf089c6578b6a4729ff674fa6fe5c56
Georgiy Lebedev <g.lebedev@tarantool.org>
2022-09-25 20:50:43 +0300

    memtx: fix loss of committed tuple in secondary index
    Concurrent transactions can try to insert tuples that intersect only by
    parts of secondary index: in this case when one of them gets prepared, the
    others get conflicted, but the committed story does not get retained
    (because the conflicting statements are not added to the committed story's
    delete statement list as opposed to primary index) and is lost after
    garbage collection: retain stories if there is a newer uncommitted story
    in the secondary indexes' history chain.
    
    Closes #7712
    
    NO_DOC=bugfix

---
+++ test/box-luatest/gh_7712_loss_of_commited_tuple_in_secondary_index_test.lua
@@ -0,0 +1,39 @@
+local server = require('test.luatest_helpers.server')
+local t = require('luatest')
+
+local pg = t.group(nil, t.helpers.matrix{idx = {'TREE', 'HASH'}})
+
+pg.before_all(function(cg)
+    cg.server = server:new{
+        alias   = 'dflt',
+        box_cfg = {memtx_use_mvcc_engine = true}
+    }
+    cg.server:start()
+    cg.server:exec(function(idx)
+        local s = box.schema.create_space('s')
+        s:create_index('pk')
+        s:create_index('sk', {type = idx, parts = {2}})
+    end, {cg.params.idx})
+end)
+
+pg.after_all(function(cg)
+    cg.server:drop()
+end)
+
+--[[
+Checks that committed tuple is not lost in secondary index.
+]]
+pg.test_idx_random_from_empty_space_tracking = function(cg)
+    local stream = cg.server.net_box:new_stream()
+    stream:begin()
+
+    stream.space.s:insert{0, 1}
+    cg.server:exec(function()
+        local t = require('luatest')
+
+        box.space.s:insert{2, 1}
+        box.internal.memtx_tx_gc(100)
+
+        t.assert_equals(box.space.s.index[1]:select{}, {{2, 1}})
+    end)
+end
