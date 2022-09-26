36571fd874e2b31d989ce387782106bd85263b28
Gleb Kashkin <g.kashkin@tarantool.org>
2022-09-26 10:20:43 +0000

    lua/datetime: clearer error msg for new()
    dt.new() will raise a clear error on wrong timestamp type.
    
    Closes #7273
    
    NO_DOC=bugfix
    NO_CHANGELOG=bugfix
    
    (cherry picked from commit 8d4cbf449703ff120957bb0a4c3eb03944943982)

---
+++ test/app-luatest/gh_7273_dt_new_err_on_cdata_test.lua
@@ -0,0 +1,8 @@
+local datetime = require('datetime')
+
+local t = require('luatest')
+local g = t.group()
+
+g.test_dt_new_err_on_cdata = function()
+    t.assert_error_msg_contains("bad timestamp ('number' expected, got 'cdata'", datetime.new, {timestamp = 1LL})
+end
