37ff420b97bbeb7bb17874a50dddf98370b445df
Nikolay Shirokovskiy <nshirokovskiy@tarantool.org>
2022-09-30 15:02:00 +0300

    test: don't call box.cfg() from test instance in gh-7288
    Such style is not recommented and going to be banned.
    
    Closes #7715
    
    NO_TEST=test refactoring
    NO_DOC=test refactoring
    NO_CHANGELOG= test refactoring

--- test/app-luatest/gh_7288_console_flavours_vs_txn_test.lua
+++ test/app-luatest/gh_7288_console_flavours_vs_txn_test.lua
@@ -1,227 +1,149 @@
 local server = require('test.luatest_helpers.server')
-local console = require('console')
-local fiber = require('fiber')
-local fio = require('fio')
-local string = require('string')
-local netbox = require('net.box')
 local t = require('luatest')
 
-local function configure_box()
-    local workdir = fio.pathjoin(server.vardir, 'gh-7288')
-    fio.rmtree(workdir)
-    fio.mkdir(workdir)
-    box.cfg{
-        memtx_use_mvcc_engine = true,
-        work_dir = workdir,
-        log = fio.pathjoin(workdir, 'self.log'),
-    }
-end
-
-local remote_mt = {
-    start = function(self)
-        local server = server:new{
-            alias = 'default',
-            box_cfg = {memtx_use_mvcc_engine = true},
-        }
-        server:start()
-        if not self.bin then
-            self.uri = fio.pathjoin(server.vardir, 'gh-7288.sock')
-            server:eval(string.format('require("console").listen("%s")', self.uri))
-        else
-            self.uri = server.net_box_uri
-        end
-        self.server = server
-    end,
-
-    stop = function(self)
-        self.server:stop()
-        self.server = nil
-        self.uri = nil
-    end,
-
-    connect = function(self, console)
-        console:send(string.format('require("console").connect("%s")', self.uri))
-    end,
-
-    disconnect = true,
-}
-
-local flavours = {
-    alocal = {},
-    remote_txt = setmetatable({bin = false}, {__index = remote_mt}),
-    remote_bin = setmetatable({bin = true}, {__index = remote_mt}),
-}
-
 local g = t.group('gh-7288', {
-    {name = 'alocal'},
-    {name = 'remote_txt'},
-    {name = 'remote_bin'},
+    {flavour = 'alocal'},
+    {flavour = 'remote_txt'},
+    {flavour = 'remote_bin'},
 })
 
-
---
--- Mock read/print methods to run a console. Without running console we can't
--- call console.connect to test remote consoles.
---
-local TestConsole = {}
-
-TestConsole.new = function(self, flavour)
-    local o = { flavour = flavour }
-    setmetatable(o, self)
-    return o
-end
-
-TestConsole.__index = {}
-TestConsole.__index.start = function(self)
-    if self.flavour.start then
-        self.flavour:start()
-    end
-end
-
-TestConsole.__index.stop = function(self)
-    if self.flavour.stop then
-        self.flavour:stop()
-    end
-end
-
-TestConsole.__index.connect = function(self)
-    self.ich = fiber.channel(10)
-    self.och = fiber.channel(10)
-    local on_start = fiber.channel()
-    console.on_start(function(console)
-        console.read = function(_)
-            return self.ich:get()
-        end
-        console.print = function(_, output)
-            self.och:put(output)
-        end
-        on_start:put(true)
-    end)
-    fiber.create(function()
-        console.start()
-        -- write console fiber exit message
-        self.och:put(true)
-    end)
-    t.assert(on_start:get(3))
-    if self.flavour.connect then
-        self.flavour:connect(self)
-    end
-end
-
-TestConsole.__index.send = function(self, input)
-    self.ich:put(input)
-    return t.assert(self.och:get(3))
-end
-
-TestConsole.__index.disconnect = function(self)
-    if self.flavour.disconnect then
-        self:send(nil)
-    end
-    self.ich:close()
-    -- read console fiber exit message
-    t.assert(self.och:get(3))
-    self.och:close()
-    self.ich = nil
-    self.och = nil
-    console.on_start(nil)
-end
-
-configure_box()
-
 g.before_all(function(cg)
-    cg.console = TestConsole:new(flavours[cg.params.name])
-    cg.console:start()
-end)
+    cg.server = server:new{alias = 'main',
+                           box_cfg = {memtx_use_mvcc_engine = true}}
+    cg.server:start()
+    cg.server:exec(function(flavour)
+        local helper = require('test.app-luatest.gh_7288_console_helper')
 
-g.after_all(function(cg)
-    cg.console:stop()
-    cg.console = nil
-end)
+        local console = helper.TestConsole:new(flavour)
+        console:start()
+        rawset(_G, 'console', console)
 
-g.before_each(function(cg)
-    cg.console:connect()
-end)
-
-g.after_each(function(cg)
-    cg.console:disconnect()
-end)
-
-local true_output = [[
+        local true_output = [[
 ---
 - true
 ...
 ]]
+        rawset(_G, 'true_output', true_output)
 
-local false_output = [[
+        local false_output = [[
 ---
 - false
 ...
 ]]
+        rawset(_G, 'false_output', false_output)
+
+    end, {cg.params.flavour})
+end)
+
+g.after_all(function(cg)
+    cg.server:exec(function()
+        _G.console:stop()
+    end)
+    cg.server:drop()
+end)
+
+g.before_each(function(cg)
+    cg.server:exec(function()
+        _G.console:connect()
+    end)
+end)
+
+g.after_each(function(cg)
+    cg.server:exec(function()
+       _G.console:disconnect()
+    end)
+end)
 
 g.test_begin_in_expr_without_error = function(cg)
-    local console = cg.console
-    console:send('box.begin()')
-    t.assert_equals(console:send('box.is_in_txn()'), true_output)
+    cg.server:exec(function()
+        local t = require('luatest')
+        local console = _G.console
+        console:send('box.begin()')
+        t.assert_equals(console:send('box.is_in_txn()'), _G.true_output)
+    end)
 end
 
 g.test_begin_in_expr_with_error = function(cg)
-    local console = cg.console
-    local expected = [[
+    cg.server:exec(function()
+        local t = require('luatest')
+        local console = _G.console
+        local expected = [[
 ---
 - error: '[string "box.begin() error("test error")"]:1: test error'
 ...
 ]]
-    t.assert_equals(console:send('box.begin() error("test error")'), expected)
-    t.assert_equals(console:send('box.is_in_txn()'), false_output)
+        t.assert_equals(console:send('box.begin() error("test error")'), expected)
+        t.assert_equals(console:send('box.is_in_txn()'), _G.false_output)
+    end)
 end
 
 g.test_error_in_different_expr = function(cg)
-    local console = cg.console
-    console:send('box.begin()')
-    console:send('error("test error")')
-    t.assert_equals(console:send('box.is_in_txn()'), true_output)
+    cg.server:exec(function()
+        local t = require('luatest')
+        local console = _G.console
+        console:send('box.begin()')
+        console:send('error("test error")')
+        t.assert_equals(console:send('box.is_in_txn()'), _G.true_output)
+    end)
 end
 
-local gb = t.group('gh-7288-bin-backcompat')
+g = t.group('gh-7288-bin-backcompat')
 
-gb.before_all(function()
-    gb.save = netbox.connect
-    netbox.connect = function(...)
-        local remote = gb.save(...)
-        remote.peer_protocol_features.streams = false
-        return remote
-    end
-    gb.console = TestConsole:new(flavours.remote_bin)
-    gb.console:start()
+g.before_all(function()
+    g.server = server:new{alias = 'backcompat',
+                           box_cfg = {memtx_use_mvcc_engine = true}}
+    g.server:start()
+    g.server:exec(function()
+        local helper = require('test.app-luatest.gh_7288_console_helper')
+        local netbox = require('net.box')
+
+        local connect_old = netbox.connect
+        netbox.connect = function(...)
+            local remote = connect_old(...)
+            remote.peer_protocol_features.streams = false
+            return remote
+        end
+        local console = helper.TestConsole:new('remote_bin')
+        console:start()
+        rawset(_G, 'console', console)
+    end)
 end)
 
-gb.after_all(function()
-    gb.console:stop()
-    gb.console = nil
-    netbox.connect = gb.save
+g.after_all(function()
+    g.server:exec(function()
+        _G.console:stop()
+    end)
+    g.server:drop()
 end)
 
-gb.before_each(function()
-    gb.console:connect()
+g.before_each(function()
+    g.server:exec(function()
+        _G.console:connect()
+    end)
 end)
 
-gb.after_each(function()
-    gb.console:disconnect()
+g.after_each(function()
+    g.server:exec(function()
+       _G.console:disconnect()
+    end)
 end)
 
-gb.test_remote_bin_no_streams_works = function(cg)
-    local console = cg.console
-    -- first check we have backcompat mode without streams
-    local expected = [[
+g.test_remote_bin_no_streams_works = function(g)
+    g.server:exec(function()
+        local t = require('luatest')
+        local console = _G.console
+        -- first check we have backcompat mode without streams
+        local expected = [[
 ---
 - error: Transaction is active at return from function
 ...
 ]]
-    t.assert_equals(console:send('box.begin()'), expected)
-    local expected = [[
+        t.assert_equals(console:send('box.begin()'), expected)
+        local expected = [[
 ---
 - 4
 ...
 ]]
-    t.assert_equals(console:send('2 + 2'), expected)
+        t.assert_equals(console:send('2 + 2'), expected)
+    end)
 end
