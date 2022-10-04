481cfb75d2aeb7bce40e30b423e82d6fbcac1650
Mergen Imeev <imeevma@tarantool.org>
2022-10-04 14:25:36 +0300

    sql: fix assertion during INDEXED BY
    This patch fixed the assertion when using INDEXED BY with an index that
    is at least the third in space.
    
    Closes #5976
    
    NO_DOC=bugfix
    
    (cherry picked from commit 22c65f967a6ee29692028316e2cf5da0d02016a1)

---
+++ test/sql-luatest/gh_5976_indexed_by_assertion_test.lua
@@ -0,0 +1,24 @@
+local server = require('test.luatest_helpers.server')
+local t = require('luatest')
+local g = t.group()
+
+g.before_all(function()
+    g.server = server:new({alias = 'test_assertion_in_indexed_by'})
+    g.server:start()
+end)
+
+g.after_all(function()
+    g.server:stop()
+end)
+
+g.test_assertion_in_indexed_by = function()
+    g.server:exec(function()
+        local t = require('luatest')
+        box.execute([[CREATE TABLE t (i INT PRIMARY KEY, s STRING);]])
+        box.space.T:create_index('I1', {parts = {2, 'string'}})
+        box.space.T:create_index('I2', {parts = {2, 'string'}})
+        local sql = [[UPDATE t INDEXED BY i2 SET s = 'a' WHERE s = 'z';]]
+        t.assert_equals(box.execute(sql), {row_count = 0})
+        box.execute([[DROP TABLE t;]])
+    end)
+end
