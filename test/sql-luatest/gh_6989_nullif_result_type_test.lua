4135a7db4830126b9e0b3e3e8dfc4a456a0996b6
Mergen Imeev <imeevma@tarantool.org>
2022-10-05 13:53:16 +0300

    sql: change rules used to determine NULLIF() type
    This patch introduces new rules to determine type of NULLIF() built-in
    function.
    
    Closes #6990
    
    @TarantoolBot document
    Title: New rules to determine type of result of NULLIF
    
    The type of the result of NULLIF() function now matches the type of the
    first argument.

---
+++ test/sql-luatest/gh_6989_nullif_result_type_test.lua
@@ -0,0 +1,36 @@
+local server = require('test.luatest_helpers.server')
+local t = require('luatest')
+local g = t.group()
+
+g.before_all(function()
+    g.server = server:new({alias = 'test_assertion_in_modulo'})
+    g.server:start()
+end)
+
+g.after_all(function()
+    g.server:stop()
+end)
+
+g.test_assertion_in_modulo = function()
+    g.server:exec(function()
+        local t = require('luatest')
+        local sql = "SELECT nullif(1, '2');"
+        local res = "integer"
+        t.assert_equals(box.execute(sql).metadata[1].type, res)
+
+        sql = "SELECT nullif('1', '1');"
+        res = "string"
+        t.assert_equals(box.execute(sql).metadata[1].type, res)
+
+        sql = "SELECT nullif([1], '2');"
+        res = "Failed to execute SQL statement: wrong arguments for function "..
+              "NULLIF()"
+        local _, err = box.execute(sql)
+        t.assert_equals(err.message, res)
+
+        sql = "SELECT nullif(1, [2]);"
+        res = "Type mismatch: can not convert array([2]) to scalar"
+        _, err = box.execute(sql)
+        t.assert_equals(err.message, res)
+    end)
+end
