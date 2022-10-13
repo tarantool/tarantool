aa2d22e294c75e66616639041a8e9837d8e11ad1
Ilya Verbin <iverbin@tarantool.org>
2022-10-13 13:22:48 +0300

    box: strengthen field type check
    Don't accept an empty string or leading part of "str" or "num" as a
    valid field type.
    
    Closes #5940
    
    NO_DOC=Partial field types weren't documented
    
    Co-authored-by: Alexander Turenko <alexander.turenko@tarantool.org>
    (cherry picked from commit 2dbaf9c2655cf81be9525edb7d4563484f8d41e6)

---
+++ test/box-luatest/gh_5940_partial_field_type_test.lua
@@ -0,0 +1,26 @@
+local t = require('luatest')
+local g = t.group('gh-5940')
+local server = require('test.luatest_helpers.server')
+
+g.before_all = function()
+    g.server = server:new{alias = 'default'}
+    g.server:start()
+end
+
+g.after_all = function()
+    g.server:drop()
+end
+
+g.test_ddl_ops = function()
+    g.server:exec(function()
+        local t = require('luatest')
+        local field_types = { '', 'n', 'nu', 's', 'st' }
+        local error_msg = 'Wrong space format: field 1 has unknown field type'
+
+        for _, type in pairs(field_types) do
+            t.assert_error_msg_content_equals(error_msg, function()
+                box.schema.create_space('a', {format = {[1] = {name = 'id', type = type}}})
+            end)
+        end
+    end)
+end
