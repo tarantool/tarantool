8e67d95b41423ab0d07c136fb66fc5553ac4a336
Ilya Verbin <iverbin@tarantool.org>
2022-10-18 17:16:27 +0300

    box: panic if snapshot has no system spaces during recovery
    Currently, if a snapshot contains some correct entries, but doesn't
    include system spaces, Tarantool crashes with segmentation fault, or
    for Debug build: void diag_raise(): Assertion `e != NULL' failed.
    This happens because memtx_engine_recover_snapshot returns -1, while
    diag is not set. Let's panic instead of a crash.
    
    Closes #7800
    
    NO_DOC=bugfix

---
+++ test/box-luatest/gh_7800_recover_snap_without_spaces_test.lua
@@ -0,0 +1,23 @@
+local t = require('luatest')
+local g = t.group('gh-7800')
+
+g.before_all(function()
+    local server = require('test.luatest_helpers.server')
+    g.server = server:new({alias = 'master',
+                           datadir = 'test/box-luatest/gh_7800_data'})
+    g.server:start({wait_for_readiness = false})
+end)
+
+g.after_all(function()
+    g.server:cleanup()
+end)
+
+g.test_recovery = function()
+    t.helpers.retrying({}, function()
+        local fio = require('fio')
+        local msg = "has no system spaces"
+        local filename = fio.pathjoin(g.server.workdir,
+                                      g.server.alias .. '.log')
+        t.assert(g.server:grep_log(msg, nil, {filename = filename}))
+    end)
+end
