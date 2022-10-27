4ec07cb807a2bb5d64a19c99182a76154e30c123
Gleb Kashkin <g.kashkin@tarantool.org>
2022-10-27 09:48:29 +0000

    lua/fio: clearer error msg for pread()
    fh:pread() will raise a clear error on wrong arguments.
    
    Closes #4963
    
    NO_DOC=bugfix
    NO_CHANGELOG=bugfix
    
    (cherry picked from commit 22ecf87a3bc48a901b6a3b7f4dea68272c30f0b7)

---
+++ test/app-luatest/gh_4963_fio_pread_err_msg_test.lua
@@ -0,0 +1,10 @@
+local fio = require('fio')
+
+local t = require('luatest')
+local g = t.group()
+
+g.test_fio_pread_err_msg = function()
+    local fh = fio.open('/dev/null', {'O_RDONLY'})
+
+    t.assert_error_msg_contains("Usage: fh:pread(buf, size[, offset]) or fh:pread(size[, offset])", fh.pread, fh)
+end
