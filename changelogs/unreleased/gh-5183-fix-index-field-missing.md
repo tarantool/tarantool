7efb27e698a46361688d5c49198cab47f122f240
Mergen Imeev <imeevma@tarantool.org>
2022-11-01 13:59:02 +0300

    sql: fix error with new field in new index
    In case the space format is changed after inserting tuples, the new
    format contains new fields, and a new index is created that works with
    those new fields, it is possible to get an assertion during SELECT. This
    problem was fixed in this path.
    
    Closes #5183
    
    NO_DOC=bugfix

---
+++ changelogs/unreleased/gh-5183-fix-index-field-missing.md
@@ -0,0 +1,4 @@
+## bugfix/sql
+
+* Creating index on newly added fields no longer leads to a wrong
+  select (gh-5183).
---
+++ test/sql-luatest/gh_5183_fix_index_field_missing_test.lua
@@ -0,0 +1,27 @@
+local server = require('test.luatest_helpers.server')
+local t = require('luatest')
+local g = t.group()
+
+g.before_all(function()
+    g.server = server:new({alias = 'test_index_field_missing'})
+    g.server:start()
+end)
+
+g.after_all(function()
+    g.server:stop()
+end)
+
+g.test_index_field_missing = function()
+    g.server:exec(function()
+        local t = require('luatest')
+        local format = {{'I', 'integer'}}
+        local s = box.schema.space.create('T', {format = format})
+        s:create_index('i')
+        box.execute('INSERT INTO t VALUES (1), (2);')
+        format[2] = {'A', 'integer', is_nullable = true}
+        s:format(format)
+        s:create_index('a', {parts={'A'}, unique = false})
+        local rows = box.execute('SELECT * FROM t WHERE a IS NULL;').rows
+        t.assert_equals(rows, {{1, box.NULL}, {2, box.NULL}})
+    end)
+end
