1c80ce2d6973d4c5ea03439236d215f2da6dac89
Mergen Imeev <imeevma@tarantool.org>
2022-12-01 16:46:03 +0300

    sql: correctly calculate argument types
    This patch fixes an issue where collation could change the computed
    type of a built-in function argument when choosing a function
    implementation.
    
    Closes #7992
    
    NO_DOC=bugfix

---
+++ test/sql-luatest/gh_7992_update_collate_test.lua
@@ -0,0 +1,20 @@
+local server = require('luatest.server')
+local t = require('luatest')
+local g = t.group()
+
+g.before_all(function()
+    g.server = server:new({alias = 'gh_7992'})
+    g.server:start()
+end)
+
+g.after_all(function()
+    g.server:stop()
+end)
+
+g.test_ignore_collate_for_builtins = function()
+    g.server:exec(function()
+        local t = require('luatest')
+        local res = box.execute([[select abs(NULL collate "unicode_ci");]])
+        t.assert_equals(res.rows, {{box.NULL}})
+    end)
+end
