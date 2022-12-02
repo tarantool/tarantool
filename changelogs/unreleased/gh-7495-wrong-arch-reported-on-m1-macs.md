106b856a748a8d4cbb7f13a3173740a93d20f494
Ilya Verbin <iverbin@tarantool.org>
2022-12-02 18:27:27 +0300

    cmake: fix wrong CPU architecture reported on M1/M2 Macs
    This patch removes an old CMake kludge, which sets CMAKE_SYSTEM_PROCESSOR
    to "x86_64" on all 64-bit architectures, even on ARM (on Darwin).
    
    Closes #7495
    
    NO_DOC=bugfix
    
    (cherry picked from commit d76be7efc00a1a28cd5be0f64e70b2e6d6e298c3)

---
+++ changelogs/unreleased/gh-7495-wrong-arch-reported-on-m1-macs.md
@@ -0,0 +1,4 @@
+## bugfix/core
+
+* Fixed wrong CPU architecture reported in `tarantool.build.target` on M1/M2
+  Macs (gh-7495).
---
+++ test/app-luatest/gh_7495_tarantool_build_target_test.lua
@@ -0,0 +1,10 @@
+local t = require('luatest')
+local g = t.group('gh-7495')
+
+-- Check that `tarantool.build.target` contains the same CPU architecture as
+-- reported by `uname -m`.
+g.test_build_target = function()
+    local tarantool = require('tarantool')
+    local arch_name = io.popen('uname -m'):read()
+    t.assert_str_contains(tarantool.build.target, '-' .. arch_name .. '-')
+end
