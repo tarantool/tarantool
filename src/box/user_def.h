49caa65c62065db1f8b3acb7b9f5ef87fed21460
Vladimir Davydov <vdavydov@tarantool.org>
2022-12-05 18:27:47 +0300

    box: add user_def_new and user_def_delete helpers
    We will need to store some extra data in the user_def struct to support
    different authentication mechanisms. Let's introduce convenient helpers
    for allocating and freeing this struct so that we don't have to patch
    all the places in the code where it's allocated or freed when we extend
    the struct.
    
    While we are at it, switch to grp_alloc, shorted the license text, and
    replace include guards with pragma.
    
    Needed for #7986
    
    NO_DOC=refactoring
    NO_TEST=refactoring
    NO_CHANGELOG=refactoring

--- src/box/user_def.h
+++ src/box/user_def.h
@@ -1,35 +1,12 @@
-#ifndef TARANTOOL_BOX_USER_DEF_H_INCLUDED
-#define TARANTOOL_BOX_USER_DEF_H_INCLUDED
 /*
- * Copyright 2010-2016, Tarantool AUTHORS, please see AUTHORS file.
+ * SPDX-License-Identifier: BSD-2-Clause
  *
- * Redistribution and use in source and binary forms, with or
- * without modification, are permitted provided that the following
- * conditions are met:
- *
- * 1. Redistributions of source code must retain the above
- *    copyright notice, this list of conditions and the
- *    following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above
- *    copyright notice, this list of conditions and the following
- *    disclaimer in the documentation and/or other materials
- *    provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * <COPYRIGHT HOLDER> OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
- * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
- * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * Copyright 2010-2022, Tarantool AUTHORS, please see AUTHORS file.
  */
+#pragma once
+
+#include <stdint.h>
+
 #include "schema_def.h" /* for SCHEMA_OBJECT_TYPE */
 #include "scramble.h" /* for SCRAMBLE_SIZE */
 #define RB_COMPACT 1
@@ -167,14 +144,20 @@ struct user_def {
 	/** User password - hash2 */
 	char hash2[SCRAMBLE_SIZE];
 	/** User name - for error messages and debugging */
-	char name[0];
+	char *name;
 };
 
-static inline size_t
-user_def_sizeof(uint32_t name_len)
-{
-	return sizeof(struct user_def) + name_len + 1;
-}
+/**
+ * Allocates and initializes a new user definition.
+ * This function never fails.
+ */
+struct user_def *
+user_def_new(uint32_t uid, uint32_t owner, enum schema_object_type type,
+	     const char *name, uint32_t name_len);
+
+/** Destroys and frees a user definition. */
+void
+user_def_delete(struct user_def *def);
 
 /** Predefined user ids. */
 enum {
@@ -189,5 +172,3 @@ enum {
 #if defined(__cplusplus)
 } /* extern "C" */
 #endif /* defined(__cplusplus) */
-
-#endif /* TARANTOOL_BOX_USER_DEF_H_INCLUDED */
