2d3c59d4606961779fa1a91aee5a8083b69d1043
Georgiy Lebedev <g.lebedev@tarantool.org>
2023-01-10 15:22:04 +0300

    coro: fix dereferencing of `void *` pointer warning
    Since the fiber function is not expected to return (i.e., the inline
    assembly is not expected to return), remove the dummy memory operand used
    to indicate that the memory pointed to by the inline assembly pointer
    operand is clobbered.
    
    Closes #8125
    
    NO_DOC=build diagnostic
    NO_CHANGELOG=build diagnostic
    NO_TEST=build diagnostic

--- third_party/coro/coro.c
+++ third_party/coro/coro.c
@@ -124,7 +124,7 @@ coro_init (void)
   "\tmovq %0, %%rdi\n"
   "\tcallq *%1\n"
   :
-  : "rm" (arg), "rm" (func), "m" (*arg)
+  : "rm" (arg), "rm" (func)
   : "rbp", "rax", "rcx", "rdx", "rsi", "rdi", "r8", "r9", "r10", "r11",
     "xmm0","xmm1", "xmm2", "xmm3", "xmm4", "xmm5", "xmm6", "xmm7",
     "xmm8","xmm9", "xmm10", "xmm11", "xmm12", "xmm13", "xmm14", "xmm15",
