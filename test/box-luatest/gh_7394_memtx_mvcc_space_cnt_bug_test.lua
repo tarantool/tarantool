d030f49c55f0a173bfb26e784e4737f8eba76e92
Georgiy Lebedev <g.lebedev@tarantool.org>
2023-01-23 13:22:12 +0300

    test: add test for #7394 (`space:count` bug in memtx MVCC)
    The memtx transaction manager MVCC invariant violation described in
    c8eccfbb could actually lead to a bug described in #7394, since
    `space:count` inherently relies on this invariant: add a test for this
    case.
    
    Closes #7394
    
    NO_CHANGELOG=<bug was fixed in c8eccfbb>
    NO_DOC=bugfix
    
    (cherry picked from commit db6245f8b9d2720fb585cb16a6af8175d50c1bd2)

---
+++ test/box-luatest/gh_7394_memtx_mvcc_space_cnt_bug_test.lua
@@ -0,0 +1,38 @@
+local server = require('luatest.server')
+local t = require('luatest')
+
+local g = t.group()
+
+g.before_all(function(cg)
+    cg.server = server:new {
+        alias   = 'dflt',
+        box_cfg = {memtx_use_mvcc_engine = true}
+    }
+    cg.server:start()
+    cg.server:exec(function()
+        local s = box.schema.create_space('s')
+        s:create_index('pk')
+        box.internal.memtx_tx_gc(100)
+    end)
+end)
+
+g.after_all(function(cg)
+    cg.server:drop()
+end)
+
+-- Checks that memtx transaction manager MVCC invariant violation does not lead
+-- to incorrect space count (see also gh-7490 test).
+g.test_memtx_mvcc_space_cnt_with_invariant_violation = function(cg)
+    cg.server:exec(function()
+        local t = require('luatest')
+
+        for _ = 1, 5 do
+            box.begin()
+            box.space.s:replace{1}
+            box.space.s:replace{2}
+            box.commit()
+        end
+
+        t.assert_equals(box.space.s:count(), #box.space.s:select{})
+    end)
+end
