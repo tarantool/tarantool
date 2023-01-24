e02bc87feba73ce42e00719f1d40f2c0d363dc72
Vladimir Davydov <vdavydov@tarantool.org>
2023-01-24 11:35:05 +0300

    box: move flightrec configuration out of CE repository
    We define flight_recorder_cfg struct, box_get_flightrec_cfg function,
    and box.internal.cfg_configure_flightrec Lua function in the CE
    repository although they are actually needed only in the EE repository.
    Let's drop them all from the CE repository and instead define stub
    functions box_check_flightrec and box_set_flightrec that would
    check/apply box.cfg flight recorder parameters.
    
    While we are at it, add missing comments to flightrec function stubs.
    
    NO_DOC=refactoring
    NO_TEST=refactoring
    NO_CHANGELOG=refactoring

--- src/box/flightrec.h
+++ src/box/flightrec.h
@@ -15,71 +15,20 @@
 extern "C" {
 #endif /* defined(__cplusplus) */
 
-#include <assert.h>
-#include <stdarg.h>
-#include <stdbool.h>
 #include <stddef.h>
-#include <stdint.h>
 
-/** Flight recorder config options. */
-struct flight_recorder_cfg {
-	/**
-	 * Whether flight recorder should be enabled or disabled.
-	 */
-	bool enabled;
-	/**
-	 * Directory to store flight_records.ttfr file.
-	 */
-	const char *dir;
-	/** Total size of stored logs. */
-	int64_t logs_size;
-	/** Max size of one log message. */
-	int64_t log_max_msg_size;
-	/** Flight recorder log level; may be different from say log level. */
-	int logs_log_level;
-	/**
-	 * Time interval (in seconds) between metrics
-	 * dump to flight recorder.
-	 */
-	double metrics_interval;
-	/**
-	 * Period (in seconds) of metrics storage, i.e. how long
-	 * metrics are stored before being overwritten.
-	 */
-	int64_t metrics_period;
-	/** Total size of stored requests and responses. */
-	int64_t requests_size;
-	/** Max size per request. */
-	int64_t requests_max_req_size;
-	/** Max size per response. */
-	int64_t requests_max_res_size;
-};
+struct obuf;
+struct obuf_svp;
 
-/** No-op in OS version. */
-static inline int
-flightrec_cfg(const struct flight_recorder_cfg *cfg)
-{
-	assert(!cfg->enabled);
-	(void)cfg;
-	return 0;
-}
-
-/** No-op in OS version. */
+/**
+ * Release resources and clean-up flight recorder.
+ */
 static inline void
 flightrec_free(void)
 {
 }
 
-/** No-op in OS version. */
-static inline int
-flightrec_check_cfg(const struct flight_recorder_cfg *cfg)
-{
-	assert(!cfg->enabled);
-	(void)cfg;
-	return 0;
-}
-
-/** No-op in OS version. */
+/** Dump request (which is already packed into msgapck) to flight recorder. */
 static inline void
 flightrec_write_request(const char *request_msgpack, size_t len)
 {
@@ -87,15 +36,35 @@ flightrec_write_request(const char *request_msgpack, size_t len)
 	(void)len;
 }
 
-struct obuf;
-struct obuf_svp;
-
-/** No-op in OS version. */
+/**
+ * Dump response to flight recorder. Given savepoint points to the start of
+ * response stored into buffer.
+ */
 static inline void
 flightrec_write_response(struct obuf *buf, struct obuf_svp *svp)
 {
 	(void)buf;
 	(void)svp;
+}
+
+/**
+ * Checks box.cfg flight recorder parameters.
+ * On success, returns 0. On error, sets diag and returns -1.
+ */
+static inline int
+box_check_flightrec(void)
+{
+	return 0;
+}
+
+/**
+ * Applies box.cfg flight recorder parameters.
+ * On success, returns 0. On error, sets diag and returns -1.
+ */
+static inline int
+box_set_flightrec(void)
+{
+	return 0;
 }
 
 #if defined(__cplusplus)
