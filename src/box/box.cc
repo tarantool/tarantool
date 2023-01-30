tarantool/tarantool

commit ec1a71ffed2aa506f693c991b9ca81387a4a238a

box: introduce pagination to memtx_tree and tuple position methods
The patch introduces C and Lua methods for extracting position
of tuple in index. Multikey and functional indexes are not supported.

Also this patch extends method index_create_iterator_after and adds
implementation of iterator_position for memtx tree. Options
after and fetch_pos are added to Lua select as well (both FFI and LuaC).
All the types of memtx tree indexes are supported (multikey, functional).

NO_CHANGELOG=see later commits
NO_DOC=see later commits

Closes #7633
Closes #7636

extra/exports
@@ -66,6 +66,7 @@ box_index_len
 box_index_max
 box_index_min
 box_index_random
+box_index_tuple_position
 box_insert
 box_iterator_free
 box_iterator_next
@@ -86,6 +87,7 @@ box_latch_new
 box_latch_trylock
 box_latch_unlock
 box_on_shutdown
+box_read_ffi_disable
 box_read_ffi_is_disabled
 box_region_aligned_alloc
 box_region_alloc

src/box/box.cc
@@ -2708,13 +2708,15 @@ box_process1(struct request *request, box_tuple_t **result)
 	return box_process_rw(request, space, result);
 }
 
-API_EXPORT int
+int
 box_select(uint32_t space_id, uint32_t index_id,
 	   int iterator, uint32_t offset, uint32_t limit,
 	   const char *key, const char *key_end,
-	   struct port *port)
+	   const char **packed_pos, const char **packed_pos_end,
+	   bool update_pos, struct port *port)
 {
 	(void)key_end;
+	assert(!update_pos || (packed_pos != NULL && packed_pos_end != NULL));
 
 	rmean_collect(rmean_box, IPROTO_SELECT, 1);
 
@@ -2739,6 +2741,20 @@ box_select(uint32_t space_id, uint32_t index_id,
 	uint32_t part_count = key ? mp_decode_array(&key) : 0;
 	if (key_validate(index->def, type, key, part_count))
 		return -1;
+	const char *pos, *pos_end;
+	if (packed_pos != NULL && *packed_pos != NULL) {
+		uint32_t pos_part_count;
+		if (iterator_position_unpack(*packed_pos, *packed_pos_end,
+					     &pos, &pos_end,
+					     &pos_part_count) != 0)
+			return -1;
+		if (exact_key_validate_nullable(index->def->cmp_def, pos,
+						pos_part_count) != 0)
+			return -1;
+	} else {
+		pos = NULL;
+		pos_end = NULL;
+	}
 
 	box_run_on_select(space, index, type, key_array);
 
@@ -2752,8 +2768,8 @@ box_select(uint32_t space_id, uint32_t index_id,
 	if (txn_begin_ro_stmt(space, &txn, &svp) != 0)
 		return -1;
 
-	struct iterator *it = index_create_iterator(index, type,
-						    key, part_count);
+	struct iterator *it = index_create_iterator_after(index, type, key,
+							  part_count, pos);
 	if (it == NULL) {
 		txn_rollback_stmt(txn);
 		return -1;
@@ -2787,15 +2803,42 @@ box_select(uint32_t space_id, uint32_t index_id,
 		 */
 		space = iterator_space(it);
 	}
-	iterator_delete(it);
 
 	if (rc != 0) {
-		port_destroy(port);
 		txn_rollback_stmt(txn);
-		return -1;
+		goto fail;
 	}
 	txn_commit_ro_stmt(txn, &svp);
+	if (update_pos) {
+		uint32_t pos_size;
+		if (iterator_position(it, &pos, &pos_size) != 0)
+			goto fail;
+		if (pos != NULL) {
+			pos_end = pos + pos_size;
+			uint32_t part_count = index->def->cmp_def->part_count;
+			uint32_t pack_size =
+				iterator_position_pack_size(pos, pos_end,
+							    part_count);
+			char *new_packed_pos =
+				(char *)region_alloc(&fiber()->gc, pack_size);
+			if (new_packed_pos == NULL)
+				goto fail;
+			iterator_position_pack(pos, pos_end, part_count,
+					       new_packed_pos,
+					       new_packed_pos + pack_size);
+			*packed_pos = new_packed_pos;
+			*packed_pos_end = new_packed_pos + pack_size;
+		} else {
+			*packed_pos = NULL;
+			*packed_pos_end = NULL;
+		}
+	}
+	iterator_delete(it);
 	return 0;
+fail:
+	iterator_delete(it);
+	port_destroy(port);
+	return -1;
 }
 
 API_EXPORT int

src/box/lua/schema.lua
@@ -66,6 +66,10 @@ ffi.cdef[[
                     const char *key, const char *key_end);
     /** \endcond public */
     /** \cond public */
+    size_t
+    box_region_used(void);
+    void
+    box_region_truncate(size_t size);
     bool
     box_txn();
     int64_t
@@ -113,10 +117,16 @@ ffi.cdef[[
     port_destroy(struct port *port);
 
     int
+    box_index_tuple_position(uint32_t space_id, uint32_t index_id,
+                             const char *tuple, const char *tuple_end,
+                             const char **pos, const char **pos_end);
+
+    int
     box_select(uint32_t space_id, uint32_t index_id,
                int iterator, uint32_t offset, uint32_t limit,
                const char *key, const char *key_end,
-               struct port *port);
+               const char **after, const char **after_end,
+               bool update_pos, struct port *port);
 
     void password_prepare(const char *password, int len,
                           char *out, int out_len);
@@ -2405,6 +2415,8 @@ local function check_select_opts(opts, key_is_nil)
     local limit = 4294967295
     local iterator = check_iterator_type(opts, key_is_nil)
     local fullscan = false
+    local after = box.NULL
+    local fetch_pos = false
     if opts ~= nil and type(opts) == "table" then
         if opts.offset ~= nil then
             offset = opts.offset
@@ -2415,8 +2427,20 @@ local function check_select_opts(opts, key_is_nil)
         if opts.fullscan ~= nil then
             fullscan = opts.fullscan
         end
+        if opts.after ~= nil then
+            after = opts.after
+            if after == "" then
+                after = box.NULL
+            elseif type(after) ~= "string" and type(after) ~= "table" and
+                    not is_tuple(after) then
+                box.error(box.error.INVALID_POSITION)
+            end
+        end
+        if opts.fetch_pos ~= nil then
+            fetch_pos = opts.fetch_pos
+        end
     end
-    return iterator, offset, limit, fullscan
+    return iterator, offset, limit, fullscan, after, fetch_pos
 end
 
 box.internal.check_select_opts = check_select_opts -- for net.box
@@ -2435,20 +2459,55 @@ local function check_select_safety(index, key_is_nil, itype, limit, offset,
     end
 end
 
+-- pointer to iterator position used by select() and tuple_pos()
+local iterator_pos = ffi.new('const char *[1]')
+local iterator_pos_end = ffi.new('const char *[1]')
+
+-- Argument pos must be a string, table or tuple, returns two C pointers:
+-- begin and end of position.
+local function normalize_position(index, pos, ret_pos, ret_pos_end)
+    if pos == nil then
+        ret_pos[0] = nil
+        ret_pos_end[0] = nil
+    elseif type(pos) == "string" then
+        ret_pos[0] = pos
+        ret_pos_end[0] = ret_pos[0] + #pos
+    else
+        local tuple_ibuf = cord_ibuf_take()
+        local tuple, tuple_end = tuple_encode(tuple_ibuf, pos)
+        local nok = builtin.box_index_tuple_position(index.space_id, index.id,
+                                                     tuple, tuple_end,
+                                                     ret_pos, ret_pos_end) ~= 0
+        cord_ibuf_put(tuple_ibuf)
+        if nok then
+            box.error()
+        end
+    end
+end
+
 base_index_mt.select_ffi = function(index, key, opts)
     if builtin.box_read_ffi_is_disabled then
         return index:select_luac(key, opts)
     end
+    local nok
     check_index_arg(index, 'select')
     local ibuf = cord_ibuf_take()
     local key, key_end = tuple_encode(ibuf, key)
     local key_is_nil = key + 1 >= key_end
-    local iterator, offset, limit, fullscan =
+    local new_position = nil
+    local iterator, offset, limit, fullscan, after, fetch_pos =
         check_select_opts(opts, key_is_nil)
     check_select_safety(index, key_is_nil, iterator, limit, offset, fullscan)
-
-    local nok = builtin.box_select(index.space_id, index.id, iterator, offset,
-                                   limit, key, key_end, port) ~= 0
+    local region_svp = builtin.box_region_used()
+    normalize_position(index, after, iterator_pos, iterator_pos_end)
+    nok = builtin.box_select(index.space_id, index.id, iterator, offset, limit,
+                             key, key_end, iterator_pos, iterator_pos_end,
+                             fetch_pos, port) ~= 0
+    if not nok and fetch_pos and iterator_pos[0] ~= nil then
+        new_position = ffi.string(iterator_pos[0],
+                                  iterator_pos_end[0] - iterator_pos[0])
+    end
+    builtin.box_region_truncate(region_svp)
     cord_ibuf_put(ibuf)
     if nok then
         return box.error()
@@ -2461,6 +2520,9 @@ base_index_mt.select_ffi = function(index, key, opts)
         entry = entry.next
     end
     builtin.port_destroy(port);
+    if fetch_pos then
+        return ret, new_position
+    end
     return ret
 end
 
@@ -2468,11 +2530,11 @@ base_index_mt.select_luac = function(index, key, opts)
     check_index_arg(index, 'select')
     local key = keify(key)
     local key_is_nil = #key == 0
-    local iterator, offset, limit, fullscan =
+    local iterator, offset, limit, fullscan, after, fetch_pos =
         check_select_opts(opts, key_is_nil)
     check_select_safety(index, key_is_nil, iterator, limit, offset, fullscan)
     return internal.select(index.space_id, index.id, iterator,
-        offset, limit, key)
+        offset, limit, key, after, fetch_pos)
 end
 
 base_index_mt.update = function(index, key, ops)
@@ -2506,6 +2568,26 @@ base_index_mt.alter = function(index, options)
         box.error(box.error.PROC_LUA, "Usage: index:alter{opts}")
     end
     return box.schema.index.alter(index.space_id, index.id, options)
+end
+base_index_mt.tuple_pos = function(index, tuple)
+    check_index_arg(index, 'tuple_pos')
+    if not tuple then
+        box.error(box.error.PROC_LUA, "Usage index:tuple_pos(tuple)")
+    end
+    local region_svp = builtin.box_region_used()
+    local ibuf = cord_ibuf_take()
+    local data, data_end = tuple_encode(ibuf, tuple)
+    local nok = builtin.box_index_tuple_position(index.space_id, index.id,
+                                                 data, data_end, iterator_pos,
+                                                 iterator_pos_end) ~= 0
+    cord_ibuf_put(ibuf)
+    if nok then
+        box.error()
+    end
+    local ret = ffi.string(iterator_pos[0],
+                           iterator_pos_end[0] - iterator_pos[0])
+    builtin.box_region_truncate(region_svp)
+    return ret
 end
 
 local read_ops = {'select', 'get', 'min', 'max', 'count', 'random', 'pairs'}

src/box/box.h
@@ -332,12 +332,22 @@ box_promote_qsync(void);
 int
 box_wait_linearization_point(double timeout);
 
-/* box_select is private and used only by FFI */
-API_EXPORT int
+/**
+ * Select data, satisfying filters (key and iterator), and dump it to port.
+ * If packed_pos is not NULL and *packed_pos is not NULL, selection begins
+ * right after tuple with position, described by this argument.
+ * If update_pos is true, packed_pos and packed_pos_end are updated to
+ * position of last selected tuple. Returned position is allocated
+ * on the fiber region.
+ * Pre-requesites: if update_pos is true, packed_pos and packed_pos_end must
+ * not be NULL.
+ */
+int
 box_select(uint32_t space_id, uint32_t index_id,
 	   int iterator, uint32_t offset, uint32_t limit,
 	   const char *key, const char *key_end,
-	   struct port *port);
+	   const char **packed_pos, const char **packed_pos_end,
+	   bool update_pos, struct port *port);
 
 /** \cond public */
 

src/box/index.cc
@@ -153,6 +153,20 @@ exact_key_validate(struct key_def *key_def, const char *key,
 	return key_validate_parts(key_def, key, part_count, false, &key_end);
 }
 
+int
+exact_key_validate_nullable(struct key_def *key_def, const char *key,
+			    uint32_t part_count)
+{
+	assert(key != NULL || part_count == 0);
+	if (key_def->part_count != part_count) {
+		diag_set(ClientError, ER_EXACT_MATCH, key_def->part_count,
+			 part_count);
+		return -1;
+	}
+	const char *key_end;
+	return key_validate_parts(key_def, key, part_count, true, &key_end);
+}
+
 char *
 box_tuple_extract_key(box_tuple_t *tuple, uint32_t space_id, uint32_t index_id,
 		      uint32_t *key_size)
@@ -179,6 +193,50 @@ check_index(uint32_t space_id, uint32_t index_id,
 	*index = index_find(*space, index_id);
 	if (*index == NULL)
 		return -1;
+	return 0;
+}
+
+int
+box_index_tuple_position(uint32_t space_id, uint32_t index_id,
+			 const char *tuple, const char *tuple_end,
+			 const char **packed_pos, const char **packed_pos_end)
+{
+	struct space *space;
+	struct index *index;
+	if (check_index(space_id, index_id, &space, &index) != 0)
+		return -1;
+	if (index->def->key_def->for_func_index) {
+		diag_set(ClientError, ER_UNSUPPORTED, "Functional index",
+			 "position by tuple");
+		return -1;
+	}
+	if (index->def->key_def->is_multikey) {
+		diag_set(ClientError, ER_UNSUPPORTED, "Multikey index",
+			 "position by tuple");
+		return -1;
+	}
+	if (tuple_validate_raw(space->format, tuple) != 0)
+		return -1;
+	struct key_def *cmp_def = index->def->cmp_def;
+	uint32_t key_size;
+	const char *key = tuple_extract_key_raw(tuple, tuple_end, cmp_def,
+						MULTIKEY_NONE, &key_size);
+	if (key == NULL)
+		return -1;
+	const char *key_end = key + key_size;
+	mp_decode_array(&key);
+	uint32_t part_count = cmp_def->part_count;
+	uint32_t pack_size = iterator_position_pack_size(key, key_end,
+							 part_count);
+	char *buf = (char *)region_alloc(&fiber()->gc, pack_size);
+	if (buf == NULL) {
+		diag_set(OutOfMemory, pack_size, "region_alloc",
+			 "box_index_tuple_position");
+		return -1;
+	}
+	iterator_position_pack(key, key_end, part_count, buf, buf + pack_size);
+	*packed_pos = buf;
+	*packed_pos_end = buf + pack_size;
 	return 0;
 }
 

src/box/index.h
@@ -253,6 +253,16 @@ box_tuple_extract_key(box_tuple_t *tuple, uint32_t space_id,
 /** \endcond public */
 
 /**
+ * Get packed position of tuple in index to pass it to box_select
+ * (multikey and func indexes are not supported). Returned position
+ * is allocated on the fiber region.
+ */
+int
+box_index_tuple_position(uint32_t space_id, uint32_t index_id,
+			 const char *tuple, const char *tuple_end,
+			 const char **packed_pos, const char **packed_pos_end);
+
+/**
  * Index statistics (index:stat())
  *
  * \param space_id space identifier
@@ -444,6 +454,16 @@ key_validate(const struct index_def *index_def, enum iterator_type type,
 int
 exact_key_validate(struct key_def *key_def, const char *key,
 		   uint32_t part_count);
+
+/**
+ * Check that the supplied key is valid for representing iterator
+ * position (i.e. the key must be fully specified, but nulls are allowed).
+ * @retval 0  The key is valid.
+ * @retval -1 The key is invalid.
+ */
+int
+exact_key_validate_nullable(struct key_def *key_def, const char *key,
+			    uint32_t part_count);
 
 /**
  * The manner in which replace in a unique index must treat

src/box/iproto.cc
@@ -2088,7 +2088,7 @@ tx_process_select(struct cmsg *m)
 	tx_inject_delay();
 	rc = box_select(req->space_id, req->index_id,
 			req->iterator, req->offset, req->limit,
-			req->key, req->key_end, &port);
+			req->key, req->key_end, NULL, NULL, false, &port);
 	if (rc < 0)
 		goto error;
 

src/box/lua/misc.cc
@@ -63,6 +63,35 @@ lbox_encode_tuple_on_gc(lua_State *L, int idx, size_t *p_len)
 }
 
 /**
+ * Convert packed position, tuple or table from Lua to C packed position.
+ * Returned position is allocated on the fiber region.
+ */
+static int
+lbox_normalize_position(lua_State *L, int idx, int space_id, int index_id,
+			const char **packed_pos, const char **packed_pos_end)
+{
+	if (luaL_isnull(L, idx)) {
+		*packed_pos = NULL;
+		*packed_pos_end = NULL;
+	} else if (lua_isstring(L, idx)) {
+		size_t size;
+		*packed_pos = lua_tolstring(L, idx, &size);
+		*packed_pos_end = *packed_pos + size;
+	} else if (lua_istable(L, idx) || luaT_istuple(L, idx) != NULL) {
+		size_t size;
+		const char *tuple = lbox_encode_tuple_on_gc(L, idx, &size);
+		if (box_index_tuple_position(space_id, index_id, tuple,
+					     tuple + size, packed_pos,
+					     packed_pos_end) != 0)
+			return -1;
+	} else {
+		diag_set(ClientError, ER_ITERATOR_POSITION);
+		return -1;
+	}
+	return 0;
+}
+
+/**
  * __index metamethod for the formatted array table that lookups field by name.
  * Metatable of the table is expected to have `field_map` that provides
  * name->index dictionary.
@@ -184,11 +213,16 @@ port_msgpack_dump_lua(struct port *base, struct lua_State *L, bool is_flat)
 static int
 lbox_select(lua_State *L)
 {
-	if (lua_gettop(L) != 6 || !lua_isnumber(L, 1) || !lua_isnumber(L, 2) ||
-		!lua_isnumber(L, 3) || !lua_isnumber(L, 4) || !lua_isnumber(L, 5)) {
+	if (lua_gettop(L) != 8 || !lua_isnumber(L, 1) || !lua_isnumber(L, 2) ||
+	    !lua_isnumber(L, 3) || !lua_isnumber(L, 4) || !lua_isnumber(L, 5) ||
+	    !lua_isboolean(L, 8)) {
 		return luaL_error(L, "Usage index:select(iterator, offset, "
-				  "limit, key)");
+				  "limit, key, after, fetch_pos)");
 	}
+
+	uint32_t svp = region_used(&fiber()->gc);
+	int ret_count = 1;
+	struct port port;
 
 	uint32_t space_id = lua_tonumber(L, 1);
 	uint32_t index_id = lua_tonumber(L, 2);
@@ -198,13 +232,16 @@ lbox_select(lua_State *L)
 
 	size_t key_len;
 	const char *key = lbox_encode_tuple_on_gc(L, 6, &key_len);
+	const char *packed_pos, *packed_pos_end;
+	bool fetch_pos = lua_toboolean(L, 8);
+	if (lbox_normalize_position(L, 7, space_id, index_id, &packed_pos,
+				    &packed_pos_end) != 0)
+		goto fail;
 
-	struct port port;
-	if (box_select(space_id, index_id, iterator, offset, limit,
-		       key, key + key_len, &port) != 0) {
-		return luaT_error(L);
-	}
-
+	if (box_select(space_id, index_id, iterator, offset, limit, key,
+		       key + key_len, &packed_pos, &packed_pos_end, fetch_pos,
+		       &port) != 0)
+		goto fail;
 	/*
 	 * Lua may raise an exception during allocating table or pushing
 	 * tuples. In this case `port' definitely will leak. It is possible to
@@ -216,7 +253,15 @@ lbox_select(lua_State *L)
 	 */
 	port_dump_lua(&port, L, false);
 	port_destroy(&port);
-	return 1; /* lua table with tuples */
+	if (fetch_pos && packed_pos != NULL) {
+		lua_pushlstring(L, packed_pos, packed_pos_end - packed_pos);
+		ret_count++;
+	}
+	region_truncate(&fiber()->gc, svp);
+	return ret_count;
+fail:
+	region_truncate(&fiber()->gc, svp);
+	return luaT_error(L);
 }
 
 /* }}} */

commit 197ff80cc8473d97a9eb29de96ef850d87ecd23a

box: make ffi select work on M1
Recently, ffi select was broken on M1 in commit ec1a71ffed2a
("box: introduce pagination to memtx_tree and tuple position methods").
It turned out that ffi on M1 poorly supports a big quantity of arguments.
Fortunately, there is a workaround - we can pass only 64-bit integer
arguments beyond the 8th argument. Let's do it.

Closes #7946

NO_TEST=reflected in existing tests
NO_CHANGELOG=bugfix for unreleased feature
NO_DOC=bugfix

extra/exports
@@ -101,7 +101,7 @@ box_replace
 box_return_mp
 box_return_tuple
 box_schema_version
-box_select
+box_select_ffi
 box_sequence_current
 box_sequence_next
 box_sequence_reset

src/box/box.cc
@@ -3075,6 +3075,22 @@ fail:
 	return -1;
 }
 
+/**
+ * A special wrapper for FFI - workaround for M1.
+ * Use 64-bit integers beyond the 8th argument.
+ * See https://github.com/LuaJIT/LuaJIT/issues/205 for details.
+ */
+extern "C" int
+box_select_ffi(uint32_t space_id, uint32_t index_id, const char *key,
+	       const char *key_end, const char **packed_pos,
+	       const char **packed_pos_end, bool update_pos, struct port *port,
+	       int64_t iterator, uint64_t offset, uint64_t limit)
+{
+	return box_select(space_id, index_id, iterator, offset, limit, key,
+			  key_end, packed_pos, packed_pos_end, update_pos,
+			  port);
+}
+
 API_EXPORT int
 box_insert(uint32_t space_id, const char *tuple, const char *tuple_end,
 	   box_tuple_t **result)

src/box/lua/schema.lua
@@ -118,11 +118,11 @@ ffi.cdef[[
                              const char **pos, const char **pos_end);
 
     int
-    box_select(uint32_t space_id, uint32_t index_id,
-               int iterator, uint32_t offset, uint32_t limit,
-               const char *key, const char *key_end,
-               const char **after, const char **after_end,
-               bool update_pos, struct port *port);
+    box_select_ffi(uint32_t space_id, uint32_t index_id, const char *key,
+                   const char *key_end, const char **packed_pos,
+                   const char **packed_pos_end, bool update_pos,
+                   struct port *port, int64_t iterator, uint64_t offset,
+                   uint64_t limit);
 
     enum priv_type {
         PRIV_R = 1,
@@ -2537,9 +2537,9 @@ base_index_mt.select_ffi = function(index, key, opts)
     check_select_safety(index, key_is_nil, iterator, limit, offset, fullscan)
     local region_svp = builtin.box_region_used()
     normalize_position(index, after, iterator_pos, iterator_pos_end)
-    nok = builtin.box_select(index.space_id, index.id, iterator, offset, limit,
-                             key, key_end, iterator_pos, iterator_pos_end,
-                             fetch_pos, port) ~= 0
+    nok = builtin.box_select_ffi(index.space_id, index.id, key, key_end,
+                                 iterator_pos, iterator_pos_end, fetch_pos,
+                                 port, iterator, offset, limit) ~= 0
     if not nok and fetch_pos and iterator_pos[0] ~= nil then
         new_position = ffi.string(iterator_pos[0],
                                   iterator_pos_end[0] - iterator_pos[0])

static-build/test/exports.test.lua
@@ -114,7 +114,6 @@ local check_symbols = {
     'coio_getaddrinfo',
     'luaT_call',
     'box_txn',
-    'box_select',
     'clock_realtime',
     'string_strip_helper',
 