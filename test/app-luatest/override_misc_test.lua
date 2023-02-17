18a00730b9199777ce4feda702dc1a37c711362e
Alexander Turenko <alexander.turenko@tarantool.org>
2023-02-17 08:11:40 +0300

    lua: allow to refuse caching in package.loaded
    Use case: an override module is designed to replace a built-in module
    only when the module is required from an application, not from the
    platform itself.
    
    Now it is possible:
    
    ```lua
    -- override/fio.lua
    local loaders = require('internal.loaders')
    
    if loaders.initializing then
        loaders.no_package_loaded.fio = true
        return loaders.builtin.fio
    end
    
    return {
        whoami = 'override.fio',
    }
    ```
    
    Follows up #7774
    
    NO_DOC=Planned as an internal API. See the previous commit.
    NO_CHANGELOG=see NO_DOC

--- test/app-luatest/override_misc_test.lua
+++ test/app-luatest/override_misc_test.lua
@@ -65,3 +65,26 @@ g.test_initializing = function(g)
     local exp = expected_output('socket')
     t.assert_equals(res, exp)
 end
+
+g.test_no_package_loaded = function()
+    local LOAD_LIMIT = 3
+    local LOAD_ATTEMPTS = LOAD_LIMIT + 1
+
+    local load_cnt = 0
+    local foo = {whoami = 'foo'}
+    package.preload.foo = function()
+        local loaders = require('internal.loaders')
+        load_cnt = load_cnt + 1
+        if load_cnt < LOAD_LIMIT then
+            loaders.no_package_loaded.foo = true
+        end
+        return foo
+    end
+
+    for _ = 1, LOAD_ATTEMPTS do
+        require('foo')
+    end
+
+    package.preload.foo = nil
+    t.assert_equals(load_cnt, LOAD_LIMIT)
+end
