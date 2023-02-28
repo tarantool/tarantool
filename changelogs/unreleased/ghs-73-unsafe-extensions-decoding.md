053ca738161227e4bcca7dbd905fa3677af7b6e8
Georgiy Lebedev <g.lebedev@tarantool.org>
2023-02-28 14:59:02 +0300

    msgpack: fix unsafe extension decoding
    In some cases unsafe extension decoding was done without bound and type
    checks: add necessary checks.
    
    Closes tarantool/security#73
    
    NO_DOC=bugfix

---
+++ changelogs/unreleased/ghs-73-unsafe-extensions-decoding.md
@@ -0,0 +1,4 @@
+## bugfix/core
+
+* Fixed various bugs related to unsafe (i.e., coming from an unknown source)
+  decoding and validating of MsgPack extensions (ghs-73).
--- src/lib/core/mp_interval.h
+++ src/lib/core/mp_interval.h
@@ -21,13 +21,14 @@ mp_sizeof_interval(const struct interval *itv);
  * Load an interval value from the buffer.
  *
  * @param data A buffer.
+ * @param len Buffer size.
  * @param[out] itv Interval to be decoded.
  * @return A pointer to the decoded interval.
  *         NULL in case of an error.
  * @post *data = next value after packed interval value.
  */
 struct interval *
-interval_unpack(const char **data, struct interval *itv);
+interval_unpack(const char **data, uint32_t len, struct interval *itv);
 
 /**
  * Decode an interval value from MsgPack data.
@@ -58,23 +59,25 @@ mp_encode_interval(char *data, const struct interval *itv);
  * @param buf Target buffer to write string to.
  * @param size Buffer size.
  * @param data MessagePack encoded interval, without MP_EXT header.
+ * @param len MessagePack size.
  * @retval < 0 Error.
  * @retval >= 0 How many bytes were written, or would have been written, if
  *              there was enough buffer space.
  */
 int
-mp_snprint_interval(char *buf, int size, const char **data);
+mp_snprint_interval(char *buf, int size, const char **data, uint32_t len);
 
 /**
  * Print interval's string representation into a stream.
  *
  * @param file Target stream to write string to.
  * @param data MessagePack encoded interval, without MP_EXT header.
+ * @param len MessagePack size.
  * @retval < 0 Error.
  * @retval >= 0 How many bytes were written.
  */
 int
-mp_fprint_interval(FILE *file, const char **data);
+mp_fprint_interval(FILE *file, const char **data, uint32_t len);
 
 /**
  * Check that the given buffer contains a valid interval.
