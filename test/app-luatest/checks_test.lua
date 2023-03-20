51458f62847b7756f6c05072add4bc9efd0bbadc
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2023-03-20 18:01:02 +0300

    test: disable override checks
    NO_DOC=Test update
    NO_CHANGELOG=Test update

--- test/app-luatest/checks_test.lua
+++ test/app-luatest/checks_test.lua
@@ -1,4 +1,9 @@
+-- luatest installs external checks module.
+-- After https://github.com/tarantool/checks/pull/54,
+-- external checks will override built-in one, yet
+-- we want to test built-in one here.
 local rock_utils = require('third_party.checks.test.rock_utils')
+rock_utils.remove_override('checks')
 rock_utils.assert_builtin('checks')
 
 require('third_party.checks.test.test')
