dd9b22ff99fed22cec69fb40817965273025da80
Oleg Chaplashkin <ochaplashkin@tarantool.org>
2023-05-26 19:51:46 +0400

    test: ban direct calling of box.cfg()
    Direct call and configuration of the runner instance is prohibited. Now
    if you need to test something with specific configuration use a server
    instance please (see luatest.Server module).
    
    In-scope-of tarantool/luatest#245
    
    NO_DOC=ban calling box.cfg
    NO_TEST=ban calling box.cfg
    NO_CHANGELOG=ban calling box.cfg
    
    (cherry picked from commit fc3426d81c2b06090b6410146c01d6c288bccceb)

---
+++ test/helper.lua
@@ -0,0 +1,20 @@
+-- There are situations when the Tarantool instance is configured
+-- for testing. For example:
+--
+--     g.test_foo = function()
+--         box.cfg{params}
+--         ...
+--         t.assert_equals(foo, bar)
+--
+--     g.test_bar = function()
+--         t.assert_equals(foo, bar)
+--
+-- We expect that the test environment will not be configured
+-- in the `test_bar` test, but this will not always be the case
+-- (depends on the order of execution of tests).
+-- We prohibit a direct call to `box.cfg` by overriding the
+-- `__call` meta method. Motivating issue tarantool/luatest#245.
+box.cfg = setmetatable({}, {__call = function()
+    error('A direct call to `box.cfg` is forbidden' ..
+          ' (you are changing the test runner instance)', 0)
+end})
