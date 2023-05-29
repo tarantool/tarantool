8741e539c62bee2d91408f46df0af7e0ef642f86
Vladimir Davydov <vdavydov@tarantool.org>
2023-05-29 17:14:58 +0300

    iproto: generate iproto_key_type from IPROTO_KEYS
    Let's merge the key value type information into IPROTO_KEYS to keep them
    close together.
    
    Follow-up #8443
    Follow-up commit b3fb883b5bd8 ("iproto: export IPROTO constants to Lua
    automatically")
    
    NO_DOC=refactoring
    NO_TEST=refactoring
    NO_CHANGELOG=refactoring

--- src/box/iproto_constants.c
+++ src/box/iproto_constants.c
@@ -30,135 +30,6 @@
  */
 #include "iproto_constants.h"
 
-const unsigned char iproto_key_type[iproto_key_MAX] =
-{
-	/* {{{ header */
-		/* 0x00 */	MP_UINT,   /* IPROTO_REQUEST_TYPE */
-		/* 0x01 */	MP_UINT,   /* IPROTO_SYNC */
-		/* 0x02 */	MP_UINT,   /* IPROTO_REPLICA_ID */
-		/* 0x03 */	MP_UINT,   /* IPROTO_LSN */
-		/* 0x04 */	MP_DOUBLE, /* IPROTO_TIMESTAMP */
-		/* 0x05 */	MP_UINT,   /* IPROTO_SCHEMA_VERSION */
-		/* 0x06 */	MP_UINT,   /* IPROTO_SERVER_VERSION */
-		/* 0x07 */	MP_UINT,   /* IPROTO_GROUP_ID */
-		/* 0x08 */	MP_UINT,   /* IPROTO_TSN */
-		/* 0x09 */	MP_UINT,   /* IPROTO_FLAGS */
-		/* 0x0a */	MP_UINT,   /* IPROTO_STREAM_ID */
-	/* }}} */
-
-	/* {{{ unused */
-		/* 0x0b */	MP_UINT,
-		/* 0x0c */	MP_UINT,
-		/* 0x0d */	MP_UINT,
-		/* 0x0e */	MP_UINT,
-		/* 0x0f */	MP_UINT,
-	/* }}} */
-
-	/* {{{ body -- integer keys */
-		/* 0x10 */	MP_UINT, /* IPROTO_SPACE_ID */
-		/* 0x11 */	MP_UINT, /* IPROTO_INDEX_ID */
-		/* 0x12 */	MP_UINT, /* IPROTO_LIMIT */
-		/* 0x13 */	MP_UINT, /* IPROTO_OFFSET */
-		/* 0x14 */	MP_UINT, /* IPROTO_ITERATOR */
-		/* 0x15 */	MP_UINT, /* IPROTO_INDEX_BASE */
-	/* }}} */
-
-	/* {{{ unused */
-		/* 0x16 */	MP_UINT,
-		/* 0x17 */	MP_UINT,
-		/* 0x18 */	MP_UINT,
-		/* 0x19 */	MP_UINT,
-		/* 0x1a */	MP_UINT,
-		/* 0x1b */	MP_UINT,
-		/* 0x1c */	MP_UINT,
-		/* 0x1d */	MP_UINT,
-		/* 0x1e */	MP_UINT,
-	/* }}} */
-
-	/* {{{ body -- all keys */
-	/* 0x1f */	MP_BOOL,  /* IPROTO_FETCH_POSITION */
-	/* 0x20 */	MP_ARRAY, /* IPROTO_KEY */
-	/* 0x21 */	MP_ARRAY, /* IPROTO_TUPLE */
-	/* 0x22 */	MP_STR, /* IPROTO_FUNCTION_NAME */
-	/* 0x23 */	MP_STR, /* IPROTO_USER_NAME */
-	/* 0x24 */	MP_STR, /* IPROTO_INSTANCE_UUID */
-	/* 0x25 */	MP_STR, /* IPROTO_REPLICASET_UUID */
-	/* 0x26 */	MP_MAP, /* IPROTO_VCLOCK */
-	/* 0x27 */	MP_STR, /* IPROTO_EXPR */
-	/* 0x28 */	MP_ARRAY, /* IPROTO_OPS */
-	/* 0x29 */	MP_MAP, /* IPROTO_BALLOT */
-	/* 0x2a */	MP_MAP, /* IPROTO_TUPLE_META */
-	/* 0x2b */	MP_MAP, /* IPROTO_OPTIONS */
-	/* 0x2c */	MP_ARRAY, /* IPROTO_OLD_TUPLE */
-	/* 0x2d */	MP_ARRAY, /* IPROTO_NEW_TUPLE */
-	/* 0x2e */	MP_STR, /* IPROTO_AFTER_POSITION */
-	/* 0x2f */	MP_ARRAY, /* IPROTO_AFTER_TUPLE */
-	/* }}} */
-
-	/* {{{ body -- response keys */
-	/* 0x30 */	MP_ARRAY, /* IPROTO_DATA */
-	/* 0x31 */	MP_STR, /* IPROTO_ERROR_24 */
-	/* 0x32 */	MP_ARRAY, /* IPROTO_METADATA */
-	/* 0x33 */	MP_ARRAY, /* IPROTO_BIND_METADATA */
-	/* 0x34 */	MP_UINT, /* IIPROTO_BIND_COUNT */
-	/* 0x35 */	MP_STR, /* IPROTO_POSITION */
-	/* }}} */
-
-	/* {{{ unused */
-	/* 0x36 */	MP_UINT,
-	/* 0x37 */	MP_UINT,
-	/* 0x38 */	MP_UINT,
-	/* 0x39 */	MP_UINT,
-	/* 0x3a */	MP_UINT,
-	/* 0x3b */	MP_UINT,
-	/* 0x3c */	MP_UINT,
-	/* 0x3d */	MP_UINT,
-	/* 0x3e */	MP_UINT,
-	/* 0x3f */	MP_UINT,
-	/* }}} */
-
-	/* {{{ body -- sql keys */
-	/* 0x40 */	MP_STR, /* IPROTO_SQL_TEXT */
-	/* 0x41 */	MP_ARRAY, /* IPROTO_SQL_BIND */
-	/* 0x42 */	MP_MAP, /* IPROTO_SQL_INFO */
-	/* 0x43 */	MP_UINT, /* IPROTO_STMT_ID */
-	/* }}} */
-
-	/* {{{ unused */
-	/* 0x44 */	MP_UINT,
-	/* 0x45 */	MP_UINT,
-	/* 0x46 */	MP_UINT,
-	/* 0x47 */	MP_UINT,
-	/* 0x48 */	MP_UINT,
-	/* 0x49 */	MP_UINT,
-	/* 0x4a */	MP_UINT,
-	/* 0x4b */	MP_UINT,
-	/* 0x4c */	MP_UINT,
-	/* 0x4d */	MP_UINT,
-	/* 0x4e */	MP_UINT,
-	/* 0x4f */	MP_UINT,
-	/* }}} */
-
-	/* {{{ body -- additional request keys */
-	/* 0x50 */	MP_BOOL, /* IPROTO_REPLICA_ANON */
-	/* 0x51 */	MP_ARRAY, /* IPROTO_ID_FILTER */
-	/* 0x52 */	MP_MAP, /* IPROTO_ERROR */
-	/* 0x53 */	MP_UINT, /* IPROTO_TERM */
-	/* 0x54 */	MP_UINT, /* IPROTO_VERSION */
-	/* 0x55 */	MP_ARRAY, /* IPROTO_FEATURES */
-	/* 0x56 */	MP_DOUBLE, /* IPROTO_TIMEOUT */
-	/* 0x57 */	MP_STR, /* IPROTO_EVENT_KEY */
-	/* 0x58 */	MP_NIL, /* IPROTO_EVENT_DATA (can be any) */
-	/* 0x59 */	MP_UINT, /* IPROTO_TXN_ISOLATION */
-	/* 0x5a */	MP_UINT, /* IPROTO_VCLOCK_SYNC */
-	/* 0x5b */	MP_STR, /* IPROTO_AUTH_TYPE */
-	/* 0x5c */	MP_STR, /* IPROTO_REPLICASET_NAME */
-	/* 0x5d */	MP_STR, /* IPROTO_INSTANCE_NAME */
-	/* 0x5e */	MP_STR, /* IPROTO_SPACE_NAME */
-	/* 0x5f */	MP_STR, /* IPROTO_INDEX_NAME */
-	/* }}} */
-};
-
 const char *iproto_type_strs[] =
 {
 	NULL,
@@ -333,6 +204,13 @@ const struct iproto_constant iproto_key_constants[] = {
 };
 
 const size_t iproto_key_constants_size = lengthof(iproto_key_constants);
+
+#define IPROTO_KEY_TYPE_MEMBER(s, v, t) \
+	[IPROTO_ ## s] = t,
+
+const unsigned char iproto_key_type[iproto_key_MAX] = {
+	IPROTO_KEYS(IPROTO_KEY_TYPE_MEMBER)
+};
 
 const struct iproto_constant iproto_metadata_key_constants[] = {
 	IPROTO_METADATA_KEYS(IPROTO_CONSTANT_MEMBER)
