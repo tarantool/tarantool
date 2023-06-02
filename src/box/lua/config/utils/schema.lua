97441f4a749391a4fb9db00a8d1a4213f4ddef06
Alexander Turenko <alexander.turenko@tarantool.org>
2023-06-03 02:53:51 +0300

    config/schema: add <schema object>:validate()
    The config module should ensure that a received configuration data
    corresponds to a given schema before start to process it.
    
    This commit implements a simple validation procedure.
    
    There are possible optimization opportunities, such as generation a
    validation code by a given schema.
    
    No optimization tricks are used here, because the configuration
    validation occurs at the configuration update. It is not a frequent
    operation.
    
    Part of #8725
    
    NO_DOC=the module is for internal use from the config code
    NO_CHANGELOG=see NO_DOC

--- src/box/lua/config/utils/schema.lua
+++ src/box/lua/config/utils/schema.lua
@@ -5,6 +5,81 @@ local schema_mt = {}
 
 local scalars = {}
 
+-- {{{ Walkthrough helpers
+
+-- Create walkthrough context.
+local function walkthrough_start(self, params)
+    local ctx = {path = {}, name = rawget(self, 'name')}
+    for k, v in pairs(params or {}) do
+        ctx[k] = v
+    end
+    return ctx
+end
+
+-- Step down to a field.
+local function walkthrough_enter(ctx, name)
+    table.insert(ctx.path, name)
+end
+
+-- Step up from the last field.
+local function walkthrough_leave(ctx)
+    table.remove(ctx.path)
+end
+
+-- Construct a string that describes the current path.
+local function walkthrough_path(ctx)
+    local res = ''
+    for _, name in ipairs(ctx.path) do
+        if type(name) == 'number' then
+            res = res .. ('[%d]'):format(name)
+        else
+            res = res .. '.' .. name
+        end
+    end
+    if res:startswith('.') then
+        res = res:sub(2)
+    end
+    return res
+end
+
+-- Generate a prefix for an error message based on the given
+-- walkthrough context.
+local function walkthrough_error_prefix(ctx)
+    if ctx.path == nil or next(ctx.path) == nil then
+        return ('[%s] '):format(ctx.name)
+    end
+    return ('[%s] %s: '):format(ctx.name, walkthrough_path(ctx))
+end
+
+-- Generate an error supplemented by details from the given
+-- walkthrough context.
+local function walkthrough_error(ctx, message, ...)
+    local error_prefix = walkthrough_error_prefix(ctx)
+    error(('%s%s'):format(error_prefix, message:format(...)), 0)
+end
+
+-- Verify that the data is a table and, if it is not so, produce a
+-- nice schema-aware error.
+--
+-- Applicable for a record, a map, an array.
+--
+-- Useful as part of validation, but also as a lightweight
+-- consistency check.
+local function walkthrough_assert_table(ctx, schema, data)
+    assert(schema.type == 'record' or schema.type == 'map' or
+        schema.type == 'array')
+
+    if type(data) == 'table' then
+        return
+    end
+
+    local article = schema.type == 'array' and 'an' or 'a'
+    walkthrough_error(ctx, 'Unexpected data type for %s %s: %q', article,
+        schema.type, type(data))
+end
+
+-- }}} Walkthrough helpers
+
 -- {{{ Scalar definitions
 
 -- A scalar definition:
@@ -12,26 +87,74 @@ local scalars = {}
 -- {
 --     -- How the scalar is named.
 --     type = <string>,
+--     -- Check given data against the type constraints.
+--
+--     -> true (means the data is valid)
+--     -> false, err (otherwise)
+--     validate_noexc = <function>,
 -- }
+
+-- Verify whether the given value (data) has expected type and
+-- produce a human readable error message otherwise.
+local function validate_type_noexc(data, exp_type)
+    -- exp_type is a Lua type like 'string'.
+    assert(type(exp_type) == 'string')
+    if type(data) ~= exp_type then
+        local err = ('Expected %q, got %q'):format(exp_type, type(data))
+        return false, err
+    end
+    return true
+end
 
 scalars.string = {
     type = 'string',
+    validate_noexc = function(data)
+        return validate_type_noexc(data, 'string')
+    end,
 }
 
 scalars.number = {
     type = 'number',
+    validate_noexc = function(data)
+        -- TODO: Should we accept cdata<int64_t> and
+        -- cdata<uint64_t> here?
+        return validate_type_noexc(data, 'number')
+    end,
 }
 
 scalars.integer = {
     type = 'integer',
+    validate_noexc = function(data)
+        -- TODO: Accept cdata<int64_t> and cdata<uint64_t>.
+        local ok, err = validate_type_noexc(data, 'number')
+        if not ok then
+            return false, err
+        end
+        if data - math.floor(data) ~= 0 then
+            -- NB: %s is chosen deliberately: it formats a
+            -- floating-point number in a more human friendly way
+            -- than %f. For example, 5.5 vs 5.500000.
+            local err = ('Expected number without a fractional part, ' ..
+                'got %s'):format(data)
+            return false, err
+        end
+        return true
+    end,
 }
 
 scalars.boolean = {
     type = 'boolean',
+    validate_noexc = function(data)
+        return validate_type_noexc(data, 'boolean')
+    end,
 }
 
 scalars.any = {
     type = 'any',
+    validate_noexc = function(_data)
+        -- No validation.
+        return true
+    end,
 }
 
 local function is_scalar(schema)
@@ -141,6 +264,130 @@ local function array(array_def)
 end
 
 -- }}} Schema node constructors: scalar, record, map, array
+
+-- {{{ <schema object>:validate()
+
+-- Verify that the given table adheres array requirements.
+--
+-- It accepts an array without holes.
+--
+-- Strictly speaking,
+--
+-- * If the table is empty it is OK.
+-- * If the table is non-empty, the constraints are the following:
+--   * all keys are numeric, without a fractional part
+--   * the lower key is 1
+--   * the higher key is equal to the number of items
+local function validate_table_is_array(data, ctx)
+    assert(type(data) == 'table')
+
+    -- Check that all the keys are numeric.
+    local key_count = 0
+    local min_key = 1/0  -- +inf
+    local max_key = -1/0 -- -inf
+    for k, _ in pairs(data) do
+        if type(k) ~= 'number' then
+            walkthrough_error(ctx, 'An array contains a non-numeric ' ..
+                'key: %q', k)
+        end
+        if k - math.floor(k) ~= 0 then
+            walkthrough_error(ctx, 'An array contains a non-integral ' ..
+                'numeric key: %s', k)
+        end
+        key_count = key_count + 1
+        min_key = math.min(min_key, k)
+        max_key = math.max(max_key, k)
+    end
+
+    -- An empty array is a valid array.
+    if key_count == 0 then
+        return
+    end
+
+    -- Check that the array starts from 1 and has no holes.
+    if min_key ~= 1 then
+        walkthrough_error(ctx, 'An array must start from index 1, ' ..
+            'got min index %d', min_key)
+    end
+
+    -- Check that the array has no holes.
+    if max_key ~= key_count then
+        walkthrough_error(ctx, 'An array must not have holes, got ' ..
+            'a table with %d integer fields with max index %d', key_count,
+            max_key)
+    end
+end
+
+local function validate_impl(schema, data, ctx)
+    if is_scalar(schema) then
+        local scalar_def = scalars[schema.type]
+        assert(scalar_def ~= nil)
+
+        local ok, err = scalar_def.validate_noexc(data)
+        if not ok then
+            walkthrough_error(ctx, 'Unexpected data for scalar %q: %s',
+                schema.type, err)
+        end
+    elseif schema.type == 'record' then
+        walkthrough_assert_table(ctx, schema, data)
+
+        for field_name, field_def in pairs(schema.fields) do
+            walkthrough_enter(ctx, field_name)
+            local field = data[field_name]
+            -- Assume fields as non-required.
+            if field ~= nil then
+                validate_impl(field_def, field, ctx)
+            end
+            walkthrough_leave(ctx)
+        end
+
+        -- Walk over the data to catch unknown fields.
+        for field_name, _ in pairs(data) do
+            local field_def = schema.fields[field_name]
+            if field_def == nil then
+                walkthrough_error(ctx, 'Unexpected field %q', field_name)
+            end
+        end
+    elseif schema.type == 'map' then
+        walkthrough_assert_table(ctx, schema, data)
+
+        for field_name, field_value in pairs(data) do
+            walkthrough_enter(ctx, field_name)
+            validate_impl(schema.key, field_name, ctx)
+            validate_impl(schema.value, field_value, ctx)
+            walkthrough_leave(ctx)
+        end
+    elseif schema.type == 'array' then
+        walkthrough_assert_table(ctx, schema, data)
+        validate_table_is_array(data, ctx)
+
+        for i, v in ipairs(data) do
+            walkthrough_enter(ctx, i)
+            validate_impl(schema.items, v, ctx)
+            walkthrough_leave(ctx)
+        end
+    else
+        assert(false)
+    end
+end
+
+-- Validate the given data against the given schema.
+--
+-- Nuances:
+--
+-- * `schema.new('<...>', schema.scalar(<...>))` doesn't accept
+--   `nil` and `box.NULL`. However,
+-- * All fields in a record are optional: they accept `nil` and
+--   `box.NULL`.
+-- * The record/map/array determination is purely schema based.
+--   mt.__serialize marks in the data are not involved anyhow.
+-- * An array shouldn't have any holes (nil values in a middle).
+function methods.validate(self, data)
+    local ctx = walkthrough_start(self)
+    validate_impl(rawget(self, 'schema'), data, ctx)
+end
+
+-- }}} <schema object>:validate()
 
 -- {{{ Schema object constructor: new
 
