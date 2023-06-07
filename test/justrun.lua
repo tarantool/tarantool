f9fb067b7844d8d861bd079faed14eddabb6757c
Vladimir Davydov <vdavydov@tarantool.org>
2023-06-07 17:51:01 +0300

    test: import justrun and testgen helpers
    Imported from the master branch 0fb64e5a277c.
    
    NO_DOC=test util
    NO_TEST=test util
    NO_CHANGELOG=test util

---
+++ test/justrun.lua
@@ -0,0 +1,140 @@
+local fun = require('fun')
+local log = require('log')
+local json = require('json')
+local fiber = require('fiber')
+local popen = require('popen')
+
+local justrun = {}
+
+local function collect_stderr(ph)
+    local f = fiber.new(function()
+        local fiber_name = "child's stderr collector"
+        fiber.name(fiber_name, {truncate = true})
+
+        local chunks = {}
+
+        while true do
+            local chunk, err = ph:read({stderr = true})
+            if chunk == nil then
+                log.warn(('%s: got error, exiting: %s'):format(
+                    fiber_name, tostring(err)))
+                break
+            end
+            if chunk == '' then
+                log.info(('%s: got EOF, exiting'):format(fiber_name))
+                break
+            end
+            table.insert(chunks, chunk)
+        end
+
+        -- Glue all chunks, strip trailing newline.
+        return table.concat(chunks):rstrip()
+    end)
+    f:set_joinable(true)
+    return f
+end
+
+local function cancel_stderr_fiber(stderr_fiber)
+    if stderr_fiber == nil then
+        return
+    end
+    stderr_fiber:cancel()
+end
+
+local function join_stderr_fiber(stderr_fiber)
+    if stderr_fiber == nil then
+        return
+    end
+    return select(2, assert(stderr_fiber:join()))
+end
+
+-- Run tarantool in given directory with given environment and
+-- command line arguments and catch its output.
+--
+-- Expects JSON lines as the output and parses it into an array
+-- (it can be disabled using `nojson` option).
+--
+-- Options:
+--
+-- - nojson (boolean, default: false)
+--
+--   Don't attempt to decode stdout as a stream of JSON lines,
+--   return as is.
+--
+-- - stderr (boolean, default: false)
+--
+--   Collect stderr and place it into the `stderr` field of the
+--   return value
+function justrun.tarantool(dir, env, args, opts)
+    assert(type(dir) == 'string')
+    assert(type(env) == 'table')
+    assert(type(args) == 'table')
+    local opts = opts or {}
+    assert(type(opts) == 'table')
+
+    local tarantool_exe = arg[-1]
+    -- Use popen.shell() instead of popen.new() due to lack of
+    -- cwd option in popen (gh-5633).
+    local env_str = table.concat(fun.iter(env):map(function(k, v)
+        return ('%s=%q'):format(k, v)
+    end):totable(), ' ')
+    local command = ('cd %s && %s %s %s'):format(dir, env_str, tarantool_exe,
+                                                 table.concat(args, ' '))
+    log.info(('Running a command: %s'):format(command))
+    local mode = opts.stderr and 'rR' or 'r'
+    local ph = popen.shell(command, mode)
+
+    local stderr_fiber
+    if opts.stderr then
+        stderr_fiber = collect_stderr(ph)
+    end
+
+    -- Read everything until EOF.
+    local chunks = {}
+    while true do
+        local chunk, err = ph:read()
+        if chunk == nil then
+            cancel_stderr_fiber(stderr_fiber)
+            ph:close()
+            error(err)
+        end
+        if chunk == '' then -- EOF
+            break
+        end
+        table.insert(chunks, chunk)
+    end
+
+    local exit_code = ph:wait().exit_code
+    local stderr = join_stderr_fiber(stderr_fiber)
+    ph:close()
+
+    -- If an error occurs, discard the output and return only the
+    -- exit code. However, return stderr.
+    if exit_code ~= 0 then
+        return {
+            exit_code = exit_code,
+            stderr = stderr,
+        }
+    end
+
+    -- Glue all chunks, strip trailing newline.
+    local res = table.concat(chunks):rstrip()
+    log.info(('Command output:\n%s'):format(res))
+
+    -- Decode JSON object per line into array of tables (if
+    -- `nojson` option is not passed).
+    local decoded
+    if opts.nojson then
+        decoded = res
+    else
+        decoded = fun.iter(res:split('\n')):map(json.decode):totable()
+    end
+
+    return {
+        exit_code = exit_code,
+        stdout = decoded,
+        stderr = stderr,
+    }
+end
+
+return justrun
---
+++ test/treegen.lua
@@ -0,0 +1,136 @@
+-- Working tree generator.
+--
+-- Generates a tree of Lua files using provided templates and
+-- filenames.
+--
+-- Basic usage:
+--
+-- | local t = require('luatest')
+-- | local treegen = require('test.treegen')
+-- |
+-- | local g = t.group()
+-- |
+-- | local SCRIPT_TEMPLATE = [[
+-- |     <...>
+-- | ]]
+-- |
+-- | g.before_all(function(g)
+-- |     treegen.init(g)
+-- |     treegen.add_template(g, '^.*$', SCRIPT_TEMPLATE)
+-- | end)
+-- |
+-- | g.after_all(function(g)
+-- |     treegen.clean(g)
+-- | end)
+-- |
+-- | g.foobar_test = function(g)
+-- |     local dir = treegen.prepare_directory(g,
+-- |         {'foo/bar.lua', 'main.lua'})
+-- |     <..test case..>
+-- | end
+
+local fio = require('fio')
+local log = require('log')
+local fun = require('fun')
+
+local treegen = {}
+
+local function find_template(g, script)
+    for _, template_def in ipairs(g.templates) do
+        if script:match(template_def.pattern) then
+            return template_def.template
+        end
+    end
+    error(("treegen: can't find a template for script %q"):format(script))
+end
+
+-- Write provided script into the given directory.
+function treegen.write_script(dir, script, body)
+    local script_abspath = fio.pathjoin(dir, script)
+    local flags = {'O_CREAT', 'O_WRONLY', 'O_TRUNC'}
+    local mode = tonumber('644', 8)
+
+    local scriptdir_abspath = fio.dirname(script_abspath)
+    log.info(('Creating a directory: %s'):format(scriptdir_abspath))
+    fio.mktree(scriptdir_abspath)
+
+    log.info(('Writing a script: %s'):format(script_abspath))
+    local fh = fio.open(script_abspath, flags, mode)
+    fh:write(body)
+    fh:close()
+end
+
+-- Generate a script that follows a template and write it at the
+-- given path in the given directory.
+local function gen_script(g, dir, script, replacements)
+    local template = find_template(g, script)
+    local replacements = fun.chain({script = script}, replacements):tomap()
+    local body = template:gsub('<(.-)>', replacements)
+    treegen.write_script(dir, script, body)
+end
+
+function treegen.init(g)
+    g.tempdirs = {}
+    g.templates = {}
+end
+
+-- Remove all temporary directories created by the test
+-- unless KEEP_DATA environment variable is set to a
+-- non-empty value.
+function treegen.clean(g)
+    local dirs = table.copy(g.tempdirs)
+    g.tempdirs = nil
+
+    local keep_data = (os.getenv('KEEP_DATA') or '') ~= ''
+
+    for _, dir in ipairs(dirs) do
+        if keep_data then
+            log.info(('Left intact due to KEEP_DATA env var: %s'):format(dir))
+        else
+            log.info(('Recursively removing: %s'):format(dir))
+            fio.rmtree(dir)
+        end
+    end
+
+    g.templates = nil
+end
+
+function treegen.add_template(g, pattern, template)
+    table.insert(g.templates, {
+        pattern = pattern,
+        template = template,
+    })
+end
+
+-- Create a temporary directory with given scripts.
+--
+-- The scripts are generated using templates added by
+-- treegen.add_template().
+--
+-- Example for {'foo/bar.lua', 'baz.lua'}:
+--
+-- /
+-- + tmp/
+--   + rfbWOJ/
+--     + foo/
+--     | + bar.lua
+--     + baz.lua
+--
+-- The return value is '/tmp/rfbWOJ' for this example.
+function treegen.prepare_directory(g, scripts, replacements)
+    local replacements = replacements or {}
+
+    assert(type(scripts) == 'table')
+    assert(type(replacements) == 'table')
+
+    local dir = fio.tempdir()
+    table.insert(g.tempdirs, dir)
+
+    for _, script in ipairs(scripts) do
+        gen_script(g, dir, script, replacements)
+    end
+
+    return dir
+end
+
+return treegen
