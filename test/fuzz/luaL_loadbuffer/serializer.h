4f68733be17dd133e5c71fc0542e232799a5870c
klauwier <r.tolkacheva@g.nsu.ru>
2023-06-11 19:59:22 +0700

    test/fuzz: fix luaJIT fuzzer timeout
    LuaJIT fuzzer used to stop due to timeout caused by infinite cycles and
    recursions. Counters were introduced for every cycle and function to
    address LuaJIT fuzzer timeouts.
    
    The idea is to add unique counters for every cycle and function to
    ensure finite code execution, if it wasn't already. For while, repeat,
    for cycles, local and global named, anonymous functions, counters will
    be initialized before the code generated from protobuf, and checked
    in the first body statement. An entry point for the serializer was
    created to count cycles and functions for counter initialization.
    
    The idea was taken from a paper "Program Reconditioning: Avoiding
    Undefined Behaviour When Finding and Reducing Compiler Bugs" [1].
    
    Here is an example of a change in serialized code made by this commit.
    
    Before:
    ```lua
    while (true) do
        foo = 'bar'
    end
    function bar()
        bar()
    end
    ```
    
    After:
    ```lua
    counter_0 = 0
    counter_1 = 0
    while (true) do
        if counter_0 > 5 then
            break
        end
        counter_0 = counter_0 + 1
        foo = 'bar'
    end
    function bar()
        if counter_1 > 5 then
            return
        end
        counter_1 = counter_1 + 1
        bar()
    end
    ```
    Protobuf structures that reproduce the timeout problem were added to
    the LuaJIT fuzzer corpus.
    
    [1] https://www.doc.ic.ac.uk/~afd/homepages/papers/pdfs/2023/PLDI.pdf
    
    NO_CHANGELOG=internal
    NO_DOC=fuzzer fix
    
    (cherry picked from commit 4d004bbe24768c6b1afe634d87d9bea8e60b1995)

--- test/fuzz/luaL_loadbuffer/serializer.h
+++ test/fuzz/luaL_loadbuffer/serializer.h
@@ -19,6 +19,11 @@ using namespace lua_grammar;
 
 /**
  * Fuzzing parameters:
+ * kMaxCounterValue - value used in the condition
+ * `if counter > kMaxCounterValue then break end` or, in functions,
+ * `if counter > kMaxCounterValue then return end`. It is used to
+ * prevent serealized code from encountering infinite recursions
+ * and cycles.
  * kMaxNumber - upper bound for all generated numbers.
  * kMinNumber - lower bound for all generated numbers.
  * kMaxStrLength - upper bound for generating string literals and identifiers.
@@ -27,11 +32,22 @@ using namespace lua_grammar;
  * Default values were chosen arbitrary but not too big for better readability
  * of generated code samples.
  */
+constexpr std::size_t kMaxCounterValue = 5;
 constexpr double kMaxNumber = 1000.0;
 constexpr double kMinNumber = -1000.0;
 constexpr size_t kMaxStrLength = 20;
 constexpr size_t kMaxIdentifiers = 10;
 constexpr char kDefaultIdent[] = "Name";
+
+/**
+ * Entry point for the serializer. Generates a Lua program from a
+ * protobuf message with all counter initializations placed above
+ * the serialized message. The purpose of the counters is to
+ * address the timeout problem caused by infinite cycles and
+ * recursions.
+ */
+std::string
+MainBlockToString(const Block &block);
 
 PROTO_TOSTRING(Block, block);
 PROTO_TOSTRING(Chunk, chunk);
