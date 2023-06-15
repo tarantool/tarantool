d64a639b055218565642f8f6c232174cadad4ca6
Vladimir Davydov <vdavydov@tarantool.org>
2023-06-15 11:57:32 +0300

    xrow: fix large bit shift error in xrow_decode_dml
    Reported by ASAN. The issue was fixed in the master branch in commit
    b9550f1926735 ("box: support space and index names in IPROTO requests").
    
    NO_TEST=asan
    NO_DOC=bug fix
    NO_CHANGELOG=minor

--- src/box/xrow.c
+++ src/box/xrow.c
@@ -909,7 +909,8 @@ error:
 		if (key < iproto_key_MAX &&
 		    iproto_key_type[key] != mp_typeof(*value))
 			goto error;
-		key_map &= ~iproto_key_bit(key);
+		if (key < 64)
+			key_map &= ~iproto_key_bit(key);
 		switch (key) {
 		case IPROTO_SPACE_ID:
 			request->space_id = mp_decode_uint(&value);
