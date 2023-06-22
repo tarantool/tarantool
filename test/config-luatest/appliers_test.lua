a827da097002f63d4abcfeb958f9c391c61d7bb7
Mergen Imeev <imeevma@tarantool.org>
2023-06-22 16:33:47 +0300

    config: introduce mkdir applier
    This patch introduces the mkdir applier. This appliers is used to create
    all required directories. This applier is called before all other
    appliers.
    
    Part of #8789
    
    NO_DOC=will be added later
    NO_CHANGELOG=will be added later

---
+++ test/config-luatest/appliers_test.lua
@@ -0,0 +1,57 @@
+local t = require('luatest')
+local fio = require('fio')
+local treegen = require('test.treegen')
+local justrun = require('test.justrun')
+
+local g = t.group()
+
+local appliers_script = [[
+    local configdata = require('internal.config.configdata')
+    local cluster_config = require('internal.config.cluster_config')
+    local cconfig = {
+        groups = {
+            ['group-001'] = {
+                replicasets = {
+                    ['replicaset-001'] = {
+                        instances = {
+                            ['instance-001'] = {
+                                database = {
+                                    rw = true,
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    }
+    local iconfig = cluster_config:instantiate(cconfig, 'instance-001')
+    config = {_configdata = configdata.new(iconfig, cconfig, 'instance-001')}
+    local mkdir = require('internal.config.applier.mkdir')
+    mkdir.apply(config)
+    %s
+    os.exit(0)
+]]
+
+g.before_all(function()
+    treegen.init(g)
+end)
+
+g.after_all(function()
+    treegen.clean(g)
+end)
+
+g.test_applier_mkdir = function()
+    local dir = treegen.prepare_directory(g, {}, {})
+    local injection = [[
+        print(config._configdata:get('wal.dir', {use_default = true}))
+    ]]
+    treegen.write_script(dir, 'main.lua', appliers_script:format(injection))
+
+    local env = {}
+    local opts = {nojson = true, stderr = false}
+    local res = justrun.tarantool(dir, env, {'main.lua'}, opts)
+    t.assert_equals(res.exit_code, 0)
+    t.assert_equals(res.stdout, 'instance-001')
+    t.assert(fio.path.is_dir(fio.pathjoin(dir, 'instance-001')))
+end
