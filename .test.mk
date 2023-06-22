7858845ba84abc307c37bcb40d7f0c573257ef57
Yaroslav Lobankov <y.lobankov@tarantool.org>
2023-06-22 19:35:13 +0400

    Small fixes 2

--- .test.mk
+++ .test.mk
@@ -72,7 +72,7 @@ test-release: build run-luajit-test run-test
 # Release ASAN build
 
 .PHONY: test-release-asan
-test-release-asan: CMAKE_ENV = CC=clang-11 CXX=clang++-11
+test-release-asan: CMAKE_ENV = CC=clang CXX=clang++
 test-release-asan: CMAKE_PARAMS = -DCMAKE_BUILD_TYPE=RelWithDebInfo \
                                   -DENABLE_WERROR=ON \
                                   -DENABLE_ASAN=ON \
