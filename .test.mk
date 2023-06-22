18ac7eb3222e26c7375f93c8b2291c1c96ecae3b
Yaroslav Lobankov <y.lobankov@tarantool.org>
2023-06-22 23:41:51 +0400

    Test commit

--- .test.mk
+++ .test.mk
@@ -72,6 +72,7 @@ test-release: build run-luajit-test run-test
 # Release ASAN build
 
 .PHONY: test-release-asan
+test-release-asan: CMAKE_ENV = CC=clang CXX=clang++
 test-release-asan: CMAKE_PARAMS = -DCMAKE_BUILD_TYPE=RelWithDebInfo \
                                   -DENABLE_WERROR=ON \
                                   -DENABLE_ASAN=ON \
