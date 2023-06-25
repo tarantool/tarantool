89e982a27d0159c0b0009599c6bd23274517669d
Mergen Imeev <imeevma@tarantool.org>
2023-06-26 00:18:52 +0300

    config: enable --config option
    This patch integrates config module with the Tarantool --config option.
    
    Part of #8789
    
    NO_DOC=will be added later
    NO_CHANGELOG=will be added later

---
+++ test/config-luatest/basic_test.lua
@@ -0,0 +1,77 @@
+local fun = require('fun')
+local yaml = require('yaml')
+local fio = require('fio')
+local t = require('luatest')
+local treegen = require('test.treegen')
+local server = require('test.luatest_helpers.server')
+local helpers = require('test.config-luatest.helpers')
+
+local g = t.group()
+
+g.before_all(function(g)
+    treegen.init(g)
+end)
+
+g.after_all(function(g)
+    treegen.clean(g)
+end)
+
+g.after_each(function(g)
+    for k, v in pairs(g) do
+        if k == 'server' or k:match('^server_%d+$') then
+            v:stop()
+        end
+    end
+end)
+
+g.test_basic = function(g)
+    local dir = treegen.prepare_directory(g, {}, {})
+    local config = {
+        credentials = {
+            users = {
+                guest = {
+                    roles = {'super'},
+                },
+            },
+        },
+        iproto = {
+            listen = 'unix/:./{{ instance_name }}.iproto',
+        },
+        groups = {
+            ['group-001'] = {
+                replicasets = {
+                    ['replicaset-001'] = {
+                        instances = {
+                            ['instance-001'] = {
+                                database = {
+                                    rw = true,
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    }
+    local config_file = treegen.write_script(dir, 'config.yaml',
+                                             yaml.encode(config))
+    local opts = {config_file = config_file, chdir = dir}
+    g.server = server:new(fun.chain(opts, {alias = 'instance-001'}):tomap())
+    g.server:start()
+    t.assert_equals(g.server:eval('return box.info.name'), g.server.alias)
+end
+
+g.test_example_single = function(g)
+    local dir = treegen.prepare_directory(g, {}, {})
+    local config_file = fio.abspath('doc/examples/config/single.yaml')
+    local opts = {config_file = config_file, chdir = dir}
+    g.server = server:new(fun.chain(opts, {alias = 'instance-001'}):tomap())
+    g.server:start()
+    t.assert_equals(g.server:eval('return box.info.name'), g.server.alias)
+end
+
+g.test_example_replicaset = function(g)
+    local dir = treegen.prepare_directory(g, {}, {})
+    local config_file = fio.abspath('doc/examples/config/replicaset.yaml')
+    helpers.start_example_replicaset(g, dir, config_file)
+end
