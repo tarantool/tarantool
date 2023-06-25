b3beba61a649ee7d8e0da1fdb5518fb17ccb969e
Alexander Turenko <alexander.turenko@tarantool.org>
2023-06-25 21:18:13 +0300

    config: accept user[:pass]@ in iproto.advertise
    The following new syntax variants are introduced for `iproto.advertise`.
    
    * `user@` -- use the given user, a password from the `credentials`
      section and `host:port` from `iproto.listen`
    * `user:pass@` -- use the given user and password, use `host:port` from
      `iproto.listen`
    * `user@host:port` -- use the given user, host and port, use a password
      from the `credentials` section
    
    It allows to don't repeat the same information in different places of
    the config.
    
    The `test_no_advertise_*` test cases are generalized: now the same
    boilerplate code is used for these and new cases.
    
    The `test.luatest_helpers.server` util gains ability to parse all the
    new `iproto.advertise` variants.
    
    The replicaset and etcd configuration examples are updated to use the
    `user@` syntax.
    
    Part of #8810
    
    NO_DOC=the old behavior was not released, the documentation request will
           be registered manually
    NO_CHANGELOG=see NO_DOC

--- test/config-luatest/basic_test.lua
+++ test/config-luatest/basic_test.lua
@@ -77,111 +77,109 @@ g.test_example_replicaset = function(g)
     helpers.start_example_replicaset(g, dir, config_file)
 end
 
-g.test_no_advertise_no_listen = function(g)
-    local dir = treegen.prepare_directory(g, {}, {})
-    local config = {
-        credentials = {
-            users = {
-                guest = {
-                    roles = {'super'},
-                },
-            },
-        },
-        groups = {
-            ['group-001'] = {
-                replicasets = {
-                    ['replicaset-001'] = {
-                        instances = {
-                            ['instance-001'] = {
-                                database = {
-                                    rw = true,
-                                },
-                                iproto = {
-                                    listen =
-                                        'unix/:./{{ instance_name }}.iproto',
-                                },
-                            },
-                            -- No iproto.advertise or iproto.listen.
-                            ['instance-002'] = {},
-                            ['instance-003'] = {},
-                        },
-                    },
-                },
-            },
-        },
-    }
-    local config_file = treegen.write_script(dir, 'config.yaml',
-        yaml.encode(config))
+local err_msg_cannot_find_user = 'box_cfg.apply: cannot find user unknown ' ..
+    'in the config to use its password in a replication peer URI'
+local err_msg_no_suitable_uris = 'box_cfg.apply: unable to build replicaset ' ..
+    '"replicaset-001" of group "group-001": instance "instance-002" has no ' ..
+    'iproto.advertise or iproto.listen URI suitable to create a client socket'
 
-    local env = {}
-    local args = {'--name', 'instance-001', '--config', config_file}
-    local opts = {nojson = true, stderr = true}
-    local res = justrun.tarantool(dir, env, args, opts)
-    t.assert_equals(res, {
-        exit_code = 1,
-        stderr = 'LuajitError: box_cfg.apply: unable to build replicaset ' ..
+-- Bad cases for building replicaset URIs from iproto.advertise
+-- and iproto.listen parameters.
+for case_name, case in pairs({
+    no_advertise_no_listen = {
+        listen = nil,
+        advertise = nil,
+        exp_err = 'box_cfg.apply: unable to build replicaset ' ..
             '"replicaset-001" of group "group-001": instance "instance-002" ' ..
-            'has neither iproto.advertise nor iproto.listen options\n' ..
-            'fatal error, exiting the event loop',
-    })
-end
-
-g.test_no_advertise_unsuitable_listen = function(g)
-    local dir = treegen.prepare_directory(g, {}, {})
-    local instance_002 = {
-        iproto = {
-            -- This option is set in the particular cases below.
-            -- listen = <...>,
-        },
-    }
-    local config = {
-        credentials = {
-            users = {
-                guest = {
-                    roles = {'super'},
+            'has neither iproto.advertise nor iproto.listen options',
+    },
+    advertise_unknown_user_uri = {
+        listen = 'unix/:./{{ instance_name }}.iproto',
+        advertise = 'unknown@unix/:./{{ instance_name }}.iproto',
+        exp_err = err_msg_cannot_find_user,
+    },
+    no_advertise_no_suitable_listen = {
+        listen = 'localhost:0,0.0.0.0:3301,[::]:3301',
+        advertise = nil,
+        exp_err = err_msg_no_suitable_uris,
+    },
+    advertise_user_no_suitable_listen = {
+        listen = 'localhost:0,0.0.0.0:3301,[::]:3301',
+        advertise = 'replicator@',
+        exp_err = err_msg_no_suitable_uris,
+    },
+    advertise_user_pass_no_suitable_listen = {
+        listen = 'localhost:0,0.0.0.0:3301,[::]:3301',
+        advertise = 'replicator:topsecret@',
+        exp_err = err_msg_no_suitable_uris,
+    },
+    advertise_unknown_user = {
+        listen = 'unix/:./{{ instance_name }}.iproto',
+        advertise = 'unknown@',
+        exp_err = err_msg_cannot_find_user,
+    },
+}) do
+    g[('test_bad_replicaset_build_%s'):format(case_name)] = function()
+        local dir = treegen.prepare_directory(g, {}, {})
+        local instance_002 = {
+            iproto = {
+                listen = case.listen,
+                advertise = case.advertise,
+            },
+        }
+        local good_listen = 'unix/:./{{ instance_name }}.iproto'
+        local config = {
+            credentials = {
+                users = {
+                    replicator = {
+                        password = {
+                            plain = 'topsecret',
+                        },
+                        roles = {'replication'},
+                    },
+                    guest = {
+                        roles = {'super'},
+                    },
                 },
             },
-        },
-        groups = {
-            ['group-001'] = {
-                replicasets = {
-                    ['replicaset-001'] = {
-                        instances = {
-                            ['instance-001'] = {
-                                database = {
-                                    rw = true,
+            groups = {
+                ['group-001'] = {
+                    replicasets = {
+                        ['replicaset-001'] = {
+                            instances = {
+                                ['instance-001'] = {
+                                    database = {
+                                        rw = true,
+                                    },
+                                    iproto = {
+                                        listen = good_listen,
+                                        advertise = 'replicator@',
+                                    },
                                 },
-                                iproto = {
-                                    listen =
-                                        'unix/:./{{ instance_name }}.iproto',
+                                ['instance-002'] = instance_002,
+                                ['instance-003'] = {
+                                    iproto = {
+                                        listen = good_listen,
+                                        advertise = 'replicator@',
+                                    },
                                 },
                             },
-                            ['instance-002'] = instance_002,
-                            ['instance-003'] = {},
                         },
                     },
                 },
             },
-        },
-    }
+        }
 
-    local exp_stderr = 'LuajitError: box_cfg.apply: unable to build ' ..
-        'replicaset "replicaset-001" of group "group-001": instance ' ..
-        '"instance-002" has no iproto.advertise option and neither ' ..
-        'of the iproto.listen URIs are suitable to create a client ' ..
-        'socket\nfatal error, exiting the event loop'
-    for _, listen in ipairs({
-        '0.0.0.0:3301',
-        '[::]:3301',
-        'localhost:0',
-    }) do
-        instance_002.iproto.listen = listen
         local config_file = treegen.write_script(dir, 'config.yaml',
             yaml.encode(config))
         local env = {}
         local args = {'--name', 'instance-001', '--config', config_file}
         local opts = {nojson = true, stderr = true}
         local res = justrun.tarantool(dir, env, args, opts)
+        local exp_stderr = table.concat({
+            ('LuajitError: %s'):format(case.exp_err),
+            'fatal error, exiting the event loop',
+        }, '\n')
         t.assert_equals(res, {
             exit_code = 1,
             stderr = exp_stderr,
@@ -189,45 +187,93 @@ g.test_no_advertise_unsuitable_listen = function(g)
     end
 end
 
-g.test_no_advertise_second_listen_suitable = function()
-    local config = {
-        credentials = {
-            users = {
-                guest = {
-                    roles = {'super'},
-                },
-                client = {
-                    password = {
-                        plain = 'secret',
+-- Successful cases for building replicaset URIs from
+-- iproto.advertise and iproto.listen parameters.
+for case_name, case in pairs({
+    advertise_user_pass_uri = {
+        listen = 'unix/:./{{ instance_name }}.iproto',
+        advertise = 'replicator:topsecret@unix/:./{{ instance_name }}.iproto',
+    },
+    advertise_user_uri = {
+        listen = 'unix/:./{{ instance_name }}.iproto',
+        advertise = 'replicator@unix/:./{{ instance_name }}.iproto',
+    },
+    advertise_uri = {
+        listen = 'unix/:./{{ instance_name }}.iproto',
+        advertise = 'unix/:./{{ instance_name }}.iproto',
+    },
+    advertise_user_pass = {
+        listen = 'unix/:./{{ instance_name }}.iproto',
+        advertise = 'replicator:topsecret@',
+    },
+    advertise_user = {
+        listen = 'unix/:./{{ instance_name }}.iproto',
+        advertise = 'replicator@',
+    },
+    advertise_user_first_listen_suitable = {
+        listen = 'unix/:./{{ instance_name }}.iproto,localhost:0',
+        advertise = 'replicator@',
+    },
+    advertise_user_second_listen_suitable = {
+        listen = 'localhost:0,unix/:./{{ instance_name }}.iproto',
+        advertise = 'replicator@',
+    },
+    advertise_guest = {
+        listen = 'unix/:./{{ instance_name }}.iproto',
+        advertise = 'guest@',
+    },
+    advertise_guest_uri = {
+        listen = 'unix/:./{{ instance_name }}.iproto',
+        advertise = 'guest@unix/:./{{ instance_name }}.iproto',
+    },
+}) do
+    g[('test_good_replicaset_build_%s'):format(case_name)] = function()
+        local config = {
+            credentials = {
+                users = {
+                    replicator = {
+                        password = {
+                            plain = 'topsecret',
+                        },
+                        roles = {'replication'},
                     },
-                    roles = {'super'},
+                    guest = {
+                        roles = {'super'},
+                    },
+                    client = {
+                        password = {
+                            plain = 'secret',
+                        },
+                        roles = {'super'},
+                    },
                 },
             },
-        },
-        iproto = {
-            listen = 'localhost:0,unix/:./{{ instance_name }}.iproto',
-        },
-        groups = {
-            ['group-001'] = {
-                replicasets = {
-                    ['replicaset-001'] = {
-                        instances = {
-                            ['instance-001'] = {
-                                database = {
-                                    rw = true,
+            iproto = {
+                listen = case.listen,
+                advertise = case.advertise,
+            },
+            groups = {
+                ['group-001'] = {
+                    replicasets = {
+                        ['replicaset-001'] = {
+                            instances = {
+                                ['instance-001'] = {
+                                    database = {
+                                        rw = true,
+                                    },
                                 },
+                                ['instance-002'] = {},
+                                ['instance-003'] = {},
                             },
-                            ['instance-002'] = {},
-                            ['instance-003'] = {},
                         },
                     },
                 },
             },
-        },
-    }
+        }
 
-    local dir = treegen.prepare_directory(g, {}, {})
-    local config_file = treegen.write_script(dir, 'config.yaml',
-        yaml.encode(config))
-    helpers.start_example_replicaset(g, dir, config_file)
+        local dir = treegen.prepare_directory(g, {}, {})
+        local config_file = treegen.write_script(dir, 'config.yaml',
+            yaml.encode(config))
+        helpers.start_example_replicaset(g, dir, config_file)
+    end
 end
