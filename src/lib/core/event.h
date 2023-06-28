5e890b6a1f3a25ca562346f435e1697db9a81891
Andrey Saranchin <Andrey22102001@gmail.com>
2023-06-28 12:47:01 +0300

    core: introduce event subsystem
    The patch introduces new event subsystem. This subsystem is designed to
    store user-defined triggers and has nothing in common with core triggers.
    
    Each trigger has its own name and is represented by func_adapter.
    Triggers are stored in events - named wrappers over rlist. Event objects
    are opaque, hence rlist field should not be used directly - event
    provides event_find_trigger, event_reset_triggers methods and
    event_trigger_iterator. Iterator provides stable iteration and all the
    non-deleted triggers will surely be traversed.
    
    On way to the goal this patch also fixes include list in func_adapter.h.
    
    Part of #8656
    
    NO_CHANGELOG=internal
    NO_DOC=internal

---
+++ src/lib/core/event.h
@@ -0,0 +1,162 @@
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright 2010-2023, Tarantool AUTHORS, please see AUTHORS file.
+ */
+#pragma once
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "small/rlist.h"
+
+#if defined(__cplusplus)
+extern "C" {
+#endif /* defined(__cplusplus) */
+
+struct func_adapter;
+
+/**
+ * List of triggers registered on event identified by name.
+ */
+struct event {
+	/** List of triggers. */
+	struct rlist triggers;
+	/** Name of event. */
+	char *name;
+	/** Reference count. */
+	uint32_t ref_count;
+	/** Number of triggers. */
+	uint32_t trigger_count;
+};
+
+/**
+ * Destroys an event and removes it from event registry.
+ * NB: The method is private and must not be called manually.
+ */
+void
+event_delete(struct event *event);
+
+/**
+ * Increments event reference counter.
+ */
+static inline void
+event_ref(struct event *event)
+{
+	event->ref_count++;
+}
+
+/**
+ * Decrements event reference counter. The event is destroyed if the counter
+ * reaches zero.
+ */
+static inline void
+event_unref(struct event *event)
+{
+	assert(event->ref_count > 0);
+	event->ref_count--;
+	if (event->ref_count == 0) {
+		assert(rlist_empty(&event->triggers));
+		assert(event->trigger_count == 0);
+		event_delete(event);
+	}
+}
+
+/**
+ * Checks if the event has no triggers.
+ */
+static inline bool
+event_has_triggers(struct event *event)
+{
+	return event->trigger_count > 0;
+}
+
+/**
+ * Finds a trigger by name in an event. All the arguments must not be NULL.
+ */
+struct func_adapter *
+event_find_trigger(struct event *event, const char *name);
+
+/**
+ * Resets trigger by name in an event.
+ * Arguments event and name must not be NULL.
+ * If new_trigger is NULL, the function removes a trigger by name from the
+ * event. Otherwise, it replaces trigger by name or inserts it in the beginning
+ * of the underlying list of event.
+ */
+void
+event_reset_trigger(struct event *event, const char *name,
+		    struct func_adapter *new_trigger);
+
+/**
+ * Iterator over triggers from event. Never invalidates.
+ */
+struct event_trigger_iterator {
+	/**
+	 * Current element in the list of triggers.
+	 * Becomes NULL when the iterator is exhausted.
+	 */
+	struct rlist *curr;
+	/** Underlying event. */
+	struct event *event;
+};
+
+/**
+ * Initializes iterator.
+ */
+void
+event_trigger_iterator_create(struct event_trigger_iterator *it,
+			      struct event *event);
+
+/**
+ * Advances iterator. Output arguments trigger and name must not be NULL.
+ */
+bool
+event_trigger_iterator_next(struct event_trigger_iterator *it,
+			    struct func_adapter **trigger, const char **name);
+
+/**
+ * Deinitializes iterator. Does not free memory.
+ */
+void
+event_trigger_iterator_destroy(struct event_trigger_iterator *it);
+
+/**
+ * Finds an event by its name. Name must be a zero-terminated string.
+ * Creates new event and inserts it to registry if there is no event with such
+ * name when flag create_if_not_exist is true.
+ */
+struct event *
+event_get(const char *name, bool create_if_not_exist);
+
+typedef bool
+event_foreach_f(struct event *event, void *arg);
+
+/**
+ * Invokes a callback for each registered event with no particular order.
+ *
+ * The callback is passed an event object and the given argument.
+ * If it returns true, iteration continues. Otherwise, iteration breaks, and
+ * the function returns false.
+ *
+ * Empty events are guaranteed to be skipped.
+ */
+bool
+event_foreach(event_foreach_f cb, void *arg);
+
+/**
+ * Initializes event submodule.
+ */
+void
+event_init(void);
+
+/**
+ * Frees event submodule.
+ */
+void
+event_free(void);
+
+#if defined(__cplusplus)
+} /* extern "C" */
+#endif /* defined(__cplusplus) */
---
+++ test/unit/event.c
@@ -0,0 +1,445 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <string.h>
+
+#include "diag.h"
+#include "error.h"
+#include "errcode.h"
+#include "fiber.h"
+#include "func_adapter.h"
+#include "memory.h"
+#include "trivia/util.h"
+#include "event.h"
+#include "tt_static.h"
+
+#define UNIT_TAP_COMPATIBLE 1
+#include "unit.h"
+
+static int func_destroy_count;
+
+/**
+ * Virtual destructor for func_adapter for the test purposes.
+ */
+void
+func_destroy(struct func_adapter *func)
+{
+	func_destroy_count++;
+}
+
+/**
+ * The test creates event with different names and check if all the basic
+ * operations works correctly.
+ */
+static void
+test_basic(void)
+{
+	plan(4 * 14);
+
+	struct func_adapter_vtab vtab = {.destroy = func_destroy};
+	struct func_adapter func = {.vtab = &vtab};
+	const char *trg_name = "my_triggers.trg[1]";
+	const char *names[] = {
+		"name",
+		"name with spaces",
+		"namespace.name",
+		"NAMESPACE[123].name"
+	};
+	for (size_t i = 0; i < lengthof(names); ++i) {
+		const char *name = names[i];
+		struct event *event = event_get(name, false);
+		is(event, NULL, "No such event - NULL must be returned");
+		event = event_get(name, true);
+		isnt(event, NULL, "Event must be created");
+		/* Reference event to prevent deletion when it'll be empty. */
+		event_ref(event);
+		struct event *found_event = event_get(name, false);
+		is(found_event, event, "Existing event must be found");
+		struct func_adapter *old = event_find_trigger(event, trg_name);
+		is(old, NULL, "No such trigger - NULL must be returned");
+		ok(!event_has_triggers(event), "Created event must be empty");
+		event_reset_trigger(event, trg_name, &func);
+		found_event = event_get(name, false);
+		is(found_event, event, "Event must still exist");
+		old = event_find_trigger(event, trg_name);
+		is(old, &func, "New trigger must be found");
+		ok(event_has_triggers(event), "Event must not be empty");
+		is(func_destroy_count, 0, "Func must not be destroyed yet")
+		event_reset_trigger(event, trg_name, NULL);
+		is(func_destroy_count, 1, "Func must be destroyed")
+		old = event_find_trigger(event, trg_name);
+		is(old, NULL, "Deleted trigger must not be found");
+		ok(!event_has_triggers(event), "Event must be empty");
+		found_event = event_get(name, false);
+		is(found_event, event, "Referenced event must not be deleted");
+		event_unref(event);
+		found_event = event_get(name, false);
+		is(found_event, NULL, "Empty unused event must be deleted");
+		func_destroy_count = 0;
+	}
+	check_plan();
+}
+
+/**
+ * A test argument for event_foreach function.
+ */
+struct test_event_foreach_arg {
+	int names_len;
+	const char **names;
+	int traversed;
+};
+
+static bool
+test_event_foreach_f(struct event *event, void *arg)
+{
+	struct test_event_foreach_arg *data =
+		(struct test_event_foreach_arg *)arg;
+	data->traversed++;
+	bool name_found = false;
+	for (int i = 0; i < data->names_len && !name_found; ++i) {
+		name_found = strcmp(event->name, data->names[i]) == 0;
+	}
+	ok(name_found, "Traversed event must really exist");
+	return true;
+}
+
+static bool
+test_event_foreach_return_false_f(struct event *event, void *arg)
+{
+	(void)event;
+	struct test_event_foreach_arg *data =
+		(struct test_event_foreach_arg *)arg;
+	data->traversed++;
+	return false;
+}
+
+static void
+test_event_foreach(void)
+{
+	plan(10);
+	const char *names[] = {
+		"event",
+		"my_events.event1",
+		"my_events.event3",
+		"my_events[15].event"
+	};
+	struct func_adapter_vtab vtab = {.destroy = func_destroy};
+	struct func_adapter func = {.vtab = &vtab};
+	for (size_t i = 0; i < lengthof(names); ++i) {
+		struct event *event = event_get(names[i], true);
+		event_ref(event);
+		const char *trg_name = tt_sprintf("%zu", i);
+		event_reset_trigger(event, trg_name, &func);
+	}
+
+	struct test_event_foreach_arg arg = {
+		.names_len = lengthof(names),
+		.names = names,
+		.traversed = 0,
+	};
+
+	bool ok = event_foreach(test_event_foreach_f, &arg);
+	ok(ok, "Traversal must return true");
+	is(arg.traversed, lengthof(names), "All the events must be traversed");
+
+	arg.traversed = 0;
+	ok = event_foreach(test_event_foreach_return_false_f, &arg);
+	ok(!ok, "Failed traversal must return false");
+	is(arg.traversed, 1, "Only one event must be traversed");
+
+	for (size_t i = 0; i < lengthof(names); ++i) {
+		struct event *event = event_get(names[i], false);
+		const char *trg_name = tt_sprintf("%zu", i);
+		event_reset_trigger(event, trg_name, NULL);
+	}
+
+	arg.traversed = 0;
+	ok = event_foreach(test_event_foreach_f, &arg);
+	ok(ok, "Traversal of empty registry must return true");
+	is(arg.traversed, 0, "All the events are empty - nothing to traverse");
+
+	/* Unreference all the events. */
+	for (size_t i = 0; i < lengthof(names); ++i) {
+		struct event *event = event_get(names[i], false);
+		fail_if(event == NULL);
+		event_unref(event);
+	}
+
+	check_plan();
+}
+
+static void
+test_event_trigger_iterator(void)
+{
+	plan(14);
+	const char *event_name = "test_event";
+	const char *trigger_names[] = {
+		"0", "1", "2", "3", "4", "5", "6", "7"
+	};
+	struct func_adapter_vtab vtab = {.destroy = func_destroy};
+	struct func_adapter func = {.vtab = &vtab};
+
+	struct event *event = event_get(event_name, true);
+	for (int i = lengthof(trigger_names) - 1; i >= 0; --i)
+		event_reset_trigger(event, trigger_names[i], &func);
+
+	struct event_trigger_iterator it;
+	event_trigger_iterator_create(&it, event);
+	size_t idx = 0;
+	struct func_adapter *curr_trg = NULL;
+	const char *curr_name = NULL;
+	while (event_trigger_iterator_next(&it, &curr_trg, &curr_name)) {
+		is(strcmp(curr_name, trigger_names[idx]), 0,
+		   "Triggers must be traversed in reversed order");
+		idx++;
+	}
+	is(idx, lengthof(trigger_names), "All the triggers must be traversed");
+	is(curr_trg, NULL, "Exhausted iterator must return NULL trigger");
+	is(curr_name, NULL, "Exhausted iterator must return NULL name");
+
+	curr_trg = &func;
+	curr_name = "garbage";
+	bool has_elems =
+		event_trigger_iterator_next(&it, &curr_trg, &curr_name);
+	ok(!has_elems, "Iterator must stay exhausted");
+	is(curr_trg, NULL, "Exhausted iterator must return NULL trigger");
+	is(curr_name, NULL, "Exhausted iterator must return NULL name");
+
+	for (size_t i = 0; i < lengthof(trigger_names); ++i)
+		event_reset_trigger(event, trigger_names[i], NULL);
+
+	event_trigger_iterator_destroy(&it);
+
+	check_plan();
+}
+
+/**
+ * Stops at breakpoint and deletes triggers which are set in del mask.
+ */
+static void
+test_event_iterator_stability_del_step(int breakpoint, const char *del_mask,
+				       int trigger_num)
+{
+	fail_unless(breakpoint < trigger_num);
+	size_t left_after_br = 0;
+	for (int i = breakpoint + 1; i < trigger_num; ++i) {
+		if (del_mask[i] == 0)
+			left_after_br++;
+	}
+	plan((breakpoint + 1) * 2 + left_after_br + 3);
+
+	const char *event_name = "test_event";
+	struct func_adapter_vtab vtab = {.destroy = func_destroy};
+	struct func_adapter func = {.vtab = &vtab};
+
+	struct event *event = event_get(event_name, true);
+	/*
+	 * Reference the event to prevent deletion for the test cases that
+	 * delete all the triggers from the event.
+	 */
+	event_ref(event);
+	for (int i = trigger_num - 1; i >= 0; --i) {
+		const char *trg_name = tt_sprintf("%d", i);
+		event_reset_trigger(event, trg_name, &func);
+	}
+
+	struct event_trigger_iterator it;
+	event_trigger_iterator_create(&it, event);
+	struct func_adapter *trg = NULL;
+	const char *name = NULL;
+	for (int i = 0; i <= breakpoint; i++) {
+		bool ok = event_trigger_iterator_next(&it, &trg, &name);
+		ok(ok, "Iterator must not be exhausted yet")
+		const char *trg_name = tt_sprintf("%d", i);
+		is(strcmp(name, trg_name), 0,
+		   "Triggers must be traversed in reversed order");
+	}
+	bool delete_all_triggers = true;
+	for (int i = 0; i < trigger_num; ++i) {
+		if (del_mask[i] != 0) {
+			const char *trg_name = tt_sprintf("%d", i);
+			event_reset_trigger(event, trg_name, NULL);
+		} else {
+			delete_all_triggers = false;
+		}
+	}
+	is(event_has_triggers(event), !delete_all_triggers,
+	   "Function event_has_triggers must work correctly");
+	for (size_t i = 0; i < left_after_br; ++i) {
+		bool ok = event_trigger_iterator_next(&it, &trg, &name);
+		ok(ok, "Traversal must continue");
+	}
+
+	bool ok = event_trigger_iterator_next(&it, &trg, &name);
+	ok(!ok, "Iterator must be exhausted");
+	event_trigger_iterator_destroy(&it);
+	is(event_has_triggers(event), !delete_all_triggers,
+	   "Function event_has_triggers must work correctly");
+
+	for (int i = 0; i < trigger_num; ++i) {
+		if (del_mask[i] != 0)
+			continue;
+		const char *trg_name = tt_sprintf("%d", i);
+		event_reset_trigger(event, trg_name, NULL);
+	}
+	event_unref(event);
+
+	check_plan();
+}
+
+/**
+ * Stops at breakpoint and replaces triggers which are set in replace mask.
+ */
+static void
+test_event_iterator_stability_replace_step(int breakpoint,
+					   const char *replace_mask,
+					   int trigger_num)
+{
+	fail_unless(breakpoint < trigger_num);
+	plan((breakpoint + 1) * 2 + 3 * (trigger_num - breakpoint - 1) + 3);
+
+	const char *event_name = "test_event";
+	struct func_adapter_vtab vtab = {.destroy = func_destroy};
+	struct func_adapter func = {.vtab = &vtab};
+	struct func_adapter new_func = {.vtab = &vtab};
+
+	struct event *event = event_get(event_name, true);
+	for (int i = trigger_num - 1; i >= 0; --i) {
+		const char *trg_name = tt_sprintf("%d", i);
+		event_reset_trigger(event, trg_name, &func);
+	}
+
+	struct event_trigger_iterator it;
+	event_trigger_iterator_create(&it, event);
+	struct func_adapter *trg = NULL;
+	const char *name = NULL;
+	for (int i = 0; i <= breakpoint; i++) {
+		bool ok = event_trigger_iterator_next(&it, &trg, &name);
+		ok(ok, "Iterator must not be exhausted yet")
+		const char *trg_name = tt_sprintf("%d", i);
+		is(strcmp(name, trg_name), 0,
+		   "Triggers must be traversed in reversed order");
+	}
+	for (int i = 0; i < trigger_num; ++i) {
+		if (replace_mask[i] != 0) {
+			const char *trg_name = tt_sprintf("%d", i);
+			event_reset_trigger(event, trg_name, &new_func);
+		}
+	}
+	ok(event_has_triggers(event), "Event must not be empty");
+	for (int i = breakpoint + 1; i < trigger_num; ++i) {
+		bool ok = event_trigger_iterator_next(&it, &trg, &name);
+		ok(ok, "Traversal must continue")
+		const char *trg_name = tt_sprintf("%d", i);
+		is(strcmp(name, trg_name), 0,
+		   "Triggers must be traversed in reversed order");
+		if (replace_mask[i] != 0) {
+			is(trg, &new_func, "Trigger must be replaced");
+		} else {
+			is(trg, &func, "Trigger must be old");
+		}
+	}
+	bool ok = event_trigger_iterator_next(&it, &trg, &name);
+	ok(!ok, "Iterator must be exhausted");
+	event_trigger_iterator_destroy(&it);
+	ok(event_has_triggers(event), "Event must not be empty");
+
+	for (int i = 0; i < trigger_num; ++i) {
+		const char *trg_name = tt_sprintf("%d", i);
+		event_reset_trigger(event, trg_name, NULL);
+	}
+
+	check_plan();
+}
+
+/**
+ * Checks if iteration is stable in the cases of deletions and replaces.
+ */
+static void
+test_event_trigger_iterator_stability(void)
+{
+	plan(6);
+	char mask[8];
+	const size_t trigger_num = lengthof(mask);
+	memset(mask, 0, trigger_num);
+	size_t br = trigger_num / 2;
+	/**
+	 * Delete or replace current trigger.
+	 */
+	mask[br] = 1;
+	test_event_iterator_stability_del_step(br, mask, trigger_num);
+	test_event_iterator_stability_replace_step(br, mask, trigger_num);
+	memset(mask, 0, trigger_num);
+	/**
+	 * Delete or replace current, previous and next triggers.
+	 */
+	mask[br - 1] = 1;
+	mask[br] = 1;
+	mask[br + 1] = 1;
+	test_event_iterator_stability_del_step(br, mask, trigger_num);
+	test_event_iterator_stability_replace_step(br, mask, trigger_num);
+	memset(mask, 0, trigger_num);
+	/**
+	 * Delete or replace all the triggers in the middle of iteration.
+	 */
+	memset(mask, 1, trigger_num);
+	test_event_iterator_stability_del_step(br, mask, trigger_num);
+	test_event_iterator_stability_replace_step(br, mask, trigger_num);
+	memset(mask, 0, trigger_num);
+
+	check_plan();
+}
+
+static void
+test_event_free(void)
+{
+	plan(1);
+
+	struct func_adapter_vtab vtab = {.destroy = func_destroy};
+	struct func_adapter func = {.vtab = &vtab};
+	const char *trigger_names[] = {
+		"trigger[1]",
+		"trigger.second",
+		"another_trigger"
+	};
+	const char *event_names[] = {
+		"name",
+		"name with spaces",
+		"namespace.name",
+		"NAMESPACE[123].name"
+	};
+	func_destroy_count = 0;
+	for (size_t i = 0; i < lengthof(event_names); ++i) {
+		const char *name = event_names[i];
+		struct event *event = event_get(name, true);
+		for (size_t j = 0; j < lengthof(trigger_names); ++j)
+			event_reset_trigger(event, trigger_names[j], &func);
+	}
+	event_free();
+	is(func_destroy_count, lengthof(event_names) * lengthof(trigger_names),
+	   "All triggers must be destroyed");
+	/* Initialize event back. */
+	event_init();
+
+	check_plan();
+}
+
+static int
+test_main(void)
+{
+	plan(5);
+	test_basic();
+	test_event_foreach();
+	test_event_trigger_iterator();
+	test_event_trigger_iterator_stability();
+	test_event_free();
+	return check_plan();
+}
+
+int
+main(void)
+{
+	event_init();
+	int rc = test_main();
+	event_free();
+	return rc;
+}
