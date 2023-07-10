3d4f818ff6999dcbfebe76cb8bece01a59700867
Rimma Tolkacheva <r.tolkacheva@g.nsu.ru>
2023-07-11 04:44:43 +0700

    test/fuzz: refactor LuaJIT fuzzer
    This refactoring will:
    
    1. Move macros from a header to the source file.
    Macros should be used in header only with undef to avoid redefinitions.
    Undef directive is not useful since we want to use these macros in the
    source file.
    
    2. Remove `using namespace lua_grammar` from header.
    https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#Rs-using-directive
    
    3. Moving serializer entry point and constant parameters into
    luajit_fuzzer namespace.
    It's a common practice in C++ to avoid name collisions.
    
    4. Move serializer functions into anonymous namespace.
    These functions are not a part of the interface so should have
    static linkage.
    https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#Rs-unnamed2
    
    5. Fix ConvertToStringDefault function.
    It was logically wrong so it would generate an identifier `123` from
    `*123`.
    
    NO_CHANGELOG=internal
    NO_DOC=fuzzer fix
    
    (cherry picked from commit 56488e15cc697164bbad2541f8e9f26f38964200)

--- test/fuzz/luaL_loadbuffer/serializer.h
+++ test/fuzz/luaL_loadbuffer/serializer.h
@@ -5,17 +5,11 @@
  */
 #pragma once
 
+#include <string>
+
 #include "lua_grammar.pb.h"
 
-using namespace lua_grammar;
-
-#define PROTO_TOSTRING(TYPE, VAR_NAME) \
-	std::string TYPE##ToString(const TYPE & (VAR_NAME))
-
-/* PROTO_TOSTRING version for nested (depth=2) protobuf messages. */
-#define NESTED_PROTO_TOSTRING(TYPE, VAR_NAME, PARENT_MESSAGE) \
-	std::string TYPE##ToString \
-	(const PARENT_MESSAGE::TYPE & (VAR_NAME))
+namespace luajit_fuzzer {
 
 /**
  * Fuzzing parameters:
@@ -47,93 +41,6 @@ constexpr char kDefaultIdent[] = "Name";
  * recursions.
  */
 std::string
-MainBlockToString(const Block &block);
+MainBlockToString(const lua_grammar::Block &block);
 
-PROTO_TOSTRING(Block, block);
-PROTO_TOSTRING(Chunk, chunk);
-
-PROTO_TOSTRING(Statement, stat);
-
-/** LastStatement and nested types. */
-PROTO_TOSTRING(LastStatement, laststat);
-NESTED_PROTO_TOSTRING(ReturnOptionalExpressionList, explist, LastStatement);
-
-/**
- * Statement options.
- */
-
-/** AssignmentList and nested types. */
-PROTO_TOSTRING(AssignmentList, assignmentlist);
-NESTED_PROTO_TOSTRING(VariableList, varlist, AssignmentList);
-
-/** FunctionCall and nested types. */
-PROTO_TOSTRING(FunctionCall, call);
-NESTED_PROTO_TOSTRING(Args, args, FunctionCall);
-NESTED_PROTO_TOSTRING(PrefixArgs, prefixargs, FunctionCall);
-NESTED_PROTO_TOSTRING(PrefixNamedArgs, prefixnamedargs, FunctionCall);
-
-/** DoBlock, WhileCycle and RepeatCycle clauses. */
-PROTO_TOSTRING(DoBlock, block);
-PROTO_TOSTRING(WhileCycle, whilecycle);
-PROTO_TOSTRING(RepeatCycle, repeatcycle);
-
-/** IfStatement and nested types. */
-PROTO_TOSTRING(IfStatement, statement);
-NESTED_PROTO_TOSTRING(ElseIfBlock, elseifblock, IfStatement);
-
-/** ForCycleName and ForCycleList clauses. */
-PROTO_TOSTRING(ForCycleName, forcyclename);
-PROTO_TOSTRING(ForCycleList, forcyclelist);
-
-/** Function and nested types. */
-PROTO_TOSTRING(Function, func);
-NESTED_PROTO_TOSTRING(FuncName, funcname, Function);
-
-PROTO_TOSTRING(NameList, namelist);
-PROTO_TOSTRING(FuncBody, body);
-NESTED_PROTO_TOSTRING(NameListWithEllipsis, namelist, FuncBody);
-NESTED_PROTO_TOSTRING(ParList, parlist, FuncBody);
-
-/** LocalFunc and LocalNames clauses. */
-PROTO_TOSTRING(LocalFunc, localfunc);
-PROTO_TOSTRING(LocalNames, localnames);
-
-/**
- * Expressions and variables.
- */
-
-/** Expressions clauses. */
-PROTO_TOSTRING(ExpressionList, explist);
-PROTO_TOSTRING(OptionalExpressionList, explist);
-PROTO_TOSTRING(PrefixExpression, prefExpr);
-
-/* Variable and nested types. */
-PROTO_TOSTRING(Variable, var);
-NESTED_PROTO_TOSTRING(IndexWithExpression, indexexpr, Variable);
-NESTED_PROTO_TOSTRING(IndexWithName, indexname, Variable);
-
-/** Expression and nested types. */
-PROTO_TOSTRING(Expression, expr);
-NESTED_PROTO_TOSTRING(AnonFunc, function, Expression);
-NESTED_PROTO_TOSTRING(ExpBinaryOpExp, binary, Expression);
-NESTED_PROTO_TOSTRING(UnaryOpExp, unary, Expression);
-
-/**
- * Tables and fields.
- */
-PROTO_TOSTRING(TableConstructor, table);
-PROTO_TOSTRING(FieldList, fieldlist);
-NESTED_PROTO_TOSTRING(FieldWithFieldSep, field, FieldList);
-
-/** Field and nested types. */
-PROTO_TOSTRING(Field, field);
-NESTED_PROTO_TOSTRING(ExpressionAssignment, assignment, Field);
-NESTED_PROTO_TOSTRING(NameAssignment, assignment, Field);
-PROTO_TOSTRING(FieldSep, sep);
-
-/** Operators. */
-PROTO_TOSTRING(BinaryOperator, op);
-PROTO_TOSTRING(UnaryOperator, op);
-
-/** Identifier (Name). */
-PROTO_TOSTRING(Name, name);
+} /* namespace luajit_fuzzer */
