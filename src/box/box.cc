5c271cbd1d892d19c927336e6e89b1db92b06a5f
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2023-08-01 22:40:13 +0200

    election: fix box.ctl.demote() nop in off-mode
    box.ctl.demote() used not to do anything with election_mode='off'
    if the synchro queue didn't belong to the caller in the same term
    as the election state.
    
    The reason could be that if the synchro queue term is "outdated",
    there is no guarantee that some other instance doesn't own it in
    the latest term right now.
    
    The "problem" is that this could be workarounded easily by just
    calling promote + demote together.
    
    There isn't much sense in fixing it for the off-mode because the
    only reasons off-mode exists are 1) for people who don't use
    synchro at all, 2) who did use it and want to stop. Hence they
    need demote just to disown the queue.
    
    The patch "legalizes" the mentioned workaround by allowing to
    perform demote in off-mode even if the synchro queue term is old.
    
    Closes #6860
    
    NO_DOC=bugfix

--- src/box/box.cc
+++ src/box/box.cc
@@ -2958,13 +2958,33 @@ box_demote(void)
 	if (!is_box_configured)
 		return 0;
 
-	/* Currently active leader is the only one who can issue a DEMOTE. */
-	bool is_leader = txn_limbo_replica_term(&txn_limbo, instance_id) ==
-			 box_raft()->term && txn_limbo.owner_id == instance_id;
-	if (box_election_mode != ELECTION_MODE_OFF)
-		is_leader = is_leader && box_raft()->state == RAFT_STATE_LEADER;
-	if (!is_leader)
-		return 0;
+	const struct raft *raft = box_raft();
+	if (box_election_mode == ELECTION_MODE_OFF) {
+		assert(raft->state == RAFT_STATE_FOLLOWER);
+		if (raft->leader != REPLICA_ID_NIL) {
+			diag_set(ClientError, ER_NOT_LEADER, raft->leader);
+			return -1;
+		}
+		if (txn_limbo.owner_id == REPLICA_ID_NIL)
+			return 0;
+		/*
+		 * If the limbo term is up to date with Raft, then it might have
+		 * a valid owner right now. Demotion would disrupt it. In this
+		 * case the user has to explicitly overthrow the old owner with
+		 * local promote(), or call demote() on the actual owner.
+		 */
+		if (txn_limbo.promote_greatest_term == raft->term &&
+		    txn_limbo.owner_id != instance_id)
+			return 0;
+	} else {
+		if (txn_limbo_replica_term(&txn_limbo, instance_id) !=
+		    raft->term)
+			return 0;
+		if (txn_limbo.owner_id != instance_id)
+			return 0;
+		if (raft->state != RAFT_STATE_LEADER)
+			return 0;
+	}
 	if (box_trigger_elections() != 0)
 		return -1;
 	if (box_election_mode != ELECTION_MODE_OFF)
