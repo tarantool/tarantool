3fbff185d6b34fb483b77d3873ec63fd53f1d843
Serge Petrenko <sergepetrenko@tarantool.org>
2023-08-23 14:10:59 +0300

    box: get rid of dummy NOPs after transactions ending with local rows
    In order to preserve transaction boundaries over replication, Tarantool
    writes a global NOP row after the last transaction row, if this row
    happens to be local. This is done to make sure that the is_commit flag,
    which is set only in the last transaction row, reaches the replica. This
    wouldn't happen if the last row was local.
    
    This workaround works fine for transactions completely authored by one
    instance: when both global and local rows come from operations of a
    single master.
    
    However, it's possible to append local rows to a remote master's
    transaction on a replica. For example, one can use on_replace triggers
    to write to replica's local space on each new transaction coming from
    master.
    
    In this case essentially a global NOP entry is added at the end of a
    remote master's transaction. This leads to several problems.
    
    First of all, this bumps replica's LSN, which is counter-intuitive,
    given that the replica might even be read-only. Besides, in a star
    topology this leads to master being unable to connect to the replica
    later on due to their vclocks becoming incompatible.
    
    Secondly, even if replication channel between master and replica is
    bidirectional, it creates a new row which should be replicated from
    replica to master, but at the same time is the last row of the master's
    transaction. Once master receives this row, it breaks its connection to
    replica due to transaction boundary violation (the last row of the
    transaction is received without its beginning).
    
    Adding a NOP row became extraneous since the previous commit, which made
    relay find transaction boundaries by itself.
    
    Closes #8958
    
    NO_DOC=bugfix

--- test/replication-luatest/gh_8746_replica_transaction_boundary_test.lua
+++ test/replication-luatest/gh_8746_replica_transaction_boundary_test.lua
@@ -54,3 +54,29 @@ g.test_replica_recovery = function(cg)
     cg.replica:restart()
     cg.replica:wait_for_vclock_of(cg.master)
 end
+
+g.before_test('test_replication_to_master', function(cg)
+    cg.box_cfg = {
+        replication = {
+            server.build_listen_uri('master', cg.replica_set.id),
+            server.build_listen_uri('replica', cg.replica_set.id),
+        },
+        replication_timeout = 0.1,
+    }
+    cg.master = cg.replica_set:build_and_add_server{
+        alias = 'master',
+        box_cfg = cg.box_cfg,
+    }
+    cg.replica = cg.replica_set:build_and_add_server{
+        alias = 'replica',
+        box_cfg = cg.box_cfg,
+    }
+end)
+
+g.test_replication_to_master = function(cg)
+    prepare(cg)
+    cg.master:wait_for_vclock_of(cg.replica)
+    t.helpers.retrying({}, function()
+        cg.master:assert_follows_upstream(cg.replica:get_instance_id())
+    end)
+end
