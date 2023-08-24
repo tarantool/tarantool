85df1c968ebf08dabffd995b5aeca6f17f9610a9
Serge Petrenko <sergepetrenko@tarantool.org>
2023-08-24 11:28:39 +0300

    box: fix force recovery for transactions with local rows
    Force recovery first tries to collect all rows of a transaction into a
    single list, and only then applies those rows.
    
    The problem was that it collected rows based on the row replica_id. For
    local rows replica_id is set to 0, but actually such rows can be part
    of a transaction coming from any instance.
    
    Fix recovery of such rows
    
    Follow-up #8746
    Follow-up #7932
    
    NO_DOC=bugfix
    NO_CHANGELOG=the broken behaviour couldn't be seen due to bug #8746

--- test/replication-luatest/gh_8746_replica_transaction_boundary_test.lua
+++ test/replication-luatest/gh_8746_replica_transaction_boundary_test.lua
@@ -15,6 +15,7 @@ end)
 local function prepare(cg)
     cg.replica_set:start()
     cg.master:exec(function()
+        box.ctl.wait_rw()
         box.schema.space.create('test')
         box.space.test:create_index('pk')
         box.schema.space.create('loc', {is_local = true})
@@ -78,5 +79,47 @@ g.test_replication_to_master = function(cg)
     cg.master:wait_for_vclock_of(cg.replica)
     t.helpers.retrying({}, function()
         cg.master:assert_follows_upstream(cg.replica:get_instance_id())
+    end)
+end
+
+g.before_test('test_replica_force_recovery', function(cg)
+    cg.master = cg.replica_set:build_and_add_server{
+        alias = 'master',
+        box_cfg = {
+            replication_timeout = 0.1,
+        },
+    }
+    cg.replica = cg.replica_set:build_and_add_server{
+        alias = 'replica',
+        box_cfg = {
+            read_only = true,
+            replication = server.build_listen_uri('master', cg.replica_set.id),
+            replication_timeout = 0.1,
+            force_recovery = true,
+        },
+    }
+end)
+
+g.test_replica_force_recovery = function(cg)
+    prepare(cg)
+    cg.master:exec(function()
+        box.begin()
+        box.space.test:replace{2}
+        box.space.test:replace{3}
+        box.commit()
+    end)
+    cg.replica:wait_for_vclock_of(cg.master)
+    cg.replica:exec(function()
+        box.cfg{read_only = false}
+        box.begin()
+        box.space.test:replace{4}
+        box.space.loc:replace{5}
+        box.space.test:replace{6}
+        box.commit()
+    end)
+    cg.replica:restart()
+    cg.replica:exec(function()
+        t.assert_equals(box.space.test:select{}, {{1}, {2}, {3}, {4}, {6}})
+        t.assert_equals(box.space.loc:select{}, {{1}, {2}, {3}, {4}, {5}, {6}})
     end)
 end
