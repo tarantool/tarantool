145af72b2ed85734ebad8e7bd76412369d079b58
Nikita Zheleztsov <n.zheleztsov@proton.me>
2023-10-26 16:02:02 +0300

    lua: introduce xlog.meta() method
    This commit introduces the new method for xlog module: xlog.meta().
    It opens an xlog file, reads and returns the meta block of the file,
    which includes its filetype, instance_uuid and vclocks.
    
    It's needed in order to introduce checking of names inside the config
    module in the following commit.
    
    Needed for #8978
    
    @TarantoolBot document
    Title: xlog.meta(file-name) method
    
    Description: Open an xlog file, and return its meta block.
    Possible errors: Failed to open a file or it does not contain properly
    formatted meta block.
    
    Example:
    
    ```lua
    tarantool> xlog = require('xlog')
    ---
    ...
    
    tarantool> xlog.meta('00000000000000000000.snap')
    ---
    - filetype: SNAP
      prev_vclock: {}
      instance_uuid: 87b2e60f-275c-4efa-9b0e-e9562e309692
      vclock: {}
    ...
    ```

--- src/box/lua/xlog.lua
+++ src/box/lua/xlog.lua
@@ -4,6 +4,11 @@ local function xlog_pairs(...)
     return fun.wrap(internal.pairs(...))
 end
 
+local function xlog_meta(...)
+    return internal.meta(...)
+end
+
 return {
     pairs = xlog_pairs,
+    meta = xlog_meta,
 }
