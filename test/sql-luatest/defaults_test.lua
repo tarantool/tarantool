953d9f4f9285da1671887446ada122bfb50cebb0
Mergen Imeev <imeevma@tarantool.org>
2023-10-26 19:01:01 +0300

    sql: do not lose defaults during ALTER TABLE
    Prior to this patch it was possible to lose BOX field default values
    after ALTER TABLE ADD COLUMN.
    
    NO_DOC=bugfix

---
+++ test/sql-luatest/defaults_test.lua
@@ -0,0 +1,36 @@
+local server = require('luatest.server')
+local t = require('luatest')
+
+local g = t.group()
+
+g.before_all(function()
+    g.server = server:new({alias = 'master'})
+    g.server:start()
+    g.server:exec(function()
+        box.execute([[SET SESSION "sql_seq_scan" = true;]])
+    end)
+end)
+
+g.after_all(function()
+    g.server:stop()
+end)
+
+-- Make sure default is not dropped after ADD COLUMN.
+g.test_default_after_add_column = function()
+    g.server:exec(function()
+        local body = 'function(a) return a + 123 end'
+        local func_def = {is_deterministic = true, body = body}
+        box.schema.func.create('F1', func_def)
+        local format = {{'I', 'integer'}, {'A', 'integer', default = 321,
+                                           default_func = 'F1'}}
+        local s = box.schema.space.create('A', {format = format})
+        local func = box.func.F1
+        t.assert_equals(s:format()[2].default, 321)
+        t.assert_equals(s:format()[2].default_func, func.id)
+        box.execute([[ALTER TABLE A ADD COLUMN B INTEGER;]])
+        t.assert_equals(s:format()[2].default, 321)
+        t.assert_equals(s:format()[2].default_func, func.id)
+        s:drop()
+        func:drop()
+    end)
+end
