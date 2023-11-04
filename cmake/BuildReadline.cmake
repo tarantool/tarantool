f3b31aeeb68923e659d464a54c2a8fde2e980072
Vladimir Davydov <vdavydov@tarantool.org>
2023-11-04 20:11:29 +0300

    cmake: allow to bundle static dependencies in main project
    The BUILD_STATIC cmake config option forces the build system link
    Tarantool binary statically with its dependencies. It expects that all
    static libraries on which Tarantool binary depends are available at
    build time.
    
    We don't use this option directly to create static binaries because it
    would produce different results on different build systems. Instead, we
    use the separate cmake config located in the static-build directory,
    which fetches all Tarantool dependencies from a predefined location
    before building a static binary.
    
    Having a separate cmake config is inconvenient. Let's enable bundling of
    static binary dependencies right in the main project cmake config, like
    we bundle, for example, libcurl. To achieve that, the new build option
    was introduced BUILD_STATIC_WITH_BUNDLED_LIBS. It implies BUILD_STATIC
    and also fetches and builds all required dependencies, like the
    static-build cmake config used to. The latter doesn't do it anymore;
    from now on, it just sets BUILD_STATIC_WITH_BUNDLED_LIBS when building
    Tarantool. We can't remove the static-build cmake config yet because
    there are quire a few CI workflows depending on it.
    
    Note that, just like BUILD_STATIC, BUILD_STATIC_WITH_BUNDLED_LIBS
    doesn't imply OPENSSL_USE_STATIC_LIBS so the latter should be set
    explicitly if one wants to use the static openssl library. However,
    setting OPENSSL_USE_STATIC_LIBS with BUILD_STATIC_WITH_BUNDLED_LIBS will
    force the build system use bundled static openssl library.
    
    This patch is relatively straightforward. It just moves the external
    projects from /static-build/cmake/AddDependencyProjects.cmake to /cmake
    adding build dependencies where required and setting variables that are
    set by the corresponding /cmake/FindXXX.cmake configs.
    
    There are a few things that should be noted separately though:
     - We dropped the ZLIB_FOUND check from the main project cmake config.
       It was used for building EE but the latter is going to be broken
       anyway once this patch is committed. We'll fix it in following
       commits.
     - FindLibUnwind referenced zlib library by ZLIB::ZLIB. We don't set
       it for bundled zlib so let's use ZLIB_LIBRARIES instead.
     - We don't need to detect dependency cflags while building bundled
       libraries as we can reuse the flags set by the main project.
     - We don't use HARDENING_LDFLAGS because it makes no sense when
       building static libraries.
    
    Closes #9242
    
    NO_DOC=build
    NO_TEST=build
    NO_CHANGELOG=build
    
    (cherry picked from commit 98b38e89ba0545543b85aa6f164f66309c2343c4)

---
+++ cmake/BuildReadline.cmake
@@ -0,0 +1,104 @@
+set(NCURSES_VERSION 6.3-20220716)
+set(NCURSES_HASH 2b7a0e31ebbd8144680f985d61f5bbd5)
+set(NCURSES_INSTALL_DIR ${PROJECT_BINARY_DIR}/build/ncurses)
+set(NCURSES_LIBRARY ${NCURSES_INSTALL_DIR}/lib/libncurses.a)
+set(NCURSES_TINFO_LIBRARY ${NCURSES_INSTALL_DIR}/lib/libtinfo.a)
+set(NCURSES_CFLAGS "${DEPENDENCY_CFLAGS} -O2")
+set(NCURSES_CXXFLAGS "${DEPENDENCY_CXXFLAGS} -O2")
+set(NCURSES_CPPFLAGS "")
+set(NCURSES_LDFLAGS "")
+
+set(READLINE_VERSION 8.0)
+set(READLINE_HASH 7e6c1f16aee3244a69aba6e438295ca3)
+set(READLINE_PATCHES_DIR ${PROJECT_SOURCE_DIR}/patches)
+set(READLINE_INSTALL_DIR ${PROJECT_BINARY_DIR}/build/readline)
+set(READLINE_INCLUDE_DIR ${READLINE_INSTALL_DIR}/include)
+set(READLINE_LIBRARY ${READLINE_INSTALL_DIR}/lib/libreadline.a)
+set(READLINE_CFLAGS "${DEPENDENCY_CFLAGS} -O2")
+set(READLINE_CPPFLAGS "")
+set(READLINE_LDFLAGS "")
+
+if(APPLE)
+    set(NCURSES_CFLAGS "${NCURSES_CFLAGS} ${CMAKE_C_SYSROOT_FLAG} ${CMAKE_OSX_SYSROOT}")
+    set(NCURSES_CXXFLAGS "${NCURSES_CXXFLAGS} ${CMAKE_C_SYSROOT_FLAG} ${CMAKE_OSX_SYSROOT}")
+    set(NCURSES_CPPFLAGS "${NCURSES_CPPFLAGS} ${CMAKE_C_SYSROOT_FLAG} ${CMAKE_OSX_SYSROOT}")
+
+    set(READLINE_CFLAGS "${READLINE_CFLAGS} ${CMAKE_C_SYSROOT_FLAG} ${CMAKE_OSX_SYSROOT}")
+    set(READLINE_CPPFLAGS "${READLINE_CPPFLAGS} ${CMAKE_C_SYSROOT_FLAG} ${CMAKE_OSX_SYSROOT}")
+endif()
+
+ExternalProject_Add(bundled-ncurses-project
+    PREFIX ${NCURSES_INSTALL_DIR}
+    URL ${BACKUP_STORAGE}/ncurses/ncurses-${NCURSES_VERSION}.tgz
+    URL_MD5 ${NCURSES_HASH}
+    CONFIGURE_COMMAND <SOURCE_DIR>/configure
+        CC=${CMAKE_C_COMPILER}
+        CXX=${CMAKE_CXX_COMPILER}
+        CFLAGS=${NCURSES_CFLAGS}
+        CXXFLAGS=${NCURSES_CXXFLAGS}
+        CPPFLAGS=${NCURSES_CPPFLAGS}
+        LDFLAGS=${NCURSES_LDFLAGS}
+
+        --prefix=<INSTALL_DIR>
+
+        # This flag enables creation of libcurses.a as a symlink to libncurses.a
+        # and disables subdir creation `ncurses` at <install_dir>/include. It is
+        # necessary for correct work of FindCurses.cmake module (this module is
+        # builtin at cmake package) which used in cmake/FindReadline.cmake
+        --enable-overwrite
+
+        # enable building libtinfo to prevent linking with libtinfo from system
+        # directories
+        --with-termlib
+
+        # set search paths for terminfo db
+        --with-terminfo-dirs=/lib/terminfo:/usr/share/terminfo:/etc/terminfo
+
+        # disable install created terminfo db, use db from system
+        --disable-db-install
+        --without-progs
+        --without-manpages
+    BUILD_BYPRODUCTS ${NCURSES_LIBRARY} ${NCURSES_TINFO_LIBRARY}
+)
+
+add_library(bundled-ncurses STATIC IMPORTED GLOBAL)
+set_target_properties(bundled-ncurses PROPERTIES IMPORTED_LOCATION
+    ${NCURSES_LIBRARY})
+add_dependencies(bundled-ncurses bundled-ncurses-project)
+
+add_library(bundled-ncurses-tinfo STATIC IMPORTED GLOBAL)
+set_target_properties(bundled-ncurses-tinfo PROPERTIES IMPORTED_LOCATION
+    ${NCURSES_TINFO_LIBRARY})
+add_dependencies(bundled-ncurses-tinfo bundled-ncurses-project)
+
+ExternalProject_Add(bundled-readline-project
+    PREFIX ${READLINE_INSTALL_DIR}
+    URL ${BACKUP_STORAGE}/readline/readline-${READLINE_VERSION}.tar.gz
+    URL_MD5 ${READLINE_HASH}
+    CONFIGURE_COMMAND <SOURCE_DIR>/configure
+        CC=${CMAKE_C_COMPILER}
+        CFLAGS=${READLINE_CFLAGS}
+        CPPFLAGS=${READLINE_CPPFLAGS}
+        LDFLAGS=${READLINE_LDFLAGS}
+
+        --prefix=<INSTALL_DIR>
+        --disable-shared
+    PATCH_COMMAND patch -d <SOURCE_DIR> -p0 -i "${READLINE_PATCHES_DIR}/readline80-001.patch"
+    COMMAND       patch -d <SOURCE_DIR> -p1 -i "${READLINE_PATCHES_DIR}/readline-tarantool-security-95.patch"
+    BUILD_BYPRODUCTS ${READLINE_LIBRARY}
+)
+
+add_library(bundled-readline STATIC IMPORTED GLOBAL)
+set_target_properties(bundled-readline PROPERTIES IMPORTED_LOCATION
+    ${READLINE_LIBRARY})
+add_dependencies(bundled-readline bundled-readline-project
+    bundled-ncurses bundled-ncurses-tinfo)
+
+set(READLINE_FOUND TRUE)
+set(READLINE_LIBRARIES ${READLINE_LIBRARY} ${NCURSES_LIBRARY}
+    ${NCURSES_TINFO_LIBRARY})
+set(READLINE_INCLUDE_DIRS ${READLINE_INCLUDE_DIR})
+
+set(HAVE_GNU_READLINE TRUE)
+
+message(STATUS "Using bundled readline")
