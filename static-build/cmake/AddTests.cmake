fa4939bd0d21a05ac9a0dea91083b4690552bba8
Vladimir Davydov <vdavydov@tarantool.org>
2023-11-09 14:47:58 +0300

    cmake: check static build dependencies in post build command
    Instead of using ctest, let's simply run the CheckDependencies.cmake as
    a post build command if Tarantool was built without dependencies. The
    good thing about it is that the check will run even if the static build
    is created directly, without the /static-build/CMakeLists.txt wrapper.
    
    Part of #9242
    
    NO_DOC=build
    NO_TEST=build
    NO_CHANGELOG=build

--- static-build/cmake/AddTests.cmake
+++
@@ -1,8 +0,0 @@
-enable_testing()
-
-add_test(
-    NAME check-dependencies
-    COMMAND ${CMAKE_COMMAND}
-        -D FILE=${TARANTOOL_BINARY}
-        -P ${CMAKE_CURRENT_LIST_DIR}/CheckDependencies.cmake
-)
