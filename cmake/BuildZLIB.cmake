be2eda3dc1cbfdd50ff93b43867708435076472f
Vladimir Davydov <vdavydov@tarantool.org>
2023-11-22 14:04:31 +0300

    cmake: use legacy layout for static-build bundled libs
    SVACE stopped working after commit 98b38e89ba054 ("cmake: allow to
    bundle static dependencies in main project") changed the bundled libs
    directory layout. To fix this, let's introduce the new cmake option
    BUNDLED_LIBS_INSTALL_DIR and set it in static-build/CMakeLists.txt to
    the legacy location. Also, let's use the legacy directories for each
    external project's PREFIX, SOURCE_DIR, BINARY_DIR, and STAMP_DIR.
    
    Follow-up #9242
    
    NO_DOC=build
    NO_TEST=build
    NO_CHANGELOG=build
    
    (cherry picked from commit 37b1c2871cb7f07d127ef5ba969a9921f5b87905)

--- cmake/BuildZLIB.cmake
+++ cmake/BuildZLIB.cmake
@@ -1,6 +1,6 @@
 set(ZLIB_VERSION 1.2.12)
 set(ZLIB_HASH 5fc414a9726be31427b440b434d05f78)
-set(ZLIB_INSTALL_DIR ${PROJECT_BINARY_DIR}/build/zlib)
+set(ZLIB_INSTALL_DIR ${BUNDLED_LIBS_INSTALL_DIR}/zlib-prefix)
 set(ZLIB_INCLUDE_DIR ${ZLIB_INSTALL_DIR}/include)
 set(ZLIB_LIBRARY ${ZLIB_INSTALL_DIR}/lib/libz.a)
 set(ZLIB_CFLAGS "${DEPENDENCY_CFLAGS} -O2")
@@ -14,6 +14,9 @@ endif()
 
 ExternalProject_Add(bundled-zlib-project
     PREFIX ${ZLIB_INSTALL_DIR}
+    SOURCE_DIR ${ZLIB_INSTALL_DIR}/src/zlib
+    BINARY_DIR ${ZLIB_INSTALL_DIR}/src/zlib-build
+    STAMP_DIR ${ZLIB_INSTALL_DIR}/src/zlib-stamp
     URL ${BACKUP_STORAGE}/zlib/zlib-${ZLIB_VERSION}.tar.gz
     URL_MD5 ${ZLIB_HASH}
     CONFIGURE_COMMAND env
