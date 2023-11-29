tarantool/tarantool

commit 0704ebb778d4b2d4907dd2c7bfc3c1b9b1293785

xlog: rework writer API
The goal of this patch is to make the xlog writer API less confusing.
It does the following changes:

 - Rename xlog_rename to xlog_materialize. The function strips the
   .inprogress suffix from an xlog file name. Usually it's called after
   the xlog was sealed (by writing the EOF marker) and closed but it may
   also be called before xlog_close in case the xlog file is going to be
   appended to (used in WAL).

 - Make xlog_sync private. It's called by xlog_close so there's no point
   in calling it manually. Make xlog_close fail if it fails to sync the
   file.

 - Make xlog_close flush the buffer so that the caller doesn't need to
   call xlog_flush manually before closing an xlog file.

 - Drop the reuse_fd argument of xlog_close. Instead, introduce a new
   function xlog_close_reuse_fd that works exactly like xlog_close
   except it doesn't close the fd and returns it in the out argument.

 - Introduce xlog_discard. It's supposed to be called on an incomplete
   xlog file to close its fd and unlink it.

Rework all the places where we write xlog files in accordance with the
new API. Note about memtx snapshot writer: use xlog_materialize and
xlog_discard instead of coio_rename and coio_unlink.

The new API (especially the xlog_discard helper) facilitates
introduction of thorough (secure) file deletion because it significantly
reduces the number of places in the code where xlog files are deleted.

Needed for tarantool/tarantool-ee#540

NO_DOC=refactoring
NO_TEST=refactoring
NO_CHANGELOG=refactoring

src/box/vy_run.c
@@ -2084,23 +2084,20 @@ vy_run_write_index(struct vy_run *run, const char *dirpath,
 
 	ERROR_INJECT(ERRINJ_VY_INDEX_FILE_RENAME, {
 		diag_set(ClientError, ER_INJECTION, "vinyl index file rename");
-		xlog_close(&index_xlog, false);
-		return -1;
+		goto fail;
 	});
 
-	if (xlog_flush(&index_xlog) < 0 ||
-	    xlog_rename(&index_xlog) < 0)
+	if (xlog_close(&index_xlog) != 0 ||
+	    xlog_materialize(&index_xlog) != 0)
 		goto fail;
 
-	xlog_close(&index_xlog, false);
 	return 0;
 
 fail_rollback:
 	region_truncate(region, mem_used);
 	xlog_tx_rollback(&index_xlog);
 fail:
-	xlog_close(&index_xlog, false);
-	unlink(path);
+	xlog_discard(&index_xlog);
 	return -1;
 }
 
@@ -2299,16 +2296,14 @@ out:
 /**
  * Destroy a run writer.
  * @param writer Writer to destroy.
- * @param reuse_fd True in a case of success run write. And else
- *        false.
  */
 static void
-vy_run_writer_destroy(struct vy_run_writer *writer, bool reuse_fd)
+vy_run_writer_destroy(struct vy_run_writer *writer)
 {
 	if (writer->last.stmt != NULL)
 		vy_stmt_unref_if_possible(writer->last.stmt);
 	if (xlog_is_open(&writer->data_xlog))
-		xlog_close(&writer->data_xlog, reuse_fd);
+		xlog_discard(&writer->data_xlog);
 	if (writer->bloom != NULL)
 		tuple_bloom_builder_delete(writer->bloom);
 	ibuf_destroy(&writer->row_index_buf);
@@ -2326,7 +2321,7 @@ vy_run_writer_commit(struct vy_run_writer *writer)
 
 	struct vy_run *run = writer->run;
 	if (vy_run_is_empty(run)) {
-		vy_run_writer_destroy(writer, false);
+		vy_run_writer_destroy(writer);
 		rc = 0;
 		goto out;
 	}
@@ -2352,9 +2347,11 @@ vy_run_writer_commit(struct vy_run_writer *writer)
 	});
 
 	/* Sync data and link the file to the final name. */
-	if (xlog_sync(&writer->data_xlog) < 0 ||
-	    xlog_rename(&writer->data_xlog) < 0)
+	if (xlog_close_reuse_fd(&writer->data_xlog, &run->fd) != 0 ||
+	    xlog_materialize(&writer->data_xlog) != 0) {
+		xlog_discard(&writer->data_xlog);
 		goto out;
+	}
 
 	if (writer->bloom != NULL) {
 		run->info.bloom = tuple_bloom_new(writer->bloom,
@@ -2366,8 +2363,6 @@ vy_run_writer_commit(struct vy_run_writer *writer)
 			       writer->space_id, writer->iid) != 0)
 		goto out;
 
-	run->fd = writer->data_xlog.fd;
-	vy_run_writer_destroy(writer, true);
 	rc = 0;
 out:
 	region_truncate(&fiber()->gc, region_svp);
@@ -2377,7 +2372,7 @@ out:
 void
 vy_run_writer_abort(struct vy_run_writer *writer)
 {
-	vy_run_writer_destroy(writer, false);
+	vy_run_writer_destroy(writer);
 }
 
 int

src/box/memtx_engine.cc
@@ -36,7 +36,7 @@
 
 #include "fiber.h"
 #include "errinj.h"
-#include "coio_file.h"
+#include "coio_task.h"
 #include "info/info.h"
 #include "tuple.h"
 #include "txn.h"
@@ -762,12 +762,19 @@ checkpoint_write_tuple(struct xlog *l, uint32_t space_id, uint32_t group_id,
 struct checkpoint {
 	/** Database read view written to the snapshot file. */
 	struct read_view rv;
+	/** Snapshot writer thread. */
 	struct cord cord;
+	/** True if tx thread is waiting for the snapshot writer thread. */
 	bool waiting_for_snap_thread;
 	/** The vclock of the snapshot file. */
 	struct vclock vclock;
+	/** Snapshot directory. */
 	struct xdir dir;
+	/** New snapshot file. */
+	struct xlog snap;
+	/** Raft request to be written to the snapshot file. */
 	struct raft_request raft;
+	/** Synchro request to be written to the snapshot file. */
 	struct synchro_request synchro_state;
 	/**
 	 * Do nothing, just touch the snapshot file - the
@@ -863,6 +870,7 @@ checkpoint_new(const char *snap_dirname, uint64_t snap_io_rate_limit)
 	opts.sync_interval = SNAP_SYNC_INTERVAL;
 	opts.free_cache = true;
 	xdir_create(&ckpt->dir, snap_dirname, SNAP, &INSTANCE_UUID, &opts);
+	xlog_clear(&ckpt->snap);
 	vclock_create(&ckpt->vclock);
 	box_raft_checkpoint_local(&ckpt->raft);
 	txn_limbo_checkpoint(&txn_limbo, &ckpt->synchro_state);
@@ -1006,13 +1014,21 @@ checkpoint_f(va_list ap)
 		ckpt->touch = false;
 	}
 
-	struct xlog snap;
-	if (xdir_create_xlog(&ckpt->dir, &snap, &ckpt->vclock) != 0)
+	struct xlog *snap = &ckpt->snap;
+	assert(!xlog_is_open(snap));
+	if (xdir_create_xlog(&ckpt->dir, snap, &ckpt->vclock) != 0) {
+		/*
+		 * We call memtx_engine_abort_checkpoint on failure to discard
+		 * an incomplete xlog file. Clear the xlog object so that it's
+		 * safe to call xlog_discard() on it.
+		 */
+		xlog_clear(snap);
 		return -1;
+	}
 
 	struct mh_i32_t *temp_space_ids = mh_i32_new();
 
-	say_info("saving snapshot `%s'", snap.filename);
+	say_info("saving snapshot `%s'", snap->filename);
 	ERROR_INJECT_SLEEP(ERRINJ_SNAP_WRITE_DELAY);
 	ERROR_INJECT(ERRINJ_SNAP_SKIP_ALL_ROWS, goto done);
 	struct space_read_view *space_rv;
@@ -1043,7 +1059,7 @@ checkpoint_f(va_list ap)
 					       space_rv->id,
 					       temp_space_ids))
 				continue;
-			rc = checkpoint_write_tuple(&snap, space_rv->id,
+			rc = checkpoint_write_tuple(snap, space_rv->id,
 						    space_rv->group_id,
 						    result.data, result.size);
 			if (rc != 0)
@@ -1057,35 +1073,33 @@ checkpoint_f(va_list ap)
 	if (rc != 0)
 		goto fail;
 	ERROR_INJECT(ERRINJ_SNAP_WRITE_CORRUPTED_INSERT_ROW, {
-		if (checkpoint_write_corrupted_insert_row(&snap) != 0)
+		if (checkpoint_write_corrupted_insert_row(snap) != 0)
 			goto fail;
 	});
 	ERROR_INJECT(ERRINJ_SNAP_WRITE_MISSING_SPACE_ROW, {
-		if (checkpoint_write_missing_space_row(&snap) != 0)
+		if (checkpoint_write_missing_space_row(snap) != 0)
 			goto fail;
 	});
 	ERROR_INJECT(ERRINJ_SNAP_WRITE_UNKNOWN_ROW_TYPE, {
-		if (checkpoint_write_unknown_row_type(&snap) != 0)
+		if (checkpoint_write_unknown_row_type(snap) != 0)
 			goto fail;
 	});
 	ERROR_INJECT(ERRINJ_SNAP_WRITE_INVALID_SYSTEM_ROW, {
-		if (checkpoint_write_invalid_system_row(&snap) != 0)
+		if (checkpoint_write_invalid_system_row(snap) != 0)
 			goto fail;
 	});
-	if (checkpoint_write_raft(&snap, &ckpt->raft) != 0)
+	if (checkpoint_write_raft(snap, &ckpt->raft) != 0)
 		goto fail;
-	if (checkpoint_write_synchro(&snap, &ckpt->synchro_state) != 0)
+	if (checkpoint_write_synchro(snap, &ckpt->synchro_state) != 0)
 		goto fail;
 	goto done;
 done:
-	if (xlog_flush(&snap) < 0)
+	if (xlog_close(snap) != 0)
 		goto fail;
-
-	xlog_close(&snap, false);
 	say_info("done");
 	return 0;
 fail:
-	xlog_close(&snap, false);
+	xlog_discard(snap);
 	return -1;
 }
 
@@ -1135,31 +1149,32 @@ memtx_engine_wait_checkpoint(struct engine *engine,
 	return result;
 }
 
+static ssize_t
+memtx_engine_commit_checkpoint_f(va_list ap)
+{
+	struct xlog *xlog = va_arg(ap, typeof(xlog));
+	if (xlog_materialize(xlog) != 0) {
+		diag_log();
+		panic("failed to commit snapshot");
+	}
+	return 0;
+}
+
 static void
 memtx_engine_commit_checkpoint(struct engine *engine,
 			       const struct vclock *vclock)
 {
 	ERROR_INJECT_TERMINATE(ERRINJ_SNAP_COMMIT_FAIL);
-	(void) vclock;
 	struct memtx_engine *memtx = (struct memtx_engine *)engine;
 
-	/* beginCheckpoint() must have been done */
 	assert(memtx->checkpoint != NULL);
-	/* waitCheckpoint() must have been done. */
 	assert(!memtx->checkpoint->waiting_for_snap_thread);
+	assert(!xlog_is_open(&memtx->checkpoint->snap));
 
 	if (!memtx->checkpoint->touch) {
-		int64_t lsn = vclock_sum(&memtx->checkpoint->vclock);
-		struct xdir *dir = &memtx->checkpoint->dir;
-		/* rename snapshot on completion */
-		char to[PATH_MAX];
-		snprintf(to, sizeof(to), "%s",
-			 xdir_format_filename(dir, lsn, NONE));
-		const char *from = xdir_format_filename(dir, lsn, INPROGRESS);
 		ERROR_INJECT_YIELD(ERRINJ_SNAP_COMMIT_DELAY);
-		int rc = coio_rename(from, to);
-		if (rc != 0)
-			panic("can't rename .snap.inprogress");
+		coio_call(memtx_engine_commit_checkpoint_f,
+			  &memtx->checkpoint->snap);
 	}
 
 	struct vclock last;
@@ -1173,28 +1188,24 @@ memtx_engine_commit_checkpoint(struct engine *engine,
 	memtx->checkpoint = NULL;
 }
 
+static ssize_t
+memtx_engine_abort_checkpoint_f(va_list ap)
+{
+	struct xlog *xlog = va_arg(ap, typeof(xlog));
+	xlog_discard(xlog);
+	return 0;
+}
+
 static void
 memtx_engine_abort_checkpoint(struct engine *engine)
 {
 	struct memtx_engine *memtx = (struct memtx_engine *)engine;
 
-	/**
-	 * An error in the other engine's first phase.
-	 */
-	if (memtx->checkpoint->waiting_for_snap_thread) {
-		/* wait for memtx-part snapshot completion */
-		if (cord_cojoin(&memtx->checkpoint->cord) != 0)
-			diag_log();
-		memtx->checkpoint->waiting_for_snap_thread = false;
-	}
+	assert(memtx->checkpoint != NULL);
+	assert(!memtx->checkpoint->waiting_for_snap_thread);
+	assert(!xlog_is_open(&memtx->checkpoint->snap));
 
-	/** Remove garbage .inprogress file. */
-	const char *filename =
-		xdir_format_filename(&memtx->checkpoint->dir,
-				     vclock_sum(&memtx->checkpoint->vclock),
-				     INPROGRESS);
-	(void) coio_unlink(filename);
-
+	coio_call(memtx_engine_abort_checkpoint_f, &memtx->checkpoint->snap);
 	checkpoint_delete(memtx->checkpoint);
 	memtx->checkpoint = NULL;
 }

src/box/vy_log.c
@@ -966,15 +966,13 @@ vy_log_open(struct xlog *xlog)
 	}
 	region_truncate(&fiber()->gc, region_svp);
 
-	if (xlog_rename(xlog) < 0)
+	if (xlog_materialize(xlog) != 0)
 		goto fail_close_xlog;
 
 	return 0;
 
 fail_close_xlog:
-	if (unlink(xlog->filename) < 0)
-		say_syserror("failed to delete file '%s'", xlog->filename);
-	xlog_close(xlog, false);
+	xlog_discard(xlog);
 fail:
 	return -1;
 }
@@ -2665,29 +2663,18 @@ vy_log_create(const struct vclock *vclock, struct vy_recovery *recovery)
 
 	ERROR_INJECT(ERRINJ_VY_LOG_FILE_RENAME, {
 		diag_set(ClientError, ER_INJECTION, "vinyl log file rename");
-		xlog_close(&xlog, false);
-		return -1;
+		goto err_write_xlog;
 	});
 
-	/* Finalize the new xlog. */
-	if (xlog_flush(&xlog) < 0 ||
-	    xlog_sync(&xlog) < 0 ||
-	    xlog_rename(&xlog) < 0)
+	if (xlog_close(&xlog) != 0 ||
+	    xlog_materialize(&xlog) != 0)
 		goto err_write_xlog;
-
-	xlog_close(&xlog, false);
 done:
 	say_verbose("done saving vylog");
 	return 0;
 
 err_write_xlog:
-	/* Delete the unfinished xlog. */
-	assert(xlog_is_open(&xlog));
-	if (unlink(xlog.filename) < 0)
-		say_syserror("failed to delete file '%s'",
-			     xlog.filename);
-	xlog_close(&xlog, false);
-
+	xlog_discard(&xlog);
 err_create_xlog:
 	return -1;
 }

src/box/wal.c
@@ -646,6 +646,19 @@ wal_sync(struct vclock *vclock)
 	return rc;
 }
 
+/**
+ * Wrapper around xlog_close() that logs errors.
+ *
+ * We can't handle xlog_close() failure in any reasonable way in WAL.
+ * The best we can do is print an error to the log.
+ */
+static void
+wal_xlog_close(struct xlog *xlog)
+{
+	if (xlog_close(xlog) != 0)
+		diag_log();
+}
+
 static int
 wal_begin_checkpoint_f(struct cbus_call_msg *data)
 {
@@ -666,8 +679,7 @@ wal_begin_checkpoint_f(struct cbus_call_msg *data)
 	if (xlog_is_open(&writer->current_wal) &&
 	    vclock_sum(&writer->current_wal.meta.vclock) !=
 	    vclock_sum(&writer->vclock)) {
-
-		xlog_close(&writer->current_wal, false);
+		wal_xlog_close(&writer->current_wal);
 		/*
 		 * The next WAL will be created on the first write.
 		 */
@@ -841,12 +853,7 @@ wal_opt_rotate(struct wal_writer *writer)
 	 */
 	if (xlog_is_open(&writer->current_wal) &&
 	    writer->current_wal.offset >= writer->wal_max_size) {
-		/*
-		 * We can not handle xlog_close()
-		 * failure in any reasonable way.
-		 * A warning is written to the error log.
-		 */
-		xlog_close(&writer->current_wal, false);
+		wal_xlog_close(&writer->current_wal);
 	}
 
 	if (xlog_is_open(&writer->current_wal))
@@ -1213,16 +1220,16 @@ wal_writer_f(va_list ap)
 		struct xlog l;
 		if (xdir_create_xlog(&writer->wal_dir, &l,
 				     &writer->vclock) == 0)
-			xlog_close(&l, false);
+			wal_xlog_close(&l);
 		else
 			diag_log();
 	}
 
 	if (xlog_is_open(&writer->current_wal))
-		xlog_close(&writer->current_wal, false);
+		wal_xlog_close(&writer->current_wal);
 
 	if (xlog_is_open(&vy_log_writer.xlog))
-		xlog_close(&vy_log_writer.xlog, false);
+		wal_xlog_close(&vy_log_writer.xlog);
 
 	cpipe_destroy(&writer->tx_prio_pipe);
 	return 0;
@@ -1385,7 +1392,7 @@ wal_rotate_vy_log_f(struct cbus_call_msg *msg)
 {
 	(void) msg;
 	if (xlog_is_open(&vy_log_writer.xlog))
-		xlog_close(&vy_log_writer.xlog, false);
+		wal_xlog_close(&vy_log_writer.xlog);
 	return 0;
 }
 

src/box/xlog.c
@@ -774,7 +774,7 @@ xdir_add_vclock(struct xdir *xdir, const struct vclock *vclock)
 /* {{{ struct xlog */
 
 int
-xlog_rename(struct xlog *l)
+xlog_materialize(struct xlog *l)
 {
 	char *filename = l->filename;
 	char new_filename[PATH_MAX];
@@ -828,16 +828,20 @@ xlog_clear(struct xlog *l)
 	l->fd = -1;
 }
 
+/**
+ * Frees the write context allocated in xlog_init().
+ * The xlog file must be closed.
+ */
 static void
-xlog_destroy(struct xlog *xlog)
+xlog_free(struct xlog *xlog)
 {
+	assert(xlog->fd < 0);
 	assert(xlog->obuf.slabc == &cord()->slabc);
 	assert(xlog->zbuf.slabc == &cord()->slabc);
 	obuf_destroy(&xlog->obuf);
 	obuf_destroy(&xlog->zbuf);
 	ZSTD_freeCCtx(xlog->zctx);
-	TRASH(xlog);
-	xlog->fd = -1;
+	xlog->zctx = NULL;
 }
 
 int
@@ -885,7 +889,6 @@ xlog_create(struct xlog *xlog, const char *name, int flags,
 	 */
 	xlog->fd = open(xlog->filename, flags, 0644);
 	if (xlog->fd < 0) {
-		say_syserror("open, [%s]", xlog->filename);
 		diag_set(SystemError, "failed to create file '%s'",
 			 xlog->filename);
 		goto err_open;
@@ -909,9 +912,10 @@ xlog_create(struct xlog *xlog, const char *name, int flags,
 	return 0;
 err_write:
 	close(xlog->fd);
+	xlog->fd = -1;
 	unlink(xlog->filename); /* try to remove incomplete file */
 err_open:
-	xlog_destroy(xlog);
+	xlog_free(xlog);
 err:
 	return -1;
 }
@@ -982,8 +986,9 @@ no_eof:
 	return 0;
 err_read:
 	close(xlog->fd);
+	xlog->fd = -1;
 err_open:
-	xlog_destroy(xlog);
+	xlog_free(xlog);
 err:
 	return -1;
 }
@@ -1037,10 +1042,8 @@ xdir_create_xlog(struct xdir *dir, struct xlog *xlog,
 		return -1;
 
 	/* Rename xlog file */
-	if (dir->suffix != INPROGRESS && xlog_rename(xlog)) {
-		int save_errno = errno;
-		xlog_close(xlog, false);
-		errno = save_errno;
+	if (dir->suffix != INPROGRESS && xlog_materialize(xlog) != 0) {
+		xlog_discard(xlog);
 		return -1;
 	}
 
@@ -1458,30 +1461,41 @@ sync_cb(eio_req *req)
 	return 0;
 }
 
-int
+/**
+ * Syncs an xlog object to disk.
+ *
+ * If the sync_is_async flag is set in xlog_opts, fsync is called
+ * asynchronously, without checking the result.
+ *
+ * Returns 0 on success. On failure, sets diag returns -1.
+ */
+static int
 xlog_sync(struct xlog *l)
 {
 	if (l->opts.sync_is_async) {
 		int fd = dup(l->fd);
 		if (fd == -1) {
-			say_syserror("%s: dup() failed", l->filename);
+			diag_set(SystemError, "failed to duplicate fd %d",
+				 l->fd);
 			return -1;
 		}
 		eio_fsync(fd, 0, sync_cb, (void *) (intptr_t) fd);
 	} else if (fsync(l->fd) < 0) {
-		say_syserror("%s: fsync failed", l->filename);
+		diag_set(SystemError, "failed to sync file '%s'", l->filename);
 		return -1;
 	}
 	return 0;
 }
 
+/**
+ * Writes the EOF marker to the xlog file.
+ *
+ * Returns 0 on success. On failure, sets diag returns -1.
+ */
 static int
 xlog_write_eof(struct xlog *l)
 {
-	ERROR_INJECT(ERRINJ_WAL_WRITE_EOF, {
-		diag_set(ClientError, ER_INJECTION, "xlog write injection");
-		return -1;
-	});
+	ERROR_INJECT(ERRINJ_WAL_WRITE_EOF, return 0);
 
 	/*
 	 * Free disk space preallocated with xlog_fallocate().
@@ -1489,41 +1503,57 @@ xlog_write_eof(struct xlog *l)
 	 * we'll get "data after eof marker" error on recovery.
 	 */
 	if (l->allocated > 0 && ftruncate(l->fd, l->offset) < 0) {
-		diag_set(SystemError, "ftruncate() failed");
+		diag_set(SystemError, "failed to truncate file '%s'",
+			 l->filename);
 		return -1;
 	}
 
 	if (fio_writen(l->fd, &eof_marker, sizeof(eof_marker)) < 0) {
-		diag_set(SystemError, "write() failed");
+		diag_set(SystemError, "failed to write to file '%s'",
+			 l->filename);
 		return -1;
 	}
 	return 0;
 }
 
 int
-xlog_close(struct xlog *l, bool reuse_fd)
+xlog_close_reuse_fd(struct xlog *l, int *fd)
 {
-	int rc = xlog_write_eof(l);
-	if (rc < 0)
-		say_error("%s: failed to write EOF marker: %s", l->filename,
-			  diag_last_error(diag_get())->errmsg);
-
-	/*
-	 * Sync the file before closing, since
-	 * otherwise we can end up with a partially
-	 * written file in case of a crash.
-	 * We sync even if file open O_SYNC, simplify code for low cost
-	 */
-	xlog_sync(l);
-
-	if (!reuse_fd) {
-		rc = close(l->fd);
-		if (rc < 0)
-			say_syserror("%s: close() failed", l->filename);
-	}
-
-	xlog_destroy(l);
+	assert(l->fd >= 0);
+	int rc = xlog_flush(l) < 0 ? -1 : 0;
+	if (rc == 0)
+		rc = xlog_write_eof(l);
+	if (rc == 0)
+		rc = xlog_sync(l);
+	*fd = l->fd;
+	l->fd = -1;
+	xlog_free(l);
 	return rc;
+}
+
+int
+xlog_close(struct xlog *l)
+{
+	int fd;
+	int rc = xlog_close_reuse_fd(l, &fd);
+	close(fd);
+	return rc;
+}
+
+void
+xlog_discard(struct xlog *l)
+{
+	if (l->fd >= 0) {
+		close(l->fd);
+		l->fd = -1;
+		xlog_free(l);
+	}
+	if (l->filename[0] != '\0') {
+		assert(l->is_inprogress);
+		if (unlink(l->filename) != 0)
+			say_syserror("failed to remove file '%s'", l->filename);
+		l->filename[0] = '\0';
+	}
 }
 
 /* }}} */

src/box/xlog.h
@@ -481,15 +481,6 @@ xlog_is_open(struct xlog *l)
 }
 
 /**
- * Rename xlog
- *
- * @retval 0 for ok
- * @retval -1 for error
- */
-int
-xlog_rename(struct xlog *l);
-
-/**
  * Allocate @size bytes of disk space at the end of the given
  * xlog file.
  *
@@ -538,25 +529,64 @@ xlog_tx_rollback(struct xlog *log);
 ssize_t
 xlog_flush(struct xlog *log);
 
-
 /**
- * Sync a log file. The exact action is defined
- * by xdir flags.
+ * Closes an xlog object.
  *
- * @retval 0 success
- * @retval -1 error
+ * This function seals the xlog file by flushing the write buffer, writing
+ * the EOF marker, and syncing the file, then closes the xlog file fd.
+ * After calling the function, the xlog file must not be used for writing
+ * anymore. However, it may still be materialized with xlog_materialize() or
+ * discarded with xlog_discard() unless it hasn't been already materialized.
+ *
+ * Returns 0 on success. On failure, sets diag and returns -1.
+ *
+ * Note that the write buffers are freed and the xlog file fd is closed
+ * on both success and error.
+ *
+ * MT note: This function must be called in the same thread where the xlog
+ * object was created because the write buffer isn't MT-safe.
  */
 int
-xlog_sync(struct xlog *l);
+xlog_close(struct xlog *l);
 
 /**
- * Close the log file and free xlog object.
- *
- * @retval 0 success
- * @retval -1 error (fclose() failed).
+ * Same as xlog_close() but doesn't close the fd. Instead, it stores it in
+ * the out argument. Useful if the caller needs to reuse the xlog fd for reads.
  */
 int
-xlog_close(struct xlog *l, bool reuse_fd);
+xlog_close_reuse_fd(struct xlog *l, int *fd);
+
+/**
+ * Materializes an xlog object.
+ *
+ * This function removes the .inprogress suffix from the xlog file name thus
+ * making it available for future reads. It may only be called only once for
+ * an xlog object created with xlog_create(). It may not be called for an xlog
+ * object created with xlog_open(). It's allowed to call this function after
+ * closing the xlog object with xlog_close().
+ *
+ * Returns 0 on success. On error, sets diag and returns -1.
+ *
+ * MT note: This function is safe to call in any thread.
+ */
+int
+xlog_materialize(struct xlog *l);
+
+/**
+ * Discards an xlog object.
+ *
+ * This function closes the xlog file fd if it's still open and deletes the
+ * xlog file if it hasn't already been deleted. The xlog object must not be
+ * materialized (see xlog_materialize()). The xlog object must not be used
+ * after calling this function but it's allowed to call xlog_discard() more
+ * than once (all subsequent calls will be no-ops).
+ *
+ * MT note: If the xlog object was closed (see xlog_close()), this function may
+ * be called in any thread, otherwise it must be called in the same thread
+ * where the xlog object was created because the write buffer isn't MT-safe.
+ */
+void
+xlog_discard(struct xlog *l);
 
 /* {{{ xlog_tx_cursor - iterate over rows in xlog transaction */
 

test/unit/xlog.c
@@ -137,7 +137,7 @@ test_dynamic_sized_ibuf(void)
 	ok(ibuf_capacity(&cursor.rbuf) == 0, "ibuf capacity decreased to 0");
 
 	xlog_cursor_close(&cursor, false);
-	fail_if(xlog_close(&xlog, false) < 0);
+	fail_if(xlog_close(&xlog) != 0);
 	unlink(filename);
 	rmdir(dirname);
 

commit 6b85fb19c36b05d064ba45d08c87c82d318e7449

vinyl: fix memory leak in vy_task_write_run
Looks like this is typo introduced in the commit 0704ebb778d4 ("xlog:
rework writer API").

Close #9428

NO_TEST=will be tested when fiber_cxx_invoke suppression will be removed
NO_CHANGELOG=introduced in 3.0.0-alpha3
NO_DOC=bugfix

src/box/vy_run.c
@@ -2363,6 +2363,7 @@ vy_run_writer_commit(struct vy_run_writer *writer)
 			       writer->space_id, writer->iid) != 0)
 		goto out;
 
+	vy_run_writer_destroy(writer);
 	rc = 0;
 out:
 	region_truncate(&fiber()->gc, region_svp);