a3819cf22d93fdd691e704a58e528c3a0e682389
Mergen Imeev <imeevma@tarantool.org>
2023-12-12 09:47:16 +0300

    sql: fix memleak in sqlSelect
    This patch fixes a memory leak in sqlSelect that was caused by pWInfo
    not being removed if an error occurred while encoding a GROUP BY
    expression.
    
    Closes #8535
    Closes tarantool/security#125
    
    NO_DOC=memleak
    NO_TEST=memleak

--- src/box/sql/select.c
+++ src/box/sql/select.c
@@ -6277,8 +6277,6 @@ sqlSelect(Parse * pParse,		/* The parser context */
 			 */
 			sqlVdbeJumpHere(v, addr1);
 			updateAccumulator(pParse, &sAggInfo);
-			if (pParse->is_aborted)
-				goto select_end;
 			sqlVdbeAddOp2(v, OP_Integer, 1, iUseFlag);
 			VdbeComment((v, "indicate data in accumulator"));
 
@@ -6292,6 +6290,8 @@ sqlSelect(Parse * pParse,		/* The parser context */
 				sqlWhereEnd(pWInfo);
 				sqlVdbeChangeToNoop(v, addrSortingIdx);
 			}
+			if (pParse->is_aborted)
+				goto select_end;
 
 			/* Output the final row of result
 			 */
