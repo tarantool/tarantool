8562d47b8438545aa5c2b2b022eb473c199e23b3
locker <vdavydov.dev@gmail.com>
2023-12-15 08:53:04 +0000

    Deploying to gh-pages from @ tarantool/tarantool@1c737bff302efd6d8bb110f36b2e6af2b1a4e9bc 🚀

--- man/CMakeFiles/man-tarantool.dir/DependInfo.cmake
+++ man/CMakeFiles/man-tarantool.dir/DependInfo.cmake
@@ -14,5 +14,9 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
 set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
   )
 
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
 # Fortran module output directory.
 set(CMAKE_Fortran_TARGET_MODULE_DIR "")
