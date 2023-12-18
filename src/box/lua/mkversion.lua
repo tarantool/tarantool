11daa099bbaf40172e174bb44aea4fb06c8dc83c
Serge Petrenko <sergepetrenko@tarantool.org>
2023-12-18 17:50:31 +0300

    mkversion: introduce get_latest method
    mkversion.get_latest() returns the latest schema version known to this
    tarantool build. It is going to be used in config module to determine
    when dd operations are allowed.
    
    It will be used in places where `schema_needs_upgrade()` can't be used,
    because it compares only the current schema version, not any version you
    pass to it.
    
    In-scope-of #9502
    
    NO_DOC=internal
    NO_TEST=covered by the next commit
    NO_CHANGELOG=internal

--- src/box/lua/mkversion.lua
+++ src/box/lua/mkversion.lua
@@ -1,7 +1,11 @@
 local bit = require('bit')
 local ffi = require('ffi')
 
-ffi.cdef([[ uint32_t box_dd_version_id(void); ]])
+ffi.cdef([[
+    uint32_t box_dd_version_id(void);
+    uint32_t box_latest_dd_version_id(void);
+]])
+
 local builtin = ffi.C
 
 local mkversion_mt = {
@@ -48,6 +52,12 @@ local function mkversion_get()
     return mkversion_from_id(version)
 end
 
+-- Latest schema version this instance recognizes.
+local function mkversion_get_latest()
+    local version = builtin.box_latest_dd_version_id()
+    return mkversion_from_id(version)
+end
+
 local function mkversion_from_tuple(tuple)
     local major, minor, patch = tuple:unpack(2, 4)
     patch = patch or 0
@@ -61,6 +71,7 @@ end
 return setmetatable({
     new = mkversion_new,
     get = mkversion_get,
+    get_latest = mkversion_get_latest,
     from_id = mkversion_from_id,
     from_tuple = mkversion_from_tuple,
     from_string = mkversion_from_string,
