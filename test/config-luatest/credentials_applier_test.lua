tarantool/tarantool

commit fa97cc0c12e9f9c051e7455c71d8be264a50d554

config: introduce droppable alerts
For now it's impossible to drop created alert in any way except
manual searching for _alerts table. However, we need to drop alerts
on missing names, when the names are set.

Let's introduce simple key-value alerts in order to easily drop them
by key.

Needed for #8978

NO_DOC=internal
NO_TEST=following commit
NO_CHANGELOG=internal

src/box/lua/config/applier/box_cfg.lua
@@ -270,7 +270,8 @@ local function apply(config)
             if v ~= box.cfg[k] then
                 local warning = 'box_cfg.apply: non-dynamic option '..k..
                     ' will not be set until the instance is restarted'
-                config:_alert({type = 'warn', message = warning})
+                config:_alert('box_cfg_apply_non_dynamic',
+                              {type = 'warn', message = warning})
                 box_cfg[k] = nil
             end
         end

src/box/lua/config/applier/credentials.lua
@@ -469,13 +469,15 @@ local privileges_action_f = function(grant_or_revoke, role_or_user, name, privs,
         err = ('credentials.apply: box.schema.%s.%s(%q, %q, %q, %q) failed: %s')
               :format(role_or_user, grant_or_revoke, name, privs, obj_type,
                       obj_name, err)
-        config:_alert({type = 'error', message = err})
+        config:_alert('credentials_grant_or_revoke_err',
+                      {type = 'error', message = err})
     else
         local msg = "credentials.apply: %s %q hasn't been created yet, " ..
                     "'box.schema.%s.%s(%q, %q, %q, %q)' will be applied later"
         msg = msg:format(obj_type, obj_name, role_or_user, grant_or_revoke,
                          name, privs, obj_type, obj_name)
-        config:_alert({type = 'warn', message = msg})
+        config:_alert('credentials_grant_or_revoke_warn',
+                      {type = 'warn', message = msg})
     end
 end
 
@@ -621,9 +623,10 @@ local function set_password(user_name, password)
     end
 
     if user_name == 'guest' then
-        config:_alert({type = 'error',
-            message = 'credentials.apply: setting a password for ' ..
-                      'the guest user is not allowed'})
+        local message = 'credentials.apply: setting a password for ' ..
+                        'the guest user is not allowed'
+        config:_alert('credentials_apply_password',
+                      {type = 'error', message = message})
     end
 
     local auth_def = box.space._user.index.name:get({user_name})[5]
@@ -729,7 +732,8 @@ local function sync_credentials_worker()
                 local msg = 'credentials.apply: Tarantool is in Read Only ' ..
                             'mode, so credentials will be set up in the ' ..
                             'background when it is switched to Read Write mode'
-                config:_alert({type = 'warn', message = msg})
+                config:_alert('credentials_apply_ro',
+                              {type = 'warn', message = msg})
                 box.ctl.wait_rw()
             end
 

src/box/lua/config/init.lua
@@ -95,7 +95,7 @@ local function broadcast(self)
     box.broadcast('config.info', self:info())
 end
 
-function methods._alert(self, alert)
+function methods._alert(self, key, alert)
     assert(alert.type == 'error' or alert.type == 'warn')
     if alert.type == 'error' then
         log.error(alert.message)
@@ -103,7 +103,14 @@ function methods._alert(self, alert)
         log.warn(alert.message)
     end
     alert.timestamp = datetime.now()
-    table.insert(self._alerts, alert)
+    self._alerts[key] = alert
+end
+
+function methods._alert_drop(self, key)
+    self._alerts[key] = nil
+    if self._status == 'check_warnings' and table.equals(self._alerts, {}) then
+        self._status = 'ready'
+    end
 end
 
 function methods._meta(self, source_name, key, value)
@@ -446,7 +453,7 @@ function methods._reload_noexc(self, opts)
 
     assert(not ok or err == nil)
     if not ok then
-        self:_alert({type = 'error', message = err})
+        self:_alert('reload_error', {type = 'error', message = err})
     end
 
     self:_set_status_based_on_alerts()
@@ -464,8 +471,17 @@ end
 
 function methods.info(self)
     selfcheck(self, 'info')
+    -- Don't return alert keys from config:info().
+    local alerts = {}
+    for _, alert in pairs(self._alerts) do
+        table.insert(alerts, alert)
+    end
+    table.sort(alerts, function(a, b)
+        return a.timestamp < b.timestamp
+    end)
+
     return {
-        alerts = self._alerts,
+        alerts = alerts,
         meta = self._metadata,
         status = self._status,
     }

commit 35edd7c927058b5703ada84424460e61155073c0

config: report all alerts of the same type
Before this patch `config:info().alerts` reports only last alert of a
certain type, while others are skipped. It is a regression from commit
fa97cc0c12e9 ("config: introduce droppable alerts").

The idea of the fix is to eliminate a key from all the alerts that are
not to be dropped later except on configuration reloading.

Fixes #9586

NO_DOC=bugfix

src/box/lua/config/applier/box_cfg.lua
@@ -106,7 +106,8 @@ local function names_alert_missing(config, missing_names)
        config._aboard:get(replicaset_name) == nil then
         local replicaset_uuid = missing_names[replicaset_name]
         local warning = msg:format(replicaset_name, replicaset_uuid)
-        config._aboard:set(replicaset_name, {type = 'warn', message = warning})
+        config._aboard:set({type = 'warn', message = warning},
+            {key = replicaset_name})
     end
 
     local unknown_msg = 'box_cfg.apply: instance %s is unknown. Possibly ' ..
@@ -122,7 +123,7 @@ local function names_alert_missing(config, missing_names)
         -- Alert may be done with 'unknown' uuid. If it was so, update alert.
         local alert = config._aboard:get(name)
         if alert == nil or alert.message ~= warning then
-            config._aboard:set(name, {type = 'warn', message = warning})
+            config._aboard:set({type = 'warn', message = warning}, {key = name})
         end
     end
 end
@@ -549,8 +550,7 @@ local function apply(config)
             if v ~= box.cfg[k] then
                 local warning = 'box_cfg.apply: non-dynamic option '..k..
                     ' will not be set until the instance is restarted'
-                config._aboard:set('box_cfg_apply_non_dynamic',
-                                   {type = 'warn', message = warning})
+                config._aboard:set({type = 'warn', message = warning})
                 box_cfg[k] = nil
             end
         end

src/box/lua/config/applier/credentials.lua
@@ -544,15 +544,13 @@ local privileges_action_f = function(grant_or_revoke, role_or_user, name, privs,
         err = ('credentials.apply: box.schema.%s.%s(%q, %q, %q, %q) failed: %s')
               :format(role_or_user, grant_or_revoke, name, privs, obj_type,
                       obj_name, err)
-        config._aboard:set('credentials_grant_or_revoke_err',
-                           {type = 'error', message = err})
+        config._aboard:set({type = 'error', message = err})
     else
         local msg = "credentials.apply: %s %q hasn't been created yet, " ..
                     "'box.schema.%s.%s(%q, %q, %q, %q)' will be applied later"
         msg = msg:format(obj_type, obj_name, role_or_user, grant_or_revoke,
                          name, privs, obj_type, obj_name)
-        config._aboard:set('credentials_grant_or_revoke_warn',
-                           {type = 'warn', message = msg})
+        config._aboard:set({type = 'warn', message = msg})
     end
 end
 
@@ -700,8 +698,7 @@ local function set_password(user_name, password)
     if user_name == 'guest' then
         local message = 'credentials.apply: setting a password for ' ..
                         'the guest user is not allowed'
-        config._aboard:set('credentials_apply_password',
-                           {type = 'error', message = message})
+        config._aboard:set({type = 'error', message = message})
     end
 
     local auth_def = box.space._user.index.name:get({user_name})[5]

src/box/lua/config/init.lua
@@ -456,7 +456,7 @@ function methods._reload_noexc(self, opts)
 
     assert(not ok or err == nil)
     if not ok then
-        self._aboard:set('reload_error', {type = 'error', message = err})
+        self._aboard:set({type = 'error', message = err})
     end
 
     self:_set_status_based_on_alerts()

src/box/lua/config/utils/aboard.lua
@@ -5,8 +5,9 @@ local log = require('internal.config.utils.log')
 
 -- Set an alert.
 --
--- The `key` is an unique identifier of the alert. Attempt to set
--- a new alert with the same key replaces the existing alert.
+-- Optional `opts.key` declares an unique identifier of the alert.
+-- Attempt to set a new alert with the same key replaces the
+-- existing alert.
 --
 -- The key can be used to drop the alert using :drop().
 --
@@ -25,7 +26,7 @@ local log = require('internal.config.utils.log')
 --
 -- A timestamp is saved on the :set() method call to show it in
 -- an :alerts() result.
-local function aboard_set(self, key, alert)
+local function aboard_set(self, alert, opts)
     assert(alert.type == 'error' or alert.type == 'warn')
     if alert.type == 'error' then
         log.error(alert.message)
@@ -33,6 +34,15 @@ local function aboard_set(self, key, alert)
         log.warn(alert.message)
     end
     alert.timestamp = datetime.now()
+
+    local key
+    if opts == nil or opts.key == nil then
+        key = self._next_key
+        self._next_key = self._next_key + 1
+    else
+        key = opts.key
+    end
+
     self._alerts[key] = alert
 end
 
@@ -62,6 +72,7 @@ end
 -- The `on_drop` callback is NOT called.
 local function aboard_clean(self)
     self._alerts = {}
+    self._next_key = 1
 end
 
 -- Serialize the alerts to show them to a user.
@@ -152,6 +163,7 @@ local function new(opts)
         --
         -- The key can be a string or a number.
         _alerts = {},
+        _next_key = 1,
         _on_drop = on_drop,
     }, mt)
 end

test/config-luatest/config_test.lua
@@ -1353,3 +1353,43 @@ g.test_replication_ssl_enterprise = function()
         t.assert_equals(res.params.ssl_password_file, {password_file})
     end, {c1c, c1k, passwd_file, ciphers})
 end
+
+-- Verify that all the non-dynamic options that are changed on the
+-- configuration reloading are reported in alerts.
+g.test_non_dynamic_alert = function(g)
+    helpers.reload_success_case(g, {
+        options = {},
+        verify = function() end,
+        options_2 = {
+            ['process.strip_core'] = false,
+            ['process.background'] = true,
+            ['process.work_dir'] = 'foo',
+        },
+        verify_2 = function()
+            local config = require('config')
+
+            local options = {}
+            local info = config:info()
+            for _, alert in ipairs(info.alerts) do
+                if alert.message:find('non%-dynamic option') then
+                    local pattern = 'non%-dynamic option (.-) will not be set'
+                    local option = alert.message:match(pattern)
+                    table.insert(options, option)
+                end
+            end
+            table.sort(options)
+
+            t.assert_equals({
+                status = info.status,
+                options = options,
+            }, {
+                status = 'check_warnings',
+                options = {
+                    'background',
+                    'strip_core',
+                    'work_dir',
+                },
+            })
+        end,
+    })
+end

test/config-luatest/credentials_applier_test.lua
@@ -16,6 +16,26 @@ g.after_all(function(g)
     treegen.clean(g)
 end)
 
+-- Collect delayed grant alerts and transform to
+-- '<space> <permission>' form.
+local function warnings()
+    local config = require('config')
+
+    local res = {}
+    for _, alert in ipairs(config:info().alerts) do
+        if alert.message:find('box.schema.user.grant') then
+            local pattern = 'box.schema.user.grant%(".-", "(.-)", ' ..
+                '"space", "(.-)"%)'
+            local permissions, space = alert.message:match(pattern)
+            for _, permission in ipairs(permissions:split(',')) do
+                table.insert(res, ('%s %s'):format(space, permission))
+            end
+        end
+    end
+    table.sort(res)
+    return res
+end
+
 g.test_converters = function()
     -- Guest privileges in format provided by box.schema.{user,role}.info()
     local box_guest_privileges = {{
@@ -1343,5 +1363,44 @@ g.test_consider_auth_type_for_passwods = function(g)
             local password_def = box.space._user.index.name:get({'myuser'})[5]
             t.assert_equals(type(password_def['pap-sha256']), 'table')
         end,
+    })
+end
+
+-- Verify that all the missed permissions are reported in
+-- config:info().alerts.
+--
+-- In this case,
+--
+-- * s read
+-- * s write
+-- * t read
+-- * t write
+g.test_delayed_grant_alert = function(g)
+    helpers.success_case(g, {
+        options = {
+            ['credentials.users.guest.privileges'] = {
+                {
+                    permissions = {'read', 'write'},
+                    spaces = {'s', 't'},
+                },
+            },
+        },
+        verify = function(warnings)
+            local config = require('config')
+
+            t.assert_equals({
+                status = config:info().status,
+                alerts = loadstring(warnings)(),
+            }, {
+                status = 'check_warnings',
+                alerts = {
+                    's read',
+                    's write',
+                    't read',
+                    't write',
+                },
+            })
+        end,
+        verify_args = {string.dump(warnings)},
     })
 end