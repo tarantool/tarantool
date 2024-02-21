0d0266c9a221e3e26063103a78096205c10f1d65
Nikolay Shirokovskiy <nshirokovskiy@tarantool.org>
2024-02-21 13:36:23 +0300

    iproto: fix assertion on dropping of a new connection
    We need to handle case of dropping new connection. When
    net_send_greeting() is executed the connection can be closed due to
    iproto_drop_connections() call.
    
    Note that in the test the Tarantool crashes for another reason. Due to
    access after sleep to the connection that is destroyed so its memory is
    poisoned. Yet we visit net_send_greeting() too in the test with patch so
    original issue is verified too. We also need to test that such
    a connection is closed. This will be done in EE version.
    
    Closes #9717
    
    NO_DOC=bugfix
    
    (cherry picked from commit a12998a52a7387936d96cdbe3d5a0e7266047487)

--- test/box-luatest/shutdown_test.lua
+++ test/box-luatest/shutdown_test.lua
@@ -1,7 +1,52 @@
 local server = require('luatest.server')
+local utils = require('luatest.utils')
+local justrun = require('test.justrun')
 local fio = require('fio')
 local fiber = require('fiber')
 local t = require('luatest')
+
+-- Luatest server currently does not allow to check process exit code.
+local g_crash = t.group('crash')
+
+g_crash.before_each(function(cg)
+    local id = ('%s-%s'):format('server', utils.generate_id())
+    cg.workdir = fio.pathjoin(server.vardir, id)
+    fio.mkdir(cg.workdir)
+end)
+
+g_crash.after_each(function(cg)
+    if cg.handle ~= nil then
+        cg.handle:close()
+    end
+    cg.handle = nil
+end)
+
+-- Test shutdown when new iproto connection is accepted but
+-- not yet fully established.
+g_crash.test_shutdown_during_new_connection = function(cg)
+    local script = [[
+        local net = require('net.box')
+        local fiber = require('fiber')
+
+        local sock = 'unix/:./iproto.sock'
+        box.cfg{listen = sock}
+        local cond = fiber.cond()
+        local in_trigger = false
+        box.session.on_connect(function()
+            in_trigger = true
+            cond:signal()
+            fiber.sleep(1)
+        end)
+        net.connect(sock, {wait_connected = false})
+        while not in_trigger do
+            cond:wait()
+        end
+        os.exit()
+    ]]
+    local result = justrun.tarantool(cg.workdir, {}, {'-e', script},
+                                     {nojson = true, quote_args = true})
+    t.assert_equals(result.exit_code, 0)
+end
 
 local g = t.group()
 
