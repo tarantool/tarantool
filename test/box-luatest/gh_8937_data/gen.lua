97a678de1de0e947378dd2f3dc469a39dd0ad763
Ilya Verbin <iverbin@tarantool.org>
2024-03-13 14:22:27 +0300

    test: fix gh_8937_recover_wrong_hint_options_test.lua snapshots
    The test fails when Tarantool with a renewed schema version recovers from
    a gh_8937_data/ snapshot, because `luatest/server_instance.lua` executes:
    box.schema.user.grant('guest', 'super', nil, nil, {if_not_exists = true})
    while insert into system spaces is forbidden until schema is upgraded.
    Fix it by adding `box.schema.user.grant()` to the snapshot.
    
    NO_DOC=test
    NO_CHANGELOG=test

--- test/box-luatest/gh_8937_data/gen.lua
+++ test/box-luatest/gh_8937_data/gen.lua
@@ -3,6 +3,10 @@
 
 box.cfg{}
 
+-- DDL is disabled until schema is upgraded (see gh-7149) so we have to grant
+-- permissions required to run the test manually.
+box.schema.user.grant('guest', 'super')
+
 local s = box.schema.create_space('gh_8937_memtx')
 box.space._index:insert{s.id, 0, "pk", "hash", {hint = true}, {{0, "unsigned"}}}
 
