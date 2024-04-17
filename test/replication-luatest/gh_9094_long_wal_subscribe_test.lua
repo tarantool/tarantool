3d0519fc7de00d06211c7696cd5cde020ef320e8
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2024-04-17 23:28:36 +0200

    relay: fix timeout on large xlog subscribe start
    When a replica subscribes, it might in the beginning try to
    position its reader cursor to the end of a large xlog file.
    
    Positioning inside of this file can take significant time during
    which the WAL reader yielded and tried to send heartbeats, but
    couldn't, because the relay thread wasn't communicating with the
    TX thread.
    
    When there are no messages from TX for too long time, the
    heartbeats to the replica are not being sent
    (commit 56571d83172f991d2e8c09b85c27f079c067fe43 ("raft: make
    followers notice leader hang")).
    
    The relay must communicate with the TX thread even when subscribe
    is just being started and opens a large xlog file.
    
    This isn't the first time when the missing heartbeats result into
    timeouts. See more here:
    
    - commit 30ad4a5549c215e4e46579ad65fae55c00558843 ("relay: yield
        explicitly every N sent rows").
    
    - commit 17289440bf3c75e573824e53db226084b241bd33 ("recovery: make
        it yield when positioning in a WAL").
    
    - commit ee6de025f7ff4a6fd8e60efafcc78bf613f1686d ("relay: send
        heartbeats while reading a WAL").
    
    Given that this is fixed fourth time, it might suggest that the
    relay has not the best architecture having some slight drawbacks.
    See more in #9968.
    
    Closes #9094
    
    NO_DOC=bugfix
    
    (cherry picked from commit f7e6686a43a700527b46557aa50b8cb785cb764f)

---
+++ test/replication-luatest/gh_9094_long_wal_subscribe_test.lua
@@ -0,0 +1,81 @@
+local t = require('luatest')
+local cluster = require('luatest.replica_set')
+
+--
+-- gh-9094: when a replica subscribes, it might in the beginning try to position
+-- its reader cursor to the end of a large xlog file. Positioning inside of this
+-- file can take significant time during which the WAL reader yielded and tried
+-- to send heartbeats, but couldn't, because the relay thread wasn't
+-- communicating with the TX thread. And when there are no messages from TX for
+-- too long time, the heartbeats to the replica are not being sent.
+--
+-- The relay must communicate with the TX thread even when subscribe is just
+-- being started and opens a large xlog file.
+--
+local g = t.group('gh_9094')
+
+g.before_all(function(cg)
+    cg.cluster = cluster:new({})
+    cg.master = cg.cluster:build_and_add_server{
+        alias = 'master',
+        box_cfg = {
+            replication_timeout = 0.05,
+        },
+    }
+    cg.replica = cg.cluster:build_and_add_server{
+        alias = 'replica',
+        box_cfg = {
+            replication = {
+                cg.master.net_box_uri,
+            },
+            replication_timeout = 0.05,
+            read_only = true,
+        },
+    }
+    cg.cluster:start()
+end)
+
+g.after_all(function(cg)
+    cg.cluster:drop()
+end)
+
+g.test_recovery = function(cg)
+    t.tarantool.skip_if_not_debug()
+
+    cg.master:exec(function()
+        box.schema.space.create('test'):create_index('pk')
+    end)
+    cg.replica:wait_for_vclock_of(cg.master)
+    cg.replica:stop()
+    -- Relay on subscribe will hang simulating a large xlog opening.
+    cg.master:exec(function()
+        box.error.injection.set('ERRINJ_RELAY_WAL_START_DELAY', true)
+        box.space.test:insert{1}
+    end)
+    cg.replica:start()
+    -- If the bug would still exist, it would take relay x4 timeouts to notice
+    -- the TX thread not "responding" and to stop sending heartbeats, and x4
+    -- timeouts for the replica to fail due to not receiving heartbeats.
+    cg.master:exec(function()
+        require('fiber').sleep(box.cfg.replication_timeout * 8)
+    end)
+    -- If during the opening the relay was processing TX messages, the replica's
+    -- upstream mustn't be in a failed state and master's TX thread must update
+    -- the idle duration of the relay.
+    cg.replica:exec(function()
+        local upstream = box.info.replication[1].upstream
+        t.helpers.retrying({}, function()
+            upstream = box.info.replication[1].upstream
+            t.assert_equals(upstream.status, 'sync')
+        end)
+        t.assert_equals(upstream.message, nil)
+    end)
+    cg.master:exec(function(replica_id)
+        t.helpers.retrying({}, function()
+            t.assert_le(box.info.replication[replica_id].downstream.idle,
+                        box.cfg.replication_timeout)
+        end)
+        box.error.injection.set('ERRINJ_RELAY_WAL_START_DELAY', false)
+    end, {cg.replica:get_instance_id()})
+    cg.replica:wait_for_vclock_of(cg.master)
+end
