tarantool/tarantool

commit 8fc59b101c10cf71fbb157b6f98b64eb8155ae63

box: add details to DML errors related to key validation
Add next error payload fields:
- space name
- space id
- index name
- index id
- key

Part of #7223

NO_CHANGELOG=unfinished
NO_DOC=minor

src/box/index.cc
@@ -74,10 +74,41 @@ BuildUnsupportedIndexFeature(const char *file, unsigned line,
 	}
 }
 
+/**
+ * Add key as the last error payload field with name "key". The key
+ * is MsgPack array without array prefix. The size of the array
+ * is given by part_count.
+ */
+static void
+error_set_key(struct error *error, const char *key, uint32_t part_count)
+{
+	const char *key_end = key;
+	for (uint32_t i = 0; i < part_count; i++)
+		mp_next(&key_end);
+	size_t region_svp = region_used(&fiber()->gc);
+	size_t key_buf_size = key_end - key + mp_sizeof_array(part_count);
+	char *key_buf = (char *)xregion_alloc(&fiber()->gc, key_buf_size);
+	char *key_pos = (char *)mp_encode_array(key_buf, part_count);
+	memcpy(key_pos, key, key_end - key);
+	error_set_mp(error, "key", key_buf, key_buf_size);
+	region_truncate(&fiber()->gc, region_svp);
+}
+
+void
+error_set_index(struct error *error, const struct index_def *index_def)
+{
+	error_set_str(error, "index", index_def->name);
+	error_set_uint(error, "index_id", index_def->iid);
+	struct space *space = space_by_id(index_def->space_id);
+	error_set_str(error, "space", space->def->name);
+	error_set_uint(error, "space_id", index_def->space_id);
+}
+
 int
 key_validate(const struct index_def *index_def, enum iterator_type type,
 	     const char *key, uint32_t part_count)
 {
+	const char *key_begin = key;
 	assert(key != NULL || part_count == 0);
 	if (part_count == 0) {
 		/*
@@ -97,28 +128,28 @@ key_validate(const struct index_def *index_def, enum iterator_type type,
 		if (part_count != 1 && part_count != d && part_count != d * 2) {
 			diag_set(ClientError, ER_KEY_PART_COUNT, d  * 2,
 				 part_count);
-			return -1;
+			goto error;
 		}
 		if (part_count == 1) {
 			if (key_part_validate(FIELD_TYPE_ARRAY, key, 0, false))
-				return -1;
+				goto error;
 			uint32_t array_size = mp_decode_array(&key);
 			if (array_size != d && array_size != d * 2) {
 				diag_set(ClientError, ER_RTREE_RECT, "Key", d,
 					 d * 2);
-				return -1;
+				goto error;
 			}
 			for (uint32_t part = 0; part < array_size; part++) {
 				if (key_part_validate(FIELD_TYPE_NUMBER, key,
-						      0, false))
-					return -1;
+						      part, false))
+					goto error;
 				mp_next(&key);
 			}
 		} else {
 			for (uint32_t part = 0; part < part_count; part++) {
 				if (key_part_validate(FIELD_TYPE_NUMBER, key,
 						      part, false))
-					return -1;
+					goto error;
 				mp_next(&key);
 			}
 		}
@@ -126,7 +157,7 @@ key_validate(const struct index_def *index_def, enum iterator_type type,
 		if (part_count > index_def->key_def->part_count) {
 			diag_set(ClientError, ER_KEY_PART_COUNT,
 				 index_def->key_def->part_count, part_count);
-			return -1;
+			goto error;
 		}
 
 		/* Partial keys are allowed only for TREE index type. */
@@ -135,14 +166,18 @@ key_validate(const struct index_def *index_def, enum iterator_type type,
 				 index_type_strs[index_def->type],
 				 index_def->key_def->part_count,
 				 part_count);
-			return -1;
+			goto error;
 		}
 		const char *key_end;
 		if (key_validate_parts(index_def->key_def, key,
 				       part_count, true, &key_end) != 0)
-			return -1;
+			goto error;
 	}
 	return 0;
+error:
+	error_set_index(diag_last_error(diag_get()), index_def);
+	error_set_key(diag_last_error(diag_get()), key_begin, part_count);
+	return -1;
 }
 
 int
@@ -153,15 +188,22 @@ exact_key_validate(struct index_def *index_def, const char *key,
 	struct key_def *key_def = index_def->key_def;
 	if (!index_def->opts.is_unique) {
 		diag_set(ClientError, ER_MORE_THAN_ONE_TUPLE);
+		error_set_index(diag_last_error(diag_get()), index_def);
 		return -1;
 	}
 	if (key_def->part_count != part_count) {
 		diag_set(ClientError, ER_EXACT_MATCH, key_def->part_count,
 			 part_count);
-		return -1;
+		goto error;
 	}
 	const char *key_end;
-	return key_validate_parts(key_def, key, part_count, false, &key_end);
+	if (key_validate_parts(key_def, key, part_count, false, &key_end) != 0)
+		goto error;
+	return 0;
+error:
+	error_set_key(diag_last_error(diag_get()), key, part_count);
+	error_set_index(diag_last_error(diag_get()), index_def);
+	return -1;
 }
 
 char *

src/box/index.h
@@ -796,6 +796,10 @@ replace_check_dup(struct tuple *old_tuple, struct tuple *dup_tuple,
 	return 0;
 }
 
+/** Add payload fields with index details to the error. */
+void
+error_set_index(struct error *error, const struct index_def *index_def);
+
 /**
  * Initialize an index instance.
  * Note, this function copies the given index definition.

test/box-luatest/error_subsystem_improvements_test.lua
@@ -449,3 +449,144 @@ g.test_error_name = function()
     local e = box.error.new(box.error.ILLEGAL_PARAMS, 'foo')
     t.assert_equals(e.name, 'ILLEGAL_PARAMS')
 end
+
+g.test_dml_key_validation_errors = function(cg)
+    cg.server:exec(function()
+        local test = box.schema.create_space('test')
+        local index
+        test:create_index('primary')
+
+        -- Test non exact key match for non RTREE index.
+        index = test:create_index('tree', {
+            parts = {1, 'unsigned', 2, 'unsigned'}
+        })
+        t.assert_error_covers({
+            space = 'test',
+            index = 'tree',
+            space_id = test.id,
+            index_id = index.id,
+            key = {1, 2, 3},
+            message = 'Invalid key part count (expected [0..2], got 3)'
+        }, index.select, index, {1, 2, 3})
+        t.assert_error_covers({
+            space = 'test',
+            index = 'tree',
+            space_id = test.id,
+            index_id = index.id,
+            key = {1, 'foo'},
+            message = 'Supplied key type of part 1 does not match ' ..
+                      'index part type: expected unsigned'
+        }, index.select, index, {1, 'foo'})
+        index:drop()
+
+        index = test:create_index('hash', {
+            type = 'HASH',
+            parts = {1, 'unsigned', 2, 'unsigned'}
+        })
+        t.assert_error_covers({
+            space = 'test',
+            index = 'hash',
+            space_id = test.id,
+            index_id = index.id,
+            key = {11},
+            message = 'HASH index  does not support selects via a partial ' ..
+                      'key (expected 2 parts, got 1). Please Consider ' ..
+                      'changing index type to TREE.'
+        }, index.select, index, {11})
+        index:drop()
+
+        -- Test non exact key match for RTREE index.
+        index = test:create_index('rtree', {type = 'RTREE',
+                                    unique = false,
+                                    parts = {2, 'array'}})
+        t.assert_error_covers({
+            space = 'test',
+            index = 'rtree',
+            space_id = test.id,
+            index_id = index.id,
+            key = {1, 2, 3},
+            message = 'Invalid key part count (expected [0..4], got 3)'
+        }, index.select, index, {1, 2, 3})
+        t.assert_error_covers({
+            space = 'test',
+            index = 'rtree',
+            space_id = test.id,
+            index_id = index.id,
+            key = {'foo'},
+            message = 'Supplied key type of part 0 does not match index ' ..
+                      'part type: expected array'
+        }, index.select, index, {'foo'})
+        t.assert_error_covers({
+            space = 'test',
+            index = 'rtree',
+            space_id = test.id,
+            index_id = index.id,
+            key = {{1, 2, 3}},
+            message = 'RTree: Key must be an array with 2 (point) or 4 ' ..
+                      '(rectangle/box) numeric coordinates'
+        }, index.select, index, {{1, 2, 3}})
+        t.assert_error_covers({
+            space = 'test',
+            index = 'rtree',
+            space_id = test.id,
+            index_id = index.id,
+            key = {{1, 'foo'}},
+            message = 'Supplied key type of part 1 does not match index ' ..
+                      'part type: expected number'
+        }, index.select, index, {{1, 'foo'}})
+        t.assert_error_covers({
+            space = 'test',
+            index = 'rtree',
+            space_id = test.id,
+            index_id = index.id,
+            key = {1, 'foo'},
+            message = 'Supplied key type of part 1 does not match index ' ..
+                      'part type: expected number'
+        }, index.select, index, {1, 'foo'})
+        index:drop()
+
+        -- Test exact key match.
+        index = test:create_index('secondary', {
+            parts = {1, 'unsigned', 2, 'unsigned'}
+        })
+        t.assert_error_covers({
+            space = 'test',
+            index = 'secondary',
+            space_id = test.id,
+            index_id = index.id,
+            key = {1, 2, 3},
+            message = 'Invalid key part count in an exact match ' ..
+                      '(expected 2, got 3)'
+        }, index.get, index, {1, 2, 3})
+        t.assert_error_covers({
+            space = 'test',
+            index = 'secondary',
+            space_id = test.id,
+            index_id = index.id,
+            key = {1, 'foo'},
+            message = 'Supplied key type of part 1 does not match index ' ..
+                      'part type: expected unsigned'
+        }, index.get, index, {1, 'foo'})
+        index:drop()
+        index = test:create_index('secondary', {
+            parts = {1, 'unsigned', 2, 'unsigned'},
+            unique = false,
+        })
+        t.assert_error_covers({
+            space = 'test',
+            index = 'secondary',
+            space_id = test.id,
+            index_id = index.id,
+            message = "Get() doesn't support partial keys and non-unique " ..
+                      "indexes"
+        }, index.delete, index, {1, 2})
+    end)
+end
+
+g.after_test('test_dml_key_validation_errors', function(cg)
+    cg.server:exec(function()
+        if box.space.test ~= nil then
+            box.space.test:drop()
+        end
+    end)
+end)

commit e3e41cbc4e2b6a479bb7050eb0d412d96cf14143

box: store space name and engine name in `index_def` for error reporting
This patch fixes a bug that was introduced by commit https://github.com/tarantool/tarantool/commit/8fc59b101c10cf71fbb157b6f98b64eb8155ae63
that adds helper functions for initializing payloads of DML errors related
to key validation.

The problem was that after the space was deleted, a situation could occur
that the `space.def->name` was still needed to generate an error message
if the key format was incorrect in the `get()` call from `read_view`.

This is exactly what happened sometimes in the function `error_set_index`.
One of the bug reproducers was to try to `get()` with an incorrect key
format (`ER_EXACT_MATCH`) in `read_view` from deleted space.

The problem has been fixed: space name as well as engine name are also
`index_def` fields now. There is no need to search for space and get these
fields from it.

Also some other places where space name and engine name could be taken
from `index_def` has been fixed.

Needed for tarantool/tarantool-ee#787

NO_DOC=bugfix
NO_TEST=ee
NO_CHANGELOG=bug not released

perf/memtx.cc
@@ -160,6 +160,7 @@ private:
 		if (key_def == nullptr)
 			panic("failed to create new key definition");
 		tree_idx_def = ::index_def_new(sid, tree_index_id, "pk",
+					       NULL, NULL,
 					       std::strlen("pk"), TREE,
 					       &idx_opts, key_def,
 					       /*pk_def=*/nullptr);

src/box/alter.cc
@@ -389,7 +389,7 @@ index_def_new_from_tuple(struct tuple *tuple, struct space *space)
 	if (key_def == NULL)
 		return NULL;
 	struct index_def *index_def =
-		index_def_new(id, index_id, name, name_len, type,
+		index_def_new(id, index_id, name, NULL, NULL, name_len, type,
 			      &opts, key_def, space_index_key_def(space, 0));
 	if (index_def == NULL)
 		return NULL;
@@ -1767,8 +1767,9 @@ alter_space_move_indexes(struct alter_space *alter, uint32_t begin,
 		 * Rebuild secondary indexes that depend on the
 		 * primary key since primary key parts have changed.
 		 */
-		new_def = index_def_new(old_def->space_id, old_def->iid,
-					old_def->name, strlen(old_def->name),
+		new_def = index_def_new(old_def->space_id, old_def->iid, old_def->name,
+					old_def->space_name, old_def->engine_name,
+					strlen(old_def->name),
 					old_def->type, &old_def->opts,
 					old_def->key_def, alter->pk_def);
 		index_def_update_optionality(new_def, min_field_count);

src/box/index.cc
@@ -51,15 +51,14 @@ UnsupportedIndexFeature::UnsupportedIndexFeature(const char *file,
 	unsigned line, struct index_def *index_def, const char *what)
 	: ClientError(file, line, ER_UNKNOWN)
 {
-	struct space *space = space_cache_find_xc(index_def->space_id);
 	code = ER_UNSUPPORTED_INDEX_FEATURE;
 	error_format_msg(this, tnt_errcode_desc(code), index_def->name,
 			 index_type_strs[index_def->type],
-			 space->def->name, space->def->engine_name, what);
+			 index_def->space_name, index_def->engine_name, what);
 	error_set_str(this, "index", index_def->name);
 	error_set_str(this, "index_type", index_type_strs[index_def->type]);
-	error_set_str(this, "space", space->def->name);
-	error_set_str(this, "engine", space->def->engine_name);
+	error_set_str(this, "space", index_def->space_name);
+	error_set_str(this, "engine", index_def->engine_name);
 	error_set_str(this, "feature", what);
 }
 
@@ -95,8 +94,7 @@ error_set_index(struct error *error, const struct index_def *index_def)
 {
 	error_set_str(error, "index", index_def->name);
 	error_set_uint(error, "index_id", index_def->iid);
-	struct space *space = space_by_id(index_def->space_id);
-	error_set_str(error, "space", space->def->name);
+	error_set_str(error, "space", index_def->space_name);
 	error_set_uint(error, "space_id", index_def->space_id);
 }
 
@@ -214,10 +212,8 @@ index_check_dup(struct index *index, struct tuple *old_tuple,
 			 * dup_replace_mode is DUP_REPLACE, and
 			 * a tuple with the same key is not found.
 			 */
-			struct space *space = space_by_id(index->def->space_id);
-			assert(space != NULL);
 			diag_set(ClientError, ER_CANT_UPDATE_PRIMARY_KEY,
-				 space->def->name, space->def->id,
+				 index->def->space_name, index->def->space_id,
 				 old_tuple, new_tuple, NULL);
 			return -1;
 		}
@@ -230,10 +226,8 @@ index_check_dup(struct index *index, struct tuple *old_tuple,
 			 * possibly delete more than one tuple
 			 * at once.
 			 */
-			struct space *space = space_by_id(index->def->space_id);
-			assert(space != NULL);
 			diag_set(ClientError, ER_TUPLE_FOUND,
-				 index->def->name, space->def->name,
+				 index->def->name, index->def->space_name,
 				 tuple_str(dup_tuple), tuple_str(new_tuple),
 				 dup_tuple, new_tuple);
 			return -1;

src/box/index_def.c
@@ -94,6 +94,7 @@ const struct opt_def index_opts_reg[] = {
 
 struct index_def *
 index_def_new(uint32_t space_id, uint32_t iid, const char *name,
+	      const char *space_name, const char* engine_name,
 	      uint32_t name_len, enum index_type type,
 	      const struct index_opts *opts,
 	      struct key_def *key_def, struct key_def *pk_def)
@@ -107,14 +108,23 @@ index_def_new(uint32_t space_id, uint32_t iid, const char *name,
 	}
 	def->name = strndup(name, name_len);
 	if (def->name == NULL) {
-		index_def_delete(def);
 		diag_set(OutOfMemory, name_len + 1, "malloc", "index_def name");
-		return NULL;
+		goto error;
 	}
-	if (identifier_check(def->name, name_len)) {
-		index_def_delete(def);
-		return NULL;
+	if (identifier_check(def->name, name_len))
+		goto error;
+	if (space_name != NULL) {
+		def->space_name = strdup(space_name);
+		if (def->space_name == NULL) {
+			diag_set(OutOfMemory, strlen(space_name) + 1, "malloc",
+				 "index_def space_name");
+			goto error;
+		}
 	}
+	memset(def->engine_name, 0, sizeof(def->engine_name));
+	if (engine_name != NULL)
+		strlcpy(def->engine_name, engine_name, ENGINE_NAME_MAX);
+
 	def->key_def = key_def_dup(key_def);
 	if (iid != 0) {
 		assert(pk_def != NULL);
@@ -136,6 +146,10 @@ index_def_new(uint32_t space_id, uint32_t iid, const char *name,
 	def->iid = iid;
 	def->opts = *opts;
 	return def;
+
+error:
+	index_def_delete(def);
+	return NULL;
 }
 
 struct index_def *
@@ -144,6 +158,9 @@ index_def_dup(const struct index_def *def)
 	struct index_def *dup = xmalloc(sizeof(*dup));
 	*dup = *def;
 	dup->name = xstrdup(def->name);
+	if (def->space_name != NULL)
+		dup->space_name = xstrdup(def->space_name);
+	strlcpy(dup->engine_name, def->engine_name, ENGINE_NAME_MAX);
 	dup->key_def = key_def_dup(def->key_def);
 	dup->cmp_def = key_def_dup(def->cmp_def);
 	dup->pk_def = key_def_dup(def->pk_def);
@@ -157,6 +174,7 @@ index_def_delete(struct index_def *index_def)
 {
 	index_opts_destroy(&index_def->opts);
 	free(index_def->name);
+	free(index_def->space_name);
 
 	if (index_def->key_def)
 		key_def_delete(index_def->key_def);

src/box/index_def.h
@@ -33,6 +33,7 @@
 
 #include "key_def.h"
 #include "opt_def.h"
+#include "schema_def.h"
 #include "small/rlist.h"
 
 #if defined(__cplusplus)
@@ -160,19 +161,23 @@ index_opts_cmp(const struct index_opts *o1, const struct index_opts *o2)
 
 /* Definition of an index. */
 struct index_def {
-	/* A link in key list. */
+	/** A link in key list. */
 	struct rlist link;
 	/** Ordinal index number in the index array. */
 	uint32_t iid;
-	/* Space id. */
+	/** Space id. */
 	uint32_t space_id;
+	/** Space name. */
+	char *space_name;
+	/** Engine name. */
+	char engine_name[ENGINE_NAME_MAX + 1];
 	/** Index name. */
 	char *name;
 	/** Index type. */
 	enum index_type type;
 	struct index_opts opts;
 
-	/* Index key definition. */
+	/** Index key definition. */
 	struct key_def *key_def;
 	/**
 	 * User-defined key definition, merged with the primary
@@ -275,6 +280,7 @@ index_def_list_add(struct rlist *index_def_list, struct index_def *index_def)
  */
 struct index_def *
 index_def_new(uint32_t space_id, uint32_t iid, const char *name,
+	      const char *space_name, const char* engine_name,
 	      uint32_t name_len, enum index_type type,
 	      const struct index_opts *opts,
 	      struct key_def *key_def, struct key_def *pk_def);

src/box/key_list.c
@@ -63,20 +63,17 @@ key_list_iterator_create(struct key_list_iterator *it, struct tuple *tuple,
 	port_destroy(&in_port);
 	if (rc != 0) {
 		/* Can't evaluate function. */
-		struct space *space = space_by_id(index_def->space_id);
 		diag_add(ClientError, ER_FUNC_INDEX_FUNC, index_def->name,
-			 space ? space_name(space) : "",
-			 "can't evaluate function");
+			 index_def->space_name, "can't evaluate function");
 		return -1;
 	}
 	uint32_t key_data_sz;
 	const char *key_data = port_get_msgpack(&out_port, &key_data_sz);
 	port_destroy(&out_port);
 	if (key_data == NULL) {
-		struct space *space = space_by_id(index_def->space_id);
 		/* Can't get a result returned by function . */
-		diag_add(ClientError, ER_FUNC_INDEX_FUNC, index_def->name,
-			 space ? space_name(space) : "",
+		diag_add(ClientError, ER_FUNC_INDEX_FUNC,
+			 index_def->name, index_def->space_name,
 			 "can't get a value returned by function");
 		return -1;
 	}
@@ -84,29 +81,26 @@ key_list_iterator_create(struct key_list_iterator *it, struct tuple *tuple,
 	it->data_end = key_data + key_data_sz;
 	assert(mp_typeof(*key_data) == MP_ARRAY);
 	if (mp_decode_array(&key_data) != 1) {
-		struct space *space = space_by_id(index_def->space_id);
 		/*
 		 * Function return doesn't follow the
 		 * convention: to many values were returned.
 		 * i.e. return 1, 2
 		 */
 		region_truncate(region, region_svp);
-		diag_set(ClientError, ER_FUNC_INDEX_FORMAT, index_def->name,
-			 space ? space_name(space) : "",
+		diag_set(ClientError, ER_FUNC_INDEX_FORMAT,
+			 index_def->name, index_def->space_name,
 			 "to many values were returned");
 		return -1;
 	}
 	if (func->def->opts.is_multikey) {
 		if (mp_typeof(*key_data) != MP_ARRAY) {
-			struct space * space = space_by_id(index_def->space_id);
 			/*
 			 * Multikey function must return an array
 			 * of keys.
 			 */
 			region_truncate(region, region_svp);
 			diag_set(ClientError, ER_FUNC_INDEX_FORMAT,
-				 index_def->name,
-				 space ? space_name(space) : "",
+				 index_def->name, index_def->space_name,
 				 "a multikey function mustn't return a scalar");
 			return -1;
 		}
@@ -138,13 +132,12 @@ key_list_iterator_next(struct key_list_iterator *it, struct tuple **value)
 	}
 
 	if (mp_typeof(*key) != MP_ARRAY) {
-		struct space *space = space_by_id(it->index_def->space_id);
 		/*
 		 * A value returned by func_index function is
 		 * not a valid key, i.e. {1}.
 		 */
-		diag_set(ClientError, ER_FUNC_INDEX_FORMAT, it->index_def->name,
-			 space ? space_name(space) : "",
+		diag_set(ClientError, ER_FUNC_INDEX_FORMAT,
+			 it->index_def->name, it->index_def->space_name,
 			 tt_sprintf("supplied key type is invalid: expected %s",
 				    field_type_strs[FIELD_TYPE_ARRAY]));
 		return -1;
@@ -153,26 +146,24 @@ key_list_iterator_next(struct key_list_iterator *it, struct tuple **value)
 	const char *rptr = key;
 	uint32_t part_count = mp_decode_array(&rptr);
 	if (part_count != key_def->part_count) {
-		struct space *space = space_by_id(it->index_def->space_id);
 		/*
 		 * The key must have exact functional index
 		 * definition's part_count(s).
 		 */
-		diag_set(ClientError, ER_FUNC_INDEX_FORMAT, it->index_def->name,
-			 space ? space_name(space) : "",
+		diag_set(ClientError, ER_FUNC_INDEX_FORMAT,
+			 it->index_def->name, it->index_def->space_name,
 			 tt_sprintf(tnt_errcode_desc(ER_EXACT_MATCH),
 				   key_def->part_count, part_count));
 		return -1;
 	}
 	if (key_validate_parts(key_def, rptr, part_count, true,
 			       &key_end) != 0) {
-		struct space *space = space_by_id(it->index_def->space_id);
 		/*
 		 * The key doesn't follow functional index key
 		 * definition.
 		 */
-		diag_add(ClientError, ER_FUNC_INDEX_FORMAT, it->index_def->name,
-			 space ? space_name(space) : "",
+		diag_add(ClientError, ER_FUNC_INDEX_FORMAT,
+			 it->index_def->name, it->index_def->space_name,
 			 "key does not follow functional index definition");
 		return -1;
 	}

src/box/schema.cc
@@ -219,6 +219,8 @@ sc_space_new(uint32_t id, const char *name,
 	struct index_def *index_def = index_def_new(id, /* space id */
 						    0 /* index id */,
 						    "primary", /* name */
+						    NULL, /* space name */
+						    NULL, /* engine name */
 						    strlen("primary"),
 						    TREE /* index type */,
 						    &index_opts_default,

src/box/space.c
@@ -576,8 +576,16 @@ space_create(struct space *space, struct engine *engine,
 			 "check_unique_constraint_map");
 		goto fail;
 	}
+
 	rlist_foreach_entry(index_def, key_list, link) {
-		struct index *index = space_create_index(space, index_def);
+		struct index_def *temp_def =
+			index_def_new(index_def->space_id, index_def->iid,
+				      index_def->name, def->name,
+				      def->engine_name, strlen(index_def->name),
+				      index_def->type, &index_def->opts,
+				      index_def->key_def, index_def->pk_def);
+		struct index *index = space_create_index(space, temp_def);
+		index_def_delete(temp_def);
 		if (index == NULL)
 			goto fail_free_indexes;
 		space->index_map[index_def->iid] = index;

src/box/sql.c
@@ -383,8 +383,8 @@ sql_ephemeral_space_new(const struct sql_space_info *info)
 		return NULL;
 
 	const char *name = "ephemer_idx";
-	struct index_def *index_def = index_def_new(0, 0, name, strlen(name),
-						    TREE, &index_opts_default,
+	struct index_def *index_def = index_def_new(0, 0, name, NULL, NULL,
+						    strlen(name), TREE, &index_opts_default,
 						    key_def, NULL);
 	key_def_delete(key_def);
 	if (index_def == NULL)

src/box/sql/build.c
@@ -2654,8 +2654,9 @@ index_fill_def(struct Parse *parse, struct index *index,
 	 * only for comparison routine. Meanwhile on front-end
 	 * side only definition is used.
 	 */
-	index->def = index_def_new(space_def->id, 0, name, name_len, TREE,
-				   &opts, key_def, NULL);
+	index->def = index_def_new(space_def->id, 0, name,
+				   space_def->name, space_def->engine_name,
+				   name_len, TREE, &opts, key_def, NULL);
 	if (index->def == NULL)
 		goto tnt_error;
 	index->def->iid = iid;

src/box/sql/where.c
@@ -895,6 +895,8 @@ constructAutomaticIndex(Parse * pParse,			/* The parsing context */
 	index_opts_create(&opts);
 	const char *idx_name = "ephemeral index";
 	struct index_def *idx_def = index_def_new(space->def->id, 0, idx_name,
+						  space->def->name,
+						  space->def->engine_name,
 						  strlen(idx_name), TREE, &opts,
 						  key_def, NULL);
 	key_def_delete(key_def);
@@ -2133,7 +2135,9 @@ tnt_error:
 
 		struct index_opts opts;
 		index_opts_create(&opts);
-		fake_index = index_def_new(space->def->id, 0,"fake_autoindex",
+		fake_index = index_def_new(space->def->id, 0, "fake_autoindex",
+					   space->def->name,
+					   space->def->engine_name,
 					   sizeof("fake_autoindex") - 1,
 					   TREE, &opts, key_def, NULL);
 		key_def_delete(key_def);

src/box/vinyl.c
@@ -1549,11 +1549,9 @@ vy_check_is_unique_primary(struct vy_tx *tx, const struct vy_read_view **rv,
 	if (vy_get(lsm, tx, rv, stmt, &found))
 		return -1;
 	if (found != NULL) {
-		struct index *index = &lsm->base;
-		struct space *space = space_by_id(index->def->space_id);
-		diag_set(ClientError, ER_TUPLE_FOUND, index->def->name,
-			 space_name(space), tuple_str(found), tuple_str(stmt),
-			 found, stmt);
+		diag_set(ClientError, ER_TUPLE_FOUND, lsm->base.def->name,
+			 lsm->base.def->space_name, tuple_str(found),
+			 tuple_str(stmt), found, stmt);
 		tuple_unref(found);
 		return -1;
 	}
@@ -1599,11 +1597,9 @@ vy_check_is_unique_secondary_one(struct vy_tx *tx, const struct vy_read_view **r
 		return 0;
 	}
 	if (found != NULL) {
-		struct index *index = &lsm->base;
-		struct space *space = space_by_id(index->def->space_id);
-		diag_set(ClientError, ER_TUPLE_FOUND, index->def->name,
-			 space_name(space), tuple_str(found), tuple_str(stmt),
-			 found, stmt);
+		diag_set(ClientError, ER_TUPLE_FOUND, lsm->base.def->name,
+			 lsm->base.def->space_name, tuple_str(found),
+			 tuple_str(stmt), found, stmt);
 		tuple_unref(found);
 		return -1;
 	}
@@ -1806,7 +1802,7 @@ vy_delete(struct vy_env *env, struct vy_tx *tx, struct txn_stmt *stmt,
  * @retval -1 Attempt to modify the primary key.
  */
 static inline int
-vy_check_update(struct space *space, const struct vy_lsm *pk,
+vy_check_update(const struct vy_lsm *pk,
 		struct tuple *old_tuple, struct tuple *new_tuple,
 		uint64_t column_mask)
 {
@@ -1814,8 +1810,8 @@ vy_check_update(struct space *space, const struct vy_lsm *pk,
 	    vy_stmt_compare(old_tuple, HINT_NONE, new_tuple,
 			    HINT_NONE, pk->key_def) != 0) {
 		diag_set(ClientError, ER_CANT_UPDATE_PRIMARY_KEY,
-			 space_name(space), space_id(space), old_tuple,
-			 new_tuple, NULL);
+			 pk->base.def->space_name, pk->base.def->space_id,
+			 old_tuple, new_tuple, NULL);
 		return -1;
 	}
 	return 0;
@@ -1935,7 +1931,7 @@ vy_update(struct vy_env *env, struct vy_tx *tx, struct txn_stmt *stmt,
 	region_truncate(&fiber()->gc, region_svp);
 	if (stmt->new_tuple == NULL)
 		return -1;
-	if (vy_check_update(space, pk, stmt->old_tuple, stmt->new_tuple,
+	if (vy_check_update(pk, stmt->old_tuple, stmt->new_tuple,
 			    column_mask) != 0)
 		return -1;
 
@@ -2194,7 +2190,7 @@ vy_upsert(struct vy_env *env, struct vy_tx *tx, struct txn_stmt *stmt,
 	region_truncate(&fiber()->gc, region_svp);
 	if (stmt->new_tuple == NULL)
 		return -1;
-	if (vy_check_update(space, pk, stmt->old_tuple, stmt->new_tuple,
+	if (vy_check_update(pk, stmt->old_tuple, stmt->new_tuple,
 			    column_mask) != 0) {
 		diag_log();
 		/*

test/unit/vy_point_lookup.c
@@ -94,7 +94,8 @@ test_basic()
 
 	struct index_opts index_opts = index_opts_default;
 	struct index_def *index_def =
-		index_def_new(512, 0, "primary", sizeof("primary") - 1, TREE,
+		index_def_new(512, 0, "primary", NULL, NULL,
+			      sizeof("primary") - 1, TREE,
 			      &index_opts, key_def, NULL);
 
 	struct vy_lsm *pk = vy_lsm_new(&lsm_env, &cache_env, &mem_env,