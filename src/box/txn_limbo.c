aa8706d25036000b2e55fca39f8f6e1a2c2d3327
Georgiy Lebedev <g.lebedev@tarantool.org>
2024-08-10 19:33:36 +0300

    box: add helper to determine whether limbo is owned by current instance
    Needed for #10459
    
    NO_CHANGELOG=<refactoring>
    NO_DOC=<refactoring>
    NO_TEST=<refactoring>

--- src/box/txn_limbo.c
+++ src/box/txn_limbo.c
@@ -86,7 +86,8 @@ bool
 txn_limbo_is_ro(struct txn_limbo *limbo)
 {
 	return limbo->owner_id != REPLICA_ID_NIL &&
-	       (limbo->owner_id != instance_id || txn_limbo_is_frozen(limbo));
+		(!txn_limbo_is_owned_by_current_instance(limbo) ||
+		txn_limbo_is_frozen(limbo));
 }
 
 void
@@ -230,7 +231,7 @@ txn_limbo_assign_remote_lsn(struct txn_limbo *limbo,
 			    struct txn_limbo_entry *entry, int64_t lsn)
 {
 	assert(limbo->owner_id != REPLICA_ID_NIL);
-	assert(limbo->owner_id != instance_id);
+	assert(!txn_limbo_is_owned_by_current_instance(limbo));
 	assert(entry->lsn == -1);
 	assert(lsn > 0);
 	(void) limbo;
@@ -249,7 +250,7 @@ txn_limbo_assign_local_lsn(struct txn_limbo *limbo,
 			   struct txn_limbo_entry *entry, int64_t lsn)
 {
 	assert(limbo->owner_id != REPLICA_ID_NIL);
-	assert(limbo->owner_id == instance_id);
+	assert(txn_limbo_is_owned_by_current_instance(limbo));
 	assert(entry->lsn == -1);
 	assert(lsn > 0);
 
@@ -273,7 +274,7 @@ void
 txn_limbo_assign_lsn(struct txn_limbo *limbo, struct txn_limbo_entry *entry,
 		     int64_t lsn)
 {
-	if (limbo->owner_id == instance_id)
+	if (txn_limbo_is_owned_by_current_instance(limbo))
 		txn_limbo_assign_local_lsn(limbo, entry, lsn);
 	else
 		txn_limbo_assign_remote_lsn(limbo, entry, lsn);
