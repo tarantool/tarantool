1c2b0c3dfe33c6a8e90a44e04e7951ba43b2210f
Maksim Tiushev <mandesero@gmail.com>
2024-08-14 14:14:54 +0000

    config: fix replication.sync_timeout default value
    This patch fixes a bug where the `box.replication_sync_timeout` field
    was being assigned a value that did not match the schema scenario.
    The problem was caused by the `schema` object being initialized before
    the fields were assigned values from the configuration, causing the
    field value to be calculated incorrectly.
    
    Closes #10280
    
    NO_DOC=bugfix
    
    (cherry picked from commit d33b14793bea58520139172599621daaaf66b947)

---
+++ test/config-luatest/gh_10280_replication_sync_timeout_default_value_test.lua
@@ -0,0 +1,57 @@
+local t = require('luatest')
+local cbuilder = require('test.config-luatest.cbuilder')
+local cluster = require('test.config-luatest.cluster')
+
+local g = t.group()
+
+g.before_all(cluster.init)
+g.after_each(cluster.drop)
+g.after_all(cluster.clean)
+
+g.test_sync_timeout_value_new = function(g)
+    local config = cbuilder.new()
+        :set_global_option('compat.box_cfg_replication_sync_timeout', 'new')
+        :add_instance('i-001', {})
+        :config()
+    local cluster = cluster.new(g, config)
+    cluster:start()
+
+    cluster['i-001']:exec(function()
+        t.assert_equals(box.cfg.replication_sync_timeout, 0)
+    end)
+end
+
+g.test_sync_timeout_value_old = function(g)
+    local config = cbuilder.new()
+        :set_global_option('compat.box_cfg_replication_sync_timeout', 'old')
+        :add_instance('i-001', {})
+        :config()
+    local cluster = cluster.new(g, config)
+    cluster:start()
+
+    cluster['i-001']:exec(function()
+        t.assert_equals(box.cfg.replication_sync_timeout, 300)
+    end)
+end
+
+g.test_sync_timeout_value_reload = function(g)
+    local config = cbuilder.new()
+        :set_global_option('replication.sync_timeout', 5)
+        :add_instance('i-001', {})
+        :config()
+    local cluster = cluster.new(g, config)
+    cluster:start()
+
+    cluster['i-001']:exec(function()
+        t.assert_equals(box.cfg.replication_sync_timeout, 5)
+    end)
+
+    local new_config = cbuilder.new()
+        :add_instance('i-001', {})
+        :config()
+    cluster:reload(new_config)
+
+    cluster['i-001']:exec(function()
+        t.assert_equals(box.cfg.replication_sync_timeout, 0)
+    end)
+end
