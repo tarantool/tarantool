9ed5084d58d6dc70cbed5b5c586898bbef5d77cf
Yaroslav Lobankov <y.lobankov@tarantool.org>
2024-08-26 21:17:45 +0400

    test: use `cbuilder` module from luatest
    Also, adapt tests in accordance with the module interface.
    
    NO_DOC=test
    NO_TEST=test
    NO_CHANGELOG=test

--- test/config-luatest/gh_10280_replication_sync_timeout_default_value_test.lua
+++ test/config-luatest/gh_10280_replication_sync_timeout_default_value_test.lua
@@ -1,5 +1,5 @@
 local t = require('luatest')
-local cbuilder = require('test.config-luatest.cbuilder')
+local cbuilder = require('luatest.cbuilder')
 local cluster = require('test.config-luatest.cluster')
 
 local g = t.group()
@@ -9,7 +9,7 @@ g.after_each(cluster.drop)
 g.after_all(cluster.clean)
 
 g.test_sync_timeout_value_new = function(g)
-    local config = cbuilder.new()
+    local config = cbuilder:new()
         :set_global_option('compat.box_cfg_replication_sync_timeout', 'new')
         :add_instance('i-001', {})
         :config()
@@ -22,7 +22,7 @@ g.test_sync_timeout_value_new = function(g)
 end
 
 g.test_sync_timeout_value_old = function(g)
-    local config = cbuilder.new()
+    local config = cbuilder:new()
         :set_global_option('compat.box_cfg_replication_sync_timeout', 'old')
         :add_instance('i-001', {})
         :config()
@@ -35,7 +35,7 @@ g.test_sync_timeout_value_old = function(g)
 end
 
 g.test_sync_timeout_value_reload = function(g)
-    local config = cbuilder.new()
+    local config = cbuilder:new()
         :set_global_option('replication.sync_timeout', 5)
         :add_instance('i-001', {})
         :config()
@@ -46,7 +46,7 @@ g.test_sync_timeout_value_reload = function(g)
         t.assert_equals(box.cfg.replication_sync_timeout, 5)
     end)
 
-    local new_config = cbuilder.new()
+    local new_config = cbuilder:new()
         :add_instance('i-001', {})
         :config()
     cluster:reload(new_config)
