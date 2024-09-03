2190327c158985e765cf650405ccd4c472938704
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2024-09-03 21:57:03 +0200

    Bump msgpuck submodule
    This update pulls the following commits:
    
    * Fix mp_compare_uint()
    * Fix mp_read_int/double on suboptimal msgpack
    
    Part of #9965
    
    NO_DOC=bugfix
    NO_CHANGELOG=later

---
+++ test/box-luatest/gh_9965_non_compact_mp.c
@@ -0,0 +1,24 @@
+#include "module.h"
+#include "msgpuck.h"
+
+int
+index_get(box_function_ctx_t *ctx, const char *args, const char *args_end)
+{
+	uint32_t arg_count = mp_decode_array(&args);
+	assert(arg_count == 3);
+	(void)arg_count;
+	uint64_t u64 = mp_decode_uint(&args);
+	assert(u64 <= UINT32_MAX);
+	uint32_t space_id = (uint32_t)u64;
+	u64 = mp_decode_uint(&args);
+	assert(u64 <= UINT32_MAX);
+	uint32_t index_id = (uint32_t)u64;
+	box_tuple_t *tuple = NULL;
+	int rc = box_index_get(space_id, index_id, args, args_end, &tuple);
+	if (rc < 0)
+		return -1;
+	if (tuple == NULL)
+		return 0;
+	box_return_tuple(ctx, tuple);
+	return 0;
+}
