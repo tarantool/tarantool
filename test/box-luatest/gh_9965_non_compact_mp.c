2dd6150fe6a143d1a4024e49a90cbc6aceeeba83
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2024-09-11 20:05:16 +0200

    vinyl: fix suboptimal msgpack assert in upsert
    Upsert operation normalizes its operations - subtracts the index
    base from the fieldnos. If the fieldno is negative, it is simply
    encoded back. If positive - the subtraction happens and then
    encoding back.
    
    The problem was that the code assumed MP_INT is always negative,
    and would blindly encode it without changes. The assumption wasn't
    right - MP_INT can be positive. Need to handle that.
    
    Part of #9965
    
    NO_DOC=bugfix
    NO_CHANGELOG=later

--- test/box-luatest/gh_9965_non_compact_mp.c
+++ test/box-luatest/gh_9965_non_compact_mp.c
@@ -22,3 +22,32 @@ index_get(box_function_ctx_t *ctx, const char *args, const char *args_end)
 	box_return_tuple(ctx, tuple);
 	return 0;
 }
+
+int
+index_upsert(box_function_ctx_t *ctx, const char *args, const char *args_end)
+{
+	uint32_t arg_count = mp_decode_array(&args);
+	assert(arg_count == 4);
+	(void)arg_count;
+	uint64_t u64 = mp_decode_uint(&args);
+	assert(u64 <= UINT32_MAX);
+	uint32_t space_id = (uint32_t)u64;
+	u64 = mp_decode_uint(&args);
+	assert(u64 <= UINT32_MAX);
+	uint32_t index_id = (uint32_t)u64;
+	assert(mp_typeof(*args) == MP_ARRAY);
+	const char *tuple_data = args;
+	mp_next(&args);
+	const char *tuple_data_end = args;
+	assert(mp_typeof(*args) == MP_ARRAY);
+	box_tuple_t *tuple = NULL;
+	int index_base = 1;
+	int rc = box_upsert(space_id, index_id, tuple_data, tuple_data_end,
+			    args, args_end, index_base, &tuple);
+	if (rc < 0)
+		return -1;
+	if (tuple == NULL)
+		return 0;
+	box_return_tuple(ctx, tuple);
+	return 0;
+}
