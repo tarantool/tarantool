b697464758bef61e1e34be772215ff043fe7b128
Sergey Bronnikov <sergeyb@tarantool.org>
2024-09-12 20:18:15 +0300

    cmake: support TAP tests by CTest
    CTest support has been added in commit a9b9f2eb4f71
    ("test: execute tests using CTest"), however, `test-run.py` was
    used for running TAP tests. The patch adds support of running TAP
    tests (suites app-tap, box-tap, engine-tap and sql-tap) by CTest
    directly, without using test-run.py.
    
    Note, that the following TAP tests were rewritten because these
    tests were used .result files for execution, see [1]:
    
    - box-tap/trigger_yield.test.lua -> box-luatest/trigger_yield_test.lua
    - app-tap/tap.test.lua -> app-luatest/tap_test.lua
    - app-tap/logger_pipe.test.lua -> app-luatest/logger_pipe_test.lua
    - app-tap/init_script.test.lua -> app-luatest/init_script_test.lua
    - app-tap/pcall.test.lua -> app-luatest/pcall_test.lua
    - app-tap/debug.test.lua -> app-luatest/debug_test.lua
    - box-tap/trigger_atexit.test.lua -> box-luatest/trigger_atexit_test.lua
    - box-tap/gh-5602-environment-vars-cfg.test.lua ->
        box-luatest/gh-5602-environment-vars-cfg_test.lua
    
    And the following TAP tests were rewritten because these
    tests required inspector functionality builtin into test-run.py,
    see [2]:
    
    - box-tap/session.storage.test.lua -> box-luatest/session_storage_test.lua
    - box-tap/net.box.test.lua -> box-luatest/net_box_test.lua
    - box-tap/session.test.lua -> test/box-luatest/session_test.lua
    
    Note, that TAP test suites use environment variable
    `MEMTX_ALLOCATOR`. Support from the test executor is not required
    for using this parameter in tests.
    
    Note, that `tarantool` was not added to "*-deps", because
    running target `box_generate_lua_sources`, that builds Lua source
    files in parallel corrupts the generated Lua files.
    
    Follows up #10216
    
    1. https://github.com/tarantool/tarantool/pull/10725
    2. https://github.com/tarantool/tarantool/pull/11021
    
    NO_CHANGELOG=testing
    NO_DOC=testing
    NO_TEST=testing

--- test/engine-tap/lib/tester.lua
+++ test/engine-tap/lib/tester.lua
@@ -10,7 +10,12 @@ local test = tap.test("errno")
 
 local ok, test_run = pcall(require, 'test_run')
 test_run = ok and test_run.new() or nil
-local engine = test_run ~= nil and test_run:get_cfg('engine') or 'memtx'
+-- Default value `memtx` was added to have ability to run a
+-- `engine-tap` test w/o `test-run.py`, see commit
+-- commit 3bd870261c462416c29226414fe0a2d79aba0c74
+-- ('box, datetime: datetime comparison for indices').
+local engine = test_run and test_run:get_cfg('engine') or
+               (os.getenv('TEST_ENGINE') or 'memtx')
 
 function test.engine(self)
     return engine
