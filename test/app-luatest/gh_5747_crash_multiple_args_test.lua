b05a35e5e99923cf0c1f773a0323931f434395c9
Sergey Bronnikov <sergeyb@tarantool.org>
2024-09-18 17:52:54 +0300

    test: use built tarantool instead a system one
    The test gh_5747_crash_multiple_args_test.lua uses a `tarantool`
    binary that is available in a current PATH environment variable.
    In some cases, PATH may point to a system `tarantool` executable
    and this could be unexpected for those who run the test. The patch
    set a path to `tarantool` executable derived from a command-line
    used to run the test.
    
    NO_CHANGELOG=codehealth
    NO_DOC=codehealth
    
    (cherry picked from commit 1e907ddbd8db2587418d50dde3e06e985c47bded)

--- test/app-luatest/gh_5747_crash_multiple_args_test.lua
+++ test/app-luatest/gh_5747_crash_multiple_args_test.lua
@@ -3,7 +3,12 @@ local t = require('luatest')
 local g = t.group()
 
 g.test_multiple_args = function()
-    local handle = io.popen('tarantool -ea=10 -e b=20 -ec=30 -e d=40 -e"print(a+b+c+d)"')
+    local TARANTOOL_PATH = arg[-1]
+    local cmd = string.format(
+        '%s -ea=10 -e b=20 -ec=30 -e d=40 -e"print(a+b+c+d)"',
+        TARANTOOL_PATH
+    )
+    local handle = io.popen(cmd)
     local result = handle:read()
     t.assert_equals(result, '100')
 end
