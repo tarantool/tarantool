7af42624ae42528741ccc269d2382bf54323e5e1
Georgiy Lebedev <g.lebedev@tarantool.org>
2024-09-28 16:49:10 +0200

    txn: ack limbo owner WAL writes to the limbo asynchronously
    Currently, a synchronous transaction must always wait for its WAL write to
    finish, acknowledge itself to the synchronous queue, and potentially wait
    for a synchronous CONFIRM request WAL write.
    
    In the scope of #10583, we want to support asynchronous wait modes for
    synchronous transactions. To enable the none mode, we need to acknowledge
    our own WAL writes for the synchronous queue asynchronously by:
    1. Moving the acknowledgment to the callback for finishing transaction WAL
       write;
    2. Detaching the CONFIRM request WAL writing from the acknowledgment, and
       making it asynchronous.
    
    To enable the latter, we introduce a worker fiber for the limbo, which is
    responsible for closing the gap between the persisted confirmed LSN and the
    volatile confirmed LSN by trying to write a CONFIRM request to the WAL, and
    retrying it on failure.
    
    To be able to test the limbo worker behaviour in the event of a concurrent
    PROMOTE/DEMOTE request processing, we add a new
    `ERRINJ_TXN_LIMBO_WORKER_DELAY` error injection.
    
    The patch is cherry-picked to 2.11 in order to bring the limbo's
    state as close to the master (3.6) as possible in order to
    fix #11790.
    
    Closes #10628
    In scope of #11790
    
    NO_CHANGELOG=<no behaviour change>
    NO_DOC=<no behaviour change>
    
    (cherry picked from commit 92774dc7661b0377b3fefb9b9b9db40a27fc0103)

---
+++ test/replication-luatest/gh_10628_ack_limbo_owner_wal_writes_to_limbo_async_test.lua
@@ -0,0 +1,83 @@
+local replica_set = require('luatest.replica_set')
+local server = require('luatest.server')
+local t = require('luatest')
+
+local g = t.group()
+
+g.before_each(function(cg)
+    cg.replica_set = replica_set:new{}
+    cg.box_cfg = {
+        replication = {
+            server.build_listen_uri('master', cg.replica_set.id),
+            server.build_listen_uri('replica', cg.replica_set.id),
+        },
+        replication_timeout = 0.1,
+        replication_synchro_timeout = 120,
+    }
+    cg.master = cg.replica_set:build_and_add_server{
+        alias = 'master',
+        box_cfg = cg.box_cfg,
+    }
+    cg.replica = cg.replica_set:build_and_add_server{
+        alias = 'replica',
+        box_cfg = cg.box_cfg,
+    }
+    cg.replica_set:start()
+    cg.replica_set:wait_for_fullmesh()
+    cg.master:exec(function()
+        rawset(_G, 'fiber', require('fiber'))
+
+        box.schema.space.create('s', {is_sync = true}):create_index('p')
+        box.ctl.promote()
+    end)
+    cg.master:wait_for_downstream_to(cg.replica)
+end)
+
+g.after_each(function(cg)
+    cg.replica_set:drop()
+end)
+
+-- Test that the CONFIRM request WAL write is not done if the limbo is
+-- `in_rollback` state.
+g.test_limbo_in_rollback_confirm_not_written = function(cg)
+    t.tarantool.skip_if_not_debug()
+
+    local fid = cg.master:exec(function()
+        -- Delay the limbo worker wake up.
+        box.error.injection.set('ERRINJ_TXN_LIMBO_WORKER_DELAY', true)
+        -- Make the PROMOTE request WAL write hang.
+        box.error.injection.set('ERRINJ_WAL_DELAY_COUNTDOWN', 1)
+        local f = _G.fiber.create(function() box.space.s:replace{0} end)
+        f:set_joinable(true)
+        t.helpers.retrying({timeout = 120}, function()
+            t.assert_equals(box.info.synchro.queue.len, 1)
+        end)
+        return f:id()
+    end)
+
+    cg.master:wait_for_downstream_to(cg.replica)
+
+    cg.replica:exec(function()
+        t.helpers.retrying({timeout = 120}, function()
+            t.assert_equals(box.info.synchro.queue.len, 1)
+        end)
+        box.cfg{replication_synchro_timeout = 0.01}
+        box.ctl.promote()
+    end)
+
+    cg.master:exec(function(fid)
+        t.helpers.retrying({timeout = 120}, function()
+            t.assert(box.error.injection.get('ERRINJ_WAL_DELAY'))
+        end)
+        t.assert(box.info.synchro.queue.busy)
+
+        local write_count = box.error.injection.get('ERRINJ_WAL_WRITE_COUNT')
+        box.error.injection.set('ERRINJ_TXN_LIMBO_WORKER_DELAY', false)
+        _G.fiber.sleep(0.1)
+        t.assert_equals(box.info.synchro.queue.len, 1)
+        box.error.injection.set('ERRINJ_WAL_DELAY', false)
+        t.assert(_G.fiber.find(fid):join())
+        -- Check that CONFIRM request was not written to the WAL.
+        t.assert(box.error.injection.get('ERRINJ_WAL_WRITE_COUNT'), write_count)
+    end, {fid})
+end
