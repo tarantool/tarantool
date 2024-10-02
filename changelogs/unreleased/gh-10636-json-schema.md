7918f38c15b9e8af78c7133bed70b41d563e6c9d
Maksim Tiushev <mandesero@gmail.com>
2024-10-02 08:45:34 +0000

    config: add method to obtain config's JSON Schema
    This patch adds a method `:jsonschema()` to `config` module.
    This method generates JSON schema of cluster config schema.
    
    Part of #10636
    
    @TarantoolBot document
    Title: ":jsonschema()" method to obtain config's JSON Schema
    Root document: https://www.tarantool.io/ru/doc/latest/reference/reference_lua/config/
    
    API Reference:
    config:jsonschema() -- Generates the JSON schema of the cluster
    configuration as a Lua table.
    
    config API/object config:
    
    `config:jsonschema()`
    JSON schema of the cluster configuration in the form
    of a Lua table.
    
    Return:
    A Lua table representing the JSON schema of the cluster
    configuration.
    
    Example:
    
    ```lua
    local config = require('config')
    local json = require('json')
    local fio = require('fio')
    
    -- Generate the JSON schema of the cluster configuration as a Lua table
    local schema = config:jsonschema()
    
    -- Encode the schema as a JSON string
    local schema_json = json.encode(schema)
    
    -- Save the schema to a file named 'config-schema.json'
    local file_path = fio.pathjoin(fio.cwd(), 'config-schema.json')
    local file = fio.open(file_path, {'O_WRONLY', 'O_CREAT'}, tonumber('644', 8))
    
    if file then
        file:write(schema_json)
        file:close()
    End
    ```
    
    - CLI Validation with `check-jsonschema`
    Once the `config-schema.json` file is generated, users can validate their
    configuration files using CLI tools such as `check-jsonschema`:
    
    ```bash
    check-jsonschema --schemafile=config-schema.json my-config-file.yaml
    ```
    
    This ensures that the configuration file is valid according to the schema.
    
    - IDE Integration
    Users can also import the generated `config-schema.json` into their IDE to
    enable autocompletion and validation. For instance, in VSCode, using the
    RedHat YAML extension:
    
    1. Install the [RedHat YAML extension](https://marketplace.visualstudio.com/items?itemName=redhat.vscode-yaml).
    2. Configure the extension to use the generated `config-schema.json` for
    your YAML configuration files by adding the following to your VSCode
    `settings.json`:
    
    ```json
    "yaml.schemas": {
        "<path to config-schema.json>": "*.yaml"
    }
    ```
    
    This will provide autocompletion and validation when editing configuration
    files, ensuring they comply with the schema.

--- changelogs/unreleased/gh-10636-json-schema.md
+++ changelogs/unreleased/gh-10636-json-schema.md
@@ -3,3 +3,9 @@
  * Added `<schema object>:jsonschema()` method to
    `experimental.config.utils.schema` for generating a JSON schema
    representation as a Lua table (gh-10636).
+
+## feature/config
+
+* Added `:jsonschema()` method to the `config` module. This method generates
+  and returns the JSON schema of the cluster configuration, providing a detailed
+  description of each field (gh-10636).
--- test/config-luatest/jsonschema_schema_test.lua
+++ test/config-luatest/jsonschema_schema_test.lua
@@ -2,8 +2,23 @@ local t = require('luatest')
 
 local schema = require('experimental.config.utils.schema')
 local cluster_config = require('internal.config.cluster_config')
+local config = require('config')
 
 local g = t.group()
+
+local function escape_descriptions(tbl)
+    if type(tbl) ~= 'table' then
+        return
+    end
+
+    for key, value in pairs(tbl) do
+        if key == 'description' then
+            tbl[key] = nil
+        elseif type(value) == 'table' then
+            escape_descriptions(value)
+        end
+    end
+end
 
 g.test_json_schema = function()
     local s = schema.new('basic', schema.record({
@@ -90,4 +105,61 @@ g.test_invalid_json_schema = function()
     local msg_2 = '[invalid_schema_map] foo.<*>: JSON does not support ' ..
     'non-string keys.'
     t.assert_error_msg_equals(msg_2, function() s_2:jsonschema()  end)
+end
+
+g.test_json_schema_from_config = function()
+    local s = config:jsonschema()
+    local wal_section = s['properties']['wal']
+
+
+    t.assert_equals(wal_section.description,
+                    'This section defines configuration parameters related ' ..
+                    'to write-ahead log.')
+
+    t.assert_equals(wal_section['properties']['dir_rescan_delay'].description,
+                    'The time interval in seconds between periodic scans of ' ..
+                    'the write-ahead-log file directory, when checking for ' ..
+                    'changes to write-ahead-log files for the sake of ' ..
+                    'replication or hot standby.')
+
+    escape_descriptions(wal_section)
+
+    local expected = {
+        additionalProperties = false,
+        properties = {
+            cleanup_delay = {type = 'number'},
+            dir = {default = 'var/lib/{{ instance_name }}', type = 'string'},
+            dir_rescan_delay = {default = 2, type = 'number'},
+            ext = {
+                additionalProperties = false,
+                default = box.NULL,
+                properties = {
+                    new = {type = 'boolean'},
+                    old = {type = 'boolean'},
+                    spaces = {
+                        additionalProperties = {
+                            additionalProperties = false,
+                            properties = {
+                                new = {default = false, type = 'boolean'},
+                                old = {default = false, type = 'boolean'},
+                            },
+                            type = 'object',
+                        },
+                        type = 'object',
+                    },
+                },
+                type = 'object',
+            },
+            max_size = {default = 268435456, type = 'integer'},
+            mode = {
+                default = 'write',
+                enum = {'none', 'write', 'fsync'},
+                type = 'string'
+            },
+            queue_max_size = {default = 16777216, type = 'integer'},
+            retention_period = {default = 0, type = 'number'},
+        },
+        type = 'object',
+    }
+    t.assert_equals(wal_section, expected)
 end
