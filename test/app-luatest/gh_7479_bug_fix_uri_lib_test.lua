f0680fd1b4cbb86024cbedef6042819dfe0e67aa
Sergey Bronnikov <sergeyb@tarantool.org>
2024-10-02 17:33:04 +0300

    test: remove testcase for tarantoolctl
    The testcase `test_tarantoolctl_connect` tests `tarantoolctl`,
    but this tool has been deprecated and removed for a long time.
    The patch removes testcases as useless.
    
    Follows up commit 216b6243bba1
    ("tools: remove tarantoolctl utility").
    
    NO_CHANGELOG=codehealth
    NO_DOC=codehealth

--- test/app-luatest/gh_7479_bug_fix_uri_lib_test.lua
+++ test/app-luatest/gh_7479_bug_fix_uri_lib_test.lua
@@ -1,6 +1,4 @@
 local t = require('luatest')
-local fio = require('fio')
-local popen = require('popen')
 local net_box = require('net.box')
 local server = require('luatest.server')
 
@@ -20,44 +18,6 @@ end)
 g.after_all(function()
     g.server:stop()
 end)
-
--- Checks whether it is possible to specify only port for tarantoolctl connect.
-g.test_tarantoolctl_connect = function()
-    -- Find tarantoolctl.
-    -- Probably test-run knows the path to tarantoolctl.
-    local TARANTOOLCTL_PATH = os.getenv('TARANTOOLCTL')
-    if TARANTOOLCTL_PATH == nil then
-        -- If test-run dose not know the path to tarantoolctl.
-        -- Assume that current directory is tarantool's build directory.
-        local BUILDDIR = os.getenv('BUILDDIR')
-        TARANTOOLCTL_PATH = ('%s/extra/dist/tarantoolctl'):format(BUILDDIR)
-        if not fio.path.exists(TARANTOOLCTL_PATH) then
-            error("Can't find tarantoolctl")
-        end
-    end
-
-    local cmd_tarantoolctl = {
-        TARANTOOLCTL_PATH,
-        'connect',
-        -- Indicate only port, without localhost:* before the port value.
-        tostring(g.server_port),
-    }
-
-    -- Connection will be established in a separate process.
-    local ph_tarantoolctl = popen.new(cmd_tarantoolctl, {
-        stdin  = 'devnull',
-        stdout = 'devnull',
-        stderr = popen.opts.PIPE,
-    })
-    -- Stderr should contain a message about successful connection.
-    local result = ph_tarantoolctl:read({stderr = true}):rstrip()
-    ph_tarantoolctl:close()
-
-    t.assert_equals(
-        result,
-        'connected to localhost:' .. tostring(g.server_port)
-    )
-end
 
 -- Checks whether it is possible to specify only port for net.box.connect.
 g.test_net_box_connect_with_only_port_indication = function()
