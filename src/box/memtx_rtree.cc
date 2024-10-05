26ae1dc050c185fd2f5fddaa25eff957656ca20f
Magomed Kostoev <m.kostoev@tarantool.org>
2024-10-05 22:19:11 +0300

    memtx: record getting the index size in the MVCC
    Currently getting the index size (aka calling the index.len() method)
    is not recorded in the MVCC. This has a side effect that if the size
    if an index is changed after the transaction first inspected it, it's
    not conflicted and may continue performing writing operations on the
    space. That's a problem since in this case the transaction can perform
    a set of operations assuming that at the time it commits the size of
    the index is the same as been checked, which may not be true.
    
    Let's fix this by informing the MVCC system about the index size check
    performed by a transaction.
    
    Closes #10149
    
    NO_DOC=bugfix
    
    (cherry picked from commit 04a54ab6439821fbedbdab8b53dfc0913687e983)

--- src/box/memtx_rtree.cc
+++ src/box/memtx_rtree.cc
@@ -209,7 +209,7 @@ memtx_rtree_index_size(struct index *base)
 	struct space *space = space_by_id(base->def->space_id);
 	/* Substract invisible count. */
 	return rtree_number_of_records(&index->tree) -
-	       memtx_tx_index_invisible_count(in_txn(), space, base);
+	       memtx_tx_track_count(in_txn(), space, base, ITER_GE, NULL, 0);
 }
 
 static ssize_t
