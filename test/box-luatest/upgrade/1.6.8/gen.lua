34ba936776acd1434f0614f9ec1d4636ed2cefd5
Nikita Zheleztsov <n.zheleztsov@proton.me>
2024-10-09 18:33:42 +0300

    upgrade: fix upgrading from schema 1.6.8
    This commit fixes upgrading schema from 1.6.8:
    
    1. Fix updating empty password for users. In 1.6 credentials were array
       in _user, in 1.7.5 they became map.
    
    2. Start to automatically change format of user spaces: previously it
       was {'a', type = 'number'}, now it's {name = 'a', type = 'number'}.
       Make sure, that space format is encoded as array.
    
    This commit also introduces the test, which checks, that we can
    properly upgrade from 1.6.8 to the latest versions, at least in basic
    cases.
    
    There're still a lot of side cases, which are not covered by the
    upgrade triggers: e.g. user can create space with format {1, 2, 3} on
    schema version 1.6, but we don't have enough info to figure out, which
    format should it be and we'll fail to start the instance. User has
    to have properly configured schema on 1.6.8 in order to upgrade.
    
    Closes #10180
    
    NO_DOC=bugfix

---
+++ test/box-luatest/upgrade/1.6.8/gen.lua
@@ -0,0 +1,61 @@
+--
+-- This script creates a snapshot on Tarantool 1.6.9-94-ga5da60bdd.
+-- Such snapshot should have all possible rows in system spaces.
+-- Available system spaces on 1.6: _cluster, _func, _index, _priv, _schema,
+-- _space, _user.
+--
+
+box.cfg{}
+
+-- Create all possible spaces.
+local f = {
+    {'a', type = 'number'},
+    {'b', type = 'num'},
+    {'c', type = 'array'},
+    {'d', type = 'array'},
+}
+box.schema.space.create('fc', {field_count = 4})
+box.schema.space.create('user', {user = 'guest'})
+box.schema.space.create('format', {format = f})
+box.schema.space.create('fc_user', {field_count = 4, user = 'guest'})
+box.schema.space.create('fc_format', {field_count = 4, format = f})
+box.schema.space.create('user_format', {user = 'guest', format = f})
+box.schema.space.create('all', {field_count = 4, user = 'guest', format = f})
+
+-- Create all possible indexes.
+box.space['all']:create_index('pk')
+box.space['all']:create_index('hash', {type = 'hash', parts = {2, 'num'}})
+box.space['all']:create_index('rtree', {type = 'rtree', parts = {3, 'array'}})
+
+box.space['format']:create_index('pk')
+box.space['format']:create_index('bs', {type = 'bitset', parts = {2, 'num'}})
+box.space['format']:create_index('rtree_1',
+    {type = 'rtree', parts = {3, 'array'}, dimension = 4})
+box.space['format']:create_index('rtree_2',
+    {type = 'rtree', parts = {4, 'array'}, distance = 'manhattan'})
+
+-- Create function.
+box.schema.func.create('calculate', {language = 'LUA'})
+
+-- Create roles and users.
+box.schema.role.create('test_role')
+box.schema.role.grant('test_role', 'read,write', 'space', 'fc')
+box.schema.user.create('user1')
+box.schema.user.create('user2', {password = 'passwd'})
+box.schema.user.grant('user1', 'execute', 'function', 'calculate')
+box.schema.user.grant('user2', 'test_role')
+
+-- DDL is disabled until schema is upgraded (see gh-7149) so we have to grant
+-- permissions required to run the test manually.
+
+-- `super` role still doesn't exist here, so let's create it. Since
+-- `role.grant` doesn't allow to specify id, we manually insert into _user.
+box.space._user:insert{31, 1, "super", "role"}
+-- Since box.priv.ALL is different on the latest version rather than on 1.6.8,
+-- manually insert 'super' role in the _priv and grant it all privileges to
+-- the 'universe'.
+box.space._priv:insert{1, 31, 'universe', 0, 4294967295}
+box.schema.user.grant('guest', 'super')
+
+box.snapshot()
+os.exit(0)
