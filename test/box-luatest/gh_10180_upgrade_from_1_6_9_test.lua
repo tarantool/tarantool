2787399f6d7b4e600764528ed46e66f7b8031052
Nikita Zheleztsov <n.zheleztsov@proton.me>
2024-11-01 17:04:26 +0300

    upgrade: fix upgrading from schema 1.6.9
    This commit fixes some cases of upgrading schema from 1.6.9:
    
    1. Fix updating empty password for users. In 1.6 credentials were array
       in _user, in 1.7.5 they became map.
    
    2. Automatically update the format of user spaces. Format of system
       spaces have been properly fixed during upgrade to 1.7.5. However,
       commit 519bc82e4fb4 ("Parse and validate space formats") introduced
       strict checking of format field in 1.7.6. So, the format of user
       spaces should be also fixed.
    
    Back in 1.6 days, it was allowed to write anything in space format.
    This commit only fixes valid uses of format:
        {name = 'a', type = 'number'}
        {'a', type = 'number'}
        {'a', 'num'}
        {'a'}
    
    Invalid use of format (e.g. {{}}, or {{5, 'number'}} will cause error
    anyway. User has to fix the format on old version and only after that
    start a new one.
    
    This commit also introduces the test, which checks, that we can
    properly upgrade from 1.6.9 to the latest versions, at least in basic
    cases.
    
    Closes #10180
    
    NO_DOC=bugfix

---
+++ test/box-luatest/gh_10180_upgrade_from_1_6_9_test.lua
@@ -0,0 +1,29 @@
+local server = require('luatest.server')
+local t = require('luatest')
+
+local g = t.group()
+
+g.before_all(function(cg)
+    cg.server = server:new({
+        datadir = 'test/box-luatest/upgrade/1.6.9',
+    })
+    cg.server:start()
+end)
+
+g.after_all(function(cg)
+    cg.server:drop()
+end)
+
+g.test_upgrade = function(cg)
+    cg.server:exec(function()
+        t.assert_equals(box.space._schema:get{'version'}, {'version', 1, 6, 8})
+        t.assert(pcall(box.schema.upgrade))
+        local version = require('internal.mkversion').get()
+        t.assert_equals(box.space._schema:get{'version'},
+            {'version', version.major, version.minor, version.patch})
+    end)
+    -- Restart the server and check that we recover properly even if the
+    -- snapshot has not been taken. Luatest automatically calls
+    -- wait_until_ready().
+    cg.server:restart()
+end
