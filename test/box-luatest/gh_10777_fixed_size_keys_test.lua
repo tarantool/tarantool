tarantool/tarantool

commit 7410a4f0f29114bda675e8bffe4eca611e85c3f4

box: add payload info for client error codes
So that error details are added as error payload. Now client don't need
to parse error message to get details.

box/error.test is changed to pass checkpatch. It is reported 'bla bla'
as possibly repeated word.

Closes #9108
Closes #6166

@TarantoolBot document
Title: Provide error details through payload fields
Product: Tarantool
Since: 3.1

Now Tarantool error details are available as payload fields (which
in turn can be accessed as error object fields). For example:

```
tarantool> e = box.error.new(box.error.SPACE_EXISTS, 'some space')
tarantool> e.message
---
- Space 'some space' already exists
...
tarantool> e.space
---
- some space
...
```

One can introspect the details using `error:unpack()`:

tarantool> e:unpack()
---
- space: some space
  code: 10
  base_type: ClientError
  type: ClientError
  message: Space 'some space' already exists
  trace:
  - file: '[string "e = box.error.new(box.error.SPACE_EXISTS, ''so..."]'
    line: 1
...

src/box/tuple_format.c
@@ -1024,6 +1024,8 @@ tuple_format_required_fields_validate(struct tuple_format *format,
 				      void *required_fields,
 				      uint32_t required_fields_sz);
 
+#define ERROR_MSG(r, v) "expected [%" r "..%" r "], got %" v
+
 /**
  * Check if integer value in `mp_data' is within an allowed range for the given
  * `field' type.
@@ -1035,7 +1037,9 @@ tuple_field_check_fixed_int_range(struct tuple_format *format,
 				  const char *mp_data)
 {
 	assert(tuple_field_type_is_fixed_int(field->type));
-	const char *min_str, *max_str, *val_str;
+	const char *details;
+	char mp_min[16], mp_max[16];
+	const char *mp_value = mp_data;
 	enum mp_type mp_type = mp_typeof(*mp_data);
 	if (mp_type == MP_NIL)
 		return 0;
@@ -1044,21 +1048,21 @@ tuple_field_check_fixed_int_range(struct tuple_format *format,
 	if (field_type_is_fixed_signed[field->type]) {
 		int64_t min = field_type_min_value[field->type];
 		int64_t max = field_type_max_value[field->type];
+		mp_encode_int(mp_min, min);
+		mp_encode_uint(mp_max, max);
 		if (mp_type == MP_INT) {
 			int64_t value = mp_decode_int(&mp_data);
 			if (value < min || value > max) {
-				min_str = tt_sprintf("%" PRId64, min);
-				max_str = tt_sprintf("%" PRId64, max);
-				val_str = tt_sprintf("%" PRId64, value);
+				details = tt_sprintf(ERROR_MSG(PRId64, PRId64),
+						     min, max, value);
 				goto error;
 			}
 		} else {
 			assert(mp_type == MP_UINT);
 			uint64_t value = mp_decode_uint(&mp_data);
 			if (value > (uint64_t)max) {
-				min_str = tt_sprintf("%" PRId64, min);
-				max_str = tt_sprintf("%" PRId64, max);
-				val_str = tt_sprintf("%" PRIu64, value);
+				details = tt_sprintf(ERROR_MSG(PRId64, PRIu64),
+						     min, max, value);
 				goto error;
 			}
 		}
@@ -1068,10 +1072,11 @@ tuple_field_check_fixed_int_range(struct tuple_format *format,
 		uint64_t value = mp_decode_uint(&mp_data);
 		uint64_t min = field_type_min_value[field->type];
 		uint64_t max = field_type_max_value[field->type];
+		mp_encode_uint(mp_min, min);
+		mp_encode_uint(mp_max, max);
 		if (value > max) {
-			min_str = tt_sprintf("%" PRIu64, min);
-			max_str = tt_sprintf("%" PRIu64, max);
-			val_str = tt_sprintf("%" PRIu64, value);
+			details = tt_sprintf(ERROR_MSG(PRIu64, PRIu64),
+					     min, max, value);
 			goto error;
 		}
 	}
@@ -1079,9 +1084,11 @@ tuple_field_check_fixed_int_range(struct tuple_format *format,
 error:
 	diag_set(ClientError, ER_FIELD_VALUE_OUT_OF_RANGE,
 		 tuple_field_path(field, format), field_type_strs[field->type],
-		 min_str, max_str, val_str);
+		 details, mp_value, mp_min, mp_max);
 	return -1;
 }
+
+#undef ERROR_MSG
 
 /**
  * Check constraints of one particular @a field.

test/box/error.result
@@ -17,27 +17,28 @@ box.error({code = 123, reason = 'test'})
  | ---
  | - error: test
  | ...
-box.error(box.error.ILLEGAL_PARAMS, "bla bla")
+box.error(box.error.ILLEGAL_PARAMS, "foo")
  | ---
- | - error: Illegal parameters, bla bla
+ | - error: Illegal parameters, foo
  | ...
 box.error()
  | ---
- | - error: Illegal parameters, bla bla
+ | - error: Illegal parameters, foo
  | ...
 e = box.error.last()
  | ---
  | ...
 e
  | ---
- | - Illegal parameters, bla bla
+ | - Illegal parameters, foo
  | ...
 e:unpack()
  | ---
  | - code: 1
  |   base_type: ClientError
  |   type: ClientError
- |   message: Illegal parameters, bla bla
+ |   details: foo
+ |   message: Illegal parameters, foo
  |   trace:
  |   - file: '[C]'
  |     line: 4294967295
@@ -52,11 +53,11 @@ e.code
  | ...
 e.message
  | ---
- | - Illegal parameters, bla bla
+ | - Illegal parameters, foo
  | ...
 tostring(e)
  | ---
- | - Illegal parameters, bla bla
+ | - Illegal parameters, foo
  | ...
 e = nil
  | ---
@@ -224,9 +225,7 @@ t;
  |   1: box.error.ILLEGAL_PARAMS
  |   2: box.error.MEMORY_ISSUE
  |   3: box.error.TUPLE_FOUND
- |   4: box.error.TUPLE_NOT_FOUND
  |   5: box.error.UNSUPPORTED
- |   6: box.error.NONMASTER
  |   7: box.error.READONLY
  |   8: box.error.INJECTION
  |   9: box.error.CREATE_SPACE
@@ -241,7 +240,6 @@ t;
  |   18: box.error.KEY_PART_TYPE
  |   19: box.error.EXACT_MATCH
  |   20: box.error.INVALID_MSGPACK
- |   21: box.error.PROC_RET
  |   22: box.error.TUPLE_NOT_ARRAY
  |   23: box.error.FIELD_TYPE
  |   24: box.error.INDEX_PART_TYPE_MISMATCH
@@ -288,15 +286,11 @@ t;
  |   65: box.error.REPLICASET_UUID_IS_RO
  |   66: box.error.INSTANCE_UUID_MISMATCH
  |   67: box.error.REPLICA_ID_IS_RESERVED
- |   68: box.error.INVALID_ORDER
  |   69: box.error.MISSING_REQUEST_FIELD
  |   70: box.error.IDENTIFIER
  |   71: box.error.DROP_FUNCTION
  |   72: box.error.ITERATOR_TYPE
  |   73: box.error.REPLICA_MAX
- |   74: box.error.INVALID_XLOG
- |   75: box.error.INVALID_XLOG_NAME
- |   76: box.error.INVALID_XLOG_ORDER
  |   77: box.error.NO_CONNECTION
  |   78: box.error.TIMEOUT
  |   79: box.error.ACTIVE_TRANSACTION
@@ -323,9 +317,7 @@ t;
  |   100: box.error.FUNCTION_LANGUAGE
  |   101: box.error.RTREE_RECT
  |   102: box.error.PROC_C
- |   103: box.error.UNKNOWN_RTREE_INDEX_DISTANCE_TYPE
  |   104: box.error.PROTOCOL
- |   105: box.error.UPSERT_UNIQUE_SECONDARY_KEY
  |   106: box.error.WRONG_INDEX_RECORD
  |   107: box.error.WRONG_INDEX_PARTS
  |   108: box.error.WRONG_INDEX_OPTIONS
@@ -338,7 +330,6 @@ t;
  |   115: box.error.SYSTEM
  |   116: box.error.LOADING
  |   117: box.error.CONNECTION_TO_SELF
- |   118: box.error.KEY_PART_IS_TOO_LONG
  |   119: box.error.COMPRESSION
  |   120: box.error.CHECKPOINT_IN_PROGRESS
  |   121: box.error.SUB_STMT_MAX
@@ -376,7 +367,6 @@ t;
  |   153: box.error.NO_SUCH_FIELD_NAME_IN_SPACE
  |   154: box.error.TRANSACTION_YIELD
  |   155: box.error.NO_SUCH_GROUP
- |   156: box.error.SQL_BIND_VALUE
  |   157: box.error.SQL_BIND_TYPE
  |   158: box.error.SQL_BIND_PARAMETER_MAX
  |   159: box.error.SQL_EXECUTE
@@ -388,7 +378,6 @@ t;
  |   165: box.error.NO_SUCH_MODULE
  |   166: box.error.NO_SUCH_COLLATION
  |   167: box.error.CREATE_FK_CONSTRAINT
- |   168: box.error.DROP_FK_CONSTRAINT
  |   169: box.error.NO_SUCH_CONSTRAINT
  |   170: box.error.CONSTRAINT_EXISTS
  |   171: box.error.SQL_TYPE_MISMATCH
@@ -407,16 +396,13 @@ t;
  |   184: box.error.SQL_SYNTAX_NEAR_TOKEN
  |   185: box.error.SQL_UNKNOWN_TOKEN
  |   186: box.error.SQL_PARSER_GENERIC
- |   187: box.error.SQL_ANALYZE_ARGUMENT
  |   188: box.error.SQL_COLUMN_COUNT_MAX
  |   189: box.error.HEX_LITERAL_MAX
  |   190: box.error.INT_LITERAL_MAX
  |   191: box.error.SQL_PARSER_LIMIT
  |   192: box.error.INDEX_DEF_UNSUPPORTED
- |   193: box.error.CK_DEF_UNSUPPORTED
  |   194: box.error.MULTIKEY_INDEX_MISMATCH
  |   195: box.error.CREATE_CK_CONSTRAINT
- |   196: box.error.CK_CONSTRAINT_FAILED
  |   197: box.error.SQL_COLUMN_COUNT
  |   198: box.error.FUNC_INDEX_FUNC
  |   199: box.error.FUNC_INDEX_FORMAT
@@ -435,7 +421,6 @@ t;
  |   212: box.error.SEQUENCE_NOT_STARTED
  |   213: box.error.NO_SUCH_SESSION_SETTING
  |   214: box.error.UNCOMMITTED_FOREIGN_SYNC_TXNS
- |   215: box.error.SYNC_MASTER_MISMATCH
  |   216: box.error.SYNC_QUORUM_TIMEOUT
  |   217: box.error.SYNC_ROLLBACK
  |   218: box.error.TUPLE_METADATA_IS_TOO_BIG

src/box/error.cc
@@ -378,6 +378,7 @@ AccessDeniedError::AccessDeniedError(const char *file, unsigned int line,
 	error_set_str(this, "object_type", object_type);
 	error_set_str(this, "object_name", object_name);
 	error_set_str(this, "access_type", access_type);
+	error_set_str(this, "user", user_name);
 }
 
 struct error *

src/box/index.cc
@@ -56,6 +56,11 @@ UnsupportedIndexFeature::UnsupportedIndexFeature(const char *file,
 	error_format_msg(this, tnt_errcode_desc(code), index_def->name,
 			 index_type_strs[index_def->type],
 			 space->def->name, space->def->engine_name, what);
+	error_set_str(this, "index", index_def->name);
+	error_set_str(this, "index_type", index_type_strs[index_def->type]);
+	error_set_str(this, "space", space->def->name);
+	error_set_str(this, "engine", space->def->engine_name);
+	error_set_str(this, "feature", what);
 }
 
 struct error *

src/box/lua/call.c
@@ -174,7 +174,7 @@ box_lua_find(lua_State *L, const char *name, const char *name_end)
 	return lua_gettop(L) - top;
 
 no_such_proc:
-	diag_set(ClientError, ER_NO_SUCH_PROC, name_end - name, name);
+	diag_set(ClientError, ER_NO_SUCH_PROC, tt_cstr(name, name_end - name));
 	return -1;
 }
 

src/box/lua/schema.lua
@@ -3510,11 +3510,14 @@ local function grant_error(name, object_name, object_type, privilege)
        privilege == 'execute' then
         box.error(box.error.ROLE_GRANTED, name, object_name)
     end
+    local object_repr
     if not is_singleton_object_type(object_type) then
-        object_name = string.format(" '%s'", object_name)
+        object_repr = string.format("%s '%s'", object_type, object_name)
+    else
+        object_repr = object_type
     end
-    box.error(box.error.PRIV_GRANTED, name, privilege, object_type,
-              object_name)
+    box.error(box.error.PRIV_GRANTED, name, privilege, object_repr,
+              object_type, object_name)
 end
 
 local function grant(uid, name, privilege, object_type,

src/box/memtx_engine.cc
@@ -1819,7 +1819,8 @@ memtx_tuple_new_raw_impl(struct tuple_format *format, const char *data,
 		goto end;
 	});
 	if (unlikely(total > memtx->max_tuple_size)) {
-		diag_set(ClientError, ER_MEMTX_MAX_TUPLE_SIZE, total);
+		diag_set(ClientError, ER_MEMTX_MAX_TUPLE_SIZE, total,
+			 memtx->max_tuple_size);
 		error_log(diag_last_error(diag_get()));
 		goto end;
 	}

commit f322a1af22796717524f86bb7c687254d99955de

box: validate fixed integer types keys range
Currently `index:get({256})` is valid request for 'uint8' field for
example.  It is not on par with error for negative values and for range
check for tuples. Let's add range check for fixed integer types for keys
too.

While at it let's move and rename `tuple_field_type_is_fixed_int`. Also
function for checking range is fixed to fill min and max MsgPack values
only on errors. This potential performance issue was introduced in
the commit 7410a4f0f291 ("box: add payload info for client error codes").

Follow-up #9548
Closes #10777

NO_DOC=bugfix

src/box/errcode.h
@@ -434,6 +434,7 @@ struct errcode_record {
 	_(ER_WAL_QUEUE_FULL, 287,		"The WAL queue is full") \
 	_(ER_INVALID_VCLOCK, 288,		"Invalid vclock", "value", STRING) \
 	_(ER_SYNC_QUEUE_FULL, 289,		"The synchronous transaction queue is full") \
+	_(ER_KEY_PART_VALUE_OUT_OF_RANGE, 290,	"The value of key part exceeds the supported range for type", "partno", UINT, "field_type", STRING, "value", MSGPACK, "min", MSGPACK, "max", MSGPACK) \
 	TEST_ERROR_CODES(_) /** This one should be last. */
 
 /*

src/box/field_def.c
@@ -328,9 +328,9 @@ field_type1_contains_type2(enum field_type type1, enum field_type type2)
 	bool type1_contains_type2 = (mp_type1 & mp_type2) == mp_type2;
 	if (!type1_contains_type2)
 		return false;
-	if (!tuple_field_type_is_fixed_int(type1))
+	if (!field_type_is_fixed_int(type1))
 		return true;
-	if (!tuple_field_type_is_fixed_int(type2)) {
+	if (!field_type_is_fixed_int(type2)) {
 		/* uint64 is an alias for unsigned. */
 		return type1 == FIELD_TYPE_UINT64 &&
 		       type2 == FIELD_TYPE_UNSIGNED;
@@ -418,6 +418,66 @@ field_type_by_name(const char *name, size_t len)
 		return FIELD_TYPE_ANY;
 	return field_type_MAX;
 }
+
+#define ERROR_MSG(r, v) "expected [%" r "..%" r "], got %" v
+
+bool
+field_mp_is_in_fixed_int_range(enum field_type type, const char *data,
+			       char *mp_min, char *mp_max,
+			       const char **details)
+{
+	assert(field_type_is_fixed_int(type));
+	enum mp_type mp_type = mp_typeof(*data);
+	if (mp_type == MP_NIL)
+		return true;
+	assert(mp_type == MP_INT || mp_type == MP_UINT);
+
+	if (field_type_is_fixed_signed[type]) {
+		int64_t min = field_type_min_value[type];
+		int64_t max = field_type_max_value[type];
+		if (mp_type == MP_INT) {
+			int64_t value = mp_decode_int(&data);
+			if (value < min || value > max) {
+				mp_encode_int(mp_min, min);
+				mp_encode_uint(mp_max, max);
+				if (details != NULL)
+					*details = tt_sprintf(ERROR_MSG(PRId64,
+									PRId64),
+							      min, max, value);
+				return false;
+			}
+		} else {
+			assert(mp_type == MP_UINT);
+			uint64_t value = mp_decode_uint(&data);
+			if (value > (uint64_t)max) {
+				mp_encode_int(mp_min, min);
+				mp_encode_uint(mp_max, max);
+				if (details != NULL)
+					*details = tt_sprintf(ERROR_MSG(PRId64,
+									PRIu64),
+							      min, max, value);
+				return false;
+			}
+		}
+	} else {
+		assert(field_type_is_fixed_unsigned[type]);
+		assert(mp_type == MP_UINT);
+		uint64_t value = mp_decode_uint(&data);
+		uint64_t min = field_type_min_value[type];
+		uint64_t max = field_type_max_value[type];
+		if (value > max) {
+			mp_encode_uint(mp_min, min);
+			mp_encode_uint(mp_max, max);
+			if (details != NULL)
+				*details = tt_sprintf(ERROR_MSG(PRIu64, PRIu64),
+						      min, max, value);
+			return false;
+		}
+	}
+	return true;
+}
+
+#undef ERROR_MSG
 
 /**
  * Parse default field value from msgpack.

src/box/field_def.h
@@ -112,6 +112,17 @@ bool
 field_type1_contains_type2(enum field_type type1, enum field_type type2);
 
 /**
+ * Return true for fixed-size integer field `type'.
+ */
+static inline bool
+field_type_is_fixed_int(enum field_type type)
+{
+	assert(type < field_type_MAX);
+	return field_type_is_fixed_signed[type] ||
+	       field_type_is_fixed_unsigned[type];
+}
+
+/**
  * Get field type by name
  */
 enum field_type
@@ -198,6 +209,25 @@ field_mp_type_is_compatible(enum field_type type, const char *data,
 		}
 	}
 }
+
+/**
+ * Check if MsgPack value fits into the range for fixed size integer type.
+ * @param type - fixed size integer type.
+ * @param data - MsgPack value.
+ * @param mp_min - buffer to hold MsgPack of minimum value for type
+ *   if value does not fit.
+ * @param mp_max - buffer to hold MsgPack of maximum value for type
+ *   if value does not fit.
+ * @param details[out] - pointer to hold pointer to statically allocated
+ *   string with error details if value does not fit. Can be NULL if
+ *   details are not required.
+ * @retval true if fits and false otherwise. If not fit then mp_min, mp_max
+ *   and details are filled with error details.
+ */
+bool
+field_mp_is_in_fixed_int_range(enum field_type type, const char *data,
+			       char *mp_min, char *mp_max,
+			       const char **details);
 
 static inline bool
 action_is_nullable(enum on_conflict_action nullable_action)

src/box/key_def.h
@@ -884,6 +884,16 @@ key_part_validate(enum field_type key_type, const char *key,
 			 field_type_strs[key_type]);
 		return -1;
 	}
+	if (field_type_is_fixed_int(key_type)) {
+		char mp_min[16], mp_max[16];
+		if (!field_mp_is_in_fixed_int_range(
+				key_type, key, mp_min, mp_max, NULL)) {
+			diag_set(ClientError, ER_KEY_PART_VALUE_OUT_OF_RANGE,
+				 field_no, field_type_strs[key_type], key,
+				 mp_min, mp_max);
+			return -1;
+		}
+	}
 	return 0;
 }
 

src/box/tuple_format.c
@@ -1002,77 +1002,6 @@ tuple_format1_can_store_format2_tuples(struct tuple_format *format1,
 	return true;
 }
 
-static int
-tuple_format_required_fields_validate(struct tuple_format *format,
-				      void *required_fields,
-				      uint32_t required_fields_sz);
-
-#define ERROR_MSG(r, v) "expected [%" r "..%" r "], got %" v
-
-/**
- * Check if integer value in `mp_data' is within an allowed range for the given
- * `field' type.
- * Return -1 and raise an error if it is out of range, or 0 if all is ok.
- */
-static int
-tuple_field_check_fixed_int_range(struct tuple_format *format,
-				  struct tuple_field *field,
-				  const char *mp_data)
-{
-	assert(tuple_field_type_is_fixed_int(field->type));
-	const char *details;
-	char mp_min[16], mp_max[16];
-	const char *mp_value = mp_data;
-	enum mp_type mp_type = mp_typeof(*mp_data);
-	if (mp_type == MP_NIL)
-		return 0;
-	assert(mp_type == MP_INT || mp_type == MP_UINT);
-
-	if (field_type_is_fixed_signed[field->type]) {
-		int64_t min = field_type_min_value[field->type];
-		int64_t max = field_type_max_value[field->type];
-		mp_encode_int(mp_min, min);
-		mp_encode_uint(mp_max, max);
-		if (mp_type == MP_INT) {
-			int64_t value = mp_decode_int(&mp_data);
-			if (value < min || value > max) {
-				details = tt_sprintf(ERROR_MSG(PRId64, PRId64),
-						     min, max, value);
-				goto error;
-			}
-		} else {
-			assert(mp_type == MP_UINT);
-			uint64_t value = mp_decode_uint(&mp_data);
-			if (value > (uint64_t)max) {
-				details = tt_sprintf(ERROR_MSG(PRId64, PRIu64),
-						     min, max, value);
-				goto error;
-			}
-		}
-	} else {
-		assert(field_type_is_fixed_unsigned[field->type]);
-		assert(mp_type == MP_UINT);
-		uint64_t value = mp_decode_uint(&mp_data);
-		uint64_t min = field_type_min_value[field->type];
-		uint64_t max = field_type_max_value[field->type];
-		mp_encode_uint(mp_min, min);
-		mp_encode_uint(mp_max, max);
-		if (value > max) {
-			details = tt_sprintf(ERROR_MSG(PRIu64, PRIu64),
-					     min, max, value);
-			goto error;
-		}
-	}
-	return 0;
-error:
-	diag_set(ClientError, ER_FIELD_VALUE_OUT_OF_RANGE,
-		 tuple_field_path(field, format), field_type_strs[field->type],
-		 details, mp_value, mp_min, mp_max);
-	return -1;
-}
-
-#undef ERROR_MSG
-
 /**
  * Check constraints of one particular @a field.
  */
@@ -1121,9 +1050,19 @@ tuple_field_validate(struct tuple_format *format, struct tuple_field *field,
 			 mp_type_strs[mp_typeof(*mp_data)]);
 		return -1;
 	}
-	if (tuple_field_type_is_fixed_int(field->type) &&
-	    tuple_field_check_fixed_int_range(format, field, mp_data) != 0)
-		return -1;
+	if (field_type_is_fixed_int(field->type)) {
+		const char *details;
+		char mp_min[16], mp_max[16];
+		if (!field_mp_is_in_fixed_int_range(
+				field->type, mp_data, mp_min, mp_max,
+				&details)) {
+			diag_set(ClientError, ER_FIELD_VALUE_OUT_OF_RANGE,
+				 tuple_field_path(field, format),
+				 field_type_strs[field->type], details,
+				 mp_data, mp_min, mp_max);
+			return -1;
+		}
+	}
 	if (tuple_field_check_constraint(field, mp_data, mp_data_end) != 0)
 		return -1;
 	return 0;

src/box/tuple_format.h
@@ -201,17 +201,6 @@ tuple_field_has_default(const struct tuple_field *tuple_field)
 }
 
 /**
- * Return true for fixed-size integer field `type'.
- */
-static inline bool
-tuple_field_type_is_fixed_int(enum field_type type)
-{
-	assert(type < field_type_MAX);
-	return field_type_is_fixed_signed[type] ||
-	       field_type_is_fixed_unsigned[type];
-}
-
-/**
  * Return path to a tuple field. Used for error reporting.
  */
 const char *

test/box-luatest/gh_10777_fixed_size_keys_test.lua
@@ -1,0 +1,131 @@
+local server = require('luatest.server')
+local t = require('luatest')
+
+local g = t.group()
+
+g.before_all(function(cg)
+    cg.server = server:new()
+    cg.server:start()
+end)
+
+g.after_all(function(cg)
+    cg.server:drop()
+end)
+
+g.after_each(function(cg)
+    cg.server:exec(function()
+        if box.space.test ~= nil then
+            box.space.test:drop()
+        end
+    end)
+end)
+
+g.test_range = function(cg)
+    cg.server:exec(function()
+        local s = box.schema.create_space('test', {format = {
+            {'i8', 'int8'},
+            {'i16', 'int16'},
+            {'i32', 'int32'},
+            {'i64', 'int64'},
+            {'u8', 'uint8'},
+            {'u16', 'uint16'},
+            {'u32', 'uint32'},
+        }})
+        local errmsg =
+                'The value of key part exceeds the supported range for type'
+        local test_signed_size = function(bits)
+            local exp = bits - 1
+            local index = s:create_index('test', {parts = {'i' .. bits}})
+            t.assert_error_covers({
+                code = box.error.KEY_PART_VALUE_OUT_OF_RANGE,
+                key = {-2^exp - 1},
+                message = errmsg,
+                partno = 0,
+                field_type = 'int' .. bits,
+                value = -2^exp - 1,
+                min = -2^exp,
+                max = 2^exp - 1,
+            }, index.get, index, {-2^exp - 1})
+            t.assert_equals(index:get{-2^exp}, nil)
+            t.assert_error_covers({
+                code = box.error.KEY_PART_VALUE_OUT_OF_RANGE,
+                key = {2^exp},
+                message = errmsg,
+                partno = 0,
+                field_type = 'int' .. bits,
+                value = 2^exp,
+                min = -2^exp,
+                max = 2^exp - 1,
+            }, index.get, index, {2^exp})
+            t.assert_equals(index:get{2^exp - 1}, nil)
+            index:drop()
+        end
+        local test_unsigned_size = function(bits)
+            local exp = bits
+            local index = s:create_index('test', {parts = {'u' .. bits}})
+            t.assert_error_covers({
+                code = box.error.KEY_PART_VALUE_OUT_OF_RANGE,
+                key = {2^exp},
+                message = errmsg,
+                partno = 0,
+                field_type = 'uint' .. bits,
+                value = 2^exp,
+                min = 0,
+                max = 2^exp - 1,
+            }, index.get, index, {2^exp})
+            t.assert_equals(index:get{2^exp - 1}, nil)
+            index:drop()
+        end
+        test_signed_size(8)
+        test_signed_size(16)
+        test_signed_size(32)
+        test_unsigned_size(8)
+        test_unsigned_size(16)
+        test_unsigned_size(32)
+        local index = s:create_index('test', {parts = {'i64'}})
+        t.assert_error_covers({
+            code = box.error.KEY_PART_VALUE_OUT_OF_RANGE,
+            key = {9223372036854775808ULL},
+            message = errmsg,
+            partno = 0,
+            field_type = 'int64',
+            value = 9223372036854775808ULL,
+            min = -9223372036854775808LL,
+            max = 9223372036854775807LL,
+        }, index.get, index, {9223372036854775808ULL})
+        t.assert_equals(index:get{9223372036854775807ULL}, nil)
+    end)
+end
+
+g.test_error_partno = function(cg)
+    cg.server:exec(function()
+        local s = box.schema.create_space('test', {format = {
+            {'f1', 'uint8'},
+            {'f2', 'int8'},
+        }})
+        local index = s:create_index('test', {parts = {'f1', 'f2'}})
+        -- Test `partno` is correct.
+        t.assert_error_covers({
+            code = box.error.KEY_PART_VALUE_OUT_OF_RANGE,
+            key = {0, 2^7},
+            partno = 1,
+            field_type = 'int8',
+            value = 2^7,
+            min = -2^7,
+            max = 2^7 - 1,
+        }, index.get, index, {0, 2^7})
+    end)
+end
+
+g.test_null_value = function(cg)
+    cg.server:exec(function()
+        local s = box.schema.create_space('test', {format = {
+            {'f1', 'unsigned'},
+            {'f2', 'uint8', is_nullable = true},
+        }})
+        s:create_index('pk')
+        local index = s:create_index('sk', {parts = {'f2'}})
+        -- Use count as it allows nullable parts.
+        t.assert_equals(index:count({box.NULL}), 0)
+    end)
+end

test/box/error.result
@@ -500,6 +500,7 @@ t;
  |   287: box.error.WAL_QUEUE_FULL
  |   288: box.error.INVALID_VCLOCK
  |   289: box.error.SYNC_QUEUE_FULL
+ |   290: box.error.KEY_PART_VALUE_OUT_OF_RANGE
  | ...
 
 test_run:cmd("setopt delimiter ''");