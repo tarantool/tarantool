4b98814cc9373d807b53d53a6e283bbea08dd002
Alexander Turenko <alexander.turenko@tarantool.org>
2024-11-27 03:08:49 +0300

    replication: allow early make_bootstrap_leader()
    This commit allows to call `box.ctl.make_bootstrap_leader()` before the
    first `box.cfg()` call. See the documentation request below for details.
    
    Fixes #10858
    
    @TarantoolBot document
    Title: Allow early `box.ctl.make_bootstrap_leader()`
    
    Dev issue: https://github.com/tarantool/tarantool/issues/10858
    
    Since Tarantool 3.4.0 it is allowed to call
    `box.ctl.make_bootstrap_leader()` before the first `box.cfg()` call to
    mark the instance as a bootstrap leader.
    
    It works if `box.cfg({bootstrap_strategy = 'supervised'})` is set,
    ignored otherwise.
    
    So, the following code is now possible:
    
    ```lua
    box.ctl.make_bootstrap_leader()
    box.cfg({
        bootstrap_strategy = 'supervised',
    })
    ```
    
    In earlier Tarantool version a user has to call `box.cfg()` in a
    separate fiber first and then call `box.ctl.make_bootstrap_leader()`.
    
    Also, earlier Tarantool versions do not allow to bootstrap a database
    using the `'supervised'` bootstrap strategy if there are no upstreams
    configured (empty `box.cfg.replication`). It means that there were no
    way to bootstrap the database without iproto listening port configured
    (`box.cfg.listen`) except using another bootstrap strategy.
    
    Now, the `'supervised'` bootstrap strategy let a user a full control
    over the replicaset bootstrapping process.
    
    If several instances call `box.ctl.make_bootstrap_leader()` to
    bootstrap the database, each bootstraps its own database and it leads to
    a JOIN failure. With great power comes great responsibility!

--- src/box/box.cc
+++ src/box/box.cc
@@ -2249,10 +2249,14 @@ int
 box_make_bootstrap_leader(void)
 {
 	if (tt_uuid_is_nil(&INSTANCE_UUID)) {
-		diag_set(ClientError, ER_UNSUPPORTED,
-			 "box.ctl.make_bootstrap_leader()",
-			 "promoting this instance before box.cfg() is called");
-		return -1;
+		/*
+		 * Can't store the UUID of the given instance as
+		 * the bootstrap leader UUID at this moment,
+		 * because there is no instance UUID yet. Postpone
+		 * this action till the bootstrap.
+		 */
+		is_supervised_bootstrap_leader = true;
+		return 0;
 	}
 	/* Bootstrap strategy is read by the time instance uuid is known. */
 	assert(bootstrap_strategy != BOOTSTRAP_STRATEGY_INVALID);
@@ -2271,6 +2275,7 @@ box_make_bootstrap_leader(void)
 		return box_set_bootstrap_leader_record();
 	} else {
 		bootstrap_leader_uuid = INSTANCE_UUID;
+		is_supervised_bootstrap_leader = true;
 		box_broadcast_ballot();
 		return 0;
 	}
