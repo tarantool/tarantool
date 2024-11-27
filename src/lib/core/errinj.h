25cd962071ddc0863619ff39b9a94aa96f4dff50
Nikita Zheleztsov <n.zheleztsov@proton.me>
2024-11-27 13:52:54 +0300

    engine: fix sending raft state during META_JOIN
    Engine (and previously relay) didn't send raft state during META_JOIN
    stage, as it was marked with GROUP_LOCAL id and skipped by the stream
    function. This may cause split brain as replica doesn't have any info
    about term before the time, it gets it in the beginning of the
    SUBSCRIBE (see sent_raft_term).
    
    Let's use GROUP_DEFAULT id, so that raft request is properly sent during
    META_JOIN stage. We must nullify state and vclock of the raft request,
    since we don't need them during raft recovery and because old replicas
    assert, that they are not set, and we don't want to break the upgrade
    process.
    
    Closes #10089
    
    NO_DOC=bugfix
    
    (cherry picked from commit 3c7ed21d264ba3a9c27e0e1150885e34f6b5ae4b)

--- src/lib/core/errinj.h
+++ src/lib/core/errinj.h
@@ -77,6 +77,7 @@ struct errinj {
 	_(ERRINJ_APPLIER_READ_TX_ROW_DELAY, ERRINJ_BOOL, {.bparam = false})\
 	_(ERRINJ_APPLIER_SLOW_ACK, ERRINJ_BOOL, {.bparam = false}) \
 	_(ERRINJ_APPLIER_STOP_DELAY, ERRINJ_BOOL, {.bparam = false}) \
+	_(ERRINJ_APPLIER_SUBSCRIBE_DELAY, ERRINJ_BOOL, {.bparam = false}) \
 	_(ERRINJ_BUILD_INDEX, ERRINJ_INT, {.iparam = -1}) \
 	_(ERRINJ_BUILD_INDEX_DELAY, ERRINJ_BOOL, {.bparam = false}) \
 	_(ERRINJ_BUILD_INDEX_DISABLE_YIELD, ERRINJ_BOOL, {.bparam = false}) \
