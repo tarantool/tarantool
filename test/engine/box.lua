cf8d76bc6cad06fdfc36eb232b23180fb543b150
Sergey Bronnikov <sergeyb@tarantool.org>
2025-01-09 19:01:19 +0300

    test: get rid of long tests
    See details in #10840, in short:
    
    - There are no precise criteria to distinguish long tests from
      other tests. Often it's a matter of taste: it feels like some
      tests take longer than others.
    - The mark "long" will not be likely to be removed, even if it is
      no longer such.
    - Splitting tests into long and short makes testing more difficult
      and reduces the frequency of running some tests.
    
    The patch removes `long` markers in tests and merges test suite
    `engine_long` with "long" tests together with the `engine` suite.
    
    Note, the test `vinyl/select_consistency.test.lua` was marked as
    "long", it is gone in commit 153463df11b3
    ("test: drop vinyl/select_consistency.test.lua").
    
    Closes #10840
    
    NO_CHANGELOG=testing
    NO_DOC=testing

--- test/engine/box.lua
+++ test/engine/box.lua
@@ -4,15 +4,16 @@ local os = require('os')
 box.cfg{
     listen              = os.getenv("LISTEN"),
     memtx_memory        = 107374182,
+    vinyl_memory        = 107374182,
     pid_file            = "tarantool.pid",
     wal_max_size        = 2500,
-    vinyl_read_threads  = 2,
-    vinyl_write_threads = 3,
-    vinyl_range_size    = 64 * 1024,
-    vinyl_page_size     = 1024,
+    vinyl_read_threads  = 3,
+    vinyl_write_threads = 5,
+    vinyl_range_size    = 1024 * 1024,
+    vinyl_page_size     = 4 * 1024,
     memtx_max_tuple_size = 1024 * 1024 * 100,
     vinyl_max_tuple_size = 1024 * 1024 * 100,
-    memtx_allocator      = os.getenv("MEMTX_ALLOCATOR")
+    memtx_allocator      = os.getenv("MEMTX_ALLOCATOR"),
 }
 
 require('console').listen(os.getenv('ADMIN'))
