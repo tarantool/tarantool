b4cb219a429fcffab4d39aee2a8ca0ae08701962
Magomed <m.kostoev@tarantool.org>
2025-01-27 08:17:57 +0300

    memtx: introduce the O(n) SK build
    NO_DOC=TBD
    NO_TEST=TBD
    NO_CHANGELOG=TBD

--- src/box/index.h
+++ src/box/index.h
@@ -650,6 +650,8 @@ struct read_view_tuple {
 	const char *data;
 	/** Size of tuple data. */
 	uint32_t size;
+	/** Pointer to the tuple. */
+	struct tuple *ptr;
 };
 
 /** Object returned if there's no more tuples matching the search criteria. */
@@ -660,6 +662,7 @@ read_view_tuple_none(void)
 	tuple.needs_upgrade = false;
 	tuple.data = NULL;
 	tuple.size = 0;
+	tuple.ptr = NULL;
 	return tuple;
 }
 
