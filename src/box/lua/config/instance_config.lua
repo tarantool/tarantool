3e726b1a309742473d17dec98a699949dbedafeb
Georgiy Belyanin <belyaningeorge@ya.ru>
2025-03-03 12:02:38 +0300

    config: add `connpool.idle_timeout` option
    This patch adds a `connpool` configuration section and a
    `connpool.idle_timeout` configuration option. It's used to configure the
    timeout in seconds for closing unused connections established by
    `connpool.filter()` and `connpool.call()`.
    
    @TarantoolBot document
    title: connpool: support timeout to close idle connections
    
    New function in `experimental.connpool()`.
    
    ```
    connpool.set_idle_timeout(timeout)
    ```
    
    Params:
    * `timeout`, `integer` -- specify a timeout (in seconds) in which the
      unused connections indirectly opened by connpool methods.
    
    New configuration option `connpool.idle_timeout`.
    
    A timeout (in seconds) in which the unused connections opened by
    connpool indirectly would be closed.

--- src/box/lua/config/instance_config.lua
+++ src/box/lua/config/instance_config.lua
@@ -2211,6 +2211,12 @@ return schema.new('instance_config', schema.record({
             default = 10,
         }),
     })),
+    connpool = schema.record({
+        idle_timeout = schema.scalar({
+            type = 'number',
+            default = 60,
+        }),
+    }),
 }), {
     methods = {
         instance_uri = instance_uri,
