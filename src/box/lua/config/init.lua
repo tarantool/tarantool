e034c072e19f4e84265639b17f2e5669b8615549
Borislav Demidov <Boris.Stepanenko@gmail.com>
2025-03-20 20:44:38 +0300

    config: add API to test if role is set for instance
    NO_DOC=WIP
    NO_CHANGELOG=WIP

--- src/box/lua/config/init.lua
+++ src/box/lua/config/init.lua
@@ -529,6 +529,56 @@ function methods.info(self, version)
         table.concat(supported_versions, ', '), version), 0)
 end
 
+function methods.is_storage(self, opts)
+    selfcheck(self, 'is_storage')
+    initcheck(self, 'is_storage', 'cluster')
+
+    opts = opts or {}
+
+    local sharding_roles = self:get(
+        'sharding.roles', {instance = opts.instance}) or {}
+
+    for _, role in ipairs(sharding_roles) do
+        if role == 'storage' then
+            return true
+        end
+    end
+    return false
+end
+
+function methods.is_router(self, opts)
+    selfcheck(self, 'is_router')
+    initcheck(self, 'is_router', 'cluster')
+
+    opts = opts or {}
+
+    local sharding_roles = self:get(
+        'sharding.roles', {instance = opts.instance}) or {}
+
+    for _, role in ipairs(sharding_roles) do
+        if role == 'router' then
+            return true
+        end
+    end
+    return false
+end
+
+function methods.has_role(self, role, opts)
+    selfcheck(self, 'has_role')
+    initcheck(self, 'has_role', 'cluster')
+
+    opts = opts or {}
+
+    local roles = self:get('roles', {instance = opts.instance}) or {}
+
+    for _, r in ipairs(roles) do
+        if r == role then
+            return true
+        end
+    end
+    return false
+end
+
 -- Cluster configuration (internal method).
 --
 -- It is given as is after merging from all the configuration
