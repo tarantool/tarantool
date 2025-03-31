c24a296ef4344e3d4304d2bbd837e04a5379d484
Nikolay Shirokovskiy <nshirokovskiy@tarantool.org>
2025-03-31 13:05:02 +0300

    box: flush WAL queue on sync
    `wal_sync()` is expected to flush all prepared txns on disk. Currently
    it is not so because we do not flush txns waiting in WAL queue. AFAIU
    the issue exists for all the places where we use `wal_sync()` (except
    for `box_wait_limbo_acked()` where we already flush WAL queue). The test
    is only added for the vinyl index build as there are explicit tickets for
    that.
    
    Closes #11118
    Closes #11119
    
    NO_DOC=bugfix
    
    (cherry picked from commit af5f033a0405e87941c115848afe500fb01e5596)

--- src/box/wal.c
+++ src/box/wal.c
@@ -667,6 +667,7 @@ wal_sync(struct vclock *vclock)
 		diag_set(ClientError, ER_CASCADE_ROLLBACK);
 		return -1;
 	}
+	journal_queue_flush();
 	struct wal_vclock_msg msg;
 	int rc = cbus_call(&writer->wal_pipe, &writer->tx_prio_pipe, &msg.base,
 			   wal_sync_f);
