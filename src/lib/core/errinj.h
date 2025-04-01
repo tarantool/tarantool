a94b7f1c636dab1e39328ecd296d70693ba9199f
Nikita Zheleztsov <n.zheleztsov@proton.me>
2025-04-01 18:25:33 +0300

    relay: fix crash during shut down
    In relay there's a message, which travels between relay and tx
    threads and which is used to deliver vclock of the relay to tx
    thread.
    
    However, when the message is in tx thread, it may happen, that
    relay thread dies and tx may try to send message back without
    checking, whether the thread is alive.
    
    Let's check `is_paired` flag, which is set to `false`, when
    relay thread dies, before sending the cbus message.
    
    Closes #9920
    
    NO_DOC=bugfix
    
    (cherry picked from commit be6fd7d4d0d78653945354a39966a3ff7edd12c8)

--- src/lib/core/errinj.h
+++ src/lib/core/errinj.h
@@ -124,6 +124,7 @@ struct errinj {
 	_(ERRINJ_RELAY_REPORT_INTERVAL, ERRINJ_DOUBLE, {.dparam = 0}) \
 	_(ERRINJ_RELAY_SEND_DELAY, ERRINJ_BOOL, {.bparam = false}) \
 	_(ERRINJ_RELAY_TIMEOUT, ERRINJ_DOUBLE, {.dparam = 0}) \
+	_(ERRINJ_RELAY_TX_STATUS_UPDATE_DELAY, ERRINJ_BOOL, {.bparam = false}) \
 	_(ERRINJ_RELAY_WAL_START_DELAY, ERRINJ_BOOL, {.bparam = false}) \
 	_(ERRINJ_RELAY_READ_ACK_DELAY, ERRINJ_BOOL, {.bparam = false}) \
 	_(ERRINJ_REPLICASET_VCLOCK, ERRINJ_BOOL, {.bparam = false}) \
