tarantool/tarantool

commit 6d7437dae2dfc4e38e6f3b3ab20e9d5af423c414

say: Add several loggers support
* Add struct log
* Add possibility to add logger configuration to global scope
* Refactor functions in say to use them with specified config,
 not only global variable

 This patch was inspired by need of additional logger in audit log
 Relates #2912

test/unit/say.c
@@ -43,13 +43,30 @@ parse_syslog_opts(const char *input)
 	return 0;
 }
 
+static int
+format_func_custom(struct log *log, char *buf, int len, int level,
+		   const char *filename, int line, const char *error,
+		   const char *format, va_list ap)
+{
+	int total = 0;
+	(void) log;
+	(void) level;
+	(void) filename;
+	(void) line;
+	(void) error;
+	SNPRINT(total, snprintf, buf, len, "\"msg\" = \"");
+	SNPRINT(total, vsnprintf, buf, len, format, ap);
+	SNPRINT(total, snprintf, buf, len, "\"\n");
+	return total;
+}
+
 int main()
 {
 	memory_init();
 	fiber_init(fiber_c_invoke);
 	say_logger_init("/dev/null", S_INFO, 0, "plain", 0);
 
-	plan(20);
+	plan(23);
 
 #define PARSE_LOGGER_TYPE(input, rc) \
 	ok(parse_logger_type(input) == rc, "%s", input)
@@ -78,6 +95,38 @@ int main()
 	PARSE_SYSLOG_OPTS("invalid=", -1);
 	PARSE_SYSLOG_OPTS("facility=local1,facility=local2", -1);
 	PARSE_SYSLOG_OPTS("identity=foo,identity=bar", -1);
+	char template[] = "/tmp/tmpdir.XXXXXX";
+	const char *tmp_dir = mkdtemp(template);
+	if (tmp_dir == NULL) {
+		diag("unit/say: failed to create temp dir: %s", strerror(errno));
+		return check_plan();
+	}
+	char tmp_filename[30];
+	sprintf(tmp_filename, "%s/1.log", tmp_dir);
+	struct log test_log;
+	log_create(&test_log, tmp_filename, false);
+	log_set_format(&test_log, say_format_plain);
+	log_say(&test_log, 0, "dumb name", 0, NULL, "hello %s\n", "user");
+	log_set_format(&test_log, say_format_json);
+	log_say(&test_log, 0, "dumb name", 0, NULL, "hello %s", "user");
+	log_set_format(&test_log, format_func_custom);
+	log_say(&test_log, 0, NULL, 0, NULL, "hello %s", "user");
 
+	FILE* fd = fopen(tmp_filename, "r");
+	char *line = NULL;
+	size_t len = 0;
+
+	if (getline(&line, &len, fd) != -1) {
+		ok(strstr(line, "hello user") != NULL, "plain");
+		getline(&line, &len, fd);
+	}
+	if (getline(&line, &len, fd) != -1) {
+		ok(strstr(line, "\"message\": \"hello user\"") != NULL, "json");
+	}
+
+	if (getline(&line, &len, fd) != -1) {
+		ok(strstr(line, "\"msg\" = \"hello user\"") != NULL, "custom");
+	}
+	log_destroy(&test_log);
 	return check_plan();
 }

extra/exports
@@ -65,6 +65,7 @@ mp_decode_float
 
 say_set_log_level
 say_logrotate
+say_set_log_format
 tarantool_uptime
 log_pid
 space_by_id

src/lua/log.lua
@@ -8,7 +8,8 @@ ffi.cdef[[
     say_set_log_level(int new_level);
 
     void
-    say_set_log_format(const char *format_name);
+    say_set_log_format(enum say_format format);
+
 
     extern sayfunc_t _say;
     extern void say_logrotate(int);
@@ -111,7 +112,13 @@ local function log_level(level)
 end
 
 local function log_format(format_name)
-    return ffi.C.say_set_log_format(format_name)
+    if format_name == "json" then
+        ffi.C.say_set_log_format(ffi.C.SF_JSON)
+    elseif format_name == "plain" then
+        ffi.C.say_set_log_format(ffi.C.SF_PLAIN)
+    else
+        error("log_format: expected 'json' or 'plain'")
+    end
 end
 
 local function log_pid()

src/say.h
@@ -36,6 +36,7 @@
 #include <stdarg.h>
 #include <errno.h>
 #include <sys/types.h> /* pid_t */
+#include "small/rlist.h"
 
 #if defined(__cplusplus)
 extern "C" {
@@ -65,7 +66,6 @@ enum say_format {
 };
 
 extern int log_level;
-extern int log_format;
 
 static inline bool
 say_log_level_is_enabled(int level)
@@ -75,17 +75,89 @@ say_log_level_is_enabled(int level)
 
 /** \endcond public */
 
+extern enum say_format log_format;
+
+enum say_logger_type {
+	SAY_LOGGER_BOOT,
+	SAY_LOGGER_STDERR,
+	SAY_LOGGER_FILE,
+	SAY_LOGGER_PIPE,
+	SAY_LOGGER_SYSLOG
+};
+
+struct log;
+
+typedef int (*log_format_func_t)(struct log *log, char *buf, int len, int level,
+				 const char *filename, int line, const char *error,
+				 const char *format, va_list ap);
+
+struct log {
+	int fd;
+	int level;
+	enum say_logger_type type;
+	char *path;
+	bool nonblock;
+	log_format_func_t format_func;
+	pid_t pid;
+	/* Application identifier used to group syslog messages. */
+	char *syslog_ident;
+	struct rlist in_log_list;
+};
+
+/**
+ * Creates new logger configuration
+ * @param log - logger struct to initialize
+ * @param init_str - box.cfg log option
+ * @param nonblock - box.cfg non-block option
+ * @return 0 on success, -1 on system error, error is written in diag
+ */
+int
+log_create(struct log *log, const char *init_str, bool nonblock);
+
+void
+log_destroy(struct log *log);
+
+/*
+ * Performs log write
+ */
+int
+log_say(struct log *log, int level, const char *filename,
+	int line, const char *error, const char *format, ...);
+
 /**
  * Set log level
+ * Can be used dynamically
+ * @param cfg config of logger
+ * @param format constant level
+ */
+void
+log_set_level(struct log *log, int level);
+
+/**
+ * Set log format
+ * Can be used dynamically
+ * @param log config of logger
+ * @param format_func function formatting log
+ */
+void
+log_set_format(struct log *log, log_format_func_t format_func);
+
+/**
+ * Set log level for default logger
+ * Can be used dynamically
+ * @param format constant level
  */
 void
 say_set_log_level(int new_level);
 
 /**
- * Set log format
+ * Set log format for default logger
+ * Can be used dynamically
+ * Can't be applied in case syslog or boot (will be ignored)
+ * @param say format
  */
 void
-say_set_log_format(enum say_format new_format);
+say_set_log_format(enum say_format format);
 
 /**
  * Return say format by name.
@@ -99,12 +171,14 @@ say_format_by_name(const char *format);
 void
 say_logrotate(int /* signo */);
 
-/* Init logger. */
-void say_logger_init(const char *init_str,
-                     int log_level, int nonblock,
-					 const char *log_format,
-					 int background);
+/* Init default logger. */
+void
+say_logger_init(const char *init_str,
+		int log_level, int nonblock,
+		const char *log_format,
+		int background);
 
+/* Free default logger */
 void
 say_logger_free();
 
@@ -179,6 +253,62 @@ CFORMAT(printf, 5, 0) extern sayfunc_t _say;
 #define panic_syserror(...)		({ say(S_FATAL, strerror(errno), __VA_ARGS__); exit(EXIT_FAILURE); })
 
 /**
+ * Format and print a message to Tarantool log file.
+ *
+ * \param log (struct log *) - logger object
+ * \param level (int) - log level (see enum \link say_level \endlink)
+ * \param format (const char * ) - printf()-like format string
+ * \param ... - format arguments
+ * \sa printf()
+ * \sa enum say_level
+ */
+#define log_say_level(log, _level, format, ...) ({ 	\
+	if (_level <= log->level) 			\
+		log_say(log, _level, __FILE__, __LINE__,\
+		format, ##__VA_ARGS__); })
+
+
+/**
+ * Format and print a message to specified logger.
+ *
+ * \param log (struct log *) - logger object
+ * \param format (const char * ) - printf()-like format string
+ * \param ... - format arguments
+ * \sa printf()
+ * \sa enum say_level
+ * Example:
+ * \code
+ * log_say_info("Some useful information: %s", status);
+ * \endcode
+ */
+#define log_say_error(log, format, ...) \
+	log_say_level(log, S_ERROR, NULL, format, ##__VA_ARGS__)
+
+/** \copydoc log_say_error() */
+#define log_say_crit(log, format, ...) \
+	log_say_level(log, S_CRIT, NULL, format, ##__VA_ARGS__)
+
+/** \copydoc log_say_error() */
+#define log_say_warn(log, format, ...) \
+	log_say_level(log, S_WARN, NULL, format, ##__VA_ARGS__)
+
+/** \copydoc log_say_error() */
+#define log_say_info(log, format, ...) \
+	log_say_level(log, S_INFO, NULL, format, ##__VA_ARGS__)
+
+/** \copydoc log_say_error() */
+#define log_say_verbose(log, format, ...) \
+	log_say_level(log, S_VERBOSE, NULL, format, ##__VA_ARGS__)
+
+/** \copydoc log_say_error() */
+#define log_say_debug(log, format, ...) \
+	log_say_level(log, S_DEBUG, NULL, format, ##__VA_ARGS__)
+
+/** \copydoc log_say_error(). */
+#define log_say_syserror(log, format, ...) \
+	log_say_level(log, S_SYSERROR, strerror(errno), format, ##__VA_ARGS__)
+
+/**
  * validates logger init string;
  * @returns 0 if validation passed or -1
  *           with an error message written to diag
@@ -187,14 +317,6 @@ int
 say_check_init_str(const char *str);
 
 /* internals, for unit testing */
-
-enum say_logger_type {
-	SAY_LOGGER_BOOT,
-	SAY_LOGGER_STDERR,
-	SAY_LOGGER_FILE,
-	SAY_LOGGER_PIPE,
-	SAY_LOGGER_SYSLOG
-};
 
 /**
  * Determine logger type and strip type prefix from init_str.
@@ -224,6 +346,28 @@ say_parse_syslog_opts(const char *init_str,
 /** Release memory allocated by the option parser. */
 void
 say_free_syslog_opts(struct say_syslog_opts *opts);
+
+/**
+ * Format functions
+ * @param log logger structure
+ * @param buf buffer where the formatted message should be written to
+ * @param len size of buffer
+ * @param level log level of message
+ * @param filename name of file where log was called
+ * @param line name of file where log was called
+ * @param error error in case of system errors
+ * @param format format of message
+ * @param ap message parameters
+ * @return number of bytes written to buf
+ */
+int
+say_format_json(struct log *log, char *buf, int len, int level,
+		const char *filename, int line, const char *error,
+		const char *format, va_list ap);
+int
+say_format_plain(struct log *log, char *buf, int len, int level,
+		 const char *filename, int line, const char *error,
+		 const char *format, va_list ap);
 
 #if defined(__cplusplus)
 } /* extern "C" */

test/app-tap/logger.test.lua
@@ -1,7 +1,7 @@
 #!/usr/bin/env tarantool
 
 local test = require('tap').test('log')
-test:plan(20)
+test:plan(22)
 
 --
 -- Check that Tarantool creates ADMIN session for #! script
@@ -95,7 +95,12 @@ log.info("json")
 local line = file:read()
 message = json.decode(line)
 test:is(message.message, "json", "check message with internal key word")
+log.log_format("plain")
+log.info("hello")
+line = file:read()
+test:ok(not line:match("{"), "log change format")
+s, e = pcall(log.log_format, "non_format")
+test:ok(not s)
 file:close()
-
 test:check()
 os.exit()

test/unit/say.result
@@ -1,4 +1,4 @@
-1..20
+1..23
 # type: file
 # next: 
 ok 1 - 
@@ -47,3 +47,6 @@ ok 17 - facility=foo,identity=bar
 ok 18 - invalid=
 ok 19 - facility=local1,facility=local2
 ok 20 - identity=foo,identity=bar
+ok 21 - plain
+ok 22 - json
+ok 23 - custom

commit 02b465b064af0dedc79175005b3f16100bc4dadb

say: do not close stderr at exit
We must not close stderr because it's used for reporting crashes and
memory leaks, but if the logger is configured to log to stderr, it's
closed at exit. Let's fix it.

While we are at it, let's also remove usage of `pm_atomic_store()` and
`pm_atomic_load()` for updating `log::type` because it makes no sense.

The bug was introduced by commit 6d7437dae2dfc ("say: Add several
loggers support").

Close #11323

NO_DOC=bug fix
NO_CHANGELOG=relevant only to developers

src/lib/core/errinj.h
@@ -196,6 +196,7 @@ struct errinj {
 	_(ERRINJ_WAL_WRITE_DISK, ERRINJ_BOOL, {.bparam = false}) \
 	_(ERRINJ_WAL_WRITE_EOF, ERRINJ_BOOL, {.bparam = false}) \
 	_(ERRINJ_WAL_WRITE_PARTIAL, ERRINJ_INT, {.iparam = -1}) \
+	_(ERRINJ_WRITE_EXIT_CODE_TO_STDERR, ERRINJ_BOOL, {.bparam = false}) \
 	_(ERRINJ_XLOG_GARBAGE, ERRINJ_BOOL, {.bparam = false}) \
 	_(ERRINJ_XLOG_META, ERRINJ_BOOL, {.bparam = false}) \
 	_(ERRINJ_XLOG_READ, ERRINJ_INT, {.iparam = -1}) \

src/lib/core/say.c
@@ -315,7 +315,7 @@ log_set_nonblock(struct log *log)
 static int
 log_rotate(struct log *log)
 {
-	if (pm_atomic_load(&log->type) != SAY_LOGGER_FILE)
+	if (log->type != SAY_LOGGER_FILE)
 		return 0;
 
 	ERROR_INJECT(ERRINJ_LOG_ROTATE, { usleep(10); });
@@ -1398,9 +1398,11 @@ log_destroy(struct log *log)
 	while(log->rotating_threads > 0)
 		tt_pthread_cond_wait(&log->rotate_cond, &log->rotate_mutex);
 	tt_pthread_mutex_unlock(&log->rotate_mutex);
-	pm_atomic_store(&log->type, SAY_LOGGER_BOOT);
-
-	if (log->fd != -1)
+	/*
+	 * Do not close stderr because it's used for reporting crashes
+	 * and memory leaks.
+	 */
+	if (log->type != SAY_LOGGER_STDERR)
 		close(log->fd);
 	free(log->syslog_ident);
 	free(log->path);

src/main.cc
@@ -1104,5 +1104,8 @@ main(int argc, char **argv)
 	if (!shutdown_finished)
 		ev_run(loop(), 0);
 	tarantool_free();
+	ERROR_INJECT(ERRINJ_WRITE_EXIT_CODE_TO_STDERR, {
+		fprintf(stderr, "Tarantool exited with code %d\n", exit_code);
+	});
 	return exit_code;
 }

test/app-luatest/gh_11323_write_to_stderr_at_exit_test.lua
@@ -1,0 +1,25 @@
+local popen = require('popen')
+local t = require('luatest')
+
+local g = t.group()
+
+g.test_write_to_stderr_at_exit = function()
+    t.tarantool.skip_if_not_debug()
+    local ph = popen.new({
+        arg[-1], '-e', [[
+            box.error.injection.set('ERRINJ_WRITE_EXIT_CODE_TO_STDERR', true)
+            require('log').cfg()
+            os.exit(42)
+        ]]
+    }, {
+        stderr = popen.opts.PIPE,
+    })
+    local output = {}
+    repeat
+        local s = ph:read({stderr = true})
+        table.insert(output, s)
+    until s == ''
+    output = table.concat(output)
+    ph:close()
+    t.assert_str_contains(output, 'Tarantool exited with code 42')
+end