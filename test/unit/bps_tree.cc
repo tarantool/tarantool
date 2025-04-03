tarantool/tarantool

commit 51c56d9b3319200940b3b3174de905849b565dd3

salad: reserve extents for matras_touch on BPS tree operations
Prior to this patch it was possible for the BPS tree to crash if one of
`matras_touch` calls is failed: the result of the function call was not
checked, because it was assumed the amount of extents we reserve prior
to an operation in the memtx engine will be enough for the functions.

Let's make the tree self-sufficient and reserve memory for `matras_touch`
calls prior to insertion and deletion.

NO_DOC=internal
NO_TEST=internal
NO_CHANGELOG=internal

src/lib/salad/bps_tree.h
@@ -817,7 +817,8 @@ bps_tree_insert_get_offset(struct bps_tree *tree, bps_tree_elem_t new_elem,
  * @brief Delete an element from a tree.
  * @param tree - pointer to a tree
  * @param elem - the element tot delete
- * @return - 0 on success or -1 if the element was not found in tree
+ * @return - 0 on success or -1 if the element was not
+ *  found in tree or a memory allocation has failed
  */
 static inline int
 bps_tree_delete(struct bps_tree *tree, bps_tree_elem_t elem);
@@ -841,7 +842,7 @@ bps_tree_delete_get_offset(struct bps_tree *tree, bps_tree_elem_t elem,
  * @param tree - pointer to a tree
  * @param elem - the element tot delete
  * @return - 0 on success or -1 if the element was not found in the
- *  tree or is not identical.
+ *  tree or is not identical or a memory allocation has failed.
  */
 static inline int
 bps_tree_delete_value(struct bps_tree *tree, bps_tree_elem_t elem,
@@ -3204,6 +3205,8 @@ bps_tree_create_leaf(struct bps_tree_common *tree, bps_tree_block_id_t *id)
 			       bps_tree_garbage_pop(tree, id);
 	if (!res)
 		res = (struct bps_leaf *)matras_alloc(tree->matras, id);
+	if (!res)
+		return NULL;
 	res->header.type = BPS_TREE_BT_LEAF;
 	tree->leaf_count++;
 	return res;
@@ -3217,8 +3220,7 @@ bps_tree_create_inner(struct bps_tree_common *tree, bps_tree_block_id_t *id)
 {
 	struct bps_inner *res = (struct bps_inner *)
 				bps_tree_garbage_pop(tree, id);
-	if (!res)
-		res = (struct bps_inner *)matras_alloc(tree->matras, id);
+	assert(res != NULL); /* We must have reserved blocks. */
 	res->header.type = BPS_TREE_BT_INNER;
 	tree->inner_count++;
 	return res;
@@ -4753,13 +4755,9 @@ bps_tree_process_insert_leaf(struct bps_tree_common *tree,
 		}
 	}
 
-	if (!bps_tree_reserve_blocks(tree, tree->depth + 1)) {
-		*inserted_in_block = (bps_tree_block_id_t)(-1);
-		*inserted_in_pos = 0;
-		return -1;
-	}
 	bps_tree_block_id_t new_block_id = (bps_tree_block_id_t)(-1);
 	struct bps_leaf *new_leaf = bps_tree_create_leaf(tree, &new_block_id);
+	assert(new_leaf != NULL); /* We've reserved blocks in the caller. */
 
 	bps_tree_touch_leaf(tree, leaf_path_elem);
 
@@ -5825,6 +5823,14 @@ bps_tree_insert_impl(struct bps_tree *t, bps_tree_elem_t new_elem,
 		return rc;
 	}
 
+	/* Reserve max blocks to create (1 per level + new root). */
+	if (!bps_tree_reserve_blocks(tree, tree->depth + 1))
+		return -1;
+
+	/* Reserve space for max blocks to touch: root + 3 per next level. */
+	if (matras_touch_reserve(tree->matras, 1 + (tree->depth - 1) * 3) != 0)
+		return -1;
+
 	struct bps_inner_path_elem path[BPS_TREE_MAX_DEPTH];
 	struct bps_leaf_path_elem leaf_path_elem;
 	bool exact;
@@ -5907,7 +5913,8 @@ bps_tree_insert_get_offset(struct bps_tree *t, bps_tree_elem_t new_elem,
  * @param[out] deleted_elem - optional pointer to the deleted value, left
  *  intact if no element deleted.
  * @param[out] offset - optional pointer to the offset to the element.
- * @return - true on success or false if the element was not found.
+ * @return - 0 on success or -1 if the element was not found or a
+ *  memory allocation has failed.
  */
 static ALWAYS_INLINE int
 bps_tree_delete_impl(struct bps_tree *t, bps_tree_elem_t elem,
@@ -5916,6 +5923,10 @@ bps_tree_delete_impl(struct bps_tree *t, bps_tree_elem_t elem,
 {
 	struct bps_tree_common *tree = &t->common;
 	if (tree->root_id == (bps_tree_block_id_t)(-1))
+		return -1;
+
+	/* Reserve space for max blocks to touch: root + 3 per next level. */
+	if (matras_touch_reserve(tree->matras, 1 + (tree->depth - 1) * 3) != 0)
 		return -1;
 
 	struct bps_inner_path_elem path[BPS_TREE_MAX_DEPTH];

test/unit/bps_tree.cc
@@ -931,7 +931,7 @@ main(void)
 	printing_test();
 	white_box_test();
 	approximate_count();
-	ok(extents_count == 0, "memory leak check");
+	ok(extents_count == allocator.num_reserved_extents, "leak check");
 	insert_get_iterator();
 	delete_value_check();
 	insert_successor_test();

test/unit/bps_tree_iterator.cc
@@ -504,7 +504,8 @@ main(void)
 	iterator_check();
 	iterator_invalidate_check();
 	iterator_freeze_check();
-	ok(total_extents_allocated == 0, "memory leak check");
+	ok(total_extents_allocated == allocator.num_reserved_extents,
+	   "leak check");
 
 	matras_allocator_destroy(&allocator);
 

test/unit/bps_tree_offset_api.cc
@@ -398,45 +398,53 @@ insert_delete_get_offset()
 	for (int i = 0; i < count; i++)
 		insert_and_check(&tree, i, (size_t)i);
 	fail_unless(test_size(&tree) == (size_t)count);
-	fail_unless((int)stats.extent_count == extent_count);
+	fail_unless((int)stats.extent_count ==
+		    extent_count - allocator.num_reserved_extents);
 	for (int i = 0; i < count; i++)
 		delete_and_check(&tree, i, 0);
 	fail_unless(test_size(&tree) == 0);
-	fail_unless((int)stats.extent_count == extent_count);
+	fail_unless((int)stats.extent_count ==
+		    extent_count - allocator.num_reserved_extents);
 	ok(true, "Insert 1..X, delete 1..X");
 
 	for (int i = 0; i < count; i++)
 		insert_and_check(&tree, i, (size_t)i);
 	fail_unless(test_size(&tree) == (size_t)count);
-	fail_unless((int)stats.extent_count == extent_count);
+	fail_unless((int)stats.extent_count ==
+		    extent_count - allocator.num_reserved_extents);
 	for (int i = count - 1; i >= 0; i--)
 		delete_and_check(&tree, i, (size_t)i);
 	fail_unless(test_size(&tree) == 0);
-	fail_unless((int)stats.extent_count == extent_count);
+	fail_unless((int)stats.extent_count ==
+		    extent_count - allocator.num_reserved_extents);
 	ok(true, "Insert 1..X, delete X..1");
 
 	for (int i = count - 1; i >= 0; i--)
 		insert_and_check(&tree, i, 0);
 	fail_unless(test_size(&tree) == (size_t)count);
-	fail_unless((int)stats.extent_count == extent_count);
+	fail_unless((int)stats.extent_count ==
+		    extent_count - allocator.num_reserved_extents);
 	for (int i = 0; i < count; i++)
 		delete_and_check(&tree, i, 0);
 	fail_unless(test_size(&tree) == 0);
-	fail_unless((int)stats.extent_count == extent_count);
+	fail_unless((int)stats.extent_count ==
+		    extent_count - allocator.num_reserved_extents);
 	ok(true, "Insert X..1, delete 1..X");
 
 	for (int i = count - 1; i >= 0; i--)
 		insert_and_check(&tree, i, 0);
 	fail_unless(test_size(&tree) == (size_t)count);
-	fail_unless((int)stats.extent_count == extent_count);
+	fail_unless((int)stats.extent_count ==
+		    extent_count - allocator.num_reserved_extents);
 	for (int i = count - 1; i >= 0; i--)
 		delete_and_check(&tree, i, (size_t)i);
 	fail_unless(test_size(&tree) == 0);
-	fail_unless((int)stats.extent_count == extent_count);
+	fail_unless((int)stats.extent_count ==
+		    extent_count - allocator.num_reserved_extents);
 	ok(true, "Insert X..1, delete X..1");
 
 	test_destroy(&tree);
-	fail_unless(extent_count == 0);
+	fail_unless(extent_count == allocator.num_reserved_extents);
 
 	footer();
 	check_plan();

commit 77828a515968c9d8ac8bb8a029d857c1dbdd25e2

salad: fix crash on insertion into bps tree with OOM conditions
On insertion into bps tree we first reserve blocks for the case we
need to insert new leaf during rebalancing. Next there can be a free
block on last extent in matras but no more free extents in tuple arena.
So `bps_tree_reserve_blocks()` is able to allocate a block but then
`bps_tree_garbage_push()` is called that touches a block. As there no
free extents touch is failed which is unexpected and causes a crash.

We don't need to touch the block on this path as block is newly allocated
and is not shared with any read view. We don't need to touch block on
other paths too (disposing a leaf or a inner node) because we move data
from this node before so it is already should be touched.

So just don't touch the block in `bps_tree_garbage_push()`.

The issue is introduced in the commit 51c56d9b3319 ("salad: reserve
extents for matras_touch on BPS tree operations") and unlocked in the
commit 32b0678a74ab ("memtx: reserve extents in the RTREE index itself")
where we removed crutches where we heuristically reserve memory for
index operations.

Closes #11326

NO_DOC=bugfix

src/lib/salad/bps_tree.h
@@ -3156,7 +3156,6 @@ static inline void
 bps_tree_garbage_push(struct bps_tree_common *tree, struct bps_block *block,
 		      bps_tree_block_id_t id)
 {
-	assert(block); (void) block;
 	bps_tree_block_id_t next_leaf_id = (bps_tree_block_id_t)(-1);
 	bps_tree_block_id_t prev_leaf_id = (bps_tree_block_id_t)(-1);
 	if (block->type == BPS_TREE_BT_LEAF) {
@@ -3165,8 +3164,7 @@ bps_tree_garbage_push(struct bps_tree_common *tree, struct bps_block *block,
 		prev_leaf_id = leaf->prev_id;
 	}
 
-	struct bps_garbage *garbage = (struct bps_garbage *)
-		bps_tree_touch_block(tree, id);
+	struct bps_garbage *garbage = (struct bps_garbage *)block;
 	garbage->header.type = BPS_TREE_BT_GARBAGE;
 	garbage->next_id = tree->garbage_head_id;
 	garbage->next_leaf_id = next_leaf_id;

test/unit/bps_tree.cc
@@ -174,11 +174,14 @@ compare(type_t a, type_t b)
 	return a < b ? -1 : a > b ? 1 : 0;
 }
 static int extents_count = 0;
+static bool extent_alloc_failure = false;
 
 static void *
 extent_alloc(struct matras_allocator *allocator)
 {
 	(void)allocator;
+	if (extent_alloc_failure)
+		return NULL;
 	++extents_count;
 	return xmalloc(BPS_TREE_EXTENT_SIZE);
 }
@@ -920,10 +923,44 @@ insert_successor_test()
 	ok(true, "successor test");
 }
 
+static void
+gh_11326_oom_on_insertion_test()
+{
+	plan(1);
+	header();
+
+	test tree;
+	test_view view;
+	type_t replaced;
+	struct test_iterator iterator;
+
+	test_create(&tree, 0, &allocator, NULL);
+	test_insert(&tree, 0, &replaced, NULL);
+	test_view_create(&view, &tree);
+
+	extent_alloc_failure = true;
+	fail_unless(test_insert(&tree, 1, &replaced, NULL) != 0);
+	debug_check(&tree);
+	fail_unless(test_size(&tree) == 1);
+	iterator = test_first(&tree);
+	type_t *v = test_iterator_get_elem(&tree, &iterator);
+	fail_unless(v != NULL && *v == 0);
+	fail_unless(test_iterator_next(&tree, &iterator) == false);
+	extent_alloc_failure = false;
+
+	test_view_destroy(&view);
+	test_destroy(&tree);
+
+	ok(true, "gh-11326: OOM on insertion test");
+
+	footer();
+	check_plan();
+}
+
 int
 main(void)
 {
-	plan(12);
+	plan(13);
 	header();
 
 	matras_allocator_create(&allocator, BPS_TREE_EXTENT_SIZE,
@@ -942,6 +979,11 @@ main(void)
 	delete_value_check();
 	insert_successor_test();
 
+	matras_allocator_destroy(&allocator);
+
+	matras_allocator_create(&allocator, BPS_TREE_EXTENT_SIZE,
+				extent_alloc, extent_free);
+	gh_11326_oom_on_insertion_test();
 	matras_allocator_destroy(&allocator);
 
 	footer();