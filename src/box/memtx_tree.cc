2ea5984a6fc34ceec07a7a97bf14877b153ad24f
Magomed Kostoev <m.kostoev@tarantool.org>
2025-04-14 05:20:10 +0300

    memtx: introduce the O(n) SK build
    Introduced a new `dump_primary_key` memtx index read view method. It's
    meant to be defined for any index that can be used as a primary one
    (currently TREE and HASH). The function is used in order to dump the
    primary key tuples into the sort data file.
    
    The new method is introduced in order to simplify the new .sortdata
    file creation procedure: this way it can be separated from the main
    .snap creation with smaller performance impact.
    
    Introduced a new `dump_sort_data` memtx index read view method. It's
    meant to be defined for any secondary key that can be sorted using the
    new O(n) algorithm (currently, only TREE index supports it). This one
    has to be an index-specific (along with the following one) in order to
    provide the expected recovery speed-up.
    
    Introduced a new `build_presorted` memtx index method using the sort
    data along with the old2new tuple pointer map created from the primary
    key to provide accelerated recovery.
    
    A new entity introduced: memtx sort data file. This is a regular XLOG
    file containing the data required to perform recovery using the O(n)
    SK sorting algorithm. The file has `.sortdata` extension and garbage
    collected the same way as regular `.snap` files.
    
    The new `memtx_sort_data_enabled` config option controls whether to
    write the sort data file on `box.snapshot()` or use it during recovery.
    The option can be changed dynamically, so user can write checkpoints
    with or without the new data regardless of had the data been used on
    current instance recovery or had not.
    
    But there are conditions when the sort data can not be used during
    recovery:
    0. The space is a system space (these generally don't benefit from the
       new SK sorting algorithm).
    1. The `_index` space has no `before_replace` triggers (so we avoid
       the sort data invalidation of an index because the index had been
       altered by a `before_replace` trigger).
    2. The space itself has no `before_replace` triggers (the new SK sort
       approach does not account this).
    
    Of course, the new approach is also not used during forced recovery.
    
    @TarantoolBot document
    Title: Document the MemTX's new secondary key build approach
    Since: 3.4.1
    
    Now it's possible to sort secondary keys using a new O(n) sorting
    algorithm using an additional data written into the snapshot. The
    feature can be enabled with a new `memtx_sort_data_enabled` option
    in `box.cfg` or `memtx.sort_data_enabled` in the instance config.
    
    With the option set to `true`, additional data is saved into a
    separated `.sortdata` file on snapshot creation and the data is
    used on recovery. The default value is `false`: the behavior has
    to be turned on by the user if required.
    
    The option is changeable in runtime. So one can, for example, enable
    it on recovery to use the new SK sorting approach, but disable it
    before creating a new snapshot, so only `.snap` file will be created
    and the O(n) SK sort will not be available for the snapshot.
    
    The performance impact of the option depends on the persistent
    storage read/write speed and the amount of tuples and secondary
    keys in spaces (the more the better the new approach is). Also,
    the approach only utilizes a single CPU core.
    
    As a downside, it creates an additional footprint overhead on
    recovery time (up to ~45 bytes per tuple) and overhead during
    the snapshot creation (since we have to write the sort data to
    the persistent storage).

--- src/box/memtx_tree.cc
+++ src/box/memtx_tree.cc
@@ -31,6 +31,7 @@
 #include "memtx_tree.h"
 #include "memtx_engine.h"
 #include "memtx_tuple_compression.h"
+#include "memtx_sort_data.h"
 #include "space.h"
 #include "schema.h" /* space_by_id(), space_cache_find() */
 #include "errinj.h"
@@ -43,6 +44,7 @@
 #include "trivia/config.h"
 #include "trivia/util.h"
 #include "tt_sort.h"
+#include "assoc.h"
 #include <small/mempool.h>
 
 /**
@@ -213,6 +215,8 @@ struct memtx_tree_index {
 	memtx_tree_t<USE_HINT> tree;
 	struct memtx_tree_data<USE_HINT> *build_array;
 	size_t build_array_size, build_array_alloc_size;
+	/** True if the MemTX sort data used for the secondary key. */
+	bool use_sk_sort_data;
 	struct memtx_gc_task gc_task;
 	memtx_tree_iterator_t<USE_HINT> gc_iterator;
 	/** Whether index is functional. */
@@ -1959,6 +1963,50 @@ memtx_tree_index_create_iterator(struct index *base, enum iterator_type type,
 		base, type, key, part_count, pos, 0);
 }
 
+/**
+ * Build the index using the O(n) sort algorithm with MemTX sort data.
+ */
+template<bool USE_HINT>
+static int
+memtx_tree_index_build_presorted(struct memtx_tree_index<USE_HINT> *index,
+				 struct memtx_sort_data_reader *msdr)
+{
+	/* Check if can use the sort data. */
+	struct key_def *cmp_def = memtx_tree_cmp_def(&index->tree);
+	if (cmp_def->for_func_index)
+		panic("func indexes aren't expected to be presorted");
+
+	/*
+	 * Load the build array. Note that the names are not canonical: the
+	 * build_array_alloc_size used to be in elements, but it's in bytes.
+	 */
+	int rc = -1;
+	size_t build_array_bsize = memtx_sort_data_reader_get_size(msdr);
+	struct memtx_tree_data<USE_HINT> *build_array =
+		(struct memtx_tree_data<USE_HINT> *)xmalloc(build_array_bsize);
+	size_t build_array_size = build_array_bsize / sizeof(build_array[0]);
+	if (memtx_sort_data_reader_get(msdr, build_array) != 0)
+		goto clean_up; /* Failed to read sort data. */
+
+	/* Fix the tuple pointers. */
+	for (size_t i = 0; i < build_array_size; i++) {
+		struct tuple *old_ptr = build_array[i].tuple;
+		struct tuple *new_ptr =
+			memtx_sort_data_reader_resolve_tuple(msdr, old_ptr);
+		if (new_ptr == NULL)
+			goto clean_up; /* Corrupted sort data. */
+		build_array[i].tuple = new_ptr;
+	}
+
+	/* Build the index. */
+	memtx_tree_build(&index->tree, build_array, build_array_size);
+	rc = 0; /* We're good now. */
+
+clean_up:
+	free(build_array);
+	return rc;
+}
+
 template <bool USE_HINT>
 static void
 memtx_tree_index_begin_build(struct index *base)
@@ -1966,7 +2014,29 @@ memtx_tree_index_begin_build(struct index *base)
 	struct memtx_tree_index<USE_HINT> *index =
 		(struct memtx_tree_index<USE_HINT> *)base;
 	assert(memtx_tree_size(&index->tree) == 0);
-	(void)index;
+
+	/* Check if the SK sort data can be used. */
+	struct memtx_engine *memtx = (struct memtx_engine *)base->engine;
+	uint32_t space_id = base->def->space_id;
+	uint32_t index_id = base->def->iid;
+	bool use_sk_sort_data = false;
+	if (memtx->msdr != NULL && index_id > 0 &&
+	    memtx_sort_data_reader_seek(memtx->msdr, space_id, index_id,
+					&use_sk_sort_data) != 0) {
+		diag_log();
+		panic("Failed to process the SK sort data");
+	}
+	struct space *space = space_by_id(space_id);
+	if (use_sk_sort_data && space_has_before_replace_triggers(space)) {
+		say_info("Skipped sort data for space '%s': the space has "
+			 "before_replace triggers.", space_name(space));
+		use_sk_sort_data = false;
+	}
+	index->use_sk_sort_data = use_sk_sort_data;
+	if (index->use_sk_sort_data) {
+		say_info("Using MemTX sort data for building index '%s' of "
+			 "space '%s'", base->def->name, space_name(space));
+	}
 }
 
 template <bool USE_HINT>
@@ -2033,10 +2103,27 @@ template <bool USE_HINT>
 static int
 memtx_tree_index_build_next(struct index *base, struct tuple *tuple)
 {
-	if (tuple_key_is_excluded(tuple, base->def->key_def, MULTIKEY_NONE))
-		return 0;
+	/* Skip if we use the MemTX sort data for SK build. */
 	struct memtx_tree_index<USE_HINT> *index =
 		(struct memtx_tree_index<USE_HINT> *)base;
+	if (index->use_sk_sort_data) {
+		assert(base->def->iid > 0);
+		return 0;
+	}
+
+	if (tuple_key_is_excluded(tuple, base->def->key_def, MULTIKEY_NONE))
+		return 0;
+
+	/* Deal with the PK sort data if required. */
+	struct memtx_engine *memtx = (struct memtx_engine *)base->engine;
+	if (memtx->msdr != NULL && base->def->iid == 0) {
+		uint32_t space_id = base->def->space_id;
+		bool is_first = index->build_array == NULL;
+		if (memtx_sort_data_reader_pk_add_tuple(memtx->msdr, space_id,
+							is_first, tuple) != 0)
+			return -1;
+	}
+
 	struct key_def *cmp_def = memtx_tree_cmp_def(&index->tree);
 	return memtx_tree_index_build_array_append(index, tuple,
 						   tuple_hint(tuple, cmp_def));
@@ -2146,10 +2233,25 @@ template <bool USE_HINT>
 static void
 memtx_tree_index_end_build(struct index *base)
 {
+	/* Build using the SK sort data if required. */
 	struct memtx_tree_index<USE_HINT> *index =
 		(struct memtx_tree_index<USE_HINT> *)base;
-	struct key_def *cmp_def = memtx_tree_cmp_def(&index->tree);
 	struct memtx_engine *memtx = (struct memtx_engine *)base->engine;
+	if (memtx->msdr != NULL && index->use_sk_sort_data) {
+		assert(base->def->iid > 0);
+		memtx_tree_index_build_presorted(index, memtx->msdr);
+		return;
+	}
+
+	/* Check the PK sort data if required. */
+	uint32_t space_id = base->def->space_id;
+	if (memtx->msdr != NULL && base->def->iid == 0 &&
+	    memtx_sort_data_reader_pk_check(memtx->msdr, space_id) != 0) {
+		diag_log();
+		panic("Failed to process the PK sort data");
+	}
+
+	struct key_def *cmp_def = memtx_tree_cmp_def(&index->tree);
 	tt_sort(index->build_array, index->build_array_size,
 		sizeof(index->build_array[0]), memtx_tree_qcompare<USE_HINT>,
 		cmp_def, memtx->sort_threads);
@@ -2183,6 +2285,8 @@ struct tree_read_view {
 	memtx_tree_view_t<USE_HINT> tree_view;
 	/** Used for clarifying read view tuples. */
 	struct memtx_tx_snapshot_cleaner cleaner;
+	/** Used for dumping into the sort data file. */
+	memtx_tree_iterator_t<USE_HINT> dump_iterator;
 };
 
 /** Read view iterator implementation. */
@@ -2276,8 +2380,10 @@ tree_read_view_iterator_next_raw(struct index_read_view_iterator *iterator,
 		if (memtx_prepare_read_view_tuple(res->tuple, &rv->base,
 						  &rv->cleaner, result) != 0)
 			return -1;
-		if (result->data != NULL)
+		if (result->data != NULL) {
+			result->ptr = res->tuple;
 			return 0;
+		}
 	}
 }
 
@@ -2410,11 +2516,66 @@ memtx_tree_index_create_read_view(struct index *base)
 	struct space *space = space_by_id(base->def->space_id);
 	assert(space != NULL);
 	memtx_tx_snapshot_cleaner_create(&rv->cleaner, space, base);
+	invalidate_tree_iterator(&rv->dump_iterator);
 	rv->index = index;
 	index_ref(base);
 	memtx_tree_view_create(&rv->tree_view, &index->tree);
 	tree_read_view_reset_key_def(rv);
 	return (struct index_read_view *)rv;
+}
+
+/** Dump the tree data into the sort data file. */
+template<bool USE_HINT>
+static int
+memtx_tree_index_read_view_dump_data(
+	struct index_read_view *base, ssize_t tuple_count,
+	struct memtx_sort_data *msd, bool *have_more)
+{
+	/* Collect the data to save. */
+	struct tree_read_view<USE_HINT> *rv =
+		(struct tree_read_view<USE_HINT> *)base;
+	struct memtx_tree_data<USE_HINT> *buffer =
+		(struct memtx_tree_data<USE_HINT> *)
+			xcalloc(tuple_count, sizeof(*buffer));
+	ssize_t dumped = 0;
+	while (dumped != tuple_count &&
+	       memtx_tree_view_iterator_next(&rv->tree_view,
+					     &rv->dump_iterator)) {
+		struct memtx_tree_data<USE_HINT> *data =
+			memtx_tree_view_iterator_get_elem(
+				&rv->tree_view, &rv->dump_iterator);
+		if (memtx_tx_snapshot_clarify(&rv->cleaner,
+					      data->tuple) == NULL)
+			continue; /* Only dump visible data. */
+		buffer[dumped++] = *data;
+	}
+
+	/* Write the collected data to the sort data file. */
+	if (memtx_sort_data_writer_put(msd, buffer, sizeof(*buffer),
+				       dumped) != 0) {
+		free(buffer);
+		return -1;
+	}
+
+	*have_more = !memtx_tree_iterator_is_invalid(&rv->dump_iterator);
+	free(buffer);
+	return 0;
+}
+
+int
+memtx_tree_index_read_view_dump_sort_data(
+	struct index_read_view *base, ssize_t tuple_count,
+	struct memtx_sort_data *msd, bool *have_more)
+{
+	struct key_def *key_def = base->def->key_def;
+	assert(!key_def->for_func_index);
+	if (base->def->opts.hint == INDEX_HINT_ON || key_def->is_multikey) {
+		return memtx_tree_index_read_view_dump_data<true>(
+			base, tuple_count, msd, have_more);
+	} else {
+		return memtx_tree_index_read_view_dump_data<false>(
+			base, tuple_count, msd, have_more);
+	}
 }
 
 /**
