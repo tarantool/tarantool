45dfe36a572dcb0525f4ccdb442450a621111ec4
Vladimir Davydov <vdavydov@tarantool.org>
2025-05-16 11:19:30 +0300

    perf: generate data for column_scan test with batch insertion
    Batch insertion is currently the only way to append data to quiver.
    Also, it works much faster than point insertion in memcs. So let's
    use it for memcs and quiver spaces.
    
    NO_DOC=perf test
    NO_TEST=perf test
    NO_CHANGELOG=perf test

--- perf/lua/column_scan_module.c
+++ perf/lua/column_scan_module.c
@@ -9,9 +9,9 @@
 #include "trivia/util.h"
 #include "arrow/abi.h"
 
-#if defined(ENABLE_MEMCS_ENGINE)
+#if defined(ENABLE_MEMCS_ENGINE) || defined(ENABLE_QUIVER_ENGINE)
 # define ENABLE_ARROW 1
-#endif /* ENABLE_MEMCS_ENGINE */
+#endif /* ENABLE_MEMCS_ENGINE || ENABLE_QUIVER_ENGINE */
 
 #if defined(ENABLE_READ_VIEW)
 static box_raw_read_view_t *rv;
@@ -26,6 +26,117 @@ init_rv(struct lua_State *L)
 	}
 }
 #endif /* ENABLE_READ_VIEW */
+
+#if defined(ENABLE_ARROW)
+static void
+arrow_schema_destroy(struct ArrowSchema *schema)
+{
+	for (int i = 0; i < schema->n_children; i++) {
+		struct ArrowSchema *child = schema->children[i];
+		child->release(child);
+		free(child);
+	}
+	free(schema->children);
+	free((void *)schema->name);
+	schema->release = NULL;
+}
+
+static void
+arrow_schema_create(struct ArrowSchema *schema, int column_count)
+{
+	*schema = (struct ArrowSchema) {
+		.format = "+s",
+		.n_children = column_count,
+		.children = xmalloc(sizeof(struct ArrowSchema *) *
+				    column_count),
+		.release = arrow_schema_destroy,
+	};
+	for (int i = 0; i < column_count; i++) {
+		char name[32];
+		snprintf(name, sizeof(name), "field_%d", i + 1);
+		schema->children[i] = xmalloc(sizeof(*schema->children[i]));
+		*schema->children[i] = (struct ArrowSchema) {
+			.format = "L",
+			.name = xstrdup(name),
+			.release = arrow_schema_destroy,
+		};
+	};
+}
+
+static void
+arrow_array_destroy(struct ArrowArray *array)
+{
+	for (int i = 0; i < array->n_children; i++) {
+		struct ArrowArray *child = array->children[i];
+		child->release(child);
+		free(child);
+	}
+	free(array->children);
+	for (int i = 0; i < array->n_buffers; i++)
+		free((void *)array->buffers[i]);
+	free(array->buffers);
+	array->release = NULL;
+}
+
+static void
+arrow_array_create(struct ArrowArray *array, int column_count,
+		   int row_count, int row_offset, int total_row_count)
+{
+	*array = (struct ArrowArray) {
+		.length = row_count,
+		.n_buffers = 1,
+		.n_children = column_count,
+		.buffers = xcalloc(1, sizeof(void *)),
+		.children = xmalloc(sizeof(struct ArrowArray *) * column_count),
+		.release = arrow_array_destroy,
+	};
+	for (int i = 0; i < column_count; i++) {
+		array->children[i] = xmalloc(sizeof(*array->children[i]));
+		*array->children[i] = (struct ArrowArray) {
+			.length = row_count,
+			.n_buffers = 2,
+			.buffers = xcalloc(2, sizeof(void *)),
+			.release = arrow_array_destroy,
+		};
+		uint64_t *data = xaligned_alloc(row_count * 8, 64);
+		for (int j = 0; j < row_count; j++) {
+			int val = row_offset + j + 1;
+			if (i % 2 == 1)
+				val = total_row_count - val + 1;
+			assert(val > 0);
+			data[j] = (uint64_t)val;
+		}
+		array->children[i]->buffers[1] = data;
+	};
+}
+
+static int
+gen_arrow_lua_func(struct lua_State *L)
+{
+	uint32_t space_id = luaL_checkinteger(L, 1);
+	int column_count = luaL_checkinteger(L, 2);
+	int row_count = luaL_checkinteger(L, 3);
+	int pct_complete = 0;
+	say_info("Generating the test data set...");
+	for (int row_offset = 0; row_offset < row_count; ) {
+		int batch_row_count = MIN(1000, row_count - row_offset);
+		struct ArrowSchema schema;
+		arrow_schema_create(&schema, column_count);
+		struct ArrowArray array;
+		arrow_array_create(&array, column_count, batch_row_count,
+				   row_offset, row_count);
+		if (box_insert_arrow(space_id, &array, &schema) != 0)
+			return luaT_error(L);
+		row_offset += batch_row_count;
+		int pct = 100 * row_offset / row_count;
+		if (pct != pct_complete) {
+			say_info("%d%% complete", pct);
+			pct_complete = pct;
+		}
+	}
+	return 0;
+}
+#endif /* defined(ENABLE_ARROW) */
 
 static int
 sum_iterator_lua_func(struct lua_State *L)
@@ -206,6 +317,9 @@ LUA_API int
 luaopen_column_scan_module(struct lua_State *L)
 {
 	static const struct luaL_Reg lib[] = {
+#if defined(ENABLE_ARROW)
+		{"gen_arrow", gen_arrow_lua_func},
+#endif /* defined(ENABLE_ARROW) */
 		{"sum_iterator", sum_iterator_lua_func},
 #if defined(ENABLE_READ_VIEW)
 		{"sum_iterator_rv", sum_iterator_rv_lua_func},
