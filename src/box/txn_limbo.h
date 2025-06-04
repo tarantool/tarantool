4742795e08deee77628de396b503d2c488544c32
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2025-06-04 19:34:30 +0200

    limbo: handle spurious wakeups on space waiting
    The limbo has a size limit. Only the entries of total sum size not
    exceeding the limit are allowed to be persisted, replicated, and
    waiting for acks at any time.
    
    The entries beyond that limit must stay volatile and wait for
    space to appear.
    
    The difference between volatile and submitted entries is that the
    volatile ones can be rolled back without any action in WAL. Since
    they aren't written there anyway.
    
    There was a bug in the waiting for limbo space. The waiters
    wouldn't form any queue with a specific order. As a result with
    some spurious wakeups it was possible to do box.commit() of
    different transactions in one order, but get them actually written
    to journal and committed in another order.
    
    Another issue but very closely related was that if some txns go
    to wait for the limbo space, and the non-last one is rolled back
    (due to fiber cancellation, for example), then it wouldn't
    cascading rollback the newer txns. This could result in data being
    corrupted even locally, without any replication.
    
    And the third issue is coming directly out of the second one -
    the rollbacks of the txns waiting for the limbo space were not
    visible anywhere. As a result it was possible on join of a new
    replica to create a read-view with some data from a txn waiting
    for space in the limbo, roll that txn back, and the join-process
    wouldn't notice. Then the replica would get a snapshot with already
    rolled back data.
    
    The patch addresses that:
    - All entries go to the limbo, regardless of the limbo's current
        size.
    - The volatile entries, when don't fit into the limbo by size,
        remain volatile and block the current fiber. Those entries'
        sizes aren't accounted in the limbo.
    - When a volatile entry is rolled back, it directly has access to
        all the newer volatile entries too, and cascade-rolls them
        back as well.
    - Since even the volatile txns are in the limbo, all the code
        trying to "wait for limbo confirm" automatically sees their
        commits and rollbacks.
    
    Part of #11180
    
    NO_DOC=bugfix
    
    (cherry picked from commit 20aad159d065bbd6e5d36bc465af8adb2bc59c30)

--- src/box/txn_limbo.h
+++ src/box/txn_limbo.h
@@ -45,6 +45,11 @@ struct txn;
 struct synchro_request;
 
 enum txn_limbo_entry_state {
+	/**
+	 * Is saved in the limbo, but isn't accounted yet and isn't persisted
+	 * anywhere.
+	 */
+	TXN_LIMBO_ENTRY_VOLATILE,
 	/** Is saved and accounted in the limbo. */
 	TXN_LIMBO_ENTRY_SUBMITTED,
 	/** Committed, not in the limbo anymore. */
@@ -292,11 +297,9 @@ txn_limbo_is_empty(struct txn_limbo *limbo)
 	return rlist_empty(&limbo->queue);
 }
 
-static inline bool
-txn_limbo_is_full(struct txn_limbo *limbo)
-{
-	return limbo->size >= limbo->max_size;
-}
+/** See if submission to the limbo would yield if done right now. */
+bool
+txn_limbo_would_block(struct txn_limbo *limbo);
 
 bool
 txn_limbo_is_ro(struct txn_limbo *limbo);
@@ -332,8 +335,8 @@ txn_limbo_last_synchro_entry(struct txn_limbo *limbo);
  * Allocate, create, and append a new transaction to the limbo.
  * The limbo entry is allocated on the transaction's region.
  */
-struct txn_limbo_entry *
-txn_limbo_append(struct txn_limbo *limbo, uint32_t id, struct txn *txn,
+int
+txn_limbo_submit(struct txn_limbo *limbo, uint32_t id, struct txn *txn,
 		 size_t approx_len);
 
 /** Remove the entry from the limbo, mark as rolled back. */
@@ -539,10 +542,6 @@ txn_limbo_shutdown(void);
 /** Set maximal limbo size in bytes. */
 void
 txn_limbo_set_max_size(struct txn_limbo *limbo, int64_t size);
-
-/** Wait for space in the limbo to add a new entry. */
-int
-txn_limbo_wait_for_space(struct txn_limbo *limbo);
 
 #if defined(__cplusplus)
 }
