6e065637ce9619302da50bf9f4a453be43a67c6f
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2025-06-12 23:39:39 +0200

    limbo: introduce limbo entry state
    The txn_limbo_entry used to have 2 flags to describe its state -
    commit or rollback or nothing (submitted, but isn't complete).
    
    It is in general problematic to maintain when one state is
    described by more than one variable. Like for example what would
    it mean if both is_commit and is_rollback are set? That would be
    nonsense, but it was theoretically possible.
    
    Soon there will appear one more state. Those 2 flags would be no
    longer enough.
    
    Lets turn them into an actual state enum.
    
    Part of #11180
    
    NO_DOC=refactoring
    NO_TEST=refactoring
    NO_CHANGELOG=refactoring
    
    (cherry picked from commit 8ffd7ef8fa5fee2caa0a330ad45bb4ed5d93a72b)

--- src/box/txn_limbo.c
+++ src/box/txn_limbo.c
@@ -301,8 +301,7 @@ txn_limbo_append(struct txn_limbo *limbo, uint32_t id, struct txn *txn,
 	e->txn = txn;
 	e->approx_len = approx_len;
 	e->lsn = -1;
-	e->is_commit = false;
-	e->is_rollback = false;
+	e->state = TXN_LIMBO_ENTRY_SUBMITTED;
 	e->insertion_time = fiber_clock();
 	rlist_add_tail_entry(&limbo->queue, e, in_queue);
 	txn_limbo_on_append(limbo, e);
@@ -323,7 +322,7 @@ txn_limbo_pop(struct txn_limbo *limbo, struct txn_limbo_entry *entry)
 {
 	assert(!rlist_empty(&entry->in_queue));
 	assert(txn_limbo_last_entry(limbo) == entry);
-	assert(entry->is_rollback);
+	assert(entry->state == TXN_LIMBO_ENTRY_ROLLBACK);
 
 	rlist_del_entry(entry, in_queue);
 	txn_limbo_on_remove(limbo, entry);
@@ -333,7 +332,8 @@ txn_limbo_pop(struct txn_limbo *limbo, struct txn_limbo_entry *entry)
 void
 txn_limbo_abort(struct txn_limbo *limbo, struct txn_limbo_entry *entry)
 {
-	entry->is_rollback = true;
+	assert(entry->state == TXN_LIMBO_ENTRY_SUBMITTED);
+	entry->state = TXN_LIMBO_ENTRY_ROLLBACK;
 	if (entry == limbo->entry_to_confirm)
 		limbo->entry_to_confirm = NULL;
 	/*
@@ -494,7 +494,7 @@ complete:
 	 * The first tx to be rolled back already performed all
 	 * the necessary cleanups for us.
 	 */
-	if (entry->is_rollback) {
+	if (entry->state == TXN_LIMBO_ENTRY_ROLLBACK) {
 		diag_set(ClientError, ER_SYNC_ROLLBACK);
 		return -1;
 	}
@@ -633,7 +633,8 @@ txn_limbo_read_confirm(struct txn_limbo *limbo, int64_t lsn)
 			e->txn = NULL;
 			continue;
 		}
-		e->is_commit = true;
+		assert(e->state == TXN_LIMBO_ENTRY_SUBMITTED);
+		e->state = TXN_LIMBO_ENTRY_COMMIT;
 		if (txn_has_flag(e->txn, TXN_WAIT_ACK))
 			limbo->confirm_lag = fiber_clock() - e->insertion_time;
 		e->txn->limbo_entry = NULL;
