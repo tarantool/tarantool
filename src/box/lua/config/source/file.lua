def1c542ba5cfc71706571fb8550bc66fd4d1f40
Borislav Demidov <Boris.Stepanenko@gmail.com>
2025-06-17 01:37:32 +0300

    config: add `include` field for config files
    Closes #11583
    Closes TNTP-229
    
    @TarantoolBot document
    Title: config: `include` section for config files.
    
    Using this section it is possible to include one or several other config
    files from any config file. This is a top level field of type for
    file-based configuration. If present, this field must be an array of
    strings. Each entry in this array can be an absolute or relative path to
    a config file to be included, or a wildcard pattern. Include section
    can be placed inside a `conditional` section. The non-existing included
    files are skipped. If an included file exists and is not a valid
    tarantool config file, an error will be raised.
    
    Example config.yaml:
    ```yaml
    include:
      - config_a.yaml
      - /etc/tarantool/config_b.yaml
      - conf.d/*.yaml
    ```
    
    For the provided example the resulting configuration will be merged
    according to the rules:
    1. Config file provided by `--config` flag is read (`config.yam`l in
       our example).
    2. `{workdir}/config_a.yaml` is merged onto it.
    3. Recursively, the includes of `{workdir}/config_a.yaml` are merged
       onto the configuration.
    4. `/etc/tarantool/config_b.yaml` is merged.
    5. Recursively, the includes of `/etc/tarantool/config_b.yaml` are
       merged onto the configuration.
    6. For each config file, matching the wildcard pattern, in alphabetical
       order the configuration from the file itself is merged, then its
       includes are processed recursively, before processing the next
       wildcard match.

--- src/box/lua/config/source/file.lua
+++ src/box/lua/config/source/file.lua
@@ -1,29 +1,30 @@
+local fio = require('fio')
+local log = require('log')
 local yaml = require('yaml')
 local file = require('internal.config.utils.file')
+local cluster_config = require('internal.config.cluster_config')
 
 local methods = {}
 local mt = {
     __index = methods,
 }
 
-function methods.sync(self, config_module, _iconfig)
-    assert(config_module._config_file ~= nil)
-
-    local data = file.universal_read(config_module._config_file, 'config file')
+local function read_config_file(path)
+    local data = file.universal_read(path, 'config file')
 
     -- Integrity module is available only in Tarantool Enterprise Edition
     -- builds.
     local ok, integrity = pcall(require, 'integrity')
-    if ok and not integrity.verify_file(config_module._config_file, data) then
+    if ok and not integrity.verify_file(path, data) then
         local err = 'Integrity check failed for configuration file %q'
-        error(err:format(config_module._config_file))
+        error(err:format(path))
     end
 
     local res
     ok, res = pcall(yaml.decode, data)
     if not ok then
         error(('Unable to parse config file %q as YAML: %s'):format(
-            config_module._config_file, res))
+            path, res))
     end
 
     -- YAML returns `nil` or `box.NULL` on empty file,
@@ -32,11 +33,7 @@ function methods.sync(self, config_module, _iconfig)
         res = {}
     end
 
-    self._values = res
-end
-
-function methods.get(self)
-    return self._values
+    return res
 end
 
 local function new()
@@ -45,6 +42,57 @@ local function new()
         type = 'cluster',
         _values = {},
     }, mt)
+end
+
+function methods.sync(self, config_module, _iconfig)
+    assert(config_module._config_file ~= nil)
+
+    local sources = {config_module._config_file}
+    local processed_sources = {}
+    local cconfig = {}
+
+    local config
+    local i = 1
+    while i <= #sources do
+        local source = sources[i]
+
+        -- Prevent looping.
+        if processed_sources[source] then
+            log.warn('skipping already processed config file: %q', source)
+            goto continue
+        end
+        processed_sources[source] = true
+
+        config = read_config_file(source)
+
+        config = cluster_config:apply_conditional(config)
+
+        if config.include ~= nil then
+            local insert_idx = 1
+            for _, glob in ipairs(config.include) do
+                local paths = fio.glob(glob)
+                for _, path in ipairs(paths) do
+                    path = fio.abspath(path)
+                    table.insert(sources, i + insert_idx, path)
+                    insert_idx = insert_idx + 1
+                end
+            end
+            config.include = nil
+        end
+
+        log.debug('processing config file %q', source)
+        cconfig = cluster_config:merge(cconfig, config)
+
+        ::continue::
+        i = i + 1
+    end
+
+    cluster_config:validate(cconfig)
+    self._values = cconfig
+end
+
+function methods.get(self)
+    return self._values
 end
 
 return {
