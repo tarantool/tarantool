8c6c2085e0f2adafe70fa3ba1d7b3dae58d2495d
Lev Kats <lev-katz@mail.ru>
2025-06-27 21:50:23 +0300

    box: introduce real user id
    Now one can use `auth_uid` field of user's credentials to get the id of
    user as it was right after the login. It is invariant to
    `box.session.su`.
    
    Part of tarantool/tarantool-ee#1278
    
    NO_TEST=ee
    NO_DOC=internal
    NO_CHANGELOG=internal

--- src/box/lua/session.c
+++ src/box/lua/session.c
@@ -194,7 +194,9 @@ lbox_session_su(struct lua_State *L)
 		luaT_error(L);
 
 	if (top == 1) {
+		uint32_t auth_uid = session->credentials.auth_uid;
 		credentials_reset(&session->credentials, user);
+		session->credentials.auth_uid = auth_uid;
 		fiber_set_user(fiber(), &session->credentials);
 		return 0; /* su */
 	}
@@ -203,6 +205,8 @@ lbox_session_su(struct lua_State *L)
 	struct credentials su_credentials;
 	struct credentials *old_credentials = fiber()->storage.credentials;
 	credentials_create(&su_credentials, user);
+	uint32_t auth_uid = session->credentials.auth_uid;
+	su_credentials.auth_uid = auth_uid;
 	fiber()->storage.credentials = &su_credentials;
 
 	int error = lua_pcall(L, top - 2, LUA_MULTRET, 0);
