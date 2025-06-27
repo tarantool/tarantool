bae3a6e4b5f42d408412c5e33c1f2f047d35a1a4
Andrey Saranchin <Andrey22102001@gmail.com>
2025-06-27 15:39:51 +0300

    box: introduce index filters option
    It will be used by MemCS to specify filters for its indexes. These
    filters are also known as BRIN-indexes or skip-indexes.
    
    Part of tarantool/tarantool-ee#1029
    
    NO_CHANGELOG=does not work with CE engines
    NO_DOC=will be done in EE

--- src/box/index_def.c
+++ src/box/index_def.c
@@ -55,6 +55,7 @@ const struct index_opts index_opts_default = {
 	/* .covered_fields      = */ NULL,
 	/* .covered_field_count = */ 0,
 	/* .layout              = */ NULL,
+	/* .filters             = */ NULL,
 };
 
 /**
@@ -133,6 +134,37 @@ index_opts_parse_layout(const char **data, void *opts, struct region *region)
 	return 0;
 }
 
+/**
+ * Parse index 'filters' option given as MsgPack in 'data' into 'opts'.
+ * The MsgPack is copied to a memory allocated on 'region'.
+ */
+static int
+index_opts_parse_filters(const char **data, void *opts,
+			 struct region *region)
+{
+	struct index_opts *index_opts = (struct index_opts *)opts;
+	const char *filters = *data;
+	if (mp_typeof(**data) != MP_ARRAY) {
+		diag_set(IllegalParams, "'filters' must be array");
+		return -1;
+	}
+	uint32_t filter_count = mp_decode_array(data);
+	if (filter_count == 0)
+		return 0;
+
+	for (uint32_t i = 0; i < filter_count; i++) {
+		if (mp_typeof(**data) != MP_MAP) {
+			diag_set(IllegalParams,
+				 "'filters' elements must be map");
+			return -1;
+		}
+		mp_next(data);
+	}
+	index_opts->filters = xregion_alloc(region, *data - filters);
+	memcpy(index_opts->filters, filters, *data - filters);
+	return 0;
+}
+
 const struct opt_def index_opts_reg[] = {
 	OPT_DEF("unique", OPT_BOOL, struct index_opts, is_unique),
 	OPT_DEF("dimension", OPT_INT64, struct index_opts, dimension),
@@ -149,6 +181,7 @@ const struct opt_def index_opts_reg[] = {
 	OPT_DEF_CUSTOM("hint", index_opts_parse_hint),
 	OPT_DEF_CUSTOM("covers", index_opts_parse_covered_fields),
 	OPT_DEF_CUSTOM("layout", index_opts_parse_layout),
+	OPT_DEF_CUSTOM("filters", index_opts_parse_filters),
 	OPT_END,
 };
 
@@ -201,6 +234,8 @@ index_opts_dup(const struct index_opts *opts, struct index_opts *dup)
 	}
 	if (dup->layout != NULL)
 		dup->layout = xstrdup(dup->layout);
+	if (dup->filters != NULL)
+		dup->filters = mp_dup(dup->filters);
 }
 
 struct index_def *
