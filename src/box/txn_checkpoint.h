c8f2102c20acc9254de6920bc60a3c7bfc4cda3d
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2025-07-09 23:08:07 +0200

    txn: introduce txn_checkpoint
    Many subsystems have checkpoints - WAL, each engine, limbo, Raft.
    But somehow the transaction subsystem was left out. It wasn't
    considered to be something having its own state.
    
    But actually the transactions have quite a large state which
    unites many others and requires them to be in exact sync: Raft,
    limbo, journal.
    
    Indeed, collecting those states separately might lead to mistakes
    like having a too old journal vclock with a newer Raft/limbo
    checkpoint. Or vice versa. Or any other possible wrong combination
    when those checkpoints are just collected at different times and
    might describe different datasets, if the instance is actively
    committing new transactions during creation of those checkpoints.
    
    The idea of the new txn_checkpoint module is to put the collection
    of those interconnected states into one place. So it is isolated
    from the code which doesn't care about the details, and so it
    could be reused.
    
    Note that this commit only moves some code. But the code is
    broken. It is going to be fixed in the following commits.
    
    The patch is cherry-picked to 2.11 in order to bring the limbo's
    state as close to the master (3.6) as possible in order to
    fix #11790 and parts of #11180.
    
    Part of #11180
    Part of #11790
    
    NO_DOC=refactoring
    NO_TEST=refactoring
    NO_CHANGELOG=refactoring
    
    (cherry picked from commit ac8fd34e3b655160670295f67cddfcc07f7def18)

---
+++ src/box/txn_checkpoint.h
@@ -0,0 +1,43 @@
+#pragma once
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright 2010-2025, Tarantool AUTHORS, please see AUTHORS file.
+ */
+#include "xrow.h"
+
+#if defined(__cplusplus)
+extern "C" {
+#endif /* defined(__cplusplus) */
+
+/** Data collected precisely when all the prepared txns are committed. */
+struct txn_checkpoint {
+	/**
+	 * VClock taken when the last known prepared txn was written to WAL. For
+	 * non-synchronous txns it is equal to their commit moment.
+	 */
+	struct vclock journal_vclock;
+	/**
+	 * Full descriptor of the Raft state machine collected exactly when the
+	 * last known synchronous txn was confirmed.
+	 */
+	struct raft_request raft_remote_checkpoint;
+	/**
+	 * Full descriptor of the txn limbo collected exactly when the last
+	 * known synchronous txn was confirmed.
+	 */
+	struct synchro_request limbo_checkpoint;
+	/** VClock of the limbo's state. */
+	struct vclock limbo_vclock;
+};
+
+/**
+ * Wait until all the currently prepared txns are committed and collect all the
+ * global transaction-related data at this exact moment.
+ */
+int
+txn_checkpoint_build(struct txn_checkpoint *out);
+
+#if defined(__cplusplus)
+} /* extern "C" */
+#endif /* defined(__cplusplus) */
