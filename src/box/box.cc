26aa5abbacaf5d048f6715d05760256c70b611bb
Roman Gromov <r.gromov.official@gmail.com>
2025-07-16 16:44:39 +0300

    box: change uuid validation in iproto requests
    In the previous patch #11531 we banned connecting to replicas with nil
    `instance_uuid` in all iproto requests, including `IPROTO_FETCH_SNAPSHOT`.
    It is necessary to allow passing a nil `instance_uuid` to this iproto
    request, because:
    1) For `IPROTO_FETCH_SNAPSHOT` it became possible to pass an
    `instance_uuid`only starting with Tarantool 3.3.
    2) While implementing the patch #10755 we wanted to leave `instance_uuid`
    field optional for this type of iproto request.
    
    To fix this issue we move checking of nil `instance_uuid` from
    `box_connect_replica` to appropriate box functions of each iproto
    replication request, excluding `IPROTO_FETCH_SNAPSHOT`.
    
    Also we change some tests:
    1) `gh_11531_crash_in_anonymous_subscribe_request_test`: add timeout in
       retrying block in order to avoid flakies. The master node may not be
       able to establish connection during 5 sec. with `net_box` instance.
    2) `persistent_gc_anon_test`: introduce `read_fetch_snapshot_response`
       into `test_fetch_snapshot_no_uuid` in order to expect a correct
       iproto response of `IPROTO_FETCH_SNAPSHOT`.
    
    Closes #11668
    
    NO_DOC=bugfix
    NO_CHANGELOG=unreleased

--- src/box/box.cc
+++ src/box/box.cc
@@ -4810,7 +4810,8 @@ box_connect_replica(const struct tt_uuid *uuid, const struct vclock *gc_vclock,
 
 	/* No replica object with nil UUID. */
 	if (tt_uuid_is_nil(uuid)) {
-		tnt_raise(ClientError, ER_NIL_UUID);
+		*out = NULL;
+		return ReplicaConnectionGuard(NULL);
 	}
 
 	struct replica *replica = replica_by_uuid(uuid);
@@ -4943,6 +4944,10 @@ box_process_register(struct iostream *io, const struct xrow_header *header)
 	if (!is_box_configured)
 		tnt_raise(ClientError, ER_LOADING);
 
+	/* No replica object with nil UUID. */
+	if (tt_uuid_is_nil(&req.instance_uuid))
+		tnt_raise(ClientError, ER_NIL_UUID);
+
 	if (tt_uuid_is_equal(&req.instance_uuid, &INSTANCE_UUID))
 		tnt_raise(ClientError, ER_CONNECTION_TO_SELF);
 
@@ -5067,6 +5072,10 @@ box_process_join(struct iostream *io, const struct xrow_header *header)
 	if (!is_box_configured)
 		tnt_raise(ClientError, ER_LOADING);
 
+	/* No replica object with nil UUID. */
+	if (tt_uuid_is_nil(&req.instance_uuid))
+		tnt_raise(ClientError, ER_NIL_UUID);
+
 	/* Forbid connection to itself */
 	if (tt_uuid_is_equal(&req.instance_uuid, &INSTANCE_UUID))
 		tnt_raise(ClientError, ER_CONNECTION_TO_SELF);
@@ -5168,6 +5177,10 @@ box_process_subscribe(struct iostream *io, const struct xrow_header *header)
 
 	struct subscribe_request req;
 	xrow_decode_subscribe_xc(header, &req);
+
+	/* No replica object with nil UUID. */
+	if (tt_uuid_is_nil(&req.instance_uuid))
+		tnt_raise(ClientError, ER_NIL_UUID);
 
 	/* Forbid connection to itself */
 	if (tt_uuid_is_equal(&req.instance_uuid, &INSTANCE_UUID))
