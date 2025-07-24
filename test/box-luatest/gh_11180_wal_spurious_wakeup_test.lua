5426e5203aac08a7cc1d3b527628ad6e4063f7be
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2025-07-24 21:50:37 +0200

    test: reuse some code in a test
    It is going to get a new test case which could use some things
    already declared in other test cases. Lets move them to a common
    place to reuse.
    
    In scope of #11712
    
    NO_DOC=test
    NO_CHANGELOG=test
    
    (cherry picked from commit 4acdfc7a0f4ad0b7077384231c8ed788b7c4dc6c)

--- test/box-luatest/gh_11180_wal_spurious_wakeup_test.lua
+++ test/box-luatest/gh_11180_wal_spurious_wakeup_test.lua
@@ -6,6 +6,16 @@ g.before_all(function(cg)
     t.tarantool.skip_if_not_debug()
     cg.server = server:new({box_cfg = {wal_queue_max_size = 1000}})
     cg.server:start()
+    cg.server:exec(function()
+        rawset(_G, 'fiber', require('fiber'))
+        rawset(_G, 'test_data', string.rep('a', 1000))
+        rawset(_G, 'test_timeout', 60)
+        rawset(_G, 'join_with_error', function(f, err_expected)
+            local ok, err = f:join()
+            t.assert_not(ok)
+            t.assert_covers(err:unpack(), err_expected)
+        end)
+    end)
 end)
 
 g.after_all(function(cg)
@@ -18,21 +28,18 @@ end)
 --
 g.test_wal_queue_rollback_in_flight = function(cg)
     cg.server:exec(function()
-        local fiber = require('fiber')
-        local data = string.rep('a', 1000)
-        local timeout = 60
         local s = box.schema.create_space('test')
         s:create_index('pk')
 
         local results = {}
         local function make_txn_fiber(id)
-            return fiber.create(function()
-                fiber.self():set_joinable(true)
+            return _G.fiber.create(function()
+                _G.fiber.self():set_joinable(true)
                 box.begin()
                 box.on_commit(function()
                     table.insert(results, id)
                 end)
-                s:replace{id, data}
+                s:replace{id, _G.test_data}
                 box.commit()
             end)
         end
@@ -44,8 +51,8 @@ g.test_wal_queue_rollback_in_flight = function(cg)
         -- next txn-fibers up in a wrong order. They should still be able to
         -- re-sort themselves.
         --
-        local f1 = fiber.create(function()
-            fiber.self():set_joinable(true)
+        local f1 = _G.fiber.create(function()
+            _G.fiber.self():set_joinable(true)
             box.begin()
             box.on_commit(function()
                 table.insert(results, 1)
@@ -56,20 +63,20 @@ g.test_wal_queue_rollback_in_flight = function(cg)
                     pcall(f2.wakeup, f2)
                 end
             end)
-            s:replace{1, data}
+            s:replace{1, _G.test_data}
             box.commit()
         end)
         -- Make sure all 3 txns are waiting and aren't committed just one by one
         -- somehow.
-        t.helpers.retrying({timeout = timeout}, function()
+        t.helpers.retrying({timeout = _G.test_timeout}, function()
             t.assert(box.error.injection.get('ERRINJ_WAL_DELAY'))
         end)
         f2 = make_txn_fiber(2)
         f3 = make_txn_fiber(3)
         box.error.injection.set('ERRINJ_WAL_DELAY', false)
-        t.assert((f1:join(timeout)))
-        t.assert((f2:join(timeout)))
-        t.assert((f3:join(timeout)))
+        t.assert((f1:join(_G.test_timeout)))
+        t.assert((f2:join(_G.test_timeout)))
+        t.assert((f3:join(_G.test_timeout)))
         t.assert_equals(results, {1, 2, 3})
         s:drop()
     end)
@@ -77,40 +84,32 @@ end
 
 g.test_wal_cancel_first_waiting = function(cg)
     cg.server:exec(function()
-        local fiber = require('fiber')
-        local data = string.rep('a', 1000)
-        local timeout = 60
         local s = box.schema.create_space('test')
         s:create_index('pk')
 
         box.error.injection.set('ERRINJ_WAL_DELAY_COUNTDOWN', 0)
-        local function join_with_error(f, err_expected)
-            local ok, err = f:join()
-            t.assert_not(ok)
-            t.assert_covers(err:unpack(), err_expected)
-        end
         -- Goes to WAL thread.
         local f2
-        local f1 = fiber.create(function()
-            fiber.self():set_joinable(true)
+        local f1 = _G.fiber.create(function()
+            _G.fiber.self():set_joinable(true)
             box.begin()
             box.on_commit(function() f2:cancel() end)
-            s:replace{1, data}
+            s:replace{1, _G.test_data}
             box.commit()
         end)
-        t.helpers.retrying({timeout = timeout}, function()
+        t.helpers.retrying({timeout = _G.test_timeout}, function()
             t.assert(box.error.injection.get('ERRINJ_WAL_DELAY'))
         end)
         -- Gets stuck in the WAL queue and cancelled right when the first txn
         -- frees the WAL queue.
-        f2 = fiber.create(function()
-            fiber.self():set_joinable(true)
-            s:replace{2, data}
+        f2 = _G.fiber.create(function()
+            _G.fiber.self():set_joinable(true)
+            s:replace{2, _G.test_data}
         end)
         box.error.injection.set('ERRINJ_WAL_DELAY', false)
         t.assert((f1:join()))
-        join_with_error(f2, {type = 'FiberIsCancelled'})
-        t.assert_equals(s:select(), {{1, data}})
+        _G.join_with_error(f2, {type = 'FiberIsCancelled'})
+        t.assert_equals(s:select(), {{1, _G.test_data}})
         s:drop()
     end)
 end
