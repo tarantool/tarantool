8a105678ba71b46a94e7fa4720fe83e5f0a27b2c
Vladimir Davydov <vdavydov@tarantool.org>
2025-07-31 11:28:08 +0300

    test: fix grep log usage in replication tests
    Server logs are processed asynchronously. They used to be processed by
    a separate process before luatest 1.1.0 and now, in luatest 1.1.0 and
    newer they are processed by a fiber. This means that we should use the
    retrying helper to check if a message is present in logs reliably.
    
    While we are at it, let's also drop the log file name specification
    because starting from luatest 1.1.0 it's deduced automatically, even
    for a stopped server.
    
    NO_DOC=test
    NO_CHANGELOG=test
    
    (cherry picked from commit 4996f923713b41029b7cc83daed610368de2a0db)

--- test/replication-luatest/instance_name_test.lua
+++ test/replication-luatest/instance_name_test.lua
@@ -1,4 +1,3 @@
-local fio = require('fio')
 local replica_set = require('luatest.replica_set')
 local server = require('luatest.server')
 local t = require('luatest')
@@ -299,13 +298,12 @@ g.test_instance_name_bootstrap_mismatch = function(lg)
         box_cfg = box_cfg,
     })
     new_replica:start({wait_until_ready = false})
-    local logfile = fio.pathjoin(new_replica.workdir,
-                                 new_replica.alias .. '.log')
     wait_for_death(new_replica)
-    t.assert(new_replica:grep_log(
-        'Instance name mismatch: name \'new%-replica%-name\' provided in ' ..
-        'config confilcts with the instance one \'other%-name\'',
-        1024, {filename = logfile}))
+    t.helpers.retrying({timeout = 60}, function()
+        t.assert(new_replica:grep_log(
+            'Instance name mismatch: name \'new%-replica%-name\' provided ' ..
+            'in config confilcts with the instance one \'other%-name\'', 1024))
+    end)
     new_replica:drop()
     --
     -- New replica has instance name, but the master didn't assign it.
@@ -327,12 +325,12 @@ g.test_instance_name_bootstrap_mismatch = function(lg)
         box_cfg = box_cfg,
     })
     new_replica:start({wait_until_ready = false})
-    logfile = fio.pathjoin(new_replica.workdir, new_replica.alias .. '.log')
     wait_for_death(new_replica)
-    t.assert(new_replica:grep_log(
-        'Instance name mismatch: name \'new%-replica%-name\' provided in ' ..
-        'config confilcts with the instance one \'<no%-name>\'',
-        1024, {filename = logfile}))
+    t.helpers.retrying({timeout = 60}, function()
+        t.assert(new_replica:grep_log(
+            'Instance name mismatch: name \'new%-replica%-name\' provided ' ..
+            'in config confilcts with the instance one \'<no%-name>\'', 1024))
+    end)
     new_replica:drop()
     --
     -- Cleanup.
@@ -362,13 +360,12 @@ g.test_instance_name_recovery_mismatch = function(lg)
     lg.replica:restart({
         box_cfg = box_cfg,
     }, {wait_until_ready = false})
-    local logfile = fio.pathjoin(lg.replica.workdir,
-                                 lg.replica.alias .. '.log')
     wait_for_death(lg.replica)
-    t.assert(lg.replica:grep_log(
-        'Instance name mismatch: name \'new%-name\' provided in config ' ..
-        'confilcts with the instance one \'replica%-name\'', 1024,
-        {filename = logfile}))
+    t.helpers.retrying({timeout = 60}, function()
+        t.assert(lg.replica:grep_log(
+            'Instance name mismatch: name \'new%-name\' provided in config ' ..
+            'confilcts with the instance one \'replica%-name\'', 1024))
+    end)
     --
     -- Has name in WAL, no name in cfg. Then the replica uses the saved name, no
     -- conflict.
@@ -402,12 +399,12 @@ g.test_instance_name_recovery_mismatch = function(lg)
     lg.replica:restart({
         box_cfg = box_cfg,
     }, {wait_until_ready = false})
-    logfile = fio.pathjoin(lg.replica.workdir, lg.replica.alias .. '.log')
     wait_for_death(lg.replica)
-    t.assert(lg.replica:grep_log(
-        'Instance name mismatch: name \'new%-name\' provided in config ' ..
-        'confilcts with the instance one \'<no%-name>\'', 1024,
-        {filename = logfile}))
+    t.helpers.retrying({timeout = 60}, function()
+        t.assert(lg.replica:grep_log(
+            'Instance name mismatch: name \'new%-name\' provided in config ' ..
+            'confilcts with the instance one \'<no%-name>\'', 1024))
+    end)
     box_cfg.instance_name = nil
     box_cfg.force_recovery = nil
     lg.replica:restart({
@@ -425,12 +422,12 @@ g.test_instance_name_recovery_mismatch = function(lg)
     lg.master:restart({
         box_cfg = box_cfg,
     }, {wait_until_ready = false})
-    logfile = fio.pathjoin(lg.master.workdir, lg.master.alias .. '.log')
     wait_for_death(lg.master)
-    t.assert(lg.master:grep_log(
-        'Instance name mismatch: name \'new%-name\' provided in config ' ..
-        'confilcts with the instance one \'master%-name\'', 1024,
-        {filename = logfile}))
+    t.helpers.retrying({timeout = 60}, function()
+        t.assert(lg.master:grep_log(
+            'Instance name mismatch: name \'new%-name\' provided in config ' ..
+            'confilcts with the instance one \'master%-name\'', 1024))
+    end)
     --
     -- No name in WAL, has name in cfg.
     --
@@ -451,12 +448,12 @@ g.test_instance_name_recovery_mismatch = function(lg)
     lg.master:restart({
         box_cfg = box_cfg,
     }, {wait_until_ready = false})
-    logfile = fio.pathjoin(lg.master.workdir, lg.master.alias .. '.log')
     wait_for_death(lg.master)
-    t.assert(lg.master:grep_log(
-        'Instance name mismatch: name \'new%-name\' provided in config ' ..
-        'confilcts with the instance one \'<no%-name>\'', 1024,
-        {filename = logfile}))
+    t.helpers.retrying({timeout = 60}, function()
+        t.assert(lg.master:grep_log(
+            'Instance name mismatch: name \'new%-name\' provided in config ' ..
+            'confilcts with the instance one \'<no%-name>\'', 1024))
+    end)
     box_cfg.instance_name = nil
     box_cfg.force_recovery = nil
     lg.master:restart({
@@ -528,12 +525,10 @@ g.test_instance_name_new_uuid = function(lg)
         box_cfg = box_cfg,
     })
     new_replica:start({wait_until_ready = false})
-    local logfile = fio.pathjoin(new_replica.workdir,
-                                 new_replica.alias .. '.log')
-    t.helpers.retrying({}, function()
+    t.helpers.retrying({timeout = 60}, function()
         assert(new_replica:grep_log(
                'ER_INSTANCE_NAME_DUPLICATE: Duplicate replica name '..
-               'replica%-name, already occupied', 1024, {filename = logfile}))
+               'replica%-name, already occupied', 1024))
     end)
     new_replica:drop()
     --
