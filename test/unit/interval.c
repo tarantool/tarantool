7333e44019914a01cd841444dad93c1c1b99cfd8
Nikolay Shirokovskiy <nshirokovskiy@tarantool.org>
2025-08-04 15:21:23 +0300

    core: fix interval MsgPack decoding
    Currently we do not check if we have enough data to read when
    loading field key.
    
    While at it also fix limits checking for `year`, `month`, `week` and
    `nanoseconds` fields we lost in the commit 01c7ae1128e1 ("msgpack: fix
    decoding intervals with int64").
    
    Also as we check the upper limit for `ajust` field let's check the lower
    limit too.
    
    Closes #10360
    
    NO_DOC=bugfix
    
    (cherry picked from commit 99517500eb5508a85d75470c631156002177ef04)

--- test/unit/interval.c
+++ test/unit/interval.c
@@ -16,6 +16,9 @@ enum {
 static void
 test_interval_sizeof(void)
 {
+	header();
+	plan(6);
+
 	struct interval itv;
 	memset(&itv, 0, sizeof(itv));
 	uint32_t size = 3;
@@ -35,6 +38,9 @@ test_interval_sizeof(void)
 	itv.sec = -2000000000;
 	size = 24;
 	is(mp_sizeof_interval(&itv), size, "Size of interval is %d", size);
+
+	check_plan();
+	footer();
 }
 
 static bool
@@ -60,6 +66,9 @@ interval_mp_recode(const struct interval *in, struct interval *out)
 static void
 test_interval_encode_decode(void)
 {
+	header();
+	plan(15);
+
 	struct interval itv;
 	memset(&itv, 0, sizeof(itv));
 	struct interval result;
@@ -95,11 +104,17 @@ test_interval_encode_decode(void)
 	is(result.sec, -2000000000, "Second value is right");
 	is(result.nsec, 0, "Nanosecond value is right");
 	is(result.adjust, DT_EXCESS, "Adjust value is right");
+
+	check_plan();
+	footer();
 }
 
 static void
 test_interval_encode_decode_values_outside_int32_limits(void)
 {
+	header();
+	plan(9);
+
 	struct interval itv;
 	memset(&itv, 0, sizeof(itv));
 	struct interval result;
@@ -137,14 +152,98 @@ test_interval_encode_decode_values_outside_int32_limits(void)
 	itv.sec = (double)INT32_MAX + 1;
 	interval_mp_recode(&itv, &result);
 	ok(is_interval_equal(&itv, &result), "Intervals are equal.");
+
+	check_plan();
+	footer();
+}
+
+static void
+test_interval_validate(void)
+{
+	header();
+	plan(24);
+
+	/* Check reading field key is checked. */
+	ok(mp_validate_interval("\x02", 1) != 0,
+	   "reading interval field 1 key is checked");
+	ok(mp_validate_interval("\x02\x00\x00", 3) != 0,
+	   "reading interval field 2 key is checked");
+
+	/* Check reading field value is checked. */
+	ok(mp_validate_interval("\x01\x00\xce", 2) != 0,
+	   "reading interval field positive value type is checked");
+	ok(mp_validate_interval("\x01\x00\xce", 3) != 0,
+	   "reading interval field positive value is checked");
+	ok(mp_validate_interval("\x01\x00\xd3", 2) != 0,
+	   "reading interval field negative value type is checked");
+	ok(mp_validate_interval("\x01\x00\xd3", 3) != 0,
+	   "reading interval field negative value is checked");
+
+	/* Check adjust decoding. */
+	ok(mp_validate_interval("\x01\x08\x03", 3) != 0,
+	   "check adjust value is not greater than DT_SNAP");
+	ok(mp_validate_interval("\x01\x08\xff", 3) != 0,
+	   "check adjust value is not less that DT_EXCESS (0)");
+
+	/* Check year decoding. */
+	ok(mp_validate_interval("\x01\x00\xd2\x80\x00\x00\x00", 7) == 0,
+	   "check year equal to INT32_MIN");
+	ok(mp_validate_interval(
+		"\x01\x00\xd3\xff\xff\xff\xff\x7f\xff\xff\xff", 11) != 0,
+		"check year less than INT32_MIN");
+	ok(mp_validate_interval("\x01\x00\xce\x7f\xff\xff\xff", 7) == 0,
+	   "check year equal to INT32_MAX");
+	ok(mp_validate_interval("\x01\x00\xce\x80\x00\x00\x00", 7) != 0,
+	   "check year larger than INT32_MAX");
+
+	/* Check month decoding. */
+	ok(mp_validate_interval("\x01\x01\xd2\x80\x00\x00\x00", 7) == 0,
+	   "check month equal to INT32_MIN");
+	ok(mp_validate_interval(
+		"\x01\x01\xd3\xff\xff\xff\xff\x7f\xff\xff\xff", 11) != 0,
+		"check month less than INT32_MIN");
+	ok(mp_validate_interval("\x01\x01\xce\x7f\xff\xff\xff", 7) == 0,
+	   "check month equal to INT32_MAX");
+	ok(mp_validate_interval("\x01\x01\xce\x80\x00\x00\x00", 7) != 0,
+	   "check month larger than INT32_MAX");
+
+	/* Check week decoding. */
+	ok(mp_validate_interval("\x01\x02\xd2\x80\x00\x00\x00", 7) == 0,
+	   "check week equal to INT32_MIN");
+	ok(mp_validate_interval(
+		"\x01\x02\xd3\xff\xff\xff\xff\x7f\xff\xff\xff", 11) != 0,
+		"check week less than INT32_MIN");
+	ok(mp_validate_interval("\x01\x02\xce\x7f\xff\xff\xff", 7) == 0,
+	   "check week equal to INT32_MAX");
+	ok(mp_validate_interval("\x01\x02\xce\x80\x00\x00\x00", 7) != 0,
+	   "check week larger than INT32_MAX");
+
+	/* Check nanosecond decoding. */
+	ok(mp_validate_interval("\x01\x07\xd2\x80\x00\x00\x00", 7) == 0,
+	   "check nanosecond equal to INT32_MIN");
+	ok(mp_validate_interval(
+		"\x01\x07\xd3\xff\xff\xff\xff\x7f\xff\xff\xff", 11) != 0,
+		"check nanosecond less than INT32_MIN");
+	ok(mp_validate_interval("\x01\x07\xce\x7f\xff\xff\xff", 7) == 0,
+	   "check nanosecond equal to INT32_MAX");
+	ok(mp_validate_interval("\x01\x07\xce\x80\x00\x00\x00", 7) != 0,
+	   "check nanosecond larger than INT32_MAX");
+
+	check_plan();
+	footer();
 }
 
 int
 main(void)
 {
-	plan(30);
+	header();
+	plan(4);
+
 	test_interval_sizeof();
 	test_interval_encode_decode();
 	test_interval_encode_decode_values_outside_int32_limits();
+	test_interval_validate();
+
+	footer();
 	return check_plan();
 }
