9d867a5291798709bf8ce6c65fc06586f36e4e7a
Andrey Saranchin <Andrey22102001@gmail.com>
2025-08-11 15:53:48 +0300

    box: return pointer to the actual tuple in tuple_encode
    A Lua helper `tuple_encode` encodes tuple on ibuf and returns its `rpos`
    and `wpos` as pointers to the tuple beginning and end. If the ibuf is not
    empty, the returned pointer to the tuple beginning will actually point
    to the previously encoded tuple. Now it doesn't break anything, but
    anyway such behavior is inconvinient, so let's fix this function and
    return pointer to the actual beginning of the encoded tuple.
    
    Along the way, rewrite `index:quantile()` in a simpler manner using the
    new function behavior.
    
    Part of #11221
    
    NO_TEST=refactoring
    NO_CHANGELOG=refactoring
    NO_DOC=refactoring
    
    (cherry picked from commit 260006b018f0b0d014ce649ddc96c378a5cbfdfa)

--- src/box/lua/tuple.lua
+++ src/box/lua/tuple.lua
@@ -112,6 +112,7 @@ local encode_array = msgpackffi.internal.encode_array
 local encode_r = msgpackffi.internal.encode_r
 
 local tuple_encode = function(tmpbuf, obj, level)
+    local used = tmpbuf:size()
     if obj == nil then
         encode_fix(tmpbuf, 0x90, 0)  -- empty array
     elseif is_tuple(obj) then
@@ -130,7 +131,7 @@ local tuple_encode = function(tmpbuf, obj, level)
         encode_fix(tmpbuf, 0x90, 1)  -- array of one element
         encode_r(tmpbuf, obj, 1, level and level + 1)
     end
-    return tmpbuf.rpos, tmpbuf.wpos
+    return tmpbuf.rpos + used, tmpbuf.wpos
 end
 
 local tuple_gc = function(tuple)
