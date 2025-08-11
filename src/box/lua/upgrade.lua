b16f3807a504ea5d0e9e49643c60d03a89096d17
Serge Petrenko <sergepetrenko@tarantool.org>
2025-08-11 18:04:04 +0300

    Generate changelog for 3.3.3
    Also remove unreleased/ entries and add new versions: 3.3.3, 3.2.2 to
    the downgrade versions list.
    
    While we're at it, remove old 3.0.0-alpha and -beta changelogs.
    
    NO_DOC=changelog
    NO_TEST=changelog
    NO_CHANGELOG=changelog

--- src/box/lua/upgrade.lua
+++ src/box/lua/upgrade.lua
@@ -2225,9 +2225,11 @@ local downgrade_versions = {
     "3.1.2",
     "3.2.0",
     "3.2.1",
+    "3.2.2",
     "3.3.0",
     "3.3.1",
     "3.3.2",
+    "3.3.3",
     -- DOWNGRADE VERSIONS END
 }
 
