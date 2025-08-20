80ab21c760009fa9b2d6feba466aba16ea462ee4
Georgiy Lebedev <g.lebedev@tarantool.org>
2025-08-20 21:45:27 +0200

    test: port test for #6198 to luatest
    To simplify the maintenance of the test for #6198 and to add a related
    test in the scope of #10033, let's port the original test to luatest.
    
    Needed for #10033
    
    NO_CHANGELOG=<testing infrastructure>
    NO_DOC=<testing infrastructure>

---
+++ test/app-luatest/max_cnt_of_tuple_fields_insert_overflow_err_msg_test.lua
@@ -0,0 +1,22 @@
+local t = require('luatest')
+
+local g = t.group()
+
+g.before_each(function()
+    t.tarantool.skip_if_not_debug()
+end)
+
+g.after_each(function()
+    box.error.injection.set('ERRINJ_TUPLE_FIELD_COUNT_LIMIT', -1)
+end)
+
+-- Checks that the overflow of the tuple field count limit returns an error
+-- (gh-6198).
+g.test_tuple_field_count_limit_overflow = function()
+    local tuple = box.tuple.new{0}
+    box.error.injection.set('ERRINJ_TUPLE_FIELD_COUNT_LIMIT', 1)
+    local msg = 'Tuple field count limit reached: see box.schema.FIELD_MAX'
+    t.assert_error_msg_equals(msg, function()
+        tuple:update{{'!', #tuple, 1}}
+    end)
+end
