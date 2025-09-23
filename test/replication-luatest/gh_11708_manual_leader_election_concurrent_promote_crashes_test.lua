f28dd28ad6abf4e32033098939a7099eb43fb0dc
Georgiy Lebedev <g.lebedev@tarantool.org>
2025-09-23 18:21:07 +0200

    box: forbid concurrent invocation of `box_raft_try_promote`
    Currently, we allow concurrent invocation of `box_raft_try_promote`, since
    we either disable the `is_in_promote` guard in `box_promote` or omit it in
    `box_cfg_xc`, while `box_raft_try_promote` yields to write the raft state.
    
    This leads to a race over the `diag` of the fiber executing the
    `box_raft_try_promote_f` trigger, and can lead to more bugs.
    
    Let's forbid concurrent execution of `box_raft_try_promote` by enabling the
    `is_in_promote` guard while `box_raft_try_promote` is called.
    
    One potential caveat is the concurrent execution of `box_promote_qsync`
    which is also guarded by the `is_in_promote`. However, it is executed in
    the raft worker fiber and can be retried until `box_raft_try_promote`
    execution finishes.
    
    To be on the safer side, let's:
    1. Call `raft_restore` before setting the fiber's `diag` to prevent any
        potential tampering with it from `raft->on_update` triggers.
    2. Set `is_box_configured` right before calling `box_raft_try_promote` to
       maintain the invariant that it is always called by `box.cfg` first. Also
       move `box_broadcast_ballot` for consistency.
    
    The `raft_leader_promote` test from the gh-6033 test group should now test
    that a concurrent promote fails rather than succeeds.
    
    Closes #11703
    Closes #11708
    
    NO_DOC=<bugfix>
    
    (cherry picked from commit b5928453962d84393d616c0b442cf7f4fedf0c07)

---
+++ test/replication-luatest/gh_11708_manual_leader_election_concurrent_promote_crashes_test.lua
@@ -0,0 +1,106 @@
+local t = require('luatest')
+local server = require('luatest.server')
+local replica_set = require('luatest.replica_set')
+
+local g = t.group()
+
+g.before_each(function(cg)
+    cg.replica_set = replica_set:new{}
+    cg.box_cfg = {
+        replication = {
+            server.build_listen_uri('server1', cg.replica_set.id),
+            server.build_listen_uri('server2', cg.replica_set.id),
+        },
+        replication_timeout = 0.1,
+    }
+end)
+
+g.after_each(function(cg)
+    cg.replica_set:drop()
+end)
+
+g.before_test('test_concurrent_promote_after_box_cfg', function(cg)
+    cg.box_cfg.bootstrap_strategy = 'auto'
+    cg.box_cfg.election_mode = 'off'
+    for i = 1, 2 do
+        local alias = 'server' .. i
+        cg[alias] = cg.replica_set:build_and_add_server{
+            alias = alias,
+            box_cfg = cg.box_cfg,
+        }
+    end
+    cg.replica_set:start()
+    cg.replica_set:wait_for_fullmesh()
+    for i = 1, 2 do
+        local alias = 'server' .. i
+        cg[alias]:exec(function()
+            box.cfg{election_mode = 'manual'}
+        end)
+    end
+end)
+
+-- Check that concurrent invocations of `box.ctl.promote` after `box.cfg` are
+-- handled correctly (gh-11708).
+g.test_concurrent_promote_after_box_cfg = function(cg)
+    cg.server1:exec(function()
+        local fiber = require('fiber')
+
+        local f = fiber.new(box.ctl.promote)
+        f:set_joinable(true)
+        local ok = pcall(box.ctl.promote)
+        t.assert(ok)
+        local err
+        ok, err = f:join()
+        t.assert_not(ok)
+        local msg = 'box.ctl.promote/demote does not support simultaneous ' ..
+                    'invocations'
+        t.assert_equals(err.message, msg)
+    end)
+end
+
+g.before_test('test_concurrent_promote_during_box_cfg', function(cg)
+    cg.box_cfg.bootstrap_strategy = 'supervised'
+    cg.box_cfg.election_mode = 'manual'
+    local run_before_cfg = [[
+        local fiber = require('fiber')
+
+        rawset(_G, "promote_ok", false)
+        rawset(_G, "promote_err", {})
+        fiber.new(function()
+            box.ctl.make_bootstrap_leader({graceful = true})
+            while box.info.status ~= 'running' do
+                fiber.yield()
+            end
+            local ok, err = pcall(box.ctl.promote)
+            _G.promote_ok = ok
+            _G.promote_err = err
+        end)
+    ]]
+    cg.server1 = cg.replica_set:build_and_add_server{
+        alias = 'server1',
+        box_cfg = cg.box_cfg,
+        env = {
+            ['TARANTOOL_RUN_BEFORE_BOX_CFG'] = run_before_cfg,
+        }
+    }
+    cg.server2 = cg.replica_set:build_and_add_server{
+        alias = 'server2',
+        box_cfg = cg.box_cfg,
+    }
+    cg.replica_set:start()
+    cg.replica_set:wait_for_fullmesh()
+end)
+
+-- Check that concurrent invocations of `box.ctl.promote` during `box.cfg` are
+-- handled correctly (gh-11703).
+g.test_concurrent_promote_during_box_cfg = function(cg)
+    cg.server1:exec(function()
+        t.assert_not(_G.promote_ok)
+        local msg = 'box.ctl.promote() is already running'
+        t.assert_equals(_G.promote_err.message, msg)
+        t.assert_equals(box.info.election.leader, box.info.id)
+        t.helpers.retrying({timeout = 120}, function()
+            t.assert_equals(box.info.synchro.queue.owner, box.info.id)
+        end)
+    end)
+end
