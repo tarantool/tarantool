f08a0683b67da9ac3decfa1f4aa148a09e0344c4
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2025-09-24 22:01:04 +0200

    box: move txn_persist_all_prepared() definition
    It is going to be needed higher in the file in the next commits.
    Lets move it, so the next commits would only change a small part
    in it instead of having a 100% diff it due to the move.
    
    In scope of #11754
    
    NO_DOC=refactoring
    NO_CHANGELOG=refactoring
    NO_TEST=refactoring

--- src/box/checkpoint.c
+++ src/box/checkpoint.c
@@ -47,6 +47,31 @@ txn_rollback_cb(struct trigger *trigger, void *event)
 }
 
 int
+txn_persist_all_prepared(struct vclock *out)
+{
+	/*
+	 * All the txns after preparation until all the journal write follow the
+	 * same path:
+	 * - The limbo volatile queue.
+	 * - The journal volatile queue.
+	 * - The journal write.
+	 *
+	 * Some steps might be skipped (for instance, the limbo might be if the
+	 * txn is force-async or just async and the limbo is empty). But the
+	 * order never changes.
+	 *
+	 * It means, that if one would want to closely follow the latest known
+	 * prepared txn until it reaches WAL, then following this path the
+	 * needed txn will be surely found before any new txn is added (except
+	 * for force-async, which might skip the volatile limbo queue and go
+	 * directly to the journal).
+	 */
+	if (txn_limbo_flush(&txn_limbo) != 0)
+		return -1;
+	return journal_sync(out);
+}
+
+int
 box_checkpoint_build_in_memory(struct box_checkpoint *out)
 {
 	struct txn_limbo *limbo = &txn_limbo;
@@ -162,29 +187,4 @@ out:
 	if (rc != 0)
 		engine_abort_checkpoint();
 	return rc;
-}
-
-int
-txn_persist_all_prepared(struct vclock *out)
-{
-	/*
-	 * All the txns after preparation until all the journal write follow the
-	 * same path:
-	 * - The limbo volatile queue.
-	 * - The journal volatile queue.
-	 * - The journal write.
-	 *
-	 * Some steps might be skipped (for instance, the limbo might be if the
-	 * txn is force-async or just async and the limbo is empty). But the
-	 * order never changes.
-	 *
-	 * It means, that if one would want to closely follow the latest known
-	 * prepared txn until it reaches WAL, then following this path the
-	 * needed txn will be surely found before any new txn is added (except
-	 * for force-async, which might skip the volatile limbo queue and go
-	 * directly to the journal).
-	 */
-	if (txn_limbo_flush(&txn_limbo) != 0)
-		return -1;
-	return journal_sync(out);
 }
