1509492c7f8ddc7b55ee84b54ca02c1ecf856f48
Temir Galeev <temir.galeev@bk.ru>
2025-11-20 23:51:46 +0300

    datetime: fix incorrect parsing %j and %m by strptime
    Ambiguous case where day of year (yday, which is defines
    calendar month and month day implicitly) and calendar month
    (without a month day) are both defined in the date text lead
    to error instead of crash with assertion fail.
    
    Addition inplace changes:
    - `tnt_strptime` implementation clarified using
      explicit `iso_8601_week_date` predicate
      & unification of `FLAG_YDAY` `FLAG_YEAR` flag checks.
    - MOD/DIV desc in `datetime.c` clarified.
    - some strptime tests with invalid date text added.
    - `TEST_RUN_PARAMS` fixed to allow arbitrary build dir:
      this fix `tnt-run.py` fail to locate the tarantool binary.
    
    Closes #11347
    See also #8525
    
    NO_DOC=bugfix

---
+++ test/app-tap/gh-11347-datetime-parse-crash.test.lua
@@ -0,0 +1,61 @@
+#!/usr/bin/env tarantool
+
+local tap = require("tap")
+local test = tap.test("errno")
+local date = require("datetime")
+
+test:plan(1)
+
+local function assert_raises(test, error_msg, func, ...)
+    local ok, err = pcall(func, ...)
+    local err_tail = err and err:gsub("^.+:%d+: ", "") or ""
+    return test:is(not ok and err_tail, error_msg,
+                   ("'%s' received, '%s' expected"):format(err_tail, error_msg))
+end
+
+local function error_could_not_parse_format(s, fmt)
+    return ("could not parse '%s' using '%s' format"):format(s, fmt)
+end
+
+--[[
+Test for bug fix #11347.
+
+Checks ambiguous case where day of year (yday, which is defines
+calendar month and month day implicitly) and calendar month
+without a day are both defined in the date text.
+
+Expects an error instead of assertion crash.
+]]
+test:test("Parse invalid string with a custom format", function(test)
+    local formats = {
+        -- gh-11347 reported case:
+        -- (month = 07, year = 00, day of year = 1)
+        -- Month of day of year 1 is 01,
+        -- but 07 is given.
+        {
+            fmt = "%m%g%j",
+            buf = "07001",
+        },
+        -- 2025-321 is 2025-11-17.
+        {
+            fmt = "%G-%j %m",
+            buf = "2025-321 01",
+        },
+        {
+            fmt = "%G-%j %m",
+            buf = "2025-321 10",
+        },
+        {
+            fmt = "%G-%j %m",
+            buf =  "2025-321 12",
+        },
+    }
+    test:plan(#formats)
+    for _, tc in pairs(formats) do
+        assert_raises(test,
+                      error_could_not_parse_format(tc.buf, tc.fmt),
+                      function() date.parse(tc.buf, {format = tc.fmt}) end)
+    end
+end)
+
+os.exit(test:check() and 0 or 1)
