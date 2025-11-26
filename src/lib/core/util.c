c6a1c7be90f2d2bce46accea9f316c0e056a78c6
Nikita Zheleztsov <n.zheleztsov@proton.me>
2025-11-26 10:27:58 +0300

    util: refactor json escape functions for different escaping
    This commit renames `json_escape` and `json_escape_inplace` to have more
    general name and allows passing escape function to them as an argument,
    so that the subsequent commits could introduce another type of string
    escaping, which will be used to make the syslog messages comply with RFC
    3164.
    
    Needed for #12006
    
    NO_TEST=refactoring
    NO_CHANGELOG=refactoring
    NO_DOC=refactoring

--- src/lib/core/util.c
+++ src/lib/core/util.c
@@ -256,14 +256,21 @@ const char *const json_char2escape[256] = {
 bool json_escape_forward_slash;
 TWEAK_BOOL(json_escape_forward_slash);
 
-int
-json_escape(char *buf, int size, const char *data)
+typedef const char *
+(*str_escape_char_f)(char c);
+
+/**
+ * Escape special characters in @a data to @a buf using custom escape rules,
+ * defined in @a escape_func.
+ */
+static int
+str_escape(char *buf, int size, const char *data, str_escape_char_f escape_func)
 {
 	int total = 0;
 	int data_len = strlen(data);
 	for (int i = 0; i < data_len; i++) {
 		char c = data[i];
-		const char *escstr = json_escape_char(c);
+		const char *escstr = escape_func(c);
 		if (escstr != NULL) {
 			SNPRINT(total, snprintf, buf, size, "%s", escstr);
 		} else {
@@ -274,7 +281,17 @@ json_escape(char *buf, int size, const char *data)
 }
 
 int
-json_escape_inplace(char *buf, int size)
+json_escape(char *buf, int size, const char *data)
+{
+	return str_escape(buf, size, data, json_escape_char);
+}
+
+/**
+ * Escape special characters in @a buf in-place using custom escape rules,
+ * defined in @a escape_func.
+ */
+static int
+str_escape_inplace(char *buf, int size, str_escape_char_f escape_func)
 {
 	/* We need at least one byte for the null terminator later. */
 	if (buf == NULL || size <= 0)
@@ -290,7 +307,7 @@ json_escape_inplace(char *buf, int size)
 	 */
 	for (char *p = buf; p < buf + size && *p != '\0'; p++) {
 		char c = *p;
-		const char *esc_str = json_escape_char(c);
+		const char *esc_str = escape_func(c);
 		int esc_len = esc_str != NULL ? strlen(esc_str) : 1;
 
 		if (total + esc_len >= size) {
@@ -310,7 +327,7 @@ json_escape_inplace(char *buf, int size)
 	int written = 0;
 	for (char *pr = buf + data_len - 1; pr >= buf; --pr) {
 		char c = *pr;
-		const char *esc_str = json_escape_char(c);
+		const char *esc_str = escape_func(c);
 
 		if (esc_str != NULL) {
 			int esc_len = strlen(esc_str);
@@ -329,6 +346,12 @@ json_escape_inplace(char *buf, int size)
 	assert(pw <= buf);
 	assert(written == total);
 	return written;
+}
+
+int
+json_escape_inplace(char *buf, int size)
+{
+	return str_escape_inplace(buf, size, json_escape_char);
 }
 
 const char *precision_fmts[] = {
