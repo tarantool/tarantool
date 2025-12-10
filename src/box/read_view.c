aa68f7b732d251ac66171197313cf437306d2652
Astronomax <fgfgfb93@gmail.com>
2025-12-10 13:16:09 +0300

    memtx: field_def_array shared ownership

--- src/box/read_view.c
+++ src/box/read_view.c
@@ -75,8 +75,7 @@ static void
 space_read_view_delete(struct space_read_view *space_rv)
 {
 	assert(space_rv->format == NULL);
-	if (space_rv->field_count > 0)
-		field_def_array_delete(space_rv->fields, space_rv->field_count);
+	field_def_array_unref(space_rv->fields, space_rv->field_count);
 	free(space_rv->format_data);
 	for (uint32_t i = 0; i <= space_rv->index_id_max; i++) {
 		struct index_read_view *index_rv = space_rv->index_map[i];
@@ -110,8 +109,8 @@ space_read_view_new(struct space *space, const struct read_view_opts *opts)
 	space_rv->id = space_id(space);
 	space_rv->group_id = space_group_id(space);
 	if (opts->enable_field_names && space->def->field_count > 0) {
-		space_rv->fields = field_def_array_dup(space->def->fields,
-						       space->def->field_count);
+		space_rv->fields = space->def->fields;
+		field_def_array_ref(space_rv->fields, space->def->field_count);
 		assert(space_rv->fields != NULL);
 		space_rv->field_count = space->def->field_count;
 	} else {
