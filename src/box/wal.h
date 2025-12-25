ec17d94ff67d7fc19395e1bb7d9fac133b4f2afa
Lev Kats <lev-katz@mail.ru>
2025-12-25 08:49:07 +0300

    recovery: fix big tuples blocking recovery
    Replace constant `WAL_ROWS_PER_YIELD` with `xlog_row_bytes_per_yield`
    tweak to ensure that the relay thread does not skip heartbeats while
    parsing large tuples, and does not yield too frequently. Introduce
    `ERRINJ_XLOG_READ_ROW_DELAY` to emulate the delay when parsing xlogs
    consisting of large rows.
    
    Fixes #12029
    
    NO_DOC=bugfix
    NO_CHANGELOG=bugfix

--- src/box/wal.h
+++ src/box/wal.h
@@ -62,15 +62,6 @@ enum wal_mode {
 	WAL_MODE_MAX
 };
 
-enum {
-	/**
-	 * Recovery yields once per that number of rows read and
-	 * applied from WAL. It allows not to block the event
-	 * loop for the whole recovery stage.
-	 */
-	WAL_ROWS_PER_YIELD = 1 << 15,
-};
-
 /** String constants for the supported modes. */
 extern const char *wal_mode_STRS[];
 
