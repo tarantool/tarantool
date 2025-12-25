f5df9b055b02ff03f153acfa18d9e73d6b7bedaf
Maksim Tiushev <mandesero@gmail.com>
2025-12-25 16:02:34 +0000

    config: add `skip_replication_names` compat option
    This patch fixes a bug when a Tarantool 3 instance configured via the
    declarative config could not join a Tarantool 2 replica set if the
    master snapshot did not contain instance and replica set names yet.
    
    To address this, introduce a new compat option, `skip_replication_names`,
    which controls instance and replica set name handling during replication
    startup. In the `old` mode, Tarantool skips applying `instance_name` and
    `replicaset_name` on startup and does not validate them against snapshot
    data. In the `new` mode, Tarantool preserves the previous behavior and
    applies and validates names on startup.
    
    Closes #10426
    
    NO_DOC=will added in separate PR
    
    (cherry picked from commit 4e6123983f67e26387a1808218e979c0bbdf222d)

--- src/lua/compat.lua
+++ src/lua/compat.lua
@@ -173,6 +173,15 @@ gc-checkpointing.
 https://tarantool.io/compat/replication_synchro_timeout
 ]]
 
+local SKIP_REPLICATION_NAMES_BRIEF = [[
+Skip applying and validating instance and replicaset names during replication
+startup. The old behavior allows a Tarantool 3.x instance configured via the
+declarative config to join a Tarantool 2.11 replicaset whose snapshot may not
+contain names yet.
+
+https://tarantool.io/compat/skip_replication_names
+]]
+
 -- Returns an action callback that toggles a tweak.
 local function tweak_action(tweak_name, old_tweak_value, new_tweak_value)
     return function(is_new)
@@ -316,6 +325,12 @@ local options = {
         brief = REPLICATION_SYNCHRO_TIMEOUT_COMPAT_BRIEF,
         action = tweak_action(
             'replication_synchro_timeout_rollback_enabled', true, false),
+    },
+    skip_replication_names = {
+        default = 'new',
+        obsolete = nil,
+        brief = SKIP_REPLICATION_NAMES_BRIEF,
+        action = function() end,
     },
 }
 
