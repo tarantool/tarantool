2cf4f27b8cf7f72599d68d992ca65b8bb23535ac
Nikolay Shirokovskiy <nshirokovskiy@tarantool.org>
2025-12-30 10:00:20 +0300

    misc: move arrow ipc implementation to EE
    In EE we are going to use arrow c++ library, so we drop nanoarrow
    dependency here we use precisely for ipc. CE now has only stubs.
    
    We also move arrow related functionality from core to box. We are going
    to use box ArrowError in EE for error reporting.
    
    Tests are moved to EE as they require `arrow_ipc_encode()` and
    `arrow_ipc_decode()` to work. Note that test in module_api.test will not
    be moved. There is no module_api.test in EE. Anyway C API
    `box_insert_arrow()` is tested in EE version in more details already.
    
    Part of tarantool/tarantool-ee#1603
    
    NO_CHANGELOG=internal
    NO_DOC=internal

--- src/trivia/config.h.cmake
+++ src/trivia/config.h.cmake
@@ -304,6 +304,7 @@
 #cmakedefine ENABLE_FAILOVER 1
 #cmakedefine ENABLE_INTEGRITY 1
 #cmakedefine ENABLE_FETCH_SNAPSHOT_CURSOR 1
+#cmakedefine ENABLE_ARROW_IPC 1
 
 #cmakedefine EXPORT_LIBCURL_SYMBOLS 1
 
