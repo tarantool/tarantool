889867a20aded271aaa56352c0292ab562e6ade1
Maksim Tiushev <mandesero@gmail.com>
2026-02-05 11:07:20 +0000

    metrics: metrics_export_role as part of tarantool
    NO_DOC=wip
    NO_CHANGELOG=wip
    NO_TEST=wip

---
+++ test/metrics-export-role-luatest/integration_error_test.lua
@@ -0,0 +1,6 @@
+require('test.metrics-export-role-luatest.helper')()
+
+-- The test needs to be reworked so that it reads the standard error
+-- from the log file. The test currently fails.
+-- t.assert_error(require(
+--     'third_party.metrics-export-role.test.integration.error_test'))
