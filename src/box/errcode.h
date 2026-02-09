ce64b10fbc248abfe48e9c553b15cc1f0ab4588b
Andrey Saranchin <Andrey22102001@gmail.com>
2026-02-09 18:44:29 +0300

    box: add new errcode for dictionary overflow
    It is needed for MemCS dictionary-encoded columns in Tarantool EE.
    
    Part of tarantool/tarantool-ee#969
    
    NO_CHANGELOG=new errcode
    NO_DOC=new errcode

--- src/box/errcode.h
+++ src/box/errcode.h
@@ -443,6 +443,7 @@ struct errcode_record {
 	_(ER_FIELD_IRREPRESENTABLE_VALUE, 296,	"The value does not fit in field type", "field", STRING, "value", MSGPACK) \
 	_(ER_KEY_PART_IRREPRESENTABLE_VALUE, 297, "The value of key part does not fit in type", "partno", UINT, "value", MSGPACK) \
 	_(ER_INVALID_SORTDATA_FILE, 298,	"Invalid MemTX sort data file", "path", STRING, "details", STRING) \
+	_(ER_DICT_OVERFLOW, 299,		"Dictionary has no space - too many unique values are used", "max_size", UINT) \
 	TEST_ERROR_CODES(_) /** This one should be last. */
 
 /*
