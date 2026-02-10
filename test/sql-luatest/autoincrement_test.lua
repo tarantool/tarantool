5f1f9e44b81ca8e90aedf5be514d18171bae00d9
eliza <eliza.pevts721@yandex.ru>
2026-02-10 22:54:54 +0300

    test: rework SQL test of #4256
    This patch converts the diff-test for #4256 to a luatest test.
    
    Part of #11989
    
    NO_DOC=tests
    NO_CHANGELOG=tests

--- test/sql-luatest/autoincrement_test.lua
+++ test/sql-luatest/autoincrement_test.lua
@@ -58,3 +58,29 @@ g.test_2981_check_autoinc = function(cg)
         box.func.check_t3_ck_unnamed_t3_1:drop()
     end)
 end
+
+--
+-- Make sure that when inserting, values are inserted in the given
+-- order when ephemeral space is used.
+--
+g.test_4256_do_not_change_order_during_insertion = function(cg)
+    cg.server:exec(function()
+        box.execute([[CREATE TABLE t (i INT PRIMARY KEY AUTOINCREMENT);]])
+
+        -- In order for this INSERT to use the ephemeral space, we created
+        -- this trigger.
+        box.execute([[CREATE TRIGGER r AFTER INSERT ON t
+                      FOR EACH ROW BEGIN SELECT 1; END;]])
+        local res = box.execute([[INSERT INTO t VALUES (1), (NULL), (10),
+                                  (NULL), (NULL), (3), (NULL);]])
+        local exp = {
+            autoincrement_ids = {2, 11, 12, 13},
+            row_count = 7,
+        }
+        t.assert_equals(res, exp)
+        res = box.execute([[SELECT * FROM t;]])
+        t.assert_equals(res.rows, {{1}, {2}, {3}, {10}, {11}, {12}, {13}})
+
+        box.execute([[DROP TABLE t;]])
+    end)
+end
