335e35885554934e66ae5cc71bb1b36730dd3a1a
Magomed Kostoev <m.kostoev@tarantool.org>
2026-02-10 19:51:48 +0300

    box/lua: introduce index:delete_range()
    Let's make it able to delete a data range using Lua API via index
    and space metatables. None of CE engines seem to support it in the
    visible future, so let's document and create a changelog in the EE
    repository.
    
    Part of #11335
    
    NO_DOC=ee
    NO_CHANGELOG=ee

--- test/box-luatest/delete_range_test.lua
+++ test/box-luatest/delete_range_test.lua
@@ -11,6 +11,17 @@ g.after_all(function(cg)
     cg.server:drop()
 end)
 
+g.after_each(function(cg)
+    cg.server:exec(function()
+        if box.space.memtx ~= nil then
+            box.space.memtx:drop()
+        end
+        if box.space.vinyl ~= nil then
+            box.space.vinyl:drop()
+        end
+    end)
+end)
+
 -- Test the IPROTO interface is not implemented.
 g.test_iproto = function(cg)
     cg.server:exec(function(net_box_uri)
@@ -46,4 +57,26 @@ g.test_iproto = function(cg)
         t.assert_equals(body[box.iproto.key.ERROR_24],
                         "Unknown request type 18")
     end, {cg.server.net_box_uri})
+end
+
+-- Test the feature does not work in CE engines.
+g.test_lua = function(cg)
+    cg.server:exec(function()
+        local space_memtx = box.schema.create_space('memtx', {engine = 'memtx'})
+        local space_vinyl = box.schema.create_space('vinyl', {engine = 'vinyl'})
+
+        space_memtx:create_index('pk')
+        space_vinyl:create_index('pk')
+
+        t.assert_error_msg_equals('memtx does not support range deletion',
+                                  space_memtx.delete_range, space_memtx, {}, {})
+        t.assert_error_msg_equals('memtx does not support range deletion',
+                                  space_memtx.index.pk.delete_range,
+                                  space_memtx.index.pk, {}, {})
+        t.assert_error_msg_equals('vinyl does not support range deletion',
+                                  space_vinyl.delete_range, space_vinyl, {}, {})
+        t.assert_error_msg_equals('vinyl does not support range deletion',
+                                  space_vinyl.index.pk.delete_range,
+                                  space_vinyl.index.pk, {}, {})
+    end)
 end
