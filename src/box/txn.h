9d4ce5660040ff7dd3d5103a9f1118ee432a84ee
Magomed Kostoev <m.kostoev@tarantool.org>
2026-02-27 15:04:54 +0300

    memtx: support multiple deletions in one statement in MVCC
    We used to maintain a linked list of statements that has deleted a
    story. This machinery assumes the amount of stories deleted by a
    statement is 1 or 0. Let's introduce a list of deleted stories in
    the `struct txn_stmt` instead. To keep the possibility to traverse
    over other statements that had deleted the story the list is made
    two-dimentional: one for the deleted story list (`next`) and the
    other for the deleting statement list (`next_del_stmt`).
    
    Needed-for #11335
    
    NO_CHANGELOG=internal
    
    @TarantoolBot document
    Title: new `box.stat.memtx.tx().mvcc.lists` member
    Since: 3.7
    Root document: https://www.tarantool.io/en/doc/latest/reference/reference_lua/box_stat/memtx/#box-stat-memtx-tx
    
    The new member shows the amount of memory used by the linked lists
    referencing the stories deleted by statements.

--- src/box/txn.h
+++ src/box/txn.h
@@ -300,6 +300,9 @@ struct txn_stmt_rollback_info {
 	struct tuple *new_tuple;
 };
 
+struct txn_stmt;
+struct memtx_del_story_link;
+
 /**
  * A single statement of a multi-statement
  * transaction: undo and redo info.
@@ -330,19 +333,16 @@ struct txn_stmt {
 	/**
 	 * If new_tuple == NULL and this transaction was not prepared,
 	 * this member holds added story of the old_tuple.
+	 *
+	 * If the statement is IPROTO_DELETE_RANGE, it holds all deleted
+	 * tuple stories (next != NULL if deleted more than one).
+	 *
+	 * Only one prepared TX can delete a tuple and a story. But when
+	 * there are several in-progress transactions and they delete the
+	 * same tuple we have to remember several delete statements for
+	 * one story. This is done in this list.
 	 */
-	struct memtx_story *del_story;
-	/**
-	 * Link in memtx_story::del_stmt linked list.
-	 * Only one prepared TX can delete a tuple and a story. But
-	 * when there are several in-progress transactions and they delete
-	 * the same tuple we have to store several delete statements in one
-	 * story. It's implemented in that way: story has a pointer to the first
-	 * deleting statement, that statement has a pointer to the next etc,
-	 * with NULL in the end.
-	 * That member is that the pointer to next deleting statement.
-	 */
-	struct txn_stmt *next_in_del_list;
+	struct memtx_del_story_link *del_stories;
 	/** Engine savepoint for the start of this statement. */
 	void *engine_savepoint;
 	/** Redo info: the binary log row */
