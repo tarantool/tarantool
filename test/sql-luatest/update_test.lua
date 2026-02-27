9ac1b1102804cc4669368d9bceae6ad0b189e1be
eliza <eliza.pevts721@yandex.ru>
2026-02-27 10:39:00 +0300

    test: rework SQL test for update with nested select
    This patch converts the diff-test for update with nested select
    to a luatest test.
    
    Part of #11989
    
    NO_DOC=tests
    NO_CHANGELOG=tests

--- test/sql-luatest/update_test.lua
+++ test/sql-luatest/update_test.lua
@@ -50,3 +50,29 @@ g.test_2251_multiple_update = function(cg)
         box.execute("DROP TABLE t2")
     end)
 end
+
+--
+-- Make sure that the nested SELECT statement within the UPDATE statement
+-- executes correctly.
+--
+g.test_update_with_nested_select = function(cg)
+    cg.server:exec(function ()
+        -- Create space.
+        box.execute([[CREATE TABLE t1(a INTEGER PRIMARY KEY,
+                                      b INT UNIQUE, e INT);]])
+
+        -- Seed entries.
+        box.execute("INSERT INTO t1 VALUES(1, 4, 6);")
+        box.execute("INSERT INTO t1 VALUES(2, 5, 7);")
+
+        -- Both entries must be updated.
+        local sql = "UPDATE t1 SET e = e + 1 WHERE b IN (SELECT b FROM t1);"
+        t.assert_equals(box.execute(sql), {row_count = 2})
+
+        -- Check.
+        t.assert_equals(box.execute("SELECT e FROM t1;").rows, {{7}, {8}})
+
+        -- Cleanup.
+        box.execute("DROP TABLE t1;")
+    end)
+end
