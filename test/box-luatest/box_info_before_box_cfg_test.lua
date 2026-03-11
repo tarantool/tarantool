425758c65221e01492cdebf83dc7c68d6ac9c3b8
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2026-03-11 19:06:11 +0100

    test: merge the tests for issues 7255 and 9173
    They were testing very close things. Keeping them separate looks a
    bit excessive. Especially given they will cover one more thing
    from a different ticket in the next commit.
    
    NO_DOC=refactoring
    NO_CHANGELOG=refactoring

--- test/box-luatest/box_info_before_box_cfg_test.lua
+++ test/box-luatest/box_info_before_box_cfg_test.lua
@@ -2,14 +2,19 @@ local t = require('luatest')
 
 local g = t.group()
 
-g.test_all_box_info = function()
+g.test_gh_7255_box_info_before_box_cfg = function()
+    local info = box.info()
+    t.assert_equals(info.status, "unconfigured")
+end
+
+g.test_gh_9173_all_box_info = function()
     -- Check that application won't be crashed
     for _, func in pairs(box.info()) do
         pcall(func)
     end
 end
 
-g.test_box_info_errors = function()
+g.test_gh_9173_box_info_errors = function()
     t.assert_error_msg_equals('Please call box.cfg{} first',
                               box.info.memory)
     t.assert_error_msg_equals('Please call box.cfg{} first',
