d164aafbd5e3fe73628e5686197b3e5fd0768067
NinaRodicova <nrodicova@gmail.com>
2026-03-17 21:32:23 +0300

    test: rework on conflict test
    This patch converts
    sql/on-conflict.test.lua into
    sql-luatest/index_test.lua,
    sql-luatest/update_test.lua,
    sql-luatest/insert_test.lua,
    sql-luatest/primary_key.lua
    
    Part of #11989
    
    NO_DOC=tests
    NO_CHANGELOG=test

---
+++ test/sql-luatest/insert_test.lua
@@ -0,0 +1,63 @@
+local server = require('luatest.server')
+local t = require('luatest')
+
+local g = t.group("insert", {{engine = 'memtx'}, {engine = 'vinyl'}})
+
+g.before_all(function(cg)
+    cg.server = server:new({alias = 'master'})
+    cg.server:start()
+    cg.server:exec(function(engine)
+        box.execute([[SET SESSION "sql_seq_scan" = true;]])
+        local sql = [[SET SESSION "sql_default_engine" = '%s';]]
+        box.execute(sql:format(engine))
+    end, {cg.params.engine})
+end)
+
+g.after_all(function(cg)
+    cg.server:drop()
+end)
+
+-- gh-3565: INSERT OR REPLACE causes assertion fault.
+g.test_3565_insert_replace_assertion_fault = function(cg)
+    cg.server:exec(function()
+        box.execute("CREATE TABLE tj (s1 INT PRIMARY KEY, s2 INT);")
+        box.execute("INSERT INTO tj VALUES (1, 2), (2, 3);")
+        box.execute("CREATE UNIQUE INDEX i ON tj (s2);")
+        box.execute("REPLACE INTO tj VALUES (1, 3);")
+
+        local res = box.execute("SELECT * FROM tj;")
+        t.assert_equals(res.rows, {{1, 3}})
+        box.execute("INSERT INTO tj VALUES (2, 4), (3, 5);")
+        box.execute("UPDATE OR REPLACE tj SET s2 = s2 + 1;")
+
+        local exp = {
+            {1, 4},
+            {3, 6},
+        }
+        res = box.execute("SELECT * FROM tj;")
+        t.assert_equals(res.rows, exp)
+
+        box.execute("DROP TABLE tj;")
+    end)
+end
+
+-- Several NOT NULL REPLACE constraints work.
+--
+g.test_several_not_null_replace_constraints_work = function(cg)
+    cg.server:exec(function()
+        local sql = "CREATE TABLE a (id INT PRIMARY KEY, a INT NOT NULL "..
+              "ON CONFLICT REPLACE DEFAULT 1, b INT NOT NULL ON "..
+              "CONFLICT REPLACE DEFAULT 2);"
+        box.execute(sql)
+        box.execute("INSERT INTO a VALUES(1, NULL, NULL);")
+        box.execute("INSERT INTO a VALUES(2, NULL, NULL);")
+
+        local exp = {
+            {1, 1, 2},
+            {2, 1, 2},
+        }
+        local res = box.execute("SELECT * FROM a;")
+        t.assert_equals(res.rows, exp)
+        box.execute("DROP TABLE a;")
+    end)
+end
