707d59be081fcfdce77c59ae8d2ee321e1fc7227
Georgiy Lebedev <g.lebedev@tarantool.org>
2026-03-22 13:49:35 +0100

    [WIP] memtx: return multikey results from `replace`
    Needed for #6385
    
    NO_CHANGELOG=<refactoring>
    NO_DOC=<refactoring>
    NO_TEST=<refactoring>

--- src/box/memtx_index.h
+++ src/box/memtx_index.h
@@ -34,6 +34,17 @@ static const struct memtx_index_entry memtx_index_entry_null = {
 	.hint = (hint_t)MULTIKEY_NONE,
 };
 
+/**
+ * Generalization of an index result/successor tuple for multikey index
+ * variants.
+ */
+struct memtx_index_replace_result {
+	/** Result value. */
+	struct memtx_index_entry entry;
+	/** Link in the list of index replace results. */
+	struct rlist link;
+};
+
 /** Virtual function table for memtx-specific index operations. */
 struct memtx_index_vtab {
 	/** Base index virtual table for common index operations. */
@@ -80,8 +91,28 @@ int
 memtx_index_replace_with_results(struct index *index, struct tuple *old_tuple,
 				 struct tuple *new_tuple,
 				 enum dup_replace_mode mode,
-				 struct tuple **result,
-				 struct tuple **successor);
+				 struct rlist *result, struct rlist *successor);
+
+/**
+ * Convert a result list to a single result tuple, assuming the result list
+ * comes from a non-multikey index replace.
+ */
+static inline struct tuple *
+memtx_index_replace_result_list_to_single_result(struct rlist *result_list)
+{
+	assert(!rlist_empty(result_list));
+	assert(rlist_first(result_list) == rlist_last(result_list));
+	return rlist_first_entry(result_list,
+				 struct memtx_index_replace_result,
+				 link)->entry.tuple;
+}
+
+/**
+ * Cleanup result list of replace after it is not needed.
+ */
+void
+memtx_index_replace_cleanup_result_list(struct index *index,
+					struct rlist *result_list);
 
 /**
  * Wrapper around `memtx_index_vtab::replace` that collects a single replaced
@@ -94,9 +125,16 @@ memtx_index_replace_with_single_result(struct index *index,
 				       enum dup_replace_mode mode,
 				       struct tuple **result)
 {
-	struct tuple *unused;
-	return memtx_index_replace_with_results(index, old_tuple, new_tuple,
-						mode, result, &unused);
+	struct rlist result_list;
+	struct rlist unused;
+	size_t region_svp = region_used(&fiber()->gc);
+	int rc = memtx_index_replace_with_results(index, old_tuple, new_tuple,
+						  mode, &result_list, &unused);
+	*result =
+		memtx_index_replace_result_list_to_single_result(&result_list);
+	memtx_index_replace_cleanup_result_list(index, &result_list);
+	region_truncate(&fiber()->gc, region_svp);
+	return rc;
 }
 
 /**
@@ -107,10 +145,22 @@ static inline int
 memtx_index_replace(struct index *index, struct tuple *old_tuple,
 		    struct tuple *new_tuple, enum dup_replace_mode mode)
 {
-	struct tuple *unused;
-	return memtx_index_replace_with_results(index, old_tuple, new_tuple,
-						mode, &unused, &unused);
+	struct rlist result;
+	struct rlist successor;
+	size_t region_svp = region_used(&fiber()->gc);
+	int rc = memtx_index_replace_with_results(index, old_tuple, new_tuple,
+						  mode, &result, &successor);
+	memtx_index_replace_cleanup_result_list(index, &result);
+	region_truncate(&fiber()->gc, region_svp);
+	return rc;
 }
+
+/**
+ * Rollback result of replace.
+ */
+void
+memtx_index_replace_rollback(struct index *index, struct tuple *new_tuple,
+			     struct rlist *result);
 
 static inline void
 memtx_index_begin_build(struct index *index)
