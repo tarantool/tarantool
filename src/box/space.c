855535da83c4ce6bd74be38f7cf92eddda05def5
Ilya Verbin <iverbin@tarantool.org>
2026-04-13 20:01:20 +0300

    applier: decode arrow ipc in applier to unload the tx thread
    Currently in case of replication the Arrow IPC data is decoded in
    the TX thread. This patch moves the decoding to the applier thread
    to achieve a more balanced load distribution.
    
    Needed for tarantool/tarantool-ee#1710
    
    NO_DOC=ee
    NO_TEST=ee
    NO_CHANGELOG=ee

--- src/box/space.c
+++ src/box/space.c
@@ -1378,8 +1378,7 @@ space_execute_insert_arrow(struct space *space, struct txn *txn,
 		tx_region_release(txn, TX_ALLOC_SYSTEM);
 		if (rc != 0)
 			goto eof;
-	} else {
-		assert(array == NULL);
+	} else if (array == NULL) {
 		assert(schema == NULL);
 		assert(request->arrow_ipc_end != NULL);
 		array = xregion_alloc_object(gc, struct ArrowArray);
@@ -1389,7 +1388,10 @@ space_execute_insert_arrow(struct space *space, struct txn *txn,
 		if (rc != 0)
 			goto eof;
 	}
-
+	/*
+	 * In case of replication the request may contain both IPC and in-memory
+	 * versions of the arrow data, which was decoded by the applier thread.
+	 */
 	rc = space->vtab->execute_insert_arrow(space, txn, array, schema);
 
 	if (array->release != NULL)
