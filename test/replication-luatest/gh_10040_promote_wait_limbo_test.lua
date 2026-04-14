bcfa4c3a2bebebae364813aca455ffdbbb34c29c
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2026-04-15 00:19:58 +0200

    limbo: fix infinite rw-waiting on failed promote
    In the end of successful election win box.ctl.promote() is waiting
    until the instance becomes writable. But sometimes it won't make
    sense even if the elections are won. Because during waiting the
    instance might stop being the leader. Then the waiting should be
    aborted.
    
    Follow up for #10040
    
    NO_DOC=bugfix
    NO_CHANGELOG=not released yet
    
    (cherry picked from commit f6d2be289458f632dc47b002a227c708850714c0)

--- test/replication-luatest/gh_10040_promote_wait_limbo_test.lua
+++ test/replication-luatest/gh_10040_promote_wait_limbo_test.lua
@@ -10,7 +10,7 @@ g.before_all(function(cg)
     local config = {
         election_mode = 'manual',
         replication = {master_uri, replica_uri},
-        replication_synchro_timeout = 30
+        replication_synchro_timeout = 1000
     }
     cg.master = cg.replica_set:build_and_add_server({
         alias = 'master',
@@ -43,14 +43,14 @@ g.test_promote_waits_for_quorum = function(cg)
     cg.replica:exec(function()
         local fiber = require('fiber')
         box.cfg{replication_synchro_quorum = 3}
-        t.helpers.retrying({timeout = 5}, function()
+        t.helpers.retrying({timeout = 60}, function()
             t.assert_gt(box.info.synchro.queue.len, 0)
         end)
         local prom_fiber = fiber.new(function()
             return pcall(box.ctl.promote)
         end)
         prom_fiber:set_joinable(true)
-        t.helpers.retrying({timeout = 5}, function()
+        t.helpers.retrying({timeout = 60}, function()
             t.assert_equals(box.info.election.state, 'leader')
         end)
         local is_finished, _ = prom_fiber:join(0.01)
@@ -60,4 +60,52 @@ g.test_promote_waits_for_quorum = function(cg)
         t.assert_not(err)
         t.assert_not(box.info.ro)
     end)
+    --
+    -- Cleanup.
+    --
+    cg.master:wait_for_vclock_of(cg.replica)
+    cg.master:exec(function()
+        box.cfg{replication_synchro_quorum = box.NULL}
+        box.ctl.promote()
+    end)
+    cg.replica:exec(function()
+        box.cfg{replication_synchro_quorum = box.NULL}
+    end)
+    cg.replica:wait_for_vclock_of(cg.master)
+end
+
+g.test_promote_aborts_wait_when_not_leader = function(cg)
+    cg.master:exec(function()
+        local fiber = require('fiber')
+        box.ctl.demote()
+        t.assert_equals(box.info.election.leader, 0)
+        box.cfg{read_only = true}
+        local is_promote_finished = false
+        local f = fiber.new(function()
+            local ok, err = pcall(box.ctl.promote)
+            is_promote_finished = true
+            return {ok, err}
+        end)
+        f:set_joinable(true)
+        t.helpers.retrying({timeout = 60}, function()
+            local info = box.info
+            t.assert_equals(info.election.leader, info.id)
+            t.assert_equals(info.election.term, info.synchro.queue.term)
+            t.assert_equals(info.synchro.queue.owner, info.id)
+        end)
+        t.assert(box.info.ro)
+        t.assert_not(is_promote_finished)
+        box.ctl.demote()
+        local ok, err = f:join(60)
+        t.assert(ok)
+        ok, err = unpack(err)
+        t.assert(err)
+        t.assert_not(ok)
+        --
+        -- Cleanup.
+        --
+        box.cfg{read_only = false}
+        box.ctl.promote()
+    end)
+    cg.replica:wait_for_vclock_of(cg.master)
 end
