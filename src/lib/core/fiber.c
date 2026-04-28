f27f0590e7f71193cef46a33d7a2bba98e3105b8
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2026-04-28 23:22:24 +0200

    fiber: introduce fiber_wakeup_trigger_cb()
    It is a callback for struct trigger to wake up a fiber stored in
    trigger->data.
    
    This use case appears a few times in the codebase. In 2 places it
    is covered by C++ lambdas, but one of them soon will move into C,
    making it more obvious that it is time to reuse the callback.
    
    In scope of #8095
    
    NO_DOC=refactoring
    NO_TEST=refactoring
    NO_CHANGELOG=refactoring
    
    (cherry picked from commit 89ddc3355e6762f4a3dd35c309ca5052af941913)

--- src/lib/core/fiber.c
+++ src/lib/core/fiber.c
@@ -2281,6 +2281,14 @@ fiber_free(void)
 	cord_destroy(&main_cord);
 }
 
+int
+fiber_wakeup_trigger_cb(struct trigger *trigger, void *event)
+{
+	(void)event;
+	fiber_wakeup(trigger->data);
+	return 0;
+}
+
 /**
  * True if fiber_signal_init was called.
  * Needed for re-entrancy of fiber signal initialization.
