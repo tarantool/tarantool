f6e8509f02b27fadf70bf0ff89f0cb2593973e79
Temir Galeev <temir.galeev@bk.ru>
2026-05-10 11:05:57 +0300

    pickle: fixed abort on pack with empty format or string
    `region_join()` and `region_alloc()` precondition of `size > 0`
    is handled now and `pack()` successfully returns in the `format = ''`
    or an empty string for the `'a'` format specifier cases.
    
    Fixes #12063
    
    NO_DOC=bugfix
    
    (cherry picked from commit 02b41622417dd2afba017e1f650c359d43f64046)

--- test/fuzz/lua/pickle_unpack_test.lua
+++ test/fuzz/lua/pickle_unpack_test.lua
@@ -9,9 +9,12 @@ local function TestOneInput(buf)
   local binary_string = fdp:consume_string(binary_string_len)
   local format_string_len = fdp:consume_integer(1, STRING_MAX_LEN)
   local format_string = fdp:consume_string(format_string_len)
-  local packed = pickle.pack(format_string,
-    pickle.unpack(format_string, binary_string))
-  assert(#packed == #binary_string)
+  local res = {pcall(pickle.unpack, format_string, binary_string)}
+  if res[1] then
+    table.remove(res, 1)
+    local packed = pickle.pack(format_string, unpack(res))
+    assert(#packed == #binary_string)
+  end
 end
 
 local args = {
