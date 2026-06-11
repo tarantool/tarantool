57819156f306725ef36689547afd55cbc3967a3d
Sergey Bronnikov <sergeyb@tarantool.org>
2026-06-11 15:23:28 +0300

    test/fuzz: fix test fails in pickle_unpack_test
    pickle_unpack_test uses the function `unpack()`, but `unpack()`
    has a limitation for a number unpacked elements [1]. The patch
    test whether a passed table is unpackable, if no the test goes to
    the next iteration.
    
    1. https://github.com/LuaJIT/LuaJIT/blob/18b087cd2cd4ddc4a79782bf155383a689d5093d/src/lib_base.c#L237-L239
    
    NO_CHANGELOG=testing
    NO_DOC=testing
    NO_TEST=testing

--- test/fuzz/lua/pickle_unpack_test.lua
+++ test/fuzz/lua/pickle_unpack_test.lua
@@ -3,6 +3,20 @@ local luzer = require("luzer")
 
 local STRING_MAX_LEN = 1e6
 
+local function err_handler(ignored_msgs)
+  return function(error_msg)
+    for _, ignored_msg in ipairs(ignored_msgs) do
+      local x, _ = string.find(error_msg, ignored_msg)
+      if x then break end
+    end
+  end
+end
+
+local unpack_ignored_msgs = {
+  "too many results to unpack",
+}
+local unpack_err_handler = err_handler(unpack_ignored_msgs)
+
 local function TestOneInput(buf)
   local fdp = luzer.FuzzedDataProvider(buf)
   local binary_string_len = fdp:consume_integer(1, STRING_MAX_LEN)
@@ -12,6 +26,10 @@ local function TestOneInput(buf)
   local res = {pcall(pickle.unpack, format_string, binary_string)}
   if res[1] then
     table.remove(res, 1)
+    -- Check for the error "too many results to unpack".
+    local ok = xpcall(unpack, unpack_err_handler, res)
+    if not ok then return end
+
     local packed = pickle.pack(format_string, unpack(res))
     assert(#packed == #binary_string)
   end
