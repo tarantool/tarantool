0f09c4027114e1591eb531dcdbf978c9fbdf0400
Sergey Bronnikov <sergeyb@tarantool.org>
2026-06-11 15:23:28 +0300

    test/fuzz: fix test fails in pickle_unpack_test
    pickle_unpack_test uses the function `unpack()`, but `unpack()`
    has a limitation for a number unpacked elements [1]. The patch
    checks that a table is unpackable and exits the function if an
    error occurs during unpacking.
    
    1. https://github.com/LuaJIT/LuaJIT/blob/18b087cd2cd4ddc4a79782bf155383a689d5093d/src/lib_base.c#L237-L239
    
    NO_CHANGELOG=testing
    NO_DOC=testing
    NO_TEST=testing

--- test/fuzz/lua/pickle_unpack_test.lua
+++ test/fuzz/lua/pickle_unpack_test.lua
@@ -12,6 +12,10 @@ local function TestOneInput(buf)
   local res = {pcall(pickle.unpack, format_string, binary_string)}
   if res[1] then
     table.remove(res, 1)
+    local ok, _ = pcall(unpack, res)
+    if not ok then
+      return
+    end
     local packed = pickle.pack(format_string, unpack(res))
     assert(#packed == #binary_string)
   end
