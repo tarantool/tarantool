1c6ed9d567ff370262a30ce3353049bf73f75b2b
Nikolay Shirokovskiy <nshirokovskiy@tarantool.org>
2026-06-17 17:34:23 +0300

    box: introduce recovery point API
    Closes #11729
    
    @TarantoolBot document
    Title: Document recovery point API
    Product: Tarantool
    Since: 3.8
    
    `box.backup.recovery_point.create(name)` is introduced to create
    recovery point. Recovery point is point in xlog to which one can
    truncate it to produce committed state at the time of point creation. The
    recovery point has `replica_id`, `lsn` pair which unambiguously specify
    a statement in xlog. The statement itself should be included on
    truncation.
    
    The result is table:
    
    ```
    tarantool> box.backup.recovery_point.create('point-1')
    ---
    - lsn: 46
      timestamp: 1782208661.7946
      name: 'point-1'
      replica_id: 1
    ...
    ```
    
    Fields are:
    
    - `name` - name given on creation, should be unique.
    - `timestamp` - Unix-time of recovery point. It is less than or equal to
      commit time of recovery point entry and larger than or equal to time
      of recovery point creation API call.
    - `replica_id` - replica_id of recovery point statement in xlog.
    - `lsn` - lsn of recovery point statement in xlog.
    
    Recovery points are persistent and replicated.
    
    New `box.backup.info().recovery_points` field shows all created recovery
    points that resides in xlogs of backup in progress.
    
    ```
    tarantool> box.backup.recovery_point.create('name-1')
    tarantool> box.backup.recovery_point.create('name-2')
    tarantool> box.backup.start()
    tarantool> box.backup.info().recovery_points
    ---
    - - lsn: 1
        timestamp: 1782208906.2023
        name: name-1
        replica_id: 1
      - lsn: 3
        timestamp: 1782208908.4776
        name: name-2
        replica_id: 1
    ...
    ```

---
+++ src/box/lua/backup.lua
@@ -0,0 +1,57 @@
+-- backup.lua (internal file)
+
+local fiber = require('fiber')
+local utils = require('internal.utils')
+
+local suid = function(f)
+    return function(...)
+        return box.session.su('admin', f, ...)
+    end
+end
+
+local api_trace = function(f)
+    return function(...)
+        return utils.call_at(1, f, ...)
+    end
+end
+
+box.internal.recovery_point_yield_loops = 1000
+
+box.backup.recovery_point = {}
+
+box.backup.info = function()
+    local info = box.internal.backup_info()
+    if info == nil then
+        return nil
+    end
+    local recovery_points = {}
+    local begin_vclock
+    assert(info.type == 'incremental' or
+           info.type == 'full', 'unexpected backup type')
+    if info.type == 'incremental' then
+        begin_vclock = info.prev_vclock
+    elseif info.type == 'full' then
+        begin_vclock = info.checkpoint_vclock
+    end
+    local loops = 1
+    for _, tuple in box.space._recovery_point.index.timestamp:pairs() do
+        local begin_lsn = begin_vclock[tuple.replica_id] or 0
+        local end_lsn = info.vclock[tuple.replica_id] or 0
+        if tuple.lsn ~= nil and
+           tuple.lsn > begin_lsn and
+           tuple.lsn <= end_lsn then
+            table.insert(recovery_points, tuple:tomap({names_only = true}))
+        end
+        if loops % box.internal.recovery_point_yield_loops == 0 then
+            fiber.yield()
+        end
+        loops = loops + 1
+    end
+    info.recovery_points = recovery_points
+    -- Clear internal field used only to filter recovery points.
+    info.checkpoint_vclock = nil
+    return info
+end
+
+box.backup.recovery_point.create = suid(box.internal.recovery_point_create)
+box.backup.recovery_point.create = api_trace(box.backup.recovery_point.create)
