a9e69108b7a20277291286f384cd2d7e92aa6748
Mergen Imeev <imeevma@tarantool.org>
2026-06-17 16:11:32 +0300

    sql: recalculate statement ID on collision
    Before this patch, there was a possibility that multiple different
    queries could share the same prepared statement ID. This could lead to
    lost prepared statements. This patch fixes this issue.
    
    Closes #12537
    
    NO_DOC=bugfix
    
    (cherry picked from commit 42706a28c768bbc04ebf9da0d9769a49657dad20)

--- test/sql-luatest/prepared_test.lua
+++ test/sql-luatest/prepared_test.lua
@@ -563,3 +563,70 @@ g.test_4825_make_sure_values_not_appear_next = function(cg)
         t.assert_equals(box.space.test, nil)
     end)
 end
+
+--
+-- gh-12537: make sure there is no collisions for prepared statement IDs.
+--
+g.test_12537_stmt_id_collision = function(cg)
+    cg.server:exec(function()
+        local HASH1 = 3135652232
+        local HASH2 = 3135652233
+
+        -- Make sure that both prepared statements would have the same ID
+        -- if there were no collisions.
+        local res = _G.prepare("SELECT 111 AS a /*xqiV8CaJLT*/")
+        t.assert_equals(res.stmt_id, HASH1)
+        _G.unprepare(HASH1)
+
+        res = _G.prepare("SELECT 222 AS b /*aJVtqcrWP5*/")
+        t.assert_equals(res.stmt_id, HASH1)
+        _G.unprepare(HASH1)
+
+        -- Make sure the IDs are different in case of a collision.
+        res = _G.prepare("SELECT 111 AS a /*xqiV8CaJLT*/")
+        t.assert_equals(res.stmt_id, HASH1)
+
+        res = _G.prepare("SELECT 111 AS a /*xqiV8CaJLT*/")
+        t.assert_equals(res.stmt_id, HASH1)
+
+        res = _G.prepare("SELECT 222 AS b /*aJVtqcrWP5*/")
+        t.assert_equals(res.stmt_id, HASH2)
+
+        res = _G.execute(HASH1)
+        t.assert_equals(res.rows, {{111}})
+
+        res = _G.execute(HASH2)
+        t.assert_equals(res.rows, {{222}})
+
+        -- It is now possible for the same query to be saved more than once.
+        _G.unprepare(HASH1)
+
+        res = _G.prepare("SELECT 222 AS b /*aJVtqcrWP5*/")
+        t.assert_equals(res.stmt_id, HASH1)
+
+        res = _G.execute(HASH1)
+        t.assert_equals(res.rows, {{222}})
+
+        res = _G.execute(HASH2)
+        t.assert_equals(res.rows, {{222}})
+
+        _G.unprepare(HASH1)
+        _G.unprepare(HASH2)
+    end)
+end
+
+g.test_12537_too_much_prepared_statements = function(cg)
+    t.tarantool.skip_if_not_debug()
+    cg.server:exec(function()
+        box.error.injection.set('ERRINJ_SQL_PREPARED_STATEMENT_ID_OVERFLOW',
+                                true)
+        local exp_err = {
+            message = "Failed to execute SQL statement: Number of prepare " ..
+                      "statements cannot be more than 4294967296",
+            name = "SQL_EXECUTE",
+        }
+        _G.prepare("SELECT 111 AS a /*xqiV8CaJLT*/")
+        t.assert_error_covers(exp_err, _G.prepare,
+                              "SELECT 222 AS b /*aJVtqcrWP5*/")
+    end)
+end
