tarantool/tarantool

commit c9daf9bf079ad33d7ee3da1c5f5f3c47411d34d1

cmake: reflect changes with CURL_DISABLE_CRYPTO_AUTH
The commit "lib: add ability to disable auths individually" [1][2] in
Curl 8.3.0 removes CURL_DISABLE_CRYPTO_AUTH and introduces CMake options
for a number crypto protocols.

The patch reflects this change in Tarantool's build infrastructure.

1. https://github.com/curl/curl/commit/e92edfbef64448ef461117769881f3ed776dec4e
2. https://github.com/curl/curl/pull/11490

Follows up #9086

NO_CHANGELOG=third_party
NO_DOC=third_party
NO_TEST=third_party

cmake/BuildLibCURL.cmake
@@ -89,7 +89,11 @@ macro(curl_build)
     # Additionaly disable some more protocols.
     list(APPEND LIBCURL_CMAKE_FLAGS "-DCURL_DISABLE_SMB=ON")
     list(APPEND LIBCURL_CMAKE_FLAGS "-DCURL_DISABLE_GOPHER=ON")
-    list(APPEND LIBCURL_CMAKE_FLAGS "-DCURL_DISABLE_CRYPTO_AUTH=ON")
+    list(APPEND LIBCURL_CMAKE_FLAGS "-DCURL_DISABLE_BASIC_AUTH=ON")
+    list(APPEND LIBCURL_CMAKE_FLAGS "-DCURL_DISABLE_BEARER_AUTH=ON")
+    list(APPEND LIBCURL_CMAKE_FLAGS "-DCURL_DISABLE_DIGEST_AUTH=ON")
+    list(APPEND LIBCURL_CMAKE_FLAGS "-DCURL_DISABLE_KERBEROS_AUTH=ON")
+    list(APPEND LIBCURL_CMAKE_FLAGS "-DCURL_DISABLE_AWS=ON")
 
     # Don't attempt to find system CA bundle/certificates at
     # libcurl configuration step (build time). Fallback to

commit 00dd91ff450bb83144ad4a7b00427b9d754a0134

build: enable basic/digest auth in libcurl build
Enabling the Basic auth fixes a regression introduced in commit
c9daf9bf079a ("cmake: reflect changes with CURL_DISABLE_CRYPTO_AUTH"),
where the Basic authorization mechanism was mistakenly disabled.

The linked commit attempts to follow libcurl's commit [1] ("lib: add
ability to disable auths individually"), where a generic
`CURL_DISABLE_CRYPTO_AUTH` build option was replaced by the following
ones:

* `CURL_DISABLE_DIGEST_AUTH`
* `CURL_DISABLE_KERBEROS_AUTH`
* `CURL_DISABLE_NEGOTIATE_AUTH`
* `CURL_DISABLE_AWS`

These ones were set to `ON`, which disables the corresponding auth
algorithms.

However, libcurl's commit also adds two new build options that allow
disabling functionality that was previously enabled unconditionally:

* `CURL_DISABLE_BASIC_AUTH`
* `CURL_DISABLE_BEARER_AUTH`

These options were set to `ON` in the linked tarantool commit as well.

As a result, tarantool's built-in http client is unable to handle the
Basic algorithm for authorization on a proxy or a target host. So, it is
enabled back by this commit.

What exactly is repaired?

* Authorization on a proxy server using the
  `https_proxy=http://user:pass@<...>` environment variable (and similar
  ones, see [2]).
* Authorization on a proxy server using the `proxy_user_pwd` option from
  the http client's Lua API.
* Authorization on a target server using the `https://user:pass@<...>`
  URL scheme.

Digest is also enabled by this commit, but the reason is different. We
can't use libcurl's Digest algorithm implementation with the built-in
http client neither for a proxy nor for a target host. Our http client
leaves `CURLOPT_HTTPAUTH` at its default, which is `CURLAUTH_BASIC`. It
doesn't add `CURLAUTH_DIGEST` to the client-side supported auth methods.

However, there is an smtp client (implemented as a separate module, see
[3]), which may use Digest once it is compiled into the library if the

cmake/BuildLibCURL.cmake
@@ -100,9 +100,9 @@ macro(curl_build)
     # Additionaly disable some more protocols.
     list(APPEND LIBCURL_CMAKE_FLAGS "-DCURL_DISABLE_SMB=ON")
     list(APPEND LIBCURL_CMAKE_FLAGS "-DCURL_DISABLE_GOPHER=ON")
-    list(APPEND LIBCURL_CMAKE_FLAGS "-DCURL_DISABLE_BASIC_AUTH=ON")
+    list(APPEND LIBCURL_CMAKE_FLAGS "-DCURL_DISABLE_BASIC_AUTH=OFF")
     list(APPEND LIBCURL_CMAKE_FLAGS "-DCURL_DISABLE_BEARER_AUTH=ON")
-    list(APPEND LIBCURL_CMAKE_FLAGS "-DCURL_DISABLE_DIGEST_AUTH=ON")
+    list(APPEND LIBCURL_CMAKE_FLAGS "-DCURL_DISABLE_DIGEST_AUTH=OFF")
     list(APPEND LIBCURL_CMAKE_FLAGS "-DCURL_DISABLE_KERBEROS_AUTH=ON")
     list(APPEND LIBCURL_CMAKE_FLAGS "-DCURL_DISABLE_NEGOTIATE_AUTH=ON")
     list(APPEND LIBCURL_CMAKE_FLAGS "-DCURL_DISABLE_AWS=ON")