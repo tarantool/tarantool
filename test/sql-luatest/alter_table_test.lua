f34aac34a3a75cd7ea6ad4f583a4ea29f9e4d709
Mergen Imeev <imeevma@tarantool.org>
2026-07-22 16:31:04 +0300

    sql: simplify `ALTER TABLE` rules
    This patch removes `alter_table_start`, `alter_add_constraint`,
    and `alter_add_column`, incorporating them into the corresponding
    `ALTER TABLE` rules. This changes the description and order of
    some errors, as the affected rules are now checked syntactically,
    not partially, but in their entirety, before being checked semantically.
    
    Note that some errors are unexpectedly not changed because `ADD` is
    not treated as a standard keyword.
    
    Part of #5485
    
    NO_DOC=no changes in behavior other than error descriptions

--- test/sql-luatest/alter_table_test.lua
+++ test/sql-luatest/alter_table_test.lua
@@ -87,3 +87,26 @@ g.test_13010_repeat_adding_constraint_column = function(cg)
         t.assert_equals(box.space.t:format()[2]['foreign_key'], exp)
     end)
 end
+
+-- gh-5485: Check changed errors for `ALTER TABLE` rules.
+g.test_alter_table_reserved_keyword_error = function(cg)
+    cg.server:exec(function()
+        box.execute([[CREATE TABLE t (id INT PRIMARY KEY, a INT);]])
+
+        local exp_err = "Syntax error at line 1 near 'REFERENCES'"
+        local queries = {
+            [[ALTER TABLE t ADD COLUMN REFERENCES INT;]],
+            [[ALTER TABLE t ADD REFERENCES INT;]],
+            [[ALTER TABLE t ADD CONSTRAINT fk FOREIGN KEY REFERENCES t(id);]],
+            [[ALTER TABLE t ADD CONSTRAINT ck CHECK REFERENCES;]],
+            [[ALTER TABLE t ADD CONSTRAINT uq UNIQUE REFERENCES;]],
+            [[ALTER TABLE t ADD CONSTRAINT pk PRIMARY KEY REFERENCES;]],
+        }
+        for _, query in ipairs(queries) do
+            local _, err = box.execute(query)
+            t.assert_equals(err.message, exp_err)
+        end
+
+        box.space.t:drop()
+    end)
+end
