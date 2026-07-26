f3eb0426b31dfd05719c7e30abc29697a796b9d0
Mergen Imeev <imeevma@gmail.com>
2026-07-26 21:02:27 +0300

    sql: create AST for UPDATE statement
    This patch introduces AST for UPDATE statement.
    
    Also, syntax errors now appear before semantic errors in the UPDATE
    statement.
    
    Part of #5485
    
    NO_DOC=change of error description
    NO_CHANGELOG=will be added in the last commit of the series

--- test/sql-luatest/parser_test.lua
+++ test/sql-luatest/parser_test.lua
@@ -86,5 +86,8 @@ g.test_syntax_errors = function(cg)
 
         _, err = box.execute([[INSERT INTO t VALUES (1) 3;]])
         t.assert_equals(err.message, "Syntax error at line 1 near '3'")
+
+        _, err = box.execute([[UPDATE t SET i = 1 4;]])
+        t.assert_equals(err.message, "Syntax error at line 1 near '4'")
     end)
 end
