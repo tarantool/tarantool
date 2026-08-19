850f1191be343445d1e4d893354b381f9f223b96
Mergen Imeev <imeevma@tarantool.org>
2026-08-19 17:29:34 +0300

    sql: remove `parse_only` field from `struct Parse`
    In this patch, the `parse_only` field has been removed from
    the `struct Parse`, as we no longer use this flag.
    
    Part of #5485
    
    NO_DOC=refactoring
    NO_TEST=refactoring
    NO_CHANGELOG=refactoring

--- src/box/sql/prepare.c
+++ src/box/sql/prepare.c
@@ -118,7 +118,6 @@ sql_expr_compile(const char *sql)
 
 	struct Parse parser;
 	sql_parser_create(&parser, SQL_DEFAULT_FLAGS);
-	parser.parse_only = true;
 	struct Expr *expr = sql_parse_function(&parser, sql);
 	sql_parser_destroy(&parser);
 	return expr;
@@ -131,7 +130,6 @@ sql_view_compile(const char *sql)
 
 	struct Parse parser;
 	sql_parser_create(&parser, SQL_DEFAULT_FLAGS);
-	parser.parse_only = true;
 	struct Select *res = sql_parse_view(&parser, sql);
 	sql_parser_destroy(&parser);
 	return res;
@@ -148,7 +146,6 @@ sql_trigger_compile(const char *sql)
 
 	struct Parse parser;
 	sql_parser_create(&parser, SQL_DEFAULT_FLAGS);
-	parser.parse_only = true;
 	struct sql_trigger *res = sql_parse_trigger(&parser, sql);
 	sql_parser_destroy(&parser);
 	return res;
@@ -219,7 +216,6 @@ void
 sql_parser_destroy(Parse *parser)
 {
 	assert(parser != NULL);
-	assert(!parser->parse_only || parser->pVdbe == NULL);
 	sqlVdbeDelete(parser->pVdbe);
 	parser_space_delete(parser);
 	while (parser->pTriggerPrg != NULL) {
