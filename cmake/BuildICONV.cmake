7693ec71d01b26654586365b9790198759fde558
Sergey Bronnikov <sergeyb@tarantool.org>
2026-09-18 16:39:47 +0000

    cmake: fix a typo in BuildICONV.cmake
    Follows up the commit 98b38e89ba0545543b85aa6f164f66309c2343c4
    ("cmake: allow to bundle static dependencies in main project").
    
    NO_CHANGELOG=build
    NO_DOC=build
    NO_TEST=build
    NO_DEPS_UPDATE
    
    (cherry picked from commit 5357a495e880104dea24676e0a28af118f8d71ee)

--- cmake/BuildICONV.cmake
+++ cmake/BuildICONV.cmake
@@ -17,7 +17,7 @@ ExternalProject_Add(bundled-iconv-project
     SOURCE_DIR ${ICONV_INSTALL_DIR}/src/iconv
     BINARY_DIR ${ICONV_INSTALL_DIR}/src/iconv-build
     STAMP_DIR ${ICONV_INSTALL_DIR}/src/iconv-stamp
-    URL URL ${BACKUP_STORAGE}/libiconv/libiconv-${ICONV_VERSION}.tar.gz
+    URL ${BACKUP_STORAGE}/libiconv/libiconv-${ICONV_VERSION}.tar.gz
     URL_MD5 ${ICONV_HASH}
     CONFIGURE_COMMAND <SOURCE_DIR>/configure
         CC=${CMAKE_C_COMPILER}
